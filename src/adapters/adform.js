0269ffa7c1f5200bb504b0597bd4c75828b1a50b
Jurij Sinickij <jurij.sinickij@adform.com>
2015-12-18 12:17:14 +0200

    single request for all tags

--- src/adapters/adform.js
+++ src/adapters/adform.js
@@ -10,23 +10,33 @@ function AdformAdapter() {
     };
 
     function _callBids(params) {
-        var callbackName, bid;
+        var callbackName = '_adf_' + utils.getUniqueIdentifierStr(), bid, noDomain = true;
         var bids = params.bids;
+        var request = [];
 
         for (var i = 0, l = bids.length; i < l; i++) {
             bid = bids[i];
-            callbackName = '_adf_' + utils.getUniqueIdentifierStr();
-
-            pbjs[callbackName] = handleCallback(bid);
-            adloader.loadScript(formRequestUrl(bid.params, callbackName));
+            if (bid.adxDomain && noDomain) {
+                noDomain = false;
+                request.unshift('//' + bid.adxDomain + '/adx/?rp=4');
+            }
+            request.push(formRequestUrl(bid.params));
         }
+
+        if (noDomain) {
+            request.unshift('//adx.adform.net/adx/?rp=4');
+        }
+
+        pbjs[callbackName] = handleCallback(bids);
+        request.push('callback=pbjs.' + callbackName);
+
+        adloader.loadScript(request.join('&'));
     }
 
-    function formRequestUrl(reqData, callbackName) {
-
+    function formRequestUrl(reqData) {
         var key;
-        var url = [ '//', reqData.adxDomain || 'adx.adform.net', '/adx/?' ];
-        
+        var url = [];
+
         var validProps = [
             'mid', 'inv', 'pdom', 'mname', 'mkw', 'mkv', 'cat', 'bcat', 'bcatrt', 'adv', 'advt', 'cntr', 'cntrt', 'maxp',
             'minp', 'sminp', 'w', 'h', 'pb', 'pos', 'cturl', 'iturl', 'cttype', 'hidedomain', 'cdims', 'test'
@@ -38,33 +48,90 @@ function AdformAdapter() {
                 url.push(key, '=', reqData[key], '&');
         }
 
-        url.push('callback=pbjs.', callbackName);
-
-        return url.join('');
+        return encode64(url.join(''));
     }
 
-    function handleCallback(bid) {
-        return function handleResponse(adItem) {
-            var bidObject, bidder = 'adform';
-
-            if (adItem && adItem.response == 'banner') {
-                bidObject = bidfactory.createBid(1);
-                bidObject.bidderCode = bidder;
-                bidObject.cpm = parseInt(adItem.win_bid, 10);
-                bidObject.cur = adItem.win_cur;
-                bidObject.ad = adItem.banner;
-                bidObject.width = adItem.width;
-                bidObject.height = adItem.height;
-
-                bidmanager.addBidResponse(bid.placementCode, bidObject);
-            } else {
-                bidObject = bidfactory.createBid(2);
-                bidObject.bidderCode = bidder;
-
-                bidmanager.addBidResponse(bid.placementCode, bidObject);
+    function handleCallback(bids) {
+        return function handleResponse(adItems) {
+            var bidObject, bidder = 'adform', adItem, bid;
+            for(var i = 0, l = adItems.length; i < l; i++){
+                adItem = adItems[i];
+                bid = bids[i];
+                if (adItem && adItem.response == 'banner') {
+                    bidObject = bidfactory.createBid(1);
+                    bidObject.bidderCode = bidder;
+                    bidObject.cpm = parseInt(adItem.win_bid, 10);
+                    bidObject.cur = adItem.win_cur;
+                    bidObject.ad = adItem.banner;
+                    bidObject.width = adItem.width;
+                    bidObject.height = adItem.height;
+                    bidmanager.addBidResponse(bid.placementCode, bidObject);
+                } else {
+                    bidObject = bidfactory.createBid(2);
+                    bidObject.bidderCode = bidder;
+                    bidmanager.addBidResponse(bid.placementCode, bidObject);
+                }
             }
         };
     }
+
+    function encode64(input) {
+        var out = [];
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
+        var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_=";
+
+        input = utf8_encode(input);
+
+        while (i < input.length) {
+
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+            out.push(_keyStr.charAt(enc1), _keyStr.charAt(enc2));
+            if (enc3 != 64)
+                out.push(_keyStr.charAt(enc3));
+            if (enc4 != 64)
+                out.push(_keyStr.charAt(enc4));
+        }
+
+        return out.join('');
+    }
+
+    function utf8_encode(string) {
+        string = string.replace(/\r\n/g, "\n");
+        var utftext = "";
+
+        for (var n = 0; n < string.length; n++) {
+
+            var c = string.charCodeAt(n);
+
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            } else if ((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            } else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+        }
+
+        return utftext;
+    }
+
 }
 
 module.exports = AdformAdapter;
