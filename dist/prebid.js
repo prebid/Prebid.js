2dcaec3f8667760a7a65af7e4d1de50ba7552304
Tom Riley <tom@coull.com>
2016-01-20 13:50:41 +0000

    Fixed Chrome issue where no req for pubmatic js. Hardcoded proto to https

--- dist/prebid.js
+++ dist/prebid.js
@@ -1,5 +1,5 @@
-/* Prebid.js v0.5.0 
-Updated : 2016-01-11 */
+/* prebid.js v0.5.0 
+Updated : 2016-01-20 */
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /** @module adaptermanger */
 
@@ -1178,7 +1178,7 @@ var PubmaticAdapter = function PubmaticAdapter() {
 			return "'" + adSlot + "'";
 		}).join(',');
 
-		content += '<scr' + 'ipt src="//ads.pubmatic.com/AdServer/js/gshowad.js"></scr' + 'ipt>';
+		content += '<scr' + 'ipt src="https://ads.pubmatic.com/AdServer/js/gshowad.js"></scr' + 'ipt>';
 		content += '<scr' + 'ipt>';
 		content += 'window.parent.pbjs.handlePubmaticCallback({progKeyValueMap: progKeyValueMap, bidDetailsMap: bidDetailsMap})';
 		content += '</scr' + 'ipt>';
