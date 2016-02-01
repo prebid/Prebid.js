67df153a5279217d7ba92c1f8835a75b0d87676b
David Babaioff <davidbabaioff@gmail.com>
2016-02-01 17:43:37 +0200

    Avoid global variable

--- src/adapters/sovrn.js
+++ src/adapters/sovrn.js
@@ -62,7 +62,7 @@ var SovrnAdapter = function SovrnAdapter() {
 					adW=bid.sizes[0][0];
 					adH=bid.sizes[0][1];
 				}
-			imp =
+			var imp =
 				{
 					id: utils.getUniqueIdentifierStr(),
 					banner: {
