7be91713557f3bc0db66be0b828049d6cfddbbeb
Matt Kendall <emailmatthere@gmail.com>
2016-02-09 11:39:37 -0500

    Remove latency tracking from appnexus.

--- src/adapters/appnexus.js
+++ src/adapters/appnexus.js
@@ -9,38 +9,6 @@ var AppNexusAdapter = function AppNexusAdapter() {
 	var baseAdapter = Adapter.createNew('appnexus');
 	var isCalled = false;
 
-	//time tracking buckets, to be used to track latency within script
-	//array index is timeslice in ms, value passed to buildTrackingTag() is impbus tracker id
-	var timeTrackingBuckets = [];
-	timeTrackingBuckets[100] = buildTrackingTag(21139);
-	timeTrackingBuckets[200] = buildTrackingTag(21140);
-	timeTrackingBuckets[300] = buildTrackingTag(21141);
-	timeTrackingBuckets[400] = buildTrackingTag(21142);
-	timeTrackingBuckets[500] = buildTrackingTag(21143);
-	timeTrackingBuckets[600] = buildTrackingTag(21144);
-	timeTrackingBuckets[700] = buildTrackingTag(21145);
-	timeTrackingBuckets[800] = buildTrackingTag(21146);
-	timeTrackingBuckets[1000] = buildTrackingTag(21147);
-	timeTrackingBuckets[1300] = buildTrackingTag(21148);
-	timeTrackingBuckets[1600] = buildTrackingTag(21149);
-	timeTrackingBuckets[2000] = buildTrackingTag(21150);
-	timeTrackingBuckets[5000] = buildTrackingTag(21151);
-	timeTrackingBuckets[10000] = buildTrackingTag(21152);
-
-	//over 10.000 tracker
-	var timeTrackerOverMaxBucket = buildTrackingTag(21154);
-	//var timeTrackerBidTimeout = buildTrackingTag(19432);
-
-	//generic bid requeted tracker
-	var timeTrackerBidRequested = buildTrackingTag(21153);
-
-	// var timeTrackerBidRequested = buildTrackingTag(19435);
-
-	//helper function to construct impbus trackers
-	function buildTrackingTag(id) {
-		return 'https://secure.adnxs.com/imptr?id=' + id + '&t=2';
-	}
-
 	baseAdapter.callBids = function(params){
 	 	var bidCode = baseAdapter.getBidderCode();
 
@@ -58,33 +26,6 @@ var AppNexusAdapter = function AppNexusAdapter() {
 			bidmanager.pbCallbackMap[callbackId] = bidReqeust;
 		}
 	};
-
-		//given a starttime and an end time, hit the correct impression tracker
-	function processAndTrackLatency(startTime, endTime, placementCode) {
-
-		if (startTime && endTime) {
-			//get the difference between times
-			var timeDiff = endTime - startTime;
-			var trackingPixelFound = false;
-			var trackingUrl = '';
-			for (var curTrackerItem in timeTrackingBuckets) {
-				//find the closest upper bound of defined tracking times
-				if (timeDiff <= curTrackerItem) {
-					trackingPixelFound = true;
-					trackingUrl = timeTrackingBuckets[curTrackerItem];
-					adloader.trackPixel(trackingUrl);
-					break;
-				}
-			}
-			//if we didn't find a bucket, assume use the catch-all time over bucket
-			if (!trackingPixelFound) {
-				trackingUrl = timeTrackerOverMaxBucket;
-				adloader.trackPixel(trackingUrl);
-			}
-
-			utils.logMessage('latency for placement code : ' + placementCode + ' : ' + timeDiff + ' ms.' + ' Tracking URL Fired : ' + trackingUrl);
-		}
-	}
 
 
 	function buildJPTCall(bid, callbackId) {
@@ -205,12 +146,6 @@ var AppNexusAdapter = function AppNexusAdapter() {
 				placementCode = bidObj.placementCode;
 				//set the status
 				bidObj.status = CONSTANTS.STATUS.GOOD;
-				//track latency
-				try {
-					processAndTrackLatency(bidObj.startTime, new Date().getTime(), placementCode);
-				} catch (e) {}
-
-				//place ad response on bidmanager._adResponsesByBidderId
 			}
 
 			// @if NODE_ENV='debug'
