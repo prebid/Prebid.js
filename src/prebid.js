4292732e2c3b96e2b52ce53dab7f633fc9b1e3e8
Michael Miller <mike@spanishdict.com>
2016-04-14 15:31:09 -0400

    Remove unneeded variable

--- src/prebid.js
+++ src/prebid.js
@@ -325,8 +325,7 @@ function resetBids() {
   pb_bidsTimedOut = false;
 }
 
-function requestAllBids(tmout) {
-  var timeout = tmout;
+function requestAllBids(timeout) {
   resetBids();
   init(timeout);
 }
