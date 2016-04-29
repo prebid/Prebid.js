d1ddc4f063ce7b13debe78e14e2886bdec879f93
astudnicky <astudnicky@sonobi.com>
2016-04-29 16:12:23 -0400

    Update Sonobi adapter to accommodate single-size arrays
    Output errors for adops integrators
    
    Do not cancel bidding without sizes

--- src/adapters/sonobi.js
+++ src/adapters/sonobi.js
@@ -4,7 +4,7 @@ var adloader = require('../adloader.js');
 var utils = require('../utils');
 
 var SonobiAdapter = function SonobiAdapter(){
-  var test = false;               //  tag tester = true || false
+  var test = false;                       //  tag tester = true || false
   var cb_map = {};
 
   function _phone_in(params){
@@ -13,36 +13,51 @@ var SonobiAdapter = function SonobiAdapter(){
     adloader.loadScript(trinity + JSON.stringify(_keymaker(bids)) + '&cv=' + _operator(), null);
   }
 
-  function _keymaker(bids){       //  Make keys
+  function _keymaker(bids){               //  Make keys
     var keyring = {};
     utils._each(bids, function(o){
-      var sizes = [];
-      utils._each(o.sizes, function(size){
-        sizes.push(size.join('x'));
-      });
-      sizes = sizes.toString();
-      if (!!o.params.ad_unit && o.params.ad_unit.length > 0) {
-        //  Cypher
-        keyring[o.params.ad_unit + '|' + o.params.dom_id] = sizes;
-        cb_map[o.params.ad_unit + '|' + o.params.dom_id] = o.placementCode;
-      } else if (!!o.params.placement_id && o.params.placement_id.length > 0) {
-        //  Morpheus
-        keyring[o.params.dom_id] = o.params.placement_id + (test ? '-test' : '')  + '|' + sizes;
-        cb_map[o.params.dom_id] = o.placementCode;
-      } else {
-        utils.logError('sonobi unable to bid: Improper parameters for ' + o.placementCode);
+      var sizes = utils.parseSizesInput(o.sizes).toString();
+      if (utils.isEmpty(sizes)){
+        utils.logWarn('Sonobi adapter expects sizes for ' + o.placementCode);
+      }
+      switch(true){
+        case (!o.params.ad_unit && !o.params.placement_id):
+          utils.logError('Sonobi unable to bid: Missing parameters for ' + o.placementCode);
+          break;
+        case (!!o.params.ad_unit && !!o.params.placement_id):
+          utils.logError('Sonobi unable to bid: Extra parameters for ' + o.placementCode);
+          break;
+        case (!!o.params.ad_unit && o.params.ad_unit.length === 0):
+          utils.logError('Sonobi unable to bid: Empty ad_unit for ' + o.placementCode);
+          break;
+        case (!!o.params.placement_id && o.params.placement_id.length === 0):
+          utils.logError('Sonobi unable to bid: Empty placement_id for ' + o.placementCode);
+          break;
+        case (!!o.params.placement_id):     //  Morpeus style 
+          keyring[o.params.dom_id] = o.params.placement_id + (test ? '-test' : '')  + '|' + sizes;
+          cb_map[o.params.dom_id] = o.placementCode;
+          break;
+        case (!!o.params.ad_unit && o.params.ad_unit.charAt(0) !== '/'):
+          //  DFP docs do not necessarily require leading slash? - add it in if it's not there.
+          o.params.ad_unit = '/' + o.params.ad_unit;
+        case (!!o.params.ad_unit):          // Cypher style
+          keyring[o.params.ad_unit + '|' + o.params.dom_id] = sizes;
+          cb_map[o.params.ad_unit + '|' + o.params.dom_id] = o.placementCode;
+          break;
+        default:                            // I don't know how it's broken, but it is.
+          utils.logError('Sonobi unable to bid: Improper parameters for ' + o.placementCode);
       }
     });
     return keyring;
   }
 
-  function _operator(){           //  Uniqify callbacks
+  function _operator(){                     //  Uniqify callbacks
     var uniq = "cb" + utils.getUniqueIdentifierStr();
     window[uniq] = _trinity;
     return uniq;
   }
 
-  function _trinity(response){    //  Callback
+  function _trinity(response){              //  Callback
     var slots = response.slots || {};
     var sbi_dc = response.sbi_dc || '';
     var bidObject = {};
@@ -55,7 +70,7 @@ var SonobiAdapter = function SonobiAdapter(){
         bidObject.width = Number(slots[slot].sbi_size.split('x')[0]);
         bidObject.height = Number(slots[slot].sbi_size.split('x')[1]);
         bidmanager.addBidResponse(cb_map[slot], bidObject);
-      } else {                      //  No aid? No ad.
+      } else {                              //  No aid? No ad.
         bidObject = bidfactory.createBid(2);
         bidObject.bidderCode = 'sonobi';
         bidmanager.addBidResponse(cb_map[slot], bidObject);
