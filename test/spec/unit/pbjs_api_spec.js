965a07e83a8a41a8ac71832f1c501c8800f69b7a
protonate <ncozi@appnexus.com>
2016-05-10 14:59:08 -0700

    comment API methods and related tests

--- test/spec/unit/pbjs_api_spec.js
+++ test/spec/unit/pbjs_api_spec.js
@@ -21,7 +21,15 @@ var Slot = function Slot(elementId, pathId) {
     },
 
     setTargeting: function setTargeting(key, value) {
-        }
+    },
+
+    getTargeting: function getTargeting() {
+      return 'targeting';
+    },
+
+    getTargetingKeys: function getTargetingKeys() {
+      return [{ testKey: ['a test targeting value'] }];
+    }
   };
   slot.spySetTargeting = sinon.spy(slot, 'setTargeting');
   return slot;
@@ -58,66 +66,66 @@ after(function () {
 });
 
 describe('Unit: Prebid Module', function () {
-  describe('getAdserverTargetingForAdUnitCodeStr', function () {
-    it('should return targeting info as a string', function () {
-      var result = pbjs.getAdserverTargetingForAdUnitCodeStr(config.adUnitCodes[0]);
-      assert.equal(result, targetingString, 'returns expected string of ad targeting info');
-    });
+  //describe('getAdserverTargetingForAdUnitCodeStr', function () {
+  //  it('should return targeting info as a string', function () {
+  //    var result = pbjs.getAdserverTargetingForAdUnitCodeStr(config.adUnitCodes[0]);
+  //    assert.equal(result, targetingString, 'returns expected string of ad targeting info');
+  //  });
+  //
+  //  it('should log message if adunitCode param is falsey', function () {
+  //    var result = pbjs.getAdserverTargetingForAdUnitCodeStr();
+  //    assert.ok(spyLogMessage.calledWith('Need to call getAdserverTargetingForAdUnitCodeStr with adunitCode'), 'expected message was logged');
+  //    assert.equal(result, undefined, 'result is undefined');
+  //  });
+  //});
+  //
+  //describe('getAdserverTargetingForAdUnitCode', function () {
+  //  it('should return targeting info as an object', function () {
+  //    var result = pbjs.getAdserverTargetingForAdUnitCode(config.adUnitCodes[0]);
+  //    assert.deepEqual(result, targetingMap[config.adUnitCodes[0]], 'returns expected targeting info object');
+  //  });
+  //
+  //  it('should return full targeting map object if adunitCode is falsey', function () {
+  //    var result = pbjs.getAdserverTargetingForAdUnitCode();
+  //    assert.deepEqual(result, targetingMap, 'the complete targeting map object is returned');
+  //  });
+  //});
 
-    it('should log message if adunitCode param is falsey', function () {
-      var result = pbjs.getAdserverTargetingForAdUnitCodeStr();
-      assert.ok(spyLogMessage.calledWith('Need to call getAdserverTargetingForAdUnitCodeStr with adunitCode'), 'expected message was logged');
-      assert.equal(result, undefined, 'result is undefined');
-    });
-  });
+  //describe('getAdServerTargeting', function () {
+  //  it('should call getAdServerTargetingForAdUnitCode', function () {
+  //    var spyGetAdServerTargetingForAdUnitCode = sinon.spy(pbjs, 'getAdserverTargetingForAdUnitCode');
+  //    pbjs.getAdserverTargeting();
+  //    assert.ok(spyGetAdServerTargetingForAdUnitCode.calledOnce, 'called the expected function');
+  //    pbjs.getAdserverTargetingForAdUnitCode.restore();
+  //  });
+  //});
 
-  describe('getAdserverTargetingForAdUnitCode', function () {
-    it('should return targeting info as an object', function () {
-      var result = pbjs.getAdserverTargetingForAdUnitCode(config.adUnitCodes[0]);
-      assert.deepEqual(result, targetingMap[config.adUnitCodes[0]], 'returns expected targeting info object');
-    });
-
-    it('should return full targeting map object if adunitCode is falsey', function () {
-      var result = pbjs.getAdserverTargetingForAdUnitCode();
-      assert.deepEqual(result, targetingMap, 'the complete targeting map object is returned');
-    });
-  });
-
-  describe('getAdServerTargeting', function () {
-    it('should call getAdServerTargetingForAdUnitCode', function () {
-      var spyGetAdServerTargetingForAdUnitCode = sinon.spy(pbjs, 'getAdserverTargetingForAdUnitCode');
-      pbjs.getAdserverTargeting();
-      assert.ok(spyGetAdServerTargetingForAdUnitCode.calledOnce, 'called the expected function');
-      pbjs.getAdserverTargetingForAdUnitCode.restore();
-    });
-  });
-
-  describe('getBidResponses', function () {
-    it('should return expected bid responses when passed an adunitCode', function () {
-      var result = pbjs.getBidResponses(config.adUnitCodes[0]);
-      var compare = require('test/fixtures/bid-responses-cloned.json')[config.adUnitCodes[0]];
-
-      assert.deepEqual(result, compare);
-    });
-
-    it('should return expected bid responses when not passed an adunitCode', function () {
-      var result = pbjs.getBidResponses();
-      var compare = require('test/fixtures/bid-responses-cloned.json');
-
-      assert.deepEqual(result, compare);
-    });
-  });
-
-  describe('getBidResponsesForAdUnitCode', function () {
-    it('should call getBidResponses with passed in adUnitCode', function () {
-      var adUnitCode = 'xyz';
-      var spyGetBidResponses = sinon.spy(pbjs, 'getBidResponses');
-
-      pbjs.getBidResponsesForAdUnitCode(adUnitCode);
-      assert.ok(spyGetBidResponses.calledWith(adUnitCode));
-      pbjs.getBidResponses.restore();
-    });
-  });
+  //describe('getBidResponses', function () {
+  //  it('should return expected bid responses when passed an adunitCode', function () {
+  //    var result = pbjs.getBidResponses(config.adUnitCodes[0]);
+  //    var compare = require('test/fixtures/bid-responses-cloned.json')[config.adUnitCodes[0]];
+  //
+  //    assert.deepEqual(result, compare);
+  //  });
+  //
+  //  it('should return expected bid responses when not passed an adunitCode', function () {
+  //    var result = pbjs.getBidResponses();
+  //    var compare = require('test/fixtures/bid-responses-cloned.json');
+  //
+  //    assert.deepEqual(result, compare);
+  //  });
+  //});
+  //
+  //describe('getBidResponsesForAdUnitCode', function () {
+  //  it('should call getBidResponses with passed in adUnitCode', function () {
+  //    var adUnitCode = 'xyz';
+  //    var spyGetBidResponses = sinon.spy(pbjs, 'getBidResponses');
+  //
+  //    pbjs.getBidResponsesForAdUnitCode(adUnitCode);
+  //    assert.ok(spyGetBidResponses.calledWith(adUnitCode));
+  //    pbjs.getBidResponses.restore();
+  //  });
+  //});
 
   describe('setTargetingForGPTAsync', function () {
     it('should log a message when googletag functions not defined', function () {
