7e8a2850c90d7fd6f5a0a57e2eb7dc4290ac4107
Ali Aydar <ali@eyedars-MacBook-Pro.local>
2016-05-13 12:48:49 -0700

    added info-level log message to setPriceGranularity()

--- src/prebid.js
+++ src/prebid.js
@@ -993,6 +993,7 @@ pbjs.aliasBidder = function (bidderCode, alias) {
 };
 
 pbjs.setPriceGranularity = function (granularity) {
+  utils.logInfo('Invoking pbjs.setPriceGranularity', arguments);
   if (!granularity) {
     utils.logError('Prebid Error: no value passed to `setPriceGranularity()`');
   } else {
