579cf73cd454bdee7f81160b3326ca6699314458
Kevin Jennison <kevin.jennison1@gmail.com>
2016-06-14 14:54:38 -0700

    Move `getAdserverTargeting` tests to test against public API.

--- test/spec/unit/pbjs_api_spec.js
+++ test/spec/unit/pbjs_api_spec.js
@@ -20,6 +20,7 @@ pbjs._bidsRequested = getBidRequests();
 pbjs._bidsReceived = getBidResponses();
 
 function resetAuction() {
+  pbjs.disableSendAllBids(); // TODO: don't have this be part of public API
   pbjs.clearAuction();
   pbjs._bidsRequested = getBidRequests();
   pbjs._bidsReceived = getBidResponses();
@@ -110,11 +111,89 @@ describe('Unit: Prebid Module', function () {
   });
 
   describe('getAdServerTargeting', function () {
+
+    beforeEach(() => {
+      resetAuction();
+    });
+
+    afterEach(() => {
+      resetAuction();
+    });
+
     it('should return current targeting data for slots', function () {
+      pbjs.enableSendAllBids();
       const targeting = pbjs.getAdserverTargeting();
       const expected = getAdServerTargeting();
-      pbjs.enableSendAllBids();
       assert.deepEqual(targeting, expected, 'targeting ok');
+    });
+
+    it('should return correct targeting with default settings', () => {
+      var targeting = pbjs.getAdserverTargeting();
+      var expected = {
+        "/19968336/header-bid-tag-0": {
+          "foobar": "300x250",
+          "hb_size": "300x250",
+          "hb_pb": "10.00",
+          "hb_adid": "233bcbee889d46d",
+          "hb_bidder": "appnexus"
+        },
+        "/19968336/header-bid-tag1": {
+          "foobar": "728x90",
+          "hb_size": "728x90",
+          "hb_pb": "10.00",
+          "hb_adid": "24bd938435ec3fc",
+          "hb_bidder": "appnexus"
+        }
+      };
+      assert.deepEqual(targeting, expected);
+    });
+
+    it('should return correct targeting with bid landscape targeting on', () => {
+      pbjs.enableSendAllBids();
+      var targeting = pbjs.getAdserverTargeting();
+      var expected = getAdServerTargeting();
+      assert.deepEqual(targeting, expected);
+    });
+
+    it("should include a losing bid's custom ad targeting key when the bid has `alwaysUseBid` set to `true`", () => {
+
+      // Let's make sure we're getting the expected losing bid.
+      assert.equal(pbjs._bidsReceived[0]['bidderCode'], 'triplelift');
+      assert.equal(pbjs._bidsReceived[0]['cpm'], 0.112256);
+
+      // Modify the losing bid to have `alwaysUseBid=true` and a custom `adserverTargeting` key.
+      pbjs._bidsReceived[0]['alwaysUseBid'] = true;
+      pbjs._bidsReceived[0]['adserverTargeting'] = {
+        'always_use_me': 'abc',
+      };
+
+      var targeting = pbjs.getAdserverTargeting();
+
+      // Ensure targeting for both ad placements includes the custom key. 
+      assert.equal(
+        targeting['/19968336/header-bid-tag-0'].hasOwnProperty('always_use_me'),
+        true
+      );
+
+      var expected = {
+        "/19968336/header-bid-tag-0": {
+          "foobar": "300x250",
+          "hb_size": "300x250",
+          "hb_pb": "10.00",
+          "hb_adid": "233bcbee889d46d",
+          "hb_bidder": "appnexus",
+          "always_use_me": "abc"
+        },
+        "/19968336/header-bid-tag1": {
+          "foobar": "728x90",
+          "hb_size": "728x90",
+          "hb_pb": "10.00",
+          "hb_adid": "24bd938435ec3fc",
+          "hb_bidder": "appnexus"
+        }
+      };
+
+      assert.deepEqual(targeting, expected);
     });
   });
 
