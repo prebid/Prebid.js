100c0a40b32cbadb1271dab8f6edd067554032bc
Marton Hipszki <mhipszki@gmail.com>
2016-06-27 10:29:21 +0100

    removed empty lines, refactored adapter loading step

--- loaders/adapterLoader.js
+++ loaders/adapterLoader.js
@@ -8,9 +8,9 @@
 
 const fs = require('fs');
 const blockLoader = require('block-loader');
+const getAdapters = require('./getAdapters');
 
-const adapters = require('./getAdapters')('../adapters.json');
-
+const adapters = getAdapters('../adapters.json');
 const files = fs.readdirSync('src/adapters').map((file) => file.replace(/\.[^/.]+$/, ''));
 const adapterNames = adapters.map(getNames).filter(getUniques);
 const aliases = adapters.filter(getAliases);
