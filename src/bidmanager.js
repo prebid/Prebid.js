6e9068ec8d8c06e2e3b3d862709f7b2ffe39290a
mkendall07 <emailmatthere@gmail.com>
2016-06-28 17:11:05 -0400

    Link bid.adId and bidRequest.bidId.

--- src/bidmanager.js
+++ src/bidmanager.js
@@ -69,6 +69,9 @@ function getBidSetForBidder(bidder) {
  */
 exports.addBidResponse = function (adUnitCode, bid) {
   if (bid) {
+    //first lookup bid request and assign it back the bidId if it matches the adUnitCode
+    let bidRequest = getBidSetForBidder(bid.bidderCode).bids.find(bidRequest => bidRequest.placementCode === adUnitCode);
+    bid.adId = bidRequest.bidId;
     Object.assign(bid, {
       responseTimestamp: timestamp(),
       requestTimestamp: getBidSetForBidder(bid.bidderCode).start,
