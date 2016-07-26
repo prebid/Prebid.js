2c08e5e1072c589b505f42647eb5c0f29d574ff2
Matt Lane <mlane@appnexus.com>
2016-07-26 00:32:02 -0700

    Build minimal request object

--- src/adapters/ut.js
+++ src/adapters/ut.js
@@ -1,16 +1,37 @@
 import { ajax } from '../ajax';
+import { generateUUID } from '../utils';
 
 const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
 
 const UtAdapter = function UtAdapter() {
 
   function _callBids(params) {
+    let tags = [];
 
-    let bids = [];
     for (let bid of params.bids) {
+      let tag = {};
+
+      tag.sizes = bid.sizes;
+      tag.uuid = generateUUID();
+      tag.id = Number.parseInt(bid.params.placementId);
+      tag.prebid = true;
+      tag.allow_smaller_sizes = false;
+      tag.disable_psa = true;
+      tag.ad_types = [0];
+
+      tags.push(tag);
     }
 
-    ajax(ENDPOINT, null, bids);
+    const payload = JSON.stringify({
+      tags: [...tags],
+      uuid: generateUUID(),
+      member_id: "none"
+    });
+
+    ajax(ENDPOINT, handleResponse, payload);
+  }
+
+  function handleResponse(response, xhr) {
   }
 
   return {
