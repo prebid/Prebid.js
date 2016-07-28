53cc39143a61a51f7cb442c2df08618f03f5d585
Matt Lane <mlane@appnexus.com>
2016-07-27 17:06:15 -0700

    Implement inital response handler function

--- src/adapters/ut.js
+++ src/adapters/ut.js
@@ -1,19 +1,29 @@
-import { ajax } from '../ajax';
-import { generateUUID, getSizes } from '../utils';
+import bidfactory from 'src/bidfactory';
+import bidmanager from 'src/bidmanager';
+import { ajax } from 'src/ajax';
+import { createTrackPixelHtml, getSizes, logError } from 'src/utils';
+import CONSTANTS from 'src/constants';
 
 const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
 
 function UtAdapter() {
+
+  let placements = {};
+
   function callBids(params) {
-    const tags = params.bids.map(bid => {
+    // TODO: validate params
+    const bids = params.bids || [];
+    const tags = bids.map(bid => {
       let tag = {};
 
       const sizes = getSizes(bid.sizes);
       tag.sizes = sizes;
       tag.primary_size = sizes[0];
 
-      tag.uuid = generateUUID();
+      tag.uuid = bid.bidId;
       tag.id = Number.parseInt(bid.params.placementId);
+      placements[bid.bidId] = bid.placementCode;
+
       tag.prebid = true;
       tag.allow_smaller_sizes = false;
       tag.disable_psa = true;
@@ -22,20 +32,41 @@ function UtAdapter() {
       return tag;
     });
 
-    const request = {
-      tags: [...tags],
-      uuid: generateUUID(),
-      member_id: "none"
-    };
+    const payload = {tags: [...tags]};
 
-    const payload = JSON.stringify(request);
-    ajax(ENDPOINT, handleResponse, payload);
+    ajax(ENDPOINT, registerResponse, JSON.stringify(payload));
   }
 
-  function handleResponse(response, xhr) {
+  function registerResponse(response) {
+    const tags = JSON.parse(response).tags || [];
+
+    tags.forEach(tag => {
+      const ad = tag.ads[0];
+      const bid = bidfactory.createBid(CONSTANTS.STATUS.GOOD);
+      bid.code = 'ut';
+      bid.bidderCode = 'ut';
+      bid.creative_id = ad.creativeId;
+      bid.cpm = ad.cpm;
+      bid.ad = ad.rtb.banner.content;
+      try {
+        const url = ad.rtb.trackers[0].impression_urls[0];
+        const tracker = createTrackPixelHtml(url);
+        bid.ad += tracker;
+      } catch (e) {
+        logError('Error appending tracking pixel', 'appnexusAst.js:_requestAds', e);
+      }
+
+      bid.width = ad.rtb.banner.width;
+      bid.height = ad.rtb.banner.height;
+
+      bidmanager.addBidResponse(placements[tag.uuid], bid);
+    });
   }
 
   return {callBids};
+
 }
 
+// using this instead of ES2015 equivalent because `export default` compiles to
+// `exports.default`, not `module.exports` as needed in build system
 module.exports = UtAdapter;
