3d59d20f81807f5000d2ae7d348a48f6e85b868c
Matt Lane <mlane@appnexus.com>
2016-08-01 17:34:17 -0700

    Test response handling function

--- test/spec/adapters/appnexus_spec.js
+++ test/spec/adapters/appnexus_spec.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 import adapter from 'src/adapters/ut';
-
+import bidmanager from 'src/bidmanager';
 const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
-const params = {
+
+const PARAMS = {
   "bidderCode": "ut",
   "requestId": "d3e07445-ab06-44c8-a9dd-5ef9af06d2a6",
   "bidderRequestId": "7101db09af0db2",
@@ -26,29 +27,134 @@ const params = {
   "timeout": 3000
 };
 
+const RESPONSE = {
+    "version": "0.0.1",
+    "tags": [{
+        "uuid": "8160b2bea694fc",
+        "tag_id": 4799418,
+        "auction_id": "2256922143947979797",
+        "no_ad_url": "http://lax1-ib.adnxs.com/no-ad",
+        "timeout_ms": 2500,
+        "ads": [{
+            "content_source": "rtb",
+            "ad_type": "banner",
+            "buyer_member_id": 958,
+            "creative_id": 33989846,
+            "media_type_id": 1,
+            "media_subtype_id": 1,
+            "cpm": 0.500000,
+            "cpm_publisher_currency": 0.500000,
+            "publisher_currency_code": "$",
+            "client_initiated_ad_counting": true,
+            "rtb": {
+                "banner": {
+                    "width": 728,
+                    "height": 90,
+                    "content": "<!-- Creative -->"
+                },
+                "trackers": [{
+                    "impression_urls": ["http://lax1-ib.adnxs.com/impression"]
+                }]
+            }
+        }]
+    }, {
+        "uuid": "9f6e9235559e8e",
+        "tag_id": 4799418,
+        "auction_id": "5911338687906386297",
+        "no_ad_url": "http://lax1-ib.adnxs.com/no-ad",
+        "timeout_ms": 2500,
+        "ads": [{
+            "content_source": "rtb",
+            "ad_type": "banner",
+            "buyer_member_id": 958,
+            "creative_id": 29681110,
+            "media_type_id": 1,
+            "media_subtype_id": 1,
+            "cpm": 0.500000,
+            "cpm_publisher_currency": 0.500000,
+            "publisher_currency_code": "$",
+            "client_initiated_ad_counting": true,
+            "rtb": {
+                "banner": {
+                    "width": 300,
+                    "height": 250,
+                    "content": "<!-- Creative -->"
+                },
+                "trackers": [{
+                    "impression_urls": ["http://lax1-ib.adnxs.com/impression"]
+                }]
+            }
+        }]
+    }]
+};
+
 describe('AppNexusAdapter', () => {
 
-  let xhr;
-  let requests;
+  describe('request', () => {
 
-  beforeEach(() => {
-    xhr = sinon.useFakeXMLHttpRequest();
-    requests = [];
-    xhr.onCreate = request => requests.push(request);
+    let xhr;
+    let requests;
+
+    beforeEach(() => {
+      xhr = sinon.useFakeXMLHttpRequest();
+      requests = [];
+      xhr.onCreate = request => requests.push(request);
+    });
+
+    afterEach(() => xhr.restore());
+
+    it('exists and is a function', () => {
+      expect(adapter().callBids).to.exist.and.to.be.a('function');
+    });
+
+    it('sends bid request to ENDPOINT via POST', () => {
+      adapter().callBids(PARAMS);
+      expect(requests[0].url).to.equal(ENDPOINT);
+      expect(requests[0].method).to.equal('POST');
+    });
+
   });
 
-  afterEach(() => {
-    xhr.restore();
-  });
+  describe('response', () => {
 
-  it('exists and is a function', () => {
-    expect(adapter).to.exist.and.to.be.a('function');
-  });
+    let server;
 
-  it('sends bid request to ENDPOINT via POST', () => {
-    adapter().callBids(params);
-    expect(requests[0].url).to.equal(ENDPOINT);
-    expect(requests[0].method).to.equal('POST');
+    beforeEach(() => {
+      server = sinon.fakeServer.create();
+      sinon.stub(bidmanager, 'addBidResponse');
+    });
+
+    afterEach(() => {
+      server.restore()
+      bidmanager.addBidResponse.restore();
+    });
+
+    it('registers bid responses', () => {
+      server.respondWith(JSON.stringify(RESPONSE));
+
+      adapter().callBids(PARAMS);
+      server.respond();
+      sinon.assert.calledTwice(bidmanager.addBidResponse);
+
+      const response = bidmanager.addBidResponse.firstCall.args[1];
+      expect(response).to.have.property('statusMessage', 'Bid available');
+      expect(response).to.have.property('cpm', 0.5);
+    });
+
+    it('handles blank bids', () => {
+      server.respondWith(JSON.stringify({
+        "tags": [{}]
+      }));
+
+      adapter().callBids(PARAMS);
+      server.respond();
+      sinon.assert.calledOnce(bidmanager.addBidResponse);
+
+      const response = bidmanager.addBidResponse.firstCall.args[1];
+      expect(response).to.have.property('statusMessage',
+        'Bid returned empty or error response');
+    });
+
   });
 
 });
