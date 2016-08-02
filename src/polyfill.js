45fd284b219182b9a0406811ecd677a95b098eae
Eric Hochberger <eric@mediavine.com>
2016-08-02 13:35:06 -0400

    IE Bug Fix: Add includes polyfill (#464)
    * Add includes polyfill
    
    * switch includes polyfill to use Object.defineProperty

--- src/polyfill.js
+++ src/polyfill.js
@@ -26,3 +26,33 @@ if (!Array.prototype.find) {
     }
   });
 }
+
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, "includes", {
+    value: function(searchElement) {
+      var O = Object(this);
+      var len = parseInt(O.length, 10) || 0;
+      if (len === 0) {
+        return false;
+      }
+      var n = parseInt(arguments[1], 10) || 0;
+      var k;
+      if (n >= 0) {
+        k = n;
+      } else {
+        k = len + n;
+        if (k < 0) {k = 0;}
+      }
+      var currentElement;
+      while (k < len) {
+        currentElement = O[k];
+        if (searchElement === currentElement ||
+           (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+          return true;
+        }
+        k++;
+      }
+      return false;
+    }
+  });
+}
