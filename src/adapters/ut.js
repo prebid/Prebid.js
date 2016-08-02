7277b5e87c0039722f4f1119cd648d0f1baee234
Matt Lane <mlane@appnexus.com>
2016-08-01 17:33:33 -0700

    Create bid based on response status

--- src/adapters/ut.js
+++ src/adapters/ut.js
@@ -1,22 +1,22 @@
 import bidfactory from 'src/bidfactory';
 import bidmanager from 'src/bidmanager';
 import { ajax } from 'src/ajax';
-import { createTrackPixelHtml, getSizes, logError } from 'src/utils';
+import * as utils from 'src/utils';
 import CONSTANTS from 'src/constants';
 
 const ENDPOINT = 'http://ib.adnxs.com/ut/v2';
+const CODE = 'ut';
 
 function UtAdapter() {
 
   let placements = {};
 
   function callBids(params) {
-    // TODO: validate params
     const bids = params.bids || [];
     const tags = bids.map(bid => {
       let tag = {};
 
-      const sizes = getSizes(bid.sizes);
+      const sizes = utils.getSizes(bid.sizes);
       tag.sizes = sizes;
       tag.primary_size = sizes[0];
 
@@ -34,30 +34,38 @@ function UtAdapter() {
 
     const payload = {tags: [...tags]};
 
-    ajax(ENDPOINT, registerResponse, JSON.stringify(payload));
+    ajax(ENDPOINT, handleResponse, JSON.stringify(payload));
   }
 
-  function registerResponse(response) {
-    const tags = JSON.parse(response).tags || [];
+  function handleResponse(response) {
+    const parsed = JSON.parse(response);
+    if (!parsed || parsed.error) {return;}
 
-    tags.forEach(tag => {
-      const ad = tag.ads[0];
-      const bid = bidfactory.createBid(CONSTANTS.STATUS.GOOD);
-      bid.code = 'ut';
-      bid.bidderCode = 'ut';
-      bid.creative_id = ad.creativeId;
-      bid.cpm = ad.cpm;
-      bid.ad = ad.rtb.banner.content;
-      try {
-        const url = ad.rtb.trackers[0].impression_urls[0];
-        const tracker = createTrackPixelHtml(url);
-        bid.ad += tracker;
-      } catch (e) {
-        logError('Error appending tracking pixel', 'appnexusAst.js:_requestAds', e);
+    parsed.tags.forEach(tag => {
+      let bid;
+
+      if (!tag.error && !utils.isEmpty(tag)) {
+        bid = bidfactory.createBid(CONSTANTS.STATUS.GOOD);
+        const ad = tag.ads[0];
+        bid.code = CODE;
+        bid.bidderCode = CODE;
+        bid.creative_id = ad.creativeId;
+        bid.cpm = ad.cpm;
+        bid.ad = ad.rtb.banner.content;
+        try {
+          const url = ad.rtb.trackers[0].impression_urls[0];
+          const tracker = utils.createTrackPixelHtml(url);
+          bid.ad += tracker;
+        } catch (e) {
+          utils.logError('Error appending tracking pixel', 'appnexusAst.js:_requestAds', e);
+        }
+        bid.width = ad.rtb.banner.width;
+        bid.height = ad.rtb.banner.height;
+      } else {
+        bid = bidfactory.createBid(CONSTANTS.STATUS.NO_BID);
+        bid.code = CODE;
+        bid.bidderCode = CODE;
       }
-
-      bid.width = ad.rtb.banner.width;
-      bid.height = ad.rtb.banner.height;
 
       bidmanager.addBidResponse(placements[tag.uuid], bid);
     });
@@ -67,6 +75,4 @@ function UtAdapter() {
 
 }
 
-// using this instead of ES2015 equivalent because `export default` compiles to
-// `exports.default`, not `module.exports` as needed in build system
 module.exports = UtAdapter;
