9fc10c00763bd33d53f4410860dec93b7e58811e
Artem Dmitriev <art.dm.ser@gmail.com>
2016-08-17 17:47:14 +0300

    getintent adapter initial version.

---
+++ src/adapters/getintent.js
@@ -0,0 +1,61 @@
+var bidfactory = require('../bidfactory.js');
+var bidmanager = require('../bidmanager.js');
+var adloader = require('../adloader.js');
+var utils = require('../utils.js');
+
+var GetIntentAdapter = function GetIntentAdapter() {
+  var headerBiddingStaticJS = window.location.protocol + '//cdn.adhigh.net/adserver/hb.js';
+
+  function _callBids(params) {
+    if (typeof window.gi_hb === 'undefined') {
+      adloader.loadScript(headerBiddingStaticJS, function () { bid(params); }, true);
+    } else {
+      bid(params);
+    }
+  }
+
+  function bid(params) {
+    var bids = params.bids;
+    for (var i = 0; i < bids.length; i++) {
+      var bidRequest = bids[i];
+      var callback = bidResponseCallback(bidRequest);
+      window.gi_hb.makeBid({
+        pid: bidRequest.params.pid,
+        tid: bidRequest.params.tid,
+        cur: bidRequest.params.cur,
+        size: bidRequest.params.size,
+        floor: bidRequest.params.floor,
+        known: 0
+      }, callback);
+    }
+  };
+
+  function bidResponseCallback(bid) {
+    return function(bidResponse) {
+      bidResponseAvailable(bid, bidResponse);
+    };
+  }
+
+  function bidResponseAvailable(bidRequest, bidResponse) {
+    if (bidResponse.no_bid == 1) {
+      var passback = bidfactory.createBid(2);
+      passback.bidderCode = bidRequest.bidder;
+      bidmanager.addBidResponse(bidRequest.placementCode, passback);
+    } else {
+      var adSize = bidRequest.params.size.toUpperCase().split('x');
+      var bid = bidfactory.createBid(1);
+      bid.bidderCode = bidRequest.bidder;
+      bid.cpm = bidResponse.cpm;
+      bid.ad = bidResponse.ad;
+      bid.width = adSize[0];
+      bid.height = adSize[1];
+      bidmanager.addBidResponse(bidRequest.placementCode, bid);
+    }
+  }
+
+  return {
+    callBids: _callBids
+  };
+};
+
+module.exports = GetIntentAdapter;
