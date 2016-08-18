6b8af6384e8d1610f7217e8c54c4d56330d60e31
Rich Snapp <rsnapp@rubiconproject.com>
2016-08-17 18:01:43 -0600

    custom sendStandardTargeting implemented in bidderSettings per bidder

--- test/spec/bidmanager_spec.js
+++ test/spec/bidmanager_spec.js
@@ -372,7 +372,45 @@ describe('bidmanager.js', function () {
 
       var response = bidmanager.getKeyValueTargetingPairs(bidderCode, bid);
       assert.deepEqual(response, expected);
-    })
+    });
+
+    it('sendStandardTargeting=false and inherit custom', function () {
+      $$PREBID_GLOBAL$$.bidderSettings =
+      {
+        appnexus: {
+          alwaysUseBid: true,
+          sendStandardTargeting: false,
+          adserverTargeting: [
+            {
+              key: "hb_bidder",
+              val: function (bidResponse) {
+                return bidResponse.bidderCode;
+              }
+            }, {
+              key: "hb_adid",
+              val: function (bidResponse) {
+                return bidResponse.adId;
+              }
+            }, {
+              key: "hb_pb",
+              val: function (bidResponse) {
+                return bidResponse.pbHg;
+              }
+            }, {
+              key: "custom",
+              val: 42
+            }
+          ]
+        }
+      };
+
+      var expected = {
+        "custom": 42
+      };
+      var response = bidmanager.getKeyValueTargetingPairs(bidderCode, bid);
+      assert.deepEqual(response, expected);
+
+    });
 
   });
 
