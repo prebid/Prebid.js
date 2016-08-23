adac845e23dd7246044f841e6b37e8c8f0936f89
Yogesh Ingale <yogesh.ingale@adsparc.com>
2016-08-24 00:59:46 +0530

    Update adsparc.js

--- src/adapters/adsparc.js
+++ src/adapters/adsparc.js
@@ -21,23 +21,22 @@ var AdsparcAdapter = function AdsparcAdapter() {
     }
 	
     var getJSON = function(url) {
-     return new Promise(function(resolve, reject) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('get', url, true);
-      xhr.onreadystatechange = function() {
-        var status;
-        var data;
-        if (xhr.readyState === 4) { // `DONE`
-          status = xhr.status;
-          if (status === 200) {
-            resolve(xhr.response);
-          } else {
-            reject(status);
+      return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', url, true);
+        xhr.onreadystatechange = function() {
+          var status;
+          if (xhr.readyState === 4) { // `DONE`
+            status = xhr.status;
+            if (status === 200) {
+              resolve(xhr.response);
+            } else {
+              reject(status);
+            }
           }
-        }
-      };
-      xhr.send();
-     });
+        };
+        xhr.send();
+      });
     };
 
 
