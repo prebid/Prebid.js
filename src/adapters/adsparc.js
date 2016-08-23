0174ace6ae97c6992c682f42872178b13ab35699
Yogesh Ingale <yogesh.ingale@adsparc.com>
2016-08-23 22:44:33 +0530

    Create adsparc.js
    Added Adsparc Adaptor JS Code

---
+++ src/adapters/adsparc.js
@@ -0,0 +1,109 @@
+var CONSTANTS = require('../constants.json');
+var utils = require('../utils.js');
+var bidfactory = require('../bidfactory.js');
+var bidmanager = require('../bidmanager.js');
+var adloader = require('../adloader');
+/**
+ * Adapter for requesting bids from Pubmatic.
+ *
+ * @returns {{callBids: _callBids}}
+ * @constructor
+ */
+var AdsparcAdapter = function AdsparcAdapter() {
+	
+    function _callBids(params) {
+        bids = params.bids || [];
+        for (var i = 0; i < bids.length; i++) {
+            var bid = bids[i];
+		
+        _requestBid(bid);
+
+        }
+        // Only make one request per "nid"
+
+    }
+
+    function _getUniqueNids(bids) {
+        var key;
+        var map = {};
+        var nids = [];
+        bids.forEach(function(bid) {
+            map[bid.params.nid] = bid;
+        });
+        for (key in map) {
+            if (map.hasOwnProperty(key)) {
+                nids.push(map[key]);
+            }
+        }
+		console.log(nids);
+        return nids;
+    }
+
+    function _requestBid(bid) {
+		var placementCode = '';
+		var bids;
+		var scriptUrl = 'http://pubs.adsparc.net/bid/ad.json';
+		var size;
+		var pubId;
+		var siteUrl;
+		var refUrl;
+        var content = bid.params.unit;
+        var adUnit;
+        var adData;
+		var adResponse;
+		placementCode = bid.placementCode;
+		//load page options from bid request
+		if (bid.params.pubId) {
+			pubId = bid.params.pubId;
+
+		}
+		if (bid.params.size) {
+			size = bid.params.size;		
+
+
+		}
+		if (bid.params.pageUrl) {
+			siteUrl = bid.params.pageUrl;
+
+		}
+		if (bid.params.refUrl) {
+			refUrl =bid.params.refUrl;
+
+		}
+		var sizes = size.split("x");
+        var Url = scriptUrl +'?type=1&p='+ pubId + '&sz=' + size + '&pageUrl=' + siteUrl + '&refUrl=' + refUrl;
+        var response;
+           	$.get(Url, function( data ) {
+				 response = data;				
+                    // Add a response for each bid matching the "nid"
+                        if (response) {
+							var timestamp = $.now();
+                            adResponse = bidfactory.createBid(1);
+                            adResponse.bidderCode = 'adsparc';
+                            adResponse.cpm = Number(response.eCpm);
+							var replaceArray = ['[CACHEBUSTER]', '{RANDOM}'];
+							var adcode = response.adCode;
+							for(var i = 0; i < replaceArray.length; i++) {
+								adcode = adcode.replace(replaceArray[i],timestamp);
+							}
+                            adResponse.ad ='<script src="' + adcode + '"></scr' + 'ipt>';
+							adResponse.width = sizes[0];
+							adResponse.height = sizes[1];
+                        } else {
+                            // Indicate an ad was not returned
+                            adResponse = bidfactory.createBid(2);
+                            adResponse.bidderCode = 'adsparc';
+                        }
+                        bidmanager.addBidResponse(bid.placementCode, adResponse);
+
+				},"json");
+
+
+    }
+
+    return {
+        callBids: _callBids
+    };
+};
+
+module.exports = AdsparcAdapter;
