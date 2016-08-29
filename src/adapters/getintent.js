d872a727ad880524403716259e4fc06fb161a230
Artem Dmitriev <art.dm.ser@gmail.com>
2016-08-29 20:44:46 +0300

    rollback and silence JSHint.

--- src/adapters/getintent.js
+++ src/adapters/getintent.js
@@ -1,3 +1,5 @@
+/*jshint loopfunc: true */
+
 var bidfactory = require('../bidfactory.js');
 var bidmanager = require('../bidmanager.js');
 var adloader = require('../adloader.js');
@@ -5,48 +7,20 @@ var adloader = require('../adloader.js');
 var GetIntentAdapter = function GetIntentAdapter() {
   var headerBiddingStaticJS = window.location.protocol + '//cdn.adhigh.net/adserver/hb.js';
 
-  function _callBids(params) { 
-    if (typeof window.gi_hb === 'undefined') {   
+  function _callBids(params) {
+    if (typeof window.gi_hb === 'undefined') {
       adloader.loadScript(headerBiddingStaticJS, function() {
         bid(params);
-      }, true);  
-    } else {   
-      bid(params);  
-    } 
-  } 
-
-  var _bidMaker = function(params) {
-    var bidReq = params.bidReq;
-    var bidResp = params.bidResp;
-    if (bidResp.no_bid === 1) {
-      var nobid = bidfactory.createBid(2);
-      nobid.bidderCode = bidReq.bidder;
-      bidmanager.addBidResponse(bidReq.placementCode, nobid);
+      }, true);
     } else {
-      var size = bidResp.size.split('x');
-      var bid = bidfactory.createBid(1);
-      bid.bidderCode = bidReq.bidder;
-      bid.cpm = bidResp.cpm;
-      bid.ad = bidResp.ad;
-      bid.width = size[0];
-      bid.height = size[1];
-      bidmanager.addBidResponse(bidReq.placementCode, bid);
+      bid(params);
     }
-  };
-
-  function bidMaker(bidReq) {
-    return function(bidResp) {
-      _bidMaker({
-        bidReq : bidReq,
-        bidResp : bidResp
-      });
-    };
   }
 
-  function bid(params) {  
-    var bids = params.bids || [];  
-    for (var i = 0; i < bids.length; i++) {   
-      var bidRequest = bids[i];   
+  function bid(params) {
+    var bids = params.bids || [];
+    for (var i = 0; i < bids.length; i++) {
+      var bidRequest = bids[i];
       window.gi_hb.makeBid({
         pid: bidRequest.params.pid, // required
         tid: bidRequest.params.tid, // required
@@ -54,12 +28,27 @@ var GetIntentAdapter = function GetIntentAdapter() {
         floor: bidRequest.params.floor, // optional
         known: bidRequest.params.known || 1, // optional
         size: bidRequest.sizes[0].join("x"),
-      }, bidMaker(bidRequest));
-    } 
+      }, function(bidResponse) {
+        if (bidResponse.no_bid === 1) {
+          var nobid = bidfactory.createBid(2);
+          nobid.bidderCode = bidRequest.bidder;
+          bidmanager.addBidResponse(bidRequest.placementCode, nobid);
+        } else {
+          var size = bidResponse.size.split('x');
+          var bid = bidfactory.createBid(1);
+          bid.bidderCode = bidRequest.bidder;
+          bid.cpm = bidResponse.cpm;
+          bid.ad = bidResponse.ad;
+          bid.width = size[0];
+          bid.height = size[1];
+          bidmanager.addBidResponse(bidRequest.placementCode, bid);
+        }
+      });
+    }
   }
 
-  return {  
-    callBids: _callBids 
+  return {
+    callBids: _callBids
   };
 };
 
