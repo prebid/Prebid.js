3b25fa421fc2c78ef5c88332189b9611d60f55d4
Joseph Frazer <frazjp65@gmail.com>
2016-08-31 07:47:17 -0400

    fix domain so it does not use a dev one send different requests for each ad unit size replace macros

--- src/adapters/adblade.js
+++ src/adapters/adblade.js
@@ -13,81 +13,85 @@ var AdbladeAdapter = function AdbladeAdapter() {
   'use strict';
 
   const BIDDER_CODE       = 'adblade';
-  const BASE_URI          = '//devex.adblade.com:8680/prebidjs/bid?';
+  const BASE_URI          = '//rtb.adblade.com/prebidjs/bid?';
   const DEFAULT_BID_FLOOR = 0.0000000001;
 
   function _callBids(params) {
-    var bids      = params.bids || [],
-        bidCount  = 0,
-        referrer  = utils.getTopWindowUrl(),
-        loc       = utils.getTopWindowLocation(),
-        domain    = loc.hostname,
-        partnerId = 0;
+    var bids        = params.bids || [],
+        referrer    = utils.getTopWindowUrl(),
+        loc         = utils.getTopWindowLocation(),
+        domain      = loc.hostname,
+        partnerId   = 0,
+        bidRequests = {};
 
     if (bids.length > 0) {
       partnerId = '' + bids[0].params.partnerId;
     }
 
-    var bidRequest = {
-      'site': {
-        'id': partnerId,
-        'page': referrer,
-        'domain': domain,
-        'publisher': {
-          'id': partnerId,
-          'name': referrer,
-          'domain': domain
-        }
-      },
-      'id': params.requestId,
-      'imp': [],
-      'device': {
-        'ua': window.navigator.userAgent,
-      },
-      'cur': ['USD'],
-      'user': {}
-    };
-
     utils._each(bids, function(bid) {
       utils._each(bid.sizes, function(size) {
-        bidCount++;
+        let key = size[0] + 'x' + size[1];
 
-        bidRequest.imp.push({
+        bidRequests[key] = bidRequests[key] || {
+          'site': {
+            'id': partnerId,
+            'page': referrer,
+            'domain': domain,
+            'publisher': {
+              'id': partnerId,
+              'name': referrer,
+              'domain': domain
+            }
+          },
+          'id': params.requestId,
+          'imp': [],
+          'device': {
+            'ua': window.navigator.userAgent,
+          },
+          'cur': ['USD'],
+          'user': {}
+        };
+
+        bidRequests[key].imp.push({
           'id': bid.bidId,
           'bidfloor': bid.params.bidFloor || DEFAULT_BID_FLOOR,
           'tag': bid.placementCode,
           'banner': {
-            "w": size[0],
-            "h": size[1],
+            'w': size[0],
+            'h': size[1],
           },
           'secure': 0 + (loc.protocol === 'https')
         });
       });
     });
 
-    adloader.loadScript(
-      utils.tryAppendQueryString(
+    utils._each(bidRequests, function (bidRequest) {
+      adloader.loadScript(
         utils.tryAppendQueryString(
-          BASE_URI,
-          'callback',
-          '$$PREBID_GLOBAL$$.adbladeResponse'
-        ),
-        'json',
-        JSON.stringify(
-          bidRequest
+          utils.tryAppendQueryString(
+            BASE_URI,
+            'callback',
+            '$$PREBID_GLOBAL$$.adbladeResponse'
+          ),
+          'json',
+          JSON.stringify(
+            bidRequest
+          )
         )
-      )
-    );
+      );
+    });
   }
 
   $$PREBID_GLOBAL$$.adbladeResponse = function (response) {
-    var bid;
+    var auctionIdRe    = /\$(%7B|\{)AUCTION_ID(%7D|\})/gi,
+        auctionPriceRe = /\$(%7B|\{)AUCTION_PRICE(%7D|\})/gi,
+        clickUrlRe     = /\$(%7B|\{)CLICK_URL(%7D|\})/gi;
 
     if (typeof(response) === 'undefined' || !response.hasOwnProperty('seatbid') || utils.isEmpty(response.seatbid)) {
       // handle empty bids
       var bidsRequested = $$PREBID_GLOBAL$$._bidsRequested.find(bidSet => bidSet.bidderCode === BIDDER_CODE).bids;
       if (bidsRequested.length > 0) {
-        bid = bidfactory.createBid(2);
+        let bid = bidfactory.createBid(2);
         bid.bidderCode = BIDDER_CODE;
         bidmanager.addBidResponse(bidsRequested[0].placementCode, bid);
       }
@@ -97,13 +101,18 @@ var AdbladeAdapter = function AdbladeAdapter() {
 
     utils._each(response.seatbid, function(seatbid) {
       utils._each(seatbid.bid, function(seatbidBid) {
-        var bidRequest = utils.getBidRequest(seatbidBid.impid);
+        var bidRequest = utils.getBidRequest(seatbidBid.impid),
+            ad         = seatbidBid.adm + utils.createTrackPixelHtml(seatbidBid.nurl);
 
-        bid = bidfactory.createBid(1);
+        ad = ad.replace(auctionIdRe, seatbidBid.impid);
+        ad = ad.replace(clickUrlRe, '');
+        ad = ad.replace(auctionPriceRe, seatbidBid.price);
+
+        let bid = bidfactory.createBid(1);
 
         bid.bidderCode = BIDDER_CODE;
         bid.cpm = seatbidBid.price;
-        bid.ad = seatbidBid.adm + utils.createTrackPixelHtml(seatbidBid.nurl);
+        bid.ad = ad;
         bid.width = seatbidBid.w;
         bid.height = seatbidBid.h;
         bidmanager.addBidResponse(bidRequest.placementCode, bid);
