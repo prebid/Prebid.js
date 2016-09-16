9bd56ef0a279e2720856edacbbb67c4adafceef2
Matt Lane <mlane@appnexus.com>
2016-09-16 10:08:25 -0700

    Make preflight opt-in and set default content-type to text-plain

--- src/ajax.js
+++ src/ajax.js
@@ -53,13 +53,11 @@ export function ajax(url, callback, data, options = {}) {
   if (!useXDomainRequest) {
     if (options.withCredentials) {
       x.withCredentials = true;
-    } else {
-      if (options.preflight !== false) {
-        x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      }
-      x.setRequestHeader('Content-Type',
-        options.contentType || 'application/json;charset=UTF-8');
     }
+    if (options.preflight) {
+      x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    }
+    x.setRequestHeader('Content-Type', options.contentType || 'text/plain');
   }
 
   x.send(method === 'POST' && data);
