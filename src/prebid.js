41d406d1d0f45db9447cc3ae92ad8900ea8df6b9
protonate <ncozi@appnexus.com>
2016-10-10 07:45:54 -0700

    and postMessage listener and handler

--- src/prebid.js
+++ src/prebid.js
@@ -517,6 +517,35 @@ $$PREBID_GLOBAL$$.clearAuction = function() {
   }
 };
 
+var sendAdToCreative = function (ev) {
+  var key = ev.message ? "message" : "data";
+  var data = {};
+  try {
+    data = JSON.parse(ev[key]);
+  } catch (e) {
+    // Do nothing.  No ad found.
+  }
+  if (data.adId) {
+    var adObject = $$PREBID_GLOBAL$$._bidsReceived.find(function (bid) {
+      return bid.adId === data.adId;
+    });
+    var ad = adObject.ad;
+    var adUrl = adObject.adUrl;
+    var message = JSON.stringify({
+      message: 'send ad',
+      ad: ad,
+      adUrl: adUrl
+    });
+    ev.source.postMessage(message, 'https://amp.prebidapp.com:5000');
+
+    // document.getElementsByTagName('iframe')[0].contentWindow.postMessage(message, 'https://prebid.org');
+  }
+};
+
+var listenAdRequestFromCreative = function () {
+  addEventListener('message', sendAdToCreative, false);
+};
+
 /**
  *
  * @param bidsBackHandler
@@ -571,6 +600,7 @@ $$PREBID_GLOBAL$$.requestBids = function ({ bidsBackHandler, timeout, adUnits, a
   }
 
   adaptermanager.callBids({ adUnits, adUnitCodes, cbTimeout });
+  listenAdRequestFromCreative();
 };
 
 /**
