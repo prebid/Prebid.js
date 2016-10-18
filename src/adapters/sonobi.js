d6adf1eab9a0a454bc3a5ad152f596c2ae07522f
edahood-sonobi <edahood@sonobi.com>
2016-10-18 19:45:16 -0400

    Ensuring that ref parameter is always set to correct value in sonobi requests (#712)

--- src/adapters/sonobi.js
+++ src/adapters/sonobi.js
@@ -11,8 +11,8 @@ var SonobiAdapter = function SonobiAdapter(){
     var trinity = 'https://apex.go.sonobi.com/trinity.js?key_maker=';
     var adSlots = request.bids || [];
     var bidderRequestId = request.bidderRequestId;
-    var ref = (window.frameElement) ? encodeURI(top.location.host || document.referrer) : null;
-    adloader.loadScript(trinity + JSON.stringify(_keymaker(adSlots)) + '&cv=' + _operator(bidderRequestId) + (ref) ? '&ref=' + ref : '');
+    var ref = (window.frameElement) ? '&ref=' + encodeURI(top.location.host || document.referrer) : '';
+    adloader.loadScript(trinity + JSON.stringify(_keymaker(adSlots)) + '&cv=' + _operator(bidderRequestId) + ref );
   }
 
   function _keymaker(adSlots){
