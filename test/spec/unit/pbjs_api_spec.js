6806cc6e0a0b47f5a012cb3260f94bbba4ae0620
protonate <ncozi@appnexus.com>
2016-11-04 11:47:50 -0700

    restore shuffle bidders test and resetAuction

--- test/spec/unit/pbjs_api_spec.js
+++ test/spec/unit/pbjs_api_spec.js
@@ -1271,21 +1271,22 @@ describe('Unit: Prebid Module', function () {
     });
   });
 
-  // describe('setBidderSequence', () => {
-  //   it('setting to `random` uses shuffled order of adUnits', () => {
-  //     sinon.spy(utils, 'shuffle');
-  //     const requestObj = {
-  //       bidsBackHandler: function bidsBackHandlerCallback() {},
-  //       timeout: 2000
-  //     };
-  //
-  //     $$PREBID_GLOBAL$$.setBidderSequence('random');
-  //     $$PREBID_GLOBAL$$.requestBids(requestObj);
-  //
-  //     sinon.assert.calledOnce(utils.shuffle);
-  //     utils.shuffle.restore();
-  //   });
-  // });
+  describe('setBidderSequence', () => {
+    it('setting to `random` uses shuffled order of adUnits', () => {
+      sinon.spy(utils, 'shuffle');
+      const requestObj = {
+        bidsBackHandler: function bidsBackHandlerCallback() {},
+        timeout: 2000
+      };
+
+      $$PREBID_GLOBAL$$.setBidderSequence('random');
+      $$PREBID_GLOBAL$$.requestBids(requestObj);
+
+      sinon.assert.calledOnce(utils.shuffle);
+      utils.shuffle.restore();
+      resetAuction();
+    });
+  });
 
   describe('getHighestCpm', () => {
     it('returns an array of winning bid objects for each adUnit', () => {
