f71ae7b66942f9de52a97e5593d318e456ad2e9d
Chris Combs <ccombs@gmail.com>
2016-12-05 19:17:49 -0500

    remove accidental commits

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gulp build --adapters ../washingtonian/dfp-ads/prebid-adapters-wash.json
-cp build/dev/prebid.js ../washingtonian/dfp-ads/assets/js/prebid-`date +%s`.js
-ls -l ../washingtonian/dfp-ads/assets/js/prebid*
