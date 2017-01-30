ec830c3d6c32680fcab61dc0473e6d859f69b9cf
m.sorochuk <m.sorochuk@vertamedia.com>
2017-01-30 15:56:06 +0200

    fix spaces

--- src/adapters/vertamedia.js
+++ src/adapters/vertamedia.js
@@ -8,94 +8,94 @@ import { STATUS } from 'src/constants';
 const ENDPOINT = '//rtb.vertamedia.com/hb/';
 
 function VertamediaAdapter() {
-    var baseAdapter = Adapter.createNew('vertamedia'),
-        bidRequest;
+  var baseAdapter = Adapter.createNew('vertamedia'),
+      bidRequest;
 
-    baseAdapter.callBids = function (bidRequests) {
-        if (!bidRequests || !bidRequests.bids || bidRequests.bids.length == 0) {
-            return;
-        }
-
-        var RTBDataParams = prepareAndSaveRTBRequestParams(bidRequests.bids[0]);
-
-        if (!RTBDataParams) {
-            return;
-        }
-
-        ajax(ENDPOINT, handleResponse, RTBDataParams, {
-            contentType: 'text/plain',
-            withCredentials: true,
-            method: 'GET'
-        });
-    };
-
-    function prepareAndSaveRTBRequestParams(bid) {
-        if (!bid || !bid.params || !bid.params.aid || !bid.placementCode) {
-            return;
-        }
-
-        bidRequest = bid;
-        bidRequest.width = parseInt(bid.sizes[0], 10) || undefined;
-        bidRequest.height = parseInt(bid.sizes[1], 10) || undefined;
-
-        return {
-            aid: bid.params.aid,
-            w: parseInt(bid.sizes[0], 10) || undefined,
-            h: parseInt(bid.sizes[1], 10) || undefined,
-            domain: document.location.hostname
-        };
+  baseAdapter.callBids = function (bidRequests) {
+    if (!bidRequests || !bidRequests.bids || bidRequests.bids.length === 0) {
+      return;
     }
 
-    /* Notify Prebid of bid responses so bids can get in the auction */
-    function handleResponse(response) {
-        var parsed;
+    var RTBDataParams = prepareAndSaveRTBRequestParams(bidRequests.bids[0]);
 
-        try {
-            parsed = JSON.parse(response);
-        } catch (error) {
-            utils.logError(error);
-        }
-
-        if (!parsed || parsed.error || !parsed.bids || !parsed.bids.length) {
-            utils.logError(`in response for ${baseAdapter.getBidderCode()} adapter`);
-            bidmanager.addBidResponse(bidRequest.placementCode, createBid(STATUS.NO_BID));
-
-            return;
-        }
-
-        bidmanager.addBidResponse(bidRequest.placementCode, createBid(STATUS.GOOD, parsed.bids[0]));
+    if (!RTBDataParams) {
+      return;
     }
 
-    function createBid(status, tag) {
-        var bid = bidfactory.createBid(status, tag);
+    ajax(ENDPOINT, handleResponse, RTBDataParams, {
+      contentType: 'text/plain',
+      withCredentials: true,
+      method: 'GET'
+    });
+  };
 
-        bid.code = baseAdapter.getBidderCode();
-        bid.bidderCode = bidRequest.bidder;
-
-        if (!tag || status !== STATUS.GOOD) {
-            return bid;
-        }
-
-        bid.mediaType = 'video';
-        bid.cpm = tag.cpm;
-        bid.creative_id = tag.cmpId;
-        bid.width = bidRequest.width;
-        bid.height = bidRequest.height;
-        bid.vastUrl = tag.url;
-
-        return bid;
+  function prepareAndSaveRTBRequestParams(bid) {
+    if (!bid || !bid.params || !bid.params.aid || !bid.placementCode) {
+      return;
     }
+
+    bidRequest = bid;
+    bidRequest.width = parseInt(bid.sizes[0], 10) || undefined;
+    bidRequest.height = parseInt(bid.sizes[1], 10) || undefined;
 
     return {
-        createNew: VertamediaAdapter.createNew,
-        callBids: baseAdapter.callBids,
-        setBidderCode: baseAdapter.setBidderCode
+      aid: bid.params.aid,
+      w: parseInt(bid.sizes[0], 10) || undefined,
+      h: parseInt(bid.sizes[1], 10) || undefined,
+      domain: document.location.hostname
     };
+  }
+
+  /* Notify Prebid of bid responses so bids can get in the auction */
+  function handleResponse(response) {
+    var parsed;
+
+    try {
+      parsed = JSON.parse(response);
+    } catch (error) {
+      utils.logError(error);
+    }
+
+    if (!parsed || parsed.error || !parsed.bids || !parsed.bids.length) {
+      utils.logError(`in response for ${baseAdapter.getBidderCode()} adapter`);
+      bidmanager.addBidResponse(bidRequest.placementCode, createBid(STATUS.NO_BID));
+
+      return;
+    }
+
+    bidmanager.addBidResponse(bidRequest.placementCode, createBid(STATUS.GOOD, parsed.bids[0]));
+  }
+
+  function createBid(status, tag) {
+    var bid = bidfactory.createBid(status, tag);
+
+    bid.code = baseAdapter.getBidderCode();
+    bid.bidderCode = bidRequest.bidder;
+
+    if (!tag || status !== STATUS.GOOD) {
+      return bid;
+    }
+
+    bid.mediaType = 'video';
+    bid.cpm = tag.cpm;
+    bid.creative_id = tag.cmpId;
+    bid.width = bidRequest.width;
+    bid.height = bidRequest.height;
+    bid.vastUrl = tag.url;
+
+    return bid;
+  }
+
+  return {
+    createNew: VertamediaAdapter.createNew,
+    callBids: baseAdapter.callBids,
+    setBidderCode: baseAdapter.setBidderCode
+  };
 
 }
 
 VertamediaAdapter.createNew = function () {
-    return new VertamediaAdapter();
+  return new VertamediaAdapter();
 };
 
 module.exports = VertamediaAdapter;
