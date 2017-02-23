9ad8b18badb96eaa512a56397fa7393924cdae57
Gary Kertis <gary.kertis@gmail.com>
2017-02-23 13:22:56 -0500

    Add initial bid response handling logic.

--- src/adapters/serverbid.js
+++ src/adapters/serverbid.js
@@ -1,40 +1,77 @@
-// var bidfactory = require('../bidfactory.js');
-// var bidmanager = require('../bidmanager.js');
+import { getBidRequest } from '../utils.js';
+
+var bidfactory = require('../bidfactory.js');
+var bidmanager = require('../bidmanager.js');
 var Ajax = require('../ajax');
 
 var BidderNameAdapter = function BidderNameAdapter() {
 
-  const BASE_URI = '//e.serverbid.com/ados';
+  const BASE_URI = '//e.serverbid.com/api/v2';
 
   var timestamp = Date.now();
 
-  var request = {
-    Placements: [],
-    Keywords: undefined,
-    Referrer: "",
-    IsAsync: true
-  };
-
-
   function _callBids(params) {
-    var bids = params.bids || [];
-    for (var i = 0; i < bids.length; i++) {
-      var bid = bids[i];
-      bid.params.D = bid.bidId;
-      request.Placements.push(bid.params);
-    }
 
     var data = {
-      t: timestamp,
-      request: JSON.stringify(request)
+      placements: [],
+      time: timestamp,
+      user: {}
     };
 
-    Ajax.ajax(BASE_URI, _responseCallback, data, { method: 'GET', withCredentials: false });
+    var bids = params.bids || [];
+    for (var i = 0; i < bids.length; i++) {
+
+      var bid = bids[i];
+
+      var bid_data = {
+        networkId: bid.params.networkId,
+        siteId: bid.params.siteId,
+        divName: bid.bidId,
+        adTypes: bid.params.adTypes,
+        includePricingData: true
+      };
+
+      // data.user.key = readCookie('azk');
+      // console.log(data.user.key);
+
+      data.placements.push(bid_data);
+    }
+
+    Ajax.ajax(BASE_URI, _responseCallback, JSON.stringify(data), { method: 'POST', withCredentials: false, contentType: 'application/json' });
 
   }
 
   function _responseCallback(result) {
-    console.log(result);
+
+    result = JSON.parse(result);
+
+    if (result && result.decisions) {
+      for (var key in result.decisions) {
+
+        var bidObj = getBidRequest(key);
+        var decision = result.decisions[key];
+        var bidCode;
+        var placementCode;
+
+        if (bidObj) {
+
+          var bid = bidfactory.createBid(1, bidObj);
+
+          bidCode = bidObj.bidder;
+          placementCode = bidObj.placementCode;
+
+          bid.bidderCode = bidCode;
+          bid.cpm = decision.pricing && decision.pricing.clearPrice || Math.floor(Math.random() * 10);
+          bid.width = decision.width;
+          bid.height = decision.height;
+
+          console.log(bid);
+
+          bidmanager.addBidResponse(placementCode, bid);
+
+        }
+      }
+    }
   }
 
   // Export the `callBids` function, so that Prebid.js can execute
@@ -42,7 +79,6 @@ var BidderNameAdapter = function BidderNameAdapter() {
   return {
     callBids: _callBids
   };
-
 
 };
 
