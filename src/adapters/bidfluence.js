79513118231ed0f18ae56f8b906054e10de1b52d
Francesco <francescocristallo@users.noreply.github.com>
2017-03-08 04:39:58 -0600

    Addressed change request (all but the loader one)
    -Changed window with $$PREBID_GLOBAL$$
    -Added bid request as second param to match request response pair
    -Didn't change the loader since does not support custom Id to script element, necessary for our code to work.

--- src/adapters/bidfluence.js
+++ src/adapters/bidfluence.js
@@ -3,21 +3,23 @@ const bidfactory = require('../bidfactory.js');
 const utils = require('../utils.js');
 
 var BidfluenceAdapter = function BidfluenceAdapter() {
-
     //Callback
-    window.bfPbjsCB = function (bfr) {
+
+    $$PREBID_GLOBAL$$.bfPbjsCB = function (bfr) {
+        var bidRequest = utils.getBidRequest(bfr.cbID);
         var bidObject = null;
         if (bfr.cpm > 0) {
-            bidObject = bidfactory.createBid(1);
+            bidObject = bidfactory.createBid(1, bidRequest);
             bidObject.bidderCode = 'bidfluence';
             bidObject.cpm = bfr.cpm;
             bidObject.ad = bfr.ad;
             bidObject.width = bfr.width;
             bidObject.height = bfr.height;
         } else {
-            bidObject = bidfactory.createBid(2);
+            bidObject = bidfactory.createBid(2, bidRequest);
             bidObject.bidderCode = 'bidfluence';
         }
+
         bidmanager.addBidResponse(bfr.placementCode, bidObject);
     };
 
@@ -35,9 +37,11 @@ var BidfluenceAdapter = function BidfluenceAdapter() {
         var publisherId = utils.getBidIdParameter('pubId', bid.params);
         var reservePrice = utils.getBidIdParameter('reservePrice', bid.params);
         var pbjsBfobj = {
-            placementCode: bid.placementCode
+            placementCode: bid.placementCode,
+            cbID: bid.bidId
         };
         /* jshint ignore:end */
+
         var s = document.createElement('script');
         s.async = true;
         s.id = adunitId;
@@ -56,4 +60,5 @@ var BidfluenceAdapter = function BidfluenceAdapter() {
         callBids: _callBids
     };
 };
+
 module.exports = BidfluenceAdapter;
