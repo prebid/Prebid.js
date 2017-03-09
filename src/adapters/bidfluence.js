1e2327412df815eed8c9d1fa90ae24d493202b24
Francesco <francescocristallo@users.noreply.github.com>
2017-03-08 21:24:53 -0600

    Fixed global variable causing validation error.

--- src/adapters/bidfluence.js
+++ src/adapters/bidfluence.js
@@ -5,54 +5,53 @@ const adloader = require('../adloader');
 
 var BidfluenceAdapter = function BidfluenceAdapter() {
 
-    const scriptUrl = "//bidfluence.azureedge.net/forge.js";
+  const scriptUrl = "//bidfluence.azureedge.net/forge.js";
 
-    $$PREBID_GLOBAL$$.bfPbjsCB = function (bfr) {
-        var bidRequest = utils.getBidRequest(bfr.cbID);
-        var bidObject = null;
-        if (bfr.cpm > 0) {
-            bidObject = bidfactory.createBid(1, bidRequest);
-            bidObject.bidderCode = 'bidfluence';
-            bidObject.cpm = bfr.cpm;
-            bidObject.ad = bfr.ad;
-            bidObject.width = bfr.width;
-            bidObject.height = bfr.height;
-        } else {
-            bidObject = bidfactory.createBid(2, bidRequest);
-            bidObject.bidderCode = 'bidfluence';
-        }
-
-        bidmanager.addBidResponse(bfr.placementCode, bidObject);
-    };
-
-    function _callBids(params) {
-        var bfbids = params.bids || [];
-        for (var i = 0; i < bfbids.length; i++) {
-            var bid = bfbids[i];
-            call(bid);
-        }
+  $$PREBID_GLOBAL$$.bfPbjsCB = function (bfr) {
+    var bidRequest = utils.getBidRequest(bfr.cbID);
+    var bidObject = null;
+    if (bfr.cpm > 0) {
+      bidObject = bidfactory.createBid(1, bidRequest);
+      bidObject.bidderCode = 'bidfluence';
+      bidObject.cpm = bfr.cpm;
+      bidObject.ad = bfr.ad;
+      bidObject.width = bfr.width;
+      bidObject.height = bfr.height;
+    } else {
+      bidObject = bidfactory.createBid(2, bidRequest);
+      bidObject.bidderCode = 'bidfluence';
     }
 
-    function call(bid) {
-       /* jshint ignore:start */
-        var adunitId = utils.getBidIdParameter('adunitId', bid.params);
-        var publisherId = utils.getBidIdParameter('pubId', bid.params);
-        var reservePrice = utils.getBidIdParameter('reservePrice', bid.params);
-        var pbjsBfobj = {
-            placementCode: bid.placementCode,
-            cbID: bid.bidId
-        };
-        /* jshint ignore:end */
-     
-        var cb = function () {
-            /* jshint ignore:start */
-            FORGE.init([adunitId, publisherId, pbjsBfobj, reservePrice]);
-            /* jshint ignore:end */
-        };
-        adloader.loadScript(scriptUrl, cb);
+    bidmanager.addBidResponse(bfr.placementCode, bidObject);
+  };
+
+  function _callBids(params) {
+    var bfbids = params.bids || [];
+    for (var i = 0; i < bfbids.length; i++) {
+      var bid = bfbids[i];
+      call(bid);
     }
-    return {
-        callBids: _callBids
+  }
+  function call(bid) {
+
+    var adunitId = utils.getBidIdParameter('adunitId', bid.params);
+    var publisherId = utils.getBidIdParameter('pubId', bid.params);
+    var reservePrice = utils.getBidIdParameter('reservePrice', bid.params);
+    var pbjsBfobj = {
+      placementCode: bid.placementCode,
+      cbID: bid.bidId
+    }; 
+
+    var cb = function () {
+      /* globals FORGE */
+      FORGE.init([adunitId, publisherId, pbjsBfobj, reservePrice]);
     };
+
+    adloader.loadScript(scriptUrl, cb);
+  }
+  return {
+    callBids: _callBids
+  };
 };
+
 module.exports = BidfluenceAdapter;
