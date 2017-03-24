4186cdcbfeb749a35a8e7671cf17d9fe58c5ec22
Eric "dantarion" Sheppard <dantarion@dantarion.com>
2017-03-24 14:40:58 -0700

    Rewrite of UnderdogMedia Adapter (#1061)
    * Rewrite of UnderdogMedia Adapter
    
    * underdogmedia adapter: ensure compatibility with single array size param, ensure cpm is a Number

--- test/spec/adapters/underdogmedia_spec.js
+++ test/spec/adapters/underdogmedia_spec.js
@@ -2,96 +2,76 @@
 
 import Adapter from '../../../src/adapters/underdogmedia';
 import bidManager from '../../../src/bidmanager';
-import {expect} from 'chai';
+import adloader from '../../../src/adloader';
 
-describe('underdog media adapter test', () => {
+import {
+  expect
+} from 'chai';
+
+describe('underdogmedia adapter test', () => {
 
   let adapter;
   let server;
 
-  // Minimal stub implementation of underdog media header bid API
-  // This will prevent the need to load underdog's static library, and to make requests to underdog's server
-  window.udm_header_lib = {
+  // The third bid here is an invalid site id and should return a 'no-bid'.
 
-    BidRequest: function(options){
-      return {
-        send: function(){
-            var siteId = options.siteId;
-            if(siteId == 10272){
-              // Only bid on this particular site id
-              var bids = [];
-              options.sizes.forEach(function(size){
-                bids.push({
-                  cpm: 3.14,
-                  ad_html: `Ad HTML for site ID ${siteId} size ${size[0]}x${size[1]}`,
-                  width:   size[0],
-                  height:  size[1]
-                });
-              });
-              options.callback({
-                bids: bids
-              });
-            } else {
-              options.callback({
-                bids: []
-              });
-            }
-
+  var bidderRequest = {
+    bidderCode: 'underdogmedia',
+    bids: [{
+        bidder: 'underdogmedia',
+        adUnitCode: 'foo',
+        sizes: [
+          [728, 90]
+        ],
+        params: {
+          siteId: '10272'
         }
-      };
-    },
-
-    BidRequestArray: function(arr){
-      return {
-        send: function(){
-          arr.forEach(function(bidRequest){
-            var req = new window.udm_header_lib.BidRequest(bidRequest);
-            req.send();
-          });
+      },
+      {
+        bidder: 'underdogmedia',
+        adUnitCode: 'bar',
+        sizes: [
+          [300, 250]
+        ],
+        params: {
+          siteId: '10272',
+          subId: 'TEST_SUBID'
         }
-      };
-    }
+      },
+      {
+        bidder: 'underdogmedia',
+        adUnitCode: 'nothing',
+        sizes: [160, 600],
+        params: {
+          siteId: '31337'
+        }
+      }
+    ]
+  };
+  var response = {
+    "mids": [{
+        "width": 728,
+        "notification_url": "//udmserve.net/notification_url",
+        "height": 90,
+        "cpm": 2.5,
+        "ad_code_html": "Ad HTML for site ID 10272 size 728x90"
+      },
+      {
+        "width": 300,
+        "notification_url": "//udmserve.net/notification_url",
+        "height": 250,
+        "cpm": 2.0,
+        "ad_code_html": "Ad HTML for site ID 10272 size 300x250"
+      }
+    ]
   };
 
-  // The third bid here is an invalid site id and should return a 'no-bid'.
-  function request() {
-    adapter.callBids({
-      bidderCode: 'underdogmedia',
-      bids: [
-        {
-          bidder: 'underdogmedia',
-          adUnitCode: 'foo',
-          sizes: [[728, 90]],
-          params: {
-            siteId: '10272'
-          }
-        },
-        {
-          bidder: 'underdogmedia',
-          adUnitCode: 'bar',
-          sizes: [[300, 250]],
-          params: {
-            siteId: '10272'
-          }
-        },
-        {
-          bidder: 'underdogmedia',
-          adUnitCode: 'nothing',
-          sizes: [[160, 600]],
-          params: {
-            siteId: '31337'
-          }
-        }
-      ]
-    });
-  }
 
   beforeEach(() => {
     adapter = new Adapter();
   });
 
-  afterEach(() => {
-  });
+  afterEach(() => {});
 
   describe('adding bids to the manager', () => {
 
@@ -101,7 +81,10 @@ describe('underdog media adapter test', () => {
 
     beforeEach(() => {
       sinon.stub(bidManager, 'addBidResponse');
-      request();
+      sinon.stub(adloader, 'loadScript');
+
+      adapter.callBids(bidderRequest);
+      $$PREBID_GLOBAL$$.handleUnderdogMediaCB(JSON.parse(JSON.stringify(response)));
       firstBid = bidManager.addBidResponse.firstCall.args[1];
       secondBid = bidManager.addBidResponse.secondCall.args[1];
       thirdBid = bidManager.addBidResponse.thirdCall.args[1];
@@ -109,6 +92,7 @@ describe('underdog media adapter test', () => {
 
     afterEach(() => {
       bidManager.addBidResponse.restore();
+      adloader.loadScript.restore();
     });
 
     it('will add a bid object for each bid', () => {
@@ -116,12 +100,14 @@ describe('underdog media adapter test', () => {
     });
 
     it('will add the ad html to the bid object', () => {
-      expect(firstBid).to.have.property('ad', 'Ad HTML for site ID 10272 size 728x90');
-      expect(secondBid).to.have.property('ad', 'Ad HTML for site ID 10272 size 300x250');
+
+      expect(firstBid).to.have.property('ad').includes('Ad HTML for site ID 10272 size 728x90');
+      expect(secondBid).to.have.property('ad').includes('Ad HTML for site ID 10272 size 300x250').and.includes('TEST_SUBID');
       expect(thirdBid).to.not.have.property('ad');
     });
 
     it('will have the right size attached', () => {
+
       expect(firstBid).to.have.property('width', 728);
       expect(firstBid).to.have.property('height', 90);
       expect(secondBid).to.have.property('width', 300);
@@ -129,8 +115,8 @@ describe('underdog media adapter test', () => {
     });
 
     it('will add the CPM to the bid object', () => {
-      expect(firstBid).to.have.property('cpm', 3.14);
-      expect(secondBid).to.have.property('cpm', 3.14);
+      expect(firstBid).to.have.property('cpm', 2.5);
+      expect(secondBid).to.have.property('cpm', 2.0);
       expect(thirdBid).to.not.have.property('cpm');
     });
 
