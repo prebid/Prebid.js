5609681303953de82ad80c7daa4973587d9c2467
GLStephen <stephenj@launchpowered.com>
2017-05-05 12:33:28 +0000

    Updates based on Feedback

--- src/adapters/analytics/pubwiseanalytics.js
+++ src/adapters/analytics/pubwiseanalytics.js
@@ -1,6 +1,5 @@
 import {ajax} from 'src/ajax';
 import adapter from 'AnalyticsAdapter';
-import CONSTANTS from 'src/constants.json';
 const utils = require('../../utils');
 
 /****
@@ -9,22 +8,10 @@ const utils = require('../../utils');
  * Developer: Stephen Johnston
  */
 
-const AUCTION_INIT = CONSTANTS.EVENTS.AUCTION_INIT;
-const BID_REQUESTED = CONSTANTS.EVENTS.BID_REQUESTED;
-const BID_RESPONSE = CONSTANTS.EVENTS.BID_RESPONSE;
-const BID_WON = CONSTANTS.EVENTS.BID_WON;
-
-/****
- *  later implementation
- *  const BID_TIMEOUT = CONSTANTS.EVENTS.BID_TIMEOUT;
- *  const BID_ADJUSTMENT = CONSTANTS.EVENTS.BID_ADJUSTMENT;
- *  const AUCTION_END = CONSTANTS.EVENTS.AUCTION_END;
- */
-
 const analyticsType = 'endpoint';
 let target_site = 'unknown';
 let target_url = 'https://staging.api.pubwise.io';
-let pw_version = '2.1.2';
+let pw_version = '2.1.3';
 
 let pubwiseAnalytics = Object.assign(adapter(
   {
@@ -40,27 +27,19 @@ let pubwiseAnalytics = Object.assign(adapter(
        it is available on the first call and we can setup our config. Potential additional
        PR for later, but this solves this for now.
        */
-      if (args !== undefined && args.config !== undefined) {
+      if (args !== undefined && args.config !== undefined && args.config.site !== undefined && args.config.endpoint !== undefined) {
         target_site = args.config.site;
-      }
-      if (args !== undefined && args.config !== undefined) {
         target_url = args.config.endpoint;
       }
-      utils.logMessage('Sending PubWise Analytics Event ' + eventType);
-      if (eventType === AUCTION_INIT || eventType === BID_RESPONSE || eventType === BID_REQUESTED || eventType === BID_WON) {
-        ajax(target_url,
-          (result) => utils.logInfo('PubWise Analytics Result', result), JSON.stringify({
+      utils.logInfo('Sending PubWise Analytics Event ' + eventType, args);
+      ajax(target_url,
+        (result) => utils.logInfo('PubWise Analytics Result', result), JSON.stringify({
           eventType,
           args,
           target_site,
           pw_version
         })
-      )
-        ;
-      }
+      );
     }
   });
-
 export default pubwiseAnalytics;
-
-
