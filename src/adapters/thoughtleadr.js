dc636168f2aa62ead8b11a49cb5000dd6b786fdd
Ilya Pirogov <ilja.pirogov@gmail.com>
2017-05-08 12:11:08 -0700

    two stage of bidding thoughtleadr

--- src/adapters/thoughtleadr.js
+++ src/adapters/thoughtleadr.js
@@ -1,29 +1,47 @@
 "use strict";
 var bidfactory = require("../bidfactory");
 var bidmanager = require("../bidmanager");
-var utils = require('../utils');
-var adloader_1 = require("../adloader");
-var ROOT_URL = "//cdn.thoughtleadr.com/v4/";
+var utils = require("../utils");
+var ajax_1 = require("../ajax");
+var UID_KEY = "tldr_uid";
+var URL_API = "//a.thoughtleadr.com/v4";
+var URL_CDN = "//cdn.thoughtleadr.com/v4";
 var BID_AVAILABLE = 1;
+var BID_UNAVAILABLE = 2;
+
+function getVal(key) {
+  if ("localStorage" in window) {
+    return localStorage[key];
+  }
+  if ("sessionStorage" in window) {
+    return sessionStorage[key];
+  }
+  return null;
+}
+
+function setVal(key, val) {
+  if ("localStorage" in window) {
+    localStorage[key] = val;
+  }
+  if ("sessionStorage" in window) {
+    sessionStorage[key] = val;
+  }
+}
+
+function getUid() {
+  var uid = getVal(UID_KEY);
+  if (!uid) {
+    uid = utils.generateUUID(null);
+    setVal(UID_KEY, uid);
+  }
+  return uid;
+}
 
 var ThoughtleadrAdapter = (function () {
   function ThoughtleadrAdapter() {
   }
 
   ThoughtleadrAdapter.prototype.callBids = function (params) {
-    if (!window.tldr || !window.tldr.requestPrebid) {
-      var rootUrl = ROOT_URL;
-      if (window.tldr && window.tldr.config && window.tldr.config.root_url) {
-        rootUrl = window.tldr.config.root_url;
-      }
-      adloader_1.loadScript(rootUrl + "page.js", this.handleBids.bind(this, params), true);
-    }
-    else {
-      this.handleBids(params);
-    }
-  };
-
-  ThoughtleadrAdapter.prototype.handleBids = function (params) {
     var bids = (params.bids || []).filter(function (bid) {
       return ThoughtleadrAdapter.valid(bid);
     });
@@ -35,50 +53,26 @@ var ThoughtleadrAdapter = (function () {
   };
 
   ThoughtleadrAdapter.prototype.requestPlacement = function (bid) {
-    var _this = this;
-    var rid = utils.generateUUID(null);
+    var uid = getUid();
     var size = ThoughtleadrAdapter.getSizes(bid.sizes);
 
-    window.tldr.requestPrebid(bid.params.placementId, rid).then(function (params) {
-      if (!params || !params.bid) {
-        utils.logError("invalid response from tldr.requestPrebid", undefined, undefined);
-        return;
-      }
-
-      _this.receiver = function (ev) {
-        if (ev.origin === location.origin &&
-          ev.data && ev.data.TLDR_REQUEST && ev.data.TLDR_REQUEST.rid === rid) {
-          ev.source.postMessage({TLDR_RESPONSE: {config: params.config, rid: rid}}, location.origin);
-        }
-        _this.stopListen();
-      };
-      window.addEventListener("message", _this.receiver, false);
-      setTimeout(function () {
-        return _this.stopListen();
-      }, 5000);
-
+    ajax_1.ajax(URL_API + "/" + bid.placementCode + "/bid.json?uid=" + uid, function (wonBid) {
       var bidObject;
-      if (params.bid.code === BID_AVAILABLE) {
-        bidObject = bidfactory.createBid(params.bid.code);
+      if (wonBid && wonBid.bid_amount) {
+        bidObject = bidfactory.createBid(BID_AVAILABLE);
         bidObject.bidderCode = 'thoughtleadr';
-        bidObject.cpm = params.bid.cpm;
-        bidObject.ad = params.bid.ad;
+        bidObject.cpm = wonBid.bid_amount;
+        bidObject.ad =
+          "<script src=\"" + URL_CDN + "/bid.js\" header-bid-token=\"" + wonBid.header_bid_token + "\"></script>";
         bidObject.width = size.width;
         bidObject.height = size.height;
       }
       else {
-        bidObject = bidfactory.createBid(params.bid.code);
+        bidObject = bidfactory.createBid(BID_UNAVAILABLE);
         bidObject.bidderCode = 'thoughtleadr';
       }
       bidmanager.addBidResponse(bid.placementCode, bidObject);
-    });
-  };
-
-  ThoughtleadrAdapter.prototype.stopListen = function () {
-    if (this.receiver) {
-      window.removeEventListener("message", this.receiver);
-      this.receiver = undefined;
-    }
+    }, null);
   };
 
   ThoughtleadrAdapter.valid = function (bid) {
@@ -92,7 +86,7 @@ var ThoughtleadrAdapter = (function () {
     }
     return {
       width: sizes[0],
-      height: sizes[1]
+      height: sizes[1],
     };
   };
   return ThoughtleadrAdapter;
