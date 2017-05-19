d8794cc477a0591552ed2e703486357aca4b155c
Julien Boulanger <jb@adyoulike.com>
2017-05-19 17:27:44 +0200

    update adyoulike.js and adyoulike_spec.js syntax

--- src/adapters/adyoulike.js
+++ src/adapters/adyoulike.js
@@ -7,29 +7,30 @@ import { ajax } from 'src/ajax';
 import { STATUS } from 'src/constants';
 
 var AdyoulikeAdapter = function AdyoulikeAdapter() {
-  const _VERSION = "0.1";
+  const _VERSION = '0.1';
 
   const baseAdapter = Adapter.createNew('adyoulike');
 
   baseAdapter.callBids = function (bidRequest) {
-      const bidRequests = {};
-      const bids = bidRequest.bids || [];
+    const bidRequests = {};
+    const bids = bidRequest.bids || [];
 
-      const validBids = bids.filter(valid);
-      validBids.forEach(bid => { bidRequests[bid.params.placement] = bid; });
+    const validBids = bids.filter(valid);
+    validBids.forEach(bid => { bidRequests[bid.params.placement] = bid; });
 
-      const placements = validBids.map(bid => bid.params.placement);
-      if (!utils.isEmpty(placements)) {
-        const body = createBody(placements);
-        const endpoint = createEndpoint();
-        ajax(endpoint, (response) => {
+    const placements = validBids.map(bid => bid.params.placement);
+    if (!utils.isEmpty(placements)) {
+      const body = createBody(placements);
+      const endpoint = createEndpoint();
+      ajax(endpoint,
+        (response) => {
           handleResponse(bidRequests, response);
         }, body, {
           contentType: 'text/json',
           withCredentials: true
         });
-      }
-    };
+    }
+  };
 
   /* Create endpoint url */
   function createEndpoint() {
@@ -44,20 +45,20 @@ var AdyoulikeAdapter = function AdyoulikeAdapter() {
   /* Create endpoint query string */
   function createEndpointQS() {
     const qs = {};
-    
+
     const ref = getReferrerUrl();
-    if(ref){
+    if (ref) {
       qs.RefererUrl = encodeURIComponent(ref);
     }
 
     const can = getCanonicalUrl();
-    if(can){
+    if (can) {
       qs.CanonicalUrl = encodeURIComponent(can);
     }
 
     return qs;
   }
-  
+
   /* Create request body */
   function createBody(placements) {
     const body = {
@@ -92,7 +93,7 @@ var AdyoulikeAdapter = function AdyoulikeAdapter() {
   /* Check that a bid has required parameters */
   function valid(bid) {
     const sizes = getSize(bid.sizes);
-    if(!bid.params.placement || !sizes.width || !sizes.height){
+    if (!bid.params.placement || !sizes.width || !sizes.height) {
       return false;
     }
     return true;
@@ -102,16 +103,16 @@ var AdyoulikeAdapter = function AdyoulikeAdapter() {
   function getReferrerUrl() {
     try {
       return window.top.document.referrer;
-    } catch (e) {}
+    } catch (e) { }
     return document.referrer;
   }
-  
+
   /* Get current page canonical url */
   function getCanonicalUrl() {
-    if(utils.inIframes){
-      try{
+    if (utils.inIframes) {
+      try {
         return (window.top.doucment.head.querySelector('link[rel="canonical"][href]') || {}).href;
-      }catch(e){}
+      } catch (e) { }
     }
     return (document.head.querySelector('link[rel="canonical"][href]') || {}).href;
   }
@@ -119,7 +120,7 @@ var AdyoulikeAdapter = function AdyoulikeAdapter() {
   /* Get parsed size from request size */
   function getSize(requestSizes) {
     const parsed = {},
-        size = utils.parseSizesInput(requestSizes)[0];
+      size = utils.parseSizesInput(requestSizes)[0];
 
     if (typeof size !== 'string') {
       return parsed;
@@ -127,24 +128,24 @@ var AdyoulikeAdapter = function AdyoulikeAdapter() {
 
     const parsedSize = size.toUpperCase().split('X');
     const width = parseInt(parsedSize[0], 10);
-    if(width){
+    if (width) {
       parsed.width = width;
     }
 
     const height = parseInt(parsedSize[1], 10);
-    if(height){
+    if (height) {
       parsed.height = height;
     }
 
     return parsed;
   }
-  
+
   /* Create bid from response */
   function createBid(placementId, bidRequest, response) {
     let bid;
-    if(!response || !response.Banner){
+    if (!response || !response.Banner) {
       bid = bidfactory.createBid(STATUS.NO_BID, bidRequest);
-    }else{
+    } else {
       bid = bidfactory.createBid(STATUS.GOOD, bidRequest);
       const size = getSize(bidRequest.sizes);
       bid.width = size.width;
@@ -157,10 +158,10 @@ var AdyoulikeAdapter = function AdyoulikeAdapter() {
 
     return bid;
   }
-  
+
   /* Add response to bidmanager */
-  function addResponse(placementId, bidRequest, response){
-    const bid = createBid(placementId,bidRequest, response);
+  function addResponse(placementId, bidRequest, response) {
+    const bid = createBid(placementId, bidRequest, response);
     const placement = bidRequest.placementCode;
     bidmanager.addBidResponse(placement, bid);
   }
--- test/spec/adapters/adyoulike_spec.js
+++ test/spec/adapters/adyoulike_spec.js
@@ -1,118 +1,118 @@
-import {expect} from "chai";
-import {parse} from "../../../src/url";
-import AdyoulikAdapter from "../../../src/adapters/adyoulike";
+import { expect } from 'chai';
+import { parse } from '../../../src/url';
+import AdyoulikAdapter from '../../../src/adapters/adyoulike';
 import bidmanager from 'src/bidmanager';
 import { STATUS } from 'src/constants';
 
-describe("Adyoulike Adapter", () => {
-  const endpoint = "http://hb-api.omnitagjs.com/hb-api/prebid";
-  const canonicalUrl = "http://canonical.url/?t=%26";
-  const bidderCode = "adyoulike";
+describe('Adyoulike Adapter', () => {
+  const endpoint = 'http://hb-api.omnitagjs.com/hb-api/prebid';
+  const canonicalUrl = 'http://canonical.url/?t=%26';
+  const bidderCode = 'adyoulike';
   const bidRequestWithEmptyPlacement = {
-    "bidderCode": "adyoulike",
-    "bids": [
-        {
-          "bidId": "bid_id_0",
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-0",
-          "params": {},
-          "sizes":"300x250"
-        }
+    'bidderCode': 'adyoulike',
+    'bids': [
+      {
+        'bidId': 'bid_id_0',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-0',
+        'params': {},
+        'sizes': '300x250'
+      }
     ],
   };
   const bidRequestWithEmptySizes = {
-    "bidderCode": "adyoulike",
-    "bids": [
-        {
-          "bidId": "bid_id_0",
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-0",
-          "params": {
-            "placement": "placement_0"
-          }
+    'bidderCode': 'adyoulike',
+    'bids': [
+      {
+        'bidId': 'bid_id_0',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-0',
+        'params': {
+          'placement': 'placement_0'
         }
+      }
     ],
   };
   const bidRequestWithSinglePlacement = {
-    "bidderCode": "adyoulike",
-    "bids": [
-        {
-          "bidId": "bid_id_0",
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-0",
-          "params": {
-            "placement": "placement_0"
-          },
-          "sizes":"300x250"
-        }
+    'bidderCode': 'adyoulike',
+    'bids': [
+      {
+        'bidId': 'bid_id_0',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-0',
+        'params': {
+          'placement': 'placement_0'
+        },
+        'sizes': '300x250'
+      }
     ],
   };
   const bidRequestMultiPlacements = {
-    "bidderCode": "adyoulike",
-    "bids": [
-        {
-          "bidId": "bid_id_0",            
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-0",
-          "params": {
-            "placement": "placement_0"
-          },
-          "sizes":"300x250"
+    'bidderCode': 'adyoulike',
+    'bids': [
+      {
+        'bidId': 'bid_id_0',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-0',
+        'params': {
+          'placement': 'placement_0'
         },
-        {
-          "bidId": "bid_id_1",         
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-1",
-          "params": {
-            "placement": "placement_1"
-          },
-          "sizes":[[300,600]]
+        'sizes': '300x250'
+      },
+      {
+        'bidId': 'bid_id_1',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-1',
+        'params': {
+          'placement': 'placement_1'
         },
-        {
-          "bidId": "bid_id_2",            
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-2",
-          "params": {},
-          "sizes":"300x400"
-        },
-        {
-          "bidId": "bid_id_3",            
-          "bidder": "adyoulike",
-          "placementCode": "adunit/hb-3",
-          "params": {
-            "placement": "placement_3"
-          }
+        'sizes': [[300, 600]]
+      },
+      {
+        'bidId': 'bid_id_2',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-2',
+        'params': {},
+        'sizes': '300x400'
+      },
+      {
+        'bidId': 'bid_id_3',
+        'bidder': 'adyoulike',
+        'placementCode': 'adunit/hb-3',
+        'params': {
+          'placement': 'placement_3'
         }
+      }
     ],
   };
-  
+
   const responseWithEmptyPlacement = [
     {
-      "Placement": "placement_0"
+      'Placement': 'placement_0'
     }
   ];
   const responseWithSinglePlacement = [
     {
-      "Placement": "placement_0",
-      "Banner": "placement_0",
-      "Price":0.5
+      'Placement': 'placement_0',
+      'Banner': 'placement_0',
+      'Price': 0.5
     }
   ];
   const responseWithMultiplePlacements = [
     {
-      "Placement": "placement_0",
-      "Banner": "placement_0",
-      "Price":0.5
+      'Placement': 'placement_0',
+      'Banner': 'placement_0',
+      'Price': 0.5
     },
     {
-      "Placement": "placement_1",
-      "Banner": "placement_1",
-      "Price":0.6
+      'Placement': 'placement_1',
+      'Banner': 'placement_1',
+      'Price': 0.6
     }
   ];
 
-  let adapter;  
-  
+  let adapter;
+
   beforeEach(() => {
     adapter = new AdyoulikAdapter();
   });
@@ -131,15 +131,15 @@ describe("Adyoulike Adapter", () => {
     });
   });
 
-  describe("request function", () => {
+  describe('request function', () => {
     let xhr;
     let requests;
     let canonical;
     let addBidResponse;
-    
+
     beforeEach(() => {
-      canonical = document.createElement("link");
-      canonical.rel = "canonical";
+      canonical = document.createElement('link');
+      canonical.rel = 'canonical';
       canonical.href = canonicalUrl;
       document.head.appendChild(canonical);
 
@@ -147,7 +147,7 @@ describe("Adyoulike Adapter", () => {
       xhr = sinon.useFakeXMLHttpRequest();
       xhr.onCreate = request => requests.push(request);
 
-      addBidResponse = sinon.stub(bidmanager, 'addBidResponse');  
+      addBidResponse = sinon.stub(bidmanager, 'addBidResponse');
     });
 
     afterEach(() => {
@@ -156,61 +156,61 @@ describe("Adyoulike Adapter", () => {
       document.head.removeChild(canonical);
     });
 
-    it("requires placement request", () => {
+    it('requires placement request', () => {
       adapter.callBids(bidRequestWithEmptyPlacement);
       expect(requests).to.be.empty;
       expect(addBidResponse.calledOnce).to.equal(false);
     });
 
-    it("requires sizes in request", () => {
+    it('requires sizes in request', () => {
       adapter.callBids(bidRequestWithEmptySizes);
       expect(requests).to.be.empty;
       expect(addBidResponse.calledOnce).to.equal(false);
     });
 
-    it("sends bid request to endpoint with single placement", () => {
+    it('sends bid request to endpoint with single placement', () => {
       adapter.callBids(bidRequestWithSinglePlacement);
       expect(requests[0].url).to.contain(endpoint);
-      expect(requests[0].method).to.equal("POST");
+      expect(requests[0].method).to.equal('POST');
 
-      expect(requests[0].url).to.contains("CanonicalUrl="+encodeURIComponent(canonicalUrl));
-      
+      expect(requests[0].url).to.contains('CanonicalUrl=' + encodeURIComponent(canonicalUrl));
+
       let body = JSON.parse(requests[0].requestBody);
-      expect(body.Version).to.equal("0.1");  
-      expect(body.Placements).deep.equal(["placement_0"]); 
-      expect(body.PageRefreshed).to.equal(false); 
+      expect(body.Version).to.equal('0.1');
+      expect(body.Placements).deep.equal(['placement_0']);
+      expect(body.PageRefreshed).to.equal(false);
     });
 
-    it("sends bid request to endpoint with multiple placements", () => {
+    it('sends bid request to endpoint with multiple placements', () => {
       adapter.callBids(bidRequestMultiPlacements);
       expect(requests[0].url).to.contain(endpoint);
-      expect(requests[0].method).to.equal("POST");
+      expect(requests[0].method).to.equal('POST');
 
-      expect(requests[0].url).to.contains("CanonicalUrl="+encodeURIComponent(canonicalUrl));
+      expect(requests[0].url).to.contains('CanonicalUrl=' + encodeURIComponent(canonicalUrl));
 
       let body = JSON.parse(requests[0].requestBody);
-      expect(body.Version).to.equal("0.1");  
-      expect(body.Placements).deep.equal(["placement_0", "placement_1"]); 
-      expect(body.PageRefreshed).to.equal(false); 
+      expect(body.Version).to.equal('0.1');
+      expect(body.Placements).deep.equal(['placement_0', 'placement_1']);
+      expect(body.PageRefreshed).to.equal(false);
     });
   });
 
-  describe("response function", () => {
+  describe('response function', () => {
     let server;
     let addBidResponse;
 
-    beforeEach( () => {
+    beforeEach(() => {
       server = sinon.fakeServer.create();
       addBidResponse = sinon.stub(bidmanager, 'addBidResponse');
     });
 
-    afterEach( () => {
+    afterEach(() => {
       server.restore();
       bidmanager.addBidResponse.restore();
     });
 
     it('invalid json', () => {
-      server.respondWith("{");
+      server.respondWith('{');
       adapter.callBids(bidRequestWithSinglePlacement);
       server.respond();
 
@@ -240,7 +240,7 @@ describe("Adyoulike Adapter", () => {
       expect(addBidResponse.args[0]).to.have.lengthOf(2);
       expect(addBidResponse.args[0][1].getStatusCode()).to.equal(STATUS.GOOD);
       expect(addBidResponse.args[0][1].cpm).to.equal(0.5);
-      expect(addBidResponse.args[0][1].ad).to.equal("placement_0");
+      expect(addBidResponse.args[0][1].ad).to.equal('placement_0');
       expect(addBidResponse.args[0][1].width).to.equal(300);
       expect(addBidResponse.args[0][1].height).to.equal(250);
     });
@@ -256,20 +256,19 @@ describe("Adyoulike Adapter", () => {
       expect(addBidResponse.args[0][1].getStatusCode()).to.equal(STATUS.GOOD);
       expect(addBidResponse.args[0][1].bidderCode).to.equal(bidderCode);
       expect(addBidResponse.args[0][1].cpm).to.equal(0.5);
-      expect(addBidResponse.args[0][1].ad).to.equal("placement_0");
+      expect(addBidResponse.args[0][1].ad).to.equal('placement_0');
       expect(addBidResponse.args[0][1].width).to.equal(300);
       expect(addBidResponse.args[0][1].height).to.equal(250);
-      
+
       expect(addBidResponse.args[1]).to.have.lengthOf(2);
       expect(addBidResponse.args[1][1].getStatusCode()).to.equal(STATUS.GOOD);
       expect(addBidResponse.args[1][1].bidderCode).to.equal(bidderCode);
       expect(addBidResponse.args[1][1].cpm).to.equal(0.6);
-      expect(addBidResponse.args[1][1].ad).to.equal("placement_1");
+      expect(addBidResponse.args[1][1].ad).to.equal('placement_1');
       expect(addBidResponse.args[1][1].width).to.equal(300);
       expect(addBidResponse.args[1][1].height).to.equal(600);
-  
     });
-    
+
     it('receive reponse with invalid placement number', () => {
       server.respondWith(JSON.stringify(responseWithSinglePlacement));
       adapter.callBids(bidRequestMultiPlacements);
@@ -281,7 +280,7 @@ describe("Adyoulike Adapter", () => {
       expect(addBidResponse.args[0][1].getStatusCode()).to.equal(STATUS.GOOD);
       expect(addBidResponse.args[0][1].bidderCode).to.equal(bidderCode);
       expect(addBidResponse.args[0][1].cpm).to.equal(0.5);
-      expect(addBidResponse.args[0][1].ad).to.equal("placement_0");
+      expect(addBidResponse.args[0][1].ad).to.equal('placement_0');
       expect(addBidResponse.args[0][1].width).to.equal(300);
       expect(addBidResponse.args[0][1].height).to.equal(250);
 
