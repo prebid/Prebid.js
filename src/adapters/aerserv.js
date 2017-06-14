45c257c46ee6c248cc12d574cfbc3d92e8c82463
Daniel Lawrence <dlawrence@aerserv.com>
2017-06-14 09:54:14 -0700

    added unit tests, slight refactor/formatting of adapter code, added to adapters json

--- src/adapters/aerserv.js
+++ src/adapters/aerserv.js
@@ -16,14 +16,13 @@ const AerServAdapter = function AerServAdapter() {
   const VIDEO_PATH = '/as/json/pbjsvast/v1?';
   const BIDDER_CODE = 'aerserv';
   const REQUIRED_PARAMS = ['plc'];
-  const VIDEO_TARGETING = [];
 
   function _isResponseValid(bidRequest, response) {
     return ((bidRequest.mediaType === 'video' && response.vastUrl) || (bidRequest.mediaType !== 'video' && response.adm)) &&
     response.cpm && response.cpm > 0;
   }
 
-  function _addBidResponse(bidRequest, response) {
+  function _createBid(bidRequest, response) {
     if (_isResponseValid(bidRequest, response)) {
       let bid = bidfactory.createBid(1, bidRequest);
       bid.bidderCode = BIDDER_CODE;
@@ -45,46 +44,44 @@ const AerServAdapter = function AerServAdapter() {
   function _buildQueryParameters(bidRequest, requestParams) {
     Object.keys(bidRequest.params).forEach(param => requestParams[param] = bidRequest.params[param]);
 
-    if (bidRequest.params.video) {
-      Object.keys(bidRequest.params.video)
-      .filter(param => VIDEO_TARGETING.includes(param))
-      .forEach(param => requestParams[param] = bidRequest.params.video[param]);
-    }
-
     return utils.parseQueryStringParameters(requestParams);
   }
 
-  function _callBids(params) {
+  function _handleResponse(bidRequest) {
+    return response => {
+      if (!response && response.length <= 0) {
+        bidmanager.addBidResponse(bidRequest.placementCode, bidfactory.createBid(STATUS.NO_BID, bidRequest));
+        utils.logError('Empty response');
+        return;
+      }
+
+      try {
+        response = JSON.parse(response);
+      } catch (e) {
+        bidmanager.addBidResponse(bidRequest.placementCode, bidfactory.createBid(STATUS.NO_BID, bidRequest));
+        utils.logError('Invalid JSON in response');
+        return;
+      }
+
+      _createBid(bidRequest, response);
+    };
+  }
+
+  function _callBids(bidRequests) {
     let currentUrl = (window.parent !== window) ? document.referrer : window.location.href;
     currentUrl = currentUrl && encodeURIComponent(currentUrl);
 
-    let bids = params.bids || [];
+    let bids = bidRequests.bids || [];
     bids.forEach(bidRequest => {
       if (utils.hasValidBidRequest(bidRequest.params, REQUIRED_PARAMS, BIDDER_CODE)) {
         let env = ENVIRONMENTS[bidRequest.params['env']] || ENVIRONMENTS['prod'];
         let requestPath = bidRequest.mediaType === 'video' ? VIDEO_PATH : BANNER_PATH;
-        let requestParams = {url: currentUrl};
-        let parameterStr = _buildQueryParameters(bidRequest, requestParams);
+        let pageParameters = {url: currentUrl};
+        let parameterStr = _buildQueryParameters(bidRequest, pageParameters);
 
         let url = `//${env}${requestPath}${parameterStr}`;
         utils.logMessage('sending request to: ' + url);
-        ajax(url, response => {
-          if (!response && response.length <= 0) {
-            bidmanager.addBidResponse(bidRequest.placementCode, bidfactory.createBid(STATUS.NO_BID, bidRequest));
-            utils.logError('Empty response');
-            return;
-          }
-
-          try {
-            response = JSON.parse(response);
-          } catch (e) {
-            bidmanager.addBidResponse(bidRequest.placementCode, bidfactory.createBid(STATUS.NO_BID, bidRequest));
-            utils.logError('Invalid JSON in response');
-            return;
-          }
-
-          _addBidResponse(bidRequest, response);
-        }, null, {withCredentials: true});
+        ajax(url, _handleResponse(bidRequest), null, {withCredentials: true});
       } else {
         bidmanager.addBidResponse(bidRequest.placementCode, bidfactory.createBid(STATUS.NO_BID, bidRequest));
       }
@@ -93,7 +90,7 @@ const AerServAdapter = function AerServAdapter() {
 
   return {
     callBids: _callBids
-  };
+  }
 };
 
 module.exports = AerServAdapter;
