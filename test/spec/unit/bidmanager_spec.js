74ebd030cb267714186c61b5063bb558042b18d1
Dave Bemiller <dbemiller@appnexus.com>
2017-06-23 11:24:03 -0400

    Added more thorough tests, and refactored a bit for better code reuse.

--- test/spec/unit/bidmanager_spec.js
+++ test/spec/unit/bidmanager_spec.js
@@ -6,60 +6,83 @@ import bidRequest from 'test/fixtures/video/bidRequest';
 import bidResponse from 'test/fixtures/video/bidResponse';
 
 describe('The Bid Manager', () => {
-  describe('addBidResponse() function', () => {
-    before(() => {
-      $$PREBID_GLOBAL$$.cbTimeout = 5000;
-      $$PREBID_GLOBAL$$.timeoutBuffer = 50;
-    });
+  before(() => {
+    $$PREBID_GLOBAL$$.cbTimeout = 5000;
+    $$PREBID_GLOBAL$$.timeoutBuffer = 50;
+  });
 
+  describe('addBidResponse() function,', () => {
     useVideoCacheStubs({
       store: [{ cacheId: 'FAKE_UUID' }],
     });
 
-    describe('when more bids are expected after this one', () => {
-      beforeEach(() => {
-        const tweakedBidRequest = JSON.parse(JSON.stringify(bidRequest));
-        const tweakedBidRequestBid = Object.assign({}, bidRequest.bids[0], { placementCode: 'video2' });
-        tweakedBidRequest.bids.push(tweakedBidRequestBid);
-        tweakedBidRequest.start = new Date().getTime();
-
-        $$PREBID_GLOBAL$$.adUnits = [adUnit, Object.assign({}, adUnit, { code: 'video2' })];
-        $$PREBID_GLOBAL$$._bidsRequested = [tweakedBidRequest];
-        $$PREBID_GLOBAL$$._bidsReceived = [];
-        $$PREBID_GLOBAL$$._adUnitCodes = $$PREBID_GLOBAL$$.adUnits.map(unit => unit.code);
-      });
-
-      it('should add video bids, but *not* call the end-of-auction callbacks', () => {
+    /**
+     * Add the bidResponse fixture as a bid into the auction, and run some assertions
+     * to verify:
+     *
+     * 1. Whether or not that bid got added.
+     * 2. Whether or not the "end of auction" callbacks got called.
+     */
+    function testAddBidResponse(expectBidAdded, expectCallbackCalled) {
+      return function() {
         const mockResponse = Object.assign({}, bidResponse);
-        const spy = sinon.spy();
-        bidManager.addOneTimeCallback(spy);
+        const callback = sinon.spy();
+        bidManager.addOneTimeCallback(callback);
 
         mockResponse.getSize = function() {
-          return `${this.height}x${this.width}`
+          return `${this.height}x${this.width}`;
         };
         bidManager.addBidResponse(adUnit.code, mockResponse);
 
-        expect($$PREBID_GLOBAL$$._bidsReceived.length).to.equal(1);
+        const expectedBidsReceived = expectBidAdded ? 1 : 0;
+        expect($$PREBID_GLOBAL$$._bidsReceived.length).to.equal(expectedBidsReceived);
 
-        const bid = $$PREBID_GLOBAL$$._bidsReceived[0];
-        expect(bid.vastUrl).to.equal('www.myVastUrl.com');
-        expect(bid.videoCacheKey).to.equal('FAKE_UUID');
+        if (expectedBidsReceived === 1) {
+          const bid = $$PREBID_GLOBAL$$._bidsReceived[0];
+          expect(bid.vastUrl).to.equal('www.myVastUrl.com');
+          expect(bid.videoCacheKey).to.equal('FAKE_UUID');
+        }
+        if (expectCallbackCalled) {
+          expect(callback.calledOnce).to.equal(true);
+        } else {
+          expect(callback.called).to.equal(false);
+        }
+      };
+    }
 
-        expect(spy.called).to.equal(false);
-      });
-
-      it("shouldn't add bids which arrive after the auction times out", () => {
-        expect(true).to.equal(true);
-      });
-    });
-
-    describe('when this is the last bid expected in the auction', () => {
+    function prepAuction(adUnits, bidRequestTweaker) {
       beforeEach(() => {
-        $$PREBID_GLOBAL$$.adUnits = [adUnit];
-        $$PREBID_GLOBAL$$._bidsRequested = [bidRequest];
+        let thisBidRequest = bidRequest;
+        if (bidRequestTweaker) {
+          thisBidRequest = JSON.parse(JSON.stringify(bidRequest));
+          bidRequestTweaker(thisBidRequest);
+        }
+
+        $$PREBID_GLOBAL$$.adUnits = adUnits;
+        $$PREBID_GLOBAL$$._bidsRequested = [thisBidRequest];
         $$PREBID_GLOBAL$$._bidsReceived = [];
         $$PREBID_GLOBAL$$._adUnitCodes = $$PREBID_GLOBAL$$.adUnits.map(unit => unit.code);
       });
+    }
+
+    describe('when more bids are expected after this one', () => {
+      // Set up the global state so that we expect two bids, and the auction started just now
+      // (so as to reduce the chance of timeout. This assumes that the unit test runs run in < 5000 ms).
+      prepAuction(
+        [adUnit, Object.assign({}, adUnit, { code: 'video2' })],
+        (bidRequest) => {
+          const tweakedBidRequestBid = Object.assign({}, bidRequest.bids[0], { placementCode: 'video2' });
+          bidRequest.bids.push(tweakedBidRequestBid);
+          bidRequest.start = new Date().getTime();
+        });
+
+      it('should add video bids, but *not* call the end-of-auction callbacks', testAddBidResponse(true, false));
+    });
+
+    describe('when this is the last bid expected in the auction', () => {
+      // Set up the global state so that we expect only one bid, and the auction started just now
+      // (so as to reduce the chance of timeout. This assumes that the unit test runs run in < 5000 ms).
+      prepAuction([adUnit], (bidRequest) => bidRequest.start = new Date().getTime());
 
       it("shouldn't add invalid bids", () => {
         bidManager.addBidResponse('', { });
@@ -68,24 +91,25 @@ describe('The Bid Manager', () => {
         expect($$PREBID_GLOBAL$$._bidsReceived.length).to.equal(0);
       });
 
-      it('should add valid video bids and then execute the callbacks signaling the end of the auction', () => {
-        const mockResponse = Object.assign({}, bidResponse);
-        const spy = sinon.spy();
-        bidManager.addOneTimeCallback(spy);
-
-        mockResponse.getSize = function() {
-          return `${this.height}x${this.width}`
-        };
-        bidManager.addBidResponse(adUnit.code, mockResponse);
-
-        expect($$PREBID_GLOBAL$$._bidsReceived.length).to.equal(1);
-
-        const bid = $$PREBID_GLOBAL$$._bidsReceived[0];
-        expect(bid.vastUrl).to.equal('www.myVastUrl.com');
-        expect(bid.videoCacheKey).to.equal('FAKE_UUID');
-
-        expect(spy.calledOnce).to.equal(true);
-      });
+      it('should add valid video bids and then execute the callbacks signaling the end of the auction',
+         testAddBidResponse(true, true));
     });
+
+    describe('when the auction has timed out', () => {
+      // Set up the global state to expect two bids, and mock an auction which happened long enough
+      // in the past that it will *seem* like this bid is arriving after the timeouts.
+      prepAuction(
+        [adUnit, Object.assign({}, adUnit, { code: 'video2' })],
+        (bidRequest) => {
+          const tweakedBidRequestBid = Object.assign({}, bidRequest.bids[0], { placementCode: 'video2' });
+          bidRequest.bids.push(tweakedBidRequestBid);
+          bidRequest.start = new Date().getTime() - $$PREBID_GLOBAL$$.cbTimeout - $$PREBID_GLOBAL$$.timeoutBuffer - 1;
+        });
+
+      // Because of the preconditions, this makes sure that the end-of-auction callbacks get called when
+      // the auction hits the timeout.
+      it('should add the bid, but also execute the callbacks signaling the end of the auction',
+         testAddBidResponse(true, true));
+    })
   });
 });
