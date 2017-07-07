1e92807b830c2f69e80a3aab186bc8010f016bac
Ilya Pirogov <ilja.pirogov@gmail.com>
2017-07-07 03:35:08 -0700

    add cookie sync

--- modules/thoughtleadrBidAdapter.js
+++ modules/thoughtleadrBidAdapter.js
@@ -1,30 +1,31 @@
-"use strict";
-var bidfactory = require("../bidfactory");
-var bidmanager = require("../bidmanager");
-var utils = require("../utils");
-var ajax_1 = require("../ajax");
+var bidfactory = require('src/bidfactory');
+var bidmanager = require('src/bidmanager');
+var utils = require('src/utils');
+var ajax_1 = require('src/ajax');
 var adaptermanager = require('src/adaptermanager');
-var UID_KEY = "tldr_uid";
-var URL_API = "//a.thoughtleadr.com/v4";
-var URL_CDN = "//cdn.thoughtleadr.com/v4";
+
+var COOKIE_SYNC_ID = 'tldr-cookie-sync-div';
+var UID_KEY = 'tldr_uid';
+var URL_API = 'tldr' in window && tldr.config.root_url ? tldr.config.root_url : '//a.thoughtleadr.com/v4/';
+var URL_CDN = 'tldr' in window && tldr.config.cdn_url ? tldr.config.cdn_url : '//cdn.thoughtleadr.com/v4/';
 var BID_AVAILABLE = 1;
 var BID_UNAVAILABLE = 2;
 
 function getVal(key) {
-  if ("localStorage" in window) {
+  if ('localStorage' in window) {
     return localStorage[key];
   }
-  if ("sessionStorage" in window) {
+  if ('sessionStorage' in window) {
     return sessionStorage[key];
   }
   return null;
 }
 
 function setVal(key, val) {
-  if ("localStorage" in window) {
+  if ('localStorage' in window) {
     localStorage[key] = val;
   }
-  if ("sessionStorage" in window) {
+  if ('sessionStorage' in window) {
     sessionStorage[key] = val;
   }
 }
@@ -36,6 +37,23 @@ function getUid() {
     setVal(UID_KEY, uid);
   }
   return uid;
+}
+
+function parseElement(tag) {
+  var tmp = document.createElement('div');
+  tmp.innerHTML = tag;
+  var els = tmp.getElementsByTagName('*');
+
+  if (els.length > 0) {
+    var el = els[0];
+    var newEl = document.createElement(el.nodeName);
+    for (var i = el.attributes.length; i--;) {
+      var attr = el.attributes.item(i);
+      newEl.setAttribute(attr.name, attr.value);
+    }
+    newEl.innerHTML = el.innerHTML;
+    return newEl;
+  }
 }
 
 var ThoughtleadrAdapter = (function () {
@@ -54,17 +72,23 @@ var ThoughtleadrAdapter = (function () {
   };
 
   ThoughtleadrAdapter.prototype.requestPlacement = function (bid) {
+    var _this = this;
     var uid = getUid();
     var size = ThoughtleadrAdapter.getSizes(bid.sizes);
 
-    ajax_1.ajax(URL_API + "/" + bid.placementCode + "/bid.json?uid=" + uid, function (wonBid) {
+    ajax_1.ajax('' + URL_API + bid.placementCode + '/header-bid.json?uid=' + uid, function (response) {
+      var wonBid = JSON.parse(response);
+      if (wonBid.cookie_syncs) {
+        _this.syncCookies(wonBid.cookie_syncs);
+      }
+
       var bidObject;
       if (wonBid && wonBid.bid_amount) {
         bidObject = bidfactory.createBid(BID_AVAILABLE);
         bidObject.bidderCode = 'thoughtleadr';
         bidObject.cpm = wonBid.bid_amount;
         bidObject.ad =
-          "<script src=\"" + URL_CDN + "/bid.js\" header-bid-token=\"" + wonBid.header_bid_token + "\"></script>";
+          '<script src="' + URL_CDN + 'bid.js" header-bid-token="' + wonBid.header_bid_token + '"></script>';
         bidObject.width = size.width;
         bidObject.height = size.height;
       }
@@ -76,8 +100,31 @@ var ThoughtleadrAdapter = (function () {
     }, null);
   };
 
+  ThoughtleadrAdapter.prototype.syncCookies = function (tags) {
+    if (!tags || !tags.length) {
+      return;
+    }
+
+    var container = document.getElementById(COOKIE_SYNC_ID);
+    if (!container) {
+      container = document.createElement('div');
+      container.id = COOKIE_SYNC_ID;
+      container.style.width = '0';
+      container.style.height = '0';
+      document.body.appendChild(container);
+    }
+
+    for (var _i = 0, tags_1 = tags; _i < tags_1.length; _i++) {
+      var tag = tags_1[_i];
+      var element = parseElement(tag);
+      if (element) {
+        container.appendChild(element);
+      }
+    }
+  };
+
   ThoughtleadrAdapter.valid = function (bid) {
-    return !!(bid && bid.params && typeof bid.params.placementId === "string");
+    return !!(bid && bid.params && typeof bid.params.placementId === 'string');
   };
 
   ThoughtleadrAdapter.getSizes = function (sizes) {
@@ -85,11 +132,13 @@ var ThoughtleadrAdapter = (function () {
     if (Array.isArray(first)) {
       return ThoughtleadrAdapter.getSizes(first);
     }
+
     return {
       width: sizes[0],
       height: sizes[1]
     };
   };
+
   return ThoughtleadrAdapter;
 }());
 
