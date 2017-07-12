dc1023b31f1deb95ca38c431bf25681c0a8b595a
Ilya Pirogov <ilja.pirogov@gmail.com>
2017-07-12 01:38:34 -0700

    fix thoughtleadr tests

--- test/spec/modules/thoughtleadrBidAdapter_spec.js
+++ test/spec/modules/thoughtleadrBidAdapter_spec.js
@@ -1,6 +1,6 @@
 "use strict";
 var chai_1 = require("chai");
-var ta = require("../../../src/adapters/thoughtleadr");
+var ta = require("../../../modules/thoughtleadrBidAdapter");
 var bidfactory = require("../../../src/bidfactory");
 var ajax = require("../../../src/ajax");
 var Adapter = ta;
@@ -61,18 +61,51 @@ describe("thoughtleadr adapter tests", function () {
   });
 
   describe("requestPlacement", function () {
-    it("should request bid.js", function () {
+    it("should request header-bid.json", function () {
       ajaxMock = sandbox.stub(ajax, "ajax", function (url, cb) {
-        cb({
+        cb(JSON.stringify({
           header_bid_token: "asd",
           media_type: "article",
-          bid_amount: 2,
-        });
+          amount: 2
+        }));
       });
       adapter.callBids(request);
       chai_1.expect(ajaxMock.callCount).to.be.equal(1);
-      chai_1.expect(ajaxMock.firstCall.args[0]).to.contains("//a.thoughtleadr.com/v4/abc-123/bid.json?uid=");
+      chai_1.expect(ajaxMock.firstCall.args[0]).to.contains(
+        "//a.thoughtleadr.com/v4/test-placement/header-bid.json?uid=");
       chai_1.expect(createBid.firstCall.args[0]).to.be.equal(1);
+    });
+
+    it("should request header-bid.json without bids", function () {
+      ajaxMock = sandbox.stub(ajax, "ajax", function (url, cb) {
+        cb(JSON.stringify({}));
+      });
+
+      adapter.callBids(request);
+      chai_1.expect(ajaxMock.callCount).to.be.equal(1);
+      chai_1.expect(ajaxMock.firstCall.args[0]).to.contains(
+        "//a.thoughtleadr.com/v4/test-placement/header-bid.json?uid=");
+      chai_1.expect(createBid.firstCall.args[0]).to.be.equal(2);
+    });
+
+    it("should sync cookies", function () {
+      ajaxMock = sandbox.stub(ajax, "ajax", function (url, cb) {
+        cb(JSON.stringify({
+          header_bid_token: "asd",
+          media_type: "article",
+          amount: 2,
+          cookie_syncs: ['<script src="/path/to/script"></script>']
+        }));
+      });
+      adapter.callBids(request);
+
+      var element = document.getElementById("tldr-cookie-sync-div");
+      chai_1.expect(element.outerHTML).to.be.equal(
+        '<div id="tldr-cookie-sync-div" style="width: 0px; height: 0px;">' +
+        '<iframe style="width: 0px; height: 0px; border: 0px none;" src="javascript:false"></iframe></div>');
+
+      var iframe =  element.getElementsByTagName("iframe")[0];
+      chai_1.expect(iframe.contentDocument.body.innerHTML).to.be.equal('<script src="/path/to/script"></script>');
     });
   });
 });
