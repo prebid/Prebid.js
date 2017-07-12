50b305274d4e20c2fded33820cbb2b6c560aad30
Ilya Pirogov <ilja.pirogov@gmail.com>
2017-07-12 01:51:13 -0700

    single quotes

--- test/spec/modules/thoughtleadrBidAdapter_spec.js
+++ test/spec/modules/thoughtleadrBidAdapter_spec.js
@@ -1,11 +1,11 @@
-"use strict";
-var chai_1 = require("chai");
-var ta = require("../../../modules/thoughtleadrBidAdapter");
-var bidfactory = require("../../../src/bidfactory");
-var ajax = require("../../../src/ajax");
+'use strict';
+var chai_1 = require('chai');
+var ta = require('modules/thoughtleadrBidAdapter');
+var bidfactory = require('src/bidfactory');
+var ajax = require('src/ajax');
 var Adapter = ta;
 
-describe("thoughtleadr adapter tests", function () {
+describe('thoughtleadr adapter tests', function () {
   var sandbox;
   var adapter;
   var request;
@@ -17,17 +17,16 @@ describe("thoughtleadr adapter tests", function () {
   });
 
   beforeEach(function () {
-    // ajaxMock = sandbox.stub(ajax, "ajax");
-    createBid = sandbox.spy(bidfactory, "createBid");
+    createBid = sandbox.spy(bidfactory, 'createBid');
     adapter = new Adapter();
     request = {
-      bidderCode: "thoughtleadr",
+      bidderCode: 'thoughtleadr',
       bids: [{
-        bidder: "thoughtleadr",
-        placementCode: "abc-123",
+        bidder: 'thoughtleadr',
+        placementCode: 'abc-123',
         sizes: [[300, 250], [400, 400]],
         params: {
-          placementId: "test-placement",
+          placementId: 'test-placement',
         },
       }],
     };
@@ -37,74 +36,74 @@ describe("thoughtleadr adapter tests", function () {
     sandbox.restore();
   });
 
-  describe("handleBids", function () {
-    it("should filter invalid bids", function () {
+  describe('handleBids', function () {
+    it('should filter invalid bids', function () {
       request.bids.unshift({
-        bidder: "thoughtleadr",
-        placementCode: "abc-123",
+        bidder: 'thoughtleadr',
+        placementCode: 'abc-123',
         sizes: [[300, 250], [400, 400]],
         params: {},
       });
       request.bids.push({
-        bidder: "thoughtleadr",
-        placementCode: "abc-123",
+        bidder: 'thoughtleadr',
+        placementCode: 'abc-123',
         sizes: [[300, 250], [400, 400]],
         params: {
           incorrectParam: 123,
         },
       });
-      var requestPlacement = sinon.spy(adapter, "requestPlacement");
+      var requestPlacement = sinon.spy(adapter, 'requestPlacement');
       adapter.callBids(request);
       chai_1.expect(requestPlacement.callCount).to.be.equal(1);
       chai_1.expect(requestPlacement.getCall(0).args[0]).to.be.equal(request.bids[1]);
     });
   });
 
-  describe("requestPlacement", function () {
-    it("should request header-bid.json", function () {
-      ajaxMock = sandbox.stub(ajax, "ajax", function (url, cb) {
+  describe('requestPlacement', function () {
+    it('should request header-bid.json', function () {
+      ajaxMock = sandbox.stub(ajax, 'ajax', function (url, cb) {
         cb(JSON.stringify({
-          header_bid_token: "asd",
-          media_type: "article",
+          header_bid_token: 'asd',
+          media_type: 'article',
           amount: 2
         }));
       });
       adapter.callBids(request);
       chai_1.expect(ajaxMock.callCount).to.be.equal(1);
       chai_1.expect(ajaxMock.firstCall.args[0]).to.contains(
-        "//a.thoughtleadr.com/v4/test-placement/header-bid.json?uid=");
+        '//a.thoughtleadr.com/v4/test-placement/header-bid.json?uid=');
       chai_1.expect(createBid.firstCall.args[0]).to.be.equal(1);
     });
 
-    it("should request header-bid.json without bids", function () {
-      ajaxMock = sandbox.stub(ajax, "ajax", function (url, cb) {
+    it('should request header-bid.json without bids', function () {
+      ajaxMock = sandbox.stub(ajax, 'ajax', function (url, cb) {
         cb(JSON.stringify({}));
       });
 
       adapter.callBids(request);
       chai_1.expect(ajaxMock.callCount).to.be.equal(1);
       chai_1.expect(ajaxMock.firstCall.args[0]).to.contains(
-        "//a.thoughtleadr.com/v4/test-placement/header-bid.json?uid=");
+        '//a.thoughtleadr.com/v4/test-placement/header-bid.json?uid=');
       chai_1.expect(createBid.firstCall.args[0]).to.be.equal(2);
     });
 
-    it("should sync cookies", function () {
-      ajaxMock = sandbox.stub(ajax, "ajax", function (url, cb) {
+    it('should sync cookies', function () {
+      ajaxMock = sandbox.stub(ajax, 'ajax', function (url, cb) {
         cb(JSON.stringify({
-          header_bid_token: "asd",
-          media_type: "article",
+          header_bid_token: 'asd',
+          media_type: 'article',
           amount: 2,
           cookie_syncs: ['<script src="/path/to/script"></script>']
         }));
       });
       adapter.callBids(request);
 
-      var element = document.getElementById("tldr-cookie-sync-div");
+      var element = document.getElementById('tldr-cookie-sync-div');
       chai_1.expect(element.outerHTML).to.be.equal(
         '<div id="tldr-cookie-sync-div" style="width: 0px; height: 0px;">' +
         '<iframe style="width: 0px; height: 0px; border: 0px none;" src="javascript:false"></iframe></div>');
 
-      var iframe =  element.getElementsByTagName("iframe")[0];
+      var iframe =  element.getElementsByTagName('iframe')[0];
       chai_1.expect(iframe.contentDocument.body.innerHTML).to.be.equal('<script src="/path/to/script"></script>');
     });
   });
