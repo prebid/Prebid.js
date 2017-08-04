d5a200c74318eec56f278f26233f6176104220ad
Matt Lane <mlane@appnexus.com>
2017-08-04 10:28:03 -0700

    Add setConfig method (#1325)
    * Add setConfig API method
    
    * Use getter/setter methods in config module to manage configuration
    
    * Use getter/setter on object props to manage legacy api access
    
    * Convert bidderTimeout
    
    * Add and use getConfig
    
    * Convert publisherDomain
    
    * Convert cookieSyncDelay
    
    * priceGranularity calls setPriceGranularity
    
    * Convert enableSendAllBids
    
    * s2sConfig calls setS2SConfig
    
    * bidderSequence calls setBidderSequence
    
    * Add subscribe function
    
    * Enable subscribing to specific configuration changes
    
    * Simplify function exposure
    
    * Add missing pbjs
    
    * Merge subscribe into getConfig and return unsubscribe function
    
    * Expose getConfig on global
    
    * Reset config in tests
    
    * Refactor

--- modules/rhythmoneBidAdapter.js
+++ modules/rhythmoneBidAdapter.js
@@ -1,5 +1,6 @@
 import {ajax} from 'src/ajax';
 import adaptermanager from 'src/adaptermanager';
+import { config } from 'src/config';
 
 const bidmanager = require('src/bidmanager.js');
 const bidfactory = require('src/bidfactory.js');
@@ -215,7 +216,7 @@ function RhythmoneAdapter (bidManager, global, loader) {
     data.response_ms = (new Date()).getTime() - loadStart;
     data.placement_codes = configuredPlacements.join(',');
     data.bidder_version = version;
-    data.prebid_timeout = prebid_instance.cbTimeout || prebid_instance.bidderTimeout;
+    data.prebid_timeout = prebid_instance.cbTimeout || config.getConfig('bidderTimeout');
 
     for (var k in data) {
       q.push(encodeURIComponent(k) + '=' + encodeURIComponent((typeof data[k] === 'object' ? JSON.stringify(data[k]) : data[k])));
