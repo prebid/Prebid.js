6956a56383d9f530c961640c4421d5f44f087c6d
Erik Dubbelboer <erik@dubbelboer.com>
2017-08-11 02:16:01 +0200

    Atomx adapter: add multiple size support (#1470)

--- modules/atomxBidAdapter.js
+++ modules/atomxBidAdapter.js
@@ -26,11 +26,14 @@ var AtomxAdapter = function AtomxAdapter() {
     for (var i = 0, ln = bids.length; i < ln; i++) {
       var bid = bids[i];
       if (bid.params && bid.params.id) {
-        Ajax.ajax(url, _responseCallback.bind(this, bid), {
-          id: bid.params.id,
-          size: utils.parseSizesInput(bid.sizes)[0],
-          prebid: bid.placementCode
-        }, {method: 'GET'});
+        var sizes = utils.parseSizesInput(bid.sizes);
+        for (var j = 0; j < sizes.length; j++) {
+          Ajax.ajax(url, _responseCallback.bind(this, bid), {
+            id: bid.params.id,
+            size: sizes[j],
+            prebid: bid.placementCode
+          }, {method: 'GET'});
+        }
       } else {
         var bidObject = bidfactory.createBid(CONSTANTS.STATUS.NO_BID, bid);
         bidObject.bidderCode = 'atomx';
