031fa560069330891e49edb04d513a3a127f1c8a
Paul Wyrembak <paul@trustx.org>
2017-08-15 15:43:08 +0300

    Add trustx adapter and tests for it

---
+++ modules/trustxBidAdapter.js
@@ -0,0 +1,210 @@
+var utils = require('src/utils.js');
+var bidfactory = require('src/bidfactory.js');
+var bidmanager = require('src/bidmanager.js');
+var adloader = require('src/adloader');
+var adaptermanager = require('src/adaptermanager');
+
+var TrustxAdapter = function TrustxAdapter() {
+  var bidderCode = 'trustx';
+  var reqHost = '//sofia.trustx.org';
+  var reqPath = '/hb?';
+  var LOG_ERROR_MESS = {
+    noAuid: 'Bid from response has no auid parameter - ',
+    noAdm: 'Bid from response has no adm parameter - ',
+    noBid: 'Array of bid objects is empty',
+    noPlacementCode: 'Can\'t find placementCode for bid with auid - ',
+    havePCodeFor: ', placementCode is available only for the following uids - ',
+    emptyUids: 'Uids should be not empty',
+    emptySeatbid: 'Seatbid array from response has empty item',
+    emptyResponse: 'Response is empty',
+    hasEmptySeatbidArray: 'Response has empty seatbid array',
+    hasNoArrayOfBids: 'Seatbid from response has no array of bid objects - '
+  };
+
+  function _makeHandler(auids, placementMap, onTimeoutWorker, needToSendStat) {
+    var cbName = bidderCode + '_callback_wrapper_' + auids.join('_');
+    $$PREBID_GLOBAL$$[cbName] = function(resp) {
+      delete $$PREBID_GLOBAL$$[cbName];
+      _responseProcessing(resp, auids, placementMap, onTimeoutWorker, needToSendStat);
+    };
+    return '$$PREBID_GLOBAL$$.' + cbName;
+  }
+
+  function _sendRequest(auids, placementMap, onTimeoutWorker) {
+    var query = [];
+    var path = reqPath;
+    var needToSendStat = Math.random() < 0.1;
+    query.push('u=' + encodeURIComponent(location.href));
+    query.push('auids=' + encodeURIComponent(auids.join(',')));
+    query.push('cb=' + _makeHandler(auids, placementMap, onTimeoutWorker, needToSendStat));
+    query.push('pt=' + (window.globalPrebidTrustxPriceType === 'net' ? 'net' : 'gross'));
+    query.push('hbstat=' + (needToSendStat ? '1' : '0'));
+
+    adloader.loadScript(reqHost + path + query.join('&'));
+  }
+
+  function _makeTimeoutWorker() {
+    var active = true;
+    var handler = function() {
+      worker.called = true;
+      $$PREBID_GLOBAL$$.offEvent('bidTimeout', handler);
+    };
+    $$PREBID_GLOBAL$$.onEvent('bidTimeout', handler);
+    var worker = {
+      startTime: new Date().getTime(),
+      off: function() {
+        if (active && !this.called) {
+          active = false;
+          $$PREBID_GLOBAL$$.offEvent('bidTimeout', handler);
+        }
+      },
+      called: false
+    };
+    return worker;
+  }
+
+  function _callBids(params) {
+    var auids = [];
+    var placementMap = {};
+    var hasBid;
+    var bid;
+    var bids = params.bids || [];
+    for (var i = 0; i < bids.length; i++) {
+      bid = bids[i];
+      if (bid && bid.bidder === bidderCode && bid.placementCode) {
+        hasBid = true;
+        if (bid.params && bid.params.uid) {
+          if (!placementMap[bid.params.uid]) {
+            placementMap[bid.params.uid] = [bid.placementCode];
+            auids.push(bid.params.uid);
+          } else {
+            placementMap[bid.params.uid].push(bid.placementCode);
+          }
+        }
+      }
+    }
+
+    if (auids.length) {
+      var onTimeoutWorker = _makeTimeoutWorker();
+      _sendRequest(auids, placementMap, onTimeoutWorker);
+    } else if (hasBid) {
+      utils.logError(LOG_ERROR_MESS.emptyUids);
+    }
+  }
+
+  function tryToSendStat(startTime, timeout, auids, isTimeouted, error) {
+    var protocol = location.protocol === 'https:' ? 'https:' : 'http:';
+    var stat = {
+      wrapper: 'prebid',
+      tt: timeout,
+      ct: new Date().getTime() - startTime,
+      st: isTimeouted ? 'to' : error ? 'err' : 'ok',
+      cr: 10
+    };
+    stat = encodeURIComponent(JSON.stringify(stat));
+    auids = encodeURIComponent(auids.join(','));
+    (new Image()).src = protocol + '//sofia.trustx.org/hbstat?auids=' + auids + '&stat=' + stat + '&rnd=' + Math.random();
+  }
+
+  function _getBidFromResponse(resp) {
+    if (!resp) {
+      utils.logError(LOG_ERROR_MESS.emptySeatbid);
+    } else if (!resp.bid) {
+      utils.logError(LOG_ERROR_MESS.hasNoArrayOfBids + JSON.stringify(resp));
+    } else if (!resp.bid[0]) {
+      utils.logError(LOG_ERROR_MESS.noBid);
+    }
+    return resp && resp.bid && resp.bid[0];
+  }
+
+  function _forEachPlacement(error, bid, placementCode) {
+    var bidObject;
+    if (error) {
+      bidObject = bidfactory.createBid(2);
+    } else {
+      bidObject = bidfactory.createBid(1);
+      bidObject.cpm = bid.price;
+      bidObject.ad = bid.adm;
+      bidObject.width = bid.w;
+      bidObject.height = bid.h;
+      if (bid.dealid) {
+        bidObject.dealId = bid.dealid;
+      }
+    }
+    bidObject.bidderCode = bidderCode;
+    bidmanager.addBidResponse(placementCode, bidObject);
+  }
+
+  function _addBidResponse(bid, auids, placementMap) {
+    if (!bid) return;
+    var errorMessage, placementCodes;
+    if (!bid.auid) errorMessage = LOG_ERROR_MESS.noAuid + JSON.stringify(bid);
+    else {
+      placementCodes = placementMap.hasOwnProperty(bid.auid) && placementMap[bid.auid];
+      if (!placementCodes) {
+        errorMessage = LOG_ERROR_MESS.noPlacementCode + bid.auid + LOG_ERROR_MESS.havePCodeFor + auids.join(',');
+      }
+    }
+
+    if (!errorMessage) {
+      if (!bid.adm) errorMessage = LOG_ERROR_MESS.noAdm + JSON.stringify(bid);
+
+      var l = placementCodes.length;
+      while (l--) {
+        _forEachPlacement(errorMessage, bid, placementCodes[l]);
+      }
+
+      delete placementMap[bid.auid];
+    }
+
+    if (errorMessage) {
+      utils.logError(errorMessage);
+    }
+  }
+
+  function _responseProcessing(resp, auids, placementMap, onTimeoutWorker, needToSendStat) {
+    var errorMessage;
+
+    if (!resp) errorMessage = LOG_ERROR_MESS.emptyResponse;
+    else if (resp.seatbid && !resp.seatbid.length) errorMessage = LOG_ERROR_MESS.hasEmptySeatbidArray;
+
+    if (!errorMessage) {
+      resp = resp.seatbid || [];
+      var l = resp.length;
+      while (l--) {
+        _addBidResponse(_getBidFromResponse(resp[l]), auids, placementMap);
+      }
+    }
+
+    var n, bidObj;
+    for (var auid in placementMap) {
+      if (placementMap.hasOwnProperty(auid) && placementMap[auid]) {
+        n = placementMap[auid].length;
+        while (n--) {
+          bidObj = bidfactory.createBid(2);
+          bidObj.bidderCode = bidderCode;
+          bidmanager.addBidResponse(placementMap[auid][n], bidObj);
+        }
+      }
+    }
+
+    onTimeoutWorker.off();
+    needToSendStat && tryToSendStat(
+      onTimeoutWorker.startTime,
+      $$PREBID_GLOBAL$$.cbTimeout + $$PREBID_GLOBAL$$.timeoutBuffer,
+      auids,
+      onTimeoutWorker.called,
+      errorMessage
+    );
+
+    if (errorMessage) utils.logError(errorMessage);
+  }
+
+  return {
+    callBids: _callBids
+  };
+};
+
+adaptermanager.registerBidAdapter(new TrustxAdapter(), 'trustx');
+
+module.exports = TrustxAdapter;
