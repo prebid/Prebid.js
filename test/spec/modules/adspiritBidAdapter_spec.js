f79fd3d877e04f4de87d46128dcbbbce3f6768c6
SpreeGorilla <31438227+SpreeGorilla@users.noreply.github.com>
2017-09-07 17:44:32 +0200

    Add files via upload

---
+++ test/spec/modules/adspiritBidAdapter_spec.js
@@ -0,0 +1,122 @@
+var chai = require('chai');
+var Adapter = require('modules/adspiritBidAdapter')();
+var Ajax = require('src/ajax');
+var adLoader = require('src/adloader');
+var bidmanager = require('src/bidmanager.js');
+var CONSTANTS = require('src/constants.json');
+
+describe('Adspirit adapter tests', function () {
+  var validData_1 = {
+    bids: [
+      {
+        bidder: 'adspirit',
+        bidId: 'bid_id',
+        params: {placementId: 1, host: '-'},
+        placementCode: 'ad-1',
+      }
+    ]
+  };
+  var validData_2 = {
+    bids: [
+      {
+        bidder: 'adspirit',
+        bidId: 'bid_id',
+        params: {placementId: 2, host: '-'},
+        placementCode: 'ad-1',
+      }
+    ]
+  };
+
+  var invalidData = {
+    bids: [
+      {
+        bidder: 'adspirit',
+        bidId: 'bid_id',
+        params: {},
+        placementCode: 'ad-1',
+      }
+    ]
+  };
+
+  var responseWithAd = JSON.stringify({
+    'cpm': 2.2,
+    'ad': '<div>AD</div>',
+    'width': 300,
+    'height': 250,
+    'code': 'ad-1'
+  });
+  var responseWithoutAd = JSON.stringify({
+    'cpm': 0,
+    'ad': '',
+    'width': 300,
+    'height': 250,
+    'code': 'ad-1'
+  });
+
+  var responseEmpty = '';
+
+  describe('bid request with valid data - ', function () {
+    var stubAjax;
+    beforeEach(function () {
+      stubAjax = sinon.stub(Ajax, 'ajax');
+    });
+    afterEach(function () {
+      stubAjax.restore();
+    });
+    it('bid request should be called. sizes style -> [[],[]]', function () {
+      Adapter.callBids(validData_1);
+      sinon.assert.calledOnce(stubAjax);
+    });
+    it('bid request should be called. sizes style -> []', function () {
+      Adapter.callBids(validData_2);
+      sinon.assert.calledOnce(stubAjax);
+    });
+  });
+  describe('bid request with invalid data - ', function () {
+    var addBidResponse, stubAjax;
+    beforeEach(function () {
+      addBidResponse = sinon.stub(bidmanager, 'addBidResponse');
+      stubAjax = sinon.stub(Ajax, 'ajax');
+    });
+    afterEach(function () {
+      addBidResponse.restore();
+      stubAjax.restore();
+    });
+    it('ajax shouldn\'t be called', function () {
+      Adapter.callBids(invalidData);
+      sinon.assert.notCalled(stubAjax);
+    });
+    it('bidmanager.addBidResponse status code must to be equal "' + CONSTANTS.STATUS.NO_BID + '"', function () {
+      Adapter.callBids(invalidData);
+      expect(addBidResponse.firstCall.args[1].getStatusCode()).to.equal(CONSTANTS.STATUS.NO_BID);
+      expect(addBidResponse.firstCall.args[1].bidderCode).to.equal('adspirit');
+    });
+  });
+  describe('bid response - ', function () {
+    var addBidResponse;
+    beforeEach(function () {
+      addBidResponse = sinon.stub(bidmanager, 'addBidResponse');
+    });
+    afterEach(function () {
+      addBidResponse.restore();
+    });
+    it('with ad. bidmanager.addBidResponse status code must to be equal "' + CONSTANTS.STATUS.GOOD + '"', function () {
+      Adapter.responseCallback(validData_1.bids[0], responseWithAd);
+      var arg = addBidResponse.firstCall.args[1];
+      expect(arg.getStatusCode()).to.equal(CONSTANTS.STATUS.GOOD);
+      expect(arg.bidderCode).to.equal('adspirit');
+    });
+    it('without ad. bidmanager.addBidResponse status code must to be equal "' + CONSTANTS.STATUS.NO_BID, function () {
+      Adapter.responseCallback(validData_1.bids[0], responseWithoutAd);
+      var arg = addBidResponse.firstCall.args[1];
+      expect(arg.getStatusCode()).to.equal(CONSTANTS.STATUS.NO_BID);
+      expect(arg.bidderCode).to.equal('adspirit');
+    });
+    it('empty. bidmanager.addBidResponse status code must to be equal "' + CONSTANTS.STATUS.NO_BID, function () {
+      Adapter.responseCallback(validData_1.bids[0], responseEmpty);
+      var arg = addBidResponse.firstCall.args[1];
+      expect(arg.getStatusCode()).to.equal(CONSTANTS.STATUS.NO_BID);
+      expect(arg.bidderCode).to.equal('adspirit');
+    })
+  });
+});
