cd71f0826af698d47e309f5f63ae03c12e2c7d8f
mkendall07 <emailmatthere@gmail.com>
2017-09-18 16:41:22 -0400

    fix for #1582

--- modules/prebidServerBidAdapter.js
+++ modules/prebidServerBidAdapter.js
@@ -212,7 +212,9 @@ function PrebidServer() {
    */
   baseAdapter.queueSync = function({bidderCodes}) {
     let syncedList = StorageManager.get(pbjsSyncsKey) || [];
-    if (_cookiesQueued || syncedList.length === bidderCodes.length) {
+    // filter synced bidders - https://github.com/prebid/Prebid.js/issues/1582
+    syncList = bidderCodes.filter(bidder => !syncList.includes(bidder));
+    if (_cookiesQueued || syncedList.length === 0) {
       return;
     }
     _cookiesQueued = true;
