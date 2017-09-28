b285ce877216846fab3b60a2467715224759eabb
Igor Tchibirev <igor@realvu.com>
2017-09-28 14:24:16 -0400

    rvaa+ox

--- modules/realvuAnalyticsAdapter.js
+++ modules/realvuAnalyticsAdapter.js
@@ -2,16 +2,41 @@
 import adapter from 'src/AnalyticsAdapter';
 import adaptermanager from 'src/adaptermanager';
 
-var adloader = require('src/adloader');
+var realvuAnalyticsAdapter = adapter({
+  global: 'realVuAnalytics',
+  handler: 'on',
+  analyticsType: 'library',
+  url: '//ac.realvu.net/realvu_boost.js'
+});
 
-const url = '//ac.realvu.net/realvu_boost.js';
+realvuAnalyticsAdapter.enableAnalytics = function (config) {
+};
 
-var realvuAnalyticsAdapter = adapter({global: 'RealVuPrebidAnalytics', handler: 'on', analyticsType: 'library'});
+realvuAnalyticsAdapter.track = function (eventType, args) {
+}
+var top1 = window;
+try {
+  var wnd = window;
+  while ((top != top1) && (typeof (wnd.document) != 'undefined')) {
+    top1 = wnd;
+    wnd = wnd.parent;
+  }
+} catch (e) { };
+
+realvuAnalyticsAdapter.inView = function (bid, partner_id) {
+  return top1.realvu_boost.addUnitById({unit_id: bid.placementCode, partner_id: partner_id, size: bid.sizes});
+}
+
+realvuAnalyticsAdapter.queue = function (callback) {
+  if (typeof callback === 'function') {
+    top1.boost_fifo = top.boost_fifo || [];
+    top1.boost_fifo.push(callback);
+  }
+}
 
 adaptermanager.registerAnalyticsAdapter({
   adapter: realvuAnalyticsAdapter,
   code: 'realvu'
 });
 
-adloader.loadScript(url, null, true);
-export default realvuAnalyticsAdapter;
+module.exports = realvuAnalyticsAdapter;
