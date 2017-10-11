3252fd8d2f14e9425fba3d980830284f13f90f39
ANTSProgrammatic.com <30523562+ANTSProgrammatic@users.noreply.github.com>
2017-10-11 16:29:50 +0700

    Add files via upload
    ANTSProgrammatic ANTS Programmatic adapter - unit tests

--- test/spec/modules/antsBidAdapter_spec.js
+++ test/spec/modules/antsBidAdapter_spec.js
@@ -1,12 +1,14 @@
 import {expect} from 'chai';
-import Adapter from '../../../modules/antsBidAdapter';
+import AntsAdapter from '../../../modules/antsBidAdapter';
 import bidManager from '../../../src/bidmanager';
 import adLoader from '../../../src/adloader';
 
 describe('Ants Adapter', () => {
   let adapter;
 
-  const REQUEST = {
+  beforeEach(() => adapter = new AntsAdapter());
+
+  const AD_REQUEST = {
     bidderCode: 'ants',
     bids: [
       {
@@ -22,12 +24,79 @@ describe('Ants Adapter', () => {
     ]
   };
 
-  sinon.stub(bidManager, 'addBidResponse');
-  const adLoaderStub = sinon.stub(adLoader, 'loadScript');
+  const AD_RESPONSE = {
+    callback_uid: 'ants_1234',
+    result: {
+      width: 300,
+      height: 250,
+      creative_id: 583985728,
+      cpm: 0.05,
+      ad: '<creative></creative>'
+    }
+  };
+
+  const DELIVERY_URL = 'd.ants.vn/hb/583906268.json'
+  let stubAddBidResponse = sinon.stub(bidManager, 'addBidResponse');
 
   describe('callBids', () => {
-    adapter = new Adapter();
-    adapter.callBids(REQUEST);
-    expect(adLoaderStub.getCall(0).args[0]).to.contain('hb/583906268.json');
+    it('exists and is a function', () => {
+      expect(adapter.callBids).to.exist.and.to.be.a('function');
+    });
+
+    describe('bid request', () => {
+      const adLoaderStub = sinon.stub(adLoader, 'loadScript');
+      describe('Delivery api', () => {
+        it('requires parameters to be made', () => {
+          adapter.callBids({});
+          expect(adLoaderStub.getCall(0)).to.be.null;
+        });
+
+        it('should hit the Delivery api endpoint', () => {
+          adapter.callBids(AD_REQUEST);
+          expect(adLoaderStub.getCall(0).args[0]).to.contain(DELIVERY_URL);
+        });
+      });
+    });
+
+    describe('bid response without bided', () => {
+      before(() => {
+        adapter.callBids(AD_REQUEST)
+      });
+      $$PREBID_GLOBAL$$.antsResponse({callback_uid: 'ants_1234', result: {}});
+      let bid = stubAddBidResponse.getCall(0).args[1];
+
+      it('should not bit with creative', () => {
+        expect(bid.cpm).to.undefined
+        expect(bid.height).to.equal(0)
+        expect(bid.width).to.equal(0)
+        expect(bid.ad).to.undefined
+      });
+    });
+
+    describe('bid response with bided', () => {
+      before(() => {
+        adapter.callBids(AD_REQUEST)
+      });
+      $$PREBID_GLOBAL$$.antsResponse(AD_RESPONSE);
+      let bid = stubAddBidResponse.getCall(1).args[1];
+
+      it('should bid with creative', () => {
+        expect(bid.cpm).to.equal(0.05);
+        expect(bid.height).to.equal(250);
+        expect(bid.width).to.equal(300);
+        expect(bid.ad).to.equal('<creative></creative>');
+      });
+    });
+
+    describe('no bid response', () => {
+      before(() => {
+        adapter.callBids(AD_REQUEST)
+      });
+      $$PREBID_GLOBAL$$.antsResponse({});
+
+      it('should not bit with creative', () => {
+        expect(stubAddBidResponse.calledOnce).to.be.false;
+      });
+    });
   });
 });
