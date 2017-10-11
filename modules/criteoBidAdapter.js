b6534c4150c0c229f5f7250550d19fb14bd11466
Nicolas Faure <n.faure@criteo.com>
2017-10-11 17:07:31 +0200

    Convert Criteo adapter to bidderFactory

---
+++ modules/criteoBidAdapter.js
@@ -0,0 +1,143 @@
+import { registerBidder } from 'src/adapters/bidderFactory';
+import { parse } from 'src/url';
+import * as utils from 'src/utils';
+
+const BIDDER_CODE = 'criteo';
+const CDB_ENDPOINT = '//bidder.criteo.com/cdb';
+const PROFILE_ID = 207;
+const ADAPTER_VERSION = 2;
+const INTEGRATION_MODES = {
+  'amp': 1,
+};
+
+/** @type {BidderSpec} */
+export const spec = {
+  code: BIDDER_CODE,
+
+  /**
+   * @param {object} bid
+   * @return {boolean}
+   */
+  isBidRequestValid: bid => (
+    !!(bid && bid.params && bid.params.zoneId)
+  ),
+
+  /**
+   * @param {BidRequest[]} bidRequests
+   * @return {ServerRequest}
+   */
+  buildRequests: bidRequests => {
+    const context = buildContext(bidRequests);
+    return {
+      method: 'POST',
+      url: buildCdbUrl(context),
+      data: buildCdbRequest(context, bidRequests),
+    };
+  },
+
+  /**
+   * @param {*} response
+   * @param {ServerRequest} request
+   * @return {Bid[]}
+   */
+  interpretResponse: (response, request) => {
+    const bids = [];
+
+    if (response.slots && utils.isArray(response.slots)) {
+      response.slots.forEach(slot => {
+        const bid = {
+          requestId: slot.impid,
+          cpm: slot.cpm,
+          // currency: 'USD', // FIXME: should return currency from CDB
+          // netRevenue: true, // FIXME: are bids net or gross?
+          ad: slot.creative,
+          width: slot.width,
+          height: slot.height,
+        }
+        bids.push(bid);
+      });
+    }
+
+    return bids;
+  },
+};
+
+/**
+ * @param {BidRequest[]} bidRequests
+ * @return {CriteoContext}
+ */
+function buildContext(bidRequests) {
+  const url = utils.getTopWindowUrl();
+  const queryString = parse(url).search;
+
+  const context = {
+    url: url,
+    debug: queryString['pbt_debug'] === '1',
+    noLog: queryString['pbt_nolog'] === '1',
+    integrationMode: undefined,
+  };
+
+  bidRequests.forEach(bidRequest => {
+    if (bidRequest.params.integrationMode) {
+      context.integrationMode = bidRequest.params.integrationMode;
+    }
+  })
+
+  return context;
+}
+
+/**
+ * @param {CriteoContext} context
+ * @return {string}
+ */
+function buildCdbUrl(context) {
+  let url = CDB_ENDPOINT;
+  url += '?profileId=' + PROFILE_ID;
+  url += '&av=' + String(ADAPTER_VERSION);
+  url += '&cb=' + String(Math.floor(Math.random() * 99999999999));
+
+  if (context.integrationMode in INTEGRATION_MODES) {
+    url += '&im=' + INTEGRATION_MODES[context.integrationMode];
+  }
+  if (context.debug) {
+    url += '&debug=1';
+  }
+  if (context.noLog) {
+    url += '&nolog=1';
+  }
+
+  return url;
+}
+
+/**
+ * @param {CriteoContext} context
+ * @param {BidRequest[]} bidRequests
+ * @return {*}
+ */
+function buildCdbRequest(context, bidRequests) {
+  let networkId;
+  const request = {
+    publisher: {
+      url: context.url,
+    },
+    slots: bidRequests.map(bidRequest => {
+      networkId = bidRequest.params.networkId || networkId;
+      const slot = {
+        impid: bidRequest.bidId,
+        zoneid: bidRequest.params.zoneId,
+        transactionid: bidRequest.transactionId,
+        sizes: bidRequest.sizes.map(size => size[0] + 'x' + size[1]),
+      };
+      if (bidRequest.params.publisherSubId) {
+        slot.publishersubid = bidRequest.params.publisherSubId;
+      }
+      return slot;
+    }),
+  };
+  if (networkId) {
+    request.publisher.networkid = networkId;
+  }
+  return request;
+}
+
+registerBidder(spec);
