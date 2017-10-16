6877b895d2eba825e09b90338553fa902176b55a
John Salis <johnsalis@beachfrontmedia.com>
2017-10-16 12:08:04 -0400

    Fix lint errors

--- test/spec/video_spec.js
+++ test/spec/video_spec.js
@@ -2,29 +2,29 @@ import { isValidVideoBid } from 'src/video';
 import { auctionManager } from 'src/auctionManager';
 
 describe('video.js', () => {
-	beforeEach(() => {
-		sinon.stub(auctionManager, 'getBidsRequested');
-	});
+  beforeEach(() => {
+    sinon.stub(auctionManager, 'getBidsRequested');
+  });
 
   afterEach(() => {
-	  auctionManager.getBidsRequested.restore();
+    auctionManager.getBidsRequested.restore();
   });
 
   it('validates valid instream bids', () => {
-	  auctionManager.getBidsRequested.returns([
-		  {
-			  bids: [{
-				  bidId: '123abc',
-				  bidder: 'appnexusAst',
-				  mediaTypes: {
-					  video: { context: 'instream' }
-				  }
-			  }]
-		  }
-	  ]);
+    auctionManager.getBidsRequested.returns([
+      {
+        bids: [{
+          bidId: '123abc',
+          bidder: 'appnexusAst',
+          mediaTypes: {
+            video: { context: 'instream' }
+          }
+        }]
+      }
+    ]);
 
     const valid = isValidVideoBid({
-	    adId: '123abc',
+      adId: '123abc',
       vastUrl: 'http://www.example.com/vastUrl'
     });
 
@@ -32,40 +32,40 @@ describe('video.js', () => {
   });
 
   it('catches invalid instream bids', () => {
-	  auctionManager.getBidsRequested.returns([
-		  {
-			  bids: [{
-				  bidId: '123abc',
-				  bidder: 'appnexusAst',
-				  mediaTypes: {
-					  video: { context: 'instream' }
-				  }
-			  }]
-		  }
-	  ]);
+    auctionManager.getBidsRequested.returns([
+      {
+        bids: [{
+          bidId: '123abc',
+          bidder: 'appnexusAst',
+          mediaTypes: {
+            video: { context: 'instream' }
+          }
+        }]
+      }
+    ]);
 
     const valid = isValidVideoBid({
-	    adId: '123abc'
+      adId: '123abc'
     });
 
     expect(valid).to.be(false);
   });
 
   it('validates valid outstream bids', () => {
-	  auctionManager.getBidsRequested.returns([
-		  {
-			  bids: [{
-				  bidId: '123abc',
-				  bidder: 'appnexusAst',
-				  mediaTypes: {
-					  video: { context: 'outstream' }
-				  }
-			  }]
-		  }
-	  ]);
+    auctionManager.getBidsRequested.returns([
+      {
+        bids: [{
+          bidId: '123abc',
+          bidder: 'appnexusAst',
+          mediaTypes: {
+            video: { context: 'outstream' }
+          }
+        }]
+      }
+    ]);
 
     const valid = isValidVideoBid({
-	    adId: '123abc',
+      adId: '123abc',
       renderer: {
         url: 'render.url',
         render: () => true,
@@ -76,20 +76,20 @@ describe('video.js', () => {
   });
 
   it('catches invalid outstream bids', () => {
-	  auctionManager.getBidsRequested.returns([
-		  {
-			  bids: [{
-				  bidId: '123abc',
-				  bidder: 'appnexusAst',
-				  mediaTypes: {
-					  video: { context: 'outstream' }
-				  }
-			  }]
-		  }
-	  ]);
+    auctionManager.getBidsRequested.returns([
+      {
+        bids: [{
+          bidId: '123abc',
+          bidder: 'appnexusAst',
+          mediaTypes: {
+            video: { context: 'outstream' }
+          }
+        }]
+      }
+    ]);
 
     const valid = isValidVideoBid({
-	    adId: '123abc'
+      adId: '123abc'
     });
 
     expect(valid).to.be(false);
