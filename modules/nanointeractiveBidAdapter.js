a60630bc29480d4e0af925283c4b2024ad25782f
goranbozicic <gbozicic@nanointeractive.com>
2017-10-16 11:06:39 +0200

    nanointeractive bid adapter

--- modules/nanointeractiveBidAdapter.js
+++ modules/nanointeractiveBidAdapter.js
@@ -1,156 +1,101 @@
-import bidfactory from 'src/bidfactory';
-import bidmanager from 'src/bidmanager';
 import * as utils from 'src/utils';
-import { ajax } from 'src/ajax';
-import { STATUS } from 'src/constants';
-import adaptermanager from 'src/adaptermanager';
+import { registerBidder } from '../src/adapters/bidderFactory';
+import { BANNER } from '../src/mediaTypes';
 
-const BIDDER_CODE = 'nanointeractive';
-const ENGINE_BASE_URL = 'https://www.audiencemanager.de/ad?type=js';
+export const BIDDER_CODE = 'nanointeractive';
+export const ENGINE_BASE_URL = 'http://tmp.audiencemanager.de/hb';
 
-const SECURITY = 'sec';
-const DATA_PARTNER_ID = 'dpid';
-const DATA_PARTNER_PIXEL_ID = 'pid';
-const ALG = 'a§lg';
-const NQ = 'nq';
-const NQ_NAME = 'name';
-const CATEGORY = 'category';
+export const SECURITY = 'sec';
+export const DATA_PARTNER_ID = 'dpid';
+export const DATA_PARTNER_PIXEL_ID = 'pid';
+export const ALG = 'alg';
+export const NQ = 'nq';
+export const NQ_NAME = 'name';
+export const CATEGORY = 'category';
 
 const DEFAULT_ALG = 'ih';
 
-const REQUEST_NQ = 'nq[]';
-const REQUEST_SIZE = 'size[]';
-const REQUEST_ALGORITHM = 'alg';
-const REQUEST_HEADER_BIDDING = 'hb';
-const REQUEST_HEADER_BID_ID = 'hbid';
-const REQUEST_DIRECT_CALL = 'dirCall';
+export const spec = {
 
-class NanointeractiveBidAdapter {
-  constructor() {
-    this.QUERY_STRING = utils.getTopWindowLocation().href.split('?');
-    this.QUERY_PARAMS = this.QUERY_STRING.length > 1 ? this.QUERY_STRING[1].split('&') : [];
-  }
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER],
 
-  callBids(params) {
-    params.bids.forEach(bid => {
-      this.timeoutResponse(bid, params.timeout);
-      this.formatParamsAndCallEngine(bid);
-    })
-  }
-
-  timeoutResponse(bid, timeout) {
-    setTimeout(() => {
-      this.addNoBidResponse(bid);
-    }, timeout)
-  }
-
-  formatParamsAndCallEngine(bid) {
+  isBidRequestValid(bid) {
     const sec = bid.params[SECURITY];
     const dpid = bid.params[DATA_PARTNER_ID];
     const pid = bid.params[DATA_PARTNER_PIXEL_ID];
-    const nq = bid.params[NQ_NAME] ? this.getQueryParam(bid.params[NQ_NAME]) : bid.params[NQ] || '';
-    const alg = bid.params[ALG] || DEFAULT_ALG;
-    const category = bid.params[CATEGORY];
-
-    const sizes = bid.sizes.map(value => value[0] + 'x' + value[1]);
-
-    if (!sec || !dpid || !pid) {
-      utils.logError('Required params are missing', bid);
-      this.addNoBidResponse(bid);
-    } else {
-      this.callEngine(sec, dpid, pid, nq, alg, category, sizes, bid);
-    }
-  }
-
-  callEngine(sec, dpid, pid, nq, alg, category, sizes, bid) {
-    ajax(
-      this.getEndpoint(sec, dpid, pid, nq, alg, category, sizes, bid.bidId),
-      {
-        success: response => this.successCallback(bid, response),
-        error: err => this.handleEngineResponseError(bid, err)
+    return !!(sec && dpid && pid);
+  },
+  buildRequests(bidRequests) {
+    let payload = [];
+    bidRequests.forEach(bid => payload.push(createSingleBidRequest(bid)));
+    return {
+      method: 'POST',
+      url: ENGINE_BASE_URL,
+      data: JSON.stringify(payload)
+    };
+  },
+  interpretResponse(serverResponse) {
+    const bids = [];
+    serverResponse.forEach(serverBid => {
+      if (isEngineResponseValid(serverBid)) {
+        bids.push(createSingleBidResponse(serverBid));
       }
-    );
+    });
+    return bids;
   }
+};
 
-  getQueryParam(nq) {
-    for (let i = 0; i < this.QUERY_PARAMS.length; i++) {
-      const pair = this.QUERY_PARAMS[i].split('=');
-      if (pair[0] === nq) {
-        return decodeURIComponent(pair[1]) || null;
-      }
-    }
-    return null;
+function createSingleBidRequest(bid) {
+  return {
+    [SECURITY]: bid.params[SECURITY],
+    [DATA_PARTNER_ID]: bid.params[DATA_PARTNER_ID],
+    [DATA_PARTNER_PIXEL_ID]: bid.params[DATA_PARTNER_PIXEL_ID],
+    [ALG]: bid.params[ALG] || DEFAULT_ALG,
+    [NQ]: [createNqParam(bid), createCategoryParam(bid)],
+    sizes: bid.sizes.map(value => value[0] + 'x' + value[1]),
+    bidId: bid.bidId,
+    cors: document.domain.includes('localhost') ? null : document.domain
   };
-
-  getEndpoint(sec, dpid, pid, nq, alg, category, sizes, bidId) {
-    return ENGINE_BASE_URL +
-      '&' + SECURITY + '=' + sec +
-      '&' + DATA_PARTNER_ID + '=' + dpid +
-      '&' + DATA_PARTNER_PIXEL_ID + '=' + pid +
-      this.formatSizes(sizes) +
-      '&' + REQUEST_NQ + '=' + nq +
-      '&' + REQUEST_ALGORITHM + '=' + alg +
-      this.formatCategory(category) +
-      '&' + REQUEST_HEADER_BIDDING + '=' + true +
-      '&' + REQUEST_HEADER_BID_ID + '=' + bidId +
-      '&' + REQUEST_DIRECT_CALL + '=' + 1
-  }
-
-  formatSizes(sizes) {
-    let sizesFormatted = '';
-    for (let i = 0; i < sizes.length; i++) {
-      sizesFormatted += '&' + REQUEST_SIZE + '=' + sizes[i];
-    }
-    return sizesFormatted;
-  }
-
-  formatCategory(category) {
-    return category ? '&nq=' + category : ''
-  }
-
-  successCallback(bid, response) {
-    try {
-      response = JSON.parse(response);
-      this.handleEngineResponse(bid, response);
-    } catch (err) {
-      this.handleEngineResponseError(bid, err);
-    }
-  }
-
-  handleEngineResponse(bid, response) {
-    if (this.isEngineResponseValid(response) === true) {
-      this.addBidResponse(bid, response);
-    } else {
-      this.addNoBidResponse(bid);
-    }
-  }
-
-  handleEngineResponseError(bid, error) {
-    utils.logError('Bid Response Error', bid, error);
-    this.addNoBidResponse(bid);
-  }
-
-  isEngineResponseValid(response) {
-    return !!response.cpm && !!response.ad;
-  }
-
-  addBidResponse(bid, response) {
-    let bidResponse = bidfactory.createBid(STATUS.GOOD, {bidId: bid.bidId});
-    bidResponse.cpm = response.cpm;
-    bidResponse.ad = response.ad;
-    bidResponse.width = response.width;
-    bidResponse.height = response.height;
-    bidResponse.bidderCode = BIDDER_CODE;
-    bidmanager.addBidResponse(bid.placementCode, bidResponse);
-  }
-
-  addNoBidResponse(bid) {
-    let bidResponse = bidfactory.createBid(STATUS.NO_BID, bid);
-    bidResponse.bidderCode = BIDDER_CODE;
-    bidmanager.addBidResponse(bid.placementCode, bidResponse);
-  }
 }
 
-adaptermanager.registerBidAdapter(new NanointeractiveBidAdapter(), BIDDER_CODE);
+function createSingleBidResponse(serverBid) {
+  return {
+    requestId: serverBid.id,
+    bidderCode: serverBid.bidderCode,
+    cpm: serverBid.cpm,
+    width: serverBid.width,
+    height: serverBid.height,
+    ad: serverBid.ad,
+    ttl: serverBid.ttl,
+    creativeId: serverBid.creativeId,
+    netRevenue: serverBid.netRevenue || true,
+    currency: serverBid.currency,
+  };
+}
 
-module.exports = NanointeractiveBidAdapter;
+function createNqParam(bid) {
+  return bid.params[NQ_NAME] ? getQueryParam(bid.params[NQ_NAME]) : bid.params[NQ] || null;
+}
+
+function createCategoryParam(bid) {
+  return bid.params[CATEGORY] || null;
+}
+
+function getQueryParam(nq) {
+  const queryString = utils.getTopWindowLocation().href.split('?');
+  const queryParams = queryString.length > 1 ? queryString[1].split('&') : [];
+  for (let i = 0; i < queryParams.length; i++) {
+    const pair = queryParams[i].split('=');
+    if (pair[0] === nq) {
+      return decodeURIComponent(pair[1]) || null;
+    }
+  }
+  return null;
+}
+
+function isEngineResponseValid(response) {
+  return !!response.cpm && !!response.ad;
+}
+
+registerBidder(spec);
--- test/spec/modules/nanointeractiveBidAdapter_spec.js
+++ test/spec/modules/nanointeractiveBidAdapter_spec.js
@@ -1,12 +1,11 @@
 import { expect } from 'chai';
-import bidmanager from 'src/bidmanager';
-import * as utils from 'src/utils';
-import NanointeractiveBidAdapter from 'modules/nanointeractiveBidAdapter';
-import * as ajax from 'src/ajax';
-import CONSTANTS from 'src/constants.json';
+import {
+  ALG,
+  BIDDER_CODE, CATEGORY, DATA_PARTNER_ID, DATA_PARTNER_PIXEL_ID, ENGINE_BASE_URL, NQ, NQ_NAME, SECURITY,
+  spec
+} from '../../../modules/nanointeractiveBidAdapter';
 
 describe('nanointeractive adapter tests', function () {
-  const BIDDER_CODE = 'nanointeractive';
   const SEARCH_QUERY = 'rumpelstiltskin';
   const WIDTH = 300;
   const HEIGHT = 250;
@@ -14,259 +13,99 @@ describe('nanointeractive adapter tests', function () {
   const AD = '<script type="text/javascript" src="https://trc.audiencemanager.de/ad/?pl=58c2829beb0a193456047a27&cb=${CACHEBUSTER}&tc=${CLICK_URL_ENC}"></script> <noscript> <a href="https://trc.audiencemanager.de/ad/?t=c&pl=58c2829beb0a193456047a27&cb=${CACHEBUSTER}&tc=${CLICK_URL_ENC}"> <img src="https://trc.audiencemanager.de/ad/?t=i&pl=58c2829beb0a193456047a27&cb=${CACHEBUSTER}" alt="Click Here" border="0"> </a> </noscript>';
   const CPM = 1;
 
-  const REQUEST = function (secParam, nameParam, nqParam) {
+  function getBid(isValid) {
     return {
-      bidderCode: BIDDER_CODE,
-      requestId: 'ac15bb68-4ef0-477f-93f4-de91c47f00a9',
-      bidderRequestId: '189135372acd55',
-      bids: [
-        {
-          bidder: BIDDER_CODE,
-          params: (function () {
-            return {
-              sec: secParam === true ? 'sec1' : null,
-              dpid: 'dpid1',
-              pid: 'pid1',
-              nq: nqParam === false ? null : SEARCH_QUERY,
-              name: nameParam === true ? 'nq' : null
-            }
-          })(),
-          placementCode: 'div-gpt-ad-1460505748561-0',
-          transactionId: 'ee335735-ddd3-41f2-b6c6-e8aa99f81c0f',
-          sizes: SIZES,
-          bidId: '24a1c9ec270973',
-          bidderRequestId: '189135372acd55',
-          requestId: 'ac15bb68-4ef0-477f-93f4-de91c47f00a9'
+      bidder: BIDDER_CODE,
+      params: (function () {
+        return {
+          [SECURITY]: isValid === true ? 'sec1' : null,
+          [DATA_PARTNER_ID]: 'dpid1',
+          [DATA_PARTNER_PIXEL_ID]: 'pid1',
+          [ALG]: 'ih',
+          [NQ]: SEARCH_QUERY,
+          [NQ_NAME]: null,
+          [CATEGORY]: null,
         }
-      ],
-      start: 1503482467787,
-      auctionStart: 1503482467785,
-      timeout: 3000
-    };
+      })(),
+      placementCode: 'div-gpt-ad-1460505748561-0',
+      transactionId: 'ee335735-ddd3-41f2-b6c6-e8aa99f81c0f',
+      sizes: SIZES,
+      bidId: '24a1c9ec270973',
+      bidderRequestId: '189135372acd55',
+      requestId: 'ac15bb68-4ef0-477f-93f4-de91c47f00a9'
+    }
+  }
+
+  const SINGlE_BID_REQUEST = {
+    [SECURITY]: 'sec1',
+    [DATA_PARTNER_ID]: 'dpid1',
+    [DATA_PARTNER_PIXEL_ID]: 'pid1',
+    [ALG]: 'ih',
+    [NQ]: [SEARCH_QUERY, null],
+    sizes: [WIDTH + 'x' + HEIGHT],
+    bidId: '24a1c9ec270973',
+    cors: null
   };
 
-  const VALID_RESPONSE = {
+  function getSingleBidResponse(isValid) {
+    return {
+      id: '24a1c9ec270973',
+      bidderCode: spec.code,
+      cpm: isValid === true ? CPM : null,
+      width: WIDTH,
+      height: HEIGHT,
+      ad: AD,
+      ttl: 360,
+      creativeId: 'TEST_ID',
+      netRevenue: false,
+      currency: 'EUR',
+    }
+  }
+
+  const VALID_BID = {
+    requestId: '24a1c9ec270973',
+    bidderCode: spec.code,
     cpm: CPM,
-    ad: AD,
     width: WIDTH,
     height: HEIGHT,
-    bidderCode: BIDDER_CODE,
-  };
-
-  const INVALID_RESPONSE = {
-    cpm: null,
     ad: AD,
-    width: WIDTH,
-    height: HEIGHT,
-    bidderCode: BIDDER_CODE,
+    ttl: 360,
+    creativeId: 'TEST_ID',
+    netRevenue: false,
+    currency: 'EUR',
   };
-
-  function createAjaxSuccessStub(response) {
-    return sinon.stub(ajax, 'ajax', (url, callbacks) => {
-      callbacks.success(
-        JSON.stringify(response)
-      );
-    });
-  }
-
-  function createAjaxErrorStub() {
-    return sinon.stub(ajax, 'ajax', (url, callbacks) => {
-      callbacks.error('Error');
-    });
-  }
 
   describe('NanoAdapter', () => {
-    let nanoBidAdapter;
-    let addBidResponse;
-
-    let getTopWindowLocation = sinon.stub(utils, 'getTopWindowLocation');
-    getTopWindowLocation.onFirstCall().returns({href: 'test?nq=TEST'});
-    getTopWindowLocation.returns({href: 'test'});
-
-    beforeEach(() => {
-      addBidResponse = sinon.stub(bidmanager, 'addBidResponse');
-      nanoBidAdapter = new NanointeractiveBidAdapter();
-    });
-    afterEach(() => {
-      addBidResponse.restore();
-    });
-
-    it('exists and is a function', () => {
-      expect(nanoBidAdapter.callBids).to.exist.and.to.be.a('function');
-    });
-
-    describe('Params', () => {
-      it('Test invalid sec params', function () {
-        let addNoBidResponseSinon = sinon.stub(nanoBidAdapter, 'addNoBidResponse');
-        nanoBidAdapter.callBids(REQUEST(false));
-        sinon.assert.calledOnce(addNoBidResponseSinon);
-      });
-
-      it('Test without nq param', function () {
-        let callEngineSinon = sinon.stub(nanoBidAdapter, 'callEngine');
-        nanoBidAdapter.callBids(REQUEST(true, false, false));
-        sinon.assert.calledOnce(callEngineSinon);
-      });
-
-      it('Test valid bid params', function () {
-        let callEngineSinon = sinon.stub(nanoBidAdapter, 'callEngine');
-        nanoBidAdapter.callBids(REQUEST(true));
-        sinon.assert.calledOnce(callEngineSinon);
-      });
-
-      it('Test name bid param', function () {
-        let getQueryParamSinon = sinon.stub(nanoBidAdapter, 'getQueryParam');
-        nanoBidAdapter.callBids(REQUEST(false, true));
-        sinon.assert.calledOnce(getQueryParamSinon);
-      });
-    });
+    let nanoBidAdapter = spec;
 
     describe('Methods', () => {
-      it('Test getQueryParam() with valid param', function () {
-        nanoBidAdapter.QUERY_PARAMS = ['testParam=TEST2', 'nq=TEST'];
-        expect(nanoBidAdapter.getQueryParam('nq')).to.equal('TEST')
+      it('Test isBidRequestValid() with valid param', function () {
+        expect(nanoBidAdapter.isBidRequestValid(getBid(true))).to.equal(true);
       });
-
-      it('Test getQueryParam() with invalid param', function () {
-        nanoBidAdapter.QUERY_PARAMS = ['nq='];
-        expect(nanoBidAdapter.getQueryParam('nq')).to.equal(null)
+      it('Test isBidRequestValid() with invalid param', function () {
+        expect(nanoBidAdapter.isBidRequestValid(getBid(false))).to.equal(false);
       });
-
-      it('Test getQueryParam() without params', function () {
-        nanoBidAdapter.QUERY_PARAMS = [];
-        expect(nanoBidAdapter.getQueryParam('nq')).to.equal(null)
+      it('Test buildRequests()', function () {
+        let request = nanoBidAdapter.buildRequests([getBid(true)]);
+        expect(request.method).to.equal('POST');
+        expect(request.url).to.equal(ENGINE_BASE_URL);
+        expect(request.data).to.equal(JSON.stringify([SINGlE_BID_REQUEST]));
       });
-
-      it('Test getEndpoint() without category', function () {
-        const endpoint = 'https://www.audiencemanager.de/ad?type=js' +
-          '&sec=sec1' +
-          '&dpid=dpid1' +
-          '&pid=pid1' +
-          '&size[]=300x250' +
-          '&size[]=728x90' +
-          '&nq[]=auto' +
-          '&alg=r' +
-          '&hb=true' +
-          '&hbid=testBidId' +
-          '&dirCall=1';
-        expect(nanoBidAdapter.getEndpoint('sec1', 'dpid1', 'pid1', 'auto', 'r', undefined, ['300x250', '728x90'], 'testBidId')).to.equal(endpoint)
+      it('Test interpretResponse() length', function () {
+        let bids = nanoBidAdapter.interpretResponse([getSingleBidResponse(true), getSingleBidResponse(false)]);
+        expect(bids.length).to.equal(1);
       });
-
-      it('Test getEndpoint() with category', function () {
-        const endpoint = 'https://www.audiencemanager.de/ad?type=js' +
-          '&sec=sec1' +
-          '&dpid=dpid1' +
-          '&pid=pid1' +
-          '&size[]=300x250' +
-          '&size[]=728x90' +
-          '&nq[]=auto' +
-          '&alg=r' +
-          '&nq=AUTO' +
-          '&hb=true' +
-          '&hbid=testBidId' +
-          '&dirCall=1';
-        expect(nanoBidAdapter.getEndpoint('sec1', 'dpid1', 'pid1', 'auto', 'r', 'AUTO', ['300x250', '728x90'], 'testBidId')).to.equal(endpoint)
-      });
-
-      it('Test formatSizes()', function () {
-        const sizes = ['300x250', '728x90'];
-        const sizesFormatted = '&size[]=300x250&size[]=728x90';
-        expect(nanoBidAdapter.formatSizes(sizes)).to.equal(sizesFormatted)
-      });
-
-      it('Test successCallback() with valid response', function () {
-        let handleEngineResponseSinon = sinon.stub(nanoBidAdapter, 'handleEngineResponse');
-        nanoBidAdapter.successCallback(REQUEST(true).bids[0], JSON.stringify(VALID_RESPONSE));
-        sinon.assert.calledOnce(handleEngineResponseSinon);
-      });
-
-      it('Test successCallback() with invalid response', function () {
-        let handleEngineResponseErrorSinon = sinon.stub(nanoBidAdapter, 'handleEngineResponseError');
-        nanoBidAdapter.successCallback(REQUEST(true).bids[0], null);
-        sinon.assert.calledOnce(handleEngineResponseErrorSinon);
-      });
-
-      it('Test handleEngineResponse() with valid response', function () {
-        let addBidResponseSinon = sinon.stub(nanoBidAdapter, 'addBidResponse');
-        nanoBidAdapter.handleEngineResponse(REQUEST(true).bids[0], VALID_RESPONSE);
-        sinon.assert.calledOnce(addBidResponseSinon);
-      });
-
-      it('Test handleEngineResponse() with invalid response', function () {
-        let addNoBidResponseSinon = sinon.stub(nanoBidAdapter, 'addNoBidResponse');
-        nanoBidAdapter.handleEngineResponse(REQUEST(true).bids[0], INVALID_RESPONSE);
-        sinon.assert.calledOnce(addNoBidResponseSinon);
-      });
-
-      it('Test handleEngineResponseError()', function () {
-        let addNoBidResponseSinon = sinon.stub(nanoBidAdapter, 'addNoBidResponse');
-        let logErrorSinon = sinon.stub(utils, 'logError');
-        nanoBidAdapter.handleEngineResponseError(REQUEST(true).bids[0], 'Error');
-        sinon.assert.calledOnce(addNoBidResponseSinon);
-        sinon.assert.calledOnce(logErrorSinon);
-      });
-
-      it('Test isEngineResponseValid() with valid response', function () {
-        expect(nanoBidAdapter.isEngineResponseValid(VALID_RESPONSE)).to.equal(true);
-      });
-
-      it('Test isEngineResponseValid() with invalid response', function () {
-        expect(nanoBidAdapter.isEngineResponseValid(INVALID_RESPONSE)).to.equal(false);
-      });
-
-      it('Test addBidResponse()', function () {
-        nanoBidAdapter.addBidResponse(REQUEST(true).bids[0], VALID_RESPONSE);
-        let arg = addBidResponse.firstCall.args[1];
-        expect(arg.bidderCode).to.equal(BIDDER_CODE);
-        expect(arg.width).to.equal(WIDTH);
-        expect(arg.height).to.equal(HEIGHT);
-        expect(arg.cpm).to.equal(CPM);
-        expect(arg.ad).to.equal(AD);
-        expect(arg.getStatusCode()).to.equal(CONSTANTS.STATUS.GOOD);
-      });
-
-      it('Test addNoBidResponse()', function () {
-        nanoBidAdapter.addNoBidResponse(REQUEST(true).bids[0]);
-        sinon.assert.calledOnce(addBidResponse);
-      });
-    });
-
-    describe('Ajax success request', () => {
-      it('Test success Ajax call', function () {
-        let stubAjaxSuccess = createAjaxSuccessStub(VALID_RESPONSE);
-        nanoBidAdapter.callBids(REQUEST(true));
-        sinon.assert.calledOnce(stubAjaxSuccess);
-        stubAjaxSuccess.restore();
-
-        let arg = addBidResponse.firstCall.args[1];
-        expect(arg.bidderCode).to.equal(BIDDER_CODE);
-        expect(arg.width).to.equal(WIDTH);
-        expect(arg.height).to.equal(HEIGHT);
-        expect(arg.cpm).to.equal(CPM);
-        expect(arg.ad).to.equal(AD);
-        expect(arg.getStatusCode()).to.equal(CONSTANTS.STATUS.GOOD);
-      });
-    });
-
-    describe('Ajax error request', () => {
-      it('Test error Ajax call', function () {
-        let stubAjaxError = createAjaxErrorStub();
-        nanoBidAdapter.callBids(REQUEST(true));
-        sinon.assert.calledOnce(stubAjaxError);
-        stubAjaxError.restore();
-
-        let arg = addBidResponse.firstCall.args[1];
-        expect(arg.getStatusCode()).to.equal(CONSTANTS.STATUS.NO_BID);
-      });
-    });
-
-    describe('Bid timeout', () => {
-      it('Test timeout response', function () {
-        nanoBidAdapter.timeoutResponse(REQUEST(true).bids[0], 0);
-        setTimeout(() => {
-          let addNoBidResponseSinon = sinon.stub(nanoBidAdapter, 'addNoBidResponse');
-          sinon.assert.calledOnce(addNoBidResponseSinon);
-        })
+      it('Test interpretResponse() bids', function () {
+        let bid = nanoBidAdapter.interpretResponse([getSingleBidResponse(true), getSingleBidResponse(false)])[0];
+        expect(bid.requestId).to.equal(VALID_BID.requestId);
+        expect(bid.bidderCode).to.equal(VALID_BID.bidderCode);
+        expect(bid.cpm).to.equal(VALID_BID.cpm);
+        expect(bid.width).to.equal(VALID_BID.width);
+        expect(bid.height).to.equal(VALID_BID.height);
+        expect(bid.ad).to.equal(VALID_BID.ad);
+        expect(bid.ttl).to.equal(VALID_BID.ttl);
+        expect(bid.creativeId).to.equal(VALID_BID.creativeId);
+        expect(bid.currency).to.equal(VALID_BID.currency);
       });
     });
   });
