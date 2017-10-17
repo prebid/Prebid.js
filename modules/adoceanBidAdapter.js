15c6a0236f8b974eedf58c8d800253b45b7671e0
Jacek Drobiecki <jacek.drobiecki@gemius.com>
2017-10-17 13:15:49 +0200

    formating after lint

--- modules/adoceanBidAdapter.js
+++ modules/adoceanBidAdapter.js
@@ -1,104 +1,103 @@
 import * as utils from 'src/utils';
 import { registerBidder } from 'src/adapters/bidderFactory';
-import { config } from 'src/config';
 
 const BIDDER_CODE = 'adocean';
 
 function buildEndpointUrl(emiter, payload) {
-    let payloadString = "";
-    utils._each(payload, function(v, k) {
-        if (payloadString.length) {
-            payloadString += "&";
-        }
-        payloadString += k + "=" + encodeURIComponent(v);
-    });
+  let payloadString = '';
+  utils._each(payload, function(v, k) {
+    if (payloadString.length) {
+      payloadString += '&';
+    }
+    payloadString += k + '=' + encodeURIComponent(v);
+  });
 
-    return 'https://' + emiter + '/ad.json?' + payloadString;
+  return 'https://' + emiter + '/ad.json?' + payloadString;
 }
 
 function buildRequest(masterBidRequests, masterId) {
-    const firstBid = masterBidRequests[0];
-    const payload = {
-        id: masterId,
-    };
+  const firstBid = masterBidRequests[0];
+  const payload = {
+    id: masterId,
+  };
 
-    const bidIdMap = {};
+  const bidIdMap = {};
 
-    utils._each(masterBidRequests, function(v) {
-        bidIdMap[v.slaveId] = v.bidId;
-    });
+  utils._each(masterBidRequests, function(v) {
+    bidIdMap[v.slaveId] = v.bidId;
+  });
 
-    return {
-        method: 'GET',
-        url: buildEndpointUrl(firstBid.emiter, payload),
-        data: {},
-        bidIdMap: bidIdMap
-    };
+  return {
+    method: 'GET',
+    url: buildEndpointUrl(firstBid.emiter, payload),
+    data: {},
+    bidIdMap: bidIdMap
+  };
 }
 
 function assignToMaster(bidRequest, bidRequestsByMaster) {
-    const masterId = bidRequest.masterId;
-    bidRequestsByMaster[masterId] = bidRequestsByMaster[masterId] || [];
-    bidRequestsByMaster[masterId].push(bidRequest);
+  const masterId = bidRequest.masterId;
+  bidRequestsByMaster[masterId] = bidRequestsByMaster[masterId] || [];
+  bidRequestsByMaster[masterId].push(bidRequest);
 }
 
 function interpretResponse(placementResponse, bidRequest, bids) {
-    if (!placementResponse.error) {
-        let adCode = "<script type=\"application/javascript\">(function(){var wu='" + (placementResponse.winUrl || '') + "',su='" + (placementResponse.statsUrl || '') + "'.replace(/\\[TIMESTAMP\\]/,(new Date()).getTime());";
-        adCode += "if(navigator.sendBeacon){if(wu){navigator.sendBeacon(wu)||((new Image(1,1)).src=wu)};if(su){navigator.sendBeacon(su)||((new Image(1,1)).src=su)}}";
-        adCode += "else{if(wu){(new Image(1,1)).src=wu;}if(su){(new Image(1,1)).src=su;}}";
-        adCode += "})();<\/script>";
-        adCode += decodeURIComponent(placementResponse.code);
+  if (!placementResponse.error) {
+    let adCode = '<script type="application/javascript">(function(){var wu="' + (placementResponse.winUrl || '') + '",su="' + (placementResponse.statsUrl || '') + '".replace(/\\[TIMESTAMP\\]/,(new Date()).getTime());';
+    adCode += 'if(navigator.sendBeacon){if(wu){navigator.sendBeacon(wu)||((new Image(1,1)).src=wu)};if(su){navigator.sendBeacon(su)||((new Image(1,1)).src=su)}}';
+    adCode += 'else{if(wu){(new Image(1,1)).src=wu;}if(su){(new Image(1,1)).src=su;}}';
+    adCode += '})();<\/script>';
+    adCode += decodeURIComponent(placementResponse.code);
 
-        const bid = {
-            ad: adCode,
-            bidderCode: BIDDER_CODE,
-            cpm: parseFloat(placementResponse.price),
-            currency: placementResponse.currency,
-            height: parseInt(placementResponse.height, 10),
-            requestId: bidRequest.bidIdMap[placementResponse.id],
-            width: parseInt(placementResponse.width, 10),
-        };
+    const bid = {
+      ad: adCode,
+      bidderCode: BIDDER_CODE,
+      cpm: parseFloat(placementResponse.price),
+      currency: placementResponse.currency,
+      height: parseInt(placementResponse.height, 10),
+      requestId: bidRequest.bidIdMap[placementResponse.id],
+      width: parseInt(placementResponse.width, 10),
+    };
 
-        bids.push(bid);
-    }
+    bids.push(bid);
+  }
 }
 
 export const spec = {
-    code: BIDDER_CODE,
+  code: BIDDER_CODE,
 
-    isBidRequestValid: function(bid) {
-        return !!(bid.slaveId && bid.masterId && bid.emiter);
-    },
+  isBidRequestValid: function(bid) {
+    return !!(bid.slaveId && bid.masterId && bid.emiter);
+  },
 
-    buildRequests: function(validBidRequests) {
-        const bidRequestsByMaster = {},
-	      requests = [];
+  buildRequests: function(validBidRequests) {
+    const bidRequestsByMaster = {};
+    let requests = [];
 
-        utils._each(validBidRequests, function(v) {
-            assignToMaster(v, bidRequestsByMaster);
-        });
-        requests = utils._map(bidRequestsByMaster, function(v, k) {
-            return buildRequest(v, k);
-        });
+    utils._each(validBidRequests, function(v) {
+      assignToMaster(v, bidRequestsByMaster);
+    });
+    requests = utils._map(bidRequestsByMaster, function(v, k) {
+      return buildRequest(v, k);
+    });
 
-        return requests;
-    },
+    return requests;
+  },
 
-    interpretResponse: function(serverResponse, bidRequest) {
-        let bids = [];
+  interpretResponse: function(serverResponse, bidRequest) {
+    let bids = [];
 
-        if (utils.isArray(serverResponse)) {
-            utils._each(serverResponse, function(placementResponse) {
-                interpretResponse(placementResponse, bidRequest, bids);
-            });
-        }
-
-        return bids;
-    },
-
-    getUserSyncs: function(syncOptions) {
-        // empty
+    if (utils.isArray(serverResponse)) {
+      utils._each(serverResponse, function(placementResponse) {
+        interpretResponse(placementResponse, bidRequest, bids);
+      });
     }
+
+    return bids;
+  },
+
+  getUserSyncs: function(syncOptions) {
+    // empty
+  }
 };
 registerBidder(spec);
