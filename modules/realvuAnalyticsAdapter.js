057d7e47f4854b239827fc25f76e4d486e1b28d4
Igor Tchibirev <igor@realvu.com>
2017-10-18 12:14:23 -0400

    _spec works

--- modules/realvuAnalyticsAdapter.js
+++ modules/realvuAnalyticsAdapter.js
@@ -3,17 +3,29 @@ import adapter from 'src/AnalyticsAdapter';
 import adaptermanager from 'src/adaptermanager';
 
 var realvuAnalyticsAdapter = adapter({
-  global: 'realVuAnalytics',
+  global: 'realvu',
   handler: 'on',
   analyticsType: 'library',
   url: '//ac.realvu.net/realvu_boost.js'
 });
 
+// save the base class function
+// QQQ realvuAnalyticsAdapter.originEnableAnalytics = adapter.enableAnalytics;
+
+// override enableAnalytics so we can get access to the config passed in from the page
 realvuAnalyticsAdapter.enableAnalytics = function (config) {
+  var msg = document.getElementById("msg_an");
+  if(msg) {
+    msg.innerHTML += 'config:<br>' + JSON.stringify(config);
+  }
+// QQQ  initOptions = config.options;
+// QQQ  realvuAnalyticsAdapter.originEnableAnalytics(config);  // call the base class function
 };
 
 realvuAnalyticsAdapter.track = function (eventType, args) {
+
 }
+
 var top1 = window;
 try {
   var wnd = window;
@@ -27,9 +39,10 @@ realvuAnalyticsAdapter.inView = function (bid, partner_id) {
   return top1.realvu_boost.addUnitById({unit_id: bid.placementCode, partner_id: partner_id, size: bid.sizes});
 }
 
+// queue() is a proxy function to add a callback function to boost_fifo to be async executed in realvu_boost
 realvuAnalyticsAdapter.queue = function (callback) {
   if (typeof callback === 'function') {
-    top1.boost_fifo = top.boost_fifo || [];
+    top1.boost_fifo = top1.boost_fifo || [];
     top1.boost_fifo.push(callback);
   }
 }
--- test/spec/modules/realvuAnalyticsAdapter_spec.js
+++ test/spec/modules/realvuAnalyticsAdapter_spec.js
@@ -1,23 +1,48 @@
 import {expect} from 'chai';
 import bidmanager from '../../../src/bidmanager';
 import adloader from '../../../src/adloader';
-describe('RealVu Analytics Adapter Test', () => {
-  var bidResponseStub = sinon.stub(bidmanager, 'addBidResponse');
-  var adloaderStub = sinon.stub(adloader, 'loadScript');
-  var realvuAnalyticsAdapter;
 
-  describe('load realvu_boost', () => {
+describe('RealVu Adapter Test.', () => {
+  var top1 = window;
+  try {
+    var wnd = window;
+    while ((top != top1) && (typeof (wnd.document) != 'undefined')) {
+      top1 = wnd;
+      wnd = wnd.parent;
+    }
+  } catch (e) { };
+  top1.realvu_boost = {
+    addUnitById: function() {
+      return 'yes';
+    }
+  };
+  top1.boost_fifo = top1.boost_fifo || [];
+
+  var realvuAnalyticsAdapter;
+  var adloaderStub;
+
+  beforeEach(function() {
+    adloaderStub = sinon.stub(adloader, 'loadScript');
+  });
+
+  afterEach(function() {
+    adloader.loadScript.restore();
+  });
+
+  it('Load realvu_boost.js', () => {
     realvuAnalyticsAdapter = require('../../../modules/realvuAnalyticsAdapter');
     expect(adloaderStub.getCall(0).args[0]).to.contain('realvu_boost.js');
   });
-  describe('inView', () => {
-    var div1 = document.createElement('DIV');
-    div1.id = 'rvaa_test_div';
-    div1.style.width = '300px';
-    div1.style.height = '250px';
-    document.body.appendChild(div1);
-    realvuAnalyticsAdapter.queue(function() {
-      expect(realvuAnalyticsAdapter.inView({placementCode: 'rvaa_test_div', sizes: [300, 250]}, '1Y')).to.equal('yes');
-    });
+
+  it('inView call', () => {
+    var addunitStub = sinon.stub(top1.realvu_boost, 'addUnitById', function() { return 'yes'; });
+    realvuAnalyticsAdapter.inView({placementCode: 'unitA', sizes: [[300], [250]] }, '1Y');
+    sinon.assert.calledWith(addunitStub, sinon.match({ unit_id: 'unitA', partner_id: '1Y', size: [[300], [250]]}));
+    addunitStub.restore();
+  });
+
+  it('boost_fifo', () => {
+    realvuAnalyticsAdapter.queue(function() { });
+    expect(top1.boost_fifo.length).to.equal(1);
   });
 });
