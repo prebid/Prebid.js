d99851e2c75740d7bd8ac5ed3291af80275898ab
Li Xinyang <lixinyang1026@gmail.com>
2017-10-20 08:53:53 +0800

    Implemenent test cases

--- test/spec/modules/quantcastBidAdapter_spec.js
+++ test/spec/modules/quantcastBidAdapter_spec.js
@@ -1,9 +1,30 @@
+import * as utils from "src/utils";
 import { expect } from "chai";
-import { spec as qcSpec } from "../../../modules/quantcastBidAdapter";
+import {
+  spec as qcSpec,
+  QUANTCAST_CALLBACK_URL,
+  QUANTCAST_CALLBACK_URL_TEST
+} from "../../../modules/quantcastBidAdapter";
 import { newBidder } from "../../../src/adapters/bidderFactory";
 
 describe("Quantcast adapter", () => {
   const quantcastAdapter = newBidder(qcSpec);
+  let bidRequest;
+
+  beforeEach(() => {
+    bidRequest = {
+      bidder: "quantcast",
+      bidId: "2f7b179d443f14",
+      requestId: "595ffa73-d78a-46c9-b18e-f99548a5be6b",
+      bidderRequestId: "1cc026909c24c8",
+      placementCode: "div-gpt-ad-1438287399331-0",
+      params: {
+        publisherId: "test-publisher", // REQUIRED - Publisher ID provided by Quantcast
+        battr: [1, 2] // OPTIONAL - Array of blocked creative attributes as per OpenRTB Spec List 5.3
+      },
+      sizes: [[300, 250]]
+    };
+  });
 
   describe("inherited functions", () => {
     it("exists and is a function", () => {
@@ -12,19 +33,94 @@ describe("Quantcast adapter", () => {
   });
 
   describe("`isBidRequestValid`", () => {
-    it("should return `false` when bid `mediaType` is `video`", () => {});
+    it("should return `false` when bid is not passed", () => {
+      expect(qcSpec.isBidRequestValid()).to.equal(false);
+    });
 
-    it("should return `true` when bid contains required params", () => {});
+    it("should return `false` when bid `mediaType` is `video`", () => {
+      const bidRequest = { mediaType: "video" };
+
+      expect(qcSpec.isBidRequestValid(bidRequest)).to.equal(false);
+    });
+
+    it("should return `true` when bid contains required params", () => {
+      const bidRequest = { mediaType: "banner" };
+
+      expect(qcSpec.isBidRequestValid(bidRequest)).to.equal(true);
+    });
   });
 
   describe("`buildRequests`", () => {
-    it("sends bid requests to Quantcast Canary Endpoint if `publisherId` is `test-publisher`", () => {});
+    // it("sends bid requests to Quantcast Canary Endpoint if `publisherId` is `test-publisher`", () => {
+    //   const requests = qcSpec.buildRequests([bidRequest]);
 
-    it("sends bid requests to Quantcast Header Bidding Endpoints via POST", () => {});
+    //   expect(requests[0]["url"]).to.satisfy(URl => {
+    //     switch (URL) {
+    //       case `https://${QUANTCAST_CALLBACK_URL_TEST}:8443/qchb`:
+    //       case `http://${QUANTCAST_CALLBACK_URL_TEST}:8080/qchb`:
+    //         return true;
+    //       default:
+    //         return false;
+    //     }
+    //   });
+    // });
 
-    it("sends bid requests with `withCredentials` enabled", () => {});
+    // it("sends bid requests to Quantcast Global Endpoint for regular `publisherId`", () => {
+    //   const bidRequest = { publisherId: "regular-publisher" };
+    //   const requests = qcSpec.buildRequests([bidRequest]);
 
-    it("sends bid requests contains all the required parameters", () => {});
+    //   expect(requests[0].url).to.satisfy(URL => {
+    //     switch (URL) {
+    //       case `https://${QUANTCAST_CALLBACK_URL}:8443/qchb`:
+    //       case `http://${QUANTCAST_CALLBACK_URL}:8080/qchb`:
+    //         return true;
+    //       default:
+    //         return false;
+    //     }
+    //   });
+    // });
+
+    it("sends bid requests to Quantcast Header Bidding Endpoints via POST", () => {
+      const requests = qcSpec.buildRequests([bidRequest]);
+
+      expect(requests[0].method).to.equal("POST");
+    });
+
+    it("sends bid requests with `withCredentials` enabled", () => {
+      const requests = qcSpec.buildRequests([bidRequest]);
+
+      expect(requests[0].withCredentials).to.equal(true);
+    });
+
+    it("sends bid requests contains all the required parameters", () => {
+      const referrer = utils.getTopWindowUrl();
+      const loc = utils.getTopWindowLocation();
+      const domain = loc.hostname;
+
+      const requests = qcSpec.buildRequests([bidRequest]);
+      const expectedBidRequest = {
+        publisherId: "test-publisher",
+        requestId: "2f7b179d443f14",
+        imp: [
+          {
+            banner: {
+              battr: [1, 2],
+              size: [{ width: 300, height: 250 }]
+            },
+            placementCode: "div-gpt-ad-1438287399331-0",
+            bidFloor: 1e-10
+          }
+        ],
+        site: {
+          page: loc.href,
+          referrer,
+          domain
+        },
+        bidId: "2f7b179d443f14"
+      };
+
+      expect(requests[0].data).to.equal(JSON.stringify(expectedBidRequest));
+    });
   });
 
   describe("`interpretResponse`", () => {
@@ -45,18 +141,53 @@ describe("Quantcast adapter", () => {
       ]
     };
 
-    it("should return an empty array if `serverResponse` is empty", () => {});
+    it("should return an empty array if `serverResponse` is `undefined`", () => {
+      const interpretedResponse = qcSpec.interpretResponse();
 
-    it("should return an empty array if there is an error to parse the `serverResponse`", () => {});
+      expect(interpretedResponse.length).to.equal(0);
+    });
 
-    it("should return an empty array if the parsed response does NOT include `bids`", () => {});
+    it("should return an empty array if the parsed response does NOT include `bids`", () => {
+      const interpretedResponse = qcSpec.interpretResponse({});
 
-    it("should return an empty array if the parsed response has an empty `bids`", () => {});
+      expect(interpretedResponse.length).to.equal(0);
+    });
 
-    it("should return an empty array if the parsed response is `null`", () => {});
+    it("should return an empty array if the parsed response has an empty `bids`", () => {
+      const interpretedResponse = qcSpec.interpretResponse({ bids: [] });
 
-    it("should get correct bid response", () => {});
+      expect(interpretedResponse.length).to.equal(0);
+    });
 
-    it("handles no bid response", () => {});
+    it("should get correct bid response", () => {
+      const expectedResponse = {
+        ad:
+          '<!DOCTYPE html> <div style="height: 250; width: 300; display: table-cell; vertical-align: middle;"> <div style="width: 300px; margin-left: auto; margin-right: auto;"> <script src="https://adserver.adtechus.com/addyn/3.0/5399.1/2394397/0/-1/QUANTCAST;size=300x250;target=_blank;alias=;kvp36=;sub1=;kvl=;kvc=;kvs=300x250;kvi=;kva=;sub2=;rdclick=http://exch.quantserve.com/r?a=;labels=_qc.clk,_click.adserver.rtb,_click.rand.;rtbip=;rtbdata2=;redirecturl2=" type="text/javascript"></script> <img src="https://exch.quantserve.com/pixel/p_12345.gif?media=ad&p=&r=&rand=&labels=_qc.imp,_imp.adserver.rtb&rtbip=&rtbdata2=" style="display: none;" border="0" height="1" width="1" alt="Quantcast"/> </div> </div>',
+        cpm: 4.5,
+        width: 300,
+        height: 250,
+        requestId: "erlangcluster@qa-rtb002.us-ec.adtech.com-11417780270886458",
+        bidderCode: "qcx"
+      };
+      const expectedResponse0 =
+        '{"ad":"<!DOCTYPE html>\\n\\n\\n<div style=\\"height: 250; width: 300; display: table-cell; vertical-align: middle;\\">\\n<div style=\\"width: 300px; margin-left: auto; margin-right: auto;\\">  \\n\\n  <script src=\\"https://adserver.adtechus.com/addyn/3.0/5399.1/2394397/0/-1/QUANTCAST;size=300x250;target=_blank;alias=;kvp36=;sub1=;kvl=;kvc=;kvs=300x250;kvi=;kva=;sub2=;rdclick=http://exch.quantserve.com/r?a=;labels=_qc.clk,_click.adserver.rtb,_click.rand.;rtbip=;rtbdata2=;redirecturl2=\\" type=\\"text/javascript\\"></script>\\n\\n<img src=\\"https://exch.quantserve.com/pixel/p_12345.gif?media=ad&p=&r=&rand=&labels=_qc.imp,_imp.adserver.rtb&rtbip=&rtbdata2=\\" style=\\"display: none;\\" border=\\"0\\" height=\\"1\\" width=\\"1\\" alt=\\"Quantcast\\"/>\\n\\n</div>\\n</div>","cpm":4.5,"width":300,"height":250,"requestId":"erlangcluster@qa-rtb002.us-ec.adtech.com-11417780270886458","bidderCode":"qcx"}';
+      const interpretedResponse = qcSpec.interpretResponse(response);
+
+      expect(JSON.stringify(interpretedResponse[0])).to.equal(
+        expectedResponse0
+      );
+    });
+
+    it("handles no bid response", () => {
+      const response = {
+        bidderCode: "qcx", // Renaming it to use CamelCase since that is what is used in the Prebid.js variable name
+        requestId: "erlangcluster@qa-rtb002.us-ec.adtech.com-11417780270886458", // Added this field. This is not used now but could be useful in troubleshooting later on. Specially for sites using iFrames
+        bids: []
+      };
+      const expectedResponse = [];
+      const interpretedResponse = qcSpec.interpretResponse(response);
+
+      expect(interpretedResponse.length).to.equal(0);
+    });
   });
 });
