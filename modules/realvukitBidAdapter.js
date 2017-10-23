a407f2acb7b6722127e99eada0af466c593a82a7
Igor Tchibirev <igor@realvu.com>
2017-10-23 18:28:26 -0400

    lint ok

--- modules/realvukitBidAdapter.js
+++ modules/realvukitBidAdapter.js
@@ -15,10 +15,10 @@ var RealVuKitAdapter = function RealVuKitAdapter() {
   baseAdapter.callBids = function (params) {
     // utils.logMessage('realvuBidAdapter params: ' + JSON.stringify(params));
     var pbids = params.bids;
-    //utils.logMessage('RealVu params: '+JSON.stringify(params)); 
+    // utils.logMessage('RealVu params: '+JSON.stringify(params)); 
 
-    for(var i = 0;i < pbids.length;i++){
-      var bid_rq = pbids[i];    
+    for (var i = 0; i < pbids.length; i++) {
+      var bid_rq = pbids[i];
       var bidId = bid_rq.bidId;
       adloader.loadScript(buildRvCall(bid_rq, bidId));
     }
@@ -27,29 +27,29 @@ var RealVuKitAdapter = function RealVuKitAdapter() {
   var kitMap = [];
 
   function buildRvCall(bid, bidId) {
-    //determine tag params
+    // determine tag params
     var p = utils.getBidIdParameter('unit_id', bid.params);
     var c = utils.getBidIdParameter('partner_id', bid.params);
-    var sz =  bid.sizes[0][0]+'x'+bid.sizes[0][1];
-    var rv_call = '//pr.realvu.net/flip/2/p='+p+'_f=unit_s='+sz+'_js=1_c='+c;
-    //was rv_call +='?uid='+bid.placementCode+'&callback=$$PREBID_GLOBAL$$.rvkit_handler&bid_id='+bidId;
-    rv_call += '?callback=$$PREBID_GLOBAL$$.rvkit_handler&uid='+encodeURIComponent(bid.placementCode);
-    kitMap.push({p:p, uid:bid.placementCode, bid_id:bidId}); 
-    //utils.logMessage('realvu request built: ' + rv_call);
+    var sz = bid.sizes[0][0] + 'x' + bid.sizes[0][1];
+    var rv_call = '//pr.realvu.net/flip/2/p=' + p + '_f=unit_s=' + sz + '_js=1_c=' + c;
+    // was rv_call +='?uid='+bid.placementCode+'&callback=$$PREBID_GLOBAL$$.rvkit_handler&bid_id='+bidId;
+    rv_call += '?callback=$$PREBID_GLOBAL$$.rvkit_handler&uid=' + encodeURIComponent(bid.placementCode);
+    kitMap.push({p: p, uid: bid.placementCode, bid_id: bidId});
+    // utils.logMessage('realvu request built: ' + rv_call);
     bid.startTime = new Date().getTime();
     return rv_call;
   }
 
-  $$PREBID_GLOBAL$$.rvkit_handler = function(rv_response){
-    //utils.logMessage('realvu rv_response: ' + JSON.stringify(rv_response) );
-    for(var i=0;i<rv_response.length;i++){
-      var ri=rv_response[i];
+  $$PREBID_GLOBAL$$.rvkit_handler = function(rv_response) {
+    // utils.logMessage('realvu rv_response: ' + JSON.stringify(rv_response) );
+    for (var i = 0; i < rv_response.length; i++) {
+      var ri = rv_response[i];
       // restore bid_id from kitMap with p
-      var pr = ri.p
-      for(var j=0;j<kitMap.length;j++) {
+      // var pr = ri.p
+      for (var j = 0; j < kitMap.length; j++) {
         var aj = kitMap[j];
-        if(ri.p === aj.p && ri.uid === aj.uid) {
-          ri.bid_id=aj.bid_id;
+        if (ri.p === aj.p && ri.uid === aj.uid) {
+          ri.bid_id = aj.bid_id;
           kitMap.splice(j, 1);
           break;
         }
@@ -66,22 +66,21 @@ var RealVuKitAdapter = function RealVuKitAdapter() {
           bidObj.status = CONSTANTS.STATUS.GOOD;
         }
         var bid = {};
-        var rvbid=ri.bid;
+        var rvbid = ri.bid;
         if (rvbid && rvbid.cpm && rvbid.cpm !== 0) {
           var adId = rvbid.creative_id;
           bid = bidfactory.createBid(1, bidObj);
           bid.creative_id = adId;
           bid.bidderCode = bidCode;
           bid.cpm = parseFloat(rvbid.cpm);
-          bid.ad = rvbid.ad; 
+          bid.ad = rvbid.ad;
           bid.adUrl = rvbid.adUrl;
           bid.width = rvbid.width;
           bid.height = rvbid.height;
           bid.dealId = rvbid.deal_id;
           bidmanager.addBidResponse(placementCode, bid);
-        } 
-        else {
-          //respond no bid
+        } else {
+          // respond no bid
           bid = bidfactory.createBid(2, bidObj);
           bid.bidderCode = bidCode;
           bidmanager.addBidResponse(placementCode, bid);
