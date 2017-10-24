5276b331847339a7914e61fbb5989a7a1bfb857a
Norbert <norbert.wlosinski@justpremium.com>
2017-10-24 10:30:16 +0200

    changes for adpater and new tests

--- modules/justpremiumBidAdapter.js
+++ modules/justpremiumBidAdapter.js
@@ -1,67 +1,49 @@
 import {registerBidder} from 'src/adapters/bidderFactory';
 const BIDDER_CODE = 'justpremium';
-// const ENDPOINT_URL = 'http://pre.ads.justpremium.com/v/2.0/t/ixhr';
-const ENDPOINT_URL = 'http://localhost:63342/header-bidder/public/xhr.txt';
+const ENDPOINT_URL = 'http://pre.ads.justpremium.com/v/2.0/t/ixhr';
 
 export const spec = {
   code: BIDDER_CODE,
   time: 60000,
-  aliases: ['jpx'], // short code
-  /**
-   * Determines whether or not the given bid request is valid.
-   *
-   * @param {BidRequest} bid The bid params to validate.
-   * @return boolean True if this is a valid bid, and false otherwise.
-   */
+
   isBidRequestValid: function(bid) {
     return !!(bid && bid.params && bid.params.zone);
   },
-  /**
-   * Make a server request from the list of BidRequests.
-   *
-   * @param {validBidRequests[]} - an array of bids
-   * @return ServerRequest Info describing the request to the server.
-   */
+
   buildRequests: function(validBidRequests) {
-    const c = preparePubCond(validBidRequests);
-    const payload = {
-      zone: [...new Set(validBidRequests.map(b => parseInt(b.params.zone)))].join(','),
+    var c = preparePubCond(validBidRequests);
+    var payload = {
+      zone: [].concat(toConsumableArray(new Set(validBidRequests.map(function (b) {
+        return parseInt(b.params.zone);
+      })))).join(','),
       hostname: top.document.location.hostname,
       protocol: top.document.location.protocol.replace(':', ''),
       sw: window.top.screen.width,
       sh: window.top.screen.height,
       ww: window.top.innerWidth,
       wh: window.top.innerHeight,
-      c: encodeURIComponent(JSON.stringify(c)),
+      c: c,
       id: validBidRequests[0].params.zone,
-      i: (+new Date())
+      i: +new Date()
     };
 
-    const payloadString = JSON.stringify(payload);
+    var payloadString = JSON.stringify(payload);
+
     return {
       method: 'POST',
       url: ENDPOINT_URL,
       data: payloadString,
-      bidRequests: validBidRequests
-    }
+      bids: validBidRequests
+    };
   },
-  /**
-   * Unpack the response from the server into a list of bids.
-   *
-   * @param {*} serverResponse A successful response from the server.
-   * @return {Bid[]} An array of bids which were nested inside the server.
-   */
-  interpretResponse: function(serverResponse, bidRequest) {
-    const bidResponses = [];
-    const allRequests = Object.keys(bidRequest.bidRequests);
 
-    allRequests.forEach(function (key) {
-      const adUnit = bidRequest.bidRequests[key].params;
-      const bidId = bidRequest.bidRequests[key].bidId;
-      const bid = findBid(adUnit, serverResponse.bid);
-
+  interpretResponse: function interpretResponse(serverResponse, bidRequests) {
+    var bidResponses = [];
+    bidRequests.bids.forEach(function (adUnit) {
+      var bidId = adUnit.bidId;
+      var bid = findBid(adUnit.params, serverResponse.bid);
       if (bid) {
-        const bidResponse = {
+        var bidResponse = {
           requestId: bidId,
           bidderCode: spec.code,
           width: bid.width,
@@ -69,7 +51,7 @@ export const spec = {
           ad: bid.adm,
           cpm: bid.price,
           currency: 'USD',
-          ttl: spec.time,
+          ttl: spec.time
         };
         bidResponses.push(bidResponse);
       }
@@ -78,13 +60,22 @@ export const spec = {
     return bidResponses;
   },
 
-  getUserSyncs: function(syncOptions) {
-    // if (syncOptions.iframeEnabled) {
+  getUserSyncs: function() {
     return [{
-      type: 'iframe',
+      type: 'script',
       url: top.document.location.protocol + '//ox-d.justpremium.com/w/1.0/cj'
     }];
-    // }
+  }
+}
+
+function toConsumableArray (arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
+      arr2[i] = arr[i];
+    }
+    return arr2;
+  } else {
+    return Array.from(arr);
   }
 }
 
@@ -144,26 +135,32 @@ function preparePubCond(bids) {
     exclude.forEach(function (e) {
       if (!cond[zone][1][e]) {
         cond[zone][1][e] = 1;
-      } else cond[zone][1][e]++;
+      } else {
+        cond[zone][1][e]++;
+      }
     });
 
     count[zone] = count[zone] || 0;
-    if (exclude.length) count[zone]++;
+    if (exclude.length) {
+      count[zone]++;
+    }
   });
 
-  Object.keys(count).forEach(zone => {
+  Object.keys(count).forEach(function(zone) {
     if (cond[zone] === 1) return;
 
     const exclude = [];
-    Object.keys(cond[zone][1]).forEach(format => {
-      if (cond[zone][1][format] === count[zone]) exclude.push(format);
+    Object.keys(cond[zone][1]).forEach(function(format) {
+      if (cond[zone][1][format] === count[zone]) {
+        exclude.push(format);
+      }
     });
     cond[zone][1] = exclude;
   });
 
-  Object.keys(cond).forEach(zone => {
+  Object.keys(cond).forEach(function(zone) {
     if (cond[zone] !== 1 && cond[zone][1].length) {
-      cond[zone][0].forEach(r => {
+      cond[zone][0].forEach(function(r) {
         var idx = cond[zone][1].indexOf(r);
         if (idx > -1) {
           cond[zone][1].splice(idx, 1);
@@ -172,7 +169,9 @@ function preparePubCond(bids) {
       cond[zone][0].length = 0;
     }
 
-    if (cond[zone] !== 1 && !cond[zone][0].length && !cond[zone][1].length) cond[zone] = 1;
+    if (cond[zone] !== 1 && !cond[zone][0].length && !cond[zone][1].length) {
+      cond[zone] = 1;
+    }
   });
 
   return cond;
@@ -180,8 +179,8 @@ function preparePubCond(bids) {
 
 function arrayUnique(array) {
   const a = array.concat();
-  for (let i = 0; i < a.length; ++i) {
-    for (let j = i + 1; j < a.length; ++j) {
+  for (var i = 0; i < a.length; ++i) {
+    for (var j = i + 1; j < a.length; ++j) {
       if (a[i] === a[j]) {
         a.splice(j--, 1);
       }
