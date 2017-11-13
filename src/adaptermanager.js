04e41b58fcf84ff01343c7ad78e8ac3883dffd08
Eric Harper <eharper@rubiconproject.com>
2017-11-13 15:57:48 -0500

    fix adaptermanager s2sTest unit tests

--- src/adaptermanager.js
+++ src/adaptermanager.js
@@ -15,7 +15,10 @@ let s2sTestingModule; // store s2sTesting module if it's loaded
 var _bidderRegistry = {};
 exports.bidderRegistry = _bidderRegistry;
 
-let _s2sConfig = config.getConfig('s2sConfig');
+let _s2sConfig = {};
+config.getConfig('s2sConfig', config => {
+  _s2sConfig = config.s2sConfig;
+});
 
 var _analyticsRegistry = {};
 
@@ -72,15 +75,15 @@ function getAdUnitCopyForPrebidServer(adUnits) {
   let adaptersServerSide = _s2sConfig.bidders;
   let adUnitsCopy = utils.cloneJson(adUnits);
 
-  // filter out client side bids
   adUnitsCopy.forEach((adUnit) => {
     if (adUnit.sizeMapping) {
       adUnit.sizes = mapSizes(adUnit);
       delete adUnit.sizeMapping;
     }
     adUnit.sizes = transformHeightWidth(adUnit);
+    // filter out client side bids
     adUnit.bids = adUnit.bids.filter((bid) => {
-      return adaptersServerSide.includes(bid.bidder) && (!s2sTesting || bid.finalSource !== s2sTestingModule.CLIENT);
+      return adaptersServerSide.includes(bid.bidder) && (!doingS2STesting() || bid.finalSource !== s2sTestingModule.CLIENT);
     }).map((bid) => {
       bid.bid_id = utils.getUniqueIdentifierStr();
       return bid;
@@ -94,6 +97,25 @@ function getAdUnitCopyForPrebidServer(adUnits) {
   return adUnitsCopy;
 }
 
+function getAdUnitCopyForClientAdapters(adUnits) {
+  let adUnitsClientCopy = utils.cloneJson(adUnits);
+  // filter out s2s bids
+  adUnitsClientCopy.forEach((adUnit) => {
+    adUnit.bids = adUnit.bids.filter((bid) => {
+      return !doingS2STesting() || bid.finalSource !== s2sTestingModule.SERVER;
+    })
+  });
+
+  // don't send empty requests
+  adUnitsClientCopy = adUnitsClientCopy.filter(adUnit => {
+    return adUnit.bids.length !== 0;
+  });
+
+  return adUnitsClientCopy;
+}
+
+// ericeric: why is this broken out into a public function?  why not just call from callBids()?
+// I see auction.js is tracking _bidderRequests, but maybe callBids can just return them directly?
 exports.makeBidRequests = function(adUnits, auctionStart, auctionId, cbTimeout) {
   let bidRequests = [];
   let bidderCodes = getBidderCodes(adUnits);
@@ -107,12 +129,11 @@ exports.makeBidRequests = function(adUnits, auctionStart, auctionId, cbTimeout) 
     s2sAdapter.queueSync({bidderCodes});
   }
 
+  let clientBidderCodes = bidderCodes;
   let clientTestAdapters = [];
-  let s2sTesting = false;
   if (_s2sConfig.enabled) {
     // if s2sConfig.bidderControl testing is turned on
-    s2sTesting = _s2sConfig.testing && typeof s2sTestingModule !== 'undefined';
-    if (s2sTesting) {
+    if (doingS2STesting()) {
       // get all adapters doing client testing
       clientTestAdapters = s2sTestingModule.getSourceBidderMap(adUnits)[s2sTestingModule.CLIENT];
     }
@@ -121,11 +142,11 @@ exports.makeBidRequests = function(adUnits, auctionStart, auctionId, cbTimeout) 
     let adaptersServerSide = _s2sConfig.bidders;
 
     // don't call these client side (unless client request is needed for testing)
-    bidderCodes = bidderCodes.filter((elm) => {
+    clientBidderCodes = bidderCodes.filter((elm) => {
       return !adaptersServerSide.includes(elm) || clientTestAdapters.includes(elm);
     });
-    let adUnitsS2SCopy = getAdUnitCopyForPrebidServer(adUnits);
 
+    let adUnitsS2SCopy = getAdUnitCopyForPrebidServer(adUnits);
     let tid = utils.generateUUID();
     adaptersServerSide.forEach(bidderCode => {
       const bidderRequestId = utils.getUniqueIdentifierStr();
@@ -145,27 +166,15 @@ exports.makeBidRequests = function(adUnits, auctionStart, auctionId, cbTimeout) 
     });
   }
 
-  // client side adapters
-  let adUnitsClientCopy = utils.cloneJson(adUnits);
-  // filter out s2s bids
-  adUnitsClientCopy.forEach((adUnit) => {
-    adUnit.bids = adUnit.bids.filter((bid) => {
-      return !s2sTesting || bid.finalSource !== s2sTestingModule.SERVER;
-    })
-  });
-
-  // don't send empty requests
-  adUnitsClientCopy = adUnitsClientCopy.filter(adUnit => {
-    return adUnit.bids.length !== 0;
-  });
-
-  bidderCodes.forEach(bidderCode => {
+  // client adapters
+  let adUnitsClientCopy = getAdUnitCopyForClientAdapters(adUnits);
+  clientBidderCodes.forEach(bidderCode => {
     const bidderRequestId = utils.getUniqueIdentifierStr();
     const bidderRequest = {
       bidderCode,
       auctionId,
       bidderRequestId,
-      bids: getBids({bidderCode, auctionId, bidderRequestId, adUnits}),
+      bids: getBids({bidderCode, auctionId, bidderRequestId, 'adUnits': adUnitsClientCopy}),
       auctionStart: auctionStart,
       timeout: cbTimeout
     };
@@ -177,6 +186,7 @@ exports.makeBidRequests = function(adUnits, auctionStart, auctionId, cbTimeout) 
 }
 
 exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
+  // handle s2s requests
   let serverBidRequests = bidRequests.filter(bidRequest => {
     return bidRequest.src && bidRequest.src === CONSTANTS.S2S.SRC;
   });
@@ -194,8 +204,13 @@ exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
       }
     }
   }
+
+  // handle client adapter requests
   let ajax = ajaxBuilder(bidRequests[0].timeout);
-  bidRequests.forEach(bidRequest => {
+  // first filter out s2s requests
+  bidRequests.filter(bidRequest => {
+    return !serverBidRequests.includes(bidRequest);
+  }).forEach(bidRequest => {
     bidRequest.start = new Date().getTime();
     // TODO : Do we check for bid in pool from here and skip calling adapter again ?
     const adapter = _bidderRegistry[bidRequest.bidderCode];
@@ -209,6 +224,10 @@ exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
       utils.logError(`Adapter trying to be called which does not exist: ${bidRequest.bidderCode} adaptermanager.callBids`);
     }
   });
+}
+
+function doingS2STesting() {
+  return _s2sConfig && _s2sConfig.enabled && _s2sConfig.testing && s2sTestingModule;
 }
 
 function transformHeightWidth(adUnit) {
--- test/spec/unit/core/adapterManager_spec.js
+++ test/spec/unit/core/adapterManager_spec.js
@@ -251,6 +251,191 @@ describe('adapterManager tests', () => {
     });
   }); // end s2s tests
 
+  describe('s2sTesting', () => {
+    function getTestAdUnits() {
+      // copy adUnits
+      return JSON.parse(JSON.stringify(getAdUnits()));
+    }
+
+    function callBids(adUnits = getTestAdUnits()) {
+      let bidRequests = AdapterManager.makeBidRequests(adUnits, 1111, 2222, 1000);
+      AdapterManager.callBids(adUnits, bidRequests, () => {}, () => {});
+    }
+
+    function checkServerCalled(numAdUnits, numBids) {
+      sinon.assert.calledOnce(prebidServerAdapterMock.callBids);
+      var requestObj = prebidServerAdapterMock.callBids.firstCall.args[0];
+      expect(requestObj.ad_units.length).to.equal(numAdUnits);
+      for (let i = 0; i < numAdUnits; i++) {
+        expect(requestObj.ad_units[i].bids.filter((bid) => {
+          return bid.bidder === 'appnexus' || bid.bidder === 'adequant';
+        }).length).to.equal(numBids);
+      }
+    }
+
+    function checkClientCalled(adapter, numBids) {
+      sinon.assert.calledOnce(adapter.callBids);
+      expect(adapter.callBids.firstCall.args[0].bids.length).to.equal(numBids);
+    }
+
+    var TESTING_CONFIG;
+    var stubGetSourceBidderMap;
+
+    beforeEach(() => {
+      TESTING_CONFIG = Object.assign(CONFIG, {
+        bidders: ['appnexus', 'adequant'],
+        testing: true
+      });
+
+      config.setConfig({s2sConfig: TESTING_CONFIG});
+      AdapterManager.bidderRegistry['prebidServer'] = prebidServerAdapterMock;
+      AdapterManager.bidderRegistry['adequant'] = adequantAdapterMock;
+      AdapterManager.bidderRegistry['appnexus'] = appnexusAdapterMock;
+
+      stubGetSourceBidderMap = sinon.stub(s2sTesting, 'getSourceBidderMap');
+
+      prebidServerAdapterMock.callBids.reset();
+      adequantAdapterMock.callBids.reset();
+      appnexusAdapterMock.callBids.reset();
+    });
+
+    afterEach(() => {
+      s2sTesting.getSourceBidderMap.restore();
+    });
+
+    it('calls server adapter if no sources defined', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: [], [s2sTesting.SERVER]: []});
+      callBids();
+
+      // server adapter
+      checkServerCalled(2, 2);
+
+      // appnexus
+      sinon.assert.notCalled(appnexusAdapterMock.callBids);
+
+      // adequant
+      sinon.assert.notCalled(adequantAdapterMock.callBids);
+    });
+
+    it('calls client adapter if one client source defined', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus'], [s2sTesting.SERVER]: []});
+      callBids();
+
+      // server adapter
+      checkServerCalled(2, 2);
+
+      // appnexus
+      checkClientCalled(appnexusAdapterMock, 2);
+
+      // adequant
+      sinon.assert.notCalled(adequantAdapterMock.callBids);
+    });
+
+    it('calls client adapters if client sources defined', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus', 'adequant'], [s2sTesting.SERVER]: []});
+      callBids();
+
+      // server adapter
+      checkServerCalled(2, 2);
+
+      // appnexus
+      checkClientCalled(appnexusAdapterMock, 2);
+
+      // adequant
+      checkClientCalled(adequantAdapterMock, 2);
+    });
+
+    it('calls client adapters if client sources defined', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus', 'adequant'], [s2sTesting.SERVER]: []});
+      callBids();
+
+      // server adapter
+      checkServerCalled(2, 2);
+
+      // appnexus
+      checkClientCalled(appnexusAdapterMock, 2);
+
+      // adequant
+      checkClientCalled(adequantAdapterMock, 2);
+    });
+
+    it('does not call server adapter for bidders that go to client', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus', 'adequant'], [s2sTesting.SERVER]: []});
+      var adUnits = getTestAdUnits();
+      adUnits[0].bids[0].finalSource = s2sTesting.CLIENT;
+      adUnits[0].bids[1].finalSource = s2sTesting.CLIENT;
+      adUnits[1].bids[0].finalSource = s2sTesting.CLIENT;
+      adUnits[1].bids[1].finalSource = s2sTesting.CLIENT;
+      callBids(adUnits);
+
+      // server adapter
+      sinon.assert.notCalled(prebidServerAdapterMock.callBids);
+
+      // appnexus
+      checkClientCalled(appnexusAdapterMock, 2);
+
+      // adequant
+      checkClientCalled(adequantAdapterMock, 2);
+    });
+
+    it('does not call client adapters for bidders that go to server', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus', 'adequant'], [s2sTesting.SERVER]: []});
+      var adUnits = getTestAdUnits();
+      adUnits[0].bids[0].finalSource = s2sTesting.SERVER;
+      adUnits[0].bids[1].finalSource = s2sTesting.SERVER;
+      adUnits[1].bids[0].finalSource = s2sTesting.SERVER;
+      adUnits[1].bids[1].finalSource = s2sTesting.SERVER;
+      callBids(adUnits);
+
+      // server adapter
+      checkServerCalled(2, 2);
+
+      // appnexus
+      sinon.assert.notCalled(appnexusAdapterMock.callBids);
+
+      // adequant
+      sinon.assert.notCalled(adequantAdapterMock.callBids);
+    });
+
+    it('calls client and server adapters for bidders that go to both', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus', 'adequant'], [s2sTesting.SERVER]: []});
+      var adUnits = getTestAdUnits();
+      adUnits[0].bids[0].finalSource = s2sTesting.BOTH;
+      adUnits[0].bids[1].finalSource = s2sTesting.BOTH;
+      adUnits[1].bids[0].finalSource = s2sTesting.BOTH;
+      adUnits[1].bids[1].finalSource = s2sTesting.BOTH;
+      callBids(adUnits);
+
+      // server adapter
+      checkServerCalled(2, 2);
+
+      // appnexus
+      checkClientCalled(appnexusAdapterMock, 2);
+
+      // adequant
+      checkClientCalled(adequantAdapterMock, 2);
+    });
+
+    it('makes mixed client/server adapter calls for mixed bidder sources', () => {
+      stubGetSourceBidderMap.returns({[s2sTesting.CLIENT]: ['appnexus', 'adequant'], [s2sTesting.SERVER]: []});
+      var adUnits = getTestAdUnits();
+      adUnits[0].bids[0].finalSource = s2sTesting.CLIENT;
+      adUnits[0].bids[1].finalSource = s2sTesting.CLIENT;
+      adUnits[1].bids[0].finalSource = s2sTesting.SERVER;
+      adUnits[1].bids[1].finalSource = s2sTesting.SERVER;
+      callBids(adUnits);
+
+      // server adapter
+      checkServerCalled(1, 2);
+
+      // appnexus
+      checkClientCalled(appnexusAdapterMock, 1);
+
+      // adequant
+      checkClientCalled(adequantAdapterMock, 1);
+    });
+  });
+
   describe('aliasBidderAdaptor', function() {
     const CODE = 'sampleBidder';
 
