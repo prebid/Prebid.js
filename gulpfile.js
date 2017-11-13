1460293c6860a224b14ead1a0dc0ddeed4b2b0d2
Jaimin Panchal <jpanchal@appnexus.com>
2017-11-13 18:15:06 -0500

    postbid bugfix for passbackTag

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ var gulpif = require('gulp-if');
 var sourcemaps = require('gulp-sourcemaps');
 var through = require('through2');
 var fs = require('fs');
+var jsEscape = require('gulp-js-escape');
 
 var prebid = require('./package.json');
 var dateString = 'Updated : ' + (new Date()).toISOString().substring(0, 10);
@@ -296,10 +297,18 @@ gulp.task('e2etest-report', function() {
   }, 5000);
 });
 
-gulp.task('build-postbid', function() {
+gulp.task('build-postbid', ['escape-postbid-config'], function() {
+  var fileContent = fs.readFileSync('./build/postbid/postbid-config.js', 'utf8');
+
   return gulp.src('./integrationExamples/postbid/oas/postbid.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('build/dist'));
+    .pipe(replace('\[%%postbid%%\]', fileContent))
+    .pipe(gulp.dest('build/postbid/'));
+});
+
+gulp.task('escape-postbid-config', function() {
+  gulp.src('./integrationExamples/postbid/oas/postbid-config.js')
+    .pipe(jsEscape())
+    .pipe(gulp.dest('build/postbid/'));
 });
 
 module.exports = nodeBundle;
