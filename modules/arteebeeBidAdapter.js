2f875ce280ed9e0e4c8923b868a9700bf1a45422
Jeff Ye <jeffyecn@gmail.com>
2017-11-16 16:18:43 -0500

    basic structure

--- modules/arteebeeBidAdapter.js
+++ modules/arteebeeBidAdapter.js
@@ -1,8 +1,14 @@
-
+import * as utils from 'src/utils';
 import { BANNER } from 'src/mediaTypes';
 import { registerBidder } from 'src/adapters/bidderFactory';
+import { config } from 'src/config';
 
 const BIDDER_CODE = 'arteebee';
+
+const DEFAULT_HOST = 'bidder.mamrtb.com';
+
+const DEFAULT_SSP = 'arteebee';
+
 export const spec = {
     code: BIDDER_CODE,
     supportedMediaTypes: [BANNER],
@@ -11,16 +17,19 @@ export const spec = {
         && bidRequest.params.source !== undefined;
     },
     buildRequests: function(validBidRequests) {
-        const payload = {};
-        const payloadString = JSON.stringify(payload);
-        return {
-            method: 'POST',
-            url: 'https://bidder.mamrtb.com/dsdds',
-            data: payloadString
-        };
+        var requests = [];
+
+        for(let i=0; i<validBidRequests.length; i++) {
+            let prebidReq = makePrebidRequest(validBidRequests[i]);
+            if ( prebidReq ) {
+                requests.push(prebidReq);
+            }
+        }
+
+        return requests;
     },
     interpretResponse: function(serverResponse, bidRequest) {
-        const bidResponses = [];
+        let bidResponses = [];
         const bidResponse = {
             requestId: bidRequest.bidId,
             cpm: 1,
@@ -42,3 +51,100 @@ export const spec = {
 }
 
 registerBidder(spec);
+
+function makePrebidRequest(req) {
+    var host = req.params.host || DEFAULT_HOST;
+    var ssp = req.params.ssp || DEFAULT_SSP;
+
+    var url = window.location.protocol + "//" + host + "/rtb/bid/" + ssp + "?type=json&register=0";
+
+    const payload = makeRtbRequest(req);
+    const payloadString = JSON.stringify(payload);
+
+    return {
+        method: 'POST',
+        url: url,
+        data: payloadString
+    };
+}
+
+function makeRtbRequest(req) {
+    var auctionId = req.requestId;
+
+    var imp = [];
+    imp.push(makeImp(req));
+
+    var rtbReq =  {
+        'id': auctionId,
+        'imp': imp,
+        'site': makeSite(req),
+        'at': 1,
+        'tmax': config.getConfig("bidderTimeout")
+    };
+
+    if ( req.params.coppa ) {
+        reqReq.regs = {coppa:1};
+    }
+
+    return rtbReq;
+}
+
+function makeImp(req) {
+    var imp = {
+        'id': req.bidId,
+        'tagid': req.placementCode
+    };
+
+    if (utils.getTopWindowLocation().protocol === 'https:') {
+        imp.secure = 1;
+    }
+
+    imp.banner = makeBanner(req);
+
+    return imp;
+}
+
+function makeBanner(req) {
+    var format = [];
+
+    for (let i=0; i<req.sizes.length; i++) {
+        format.push({
+            w: req.sizes[i][0],
+            h: req.sizes[i][1]
+        });
+    }
+    return {
+        'format':format
+    };
+}
+
+function makeSite(req) {
+    var params = req.params;
+
+    var site = {
+        "id" : params.source,
+        "page": utils.getTopWindowUrl(),
+        'ref': utils.getTopWindowReferrer(),
+        "publisher": makePublisher(req)
+    };
+
+    return site;
+}
+
+function makePublisher(req) {
+    var params = req.params;
+
+    var publisher = {
+        "id": params.pub,
+        "domain": getDomain(config.getConfig("publisherDomain"))
+    };
+
+    return publisher;
+}
+
+function getDomain(url) {
+    var a = document.createElement('a');
+    a.href = url;
+
+    return a.host;
+}
