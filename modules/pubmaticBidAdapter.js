23d990d170d8ca1b07c46d3063ddf967eb386300
PubMatic-OpenWrap <UOEDev@pubmatic.com>
2018-01-05 10:43:18 -0800

    changed auction type to 1

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -6,7 +6,7 @@ const BIDDER_CODE = 'pubmatic';
 const ENDPOINT = '//hbopenbid.pubmatic.com/translator?source=prebid-client';
 const USYNCURL = '//ads.pubmatic.com/AdServer/js/showad.js#PIX&kdntuid=1&p=';
 const CURRENCY = 'USD';
-const AUCTION_TYPE = 2;
+const AUCTION_TYPE = 1;
 const UNDEFINED = undefined;
 const CUSTOM_PARAMS = {
   'kadpageurl': '', // Custom page url
