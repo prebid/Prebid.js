b5116ce38d8c65084baba02c211478f8b171bcf1
John Salis <johnsalis@beachfrontmedia.com>
2018-02-08 10:09:27 -0500

    add os version to bid request

--- modules/beachfrontBidAdapter.js
+++ modules/beachfrontBidAdapter.js
@@ -103,6 +103,27 @@ function getFirstSize(bid) {
   return sizes.length ? sizes[0] : { w: undefined, h: undefined };
 }
 
+function getOsVersion() {
+  let clientStrings = [
+    { s: 'Android', r: /Android/ },
+    { s: 'iOS', r: /(iPhone|iPad|iPod)/ },
+    { s: 'Mac OS X', r: /Mac OS X/ },
+    { s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+    { s: 'Linux', r: /(Linux|X11)/ },
+    { s: 'Windows 10', r: /(Windows 10.0|Windows NT 10.0)/ },
+    { s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/ },
+    { s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/ },
+    { s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/ },
+    { s: 'Windows Vista', r: /Windows NT 6.0/ },
+    { s: 'Windows Server 2003', r: /Windows NT 5.2/ },
+    { s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/ },
+    { s: 'UNIX', r: /UNIX/ },
+    { s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/ }
+  ];
+  let cs = find(clientStrings, cs => cs.r.test(navigator.userAgent));
+  return cs ? cs.s : 'unknown';
+}
+
 function isMobile() {
   return (/(ios|ipod|ipad|iphone|android)/i).test(navigator.userAgent);
 }
@@ -164,6 +185,7 @@ function createBannerRequestData(bids) {
     search: topLocation.search,
     referrer: referrer,
     ua: navigator.userAgent,
+    deviceOs: getOsVersion(),
     isMobile: isMobile() ? 1 : 0,
     dnt: getDoNotTrack() ? 1 : 0,
     adapterVersion: ADAPTER_VERSION,
