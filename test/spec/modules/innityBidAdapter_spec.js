e4d8ff501902cc4c1e961f868c1f0baf55b7c22e
Eng Tat <engtat@innity.com>
2018-02-22 14:04:50 +0800

    Fix Innity Adapter Test Spec Error (#2180)

--- test/spec/modules/innityBidAdapter_spec.js
+++ test/spec/modules/innityBidAdapter_spec.js
@@ -7,8 +7,8 @@ describe('innityAdapterTest', () => {
       expect(spec.isBidRequestValid({
         bidder: 'innity',
         params: {
-          'pub': '267',
-          'zone': '62546'
+          'pub': 267,
+          'zone': 62546
         },
       })).to.equal(true);
     });
@@ -26,8 +26,8 @@ describe('innityAdapterTest', () => {
     const bidRequests = [{
       'bidder': 'innity',
       'params': {
-        'pub': '267',
-        'zone': '62546'
+        'pub': 267,
+        'zone': 62546
       },
       'adUnitCode': 'div-gpt-ad-1460505748561-0',
       'transactionId': 'd7b773de-ceaa-484d-89ca-d9f51b8d61ec',
@@ -46,8 +46,8 @@ describe('innityAdapterTest', () => {
 
     it('bidRequest data', () => {
       const requests = spec.buildRequests(bidRequests);
-      expect(requests[0].data.pub).to.equal('267');
-      expect(requests[0].data.zone).to.equal('62546');
+      expect(requests[0].data.pub).to.equal(267);
+      expect(requests[0].data.zone).to.equal(62546);
       expect(requests[0].data.width).to.equal('300');
       expect(requests[0].data.height).to.equal('250');
       expect(requests[0].data.callback_uid).to.equal('51ef8751f9aead');
@@ -88,13 +88,13 @@ describe('innityAdapterTest', () => {
     it('result is correct', () => {
       const result = spec.interpretResponse(bidResponse, bidRequest);
       expect(result[0].requestId).to.equal('51ef8751f9aead');
-      expect(result[0].cpm).to.equal(100 / 100);
-      expect(result[0].width).to.equal(300);
-      expect(result[0].height).to.equal(250);
+      expect(result[0].cpm).to.equal(1);
+      expect(result[0].width).to.equal('300');
+      expect(result[0].height).to.equal('250');
       expect(result[0].creativeId).to.equal('148186');
       expect(result[0].currency).to.equal('USD');
       expect(result[0].ttl).to.equal(60);
-      expect(result[0].ad).to.equal('<script>innity=true;</script>');
+      expect(result[0].ad).to.equal('<script src="http://cdn.innity.net/frame_util.js"></script><script>innity=true;</script>');
     });
   });
 });
