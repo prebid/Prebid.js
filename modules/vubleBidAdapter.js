c0765533e5505943e634eb81e356ed397da2d205
Geoffray Veyssié <gv@mediabong.com>
2018-02-27 18:17:37 +0100

    Add: vuble bid adapter

---
+++ modules/vubleBidAdapter.js
@@ -0,0 +1,134 @@
+// Vuble Adapter
+
+import * as utils from 'src/utils';
+import {registerBidder} from 'src/adapters/bidderFactory';
+
+const BIDDER_CODE = 'vuble';
+
+const ENVS = ['com', 'net'];
+const CURRENCIES = {
+  com: 'EUR',
+  net: 'USD'
+};
+const TTL = 60;
+
+export const spec = {
+  code: BIDDER_CODE,
+  supportedMediaTypes: ['video'],
+
+  /**
+   * Determines whether or not the given bid request is valid.
+   *
+   * @param {BidRequest} bid The bid params to validate.
+   * @return boolean True if this is a valid bid, and false otherwise.
+   */
+  isBidRequestValid: function (bid) {
+    if (utils.isEmpty(bid.sizes) || utils.parseSizesInput(bid.sizes).length == 0) {
+      return false;
+    }
+
+    if (bid.mediaType !== 'video') {
+      return false;
+    }
+
+    if (!utils.contains(ENVS, bid.params.env)) {
+      return false;
+    }
+
+    return !!(bid.params.env && bid.params.pubId && bid.params.zoneId);
+  },
+
+  /**
+   * Make a server request from the list of BidRequests.
+   *
+   * @param {validBidRequests[]} - an array of bids
+   * @return ServerRequest Info describing the request to the server.
+   */
+  buildRequests: function (validBidRequests) {
+    return validBidRequests.map(bid => {
+      // We take the first size
+      let size = utils.parseSizesInput(bid.sizes)[0].split('x');
+
+      // Get the page's url
+      let referrer = utils.getTopWindowUrl();
+      if (bid.params.referrer) {
+        referrer = bid.params.referrer;
+      }
+
+      // Get Video Context
+      let context = utils.deepAccess(bid, 'mediaTypes.video.context');
+
+      let url = '//player.mediabong.' + bid.params.env + '/prebid/request';
+      let data = {
+        width: size[0],
+        height: size[1],
+        pub_id: bid.params.pubId,
+        zone_id: bid.params.zoneId,
+        context: context,
+        floor_price: bid.params.floorPrice ? bid.params.floorPrice : 0,
+        url: referrer
+      };
+
+      return {
+        method: 'POST',
+        url: url,
+        data: data
+      };
+    });
+  },
+
+  /**
+   * Unpack the response from the server into a list of bids.
+   *
+   * @param {ServerResponse} serverResponse A successful response from the server.
+   * @return {Bid[]} An array of bids which were nested inside the server.
+   */
+  interpretResponse: function (serverResponse, bid) {
+    const size = utils.parseSizesInput(bid.sizes)[0].split('x');
+    const responseBody = serverResponse.body;
+
+    if (typeof responseBody !== 'object' || responseBody.status !== 'ok') {
+      return [];
+    }
+
+    let responses = [];
+    let reponse = {
+      requestId: bid.bidId,
+      cpm: responseBody.cpm,
+      width: size[0],
+      height: size[1],
+      ttl: TTL,
+      creativeId: responseBody.creativeId,
+      netRevenue: true,
+      currency: CURRENCIES[bid.params.env],
+      vastUrl: responseBody.url
+    };
+    responses.push(reponse);
+
+    return responses;
+  },
+
+  /**
+   * Register the user sync pixels which should be dropped after the auction.
+   *
+   * @param {SyncOptions} syncOptions Which user syncs are allowed?
+   * @param {ServerResponse[]} serverResponses List of server's responses.
+   * @return {UserSync[]} The user syncs which should be dropped.
+   */
+  getUserSyncs: function (syncOptions, serverResponses) {
+    // TODO
+    if (syncOptions.iframeEnabled) {
+      if (serverResponses.length > 0) {
+        let responseBody = serverResponses[0].body;
+        if (typeof responseBody !== 'object' || responseBody.iframeSync) {
+          return [{
+            type: 'iframe',
+            url: responseBody.iframeSync
+          }];
+        }
+      }
+    }
+  }
+};
+
+registerBidder(spec);
