221923f6ffad09e9334b2bad573c985643c7500e
Philip Watson <philip.watson@adscale.co.nz>
2018-03-05 12:26:56 +1300

    Remove debug logging, remove unused functions, remove vars

--- modules/stroeerCoreBidAdapter.js
+++ modules/stroeerCoreBidAdapter.js
@@ -261,43 +261,20 @@ Crypter.prototype.encrypt = function (anyRandomString, data) {
 
   let encryptionPad = str_hmac_sha1(this.encKey, paddedImpressionId);
 
-  console.log("BEGIN PAD");
-  for (let i = 0; i < encryptionPad.length; i++) {
-    console.log(encryptionPad.charCodeAt(i));
-  }
-  console.log("END PAD");
-
   let encryptedPrice = "";
 
-  console.log("BEGIN XOR");
   for (let i = 0; i < CIPHERTEXT_SIZE; i++) {
     let priceCharCode = (i >= data.length) ? '\x00' : data.charCodeAt(i);
-
-    console.log(`${priceCharCode} ^ ${encryptionPad.charCodeAt(i)} = ${priceCharCode ^ convertSignedByte(encryptionPad.charCodeAt(i))}`);
     encryptedPrice = encryptedPrice + String.fromCharCode(0xff & (priceCharCode ^ convertSignedByte(encryptionPad.charCodeAt(i))));
   }
-  console.log("END XOR");
-
-
-  console.log("BEGIN ENCRYPTED PRICE");
-  for (let i = 0; i < encryptedPrice.length; i++) {
-    console.log(encryptedPrice.charCodeAt(i));
-  }
-  console.log("END ENCRYPTED PRICE");
-
 
   // Integrity
 
   const dataArray = new ArrayOfCharCodes(data, CIPHERTEXT_SIZE, 0);
+
   dataArray.addString(paddedImpressionId);
 
   const signature = str_hmac_sha1(this.intKey, dataArray).substring(0, SIGNATURE_SIZE);
-
-  console.log("BEGIN SIGNATURE");
-  for (let i = 0; i < signature.length; i++) {
-    console.log(signature.charCodeAt(i));
-  }
-  console.log("END SIGNATURE");
 
   return base64EncodeUrlFriendly(paddedImpressionId + encryptedPrice + signature);
 };
@@ -325,7 +302,6 @@ function ArrayOfCharCodes(str, length, charCodePadding) {
   let remainder = length - str.length;
   this.charCodes = [];
   for (let i = 0; i < str.length; i++) {
-    console.log("pushing " + str.charCodeAt(i));
     this.charCodes.push(str.charCodeAt(i));
   }
   for (let i = 0; i < remainder; i++) {
@@ -354,42 +330,18 @@ ArrayOfCharCodes.prototype.addString = function(str) {
 };
 
 
-// http://pajhome.org.uk/crypt/md5/sha1.js
-/*
- * A JavaScript implementation of the Secure Hash Algorithm, SHA-1, as defined
- * in FIPS PUB 180-1
- * Version 2.1a Copyright Paul Johnston 2000 - 2002.
- * Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet
- * Distributed under the BSD License
- * See http://pajhome.org.uk/crypt/md5 for details.
- */
-
+// Code taken from http://pajhome.org.uk/crypt/md5/sha1.js
 /*
  * Configurable variables. You may need to tweak these to be compatible with
  * the server-side, but the defaults work in most cases.
  */
-var hexcase = 0;  /* hex output format. 0 - lowercase; 1 - uppercase        */
-var b64pad  = ""; /* base-64 pad character. "=" for strict RFC compliance   */
-var chrsz   = 8;  /* bits per input character. 8 - ASCII; 16 - Unicode      */
+const chrsz   = 8;  /* bits per input character. 8 - ASCII; 16 - Unicode      */
 
 /*
  * These are the functions you'll usually want to call
  * They take string arguments and return either hex or base-64 encoded strings
  */
-function hex_sha1(s){return binb2hex(core_sha1(str2binb(s),s.length * chrsz));}
-function b64_sha1(s){return binb2b64(core_sha1(str2binb(s),s.length * chrsz));}
-function str_sha1(s){return binb2str(core_sha1(str2binb(s),s.length * chrsz));}
-function hex_hmac_sha1(key, data){ return binb2hex(core_hmac_sha1(key, data));}
-function b64_hmac_sha1(key, data){ return binb2b64(core_hmac_sha1(key, data));}
 function str_hmac_sha1(key, data){ return binb2str(core_hmac_sha1(key, data));}
-
-/*
- * Perform a simple self-test to see if the VM is working
- */
-function sha1_vm_test()
-{
-  return hex_sha1("abc") == "a9993e364706816aba3e25717850c26c9cd0d89d";
-}
 
 /*
  * Calculate the SHA-1 of an array of big-endian words, and a bit length
@@ -400,26 +352,26 @@ function core_sha1(x, len)
   x[len >> 5] |= 0x80 << (24 - len % 32);
   x[((len + 64 >> 9) << 4) + 15] = len;
 
-  var w = Array(80);
-  var a =  1732584193;
-  var b = -271733879;
-  var c = -1732584194;
-  var d =  271733878;
-  var e = -1009589776;
+  let w = Array(80);
+  let a =  1732584193;
+  let b = -271733879;
+  let c = -1732584194;
+  let d =  271733878;
+  let e = -1009589776;
 
-  for(var i = 0; i < x.length; i += 16)
+  for(let i = 0; i < x.length; i += 16)
   {
-    var olda = a;
-    var oldb = b;
-    var oldc = c;
-    var oldd = d;
-    var olde = e;
+    const olda = a;
+    const oldb = b;
+    const oldc = c;
+    const oldd = d;
+    const olde = e;
 
-    for(var j = 0; j < 80; j++)
+    for(let j = 0; j < 80; j++)
     {
       if(j < 16) w[j] = x[i + j];
       else w[j] = rol(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
-      var t = safe_add(safe_add(rol(a, 5), sha1_ft(j, b, c, d)),
+      const t = safe_add(safe_add(rol(a, 5), sha1_ft(j, b, c, d)),
         safe_add(safe_add(e, w[j]), sha1_kt(j)));
       e = d;
       d = c;
@@ -464,17 +416,17 @@ function sha1_kt(t)
  */
 function core_hmac_sha1(key, data)
 {
-  var bkey = str2binb(key);
+  let bkey = str2binb(key);
   if(bkey.length > 16) bkey = core_sha1(bkey, key.length * chrsz);
 
-  var ipad = Array(16), opad = Array(16);
-  for(var i = 0; i < 16; i++)
+  const ipad = Array(16), opad = Array(16);
+  for(let i = 0; i < 16; i++)
   {
     ipad[i] = bkey[i] ^ 0x36363636;
     opad[i] = bkey[i] ^ 0x5C5C5C5C;
   }
 
-  var hash = core_sha1(ipad.concat(str2binb(data)), 512 + data.length * chrsz);
+  const hash = core_sha1(ipad.concat(str2binb(data)), 512 + data.length * chrsz);
   return core_sha1(opad.concat(hash), 512 + 160);
 }
 
@@ -484,8 +436,8 @@ function core_hmac_sha1(key, data)
  */
 function safe_add(x, y)
 {
-  var lsw = (x & 0xFFFF) + (y & 0xFFFF);
-  var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+  const lsw = (x & 0xFFFF) + (y & 0xFFFF);
+  const msw = (x >> 16) + (y >> 16) + (lsw >> 16);
   return (msw << 16) | (lsw & 0xFFFF);
 }
 
@@ -503,9 +455,9 @@ function rol(num, cnt)
  */
 function str2binb(str)
 {
-  var bin = Array();
-  var mask = (1 << chrsz) - 1;
-  for(var i = 0; i < str.length * chrsz; i += chrsz)
+  const bin = Array();
+  const mask = (1 << chrsz) - 1;
+  for(let i = 0; i < str.length * chrsz; i += chrsz)
     bin[i>>5] |= (str.charCodeAt(i / chrsz) & mask) << (32 - chrsz - i%32);
   return bin;
 }
@@ -515,45 +467,9 @@ function str2binb(str)
  */
 function binb2str(bin)
 {
-  var str = "";
-  var mask = (1 << chrsz) - 1;
-  for(var i = 0; i < bin.length * 32; i += chrsz)
+  let str = "";
+  const mask = (1 << chrsz) - 1;
+  for(let i = 0; i < bin.length * 32; i += chrsz)
     str += String.fromCharCode((bin[i>>5] >>> (32 - chrsz - i%32)) & mask);
-  return str;
-}
-
-/*
- * Convert an array of big-endian words to a hex string.
- */
-function binb2hex(binarray)
-{
-  var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
-  var str = "";
-  for(var i = 0; i < binarray.length * 4; i++)
-  {
-    str += hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8+4)) & 0xF) +
-      hex_tab.charAt((binarray[i>>2] >> ((3 - i%4)*8  )) & 0xF);
-  }
-  return str;
-}
-
-/*
- * Convert an array of big-endian words to a base-64 string
- */
-function binb2b64(binarray)
-{
-  var tab = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  var str = "";
-  for(var i = 0; i < binarray.length * 4; i += 3)
-  {
-    var triplet = (((binarray[i   >> 2] >> 8 * (3 -  i   %4)) & 0xFF) << 16)
-      | (((binarray[i+1 >> 2] >> 8 * (3 - (i+1)%4)) & 0xFF) << 8 )
-      |  ((binarray[i+2 >> 2] >> 8 * (3 - (i+2)%4)) & 0xFF);
-    for(var j = 0; j < 4; j++)
-    {
-      if(i * 8 + j * 6 > binarray.length * 32) str += b64pad;
-      else str += tab.charAt((triplet >> 6*(3-j)) & 0x3F);
-    }
-  }
   return str;
 }
