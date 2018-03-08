b05b4887f1d921d38f0d718920b1ba0fec8bda7d
Philip Watson <philip.watson@adscale.co.nz>
2018-03-08 17:35:00 +1300

    Start tests for "safe" price truncation to fit 8 bytes (or error)

--- test/spec/modules/stroeerCoreBidAdapter_spec.js
+++ test/spec/modules/stroeerCoreBidAdapter_spec.js
@@ -283,7 +283,7 @@ describe('stroeerssp adapter', function () {
     });
 
     describe('Auction type (ssat) set (or not) in each individual bid', function() {
-      it.only('test an auction with two valid and two non-valid auction types', function() {
+      it('test an auction with two valid and two non-valid auction types', function() {
         clock.tick(13500);
 
         const bidderRequestTwoValidOneInvalid = ({
@@ -874,6 +874,66 @@ describe('stroeerssp adapter', function () {
       });
     });
 
+    describe("price truncation", function() {
+      const d = new Decrpyter("c2xzRWh5NXhpZmxndTRxYWZjY2NqZGNhTW1uZGZya3Y=");
+      const validPrices = [
+        { price: '1.5700000', expectation: '1.570000'},
+        { price: '12345678', expectation: '12345678'},
+        { price: '1234.56789', expectation: '1234.567'},
+        { price: '12345.1234', expectation: '12345.12'},
+        { price: '123456.10', expectation: '123456.1'},
+        { price: '123456.105', expectation: '123456.1'},
+        { price: '1234567.0052', expectation: '1234567'},
+      ];
+      validPrices.forEach(test => {
+        it(`should safely truncate ${test.price} to ${test.expectation}`, function() {
+          const bidderResponse = buildBidderResponse();
+
+          const responseBid = bidderResponse.bids[0];
+          responseBid.ad = '<img src=\'tracker.com?p=${AUCTION_PRICE:ENC}></img>';
+
+          fakeServer.respondWith(JSON.stringify(bidderResponse));
+
+          adapter(win).callBids(bidderRequest);
+
+          fakeServer.respond();
+
+          const bid = bidmanager.addBidResponse.firstCall.args[1];
+          const ad = bid.generateAd({auctionPrice: test.price});
+
+          const rx = /<img src='tracker.com\?p=(.*)><\/img>/g;
+          const encryptedPrice = rx.exec(ad);
+          assert.equal(d.decrypt(encryptedPrice[1]), test.expectation);
+        });
+      });
+
+      const invalidPrices = [
+        { price: '123456789'},
+        { price: '123456.15'},
+        { price: '1234567.0152'},
+        { price: '1234567.1052'},
+      ];
+      invalidPrices.forEach(test => {
+        it(`should error when price is ${test.price}`, function () {
+          const bidderResponse = buildBidderResponse();
+
+          const responseBid = bidderResponse.bids[0];
+          responseBid.ad = '<img src=\'tracker.com?p=${AUCTION_PRICE:ENC}></img>';
+
+          fakeServer.respondWith(JSON.stringify(bidderResponse));
+
+          adapter(win).callBids(bidderRequest);
+
+          fakeServer.respond();
+
+          const bid = bidmanager.addBidResponse.firstCall.args[1];
+          const fn = () => bid.generateAd({auctionPrice: test.price});
+
+          assert.throws(fn, Error);
+        });
+      });
+    });
+
     function assertConnectJs(actualElement, expectedUrl, expectedSlotId) {
       assert.strictEqual(actualElement.tagName, 'SCRIPT');
       assert.strictEqual(actualElement.src, expectedUrl);
@@ -897,3 +957,194 @@ describe('stroeerssp adapter', function () {
     }
   });
 });
+
+
+
+function Decrpyter(encKey) {
+  this.encKey = atob(encKey);
+}
+
+function unwebSafeBase64EncodedString(str) {
+  let pad = "";
+  if (str.length % 4 === 2) {
+    pad += "==";
+  }
+  else if (str.length % 4 === 1) {
+    pad += "=";
+  }
+
+  str = str.replace(/-/g, '+')
+    .replace(/_/g, '/');
+
+  return str + pad;
+}
+
+Decrpyter.prototype.decrypt = function(str) {
+  const unencodedStr = atob(unwebSafeBase64EncodedString(str));
+  const CIPHERTEXT_SIZE = 8;
+
+  const initVector = unencodedStr.substring(0, 16);
+  const cipherText = unencodedStr.substring(16, 16 + CIPHERTEXT_SIZE);
+  // const signature = unencodedStr.substring(16 + CIPHERTEXT_SIZE);
+
+  const pad = str_hmac_sha1(this.encKey, initVector);
+
+  let unencryptedPrice = '';
+
+  for (let i = 0; i < CIPHERTEXT_SIZE; i++) {
+    let priceCharCode = cipherText.charCodeAt(i);
+    const charCode = 0xff & (priceCharCode ^ convertSignedByte(pad.charCodeAt(i)));
+    if (charCode === 0) {
+      break;
+    }
+    unencryptedPrice = unencryptedPrice + String.fromCharCode(charCode);
+  }
+
+  // ignore integrity
+
+  return unencryptedPrice;
+};
+
+function convertSignedByte(value) {
+  if (value >= 128) {
+    return value - 256;
+  } else {
+    return value;
+  }
+}
+
+
+// Code taken from http://pajhome.org.uk/crypt/md5/sha1.js
+/*
+ * Configurable variables. You may need to tweak these to be compatible with
+ * the server-side, but the defaults work in most cases.
+ */
+const chrsz = 8; // bits per input character. 8 - ASCII; 16 - Unicode
+
+/*
+ * These are the functions you'll usually want to call
+ * They take string arguments and return either hex or base-64 encoded strings
+ */
+function str_hmac_sha1(key, data) { return binb2str(core_hmac_sha1(key, data)); }
+
+/*
+ * Calculate the SHA-1 of an array of big-endian words, and a bit length
+ */
+function core_sha1(x, len) {
+  /* append padding */
+  x[len >> 5] |= 0x80 << (24 - len % 32);
+  x[((len + 64 >> 9) << 4) + 15] = len;
+
+  let w = Array(80);
+  let a = 1732584193;
+  let b = -271733879;
+  let c = -1732584194;
+  let d = 271733878;
+  let e = -1009589776;
+
+  for (let i = 0; i < x.length; i += 16) {
+    const olda = a;
+    const oldb = b;
+    const oldc = c;
+    const oldd = d;
+    const olde = e;
+
+    for (let j = 0; j < 80; j++) {
+      if (j < 16) w[j] = x[i + j];
+      else w[j] = rol(w[j - 3] ^ w[j - 8] ^ w[j - 14] ^ w[j - 16], 1);
+      const t = safe_add(safe_add(rol(a, 5), sha1_ft(j, b, c, d)),
+        safe_add(safe_add(e, w[j]), sha1_kt(j)));
+      e = d;
+      d = c;
+      c = rol(b, 30);
+      b = a;
+      a = t;
+    }
+
+    a = safe_add(a, olda);
+    b = safe_add(b, oldb);
+    c = safe_add(c, oldc);
+    d = safe_add(d, oldd);
+    e = safe_add(e, olde);
+  }
+  return [a, b, c, d, e]; // Was Array(a, b, c, d, e)
+}
+
+/*
+ * Perform the appropriate triplet combination function for the current
+ * iteration
+ */
+function sha1_ft(t, b, c, d) {
+  if (t < 20) return (b & c) | ((~b) & d);
+  if (t < 40) return b ^ c ^ d;
+  if (t < 60) return (b & c) | (b & d) | (c & d);
+  return b ^ c ^ d;
+}
+
+/*
+ * Determine the appropriate additive constant for the current iteration
+ */
+function sha1_kt(t) {
+  return (t < 20) ? 1518500249 : (t < 40) ? 1859775393
+    : (t < 60) ? -1894007588 : -899497514;
+}
+
+/*
+ * Calculate the HMAC-SHA1 of a key and some data
+ */
+function core_hmac_sha1(key, data) {
+  let bkey = str2binb(key);
+  if (bkey.length > 16) bkey = core_sha1(bkey, key.length * chrsz);
+
+  const ipad = Array(16);
+  const opad = Array(16);
+  for (let i = 0; i < 16; i++) {
+    ipad[i] = bkey[i] ^ 0x36363636;
+    opad[i] = bkey[i] ^ 0x5C5C5C5C;
+  }
+
+  const hash = core_sha1(ipad.concat(str2binb(data)), 512 + data.length * chrsz);
+  return core_sha1(opad.concat(hash), 512 + 160);
+}
+
+/*
+ * Add integers, wrapping at 2^32. This uses 16-bit operations internally
+ * to work around bugs in some JS interpreters.
+ */
+function safe_add(x, y) {
+  const lsw = (x & 0xFFFF) + (y & 0xFFFF);
+  const msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+  return (msw << 16) | (lsw & 0xFFFF);
+}
+
+/*
+ * Bitwise rotate a 32-bit number to the left.
+ */
+function rol(num, cnt) {
+  return (num << cnt) | (num >>> (32 - cnt));
+}
+
+/*
+ * Convert an 8-bit or 16-bit string to an array of big-endian words
+ * In 8-bit function, characters >255 have their hi-byte silently ignored.
+ */
+function str2binb(str) {
+  const bin = []; // was Array()
+  const mask = (1 << chrsz) - 1;
+  for (let i = 0; i < str.length * chrsz; i += chrsz) {
+    bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (32 - chrsz - i % 32);
+  }
+  return bin;
+}
+
+/*
+ * Convert an array of big-endian words to a string
+ */
+function binb2str(bin) {
+  let str = '';
+  const mask = (1 << chrsz) - 1;
+  for (let i = 0; i < bin.length * 32; i += chrsz) {
+    str += String.fromCharCode((bin[i >> 5] >>> (32 - chrsz - i % 32)) & mask);
+  }
+  return str;
+}
