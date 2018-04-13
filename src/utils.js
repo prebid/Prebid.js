49ee97db922e96d37b4e9f9a93522a343b508e1f
Matt Kendall <1870166+mkendall07@users.noreply.github.com>
2018-04-13 16:57:29 -0400

    fixes #2353 - not appending hb_uuid and hb_cache_id (#2363)

--- src/utils.js
+++ src/utils.js
@@ -775,6 +775,9 @@ export function groupBy(xs, key) {
  * @returns {*} The value found at the specified object path, or undefined if path is not found.
  */
 export function deepAccess(obj, path) {
+  if (!obj) {
+    return;
+  }
   path = String(path).split('.');
   for (let i = 0; i < path.length; i++) {
     obj = obj[path[i]];
