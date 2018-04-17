40699692b44008ff59d60ddbe54257f051486a70
Jason Snellbaker <jsnellbaker@appnexus.com>
2018-04-17 12:38:43 -0400

    added TODO comment to move this code to somewhere else at later date

--- modules/prebidServerBidAdapter.js
+++ modules/prebidServerBidAdapter.js
@@ -469,6 +469,7 @@ const OPEN_RTB_PROTOCOL = {
 
       // get bidder params in form { <bidder code>: {...params} }
       const ext = adUnit.bids.reduce((acc, bid) => {
+        // TODO: move this bidder specific out to a more ideal location (submodule?); issue# pending
         // convert all AppNexus keys to underscore format for pbs
         if (bid.bidder === 'appnexus') {
           Object.keys(bid.params).forEach(paramKey => {
