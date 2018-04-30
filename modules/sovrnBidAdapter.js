2bf45360b24ed30ae9bc2326b09c416c0141214c
Rachel Joyce <rjoyce@sovrn.com>
2018-04-30 11:59:30 -0600

    Send gdpr info

--- modules/sovrnBidAdapter.js
+++ modules/sovrnBidAdapter.js
@@ -21,7 +21,7 @@ export const spec = {
    * @param {BidRequest[]} bidRequests Array of Sovrn bidders
    * @return object of parameters for Prebid AJAX request
    */
-  buildRequests: function(bidReqs) {
+  buildRequests: function(bidReqs, bidderRequest) {
     const loc = utils.getTopWindowLocation();
     let sovrnImps = [];
     let iv;
@@ -34,6 +34,7 @@ export const spec = {
         bidfloor: utils.getBidIdParameter('bidfloor', bid.params)
       });
     });
+
     const sovrnBidReq = {
       id: utils.getUniqueIdentifierStr(),
       imp: sovrnImps,
@@ -43,6 +44,17 @@ export const spec = {
       }
     };
     if (iv) sovrnBidReq.iv = iv;
+
+    if (bidderRequest && bidderRequest.gdprConsent) {
+      sovrnBidReq.regs = {
+        ext: {
+          gdpr: +bidderRequest.gdprConsent.gdprApplies
+        }};
+      sovrnBidReq.user = {
+        ext: {
+          consent: bidderRequest.gdprConsent.consentString
+        }};
+    }
 
     return {
       method: 'POST',
