745620f01609e2afbff89937afa3f893b111a77c
luca <luca.confa80@gmail.com>
2018-05-14 14:07:30 +1000

    add test file clean up adapter by removing native referecnes

--- modules/playgroundxyzBidAdapter.js
+++ modules/playgroundxyzBidAdapter.js
@@ -1,37 +1,23 @@
 import { Renderer } from 'src/Renderer';
 import * as utils from 'src/utils';
 import { registerBidder } from 'src/adapters/bidderFactory';
-import { BANNER, NATIVE, VIDEO } from 'src/mediaTypes';
+import { BANNER, VIDEO } from 'src/mediaTypes';
 import find from 'core-js/library/fn/array/find';
 import includes from 'core-js/library/fn/array/includes';
 
 const BIDDER_CODE = 'playgroundxyz';
 const URL = 'https://ads.playground.xyz/host-config/prebid';
+//const URL = 'https://localhost:4430/host-config/prebid';
 
 const VIDEO_TARGETING = ['id', 'mimes', 'minduration', 'maxduration',
   'startdelay', 'skippable', 'playback_method', 'frameworks'];
 const USER_PARAMS = ['age', 'external_uid', 'segments', 'gender', 'dnt', 'language'];
-const NATIVE_MAPPING = {
-  body: 'description',
-  cta: 'ctatext',
-  image: {
-    serverName: 'main_image',
-    requiredParams: { required: true },
-    minimumParams: { sizes: [{}] },
-  },
-  icon: {
-    serverName: 'icon',
-    requiredParams: { required: true },
-    minimumParams: { sizes: [{}] },
-  },
-  sponsoredBy: 'sponsored_by',
-};
 const SOURCE = 'pbjs';
 
 export const spec = {
   code: BIDDER_CODE,
   aliases: ['playgroundxyz'],
-  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
+  supportedMediaTypes: [BANNER, VIDEO],
 
   /**
    * Determines whether or not the given bid request is valid.
@@ -39,7 +25,7 @@ export const spec = {
    * @param {object} bid The bid to validate.
    * @return boolean True if this is a valid bid, and false otherwise.
    */
-  isBidRequestValid: function(bid) {
+  isBidRequestValid: function (bid) {
     return !!(bid.params.placementId || (bid.params.member && bid.params.invCode));
   },
 
@@ -49,7 +35,7 @@ export const spec = {
    * @param {BidRequest[]} bidRequests A non-empty list of bid requests which should be sent to the Server.
    * @return ServerRequest Info describing the request to the server.
    */
-  buildRequests: function(bidRequests, bidderRequest) {
+  buildRequests: function (bidRequests, bidderRequest) {
     const tags = bidRequests.map(bidToTag);
     const userObjBid = find(bidRequests, hasUserInfo);
     let userObj;
@@ -98,7 +84,7 @@ export const spec = {
    * @param {*} serverResponse A successful response from the server.
    * @return {Bid[]} An array of bids which were nested inside the server.
    */
-  interpretResponse: function(serverResponse, {bidderRequest}) {
+  interpretResponse: function (serverResponse, { bidderRequest }) {
     serverResponse = serverResponse.body;
     const bids = [];
     if (!serverResponse || serverResponse.error) {
@@ -109,21 +95,25 @@ export const spec = {
     }
 
     if (serverResponse.tags) {
-      serverResponse.tags.forEach(serverBid => {
-        const rtbBid = getRtbBid(serverBid);
-        if (rtbBid) {
-          if (rtbBid.cpm !== 0 && includes(this.supportedMediaTypes, rtbBid.ad_type)) {
-            const bid = newBid(serverBid, rtbBid, bidderRequest);
-            bid.mediaType = parseMediaType(rtbBid);
-            bids.push(bid);
-          }
-        }
+      // sort tags by cpm
+      serverResponse.tags.sort(function (x, y) {
+        return y.cpm - x.cpm;
       });
+      // get only the first tag
+      let serverBid = serverResponse.tags[0];
+      const rtbBid = getRtbBid(serverBid);
+      if (rtbBid) {
+        if (rtbBid.cpm !== 0 && includes(this.supportedMediaTypes, rtbBid.ad_type)) {
+          const bid = newBid(serverBid, rtbBid, bidderRequest);
+          bid.mediaType = parseMediaType(rtbBid);
+          bids.push(bid);
+        }
+      }
     }
     return bids;
   },
 
-  getUserSyncs: function(syncOptions) {
+  getUserSyncs: function (syncOptions) {
     if (syncOptions.iframeEnabled) {
       return [{
         type: 'iframe',
@@ -178,7 +168,7 @@ function getKeywords(keywords) {
         return;
       } // unsuported types - don't send a key
     }
-    arrs.push({key: k, value: v});
+    arrs.push({ key: k, value: v });
   });
 
   return arrs;
@@ -227,32 +217,6 @@ function newBid(serverBid, rtbBid, bidderRequest) {
       bid.adResponse.ad = bid.adResponse.ads[0];
       bid.adResponse.ad.video = bid.adResponse.ad.rtb.video;
     }
-  } else if (rtbBid.rtb[NATIVE]) {
-    const nativeAd = rtbBid.rtb[NATIVE];
-    bid[NATIVE] = {
-      title: nativeAd.title,
-      body: nativeAd.desc,
-      cta: nativeAd.ctatext,
-      sponsoredBy: nativeAd.sponsored,
-      clickUrl: nativeAd.link.url,
-      clickTrackers: nativeAd.link.click_trackers,
-      impressionTrackers: nativeAd.impression_trackers,
-      javascriptTrackers: nativeAd.javascript_trackers,
-    };
-    if (nativeAd.main_img) {
-      bid['native'].image = {
-        url: nativeAd.main_img.url,
-        height: nativeAd.main_img.height,
-        width: nativeAd.main_img.width,
-      };
-    }
-    if (nativeAd.icon) {
-      bid['native'].icon = {
-        url: nativeAd.icon.url,
-        height: nativeAd.icon.height,
-        width: nativeAd.icon.width,
-      };
-    }
   } else {
     Object.assign(bid, {
       width: rtbBid.rtb.banner.width,
@@ -290,7 +254,7 @@ function bidToTag(bid) {
     tag.reserve = bid.params.reserve;
   }
   if (bid.params.position) {
-    tag.position = {'above': 1, 'below': 2}[bid.params.position] || 0;
+    tag.position = { 'above': 1, 'below': 2 }[bid.params.position] || 0;
   }
   if (bid.params.trafficSourceCode) {
     tag.traffic_source_code = bid.params.trafficSourceCode;
@@ -312,15 +276,6 @@ function bidToTag(bid) {
   }
   if (!utils.isEmpty(bid.params.keywords)) {
     tag.keywords = getKeywords(bid.params.keywords);
-  }
-
-  if (bid.mediaType === NATIVE || utils.deepAccess(bid, `mediaTypes.${NATIVE}`)) {
-    tag.ad_types.push(NATIVE);
-
-    if (bid.nativeParams) {
-      const nativeRequest = buildNativeRequest(bid.nativeParams);
-      tag[NATIVE] = {layouts: [nativeRequest]};
-    }
   }
 
   const videoMediaType = utils.deepAccess(bid, `mediaTypes.${VIDEO}`);
@@ -388,44 +343,6 @@ function getRtbBid(tag) {
   return tag && tag.ads && tag.ads.length && find(tag.ads, ad => ad.rtb);
 }
 
-function buildNativeRequest(params) {
-  const request = {};
-
-  // map standard prebid native asset identifier to /ut parameters
-  // e.g., tag specifies `body` but /ut only knows `description`.
-  // mapping may be in form {tag: '<server name>'} or
-  // {tag: {serverName: '<server name>', requiredParams: {...}}}
-  Object.keys(params).forEach(key => {
-    // check if one of the <server name> forms is used, otherwise
-    // a mapping wasn't specified so pass the key straight through
-    const requestKey =
-      (NATIVE_MAPPING[key] && NATIVE_MAPPING[key].serverName) ||
-      NATIVE_MAPPING[key] ||
-      key;
-
-    // required params are always passed on request
-    const requiredParams = NATIVE_MAPPING[key] && NATIVE_MAPPING[key].requiredParams;
-    request[requestKey] = Object.assign({}, requiredParams, params[key]);
-
-    // minimum params are passed if no non-required params given on adunit
-    const minimumParams = NATIVE_MAPPING[key] && NATIVE_MAPPING[key].minimumParams;
-
-    if (requiredParams && minimumParams) {
-      // subtract required keys from adunit keys
-      const adunitKeys = Object.keys(params[key]);
-      const requiredKeys = Object.keys(requiredParams);
-      const remaining = adunitKeys.filter(key => !includes(requiredKeys, key));
-
-      // if none are left over, the minimum params needs to be sent
-      if (remaining.length === 0) {
-        request[requestKey] = Object.assign({}, request[requestKey], minimumParams);
-      }
-    }
-  });
-
-  return request;
-}
-
 function outstreamRender(bid) {
   // push to render queue because ANOutstreamVideo may not be loaded yet
   bid.renderer.push(() => {
@@ -448,8 +365,6 @@ function parseMediaType(rtbBid) {
   const adType = rtbBid.ad_type;
   if (adType === VIDEO) {
     return VIDEO;
-  } else if (adType === NATIVE) {
-    return NATIVE;
   } else {
     return BANNER;
   }
