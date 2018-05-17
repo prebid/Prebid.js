f1fb393cf20e421efa3cd1b87600d8191a7c991d
Paul Wyrembak <paul@trustx.org>
2018-05-17 23:37:54 +0300

    Add support of gdpr to Trustx Bid Adapter

--- modules/trustxBidAdapter.js
+++ modules/trustxBidAdapter.js
@@ -32,7 +32,7 @@ export const spec = {
    * @param {BidRequest[]} validBidRequests - an array of bids
    * @return ServerRequest Info describing the request to the server.
    */
-  buildRequests: function(validBidRequests) {
+  buildRequests: function(validBidRequests, bidderRequest) {
     const auids = [];
     const bidsMap = {};
     const bids = validBidRequests || [];
@@ -58,6 +58,15 @@ export const spec = {
       auids: auids.join(','),
       r: reqId
     };
+
+    if (bidderRequest && bidderRequest.gdprConsent) {
+      if (bidderRequest.gdprConsent.consentString) {
+        payload.gdpr_consent = bidderRequest.gdprConsent.consentString;
+      }
+      payload.gdpr_applies =
+        (typeof bidderRequest.gdprConsent.gdprApplies === 'boolean')
+          ? Number(bidderRequest.gdprConsent.gdprApplies) : 1;
+    }
 
     return {
       method: 'GET',
