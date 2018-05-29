a64c1ad23cacd65067da8f4f34cfc0fac633ce32
Marcian123 <qgrudziaz@gmail.com>
2018-05-29 16:30:05 +0200

    Added GDPR integration to RTBhouse adapter

--- modules/rtbhouseBidAdapter.js
+++ modules/rtbhouseBidAdapter.js
@@ -73,15 +73,22 @@ export const spec = {
   isBidRequestValid: function (bid) {
     return !!(includes(REGIONS, bid.params.region) && bid.params.publisherId);
   },
-
   buildRequests: function (validBidRequests) {
     const request = {
       id: validBidRequests[0].auctionId,
       imp: validBidRequests.map(slot => mapImpression(slot)),
       site: mapSite(validBidRequests),
       cur: DEFAULT_CURRENCY_ARR,
-      test: validBidRequests[0].params.test || 0
+      test: validBidRequests[0].params.test || 0,
     };
+    if (validBidRequests[0].gdprConsent && validBidRequests[0].gdprConsent.gdprApplies) {
+      const consentStr = (validBidRequests[0].gdprConsent.consentString)
+        ? validBidRequests[0].gdprConsent.consentString.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '') : '';
+      const gdpr = validBidRequests[0].gdprConsent.gdprApplies ? 1 : 0;
+      request.regs = {ext: {gdpr: gdpr}};
+      request.user = {ext: {consent: consentStr}};
+    };
+
     return {
       method: 'POST',
       url: buildEndpointUrl(validBidRequests[0].params.region),
