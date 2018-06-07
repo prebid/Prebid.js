c597ce31975615da08719f4fa54bb7578d87b70c
gammassp <35954362+gammassp@users.noreply.github.com>
2018-06-07 10:33:49 +0700

    multi request

--- test/spec/modules/gammaBidAdapter_spec.js
+++ test/spec/modules/gammaBidAdapter_spec.js
@@ -5,14 +5,14 @@ import { newBidder } from 'src/adapters/bidderFactory';
 
 describe('gammaBidAdapter', function() {
   const adapter = newBidder(spec);
+  const ENDPOINT = 'hb.gammaplatform.com';
 
-  describe('isBidRequestValid', () => {
-    let bid = {
-      'bidder': 'gamma',
-      'params': {
-        siteId: '1465446377',
+  let bid = {
+    'bidder': 'gamma',
+    'params': {
+      siteId: '1465446377',
         zoneId: '1515999290'
-      },
+    },
       'adUnitCode': 'adunit-code',
       'sizes': [
         [300, 250]
@@ -20,8 +20,10 @@ describe('gammaBidAdapter', function() {
       'bidId': '23beaa6af6cdde',
       'bidderRequestId': '19c0c1efdf37e7',
       'auctionId': '61466567-d482-4a16-96f0-fe5f25ffbdf1',
-    };
-
+  };
+  let bidArray = [bid];
+  
+  describe('isBidRequestValid', () => {
     it('should return true when required params found', () => {
       expect(spec.isBidRequestValid(bid)).to.equal(true);
     });
@@ -40,56 +42,43 @@ describe('gammaBidAdapter', function() {
   });
 
   describe('buildRequests', () => {
-    let bidRequests = [
-      {
-        'bidder': 'gamma',
-        'params': {
-          siteId: '1465446377',
-          zoneId: '1515999290'
-        },
-        'adUnitCode': 'adunit-code',
-        'sizes': [
-          [300, 250]
-        ],
-        'bidId': '23beaa6af6cdde',
-        'bidderRequestId': '19c0c1efdf37e7',
-        'auctionId': '61466567-d482-4a16-96f0-fe5f25ffbdf1'
-      }
-    ];
-
-    const request = spec.buildRequests(bidRequests);
+    const request = spec.buildRequests(bidArray);
 
     it('sends bid request to our endpoint via GET', () => {
       expect(request.method).to.equal('GET');
     });
 
     it('bidRequest url', () => {
-      expect(request.url).to.match(new RegExp(`hb.gammaplatform.com`));
+       expect(request.url).to.be.equal(ENDPOINT);
     });
   });
 
   describe('interpretResponse', () => {
-    let serverResponse = {
-      body: {
-        'id': '23beaa6af6cdde',
-        'bid': '5611802021800040585',
-        'type': 'banner',
-        'cur': 'USD',
-        'seatbid': [{
-          'seat': '5611802021800040585',
-          'bid': [{
-            'id': '1515999070',
-            'impid': '1',
-            'price': 0.45,
-            'adm': '<!-- Creative -->',
-            'adid': '1515999070',
-            'dealid': 'gax-paj2qarjf2g',
-            'h': 250,
-            'w': 300
+    let serverResponse;
+
+    beforeEach(() => {
+      serverResponse = {
+        body: {
+          'id': '23beaa6af6cdde',
+          'bid': '5611802021800040585',
+          'type': 'banner',
+          'cur': 'USD',
+          'seatbid': [{
+            'seat': '5611802021800040585',
+            'bid': [{
+              'id': '1515999070',
+              'impid': '1',
+              'price': 0.45,
+              'adm': '<!-- Creative -->',
+              'adid': '1515999070',
+              'dealid': 'gax-paj2qarjf2g',
+              'h': 250,
+              'w': 300
+            }]
           }]
-        }]
-      }
-    };
+        }
+      };
+    })
 
     it('should get the correct bid response', () => {
       let expectedResponse = [{
