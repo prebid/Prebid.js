376466d5532cea3efc041b9cf57ca8354fe24daa
mkendall07 <emailmatthere@gmail.com>
2018-06-19 10:40:40 -0400

    remove find

--- test/test_index.js
+++ test/test_index.js
@@ -2,7 +2,7 @@ require('test/helpers/prebidGlobal.js');
 /* eslint-disable */
 // this is a hack, but it allows chrome to catch issues with some IE undefined values.
 Array.prototype.includes = undefined;
-Array.prototype.find = undefined;
+//Array.prototype.find = undefined;
 /* eslint-enable */
 var testsContext = require.context('.', true, /_spec$/);
 testsContext.keys().forEach(testsContext);
