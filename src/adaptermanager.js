050494e0cb3a7310c690651e2348e9f747fdcbbf
Rich Snapp <rsnapp@rubiconproject.com>
2018-06-20 15:06:41 -0600

    Max origin concurrent auctions (#2743)
    * initial attempt at limiting concurrenet auctions by origin
    
    * fix queueing of auctions for max origin
    
    * don't decrement on timeout as it is already called by onreadystatechange
    
    * move auction timer so it doesn't start until queued auction starts
    
    * set default max concurrent origin requests to 4 and make configurable
    
    * fix tests to not queue for auction.callBids
    
    * change MAX_REQUEST_PER_ORIGIN to local var

--- src/adaptermanager.js
+++ src/adaptermanager.js
@@ -273,7 +273,7 @@ exports.checkBidRequestSizes = (adUnits) => {
   return adUnits;
 }
 
-exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
+exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb, requestCallbacks) => {
   if (!bidRequests.length) {
     utils.logWarn('callBids executed with no bidRequests.  Were they filtered by labels or sizing?');
     return;
@@ -285,7 +285,10 @@ exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
   }, [[], []]);
 
   if (serverBidRequests.length) {
-    const s2sAjax = ajaxBuilder(serverBidRequests[0].timeout);
+    const s2sAjax = ajaxBuilder(serverBidRequests[0].timeout, requestCallbacks ? {
+      request: requestCallbacks.request.bind(null, 's2s'),
+      done: requestCallbacks.done
+    } : undefined);
     let adaptersServerSide = _s2sConfig.bidders;
     const s2sAdapter = _bidderRegistry[_s2sConfig.adapter];
     let tid = serverBidRequests[0].tid;
@@ -336,7 +339,6 @@ exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
     }
   }
 
-  const ajax = (clientBidRequests.length) ? ajaxBuilder(clientBidRequests[0].timeout) : null;
   // handle client adapter requests
   clientBidRequests.forEach(bidRequest => {
     bidRequest.start = timestamp();
@@ -347,6 +349,10 @@ exports.callBids = (adUnits, bidRequests, addBidResponse, doneCb) => {
       events.emit(CONSTANTS.EVENTS.BID_REQUESTED, bidRequest);
       bidRequest.doneCbCallCount = 0;
       let done = doneCb(bidRequest.bidderRequestId);
+      let ajax = ajaxBuilder(clientBidRequests[0].timeout, requestCallbacks ? {
+        request: requestCallbacks.request.bind(null, bidRequest.bidderCode),
+        done: requestCallbacks.done
+      } : undefined);
       adapter.callBids(bidRequest, addBidResponse, done, ajax);
     } else {
       utils.logError(`Adapter trying to be called which does not exist: ${bidRequest.bidderCode} adaptermanager.callBids`);
