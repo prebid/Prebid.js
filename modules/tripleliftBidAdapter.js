4319b1f247290d4f522d2efbd965eb94a366b3ae
Brittany Zellman <33695402+brittanyzellman@users.noreply.github.com>
2018-07-10 11:37:31 -0400

    Include gdpr TripleLift (#2663)
    * added triplelift bid adapter to modules
    
    * need to host
    
    * updated adapter to 1.12
    
    * removed useless conditional
    
    * fixed unused-variable error
    
    * fixed flash method
    
    * had two versions of the consent method, initially pushed the wrong one
    
    * added tmax to query string
    
    * fixed linter error
    
    * fixed linter error
    
    * tested for valid bid request
    
    * finished writing tests
    
    * added markdown file
    
    * slight change to md file
    
    * removed single quotes
    
    * deleted commented out import statement
    
    * small changes requested by MK
    
    * removed an from testing

---
+++ modules/tripleliftBidAdapter.js
@@ -0,0 +1,140 @@
+import { BANNER } from 'src/mediaTypes';
+import { registerBidder } from 'src/adapters/bidderFactory';
+import * as utils from 'src/utils';
+
+const BIDDER_CODE = 'triplelift';
+const STR_ENDPOINT = document.location.protocol + '//tlx.3lift.com/header/auction?';
+let gdprApplies = true;
+let consentString = null;
+
+export const tripleliftAdapterSpec = {
+
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER],
+  isBidRequestValid: function(bid) {
+    return (typeof bid.params.inventoryCode !== 'undefined');
+  },
+
+  buildRequests: function(bidRequests, bidderRequest) {
+    let tlCall = STR_ENDPOINT;
+    let referrer = utils.getTopWindowUrl();
+    let data = _buildPostBody(bidRequests);
+
+    tlCall = utils.tryAppendQueryString(tlCall, 'lib', 'prebid');
+    tlCall = utils.tryAppendQueryString(tlCall, 'v', '$prebid.version$');
+    tlCall = utils.tryAppendQueryString(tlCall, 'fe', _isFlashEnabled().toString());
+    tlCall = utils.tryAppendQueryString(tlCall, 'referrer', referrer);
+
+    if (bidderRequest && bidderRequest.timeout) {
+      tlCall = utils.tryAppendQueryString(tlCall, 'tmax', bidderRequest.timeout);
+    }
+
+    if (bidderRequest && bidderRequest.gdprConsent) {
+      if (typeof bidderRequest.gdprConsent.gdprApplies !== 'undefined') {
+        gdprApplies = bidderRequest.gdprConsent.gdprApplies;
+        tlCall = utils.tryAppendQueryString(tlCall, 'gdpr', gdprApplies.toString());
+      }
+      if (typeof bidderRequest.gdprConsent.consentString !== 'undefined') {
+        consentString = bidderRequest.gdprConsent.consentString;
+        tlCall = utils.tryAppendQueryString(tlCall, 'cmp_cs', consentString);
+      }
+    }
+
+    if (tlCall.lastIndexOf('&') === tlCall.length - 1) {
+      tlCall = tlCall.substring(0, tlCall.length - 1);
+    }
+    utils.logMessage('tlCall request built: ' + tlCall);
+
+    return {
+      method: 'POST',
+      url: tlCall,
+      data,
+      bidderRequest
+    };
+  },
+
+  interpretResponse: function(serverResponse, {bidderRequest}) {
+    let bids = serverResponse.body.bids || [];
+    return bids.map(function(bid) {
+      return _buildResponseObject(bidderRequest, bid);
+    });
+  },
+
+  getUserSyncs: function(syncOptions) {
+    let ibCall = '//ib.3lift.com/sync?';
+    if (consentString !== null) {
+      ibCall = utils.tryAppendQueryString(ibCall, 'gdpr', gdprApplies);
+      ibCall = utils.tryAppendQueryString(ibCall, 'cmp_cs', consentString);
+    }
+
+    if (syncOptions.iframeEnabled) {
+      return [{
+        type: 'iframe',
+        url: ibCall
+      }];
+    }
+  }
+}
+
+function _buildPostBody(bidRequests) {
+  let data = {};
+  data.imp = bidRequests.map(function(bid, index) {
+    return {
+      id: index,
+      tagid: bid.params.inventoryCode,
+      floor: bid.params.floor,
+      banner: {
+        format: _sizes(bid.sizes)
+      }
+    }
+  });
+
+  return data;
+}
+
+function _sizes(sizeArray) {
+  return sizeArray.map(function(size) {
+    return {
+      w: size[0],
+      h: size[1]
+    };
+  });
+}
+
+function _buildResponseObject(bidderRequest, bid) {
+  let bidResponse = {};
+  let width = bid.width || 1;
+  let height = bid.height || 1;
+  let dealId = bid.deal_id || '';
+  let creativeId = bid.imp_id;
+
+  if (bid.cpm != 0 && bid.ad) {
+    bidResponse = {
+      requestId: bidderRequest.bids[creativeId].bidId,
+      cpm: bid.cpm,
+      width: width,
+      height: height,
+      netRevenue: true,
+      ad: bid.ad,
+      creativeId: creativeId,
+      dealId: dealId,
+      currency: 'USD',
+      ttl: 33,
+    };
+  };
+  return bidResponse;
+}
+
+function _isFlashEnabled() {
+  let flash;
+  try {
+    flash = Boolean(new ActiveXObject('ShockwaveFlash.ShockwaveFlash'));
+  } catch (e) {
+    flash = navigator.mimeTypes &&
+      navigator.mimeTypes['application/x-shockwave-flash'] !== undefined &&
+      navigator.mimeTypes['application/x-shockwave-flash'].enabledPlugin ? 1 : 0
+  }
+  return flash ? 1 : 0;
+}
+
+registerBidder(tripleliftAdapterSpec);
