6fa26baaaa556b2856e906863f6d9d1183589687
onaydenov <onaydenov@users.noreply.github.com>
2018-08-03 12:43:23 +0200

    Add GDPR support

--- modules/fidelityBidAdapter.js
+++ modules/fidelityBidAdapter.js
@@ -3,6 +3,7 @@ import {registerBidder} from 'src/adapters/bidderFactory';
 
 const BIDDER_CODE = 'fidelity';
 const BIDDER_SERVER = 'x.fidelity-media.com';
+const FIDELITY_VENDOR_ID = 408;
 export const spec = {
   code: BIDDER_CODE,
   isBidRequestValid: function(bid) {
@@ -20,14 +21,25 @@ export const spec = {
         zoneid: bidRequest.params.zoneid,
         floor: parseFloat(bidRequest.params.floor) > 0 ? bidRequest.params.floor : 0,
         charset: document.charSet || document.characterSet,
-        defloc: utils.getTopWindowUrl(),
-        altloc: window.location.href,
         subid: 'hb',
         flashver: getFlashVersion(),
         tmax: bidderRequest.timeout,
+        defloc: utils.getTopWindowUrl(),
+        referrer: utils.getTopWindowReferrer(),
       };
-      if (document.referrer) {
-        payload.referrer = document.referrer;
+      if (bidderRequest.gdprConsent) {
+        payload.gdpr = 0;
+        payload.consent_str = '';
+        payload.consent_given = 0;
+        if (typeof bidderRequest.gdprConsent.gdprApplies !== 'undefined') {
+          payload.gdpr = bidderRequest.gdprConsent.gdprApplies ? 1 : 0;
+        }
+        if (typeof bidderRequest.gdprConsent.consentString !== 'undefined') {
+          payload.consent_str = bidderRequest.gdprConsent.consentString;
+        }
+        if (bidderRequest.gdprConsent.vendorData && bidderRequest.gdprConsent.vendorData.vendorConsents && typeof bidderRequest.gdprConsent.vendorData.vendorConsents[FIDELITY_VENDOR_ID.toString(10)] !== 'undefined') {
+          payload.consent_given = bidderRequest.gdprConsent.vendorData.vendorConsents[FIDELITY_VENDOR_ID.toString(10)] ? 1 : 0;
+        }
       }
 
       return {
@@ -59,11 +71,27 @@ export const spec = {
     }
     return bidResponses;
   },
-  getUserSyncs: function getUserSyncs(syncOptions) {
+  getUserSyncs: function getUserSyncs(syncOptions, serverResponses, gdprConsent) {
     if (syncOptions.iframeEnabled) {
+      var url = '//' + BIDDER_SERVER + '/delivery/matches.php?type=iframe';
+      if (gdprConsent) {
+        var gdpr = 0;
+        var consent_str = '';
+        var consent_given = 0;
+        if (typeof gdprConsent.gdprApplies !== 'undefined') {
+          gdpr = gdprConsent.gdprApplies ? 1 : 0;
+        }
+        if (typeof gdprConsent.consentString !== 'undefined') {
+          consent_str = gdprConsent.consentString;
+        }
+        if (gdprConsent.vendorData && gdprConsent.vendorData.vendorConsents && typeof gdprConsent.vendorData.vendorConsents[FIDELITY_VENDOR_ID.toString(10)] !== 'undefined') {
+          consent_given = gdprConsent.vendorData.vendorConsents[FIDELITY_VENDOR_ID.toString(10)] ? 1 : 0;
+        }
+        url += '&gdpr=' + gdpr + '&consent_str=' + encodeURIComponent(consent_str) + '&consent_given=' + consent_given;
+      }
       return [{
         type: 'iframe',
-        url: '//' + BIDDER_SERVER + '/delivery/matches.php?type=iframe',
+        url: url,
       }];
     }
   }
