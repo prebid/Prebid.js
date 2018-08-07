626282880d966206b58f5fa08b801e037d18edd4
Jaimin Panchal <jpanchal@appnexus.com>
2018-08-07 12:08:44 -0400

    remove commented code

--- modules/prebidServerBidAdapter/config.js
+++ modules/prebidServerBidAdapter/config.js
@@ -1,5 +1,3 @@
-// import { transformBidderParamKeywords } from 'src/utils';
-
 // accountId and bidders params are not included here, should be configured by end-user
 export const S2S_VENDORS = {
   'appnexus': {
@@ -19,46 +17,3 @@ export const S2S_VENDORS = {
     timeout: 500
   }
 }
-
-// export const paramTypes = {
-//   'appnexus': {
-//     'member': 'string',
-//     'invCode': 'string',
-//     'placementId': 'number',
-//     'keywords': transformBidderParamKeywords
-//   },
-//   'rubicon': {
-//     'accountId': 'number',
-//     'siteId': 'number',
-//     'zoneId': 'number'
-//   },
-//   'indexExchange': {
-//     'siteID': 'number'
-//   },
-//   'audienceNetwork': {
-//     'placementId': 'string'
-//   },
-//   'pubmatic': {
-//     'publisherId': 'string',
-//     'adSlot': 'string'
-//   },
-//   'districtm': {
-//     'member': 'string',
-//     'invCode': 'string',
-//     'placementId': 'number'
-//   },
-//   'pulsepoint': {
-//     'cf': 'string',
-//     'cp': 'number',
-//     'ct': 'number'
-//   },
-//   'conversant': {
-//     'site_id': 'string',
-//     'secure': 'number',
-//     'mobile': 'number'
-//   },
-//   'openx': {
-//     'unit': 'string',
-//     'customFloor': 'number'
-//   },
-// };
