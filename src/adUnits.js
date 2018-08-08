241d7e443d9343f04caff83b1133f3e546e405a7
Vedant Seta <vedant.s@media.net>
2018-08-08 13:17:51 +0530

    jsdoc added

--- src/adUnits.js
+++ src/adUnits.js
@@ -2,17 +2,33 @@ import { deepAccess } from './utils';
 
 let adUnits = {};
 
+/**
+ * Increments and returns current Adunit counter
+ * @param {string} adunit id
+ * @returns {number} current adunit count
+ */
 function incrementCounter(adunit) {
   adUnits[adunit] = adUnits[adunit] || {};
   adUnits[adunit].counter = (deepAccess(adUnits, `${adunit}.counter`) + 1) || 1;
   return adUnits[adunit].counter;
 }
 
+/**
+ * Returns current Adunit counter
+ * @param {string} adunit id
+ * @returns {number} current adunit count
+ */
 function getCounter(adunit) {
   return deepAccess(adUnits, `${adunit}.counter`) || 0;
 }
 
-exports.adunitCounter = {
+/**
+ * A module which counts how many times an adunit was called
+ * @module adunitCounter
+ */
+let adunitCounter = {
   incrementCounter,
   getCounter
 }
+
+export { adunitCounter };
