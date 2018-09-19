e5178e9899cc8e72bdfcd30afadd76a784fa9147
iijimalo1023 <iijima@lob-inc.com>
2018-09-19 12:44:51 +0900

    Added test code of getUserSyncs.

--- test/spec/modules/rdnBidAdapter_spec.js
+++ test/spec/modules/rdnBidAdapter_spec.js
@@ -4,14 +4,14 @@ import { spec } from 'modules/rdnBidAdapter'
 import { newBidder } from 'src/adapters/bidderFactory'
 
 describe('rdnBidAdapter', function() {
-  const adapter = newBidder(spec)
-  const ENDPOINT = 'https://s-bid.rmp.rakuten.co.jp/h'
+  const adapter = newBidder(spec);
+  const ENDPOINT = 'https://s-bid.rmp.rakuten.co.jp/h';
 
   describe('inherited functions', () => {
     it('exists and is a function', () => {
       expect(adapter.callBids).to.exist.and.to.be.a('function')
     })
-  })
+  });
 
   describe('isBidRequestValid', () => {
     let bid = {
@@ -19,24 +19,24 @@ describe('rdnBidAdapter', function() {
       params: {
         adSpotId: '56789'
       }
-    }
+    };
 
     it('should return true when required params found', () => {
       expect(spec.isBidRequestValid(bid)).to.equal(true)
-    })
+    });
 
     it('should return false when required params are not passed', () => {
-      bid.params.adSpotId = ''
+      bid.params.adSpotId = '';
       expect(spec.isBidRequestValid(bid)).to.equal(false)
-    })
+    });
 
     it('should return false when required params are not passed', () => {
-      let bid = Object.assign({}, bid)
-      delete bid.params
-      bid.params = {}
+      let bid = Object.assign({}, bid);
+      delete bid.params;
+      bid.params = {};
       expect(spec.isBidRequestValid(bid)).to.equal(false)
     })
-  })
+  });
 
   describe('buildRequests', () => {
     const bidRequests = [
@@ -46,14 +46,14 @@ describe('rdnBidAdapter', function() {
           adSpotId: '58278'
         }
       }
-    ]
+    ];
 
     it('sends bid request to ENDPOINT via GET', () => {
-      const request = spec.buildRequests(bidRequests)[0]
-      expect(request.url).to.equal(ENDPOINT)
+      const request = spec.buildRequests(bidRequests)[0];
+      expect(request.url).to.equal(ENDPOINT);
       expect(request.method).to.equal('GET')
     })
-  })
+  });
 
   describe('interpretResponse', () => {
     const bidRequests = {
@@ -71,7 +71,7 @@ describe('rdnBidAdapter', function() {
           pp: 'https://examples.com/hoge/muga'
         }
       }
-    }
+    };
 
     const serverResponse = {
       noAd: [],
@@ -88,14 +88,41 @@ describe('rdnBidAdapter', function() {
         referrer: utils.getTopWindowUrl(),
         ad: '<!-- adtag -->'
       }
-    }
+    };
 
     it('handles nobid responses', () => {
       const result = spec.interpretResponse(
         { body: serverResponse.noAd },
+
         bidRequests.banner
-      )
+      );
       expect(result.length).to.equal(1)
     })
-  })
-})
+  });
+  describe('spec.getUserSyncs', function () {
+    const syncResponse = [{
+      body: {
+        request_id: 'biequa9oaph4we',
+        sync_urls: ['https://rdn1.test/sync?uid=9876543210', 'https://rdn2.test/sync?uid=9876543210']
+      }
+    }];
+    const nosyncResponse = [{
+      body: {
+        request_id: 'biequa9oaph4we',
+        sync_urls: []
+      }
+    }];
+    let syncOptions
+    beforeEach(function () {
+      syncOptions = {
+        pixelEnabled: true
+      }
+    });
+    it('sucess usersync url', function () {
+      const result = [];
+      result.push({type: 'image', url: 'https://rdn1.test/sync?uid=9876543210'});
+      result.push({type: 'image', url: 'https://rdn2.test/sync?uid=9876543210'});
+      expect(spec.getUserSyncs(syncOptions, syncResponse)).to.deep.equal(result);
+    });
+  });
+});
