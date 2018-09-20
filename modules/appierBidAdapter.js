32798c33a9769aa0ef29c52e67c443a02ff75900
Hong Jen-Yee (PCMan) <pcman.hong@appier.com>
2018-09-20 14:50:47 +0800

    Handle server response for Appier adapter and add related unit tests.

--- modules/appierBidAdapter.js
+++ modules/appierBidAdapter.js
@@ -3,15 +3,28 @@ import { BANNER } from 'src/mediaTypes';
 
 const SUPPORTED_AD_TYPES = [BANNER];
 const BIDDER_API_URL = '//pmp-stg-server.tw.appier.biz/v1/prebid/bid';
+const SHOW_CALLBACK_URL = '//pmp-stg-server.tw.appier.biz/v1/prebid/show_cb';
 
 export const spec = {
   code: 'appier',
   supportedMediaTypes: SUPPORTED_AD_TYPES,
 
+  /**
+   * Determines whether or not the given bid request is valid.
+   *
+   * @param {BidRequest} bid The bid params to validate.
+   * @return boolean True if this is a valid bid, and false otherwise.
+   */
   isBidRequestValid: function (bid) {
-    return typeof bid.params.zoneId === 'string';
+    return typeof bid.params.hzid === 'string';
   },
 
+  /**
+   * Make a server request from the list of BidRequests.
+   *
+   * @param {bidRequests[]} - an array of bids
+   * @return ServerRequest Info describing the request to the server.
+   */
   buildRequests: function (bidRequests, bidderRequest) {
     if (bidRequests.length === 0) {
       return [];
@@ -20,10 +33,45 @@ export const spec = {
     return [{
       method: 'POST',
       url: BIDDER_API_URL,
-      data: bidRequests
+      data: bidRequests,
+      // keep the bidder request object for later use
+      bidderRequest: bidderRequest
     }];
   },
 
+  /**
+   * Unpack the response from the server into a list of bids.
+   *
+   * @param {serverResponse} serverResponse A successful response from the server.
+   * @return {Bid[]} An array of bids which were nested inside the server.
+   */
+  interpretResponse: function (serverResponse, serverRequest) {
+    if (!serverResponse.body || !Array.isArray(serverResponse.body)) {
+      return [];
+    }
+    // server response body is an array of bid results
+    let bidResults = serverResponse.body;
+    // our server directly returns the format needed by prebid.js so no more
+    // transformation is needed here.
+    return bidResults;
+  },
+
+  /**
+   * Register bidder specific code, which will execute if a bid from this bidder won the auction
+   * @param {Bid} The bid that won the auction
+   */
+  onBidWon: function(bid) {
+    let hzid = bid.appierParams.hzid;
+    let cpm = bid.adserverTargeting.hb_pb;
+    let currency = bid.currency;
+    let showCallbackUrl = this.generateShowCallbackUrl(hzid, cpm, currency);
+    // add the image beacon to creative html
+    bid.ad += '<img src="' + showCallbackUrl + '">';
+  },
+
+  generateShowCallbackUrl(hzid, cpm, currency) {
+    return SHOW_CALLBACK_URL + '?hzid=' + hzid + '&cpm=' + cpm + '&currency=' + currency;
+  }
 };
 
 registerBidder(spec);
--- test/spec/modules/appierBidAdapter_spec.js
+++ test/spec/modules/appierBidAdapter_spec.js
@@ -15,7 +15,7 @@ describe('AppierAdapter', function () {
     let bid = {
       'bidder': 'appier',
       'params': {
-        'zoneId': 'abcd'
+        'hzid': 'abcd'
       },
       'adUnitCode': 'adunit-code',
       'sizes': [[300, 250], [300, 600]],
@@ -37,7 +37,7 @@ describe('AppierAdapter', function () {
     it('should return false when required param zoneId has wrong type', function () {
       let bid = Object.assign({}, bid);
       bid.params = {
-        'zoneId': null
+        'hzid': null
       };
       expect(spec.isBidRequestValid(bid)).to.equal(false);
     });
@@ -54,7 +54,7 @@ describe('AppierAdapter', function () {
       let bid = {
         'bidder': 'appier',
         'params': {
-          'zoneId': 'abcd'
+          'hzid': 'abcd'
         },
         'adUnitCode': 'adunit-code',
         'sizes': [[300, 250], [300, 600]],
@@ -70,6 +70,98 @@ describe('AppierAdapter', function () {
       expect(builtRequests[0].method).to.equal('POST');
       expect(builtRequests[0].url).match(/v1\/prebid\/bid/);
       expect(builtRequests[0].data).deep.equal(fakeBidRequests);
+    });
+  });
+
+  describe('interpretResponse', function() {
+    let bid = {
+      'bidder': 'appier',
+      'params': {
+        'hzid': 'abcd'
+      },
+      'adUnitCode': 'adunit-code',
+      'sizes': [[300, 250], [300, 600]],
+      'bidId': '30b31c1838de1e',
+      'bidderRequestId': '22edbae2733bf6',
+      'auctionId': '1d1a030790a475',
+    };
+    let fakeBidRequests = [bid];
+
+    it('should return an empty aray to indicate no valid bids', function() {
+      let fakeServerResponse = {};
+
+      let bidResponses = spec.interpretResponse(fakeServerResponse, fakeBidRequests);
+
+      expect(bidResponses).is.an('array').that.is.empty;
+    });
+
+    it('should generate correct response array for bidder', function() {
+      let fakeBidResult = {
+        'requestId': '30b31c1838de1e',
+        'cpm': 0.0029346001,
+        'creativeId': 'Idl0P0d5S3Ca5kVWcia-wQ',
+        'width': 300,
+        'height': 250,
+        'currency': 'USD',
+        'netRevenue': true,
+        'ttl': 300,
+        'ad': '<div>fake html</div>',
+        'appierParams': {
+          'hzid': 'test_hzid'
+        }
+      };
+      let fakeServerResponse = {
+        headers: [],
+        body: [fakeBidResult]
+      };
+
+      let bidResponses = spec.interpretResponse(fakeServerResponse, fakeBidRequests);
+      expect(bidResponses).deep.equal([fakeBidResult]);
+    });
+  });
+
+  describe('onBidWon', function() {
+    let fakeBid = {
+      'bidder': 'appier',
+      'width': 300,
+      'height': 250,
+      'adId': '330a22bdea4cac',
+      'mediaType': 'banner',
+      'cpm': 0.28,
+      'ad': '<div>test creative</div>',
+      'requestId': '418b37f85e772c',
+      'adUnitCode': 'div-gpt-ad-1460505748561-0',
+      'size': '350x250',
+      'adserverTargeting': {
+        'hb_bidder': 'appier',
+        'hb_adid': '330a22bdea4cac',
+        'hb_pb': '0.20',
+        'hb_size': '350x250'
+      },
+      // FIXME: not sure if the following fields are available here. They are not documented.
+      'currency': 'TWD',
+      'appierParams': {
+        'hzid': 'test_hzid'
+      }
+    };
+
+    it('should generate beacon URL for show callback', function() {
+      spec.onBidWon(fakeBid);
+
+      let beaconUrl = spec.generateShowCallbackUrl('test_hzid', '0.20', 'TWD');
+      let imgTag = '<img src="' + beaconUrl + '">';
+
+      expect(fakeBid.ad).contains(imgTag);
+    });
+  });
+
+  describe('generateShowCallbackUrl', function() {
+    it('should generate a show callback url passing cpm and currency', function() {
+      let beaconUrl = spec.generateShowCallbackUrl('test_hzid', '0.20', 'TWD');
+
+      expect(beaconUrl).matches(/hzid=test_hzid/);
+      expect(beaconUrl).matches(/cpm=0.20/);
+      expect(beaconUrl).matches(/currency=TWD/);
     });
   });
 });
