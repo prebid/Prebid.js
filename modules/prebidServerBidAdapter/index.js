b799bbe986773ba8ef43a7317f1566a390fa5e25
Justas Pupelis <j.pupelis@adform.com>
2018-09-26 23:26:17 +0300

    Separating openRTB protocol request creation

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -11,6 +11,7 @@ import { isValid } from 'src/adapters/bidderFactory';
 import events from 'src/events';
 import includes from 'core-js/library/fn/array/includes';
 import { S2S_VENDORS } from './config.js';
+import { buildRequestOpenRTB } from 'src/openRTBProtocol';
 
 const getConfig = config.getConfig;
 
@@ -359,120 +360,7 @@ const OPEN_RTB_PROTOCOL = {
 
   bidMap: {},
 
-  buildRequest(s2sBidRequest, bidRequests, adUnits) {
-    let imps = [];
-    let aliases = {};
-
-    // transform ad unit into array of OpenRTB impression objects
-    adUnits.forEach(adUnit => {
-      adUnit.bids.forEach(bid => {
-        // OpenRTB response contains the adunit code and bidder name. These are
-        // combined to create a unique key for each bid since an id isn't returned
-        const key = `${adUnit.code}${bid.bidder}`;
-        this.bidMap[key] = bid;
-
-        // check for and store valid aliases to add to the request
-        if (adaptermanager.aliasRegistry[bid.bidder]) {
-          aliases[bid.bidder] = adaptermanager.aliasRegistry[bid.bidder];
-        }
-      });
-
-      let banner;
-      // default to banner if mediaTypes isn't defined
-      if (utils.isEmpty(adUnit.mediaTypes)) {
-        const sizeObjects = adUnit.sizes.map(size => ({ w: size[0], h: size[1] }));
-        banner = {format: sizeObjects};
-      }
-
-      const bannerParams = utils.deepAccess(adUnit, 'mediaTypes.banner');
-      if (bannerParams && bannerParams.sizes) {
-        const sizes = utils.parseSizesInput(bannerParams.sizes);
-
-        // get banner sizes in form [{ w: <int>, h: <int> }, ...]
-        const format = sizes.map(size => {
-          const [ width, height ] = size.split('x');
-          const w = parseInt(width, 10);
-          const h = parseInt(height, 10);
-          return { w, h };
-        });
-
-        banner = {format};
-      }
-
-      let video;
-      const videoParams = utils.deepAccess(adUnit, 'mediaTypes.video');
-      if (!utils.isEmpty(videoParams)) {
-        video = videoParams;
-      }
-
-      // get bidder params in form { <bidder code>: {...params} }
-      const ext = adUnit.bids.reduce((acc, bid) => {
-        const adapter = adaptermanager.bidderRegistry[bid.bidder];
-        if (adapter && adapter.getSpec().transformBidParams) {
-          bid.params = adapter.getSpec().transformBidParams(bid.params, isOpenRtb());
-        }
-        acc[bid.bidder] = bid.params;
-        return acc;
-      }, {});
-
-      const imp = { id: adUnit.code, ext, secure: _s2sConfig.secure };
-
-      if (banner) { imp.banner = banner; }
-      if (video) { imp.video = video; }
-
-      imps.push(imp);
-    });
-
-    const request = {
-      id: s2sBidRequest.tid,
-      source: {tid: s2sBidRequest.tid},
-      tmax: _s2sConfig.timeout,
-      imp: imps,
-      test: getConfig('debug') ? 1 : 0,
-    };
-
-    _appendSiteAppDevice(request);
-
-    const digiTrust = _getDigiTrustQueryParams();
-    if (digiTrust) {
-      request.user = { ext: { digitrust: digiTrust } };
-    }
-
-    if (!utils.isEmpty(aliases)) {
-      request.ext = { prebid: { aliases } };
-    }
-
-    if (bidRequests && bidRequests[0].gdprConsent) {
-      // note - gdprApplies & consentString may be undefined in certain use-cases for consentManagement module
-      let gdprApplies;
-      if (typeof bidRequests[0].gdprConsent.gdprApplies === 'boolean') {
-        gdprApplies = bidRequests[0].gdprConsent.gdprApplies ? 1 : 0;
-      }
-
-      if (request.regs) {
-        if (request.regs.ext) {
-          request.regs.ext.gdpr = gdprApplies;
-        } else {
-          request.regs.ext = { gdpr: gdprApplies };
-        }
-      } else {
-        request.regs = { ext: { gdpr: gdprApplies } };
-      }
-
-      let consentString = bidRequests[0].gdprConsent.consentString;
-      if (request.user) {
-        if (request.user.ext) {
-          request.user.ext.consent = consentString;
-        } else {
-          request.user.ext = { consent: consentString };
-        }
-      } else {
-        request.user = { ext: { consent: consentString } };
-      }
-    }
-
-    return request;
-  },
+  buildRequest: buildRequestOpenRTB,
 
   interpretResponse(response, bidderRequests, requestedBidders) {
     const bids = [];
@@ -583,7 +471,18 @@ export function PrebidServer() {
       queueSync(_s2sConfig.bidders, consent);
     }
 
-    const request = protocolAdapter().buildRequest(s2sBidRequest, bidRequests, adUnitsWithSizes);
+    const request = protocolAdapter().buildRequest(
+      s2sBidRequest,
+      bidRequests,
+      adUnitsWithSizes,
+      {
+        _s2sConfig,
+        getConfig,
+        _appendSiteAppDevice,
+        _getDigiTrustQueryParams,
+        isOpenRtb
+      });
+
     const requestJson = JSON.stringify(request);
 
     ajax(
