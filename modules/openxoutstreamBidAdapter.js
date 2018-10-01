3c7f4e481f35f96971cae1614cc4c6c27ea6b5f7
Andrew Holz <aholz@yieldmo.com>
2018-10-01 17:11:43 -0400

    adds openxoutstreamadapter files

---
+++ modules/openxoutstreamBidAdapter.js
@@ -0,0 +1,259 @@
+import { config } from 'src/config';
+import { registerBidder } from 'src/adapters/bidderFactory';
+import * as utils from 'src/utils';
+import { BANNER } from 'src/mediaTypes';
+import { parse } from 'src/url';
+
+const SUPPORTED_AD_TYPES = [BANNER];
+const BIDDER_CODE = 'openxoutstream';
+const CURRENCY = 'USD';
+const NET_REVENUE = true;
+const TIME_TO_LIVE = 300;
+const YM_SCRIPT = `!function(e,t){if(void 0===t._ym){var a=Math.round(5*Math.random()/3)+'';t._ym='';var m=e.createElement('script');m.type='text/javascript',m.async=!0,m.src='//static.yieldmo.com/ym.'+a+'.js',(e.getElementsByTagName('head')[0]||e.getElementsByTagName('body')[0]).appendChild(m)}else t._ym instanceof String||void 0===t._ym.chkPls||t._ym.chkPls()}(document,window);`;
+const PLACEMENT_ID = '1986307928000988495';
+const LICENSED_FORMAT_ID = '1991358644725162817';
+const PUBLISHER_ID = '1986307525700126029';
+
+export const spec = {
+  code: BIDDER_CODE,
+  supportedMediaTypes: SUPPORTED_AD_TYPES,
+  isBidRequestValid: function(bidRequest) {
+    if (utils.deepAccess(bidRequest, 'mediaTypes.banner') && bidRequest.params.delDomain) {
+      console.log('valid bid request');
+      return !!bidRequest.params.unit || utils.deepAccess(bidRequest, 'mediaTypes.banner.sizes.length') > 0;
+    }
+    return !!(bidRequest.params.unit && bidRequest.params.delDomain);
+  },
+  buildRequests: function(bidRequests, bidderRequest) {
+    if (bidRequests.length === 0) {
+      return [];
+    }
+    let requests = [];
+    requests.push(buildOXBannerRequest(bidRequests, bidderRequest));
+    return requests;
+  },
+  interpretResponse: function(serverResponse, serverRequest) {
+    return handleVastResponse(serverResponse, serverRequest.payload)
+  },
+
+  transformBidParams: function(params, isOpenRtb) {
+    return utils.convertTypes({
+      'unit': 'string',
+      'customFloor': 'number'
+    }, params);
+  }
+};
+
+function getViewportDimensions(isIfr) {
+  let width;
+  let height;
+  let tWin = window;
+  let tDoc = document;
+  let docEl = tDoc.documentElement;
+  let body;
+
+  if (isIfr) {
+    try {
+      tWin = window.top;
+      tDoc = window.top.document;
+    } catch (e) {
+      return;
+    }
+    docEl = tDoc.documentElement;
+    body = tDoc.body;
+
+    width = tWin.innerWidth || docEl.clientWidth || body.clientWidth;
+    height = tWin.innerHeight || docEl.clientHeight || body.clientHeight;
+  } else {
+    docEl = tDoc.documentElement;
+    width = tWin.innerWidth || docEl.clientWidth;
+    height = tWin.innerHeight || docEl.clientHeight;
+  }
+
+  return `${width}x${height}`;
+}
+
+function buildCommonQueryParamsFromBids(bids, bidderRequest) {
+  const isInIframe = utils.inIframe();
+  let defaultParams;
+  defaultParams = {
+    ju: config.getConfig('pageUrl') || utils.getTopWindowUrl(),
+    jr: utils.getTopWindowReferrer(),
+    ch: document.charSet || document.characterSet,
+    res: `${screen.width}x${screen.height}x${screen.colorDepth}`,
+    ifr: isInIframe,
+    tz: new Date().getTimezoneOffset(),
+    tws: getViewportDimensions(isInIframe),
+    be: 1,
+    // bc: bids[0].params.bc,
+    dddid: utils._map(bids, bid => bid.transactionId).join(','),
+    nocache: new Date().getTime()
+  };
+
+  if (utils.deepAccess(bidderRequest, 'gdprConsent')) {
+    let gdprConsentConfig = bidderRequest.gdprConsent;
+
+    if (gdprConsentConfig.consentString !== undefined) {
+      defaultParams.gdpr_consent = gdprConsentConfig.consentString;
+    }
+
+    if (gdprConsentConfig.gdprApplies !== undefined) {
+      defaultParams.gdpr = gdprConsentConfig.gdprApplies ? 1 : 0;
+    }
+
+    if (config.getConfig('consentManagement.cmpApi') === 'iab') {
+      defaultParams.x_gdpr_f = 1;
+    }
+  }
+
+  return defaultParams;
+}
+
+function buildOXBannerRequest(bids, bidderRequest) {
+  let customParamsForAllBids = [];
+  let hasCustomParam = false;
+  let queryParams = buildCommonQueryParamsFromBids(bids, bidderRequest);
+  let auids = utils._map(bids, bid => bid.params.unit);
+  queryParams.aus = utils._map(bids, bid => utils.parseSizesInput(bid.sizes).join(',')).join('|');
+  queryParams.divIds = utils._map(bids, bid => encodeURIComponent(bid.adUnitCode)).join(',');
+
+  if (auids.some(auid => auid)) {
+    queryParams.auid = auids.join(',');
+  }
+
+  if (bids.some(bid => bid.params.doNotTrack)) {
+    queryParams.ns = 1;
+  }
+
+  if (bids.some(bid => bid.params.coppa)) {
+    queryParams.tfcd = 1;
+  }
+
+  bids.forEach(function(bid) {
+    if (bid.params.customParams) {
+      let customParamsForBid = utils._map(Object.keys(bid.params.customParams), customKey => formatCustomParms(customKey, bid.params.customParams));
+      let formattedCustomParams = window.btoa(customParamsForBid.join('&'));
+      hasCustomParam = true;
+      customParamsForAllBids.push(formattedCustomParams);
+    } else {
+      customParamsForAllBids.push('');
+    }
+  });
+  if (hasCustomParam) {
+    queryParams.tps = customParamsForAllBids.join(',');
+  }
+
+  let customFloorsForAllBids = [];
+  let hasCustomFloor = false;
+  bids.forEach(function(bid) {
+    if (bid.params.customFloor) {
+      customFloorsForAllBids.push(bid.params.customFloor * 1000);
+      hasCustomFloor = true;
+    } else {
+      customFloorsForAllBids.push(0);
+    }
+  });
+  if (hasCustomFloor) {
+    queryParams.aumfs = customFloorsForAllBids.join(',');
+  }
+
+  let url = `https://‌${bids[0].params.delDomain}/v/1.0/avjp?auid=540141567&ju=${bids[0].params.publisher_page_url}&vwd=${bids[0].params.width}&vwd=${bids[0].params.height}&openrtb=%7B%22mimes%22%3A%5B%22video%2Fmp4%22%5D%7D`
+  return {
+    method: 'GET',
+    url: url,
+    data: queryParams,
+    payload: { 'bids': bids, 'startTime': new Date() }
+  };
+}
+
+function handleVastResponse(response, serverResponse) {
+  let bidResponses = [];
+  const body = response.body
+
+  if (response !== undefined && body.vastUrl !== '' && body.pub_rev !== '') {
+    let vastQueryParams = parse(body.vastUrl).search || {};
+    const openHtmltag = '<html><head></head><body>';
+    const closeHtmlTag = '</body></html>';
+    const sdkScript = createSdkScript().outerHTML;
+    const placementDiv = createPlacementDiv();
+    placementDiv.dataset.pID = PUBLISHER_ID;
+    const placementDivString = placementDiv.outerHTML;
+    const adResponse = getFakeAdResponse(body.vastUrl, PLACEMENT_ID);
+    const adResponseString = JSON.stringify(adResponse);
+    const ymAdsScript = '<script type="text/javascript"> window.__ymAds =' + adResponseString + '</script>';
+
+    let bidResponse = {};
+    bidResponse.requestId = serverResponse.bids[0].bidId;
+    bidResponse.bidderCode = BIDDER_CODE;
+    bidResponse.netRevenue = NET_REVENUE;
+    bidResponse.currency = CURRENCY;
+    bidResponse.cpm = Number(body.pub_rev) / 1000;
+    bidResponse.creativeId = body.adid;
+    bidResponse.height = body.height;
+    bidResponse.width = body.width;
+    bidResponse.vastUrl = body.vastUrl;
+    bidResponse.ttl = TIME_TO_LIVE;
+    bidResponse.mediaType = BANNER;
+    bidResponse.ad = openHtmltag + placementDivString + ymAdsScript + sdkScript + closeHtmlTag;
+
+    response.ph = vastQueryParams.ph;
+    response.colo = vastQueryParams.colo;
+    response.ts = vastQueryParams.ts;
+    bidResponses.push(bidResponse);
+  }
+  return bidResponses;
+}
+registerBidder(spec);
+
+// HELPER FUNCTIONS
+function createSdkScript() {
+  const script = document.createElement('script');
+  script.innerHTML = YM_SCRIPT;
+  return script;
+}
+function createPlacementDiv() {
+  const div = document.createElement('div');
+  div.id = `ym_${PLACEMENT_ID}`;
+  div.classList.add('ym');
+  div.dataset.lfid = `${LICENSED_FORMAT_ID}`;
+  return div
+}
+
+/**
+ * Create a fake nativeplay with the placement id and vastXML.
+ * @param vastUrl
+ * @param placementId
+ */
+const getFakeAdResponse = (vastUrl, placementId) => {
+  return {
+    availability_zone: 'us-east-1a',
+    data: [
+      {
+        ads: [
+          {
+            actions: {},
+            ad_id: 1991358644725162800,
+            adv_id: '1991358644725162800',
+            configurables: {
+              cta_button_copy: 'Learn More',
+              vast_click_tracking: 'true',
+              vast_url: vastUrl,
+            },
+            cr_id: '1991358644725162817',
+            rti: '1',
+          }
+        ],
+        column_count: 1,
+        configs: {
+          allowable_height: '248',
+          header_copy: 'You May Like',
+          ping: 'true',
+        },
+        creative_format_id: 40,
+        css: '',
+        placement_id: placementId,
+      }
+    ],
+    nc: 0,
+  };
+};
