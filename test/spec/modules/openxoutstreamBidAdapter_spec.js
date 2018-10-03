e47a2f5e020e6b2bda8c694fc7326bc7c3a85bb2
Andrew Holz <aholz@yieldmo.com>
2018-10-03 13:14:22 -0400

    fix test

--- test/spec/modules/openxoutstreamBidAdapter_spec.js
+++ test/spec/modules/openxoutstreamBidAdapter_spec.js
@@ -11,6 +11,8 @@ describe('OpenXOutstreamAdapter', function () {
   const LICENSED_FORMAT_ID = '1991358644725162817';
   const YM_SCRIPT = `!function(e,t){if(void 0===t._ym){var a=Math.round(5*Math.random()/3)+'';t._ym='';var m=e.createElement('script');m.type='text/javascript',m.async=!0,m.src='//static.yieldmo.com/ym.'+a+'.js',(e.getElementsByTagName('head')[0]||e.getElementsByTagName('body')[0]).appendChild(m)}else t._ym instanceof String||void 0===t._ym.chkPls||t._ym.chkPls()}(document,window);`;
   const PUBLISHER_ID = '1986307525700126029';
+  const CR_ID = '1991358644725162817';
+  const AD_ID = 1991358644725162800;
 
   describe('inherited functions', function () {
     it('exists and is a function', function () {
@@ -212,14 +214,14 @@ describe('OpenXOutstreamAdapter', function () {
           ads: [
             {
               actions: {},
-              ad_id: 1991358644725162800,
-              adv_id: '1991358644725162800',
+              ad_id: AD_ID,
+              adv_id: `${AD_ID}`,
               configurables: {
                 cta_button_copy: 'Learn More',
                 vast_click_tracking: 'true',
                 vast_url: vastUrl,
               },
-              cr_id: '1991358644725162817',
+              cr_id: CR_ID,
               rti: '1',
             }
           ],
