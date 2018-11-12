e6181d8fa25db3df62c5ebd32267ccfe4e7d2a65
Manasi Moghe <manasi.moghe@pubmatic.com>
2018-11-12 15:43:56 +0530

    code review changes

--- modules/pubmaticServerBidAdapter.js
+++ modules/pubmaticServerBidAdapter.js
@@ -200,49 +200,6 @@ function logAllErrors(errors) {
   });
 }
 
-function _getDataFromImpArray (impData, id, key) {
-  for (var index in impData) {
-    if (impData[index].ext.wrapper.div === id) {
-      switch (key) {
-        case 'requestId':
-          return impData[index].id;
-        case 'width':
-          return impData[index].banner.w;
-        case 'height':
-          return impData[index].banner.h;
-      }
-    }
-  }
-}
-
-function _createDummyBids (impData, bidResponses, miObj) {
-  let bidMap = window.PWT.bidMap;
-  for (var id in bidMap) {
-    for (var adapterID in bidMap[id].adapters) {
-      if (adapterID !== 'prebid') {
-        bidResponses.push({
-          requestId: _getDataFromImpArray(impData, id, 'requestId'),
-          bidderCode: BIDDER_CODE,
-          originalBidder: adapterID,
-          pubmaticServerErrorCode: undefined,
-          width: _getDataFromImpArray(impData, id, 'width'),
-          height: _getDataFromImpArray(impData, id, 'height'),
-          creativeId: 0,
-          dealId: '',
-          currency: CURRENCY,
-          netRevenue: true,
-          ttl: 300,
-          referrer: utils.getTopWindowUrl(),
-          ad: '',
-          cpm: 0,
-          serverSideResponseTime: -1,
-          mi: miObj.hasOwnProperty(adapterID) ? miObj[adapterID] : undefined
-        });
-      }
-    }
-  }
-}
-
 export const spec = {
   code: BIDDER_CODE,
 
@@ -371,52 +328,70 @@ export const spec = {
         const miObj = (response.body.ext && response.body.ext.matchedimpression) || {};
         let requestData = JSON.parse(request.data);
 
-        _createDummyBids(requestData.imp, bidResponses, miObj);
         response.body.seatbid.forEach(function (seatbidder) {
           seatbidder.bid && seatbidder.bid.forEach(function (bid) {
             if (/* bid.id !== null && */bid.ext.summary) {
               bid.ext.summary.forEach(function (summary, index) {
                 var firstSummary = index === 0;
-                bidResponses.forEach(function (br) {
-                  if (summary.errorCode === 6) {
-                    /* special handling of error code 6, where bid objects are not present in the response.
-                       here directly use the dummy bids, just update the error code and response time
-                    */
-                    if (br.requestId == bid.impid) {
-                      br.pubmaticServerErrorCode = summary.errorCode;
-                      br.serverSideResponseTime = -1
-                    }
-                  } else if (summary.bidder) {
-                    if (br.requestId == bid.impid && br.originalBidder === summary.bidder) {
-                      br.pubmaticServerErrorCode = summary.errorCode;
-                      br.cpm = (parseFloat(summary.bid) || 0).toFixed(2);
-                      br.width = summary.width || br.width;
-                      br.height = summary.height || br.height;
-                      br.creativeId = firstSummary ? bid.crid || bid.id : bid.id;
-                      br.dealId = firstSummary ? bid.dealid || UNDEFINED : UNDEFINED;
-                      br.currency = CURRENCY;
-                      br.netRevenue = true;
-                      br.ttl = 300;
-                      br.referrer = referrer;
-                      br.ad = firstSummary ? bid.adm : '';
-                      br.serverSideResponseTime = (summary.errorCode === 1 || summary.errorCode === 2)
-                        ? -1 : summary.errorCode === 5 ? 0 : partnerResponseTimeObj[summary.bidder] || 0;
-                      /* errorCodes meaning:
-                        1 = GADS_UNMAPPED_SLOT_ERROR
-                        2 = GADS_MISSING_CONF_ERROR
-                        3 = TIMEOUT_ERROR
-                        4 = NO_BID_PREBID_ERROR
-                        5 = PARTNER_TIMEDOUT_ERROR
-                        6 = INVALID_CONFIGURATION_ERROR
-                        7 = NO_GDPR_CONSENT_ERROR
-                        500 = API_RESPONSE_ERROR
-                        - setting serverSideResponseTime as 0, in cases where partnerResponseTimeObj[summary.bidder] is not available.
-                        - setting serverSideResponseTime as -1, in cases where errorCode is 1,2 or 6. In these cases we do not log this bid in logger
-                        - explicitly setting serverSideResponseTime = 0, where errorCode is 5, i.e. PARTNER_TIMEDOUT_ERROR
-                      */
-                    }
+                let newBid = {};
+                if (summary.errorCode === undefined) {
+                  newBid = {
+                    requestId: bid.impid,
+                    bidderCode: BIDDER_CODE,
+                    originalBidder: summary.bidder,
+                    pubmaticServerErrorCode: undefined,
+                    width: summary.width,
+                    height: summary.height,
+                    creativeId: firstSummary ? (bid.crid || bid.id) : bid.id,
+                    dealId: firstSummary ? (bid.dealid || UNDEFINED) : UNDEFINED,
+                    currency: CURRENCY,
+                    netRevenue: true,
+                    ttl: 300,
+                    referrer: referrer,
+                    ad: firstSummary ? bid.adm : '',
+                    cpm: (parseFloat(summary.bid) || 0).toFixed(2),
+                    serverSideResponseTime: partnerResponseTimeObj[summary.bidder] || 0,
+                    mi: miObj.hasOwnProperty(summary.bidder) ? miObj[summary.bidder] : UNDEFINED
                   }
-                });
+                } else {
+                  requestData.imp.forEach(function(impObj) {
+                    if (impObj.id === bid.impid) {
+                      newBid = {
+                        requestId: impObj.id,
+                        bidderCode: BIDDER_CODE,
+                        originalBidder: summary.bidder,
+                        pubmaticServerErrorCode: summary.errorCode,
+                        width: impObj.banner.w,
+                        height: impObj.banner.h,
+                        creativeId: 0,
+                        dealId: '',
+                        currency: CURRENCY,
+                        netRevenue: true,
+                        ttl: 300,
+                        referrer: referrer,
+                        ad: '',
+                        cpm: 0,
+                        serverSideResponseTime: (summary.errorCode === 1 || summary.errorCode === 2 || summary.errorCode === 6) ? -1
+                          : summary.errorCode === 5 ? 0 : partnerResponseTimeObj[summary.bidder] || 0,
+                        /* errorCodes meaning:
+                            1 = GADS_UNMAPPED_SLOT_ERROR
+                            2 = GADS_MISSING_CONF_ERROR
+                            3 = TIMEOUT_ERROR
+                            4 = NO_BID_PREBID_ERROR
+                            5 = PARTNER_TIMEDOUT_ERROR
+                            6 = INVALID_CONFIGURATION_ERROR
+                            7 = NO_GDPR_CONSENT_ERROR
+                            500 = API_RESPONSE_ERROR
+                            - setting serverSideResponseTime as 0, in cases where partnerResponseTimeObj[summary.bidder] is not available.
+                            - setting serverSideResponseTime as -1, in cases where errorCode is 1,2 or 6. In these cases we do not log this bid in logger
+                            - explicitly setting serverSideResponseTime = 0, where errorCode is 5, i.e. PARTNER_TIMEDOUT_ERROR
+                        */
+                        mi: miObj.hasOwnProperty(summary.bidder) ? miObj[summary.bidder] : undefined
+                      }
+                    }
+                  });
+                }
+                bidResponses.push(newBid);
               });
             }
           });
