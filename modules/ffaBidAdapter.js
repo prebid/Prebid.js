5e29f5cdf821c8657a6e8620ff79b3c471fdbb34
Nic Gallardo <nic.gallardo@gmail.com>
2018-11-21 11:29:50 -0700

    1 % change

--- modules/ffaBidAdapter.js
+++ modules/ffaBidAdapter.js
@@ -6,12 +6,17 @@ const deviceType = !freestar.deviceInfo.device.type ? "desktop" : freestar.devic
 const ENDPOINT_URL = `${freestar.msg.dispensaryURL}/floors/v2`
 
 function diceRoll() {
-  const unluckyNumbers = [81, 14, 54, 63, 88];
-  if (unluckyNumbers.indexOf(Math.floor(Math.random() * Math.floor(99))) === -1) {
+  if (Math.floor(Math.random() * Math.floor(99)) === 51) {
     return true;
   } else {
     return false;
   }
+  // const unluckyNumbers = [81, 14, 54, 63, 88];
+  // if (unluckyNumbers.indexOf(Math.floor(Math.random() * Math.floor(99))) === -1) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 }
 
 export const spec = {
