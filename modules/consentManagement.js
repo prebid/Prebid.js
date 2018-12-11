4f91ad7e20cdb87d97f8a67d28632013e3579654
Olaf Buitelaar <olaf.buitelaar@gmail.com>
2018-12-11 22:18:55 +0100

    * check if consent hook is already set

--- modules/consentManagement.js
+++ modules/consentManagement.js
@@ -23,6 +23,7 @@ export let allowAuction;
 export let staticConsentData;
 
 let consentData;
+let addedConsentHook = false;
 
 // add new CMPs here, with their dedicated lookup function
 const cmpCallMap = {
@@ -244,7 +245,9 @@ export function requestBidsHook(reqBidsConfigObj, fn) {
     if (consentTimeout === 0) {
       processCmpData(undefined, hookConfig);
     } else {
+      //clearTimeout(hookConfig.timer);
       hookConfig.timer = setTimeout(cmpTimedOut.bind(null, hookConfig), consentTimeout);
+      utils.logInfo(`CMP framework setTimeout: ` + hookConfig.timer);
     }
   }
 }
@@ -401,6 +404,9 @@ export function setConfig(config) {
       utils.logError(`consentManagement config with cmpApi: 'static' did not specify consentData. No consents will be available to adapters.`);
     }
   }
-  $$PREBID_GLOBAL$$.requestBids.addHook(requestBidsHook, 50);
+  if(!addedConsentHook){
+    $$PREBID_GLOBAL$$.requestBids.addHook(requestBidsHook, 50);
+  }
+  addedConsentHook = true;
 }
 config.getConfig('consentManagement', config => setConfig(config.consentManagement));
