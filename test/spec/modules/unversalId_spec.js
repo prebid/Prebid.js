ba79f4f992ac8b788a7e25b0d21b826d2b2ac81d
Isaac Dettman <idettman@rubiconproject.com>
2018-12-10 16:21:12 -0800

    add local storage and cookie browser support functions

--- test/spec/modules/unversalId_spec.js
+++ test/spec/modules/unversalId_spec.js
@@ -1,36 +1,78 @@
 import {
-  requestBidHook,
-  getCookie,
-  setCookie,
-  setConfig,
-  isPubcidEnabled,
-  getExpInterval,
-  initPubcid } from 'modules/pubCommonId';
-import { getAdUnits } from 'test/fixtures/fixtures';
+  enabledStorageTypes
+} from 'modules/universalId';
+import { registerBidder } from 'src/adapters/bidderFactory';
 import * as utils from 'src/utils';
 import * as auctionModule from 'src/auction';
-import { registerBidder } from 'src/adapters/bidderFactory';
-
-var assert = require('chai').assert;
-var expect = require('chai').expect;
-
-const PUB_COMMON_ID_COOKIE_NAME = '_pubcid';
-const TIMEOUT = 2000;
+import {expect, assert} from 'chai'
 
 describe('Universal ID', function () {
+  let sandbox;
 
-  afterEach(function () {
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
   });
 
-  describe('Configuration', function () {
-    // if config is empty, universalId should not initialize
-    it('empty config', function () {
+  afterEach(function() {
+    sandbox.restore();
+  });
 
-    });
+  describe('Storage function', function() {
+    describe('enabledStorageTypes', function() {
+      it('returns array with \'localStorage\' and \'cookie\' items, if both local storage and cookies are enabled', function() {
+        const result = enabledStorageTypes({
+          cookieEnabled: true
+        }, {
+          localStorage: {
+            setItem: function(key, value) {},
+            getItem: function(key) { if (key === 'prebid.cookieTest') { return '1' } }
+          },
+          cookie: ''
+        });
+        expect(result).to.deep.equal(['localStorage', 'cookie']);
+      });
 
-    // when config is set, associated submodule properties should be updated to match
-    describe('set config values', function() {
+      it('returns array with \'localStorage\' item, if only localStorage is enabled', function() {
+        const result = enabledStorageTypes({
+          cookieEnabled: false
+        }, {
+          localStorage: {
+            setItem: function(key, value) {},
+            getItem: function(key) { if (key === 'prebid.cookieTest') { return '1' } }
+          },
+          set cookie(v) {},
+          get cookie() {
+            return ''
+          }
+        });
+        expect(result).to.deep.equal(['localStorage']);
+      });
 
-    });
+      it('returns array with \'cookie\' item, if only cookie storage is enabled', function() {
+        const result = enabledStorageTypes({
+          cookieEnabled: true
+        }, {
+          localStorage: undefined,
+          set cookie(v) {},
+          get cookie() {
+            return 'prebid.cookieTest'
+          }
+        })
+        expect(result).to.deep.equal(['cookie'])
+      });
+
+      it('returns empty array if neither local storage or cookies are not enabled', function() {
+        const result = enabledStorageTypes({
+          cookieEnabled: false
+        }, {
+          localStorage: undefined,
+          set cookie(v) {},
+          get cookie() {
+            return ''
+          }
+        });
+        expect(result).to.deep.equal([]);
+      });
+    })
   });
 });
