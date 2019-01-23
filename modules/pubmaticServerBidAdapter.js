dae10b9f865ee202972150d6ce5aaf9162e08b53
pm-shashank-jain <shashank.jain@pubmatic.com>
2019-01-23 19:40:58 +0530

    Updadted pubMatic Server adapter to have sizes in format array only as per prebid page

--- modules/pubmaticServerBidAdapter.js
+++ modules/pubmaticServerBidAdapter.js
@@ -128,11 +128,9 @@ function _createImpressionObject(bid, conf) {
     banner: {
       pos: 0,
       topframe: utils.inIframe() ? 0 : 1,
-      w: bid.sizes[0][0],
-      h: bid.sizes[0][1],
       format: (function() {
         let arr = [];
-        for (let i = 1, l = bid.sizes.length; i < l; i++) {
+        for (let i = 0, l = bid.sizes.length; i < l; i++) {
           arr.push({
             w: bid.sizes[i][0],
             h: bid.sizes[i][1]
