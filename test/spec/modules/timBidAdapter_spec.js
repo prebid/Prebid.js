dc61041833efe9dd745947316c54d604d179d4ce
boris <boris@thetimmedia.com>
2019-01-24 16:18:06 +0200

    added tests. change adapter.

--- test/spec/modules/timBidAdapter_spec.js
+++ test/spec/modules/timBidAdapter_spec.js
@@ -41,14 +41,14 @@ describe('timAdapterTests', function () {
   describe('buildRequests', function () {
     const validBidRequests = [{
       'bidder': 'tim',
-      'params': {'placementCode': 'header-bid-tag-0', 'publisherid': 'testpublisherid'},
+      'params': {'placementCode': 'placementCode', 'publisherid': 'testpublisherid'},
       'mediaTypes': {'banner': {'sizes': [[300, 250]]}},
-      'adUnitCode': '99',
-      'transactionId': '6d2fd360-90fb-482d-aa57-2337b7873f48',
+      'adUnitCode': 'adUnitCode',
+      'transactionId': 'transactionId',
       'sizes': [[300, 250]],
-      'bidId': '26351bb64298e3',
-      'bidderRequestId': '1fee45c6e599eb',
-      'auctionId': '4321e9f7-b4dc-4139-8e2d-ea932c31b46f',
+      'bidId': 'bidId',
+      'bidderRequestId': 'bidderRequestId',
+      'auctionId': 'auctionId',
       'src': 'client',
       'bidRequestsCount': 1
     }];
@@ -61,6 +61,92 @@ describe('timAdapterTests', function () {
     it('bidRequest url', function () {
       const requests = spec.buildRequests(validBidRequests);
       expect(requests[0].url).to.exists;
+    });
+
+    it('bidRequest data', function () {
+      const requests = spec.buildRequests(validBidRequests);
+      expect(requests[0].data).to.exists;
+    });
+
+    it('bidRequest options', function () {
+      const requests = spec.buildRequests(validBidRequests);
+      expect(requests[0].options).to.exists;
+    });
+  });
+
+  describe('interpretResponse', function () {
+    const bidRequest = {
+      'method': 'GET',
+      'url': '//bidder.url/api/prebid/testpublisherid/header-bid-tag-0?br=%7B%22id%22%3A%223a3ac0d7fc2548%22%2C%22imp%22%3A%5B%7B%22id%22%3A%22251b8a6d3aac3e%22%2C%22banner%22%3A%7B%22w%22%3A300%2C%22h%22%3A250%7D%2C%22tagid%22%3A%22header-bid-tag-0%22%7D%5D%2C%22site%22%3A%7B%22domain%22%3A%22www.chinatimes.com%22%2C%22page%22%3A%22http%3A%2F%2Fwww.chinatimes.com%2Fa%22%2C%22publisher%22%3A%7B%22id%22%3A%22testpublisherid%22%7D%7D%2C%22device%22%3A%7B%22language%22%3A%22en%22%2C%22w%22%3A300%2C%22h%22%3A250%2C%22js%22%3A1%2C%22ua%22%3A%22Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F71.0.3578.98%20Safari%2F537.36%22%7D%2C%22bidId%22%3A%22251b8a6d3aac3e%22%7D',
+      'data': '',
+      'options': {'withCredentials': false}
+    };
+
+    const serverResponse = {
+      'body': {
+        'id': 'id',
+        'seatbid': []
+      },
+      'headers': {}
+    };
+
+    it('check empty array response', function () {
+      const result = spec.interpretResponse(serverResponse, bidRequest);
+      expect(result).to.deep.equal([]);
+    });
+
+    const validBidRequest = {
+      'method': 'GET',
+      'url': '//bidder.url/api/v2/services/prebid/testpublisherid/placementCodeTest?br=%7B%22id%22%3A%2248640869bd9db94%22%2C%22imp%22%3A%5B%7B%22id%22%3A%224746fcaa11197f3%22%2C%22banner%22%3A%7B%22w%22%3A300%2C%22h%22%3A250%7D%2C%22tagid%22%3A%22placementCodeTest%22%7D%5D%2C%22site%22%3A%7B%22domain%22%3A%22mediamart.tv%22%2C%22page%22%3A%22http%3A%2F%2Fmediamart.tv%2Fsas%2Ftests%2FDesktop%2Fcaesar%2Fdfptest.html%22%2C%22publisher%22%3A%7B%22id%22%3A%22testpublisherid%22%7D%7D%2C%22device%22%3A%7B%22language%22%3A%22en%22%2C%22w%22%3A300%2C%22h%22%3A250%2C%22js%22%3A1%2C%22ua%22%3A%22Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F71.0.3578.98%20Safari%2F537.36%22%7D%2C%22bidId%22%3A%224746fcaa11197f3%22%7D',
+      'data': '',
+      'options': {'withCredentials': false}
+    };
+    const validServerResponse = {
+      'body': {'id': 'id',
+        'seatbid': [
+          {'bid': [{'id': 'id',
+            'impid': 'impid',
+            'price': 3,
+            'nurl': 'https://bidder.url/api/v1/?price=${AUCTION_PRICE}&bidcur=USD&bidid=bidid=true',
+            'adm': '<script type=\"text/javascript\" src=\"https://domain.com/Player.php"></script>',
+            'adomain': [''],
+            'cid': '1',
+            'crid': '700',
+            'w': 300,
+            'h': 250
+          }]}],
+        'bidid': 'bidid',
+        'cur': 'USD'
+      },
+      'headers': {}
+    };
+    it('required keys', function () {
+      const result = spec.interpretResponse(validServerResponse, validBidRequest);
+
+      let requiredKeys = [
+        'requestId',
+        'creativeId',
+        'adId',
+        'cpm',
+        'width',
+        'height',
+        'currency',
+        'netRevenue',
+        'ttl',
+        'ad'
+      ];
+
+      let resultKeys = Object.keys(result[0]);
+      requiredKeys.forEach(function(key) {
+        expect(resultKeys.indexOf(key) !== -1).to.equal(true);
+      });
+    })
+  });
+
+  describe('getUserSyncs', function () {
+    it('check empty response getUserSyncs', function () {
+      const result = spec.getUserSyncs('', '');
+      expect(result).to.deep.equal([]);
     });
   });
 });
