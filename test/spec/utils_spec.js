1282028c51b1e1f0d17ca5f13fba55742aee0219
Kelvin Chappell <kelvin.chappell@theguardian.com>
2019-01-29 09:00:51 +0000

    Remove redundant test

--- test/spec/utils_spec.js
+++ test/spec/utils_spec.js
@@ -913,15 +913,6 @@ describe('Utils', function () {
         key: 'test2',
         value: ['']
       }]);
-
-      describe('insertElement', function () {
-        it('returns a node at bottom of head if no target is given', function () {
-          const toInsert = document.createElement('div');
-          const head = document.getElementsByTagName('head')[0];
-          const inserted = utils.insertElement(toInsert);
-          expect(inserted).to.equal(head.lastChild);
-        });
-      });
     });
 
     describe('insertElement', function () {
