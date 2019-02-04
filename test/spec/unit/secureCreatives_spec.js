e2df15251af1d59aacb63e545debf821d0d56b87
Jonathan Go <jgo@sonobi.com>
2019-02-04 14:43:38 -0500

    added more realistic mock values

--- test/spec/unit/secureCreatives_spec.js
+++ test/spec/unit/secureCreatives_spec.js
@@ -22,13 +22,13 @@ describe('secureCreatives', () => {
       window.googletag = null;
       const mockAdObject = {
         adId: 'someAdId',
-        ad: 'http://prebid.org/creative/${AUCTION_PRICE}',
+        ad: '<script src="http://prebid.org/creative/${AUCTION_PRICE}"></script>',
         adUrl: 'http://creative.prebid.org/${AUCTION_PRICE}',
         width: 300,
         height: 250,
         renderer: null,
         cpm: '1.00',
-        adUnitCode: 'context'
+        adUnitCode: 'some_dom_id'
       };
       const remoteDomain = '*';
       const source = {
@@ -36,7 +36,7 @@ describe('secureCreatives', () => {
       };
 
       _sendAdToCreative(mockAdObject, remoteDomain, source);
-      expect(JSON.parse(source.postMessage.args[0][0]).ad).to.equal('http://prebid.org/creative/1.00');
+      expect(JSON.parse(source.postMessage.args[0][0]).ad).to.equal('<script src="http://prebid.org/creative/1.00"></script>');
       expect(JSON.parse(source.postMessage.args[0][0]).adUrl).to.equal('http://creative.prebid.org/1.00');
       window.googletag = oldVal;
       window.apntag = oldapntag;
