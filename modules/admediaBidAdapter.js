8a52177fad8bcae471a949e02a521b63953d7442
roman <elardzhi@gmail.com>
2019-02-05 11:22:52 -0800

    Removed a commented line

--- modules/admediaBidAdapter.js
+++ modules/admediaBidAdapter.js
@@ -1,6 +1,6 @@
 import * as utils from '../src/utils';
 import {registerBidder} from '../src/adapters/bidderFactory';
-// import { config } from 'src/config';
+
 const BIDDER_CODE = 'admedia';
 const ENDPOINT_URL = '//prebid.admedia.com/bidder/';
 
