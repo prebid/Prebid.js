7b22230cb324a94b1f7c2cb536032fc0ae737ab5
Afsheen Bigdeli <afsheenb@namecast.net>
2019-02-05 08:51:23 -0500

    updated ozone unit tests

--- test/spec/modules/ozoneBidAdapter_spec.js
+++ test/spec/modules/ozoneBidAdapter_spec.js
@@ -3,11 +3,7 @@ import { spec } from 'modules/ozoneBidAdapter';
 
 const OZONEURI = 'https://elb.the-ozone-project.com/openrtb2/auction';
 const BIDDER_CODE = 'ozone';
-/*
 
-NOTE - use firefox console to deep copy the objects to use here
-
- */
 var validBidRequests = [
   {
     adUnitCode: 'div-gpt-ad-1460505748561-0',
@@ -99,6 +95,242 @@ var validResponse = {
     }
   },
   'headers': {}
+}
+
+var validResponse2Ads2Bidders = {
+  'body': {
+    'id': '81fab647-f3e4-4262-a618-231c0dfbba19',
+    'seatbid': [
+      {
+        'bid': [
+          {
+            'id': '2528653442263849051',
+            'impid': '246ecf3443c17e8',
+            'price': 0.25493,
+            'adm': '<script src="http://ams1-ib.adnxs.com/ab?referrer=http%3A%2F%2Ftpdads.com%2Fadaptortest.html&e=wqT_3QLhCOhhBAAAAwDWAAUBCIXUnuAFEMnNkbjJuLa0Zhj_lofznozw9HIqNgniOzHrxVDQPxHiOzHrxVDQPxkAAAkCACERGwApEQkAMQkZsAAAMNz46AY4-01A-01IAlD-84g5WN_0aGAAaLrQggF4uZAFgAEBigEDVVNEkgUG8HKYAawCoAH6AagBAbABALgBAsABBMgBAtABANgBAOABAPABAIoCkwF1ZignYScsIDI4OTU5NDAsIDE1NDQwMDYxNDkpO3VmKCdyJywgMTE5NjgzNTgyLCAxNTQ0MDA2MTQ5KTt1ZignYycsIDI1NDg5MTQ5Rh4AKGcnLCA2ODE1MzEyRh0AIGknLCA5NTE0OTp2APCBkgKhAiE5MGFpRGdqOTNaTU1FUDd6aURrWUFDRGY5R2d3QURnQVFBUkktMDFRM1Bqb0JsZ0FZUF9fX184UGFBQndBWGdCZ0FFQmlBRUJrQUVCbUFFQm9BRUJxQUVCc0FFQXVRRjFxdzFzbXBuSlA4RUJDLWV2MDlKUTBEX0pBUUFBQQEDqFBBXzJRRjcydUd2eVJycFAtQUJ3b2s2OVFITnpFdy1tQUlBb0FJQXRRSUEBLwR2UQkI6HdBSUJ5QUlCMEFJQjJBSUI0QUlBNkFJQS1BSUFnQU1Ca0FNQW1BTUJvZ01YQ0w2MnpRTVFBaGdCTFFBAUfIeUIyUmxabUYxYkhTb0FfM2Rrd3k2QXdsQlRWTXhPalF4TmpUZ0E2SUOaAmEhMkJVb3JnNiQBNDNfUm9JQVFvQURHYW1aAQKIbkpQem9KUVUxVE1UbzBNVFkwUUtJQ1NYdmE0YV9KR3VrX1UJfAUBAFcdDPBI2AIA4AKyrkzqAiJodHRwOi8vdHBkYWRzLmNvbS9hZGFwdG9ydGVzdC5odG1s8gITCg9DVVNUT01fTU9ERUxfSUQSAPICGgoWQy4WACBMRUFGX05BTUUBHQweChpDMh0A8JlBU1RfTU9ESUZJRUQSAIADAIgDAZADAJgDFKADAaoDAMADrALIAwDYAwDgAwDoAwD4AwOABACSBAkvb3BlbnJ0YjKYBACiBAw4MS4xNTAuMC4xMTGoBACyBAwIABAAGAAgADAAOAC4BADABADIBADSBA45OTc5I0FNUzE6NDE2NNoEAggB4AQA8AT-84g5iAUBmAUAoAX_____BQOwAaoFJDgxZmFiNjQ3LWYzZTQtNDI2Mi1hNjE4LTIzMWMwZGZiYmExOcAFAMkFaboU8D_SBQkJCQxwAADYBQHgBQHwBQH6BQQIABAAkAYAmAYAuAYAwQYJIyTwP8gGANoGFgoQCRA0AAAAAAAAAAAAABAAGAA.&s=301f133a55b6a9caf890b863f956de2b60ed2b53&pp=${AUCTION_PRICE}"></script>',
+            'adid': '119683582',
+            'adomain': [
+              'appnexus.com'
+            ],
+            'iurl': 'http://ams1-ib.adnxs.com/cr?id=119683582',
+            'cid': '9979',
+            'crid': '119683582',
+            'w': 300,
+            'h': 250,
+            'ext': {
+              'prebid': {
+                'type': 'banner'
+              },
+              'bidder': {
+                'appnexus': {
+                  'brand_id': 1,
+                  'auction_id': 7379387427817023000,
+                  'bidder_id': 2,
+                  'bid_ad_type': 0
+                }
+              }
+            }
+          },
+          {
+            'id': '452175203342206024',
+            'impid': '371102c16611c18',
+            'price': 0.25493,
+            'adm': '<script src="http://ams1-ib.adnxs.com/ab?referrer=http%3A%2F%2Ftpdads.com%2Fadaptortest.html&e=wqT_3QLhCOhhBAAAAwDWAAUBCIXUnuAFEOL4g7z--eidUxj_lofznozw9HIqNgniOzHrxVDQPxHiOzHrxVDQPxkAAAkCACERGwApEQkAMQkZsAAAMNz46AY4-01A-01IAlCQlKc5WN_0aGAAaLrQggF4uZAFgAEBigEDVVNEkgUG8HKYAcoHoAH6AagBAbABALgBAsABBMgBAtABANgBAOABAPABAIoCkwF1ZignYScsIDI4OTU5NDAsIDE1NDQwMDYxNDkpO3VmKCdyJywgMTIwMTc5MjE2LCAxNTQ0MDA2MTQ5KTt1ZignYycsIDI2MDQzMTE1Rh4AKGcnLCA2OTgxNzg5Rh0AIGknLCA5NTE0OTp2APCBkgKhAiFWMFljZ0FqcnhiVU1FSkNVcHprWUFDRGY5R2d3QURnQVFBUkktMDFRM1Bqb0JsZ0FZUF9fX184UGFBQndBWGdCZ0FFQmlBRUJrQUVCbUFFQm9BRUJxQUVCc0FFQXVRRjFxdzFzbXBuSlA4RUJDLWV2MDlKUTBEX0pBUUFBQQEDqFBBXzJRRjcydUd2eVJycFAtQUJ3b2s2OVFITnpFdy1tQUlBb0FJQXRRSUEBLwR2UQkI6HdBSUJ5QUlCMEFJQjJBSUI0QUlBNkFJQS1BSUFnQU1Ca0FNQW1BTUJvZ01YQ05xTzl3TVFBaGdCTFFBAUfIeUIyUmxabUYxYkhTb0EtdkZ0UXk2QXdsQlRWTXhPalF4TmpUZ0E2SUOaAmEhSWhYaGdRNiQBNDNfUm9JQVFvQURHYW1aAQKIbkpQem9KUVUxVE1UbzBNVFkwUUtJQ1NYdmE0YV9KR3VrX1UJfAUBAFcdDPBI2AIA4AKyrkzqAiJodHRwOi8vdHBkYWRzLmNvbS9hZGFwdG9ydGVzdC5odG1s8gITCg9DVVNUT01fTU9ERUxfSUQSAPICGgoWQy4WACBMRUFGX05BTUUBHQweChpDMh0A8JlBU1RfTU9ESUZJRUQSAIADAIgDAZADAJgDFKADAaoDAMADrALIAwDYAwDgAwDoAwD4AwOABACSBAkvb3BlbnJ0YjKYBACiBAw4MS4xNTAuMC4xMTGoBACyBAwIABAAGAAgADAAOAC4BADABADIBADSBA45OTc5I0FNUzE6NDE2NNoEAggB4AQA8ASQlKc5iAUBmAUAoAX_____BQOwAaoFJDgxZmFiNjQ3LWYzZTQtNDI2Mi1hNjE4LTIzMWMwZGZiYmExOcAFAMkFaboU8D_SBQkJCQxwAADYBQHgBQHwBQH6BQQIABAAkAYAmAYAuAYAwQYJIyTwP8gGANoGFgoQCRA0AAAAAAAAAAAAABAAGAA.&s=47aa373759af2f4d3ae5f41b8813da8d2a80dbf1&pp=${AUCTION_PRICE}"></script>',
+            'adid': '120179216',
+            'adomain': [
+              'appnexus.com'
+            ],
+            'iurl': 'http://ams1-ib.adnxs.com/cr?id=120179216',
+            'cid': '9979',
+            'crid': '120179216',
+            'w': 970,
+            'h': 250,
+            'ext': {
+              'prebid': {
+                'type': 'banner'
+              },
+              'bidder': {
+                'appnexus': {
+                  'brand_id': 1,
+                  'auction_id': 5997567442111495000,
+                  'bidder_id': 2,
+                  'bid_ad_type': 0
+                }
+              }
+            }
+          }
+        ],
+        'seat': 'appnexus'
+      },
+      {
+        'bid': [
+          {
+            'id': '6253a6e7-dccf-4674-a1ab-a267fceb88f1',
+            'impid': '246ecf3443c17e8',
+            'price': 0.01,
+            'adm': '<div id="beacon_9436" style="position:absolute;left:0px;top:0px;visibility:hidden;"><img src="https://rtb-xa.openx.net/win/prebid?p=FIRST&t=2DAABBgABAAECAAIBAAsAAgAAAJgcGAo4QmpDbUdNRVFNHBbDzL3fk8q5rDQW7fLe5MrihOfrAQAcFuiZ-sz7udPTxAEWndyjsYDmrtS8AQAWhqi9wAsVBgAsHBUCABwVAgAAHCbu-ZiDBBUEFQQm6vmYgwQW3OCegATWFAAcJsSqn4AEFrLVtYAEFuzRmoEEFqD03YAEFQIcFPQDFNgEABUEFQoWFCYURQoAAAA&ph=a3aece0c-9e80-4316-8deb-faf804779bd1"/></div><a href="http://sademo-d.openx.net/w/1.0/rc?ts=2DAABBgABAAECAAIBAAsAAgAAAJgcGAo4QmpDbUdNRVFNHBbDzL3fk8q5rDQW7fLe5MrihOfrAQAcFuiZ-sz7udPTxAEWndyjsYDmrtS8AQAWhqi9wAsVBgAsHBUCABwVAgAAHCbu-ZiDBBUEFQQm6vmYgwQW3OCegATWFAAcJsSqn4AEFrLVtYAEFuzRmoEEFqD03YAEFQIcFPQDFNgEABUEFQoWFCYURQoAAAA" target="_blank">\n  <img src="http://ox-i.sademo.servedbyopenx.com/a3a/a3aece0c-9e80-4316-8deb-faf804779bd1/80e/80eeedd896f54edfa01c82fdb6a372b7.jpg" height="250" width="300" border="0" alt=""/>\n</a>\n<div id="beacon_9165" style="position: absolute; left: 0px; top: 0px; visibility: hidden;">\n  <img src="http://sademo-d.openx.net/w/1.0/rr?ts=2DAABBgABAAECAAIBAAsAAgAAAJgcGAo4QmpDbUdNRVFNHBbDzL3fk8q5rDQW7fLe5MrihOfrAQAcFuiZ-sz7udPTxAEWndyjsYDmrtS8AQAWhqi9wAsVBgAsHBUCABwVAgAAHCbu-ZiDBBUEFQQm6vmYgwQW3OCegATWFAAcJsSqn4AEFrLVtYAEFuzRmoEEFqD03YAEFQIcFPQDFNgEABUEFQoWFCYURQoAAAA"/>\n</div>\n<iframe src="https://us-u.openx.net/w/1.0/pd?plm=6&ph=a3aece0c-9e80-4316-8deb-faf804779bd1" width="0" height="0" style="display:none;"></iframe>',
+            'crid': '538137718',
+            'w': 300,
+            'h': 250,
+            'ext': {
+              'prebid': {
+                'type': 'banner'
+              }
+            }
+          },
+          {
+            'id': '5023e9fd-9aa6-4e23-a747-9ebe84886b66',
+            'impid': '371102c16611c18',
+            'price': 0.01,
+            'adm': '<div id="beacon_9450" style="position:absolute;left:0px;top:0px;visibility:hidden;"><img src="https://rtb-xa.openx.net/win/prebid?p=FIRST&t=2DAABBgABAAECAAIBAAsAAgAAAJgcGApTWGY4Y0YwcUxmHBbDzL3fk8q5rDQW7fLe5MrihOfrAQAcFsa4sqqz__SjoAEWs9K8t6_QsLixAQAWhqi9wAsVBgAsHBUCABwVAgAAHCbu-ZiDBBUEFQQm6vmYgwQW3OCegATWFAAcJsSqn4AEFrLVtYAEFurRmoEEFqD03YAEFQIcFLQBFLALABUEFQoWFCYURQoAAAA&ph=a3aece0c-9e80-4316-8deb-faf804779bd1"/></div><a href="http://sademo-d.openx.net/w/1.0/rc?ts=2DAABBgABAAECAAIBAAsAAgAAAJgcGApTWGY4Y0YwcUxmHBbDzL3fk8q5rDQW7fLe5MrihOfrAQAcFsa4sqqz__SjoAEWs9K8t6_QsLixAQAWhqi9wAsVBgAsHBUCABwVAgAAHCbu-ZiDBBUEFQQm6vmYgwQW3OCegATWFAAcJsSqn4AEFrLVtYAEFurRmoEEFqD03YAEFQIcFLQBFLALABUEFQoWFCYURQoAAAA" target="_blank">\n  <img src="http://ox-i.sademo.servedbyopenx.com/a3a/a3aece0c-9e80-4316-8deb-faf804779bd1/0c8/0c8dd68f50234093bc08b7339ee0093d.jpg" height="90" width="728" border="0" alt=""/>\n</a>\n<div id="beacon_9308" style="position: absolute; left: 0px; top: 0px; visibility: hidden;">\n  <img src="http://sademo-d.openx.net/w/1.0/rr?ts=2DAABBgABAAECAAIBAAsAAgAAAJgcGApTWGY4Y0YwcUxmHBbDzL3fk8q5rDQW7fLe5MrihOfrAQAcFsa4sqqz__SjoAEWs9K8t6_QsLixAQAWhqi9wAsVBgAsHBUCABwVAgAAHCbu-ZiDBBUEFQQm6vmYgwQW3OCegATWFAAcJsSqn4AEFrLVtYAEFurRmoEEFqD03YAEFQIcFLQBFLALABUEFQoWFCYURQoAAAA"/>\n</div>\n<iframe src="https://us-u.openx.net/w/1.0/pd?plm=6&ph=a3aece0c-9e80-4316-8deb-faf804779bd1" width="0" height="0" style="display:none;"></iframe>',
+            'crid': '538137717',
+            'w': 728,
+            'h': 90,
+            'ext': {
+              'prebid': {
+                'type': 'banner'
+              }
+            }
+          }
+        ],
+        'seat': 'openx'
+      }
+    ],
+    'ext': {
+      'responsetimemillis': {
+        'appnexus': 21,
+        'openx': 123
+      }
+    }
+  },
+  'headers': {}
+}
+
+var validRequest2Ads2Bidders = {
+  'method': 'POST',
+  'url': 'https://elb.the-ozone-project.com/openrtb2/auction',
+  'data': '{"publisherId":"OZONENUK0001","siteId":"4204204201","placementId":"0420420421","id":"81fab647-f3e4-4262-a618-231c0dfbba19","auctionId":"d44d384b-7dd8-49bb-8dab-164fae1d6555","imp":[{"id":"246ecf3443c17e8","tagid":"undefined","secure":0,"banner":{"topframe":1,"w":300,"h":250,"format":[{"w":300,"h":250},{"w":300,"h":600}]},"publisherId":"OZONENUK0001","siteId":"4204204201","ext":{"prebid":{"storedrequest":{"id":"0420420421"}}}},{"id":"371102c16611c18","tagid":"undefined","secure":0,"banner":{"topframe":1,"w":728,"h":90,"format":[{"w":728,"h":90},{"w":970,"h":250}]},"publisherId":"OZONENUK0001","siteId":"4204204201","ext":{"prebid":{"storedrequest":{"id":"0420420421"}}}}]}',
+  'bidderRequest': {
+    'bidderCode': 'ozone',
+    'auctionId': 'd44d384b-7dd8-49bb-8dab-164fae1d6555',
+    'bidderRequestId': '10ad92670f58ba8',
+    'bids': [
+      {
+        'bidder': 'ozone',
+        'params': {
+          'publisherId': 'OZONENUK0001',
+          'siteId': '4204204201',
+          'placementId': '0420420421',
+          'id': '81fab647-f3e4-4262-a618-231c0dfbba19',
+          'auctionId': 'd44d384b-7dd8-49bb-8dab-164fae1d6555',
+          'imp': [
+            {
+              'id': '246ecf3443c17e8',
+              'tagid': 'undefined',
+              'secure': 0,
+              'banner': {
+                'topframe': 1,
+                'w': 300,
+                'h': 250,
+                'format': [
+                  {
+                    'w': 300,
+                    'h': 250
+                  },
+                  {
+                    'w': 300,
+                    'h': 600
+                  }
+                ]
+              },
+              'publisherId': 'OZONENUK0001',
+              'siteId': '4204204201',
+              'ext': {
+                'prebid': {
+                  'storedrequest': {
+                    'id': '0420420421'
+                  }
+                }
+              }
+            },
+            {
+              'id': '371102c16611c18',
+              'tagid': 'undefined',
+              'secure': 0,
+              'banner': {
+                'topframe': 1,
+                'w': 728,
+                'h': 90,
+                'format': [
+                  {
+                    'w': 728,
+                    'h': 90
+                  },
+                  {
+                    'w': 970,
+                    'h': 250
+                  }
+                ]
+              },
+              'publisherId': 'OZONENUK0001',
+              'siteId': '4204204201',
+              'ext': {
+                'prebid': {
+                  'storedrequest': {
+                    'id': '0420420421'
+                  }
+                }
+              }
+            }
+          ]
+        },
+        'adUnitCode': 'mpu',
+        'transactionId': '05c75220-47e6-4f1a-9e17-07c60fae2cc9',
+        'sizes': [
+          [
+            300,
+            250
+          ],
+          [
+            300,
+            600
+          ]
+        ],
+        'bidId': '246ecf3443c17e8',
+        'bidderRequestId': '10ad92670f58ba8',
+        'auctionId': 'd44d384b-7dd8-49bb-8dab-164fae1d6555',
+        'bidRequestsCount': 1
+      },
+      {
+        'bidder': 'ozone',
+        'params': {
+          'publisherId': 'OZONENUK0001',
+          'siteId': '4204204201',
+          'placementId': '0420420421'
+        },
+        'adUnitCode': 'leaderboard',
+        'transactionId': 'f109a297-88a8-4806-a455-079574f76271',
+        'sizes': [
+          [
+            728,
+            90
+          ],
+          [
+            970,
+            250
+          ]
+        ],
+        'bidId': '371102c16611c18',
+        'bidderRequestId': '10ad92670f58ba8',
+        'auctionId': 'd44d384b-7dd8-49bb-8dab-164fae1d6555',
+        'bidRequestsCount': 1
+      }
+    ],
+    'auctionStart': 1544006146957,
+    'timeout': 3000,
+    'start': 1544006146960,
+    'doneCbCallCount': 1
+  }
 }
 
 describe('ozone Adapter', function () {
@@ -422,15 +654,28 @@ describe('ozone Adapter', function () {
   });
 
   describe('interpretResponse', function () {
+    // new test to verify the interpretresponse code works properly with responses of [ [bidder 1, bidder 1], [bidder 2, bidder 2] ]
+    it('should return 2 appnexus winning bids', function () {
+      const result = spec.interpretResponse(validResponse2Ads2Bidders, validRequest2Ads2Bidders);
+      console.log(['interpretResponse result  = ', result]);
+      expect(result.length).to.equal(2);
+      expect(result[0].seat).to.equal('appnexus');
+      expect(result[1].seat).to.equal('appnexus');
+    });
+
     it('should build bid array', function () {
       const request = spec.buildRequests(validBidRequests, validBidderRequest);
+      console.log(['interpretResponse request  = ', request]);
       const result = spec.interpretResponse(validResponse, request);
+      console.log(['interpretResponse result  = ', result]);
       expect(result.length).to.equal(1);
     });
 
     it('should have all relevant fields', function () {
       const request = spec.buildRequests(validBidRequests, validBidderRequest);
+      console.log(['Request: ', request]);
       const result = spec.interpretResponse(validResponse, request);
+      console.log(['result[0]: ', result[0]]);
       const bid = result[0];
       expect(bid.cpm).to.equal(validResponse.body.seatbid[0].bid[0].cpm);
       expect(bid.width).to.equal(validResponse.body.seatbid[0].bid[0].width);
@@ -441,7 +686,9 @@ describe('ozone Adapter', function () {
       var validBidderRequestWithGdpr = validBidderRequest;
       validBidderRequestWithGdpr.gdprConsent = {'gdprApplies': 1, 'consentString': 'This is the gdpr consent string'};
       const request = spec.buildRequests(validBidRequests, validBidderRequestWithGdpr);
+      console.log(['interpretResponse request  = ', request]);
       const result = spec.interpretResponse(validResponse, request);
+      console.log(['interpretResponse result  = ', result]);
       expect(result.length).to.equal(1);
     });
   });
