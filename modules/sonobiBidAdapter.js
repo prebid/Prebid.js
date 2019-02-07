c08fb0e42e7bb21aaee62fb0670c19f2fa2c7ada
Isaac Dettman <idettman@rubiconproject.com>
2019-02-06 17:27:00 -0800

    renamed universalID to userId

--- modules/sonobiBidAdapter.js
+++ modules/sonobiBidAdapter.js
@@ -60,10 +60,10 @@ export const spec = {
       payload.us = config.getConfig('userSync').syncsPerBidder;
     }
 
-    if (deepAccess(validBidRequests[0], 'crumbs.pubcid') || deepAccess(validBidRequests[0], 'params.hfa')) {
+    if (deepAccess(validBidRequests[0], 'userId.pubcid')) {
+      payload.hfa = `PRE-${validBidRequests[0].userId.pubcid}`;
+    } else if (deepAccess(validBidRequests[0], 'crumbs.pubcid') || deepAccess(validBidRequests[0], 'params.hfa')) {
       payload.hfa = deepAccess(validBidRequests[0], 'params.hfa') ? deepAccess(validBidRequests[0], 'params.hfa') : `PRE-${deepAccess(validBidRequests[0], 'crumbs.pubcid')}`;
-    } else if (deepAccess(validBidRequests[0], 'universalID.pubcid')) {
-      payload.hfa = `PRE-${deepAccess(validBidRequests[0], 'universalID.pubcid')}`;
     }
 
     if (validBidRequests[0].params.referrer) {
