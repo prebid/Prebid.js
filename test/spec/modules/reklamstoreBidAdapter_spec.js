a2c245707bc3df8a139701e301904a677e5a9e65
ReklamStoreIT <it@reklamstore.com>
2019-03-14 00:44:48 +0300

    ReklamStore Bid Adapter
    ReklamStore Bid Adapter

---
+++ test/spec/modules/reklamstoreBidAdapter_spec.js
@@ -0,0 +1,57 @@
+import { expect } from 'chai';
+import { spec } from 'modules/reklamstoreBidAdapter';
+
+describe('reklamstoreBidAdapterTests', function() {
+  let bidRequestData = {
+    bids: [
+      {
+        bidder: 'reklamstore',
+        params: {
+          regionId: 532211
+        },
+        sizes: [[300, 250]]
+      }
+    ]
+  };
+  let request = [];
+
+  it('validate_params', function() {
+    expect(
+      spec.isBidRequestValid({
+        bidder: 'reklamstore',
+        params: {
+          regionId: 532211
+        }
+      })
+    ).to.equal(true);
+  });
+
+  it('validate_generated_params', function() {
+    request = spec.buildRequests(bidRequestData.bids);
+    let req_data = request[0].data;
+
+    expect(req_data.regionId).to.equal(532211);
+  });
+
+  it('validate_response_params', function() {
+    let serverResponse = {
+      body:
+        {
+          cpm: 1.2,
+          ad: 'Ad html',
+          w: 300,
+          h: 250
+        }
+    };
+
+    let bids = spec.interpretResponse(serverResponse, bidRequestData.bids[0]);
+    expect(bids).to.have.lengthOf(1);
+
+    let bid = bids[0];
+    expect(bid.ad).to.equal('Ad html');
+    expect(bid.cpm).to.equal(1.2);
+    expect(bid.width).to.equal(300);
+    expect(bid.height).to.equal(250);
+    expect(bid.currency).to.equal('USD');
+  });
+});
