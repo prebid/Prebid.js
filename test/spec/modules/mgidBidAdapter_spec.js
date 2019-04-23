30b7d450905a69be5e66c9b45c5fafc04570f6df
gaudeamus <dmitry.markelov@mgid.com>
2019-04-23 10:43:52 +0300

    native support & minor changes

--- test/spec/modules/mgidBidAdapter_spec.js
+++ test/spec/modules/mgidBidAdapter_spec.js
@@ -1,16 +1,22 @@
-import {expect} from 'chai';
+import {assert, expect} from 'chai';
 import {spec} from 'modules/mgidBidAdapter';
 import * as utils from '../../../src/utils';
 import * as urlUtils from '../../../src/url';
 
 describe('Mgid bid adapter', function () {
   let sandbox;
+  let logErrorSpy;
+  let logWarnSpy;
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
+    logErrorSpy = sinon.spy(utils, 'logError');
+    logWarnSpy = sinon.spy(utils, 'logWarn');
   });
 
   afterEach(function () {
     sandbox.restore();
+    utils.logError.restore();
+    utils.logWarn.restore();
   });
   const ua = navigator.userAgent;
   const screenHeight = screen.height;
@@ -138,6 +144,46 @@ describe('Mgid bid adapter', function () {
       const request = spec.buildRequests(bidRequests);
       expect(request.url).to.include('//newbidurl.com/1');
     });
+    it('should return overwrite default bidFloor', function () {
+      let bid = Object.assign({}, bid);
+      bid.params = {
+        bidFloor: 1.1,
+        accountId: '1',
+        placementId: '2',
+      };
+      bid.mediaTypes = {
+        banner: {
+          sizes: [[300, 250]]
+        }
+      };
+      let bidRequests = [bid];
+      const request = spec.buildRequests(bidRequests);
+      expect(request.data).to.be.a('string');
+      const data = JSON.parse(request.data);
+      expect(data).to.be.a('object');
+      expect(data.imp).to.be.a('array');
+      expect(data.imp).to.have.lengthOf(1);
+      expect(data.imp[0].bidFloor).to.deep.equal(1.1);
+    });
+    it('should return overwrite default currency', function () {
+      let bid = Object.assign({}, bid);
+      bid.params = {
+        cur: 'GBP',
+        accountId: '1',
+        placementId: '2',
+      };
+      bid.mediaTypes = {
+        banner: {
+          sizes: [[300, 250]]
+        }
+      };
+      let bidRequests = [bid];
+      const request = spec.buildRequests(bidRequests);
+      expect(request.data).to.be.a('string');
+      const data = JSON.parse(request.data);
+      expect(data).to.be.a('object');
+      expect(data.cur).to.deep.equal(['GBP']);
+    });
   });
 
   describe('buildRequests', function () {
@@ -148,7 +194,7 @@ describe('Mgid bid adapter', function () {
         placementId: '2',
       },
     };
-    it('should return proper imp', function () {
+    it('should return proper banner imp', function () {
       let bid = Object.assign({}, abid);
       bid.mediaTypes = {
         banner: {
@@ -159,7 +205,7 @@ describe('Mgid bid adapter', function () {
       const referer = utils.deepAccess(bidRequests, 'refererInfo.referer');
       const domain = urlUtils.parse(referer).hostname;
       const request = spec.buildRequests(bidRequests);
-      expect(request.url).deep.equal('//dsp.mgid.com/prebid/1');
+      expect(request.url).deep.equal('https://prebid.mgid.com/prebid/1');
       expect(request.method).deep.equal('POST');
       const data = JSON.parse(request.data);
       expect(data.site.domain).to.deep.equal(domain);
@@ -174,11 +220,59 @@ describe('Mgid bid adapter', function () {
       expect(data.imp[0].secure).to.deep.equal(secure);
       expect(request).to.deep.equal({
         'method': 'POST',
-        'url': '//dsp.mgid.com/prebid/1',
-        'data': '{\"site\":{\"domain\":\"' + domain + '\"},\"cur\":[\"USD\"],\"device\":{\"ua\":\"' + ua + '\",\"js\":1,\"dnt\":' + dnt + ',\"h\":' + screenHeight + ',\"w\":' + screenWidth + ',\"language\":\"' + lang + '\"},\"user\":{},\"regs\":{},\"ext\":{\"mgid_ver\":\"' + mgid_ver + '\",\"prebid_ver\":\"' + prebid_ver + '\"},\"imp\":[{\"tagid\":\"2\",\"banner\":{\"w\":300,\"h\":250,\"format\":[]},\"secure\":' + secure + '}]}',
+        'url': 'https://prebid.mgid.com/prebid/1',
+        'data': '{\"site\":{\"domain\":\"' + domain + '\"},\"cur\":[\"USD\"],\"device\":{\"ua\":\"' + ua + '\",\"js\":1,\"dnt\":' + dnt + ',\"h\":' + screenHeight + ',\"w\":' + screenWidth + ',\"language\":\"' + lang + '\"},\"user\":{},\"regs\":{},\"ext\":{\"mgid_ver\":\"' + mgid_ver + '\",\"prebid_ver\":\"' + prebid_ver + '\"},\"imp\":[{\"tagid\":\"2\",\"secure\":' + secure + ',\"banner\":{\"w\":300,\"h\":250,\"format\":[]}}]}',
       });
     });
-    it('should return proper request', function () {
+    it('should not return native imp if minimum asset list not requested', function () {
+      let bid = Object.assign({}, abid);
+      bid.mediaTypes = {
+        native: '',
+      };
+      bid.nativeParams = {
+        title: {required: true},
+        image: {sizes: [80, 80]},
+      };
+      let bidRequests = [bid];
+      const request = spec.buildRequests(bidRequests);
+      expect(request).to.be.undefined;
+    });
+    it('should return proper native imp', function () {
+      let bid = Object.assign({}, abid);
+      bid.mediaTypes = {
+        native: '',
+      };
+      bid.nativeParams = {
+        title: {required: true},
+        image: {sizes: [80, 80]},
+        sponsored: { },
+      };
+
+      let bidRequests = [bid];
+      const referer = utils.deepAccess(bidRequests, 'refererInfo.referer');
+      const domain = urlUtils.parse(referer).hostname;
+      const request = spec.buildRequests(bidRequests);
+      expect(request).to.be.a('object');
+      expect(request.url).deep.equal('https://prebid.mgid.com/prebid/1');
+      expect(request.method).deep.equal('POST');
+      const data = JSON.parse(request.data);
+      expect(data.site.domain).to.deep.equal(domain);
+      expect(data.cur).to.deep.equal(['USD']);
+      expect(data.device.ua).to.deep.equal(ua);
+      expect(data.device.dnt).equal(dnt);
+      expect(data.device.h).equal(screenHeight);
+      expect(data.device.w).equal(screenWidth);
+      expect(data.device.language).to.deep.equal(lang);
+      expect(data.imp[0].tagid).to.deep.equal('2');
+      expect(data.imp[0].native).is.a('object').and.to.deep.equal({'request': {'assets': [{'id': 1, 'required': 1, 'title': {'len': 80}}, {'id': 2, 'img': {'h': 80, 'type': 3, 'w': 80}, 'required': 0}, {'data': {'type': 1}, 'id': 11, 'required': 0}], 'plcmtcnt': 1}});
+      expect(data.imp[0].secure).to.deep.equal(secure);
+      expect(request).to.deep.equal({
+        'method': 'POST',
+        'url': 'https://prebid.mgid.com/prebid/1',
+        'data': '{\"site\":{\"domain\":\"' + domain + '\"},\"cur\":[\"USD\"],\"device\":{\"ua\":\"' + ua + '\",\"js\":1,\"dnt\":' + dnt + ',\"h\":' + screenHeight + ',\"w\":' + screenWidth + ',\"language\":\"' + lang + '\"},\"user\":{},\"regs\":{},\"ext\":{\"mgid_ver\":\"' + mgid_ver + '\",\"prebid_ver\":\"' + prebid_ver + '\"},\"imp\":[{\"tagid\":\"2\",\"secure\":' + secure + ',\"native\":{\"request\":{\"plcmtcnt\":1,\"assets\":[{\"id\":1,\"required\":1,\"title\":{\"len\":80}},{\"id\":2,\"required\":0,\"img\":{\"type\":3,\"w\":80,\"h\":80}},{\"id\":11,\"required\":0,\"data\":{\"type\":1}}]}}}]}',
+      });
+    });
+    it('should return proper banner request', function () {
       let bid = Object.assign({}, abid);
       bid.mediaTypes = {
         banner: {
@@ -190,7 +284,7 @@ describe('Mgid bid adapter', function () {
 
       const referer = utils.deepAccess(bidRequests, 'refererInfo.referer');
       const domain = urlUtils.parse(referer).hostname;
-      expect(request.url).deep.equal('//dsp.mgid.com/prebid/1');
+      expect(request.url).deep.equal('https://prebid.mgid.com/prebid/1');
       expect(request.method).deep.equal('POST');
       const data = JSON.parse(request.data);
       expect(data.site.domain).to.deep.equal(domain);
@@ -206,19 +300,19 @@ describe('Mgid bid adapter', function () {
 
       expect(request).to.deep.equal({
         'method': 'POST',
-        'url': '//dsp.mgid.com/prebid/1',
-        'data': '{\"site\":{\"domain\":\"' + domain + '\"},\"cur\":[\"USD\"],\"device\":{\"ua\":\"' + ua + '\",\"js\":1,\"dnt\":' + dnt + ',\"h\":' + screenHeight + ',\"w\":' + screenWidth + ',\"language\":\"' + lang + '\"},\"user\":{},\"regs\":{},\"ext\":{\"mgid_ver\":\"' + mgid_ver + '\",\"prebid_ver\":\"' + prebid_ver + '\"},\"imp\":[{\"tagid\":\"2\",\"banner\":{\"w\":300,\"h\":600,\"format\":[{\"w\":300,\"h\":600},{\"w\":300,\"h\":250}]},\"secure\":' + secure + '}]}',
+        'url': 'https://prebid.mgid.com/prebid/1',
+        'data': '{\"site\":{\"domain\":\"' + domain + '\"},\"cur\":[\"USD\"],\"device\":{\"ua\":\"' + ua + '\",\"js\":1,\"dnt\":' + dnt + ',\"h\":' + screenHeight + ',\"w\":' + screenWidth + ',\"language\":\"' + lang + '\"},\"user\":{},\"regs\":{},\"ext\":{\"mgid_ver\":\"' + mgid_ver + '\",\"prebid_ver\":\"' + prebid_ver + '\"},\"imp\":[{\"tagid\":\"2\",\"secure\":' + secure + ',\"banner\":{\"w\":300,\"h\":600,\"format\":[{\"w\":300,\"h\":600},{\"w\":300,\"h\":250}]}}]}',
       });
     });
   });
-  describe('interpretResponse', function () {
+  describe('interpretResponse banner', function () {
     it('should not push bid response', function () {
       let bids = spec.interpretResponse();
-      expect(bids).to.deep.equal([]);
+      expect(bids).to.be.undefined;
     });
-    it('should push proper bid response', function () {
+    it('should push proper banner bid response', function () {
       let resp = {
-        body: {'id': '57c0c2b1b732ca', 'bidid': '57c0c2b1b732ca', 'cur': 'USD', 'seatbid': [{'bid': [{'price': 1.5, 'h': 600, 'w': 300, 'id': '1', 'impid': '61e40632c53fc2', 'adid': '2898532/2419121/2592854/2499195', 'nurl': 'http: nurl', 'burl': 'http: nurl', 'adm': 'html: adm', 'cid': '44082', 'crid': '2898532/2419121/2592854/2499195', 'cat': ['IAB7', 'IAB14', 'IAB18-3', 'IAB1-2']}], 'seat': '44082'}]}
+        body: {'id': '57c0c2b1b732ca', 'bidid': '57c0c2b1b732ca', 'cur': 'USD', 'seatbid': [{'bid': [{'price': 1.5, 'h': 600, 'w': 300, 'id': '1', 'impid': '61e40632c53fc2', 'adid': '2898532/2419121/2592854/2499195', 'nurl': 'http: nurl', 'burl': 'http: burl', 'adm': 'html: adm', 'cid': '44082', 'crid': '2898532/2419121/2592854/2499195', 'cat': ['IAB7', 'IAB14', 'IAB18-3', 'IAB1-2']}], 'seat': '44082'}]}
       };
       let bids = spec.interpretResponse(resp);
       expect(bids).to.deep.equal([
@@ -233,9 +327,53 @@ describe('Mgid bid adapter', function () {
           'mediaType': 'banner',
           'netRevenue': true,
           'nurl': 'http: nurl',
+          'burl': 'http: burl',
           'requestId': '61e40632c53fc2',
           'ttl': 300,
           'width': 300,
+        }
+      ]);
+    });
+  });
+  describe('interpretResponse native', function () {
+    it('should push proper native bid response', function () {
+      let resp = {
+        body: {'id': '57c0c2b1b732ca', 'bidid': '57c0c2b1b732ca', 'cur': 'GBP', 'seatbid': [{'bid': [{'price': 1.5, 'h': 600, 'w': 300, 'id': '1', 'impid': '61e40632c53fc2', 'adid': '2898532/2419121/2592854/2499195', 'nurl': 'http: nurl', 'burl': 'http: burl', 'adm': '{\"native\":{\"ver\":\"1.1\",\"link\":{\"url\":\"link_url\"},\"assets\":[{\"id\":1,\"required\":0,\"title\":{\"text\":\"title1\"}},{\"id\":2,\"required\":0,\"img\":{\"w\":80,\"h\":80,\"type\":3,\"url\":\"image_src\"}},{\"id\":3,\"required\":0,\"img\":{\"w\":50,\"h\":50,\"type\":1,\"url\":\"icon_src\"}}],\"imptrackers\":[\"imptrackers1\"]}}', 'cid': '44082', 'crid': '2898532/2419121/2592854/2499195', 'cat': ['IAB7', 'IAB14', 'IAB18-3', 'IAB1-2'], 'ext': {'place': 0, 'crtype': 'native'}}], 'seat': '44082'}]}
+      };
+      let bids = spec.interpretResponse(resp);
+      expect(bids).to.deep.equal([
+        {
+          'ad': '{\"native\":{\"ver\":\"1.1\",\"link\":{\"url\":\"link_url\"},\"assets\":[{\"id\":1,\"required\":0,\"title\":{\"text\":\"title1\"}},{\"id\":2,\"required\":0,\"img\":{\"w\":80,\"h\":80,\"type\":3,\"url\":\"image_src\"}},{\"id\":3,\"required\":0,\"img\":{\"w\":50,\"h\":50,\"type\":1,\"url\":\"icon_src\"}}],\"imptrackers\":[\"imptrackers1\"]}}',
+          'cpm': 1.5,
+          'creativeId': '2898532/2419121/2592854/2499195',
+          'currency': 'GBP',
+          'dealId': '',
+          'height': 0,
+          'isBurl': true,
+          'mediaType': 'native',
+          'netRevenue': true,
+          'nurl': 'http: nurl',
+          'burl': 'http: burl',
+          'requestId': '61e40632c53fc2',
+          'ttl': 300,
+          'width': 0,
+          'native': {
+            clickTrackers: [],
+            title: 'title1',
+            image: {
+              url: 'image_src',
+              width: 80,
+              height: 80,
+            },
+            icon: {
+              url: 'icon_src',
+              width: 50,
+              height: 50,
+            },
+            impressionTrackers: ['imptrackers1'],
+            jstracker: [],
+            clickUrl: 'link_url',
+          }
         }
       ]);
     });
