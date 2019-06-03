5f9bde20ca694b55ef19bf70f10909d5364f451a
Alexander <avj83@list.ru>
2019-06-03 22:21:26 +0400

    fix adapter and tests

--- modules/radsBidAdapter.js
+++ modules/radsBidAdapter.js
@@ -4,10 +4,12 @@ import {registerBidder} from '../src/adapters/bidderFactory';
 
 const BIDDER_CODE = 'rads';
 const ENDPOINT_URL = 'https://rads.recognified.net/md.request.php';
+const ENDPOINT_URL_DEV = 'https://dcbuyer1.dspx.tv/request/';
 
 export const spec = {
     code: BIDDER_CODE,
     aliases: ['rads'],
+    supportedMediaTypes: [BANNER, VIDEO],
     isBidRequestValid: function(bid) {
         return !!(bid.params.placement);
     },
@@ -22,34 +24,42 @@ export const spec = {
             const rnd = Math.floor(Math.random() * 99999999999);
             const referrer = encodeURIComponent(bidderRequest.refererInfo.referer);
             const bidId = bidRequest.bidId;
-            const payload = {
-                rt: 'html',
-                _f: 'prebid_js',
-                _ps: placementId,
-                srw: width,
-                srh: height,
-                idt: 100,
-                rnd: rnd,
-                p: referrer,
-                bid_id: bidId,
-            };
-            if (params.pfilter !== undefined) {
-                payload.pfilter = params.pfilter;
-            }
-            if (params.bcat !== undefined) {
-                payload.bcat = params.bcat;
-            }
-            if (params.dvt !== undefined) {
-                payload.dvt = params.dvt;
+            const isDev = params.devMode || false;
+
+            let endpoint = BUYER_ENDPOINT_URL;
+            if (isDev) {
+                endpoint = BUYER_ENDPOINT_URL_DEV;
             }
 
-            if (params.latitude !== undefined) {
-                payload.latitude = params.latitude;
+            let payload = {};
+            if (isVideoRequest(bidRequest)) {
+                payload = {
+                    rt: 'vasr2',
+                    _f: 'prebid_js',
+                    _ps: placementId,
+                    srw: width,
+                    srh: height,
+                    idt: 100,
+                    rnd: rnd,
+                    p: referrer,
+                    bid_id: bidId,
+                };
+            } else {
+                payload = {
+                    rt: 'bid-response',
+                    _f: 'prebid_js',
+                    _ps: placementId,
+                    srw: width,
+                    srh: height,
+                    idt: 100,
+                    rnd: rnd,
+                    p: referrer,
+                    bid_id: bidId,
+                };
             }
+            prepareExtraParams(params, payload);
 
-            if (params.longitude !== undefined) {
-                payload.longitude = params.longitude;
-            }
+
 
             return {
                 method: 'GET',
@@ -67,7 +77,6 @@ export const spec = {
             const dealId = response.dealid || '';
             const currency = response.currency || 'EUR';
             const netRevenue = (response.netRevenue === undefined) ? true : response.netRevenue;
-            const referrer = utils.getTopWindowUrl();
             const bidResponse = {
                 requestId: response.bid_id,
                 cpm: cpm,
@@ -78,9 +87,16 @@ export const spec = {
                 currency: currency,
                 netRevenue: netRevenue,
                 ttl: config.getConfig('_bidderTimeout'),
-                referrer: referrer,
                 ad: response.adTag
             };
+
+            if (response.vastXml) {
+                bidResponse.vastXml = response.vastXml;
+                bidResponse.mediaType = 'video';
+            } else {
+                bidResponse.ad = response.adTag;
+            }
+
             bidResponses.push(bidResponse);
         }
         return bidResponses;
@@ -100,6 +116,36 @@ function objectToQueryString(obj, prefix) {
         }
     }
     return str.join('&');
+}
+
+/**
+ * Check if it's a video bid request
+ *
+ * @param {BidRequest} bid - Bid request generated from ad slots
+ * @returns {boolean} True if it's a video bid
+ */
+function isVideoRequest(bid) {
+    return bid.mediaType === 'video' || !!utils.deepAccess(bid, 'mediaTypes.video');
+}
+
+function prepareExtraParams(params, payload) {
+    if (params.pfilter !== undefined) {
+        payload.pfilter = params.pfilter;
+    }
+    if (params.bcat !== undefined) {
+        payload.bcat = params.bcat;
+    }
+    if (params.dvt !== undefined) {
+        payload.dvt = params.dvt;
+    }
+
+    if (params.latitude !== undefined) {
+        payload.latitude = params.latitude;
+    }
+
+    if (params.longitude !== undefined) {
+        payload.longitude = params.longitude;
+    }
 }
 
 registerBidder(spec);
--- test/spec/modules/radsBidAdapter_spec.js
+++ test/spec/modules/radsBidAdapter_spec.js
@@ -2,14 +2,14 @@ import { expect } from 'chai';
 import { spec } from 'modules/radsBidAdapter';
 import { newBidder } from 'src/adapters/bidderFactory';
 
-const ENDPOINT_URL = 'https://rads.recognified.net/md.request.php';
+const RADS_ENDPOINT_URL = 'https://rads.recognified.net/md.request.php';
 
 describe('radsAdapter', function () {
     const adapter = newBidder(spec);
 
     describe('isBidRequestValid', function () {
         let bid = {
-            'bidder': 'rads',
+            'bidder': 'dspx',
             'params': {
                 'placement': '6682',
                 'pfilter': {
@@ -61,6 +61,29 @@ describe('radsAdapter', function () {
             'bidId': '30b31c1838de1e',
             'bidderRequestId': '22edbae2733bf6',
             'auctionId': '1d1a030790a475'
+        }, {
+            'bidder': 'rads',
+            'params': {
+                'placement': '6682',
+                'pfilter': {
+                    'floorprice': 1000000,
+                    'private_auction': 0,
+                    'geo': {
+                        'country': 'DE'
+                    }
+                },
+                'bcat': 'IAB2,IAB4',
+                'dvt': 'desktop'
+            },
+            'mediaTypes': {
+                'video': {
+                    'playerSize': [640, 480],
+                    'context': 'instream'
+                }
+            },
+            'bidId': '30b31c1838de1e',
+            'bidderRequestId': '22edbae2733bf6',
+            'auctionId': '1d1a030790a475'
         }];
 
         let bidderRequest = {
@@ -75,16 +98,37 @@ describe('radsAdapter', function () {
             let data = request[0].data.replace(/rnd=\d+\&/g, '').replace(/ref=.*\&bid/g, 'bid');
             expect(data).to.equal('_f=html&alternative=prebid_js&inventory_item_id=6682&srw=300&srh=250&idt=100&bid_id=30b31c1838de1e&pfilter%5Bfloorprice%5D=1000000&pfilter%5Bprivate_auction%5D=0&pfilter%5Bgeo%5D%5Bcountry%5D=DE&bcat=IAB2%2CIAB4&dvt=desktop');
         });
+
+        it('sends bid video request to our rads endpoint via GET', function () {
+            expect(request[1].method).to.equal('GET');
+            let data = request[1].data.replace(/rnd=\d+\&/g, '').replace(/ref=.*\&bid/g, 'bid');
+            expect(data).to.equal('_f=vast2&alternative=prebid_js&inventory_item_id=6682&srw=640&srh=480&idt=100&bid_id=30b31c1838de1e&pfilter%5Bfloorprice%5D=1000000&pfilter%5Bprivate_auction%5D=0&pfilter%5Bgeo%5D%5Bcountry%5D=DE&bcat=IAB2%2CIAB4&dvt=desktop');
+        });
+
     });
 
     describe('interpretResponse', function () {
-        let serverResponse = {
+        let serverBannerResponse = {
             'body': {
                 'cpm': 5000000,
                 'crid': 100500,
                 'width': '300',
                 'height': '250',
-                'tag': '<!-- test creative -->',
+                'adTag': '<!-- test creative -->',
+                'requestId': '220ed41385952a',
+                'currency': 'EUR',
+                'ttl': 60,
+                'netRevenue': true,
+                'zone': '6682'
+            }
+        };
+        let serverVideoResponse = {
+            'body': {
+                'cpm': 5000000,
+                'crid': 100500,
+                'width': '300',
+                'height': '250',
+                'vastXml': '{"reason":7001,"status":"accepted"}',
                 'requestId': '220ed41385952a',
                 'currency': 'EUR',
                 'ttl': 60,
@@ -103,20 +147,52 @@ describe('radsAdapter', function () {
             currency: 'EUR',
             netRevenue: true,
             ttl: 300,
-            referrer: '',
             ad: '<!-- test creative -->'
+        }, {
+            requestId: '23beaa6af6cdde',
+            cpm: 0.5,
+            width: 0,
+            height: 0,
+            creativeId: 100500,
+            dealId: '',
+            currency: 'EUR',
+            netRevenue: true,
+            ttl: 300,
+            vastXml: '{"reason":7001,"status":"accepted"}',
+            mediaType: 'video'
         }];
 
         it('should get the correct bid response by display ad', function () {
             let bidRequest = [{
                 'method': 'GET',
-                'url': ENDPOINT_URL,
+                'url': RADS_ENDPOINT_URL,
+                'refererInfo': {
+                    'referer': ''
+                },
                 'data': {
                     'bid_id': '30b31c1838de1e'
                 }
             }];
-            let result = spec.interpretResponse(serverResponse, bidRequest[0]);
+            let result = spec.interpretResponse(serverBannerResponse, bidRequest[0]);
             expect(Object.keys(result[0])).to.have.members(Object.keys(expectedResponse[0]));
+        });
+
+        it('should get the correct rads video bid response by display ad', function () {
+            let bidRequest = [{
+                'method': 'GET',
+                'url': RADS_ENDPOINT_URL,
+                'mediaTypes': {
+                    'video': {
+                        'playerSize': [640, 480],
+                        'context': 'instream'
+                    }
+                },
+                'data': {
+                    'bid_id': '30b31c1838de1e'
+                }
+            }];
+            let result = spec.interpretResponse(serverVideoResponse, bidRequest[0]);
+            expect(Object.keys(result[0])).to.have.members(Object.keys(expectedResponse[1]));
         });
 
         it('handles empty bid response', function () {
