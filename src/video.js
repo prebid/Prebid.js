a870d4f1fe02867fa08e0340ee45015dbeddec98
Michael Rooke <github@mikerooke.com>
2019-06-05 10:38:40 -0400

    Use actual global object name in log message (#3874)
    - The global Prebid.js object name can be something other than 'pbjs'. Update log messages to reference the specified prebid global object name.

--- src/video.js
+++ src/video.js
@@ -48,7 +48,7 @@ export const checkVideoBidSetup = hook('sync', function(bid, bidRequest, videoMe
     if (!config.getConfig('cache.url') && bid.vastXml && !bid.vastUrl) {
       logError(`
         This bid contains only vastXml and will not work when a prebid cache url is not specified.
-        Try enabling prebid cache with pbjs.setConfig({ cache: {url: "..."} });
+        Try enabling prebid cache with $$PREBID_GLOBAL$$.setConfig({ cache: {url: "..."} });
       `);
       return false;
     }
