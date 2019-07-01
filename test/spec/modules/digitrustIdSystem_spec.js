0fd6f0d62be46f448d1990a490e2873a83cee6b1
Chris Cole <goosemanjack@gmail.com>
2019-07-01 13:02:47 -0700

    Removing Promise from unit test as IE doesn't like it.

--- test/spec/modules/digitrustIdSystem_spec.js
+++ test/spec/modules/digitrustIdSystem_spec.js
@@ -9,24 +9,22 @@ let expect = require('chai').expect;
 var testHook = null;
 
 describe('DigiTrust Id System', function () {
-  it('Should create the test hook', function () {
-    return new Promise(function (resolve, reject) {
-      testHook = surfaceTestHook();
-      assert.isNotNull(testHook, 'The test hook failed to surface');
-      var conf = {
-        init: {
-          member: 'unit_test',
-          site: 'foo'
-        },
-        callback: function (result) {
-        }
-      };
-      testHook.initDigitrustFacade(conf);
-      window.DigiTrust.getUser(conf);
-      expect(window.DigiTrust).to.exist;
-      expect(window.DigiTrust.isMock).to.be.true;
-      resolve();
-    });
+  it('Should create the test hook', function (done) {
+    testHook = surfaceTestHook();
+    assert.isNotNull(testHook, 'The test hook failed to surface');
+    var conf = {
+      init: {
+        member: 'unit_test',
+        site: 'foo'
+      },
+      callback: function (result) {
+      }
+    };
+    testHook.initDigitrustFacade(conf);
+    window.DigiTrust.getUser(conf);
+    expect(window.DigiTrust).to.exist;
+    expect(window.DigiTrust.isMock).to.be.true;
+    done();
   });
 
   it('Should report as client', function () {
