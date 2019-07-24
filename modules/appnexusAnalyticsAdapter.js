e04275d092b7cf4caf3531b0fd42631e695b58e9
Jason Snellbaker <jsnellbaker@appnexus.com>
2019-07-24 13:27:23 -0400

    undo changes to appnexus Analytics adapter

--- modules/appnexusAnalyticsAdapter.js
+++ modules/appnexusAnalyticsAdapter.js
@@ -5,22 +5,11 @@
 import adapter from '../src/AnalyticsAdapter';
 import adapterManager from '../src/adapterManager';
 
-var appnexusAdapterHead = adapter({
+var appnexusAdapter = adapter({
   global: 'AppNexusPrebidAnalytics',
   handler: 'on',
   analyticsType: 'bundle'
 });
-
-var appnexusAdapter = Object.assign(appnexusAdapterHead, {});
-
-// save the base class function
-appnexusAdapter.originEnableAnalytics = appnexusAdapter.enableAnalytics;
-
-// override enableAnalytics so we can get access to the config passed in from
-// the page
-appnexusAdapter.enableAnalytics = function (config) {
-  appnexusAdapter.originEnableAnalytics(config); // call the base class function
-};
 
 adapterManager.registerAnalyticsAdapter({
   adapter: appnexusAdapter,
