0e2e53fb752cce7f95b3d127f2d9391c5b27822f
Rich Snapp <rich.snapp@gmail.com>
2019-08-09 14:00:33 -0600

    lint fixes

--- modules/rdnBidAdapter.js
+++ modules/rdnBidAdapter.js
@@ -45,7 +45,7 @@ export const spec = {
         creativeId: sb.creative_id || 0,
         dealId: sb.deal_id || '',
         currency: sb.currency || 'JPY',
-        netRevenue: (typeof sb.net_revenue === "undefined") ? true : !!sb.net_revenue,
+        netRevenue: (typeof sb.net_revenue === 'undefined') ? true : !!sb.net_revenue,
         mediaType: BANNER,
         ttl: sb.ttl,
         referrer: utils.getTopWindowUrl(),
