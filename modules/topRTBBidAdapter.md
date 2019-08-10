80faa081e61157a9fbb373f4756866fc855242fa
Unnamalai57 <unnamalai.p@djaxtech.com>
2019-08-10 13:35:37 +0530

    adunit changes for valid bidReponse

--- modules/topRTBBidAdapter.md
+++ modules/topRTBBidAdapter.md
@@ -15,16 +15,32 @@ Only Banner & video format is supported.
 ```
     var adUnits = [
        {
+       // Banner adUnit
            code: 'test-div-0',
-           sizes: [[728, 90]],  // a display size
+           sizes: [[970,90]],  // a display size
            bids: [
                {
                     bidder: 'topRTB',
                     params: {
-                        adUnitId: 'c5c06f77430c4c33814a0577cb4cc978'
+                        adUnitId: '3ec5a94f00cf4cc2858bc21533542827'
                     }
                }
            ]
-       }
+       },
+       // Video instream adUnit
+        		code: 'video1',
+                mediaTypes: {
+                    video: {
+                        playerSize: [640, 480],
+                        context: 'instream'
+                    }
+                },
+                bids: [{
+                    bidder: 'topRTB',
+                    params: {
+							adUnitId: '29f929a511eb4adca361f4591be04488'
+         		          		   
+      			 		}
+                }]
     ];
 ```
--- test/spec/modules/topRTBBidAdapter_spec.js
+++ test/spec/modules/topRTBBidAdapter_spec.js
@@ -6,7 +6,7 @@ describe('topRTBBidAdapterTests', function () {
     expect(spec.isBidRequestValid({
       bidder: 'topRTB',
       params: {
-        adUnitId: 'c5c06f77430c4c33814a0577cb4cc978'
+        adUnitId: '3ec5a94f00cf4cc2858bc21533542827'
       },
       adName: 'banner'
     }));
@@ -19,9 +19,9 @@ describe('topRTBBidAdapterTests', function () {
       adName: 'banner',
       adType: '{"banner":{"sizes":[[]]}}',
       params: {
-        adUnitId: 'c5c06f77430c4c33814a0577cb4cc978'
+        adUnitId: '3ec5a94f00cf4cc2858bc21533542827'
       },
-      sizes: [[728, 90]]
+      sizes: [[970, 90]]
     }];
 
     let request = spec.buildRequests(bidRequestData);
@@ -39,7 +39,7 @@ describe('topRTBBidAdapterTests', function () {
     let serverResponse = {
       body: [{
         'cpm': 1,
-        'mediadata': "<a href='http://13.125.21.204:8080/bidder/click?url=xaCQyxrEJsY7XQj4dRGD2RVQiVaLJ%2Bar%2BVDhwhlpnR%2FSQG%2B9%2FtSmvV4X45AM9mMl%2BOSaJzKXTKN82WHc1li3gCzibhr%2ByfcqPIl%2FQHJjBKS7bznHEwRh1kZShVVnSpp3DBjS5I5WSmSD4Qbyo61IJq3LFc9OpHKmJeMgATc4bHK00MqW7atQStUWWSTuhlGO&details=l5G4hDG5UmRmJ%2BB9AdG7v2OpwH%2Bio9Y2oIETI6KooMoo7lJ4Yo7pTbfVGhA5Vn%2BaPCoBX4779c1Jqok45%2FL2ZUP0nc7F0IDfRpLdtoX%2B7Hr2tqmK2Suide0LIsB0woVDXBiq62%2BfneGrTnCi6Nq6GDIBFpmFH8CFinYL%2F%2BB33V8%3D' target='_blank'><img src='http://www.toprtb.com/uploads/d037c2d94369417ab9aea6e712723235/728-90_1546872001356_728x90.gif' alt='Banner 728x90' width='728' height='90' /></a><img src='http://13.125.21.204:8080/bidder/impression?id=e1fb1fbdb97643189827b1b4d2b51acc&impid=1' width='1' height='1' alt=''/></a><img src='http://13.124.144.40:8080/ssp/impression?id=e1fb1fbdb97643189827b1b4d2b51acc&impid=1' width='1' height='1' alt=''/>",
+        'mediadata': "<a href='http://dsp.toprtb.com/bidder/click?url=xaCQyxrEJsY7XQj4dRGD2RVQiVaLJ%2Bar%2BVDhwhlpnR%2FSQG%2B9%2FtSmvV4X45AM9mMl%2BOSaJzKXTKN82WHc1li3gCzibhr%2ByfcqPIl%2FQHJjBKS7bznHEwRh1kZShVVnSpp3DBjS5I5WSmSD4Qbyo61IJq3LFc9OpHKmJeMgATc4bHK00MqW7atQStUWWSTuhlGO&details=l5G4hDG5UmRmJ%2BB9AdG7v2OpwH%2Bio9Y2oIETI6KooMoo7lJ4Yo7pTbfVGhA5Vn%2BaPCoBX4779c1Jqok45%2FL2ZUP0nc7F0IDfRpLdtoX%2B7Hr2tqmK2Suide0LIsB0woVDXBiq62%2BfneGrTnCi6Nq6GDIBFpmFH8CFinYL%2F%2BB33V8%3D' target='_blank'><img src='http://www.toprtb.com/uploads/d037c2d94369417ab9aea6e712723235/728-90_1546872001356_728x90.gif' alt='Banner 728x90' width='728' height='90' /></a><img src='http://13.125.21.204:8080/bidder/impression?id=e1fb1fbdb97643189827b1b4d2b51acc&impid=1' width='1' height='1' alt=''/></a><img src='http://13.124.144.40:8080/ssp/impression?id=e1fb1fbdb97643189827b1b4d2b51acc&impid=1' width='1' height='1' alt=''/>",
         'width': 728,
         'currency': 'USD',
         'id': 'cd95dffec6b645afbc4e5aa9f68f2ff3',
