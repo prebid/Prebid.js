a255d89edc69de59b4c549831277eafbd7af9ca8
Olivier <osazos@adagio.io>
2019-08-28 21:49:04 +0200

    adagioAnalyticsAdapter: add and improve tests

--- modules/adagioAnalyticsAdapter.js
+++ modules/adagioAnalyticsAdapter.js
@@ -5,17 +5,20 @@
 import adapter from 'src/AnalyticsAdapter';
 import adapterManager from 'src/adapterManager';
 import CONSTANTS from 'src/constants.json';
+import * as utils from 'src/utils';
 
 const emptyUrl = '';
 const analyticsType = 'endpoint';
 const events = Object.keys(CONSTANTS.EVENTS).map(key => CONSTANTS.EVENTS[key]);
 const VERSION = '1.3.0';
 
-let adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }));
+const adagioEnqueue = function adagioEnqueue(action, data) {
+  utils.getWindowTop().ADAGIO.queue.push({ action, data, ts: Date.now() });
+}
 
 function canAccessTopWindow() {
   try {
-    if (window.top.location.href) {
+    if (utils.getWindowTop().location.href) {
       return true;
     }
   } catch (error) {
@@ -23,23 +26,27 @@ function canAccessTopWindow() {
   }
 }
 
-if (canAccessTopWindow()) {
+let adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }), {
+  track: function({ eventType, args }) {
+    if (typeof args !== 'undefined' && events.indexOf(eventType) !== -1) {
+      adagioEnqueue('pb-analytics-event', { eventName: eventType, args });
+    }
+  }
+});
+
+adagioAdapter.originEnableAnalytics = adagioAdapter.enableAnalytics;
+
+adagioAdapter.enableAnalytics = config => {
+  if (!canAccessTopWindow()) {
+    return;
+  }
+
   window.top.ADAGIO = window.top.ADAGIO || {};
   window.top.ADAGIO.queue = window.top.ADAGIO.queue || [];
   window.top.ADAGIO.versions = window.top.ADAGIO.versions || {};
   window.top.ADAGIO.versions.adagioAnalyticsAdapter = VERSION;
 
-  const adagioEnqueue = function adagioEnqueue(action, data) {
-    window.top.ADAGIO.queue.push({ action, data, ts: Date.now() });
-  }
-
-  adagioAdapter = Object.assign(adapter({ emptyUrl, analyticsType }), {
-    track({ eventType, args }) {
-      if (typeof args !== 'undefined' && events.indexOf(eventType) !== -1) {
-        adagioEnqueue('pb-analytics-event', { eventName: eventType, args });
-      }
-    }
-  });
+  adagioAdapter.originEnableAnalytics(config);
 }
 
 adapterManager.registerAnalyticsAdapter({
--- test/spec/modules/adagioAnalyticsAdapter_spec.js
+++ test/spec/modules/adagioAnalyticsAdapter_spec.js
@@ -5,6 +5,8 @@ import {
 import {
   parse as parseURL
 } from 'src/url';
+import * as utils from 'src/utils';
+
 let adapterManager = require('src/adapterManager').default;
 let events = require('src/events');
 let constants = require('src/constants.json');
@@ -110,6 +112,79 @@ describe('adagio analytics adapter', () => {
       expect(o.ts).to.not.be.undefined;
       expect(o.data).to.not.be.undefined;
       expect(o.data).to.deep.equal({eventName: constants.EVENTS.AUCTION_END, args: {}});
+    });
+  });
+
+  describe('no track', () => {
+    // delete window.top.ADAGIO;
+    const sandbox = sinon.createSandbox();
+
+    adapterManager.registerAnalyticsAdapter({
+      code: 'adagio',
+      adapter: adagioAnalyticsAdapter
+    });
+
+    beforeEach(() => {
+      sandbox.stub(utils, 'getWindowTop').throws();
+
+      adapterManager.enableAnalytics({
+        provider: 'adagio'
+      });
+    });
+
+    afterEach(() => {
+      adagioAnalyticsAdapter.disableAnalytics();
+      sandbox.restore();
+    });
+
+    it('builds and sends auction data', () => {
+      let bidRequest = {
+        bids: [{
+          adUnitCode: 'div-1',
+          params: {
+            features: {
+              siteId: '2',
+              placement: 'pave_top',
+              pagetype: 'article',
+              category: 'IAB12,IAB12-2',
+              device: '2',
+            }
+          }
+        }, {
+          adUnitCode: 'div-2',
+          params: {
+            features: {
+              siteId: '2',
+              placement: 'ban_top',
+              pagetype: 'article',
+              category: 'IAB12,IAB12-2',
+              device: '2',
+            }
+          },
+        }],
+      };
+      let bidResponse = {
+        bidderCode: 'adagio',
+        width: 300,
+        height: 250,
+        statusMessage: 'Bid available',
+        cpm: 6.2189757658226075,
+        currency: '',
+        netRevenue: false,
+        adUnitCode: 'div-1',
+        timeToRespond: 132,
+      };
+
+      // Step 1: Send bid requested event
+      events.emit(constants.EVENTS.BID_REQUESTED, bidRequest);
+
+      // Step 2: Send bid response event
+      events.emit(constants.EVENTS.BID_RESPONSE, bidResponse);
+
+      // Step 3: Send auction end event
+      events.emit(constants.EVENTS.AUCTION_END, {});
+
+      expect(window.top.ADAGIO.queue).length(0);
     });
   });
 });
