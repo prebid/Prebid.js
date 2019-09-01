c560a4b317b7f914e76599a80f216aa0f8f3d2b9
Harshad Mane <harshad.mane@pubmatic.com>
2019-08-31 17:16:55 -0700

    first commit

---
+++ modules/shareUids.js
@@ -0,0 +1,57 @@
+import {config} from '../src/config';
+import {getGlobal} from '../src/prebidGlobal';
+import { isNumber, isStr, isArray, isPlainObject, isBoolean, isFn, hasOwn, logInfo, isInteger } from '../src/utils';
+
+const MODULE_NAME = 'shareUids';
+const DFP = 'DFP';
+const DFP_KEYS_CONFIG = 'DFP_KEYS';
+
+export function addUidsToDFP(userIds, config){
+	
+	if(! isPlainObject(config)){
+		logInfo(MODULE_NAME + ': Invalid config found, not sharing userIds externally.');
+		return;
+	}
+
+	const DFP_KEYS = isPlainObject(config[DFP_KEYS_CONFIG]) ? config[DFP_KEYS_CONFIG] : {};
+	let SHARE_WITH_DFP = isBoolean(config[DFP]) ? config[DFP] : false;	
+	let DFP_API;
+
+	if(!SHARE_WITH_DFP){
+		logInfo(MODULE_NAME + ': Not enabled for ' + DFP);
+	}
+
+	// add validation for googletag.pubads().setTargeting
+	if(googletag && isFn(googletag.pubads) && hasOwn(googletag.pubads(), 'setTargeting') && isFn(googletag.pubads().setTargeting)){
+		DFP_API = googletag.pubads().setTargeting;
+	} else {
+		// for invalid case unset SHARE_WITH_DFP and log failure
+		SHARE_WITH_DFP = false;
+		logInfo(MODULE_NAME + ': Could not find googletag.pubads().setTargeting API. Not adding UIDs in targeting.')
+		return;
+	}
+
+    Object.keys(userIds).forEach(function(key){
+    	// check may be incorrect if a userId is an object
+    	if(isStr(userIds[key])){
+    		// DFP_KEYS[key] = '' means publisher do not want to send this uid
+    		if(SHARE_WITH_DFP && DFP_KEYS[key] !== ''){
+    			DFP_API( 
+    				(hasOwn(DFP_KEYS, key) ? DFP_KEYS[key] : key), 
+    				[ userIds[key] ]
+    			);
+    		}
+    	}
+    });
+}
+
+export function init(config) {
+  getGlobal().requestBids.before(function(fn, reqBidsConfigObj) {
+  	// using setTimeout to avoid delay
+  	setTimeout(addUidsToDFP, 0, (getGlobal()).getUserIds(), config.getConfig(MODULE_NAME));
+    // calling fn allows prebid to continue processing
+    return fn.call(this, reqBidsConfigObj);
+  }, 40);
+}
+
+init(config)
---
+++ modules/shareUids.md
@@ -0,0 +1,34 @@
+## shareUids Module
+This module works with userId module.
+This module is used to pass userIds to DFP in targeting so that user ids can be used to pass in EB or can be used for targeting in DFP.
+
+## Sample config
+```
+pbjs.setConfig({
+
+	// your existing userIds config
+
+	usersync: {
+		userIds: [{...}, ...]
+	},
+
+	// new shareUids config
+
+	shareUids: {
+		"DFP": true,
+		"DFP_KEYS": {
+			"tdid": "TTD_ID" // send tdid as TTD_ID
+		}
+	}
+});
+```
+
+## Config options
+- DFP: is required to be set to true if a publisher wants to send UserIds as targeting in DFP call
+- DFP_KEYS: is an optional object. If not passed then all UserIds are passed with key-name used in UserIds object.
+If a publisher wants to pass ```UserId.tdid``` as TTD_ID in targeting then set  ``` DFP_KEYS: { "tdid": "TTD_ID" }```
+If a publisher does not wants to pass ```UserId.tdid``` but wants to pass other Ids in UserId tthen set ``` DFP_KEYS: { "tdid": "" }```
+
+## Notes
+- We can add support for other external systems like DFP in future
+- We have not added support for A9/APSTag as it is call in parallel with Prebid. This module executes when ```pbjs.requestBids``` is called, in practice call to A9 is expected to execute in paralle to Prebid thus we have not covered A9 here. For sending Uids in A9 one will need to set those in params key in object passed to ```apstag.init```, ```pbjs.getUserIds``` can be used for the same. 
