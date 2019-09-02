0bdb812b8df4a2a10689683bc106f1d610ce07b8
Harshad Mane <harshad.mane@pubmatic.com>
2019-09-02 15:47:18 -0700

    indentation

--- modules/shareUserIds.js
+++ modules/shareUserIds.js
@@ -6,46 +6,45 @@ const MODULE_NAME = 'shareUserIds';
 const DFP = 'DFP';
 const DFP_KEYS_CONFIG = 'DFP_KEYS';
 
-export function shareUserIds(userIds, config){
-	
-	if(! isPlainObject(config)){
-		logInfo(MODULE_NAME + ': Invalid config found, not sharing userIds externally.');
-		return;
-	}
+export function shareUserIds(userIds, config) {
+  if (!isPlainObject(config)) {
+    logInfo(MODULE_NAME + ': Invalid config found, not sharing userIds externally.');
+    return;
+  }
 
-	const PUB_DFP_KEYS = isPlainObject(config[DFP_KEYS_CONFIG]) ? config[DFP_KEYS_CONFIG] : {};
-	let SHARE_WITH_DFP = isBoolean(config[DFP]) ? config[DFP] : false;	
-	let DFP_API;
+  const PUB_DFP_KEYS = isPlainObject(config[DFP_KEYS_CONFIG]) ? config[DFP_KEYS_CONFIG] : {};
+  let SHARE_WITH_DFP = isBoolean(config[DFP]) ? config[DFP] : false;
+  let DFP_API;
 
-	if(!SHARE_WITH_DFP){
-		logInfo(MODULE_NAME + ': Not enabled for ' + DFP);
-	}
+  if (!SHARE_WITH_DFP) {
+    logInfo(MODULE_NAME + ': Not enabled for ' + DFP);
+  }
 
-	if(googletag && isFn(googletag.pubads) && hasOwn(googletag.pubads(), 'setTargeting') && isFn(googletag.pubads().setTargeting)){
-		DFP_API = googletag.pubads().setTargeting;
-	} else {
-		SHARE_WITH_DFP = false;
-		logInfo(MODULE_NAME + ': Could not find googletag.pubads().setTargeting API. Not adding User Ids in targeting.')
-		return;
-	}
+  if (window.googletag && isFn(window.googletag.pubads) && hasOwn(window.googletag.pubads(), 'setTargeting') && isFn(window.googletag.pubads().setTargeting)) {
+    DFP_API = window.googletag.pubads().setTargeting;
+  } else {
+    SHARE_WITH_DFP = false;
+    logInfo(MODULE_NAME + ': Could not find googletag.pubads().setTargeting API. Not adding User Ids in targeting.')
+    return;
+  }
 
-    Object.keys(userIds).forEach(function(key){
-    	if(isStr(userIds[key])){
-    		// PUB_DFP_KEYS[key] = '' means publisher does not want to send this uid
-    		if(SHARE_WITH_DFP && PUB_DFP_KEYS[key] !== ''){
-    			DFP_API( 
-    				(hasOwn(PUB_DFP_KEYS, key) ? PUB_DFP_KEYS[key] : key), 
-    				[ userIds[key] ]
-    			);
-    		}
-    	}
-    });
+  Object.keys(userIds).forEach(function(key) {
+    if (isStr(userIds[key])) {
+      // PUB_DFP_KEYS[key] = '' means publisher does not want to send this uid
+      if (SHARE_WITH_DFP && PUB_DFP_KEYS[key] !== '') {
+        DFP_API(
+          (hasOwn(PUB_DFP_KEYS, key) ? PUB_DFP_KEYS[key] : key),
+          [ userIds[key] ]
+        );
+      }
+    }
+  });
 }
 
 export function init(config) {
   getGlobal().requestBids.before(function(fn, reqBidsConfigObj) {
-  	// using setTimeout to avoid delay
-  	setTimeout(shareUserIds, 0, (getGlobal()).getUserIds(), config.getConfig(MODULE_NAME));
+    // using setTimeout to avoid delay
+    setTimeout(shareUserIds, 0, (getGlobal()).getUserIds(), config.getConfig(MODULE_NAME));
     // calling fn allows prebid to continue processing
     return fn.call(this, reqBidsConfigObj);
   }, 40);
