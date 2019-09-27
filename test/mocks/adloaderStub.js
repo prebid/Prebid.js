1e732652234389cfbc6a1a42a6109e7dc4ab4958
Sumit Sharma1 <Sumit.Sharma1@SYNAPPNXS.local>
2019-09-27 14:57:13 +0530

    call callback() from stub

--- test/mocks/adloaderStub.js
+++ test/mocks/adloaderStub.js
@@ -9,7 +9,11 @@ export let loadExternalScriptStub;
 beforeEach(function() {
   sandbox = sinon.sandbox.create();
   loadExternalScript = adloader.loadExternalScript;
-  loadExternalScriptStub = sandbox.stub(adloader, 'loadExternalScript');
+  loadExternalScriptStub = sandbox.stub(adloader, 'loadExternalScript').callsFake((...args) => {
+    if (typeof args[2] === 'function') {
+      args[2]();
+    }
+  });
 });
 
 afterEach(function() {
