1b207196f50d2c9c12aba4d12b449cf1f23e7549
pm-shashank-jain <shashank.jain@pubmatic.com>
2019-10-03 14:46:05 +0530

    added secure flag

--- modules/pubmaticServerBidAdapter.js
+++ modules/pubmaticServerBidAdapter.js
@@ -7,7 +7,7 @@ const constants = require('../src/constants.json');
 
 const BIDDER_CODE = 'pubmaticServer';
 const ENDPOINT = 'https://ow.pubmatic.com/openrtb/2.5/';
-const COOKIE_SYNC = 'https://ow.pubmatic.com/cookie_sync/';
+const COOKIE_SYNC = 'https://ow.pubmatic.com/cookie_sync/?sec=1'; // Set sec=1 to identify secure flag changes at server side
 const CURRENCY = 'USD';
 const AUCTION_TYPE = 1; // PubMaticServer just picking highest bidding bid from the partners configured
 const UNDEFINED = undefined;
