e27fc40cb3831890f5e7f3d7fe5652cae816706c
bretg <bgorsline@gmail.com>
2019-10-14 16:15:17 -0400

    updating liveintent eids source (#4300)
    * updating liveintent eids source
    
    these are supposed to be domains
    
    * updating unit test

--- test/spec/modules/prebidServerBidAdapter_spec.js
+++ test/spec/modules/prebidServerBidAdapter_spec.js
@@ -1057,8 +1057,8 @@ describe('S2S Adapter', function () {
       expect(requestBid.user.ext.eids.filter(eid => eid.source === 'adserver.org')[0].uids[0].id).is.equal('abc123');
       expect(requestBid.user.ext.eids.filter(eid => eid.source === 'pubcommon')).is.not.empty;
       expect(requestBid.user.ext.eids.filter(eid => eid.source === 'pubcommon')[0].uids[0].id).is.equal('1234');
-      expect(requestBid.user.ext.eids.filter(eid => eid.source === 'liveIntent')).is.not.empty;
-      expect(requestBid.user.ext.eids.filter(eid => eid.source === 'liveIntent')[0].uids[0].id).is.equal('li-xyz');
+      expect(requestBid.user.ext.eids.filter(eid => eid.source === 'liveintent.com')).is.not.empty;
+      expect(requestBid.user.ext.eids.filter(eid => eid.source === 'liveintent.com')[0].uids[0].id).is.equal('li-xyz');
     });
 
     it('when config \'currency.adServerCurrency\' value is an array: ORTB has property \'cur\' value set to a single item array', function () {
