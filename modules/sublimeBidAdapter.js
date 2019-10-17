0b39a97d5ce245f32c6e3785f5ef21d72cf69de3
Fengyu CHEN <fengyu@sublimeskinz.com>
2019-10-17 16:53:15 +0200

    sublimeBidAdapter: send bid as a simple request(json -> text)

--- modules/sublimeBidAdapter.js
+++ modules/sublimeBidAdapter.js
@@ -188,8 +188,8 @@ function buildRequests(validBidRequests, bidderRequest) {
       url: protocol + '://' + bidHost + '/bid',
       data: payload,
       options: {
-        contentType: 'application/json',
-        withCredentials: true
+        contentType: 'text/plain',
+        withCredentials: false
       },
     }
   });
