3045d683eece82f18cc00fa89900a760f67b8824
Tim Sturtewagen <tim@adhese.com>
2019-10-23 20:49:19 +0200

    added DALE to adhese determination

--- modules/adheseBidAdapter.js
+++ modules/adheseBidAdapter.js
@@ -136,7 +136,7 @@ function getbaseAdResponse(response) {
 }
 
 function isAdheseAd(ad) {
-  return !ad.origin || ad.origin === 'JERLICIA';
+  return !ad.origin || ad.origin === 'JERLICIA' || ad.origin === 'DALE';
 }
 
 function getMediaType(markup) {
