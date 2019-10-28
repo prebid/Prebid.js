bb726df9494e323a99448a6d4d20a340a9314423
Ankit Prakash <aprakash@sovrn.com>
2019-10-28 10:55:29 -0600

    update pixel tests

--- test/spec/modules/sovrnBidAdapter_spec.js
+++ test/spec/modules/sovrnBidAdapter_spec.js
@@ -416,13 +416,61 @@ describe('sovrnBidAdapter', function() {
       expect(returnStatement).to.be.empty;
     });
 
-    it('should include pixel syncs', () => {
+    it.only('should include pixel syncs', () => {
       let pixelEnabledOptions = { iframeEnabled: false, pixelEnabled: true };
-      const returnStatement = spec.getUserSyncs(pixelEnabledOptions, serverResponse);
-      console.log(returnStatement)
-      expect(returnStatement.length).to.equal(2);
-      expect(returnStatement).to.deep.include.members([{ type: 'image', url: 'http://idprovider1.com' },
-        { type: 'image', url: 'http://idprovider2.com' }]);
+      const resp2 = {
+        'body': {
+          'id': '546956d68c757f-2',
+          'seatbid': [
+            {
+              'bid': [
+                {
+                  'id': 'a_448326_16c2ada014224bee815a90d2248322f5-2',
+                  'impid': '2a3826aae345f4',
+                  'price': 1.0099999904632568,
+                  'nurl': 'http://localhost/rtb/impression?bannerid=220958&campaignid=3890&rtb_tid=15588614-75d2-40ab-b27e-13d2127b3c2e&rpid=1295&seatid=seat1&zoneid=448326&cb=26900712&tid=a_448326_16c2ada014224bee815a90d2248322f5',
+                  'adm': 'yo a creative',
+                  'crid': 'cridprebidrtb',
+                  'w': 160,
+                  'h': 600
+                },
+                {
+                  'id': 'a_430392_beac4c1515da4576acf6cb9c5340b40c-2',
+                  'impid': '3cf96fd26ed4c5',
+                  'price': 1.0099999904632568,
+                  'nurl': 'http://localhost/rtb/impression?bannerid=220957&campaignid=3890&rtb_tid=5bc0e68b-3492-448d-a6f9-26fa3fd0b646&rpid=1295&seatid=seat1&zoneid=430392&cb=62735099&tid=a_430392_beac4c1515da4576acf6cb9c5340b40c',
+                  'adm': 'yo a creative',
+                  'crid': 'cridprebidrtb',
+                  'w': 300,
+                  'h': 250
+                },
+              ]
+            }
+          ],
+          'ext': {
+            'iid': 13487408,
+            sync: {
+              pixels: [
+                {
+                  url: 'http://idprovider3.com'
+                },
+                {
+                  url: 'http://idprovider4.com'
+                }
+              ]
+            }
+          }
+        },
+        'headers': {}
+      }
+      const returnStatement = spec.getUserSyncs(pixelEnabledOptions, [...serverResponse, resp2]);
+      expect(returnStatement.length).to.equal(4);
+      expect(returnStatement).to.deep.include.members([
+        { type: 'image', url: 'http://idprovider1.com' },
+        { type: 'image', url: 'http://idprovider2.com' },
+        { type: 'image', url: 'http://idprovider3.com' },
+        { type: 'image', url: 'http://idprovider4.com' }
+      ]);
     });
   });
 
