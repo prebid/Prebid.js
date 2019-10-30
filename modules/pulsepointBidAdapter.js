163ae078e52156f159c0fc297ef95530d3090a0b
avenkatraman <avenkatraman@pulsepoint.com>
2019-10-30 15:46:01 -0400

    Minor refactor

--- modules/pulsepointBidAdapter.js
+++ modules/pulsepointBidAdapter.js
@@ -116,7 +116,7 @@ function bidResponseAvailable(request, response) {
         adId: id,
         ttl: idToBidMap[id].exp || DEFAULT_BID_TTL,
         netRevenue: DEFAULT_NET_REVENUE,
-        currency: DEFAULT_CURRENCY
+        currency: idToBidMap[id].cur || DEFAULT_CURRENCY
       };
       if (idToImpMap[id]['native']) {
         bid['native'] = nativeResponse(idToImpMap[id], idToBidMap[id]);
@@ -135,18 +135,10 @@ function bidResponseAvailable(request, response) {
         bid.width = idToImpMap[id].banner.w;
         bid.height = idToImpMap[id].banner.h;
       }
-      applyExt(bid, idToBidMap[id])
       bids.push(bid);
     }
   });
   return bids;
-}
-
-function applyExt(bid, ortbBid) {
-  if (ortbBid && ortbBid.ext) {
-    bid.currency = ortbBid.ext.currency || bid.currency;
-    bid.netRevenue = ortbBid.ext.netRevenue != null ? ortbBid.ext.netRevenue : bid.netRevenue;
-  }
 }
 
 /**
