4964978febda6f5623838556800f041e13bfee10
Mark Monday <mmonday@rubiconproject.com>
2019-11-12 12:14:15 -0500

    Remove duplicate param to fix unit tests

--- test/spec/modules/rubiconBidAdapter_spec.js
+++ test/spec/modules/rubiconBidAdapter_spec.js
@@ -2311,7 +2311,7 @@ describe('the rubicon adapter', function () {
     });
   });
 
-  describe.only('Supply Chain Support', function() {
+  describe('Supply Chain Support', function() {
     const nodePropsOrder = ['asi', 'sid', 'hp', 'rid', 'name', 'domain'];
     let bidRequests;
     let schainConfig;
