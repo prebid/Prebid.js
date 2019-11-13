ae6b818b3e8a99420ce22641e51b2163ac485128
Rich Snapp <richard.snapp@rakuten.com>
2019-11-13 11:47:15 -0700

    Implementation of setBidderConfig and bidder-specific data (#4334)
    * initial implementation of setBidderConfig
    
    * fix ie11 test errors
    
    * Support new setBidderConfig format. Include props from both config and bidderConfig in _getConfig
    
    * Use core-js Set to avoid issues with IE
    
    * Fix tests in IE
    
    * put registerSyncs back on bidderFactory
    
    * run bidder event methods with bidder config enabled

--- src/config.js
+++ src/config.js
@@ -4,8 +4,10 @@
 import { isValidPriceConfig } from './cpmBucketManager';
 import find from 'core-js/library/fn/array/find';
 import includes from 'core-js/library/fn/array/includes';
+import Set from 'core-js/library/fn/set';
 import { parseQS } from './url';
 
+const from = require('core-js/library/fn/array/from');
 const utils = require('./utils');
 const CONSTANTS = require('./constants');
 
@@ -49,6 +51,8 @@ export function newConfig() {
   let listeners = [];
   let defaults;
   let config;
+  let bidderConfig;
+  let currBidder = null;
 
   function resetConfig() {
     defaults = {};
@@ -86,7 +90,7 @@ export function newConfig() {
         if (validatePriceGranularity(val)) {
           if (typeof val === 'string') {
             this._priceGranularity = (hasGranularity(val)) ? val : GRANULARITY_OPTIONS.MEDIUM;
-          } else if (typeof val === 'object') {
+          } else if (utils.isPlainObject(val)) {
             this._customPriceBucket = val;
             this._priceGranularity = GRANULARITY_OPTIONS.CUSTOM;
             utils.logMessage('Using custom price granularity');
@@ -111,7 +115,7 @@ export function newConfig() {
           if (validatePriceGranularity(val[item])) {
             if (typeof val === 'string') {
               aggregate[item] = (hasGranularity(val[item])) ? val[item] : this._priceGranularity;
-            } else if (typeof val === 'object') {
+            } else if (utils.isPlainObject(val)) {
               aggregate[item] = val[item];
               utils.logMessage(`Using custom price granularity for ${item}`);
             }
@@ -182,6 +186,7 @@ export function newConfig() {
     }
 
     config = newConfig;
+    bidderConfig = {};
 
     function hasGranularity(val) {
       return find(Object.keys(GRANULARITY_OPTIONS), option => val === GRANULARITY_OPTIONS[option]);
@@ -196,7 +201,7 @@ export function newConfig() {
         if (!hasGranularity(val)) {
           utils.logWarn('Prebid Warning: setPriceGranularity was called with invalid setting, using `medium` as default.');
         }
-      } else if (typeof val === 'object') {
+      } else if (utils.isPlainObject(val)) {
         if (!isValidPriceConfig(val)) {
           utils.logError('Invalid custom price value passed to `setPriceGranularity()`');
           return false;
@@ -204,6 +209,33 @@ export function newConfig() {
       }
       return true;
     }
+  }
+
+  /**
+   * Returns base config with bidder overrides (if there is currently a bidder)
+   * @private
+   */
+  function _getConfig() {
+    if (currBidder && bidderConfig && utils.isPlainObject(bidderConfig[currBidder])) {
+      let currBidderConfig = bidderConfig[currBidder];
+      const configTopicSet = new Set(Object.keys(config).concat(Object.keys(currBidderConfig)));
+
+      return from(configTopicSet).reduce((memo, topic) => {
+        if (!currBidderConfig[topic]) {
+          memo[topic] = config[topic];
+        } else if (!config[topic]) {
+          memo[topic] = currBidderConfig[topic];
+        } else {
+          if (utils.isPlainObject(currBidderConfig[topic])) {
+            memo[topic] = Object.assign({}, config[topic], currBidderConfig[topic]);
+          } else {
+            memo[topic] = currBidderConfig[topic];
+          }
+        }
+        return memo;
+      }, {});
+    }
+    return Object.assign({}, config);
   }
 
   /*
@@ -217,10 +249,17 @@ export function newConfig() {
   function getConfig(...args) {
     if (args.length <= 1 && typeof args[0] !== 'function') {
       const option = args[0];
-      return option ? utils.deepAccess(config, option) : config;
+      return option ? utils.deepAccess(_getConfig(), option) : _getConfig();
     }
 
     return subscribe(...args);
+  }
+
+  /**
+   * Internal API for modules (such as prebid-server) that might need access to all bidder config
+   */
+  function getBidderConfig() {
+    return bidderConfig;
   }
 
   /*
@@ -228,7 +267,7 @@ export function newConfig() {
    * listeners that were added by the `subscribe` function
    */
   function setConfig(options) {
-    if (typeof options !== 'object') {
+    if (!utils.isPlainObject(options)) {
       utils.logError('setConfig options must be an object');
       return;
     }
@@ -239,7 +278,7 @@ export function newConfig() {
     topics.forEach(topic => {
       let option = options[topic];
 
-      if (typeof defaults[topic] === 'object' && typeof option === 'object') {
+      if (utils.isPlainObject(defaults[topic]) && utils.isPlainObject(option)) {
         option = Object.assign({}, defaults[topic], option);
       }
 
@@ -254,7 +293,7 @@ export function newConfig() {
    * @param {object} options
    */
   function setDefaults(options) {
-    if (typeof defaults !== 'object') {
+    if (!utils.isPlainObject(defaults)) {
       utils.logError('defaults must be an object');
       return;
     }
@@ -328,13 +367,68 @@ export function newConfig() {
       .forEach(listener => listener.callback(options));
   }
 
+  function setBidderConfig(config) {
+    try {
+      check(config);
+      config.bidders.forEach(bidder => {
+        if (!bidderConfig[bidder]) {
+          bidderConfig[bidder] = {};
+        }
+        Object.keys(config.config).forEach(topic => {
+          let option = config.config[topic];
+          if (utils.isPlainObject(option)) {
+            bidderConfig[bidder][topic] = Object.assign({}, bidderConfig[bidder][topic] || {}, option);
+          } else {
+            bidderConfig[bidder][topic] = option;
+          }
+        });
+      });
+    } catch (e) {
+      utils.logError(e);
+    }
+    function check(obj) {
+      if (!utils.isPlainObject(obj)) {
+        throw 'setBidderConfig bidder options must be an object';
+      }
+      if (!(Array.isArray(obj.bidders) && obj.bidders.length)) {
+        throw 'setBidderConfig bidder options must contain a bidders list with at least 1 bidder';
+      }
+      if (!utils.isPlainObject(obj.config)) {
+        throw 'setBidderConfig bidder options must contain a config object';
+      }
+    }
+  }
+
+  /**
+   * Internal functions for core to execute some synchronous code while having an active bidder set.
+   */
+  function runWithBidder(bidder, fn) {
+    currBidder = bidder;
+    try {
+      return fn();
+    } finally {
+      currBidder = null;
+    }
+  }
+  function callbackWithBidder(bidder) {
+    return function(cb) {
+      return function(...args) {
+        return runWithBidder(bidder, utils.bind.call(cb, this, ...args))
+      }
+    }
+  }
+
   resetConfig();
 
   return {
     getConfig,
     setConfig,
     setDefaults,
-    resetConfig
+    resetConfig,
+    runWithBidder,
+    callbackWithBidder,
+    setBidderConfig,
+    getBidderConfig
   };
 }
 
