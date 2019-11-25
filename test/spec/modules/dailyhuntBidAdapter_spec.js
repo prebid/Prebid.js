2f1b10e113f8e982a3dc99af2327b9ee4d479eb8
Mitesh Thakor <mitesh.thakor@dailyhunt.in>
2019-11-25 20:58:31 +0530

    make test coverage 92%

--- test/spec/modules/dailyhuntBidAdapter_spec.js
+++ test/spec/modules/dailyhuntBidAdapter_spec.js
@@ -190,6 +190,38 @@ describe('DailyhuntAdapter', function () {
       ]
     ];
 
+    let bannerResponse = {
+      'id': 'da32def7-6779-403c-ada7-0b201dbc9744',
+      'seatbid': [
+        {
+          'bid': [
+            {
+              'id': '3db3773286ee59',
+              'impid': '1',
+              'price': 0.14,
+              'adm': "<!DOCTYPE HTML>\r\n<html lang=\"en-us\">\r\n<head>\r\n\t<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">\r\n\t<title>Widgets Magazine</title>\r\n\t<style type=\"text/css\" media=\"screen\">\r\n\t</style>\r\n\t<script src=\"https://cdn.tercept.com/js/trcpt?account_id=TCPT-1547\"></script>\r\n\t<script src=\"https://www.googletagservices.com/tag/js/gpt.js\"></script>\r\n\t<script>\r\n\t\tvar googletag = googletag || {};\r\n\t\tgoogletag.cmd = googletag.cmd || [];\r\n\t\tgoogletag.cmd.push(function() {\r\n\t\t\twindow && window.tercept && window.tercept.init && window.tercept.init(83414793);\r\n\t\t});\r\n\r\n\t\t googletag.pubads().addEventListener('slotRenderEnded', function(event) {\r\n\t\t    if (event.slot.getSlotElementId() == \"div-gpt-ad-1528458606795-1\") {\r\n\t\t\tvar containsAd = !event.isEmpty;\r\n\t\t\tif (!containsAd) {\r\n\t\t\t  parent.AdManager.renderFallbackAd(this);     \r\n\t\t\t}\r\n\t\t    }\r\n\t\t  });\r\n\r\n\t\t googletag.cmd.push(function() {\r\n\t\t   var common_wap_firstpage = googletag.defineSlot('/83414793/Common_Wap_Firstpage', [300, 250], 'div-gpt-ad-1528458606795-1').addService(googletag.pubads());\r\n\t\t   (window && window.tercept && window.tercept.setTargeting)?(window.tercept.setTargeting(common_wap_firstpage)):((common_wap_firstpage).setTargeting('tcpt', ['TCPT_NL']));\r\n\r\n\t\t   googletag.pubads().enableSingleRequest();\r\n\t\t   googletag.pubads().collapseEmptyDivs();\r\n\t\t   googletag.enableServices();\r\n\t\t });\r\n\t</script>\r\n\r\n</head>\r\n\r\n<body>\r\n\r\n<!-- /83414793/Common_WapMiddle -->\r\n<div id='div-gpt-ad-1528458606795-1' style='margin:0 auto; width: 300px; height: auto;'>\r\n<script>\r\ngoogletag.cmd.push(function() { googletag.display('div-gpt-ad-1528458606795-1'); });\r\n</script>\r\n</div>\r\n\r\n</body>\r\n</html><img class=\"ad_beacon\" style=\"display: none;\" src=\"http://money.dailyhunt.in/impression?uniqueId=asd5ddbf014cac993.66466212&adId=66658&campaignId=55044&adPlacement=web&partnerRef=&city=&state=&country=india&clientId=asd\" />",
+              'adid': '66658',
+              'crid': 'asd5ddbf014cac993.66466212',
+              'dealid': 'asd5ddbf014cac993.66466212',
+              'w': 300,
+              'h': 250,
+              'ext': {
+                'prebid': {
+                  'type': 'banner'
+                }
+              }
+            }
+          ],
+          'seat': 'dailyhunt'
+        }
+      ],
+      'ext': {
+        'responsetimemillis': {
+          'dailyhunt': 119
+        }
+      }
+    };
+
     it('should get correct native bid response', function () {
       let expectedResponse = [
         {
@@ -229,6 +261,31 @@ describe('DailyhuntAdapter', function () {
         }]
       }
       let result = spec.interpretResponse({ body: nativeResponse }, { bidderRequest });
+      expect(Object.keys(result[0])).to.have.members(Object.keys(expectedResponse[0]));
+    });
+
+    it('should get correct banner bid response', function () {
+      let expectedResponse = [
+        {
+          requestId: '12345',
+          cpm: 0.14,
+          creativeId: 'asd5ddbf014cac993.66466212',
+          width: 300,
+          height: 250,
+          ttl: 360,
+          netRevenue: true,
+          currency: 'USD',
+          ad: "<!DOCTYPE HTML>\r\n<html lang=\"en-us\">\r\n<head>\r\n\t<meta http-equiv=\"Content-type\" content=\"text/html; charset=utf-8\">\r\n\t<title>Widgets Magazine</title>\r\n\t<style type=\"text/css\" media=\"screen\">\r\n\t</style>\r\n\t<script src=\"https://cdn.tercept.com/js/trcpt?account_id=TCPT-1547\"></script>\r\n\t<script src=\"https://www.googletagservices.com/tag/js/gpt.js\"></script>\r\n\t<script>\r\n\t\tvar googletag = googletag || {};\r\n\t\tgoogletag.cmd = googletag.cmd || [];\r\n\t\tgoogletag.cmd.push(function() {\r\n\t\t\twindow && window.tercept && window.tercept.init && window.tercept.init(83414793);\r\n\t\t});\r\n\r\n\t\t googletag.pubads().addEventListener('slotRenderEnded', function(event) {\r\n\t\t    if (event.slot.getSlotElementId() == \"div-gpt-ad-1528458606795-1\") {\r\n\t\t\tvar containsAd = !event.isEmpty;\r\n\t\t\tif (!containsAd) {\r\n\t\t\t  parent.AdManager.renderFallbackAd(this);     \r\n\t\t\t}\r\n\t\t    }\r\n\t\t  });\r\n\r\n\t\t googletag.cmd.push(function() {\r\n\t\t   var common_wap_firstpage = googletag.defineSlot('/83414793/Common_Wap_Firstpage', [300, 250], 'div-gpt-ad-1528458606795-1').addService(googletag.pubads());\r\n\t\t   (window && window.tercept && window.tercept.setTargeting)?(window.tercept.setTargeting(common_wap_firstpage)):((common_wap_firstpage).setTargeting('tcpt', ['TCPT_NL']));\r\n\r\n\t\t   googletag.pubads().enableSingleRequest();\r\n\t\t   googletag.pubads().collapseEmptyDivs();\r\n\t\t   googletag.enableServices();\r\n\t\t });\r\n\t</script>\r\n\r\n</head>\r\n\r\n<body>\r\n\r\n<!-- /83414793/Common_WapMiddle -->\r\n<div id='div-gpt-ad-1528458606795-1' style='margin:0 auto; width: 300px; height: auto;'>\r\n<script>\r\ngoogletag.cmd.push(function() { googletag.display('div-gpt-ad-1528458606795-1'); });\r\n</script>\r\n</div>\r\n\r\n</body>\r\n</html><img class=\"ad_beacon\" style=\"display: none;\" src=\"http://money.dailyhunt.in/impression?uniqueId=asd5ddbf014cac993.66466212&adId=66658&campaignId=55044&adPlacement=web&partnerRef=&city=&state=&country=india&clientId=asd\" />",
+        }
+      ];
+      let bidderRequest = {
+        bids: [{
+          bidId: '3db3773286ee59',
+          adUnitCode: 'code',
+          'requestId': '12345'
+        }]
+      }
+      let result = spec.interpretResponse({ body: bannerResponse }, bidderRequest);
       expect(Object.keys(result[0])).to.have.members(Object.keys(expectedResponse[0]));
     });
   })
