10e5709e90c0a100a73f355bb1a77ff87a3e1e6c
Eric Harper <eharper@rubiconproject.com>
2019-12-03 18:16:59 -0500

    url encoding usp privacy string

--- modules/consentManagementUsp.js
+++ modules/consentManagementUsp.js
@@ -213,7 +213,7 @@ function uspapiFailed(errMsg, hookConfig, extraArgs) {
  */
 function storeUspConsentData(consentObject) {
   if (consentObject && consentObject.usPrivacy) {
-    consentData = consentObject.usPrivacy;
+    consentData = encodeURIComponent(consentObject.usPrivacy);
     uspDataHandler.setConsentData(consentData);
   }
 }
