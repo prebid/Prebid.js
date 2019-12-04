b3e736a4950a2f8bc77f9a23cd63cdafde167ada
Nick <bansawbanchee@users.noreply.github.com>
2019-12-04 11:42:39 -0500

    Add britepool userid submodule (#4314)
    * * Added BritePool User ID Submodule
    
    * * Refactor getId() and allow it to return a sync value from getter()
    
    * * Pull only primaryBPID key out of hardened API response
    
    * add check for decoded primaryBPID value and update readme
    
    * * Added BritePool User ID Submodule
    
    * * Refactor getId() and allow it to return a sync value from getter()
    
    * * Pull only primaryBPID key out of hardened API response
    
    * add check for decoded primaryBPID value and update readme
    
    * update userId_spec.js tests
    
    * update userId_spec.js tests and add britepoolIdSystem to submodules.json
    
    * moved our module specific tests to own spec file
    
    * * Update to use getId() with callback key
    * Add test for our getId() returning value or callback
    
    * * Revert comment to "Use existing cookie"
    
    * add support for prebid server
    
    * add comma back to submodules.json
    
    * updating markdown for email address
    
    * * Allow the britepool call without parameters (identifiers)
    
    * fixed merging error of double ||
    
    * * Fix the immediate value response to be in id key
    
    * * Fixed test which was expecting id key
    
    * * Touch
    
    * * Update doc
    
    * * Suggested changes to move britepoolIdSystem out of userId default
    
    * * Change weird backticks to single quotes
    
    * * Added functional identifiers

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -698,7 +698,7 @@ const OPEN_RTB_PROTOCOL = {
     }
 
     const bidUserId = utils.deepAccess(bidRequests, '0.bids.0.userId');
-    if (bidUserId && typeof bidUserId === 'object' && (bidUserId.tdid || bidUserId.pubcid || bidUserId.parrableid || bidUserId.lipb || bidUserId.id5id || bidUserId.criteoId)) {
+    if (bidUserId && typeof bidUserId === 'object' && (bidUserId.tdid || bidUserId.pubcid || bidUserId.parrableid || bidUserId.lipb || bidUserId.id5id || bidUserId.criteoId || bidUserId.britepoolid)) {
       utils.deepSetValue(request, 'user.ext.eids', []);
 
       if (bidUserId.tdid) {
@@ -761,6 +761,15 @@ const OPEN_RTB_PROTOCOL = {
           source: 'criteo.com',
           uids: [{
             id: bidUserId.criteoId
+          }]
+        });
+      }
+
+      if (bidUserId.britepoolid) {
+        request.user.ext.eids.push({
+          source: 'britepool.com',
+          uids: [{
+            id: bidUserId.britepoolid
           }]
         });
       }
