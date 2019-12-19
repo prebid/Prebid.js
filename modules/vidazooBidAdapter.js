3ec74e324d23df60dcf4e1e5ad578377a666df5e
Udi Talias <udi.talias@gmail.com>
2019-12-19 13:29:43 +0200

    refactor(module): using `utils.setDataInLocalStorage` and `utils.getDataFromLocalStorage`

--- modules/vidazooBidAdapter.js
+++ modules/vidazooBidAdapter.js
@@ -1,7 +1,7 @@
 import * as utils from '../src/utils';
 import { registerBidder } from '../src/adapters/bidderFactory';
 import { BANNER } from '../src/mediaTypes';
-export const URL = 'https://prebid.cootlogix.com';
+export const URL = '//prebid.cootlogix.com';
 const BIDDER_CODE = 'vidazoo';
 const CURRENCY = 'USD';
 const TTL_SECONDS = 60 * 5;
@@ -14,18 +14,15 @@ const EXTERNAL_SYNC_TYPE = {
   IFRAME: 'iframe',
   IMAGE: 'image'
 };
-
 function isBidRequestValid(bid) {
   const params = bid.params || {};
   return !!(params.cId && params.pId);
 }
-
 function buildRequest(bid, topWindowUrl, size, bidderRequest) {
   const { params, bidId } = bid;
   const { bidFloor, cId, pId, ext } = params;
   // Prebid's util function returns AppNexus style sizes (i.e. 300x250)
   const [width, height] = size.split('x');
-
   const dto = {
     method: 'POST',
     url: `${URL}/prebid/${cId}`,
@@ -36,19 +33,16 @@ function buildRequest(bid, topWindowUrl, size, bidderRequest) {
       bidId: bidId,
       publisherId: pId,
       consent: bidderRequest.gdprConsent && bidderRequest.gdprConsent.consentString,
-      sessionId: getLocalStorage(SESSION_NAME) || '',
+      sessionId: utils.getDataFromLocalStorage(SESSION_NAME) || '',
       width,
       height
     }
   };
-
   utils._each(ext, (value, key) => {
     dto.data['ext.' + key] = value;
   });
-
   return dto;
 }
-
 function buildRequests(validBidRequests, bidderRequest) {
   const topWindowUrl = bidderRequest.refererInfo.referer;
   const requests = [];
@@ -61,7 +55,6 @@ function buildRequests(validBidRequests, bidderRequest) {
   });
   return requests;
 }
-
 function interpretResponse(serverResponse, request) {
   if (!serverResponse || !serverResponse.body) {
     return [];
@@ -70,11 +63,9 @@ function interpretResponse(serverResponse, request) {
   if (!ad || !price) {
     return [];
   }
-
   if (sessionId) {
-    setLocalStorage(SESSION_NAME, sessionId);
+    utils.setDataInLocalStorage(SESSION_NAME, sessionId);
   }
-
   const { bidId, width, height } = request.data;
   try {
     return [{
@@ -92,31 +83,14 @@ function interpretResponse(serverResponse, request) {
     return [];
   }
 }
-
-function setLocalStorage(key, value) {
-  try {
-    window.localStorage.setItem(key, value);
-  } catch (ex) {
-  }
-}
-
-function getLocalStorage(key) {
-  try {
-    return window.localStorage.getItem(key);
-  } catch (ex) {
-  }
-}
-
 function getUserSyncs(syncOptions, responses) {
   const { iframeEnabled, pixelEnabled } = syncOptions;
-
   if (iframeEnabled) {
     return [{
       type: 'iframe',
-      url: 'https://static.cootlogix.com/basev/sync/user_sync.html'
+      url: '//static.cootlogix.com/basev/sync/user_sync.html'
     }];
   }
-
   if (pixelEnabled) {
     const lookup = {};
     const syncs = [];
@@ -140,10 +114,8 @@ function getUserSyncs(syncOptions, responses) {
     });
     return syncs;
   }
-
   return [];
 }
-
 export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER],
@@ -152,5 +124,4 @@ export const spec = {
   interpretResponse,
   getUserSyncs
 };
-
 registerBidder(spec);
