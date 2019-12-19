1feb1ddba99c338f92fd95cd408d05a477fd5ac1
Deepthi Neeladri Sravana <dsravana@C02VH0WGHTDG.local>
2019-12-19 13:46:48 +0530

    legacy ccpa support

--- modules/oneVideoBidAdapter.js
+++ modules/oneVideoBidAdapter.js
@@ -243,6 +243,7 @@ function getRequestData(bid, consentData, bidRequest) {
         }
       };
     }
+    //CCPA support
     if (bidRequest && bidRequest.uspConsent) {
       bidData.regs.ext.us_privacy = bidRequest.uspConsent
     }
