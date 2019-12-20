9996f3e76c83c30a37c8c80c40b8c97efd655981
Shrikant Patwari <shrikant.patwari@yuktamedia.com>
2019-12-20 18:26:52 +0530

    Updated YuktaMedia Analytics Adapter as per 3.0 prebid changes

--- modules/yuktamediaAnalyticsAdapter.js
+++ modules/yuktamediaAnalyticsAdapter.js
@@ -1,13 +1,13 @@
-import { ajax } from 'src/ajax';
-import adapter from 'src/AnalyticsAdapter';
-import adaptermanager from 'src/adaptermanager';
-import CONSTANTS from 'src/constants.json';
-import * as url from 'src/url';
-import * as utils from 'src/utils';
+import { ajax } from '../src/ajax';
+import adapter from '../src/AnalyticsAdapter';
+import adapterManager from '../src/adapterManager';
+import CONSTANTS from '../src/constants.json';
+import * as url from '../src/url';
+import * as utils from '../src/utils';
 
 const emptyUrl = '';
 const analyticsType = 'endpoint';
-const yuktamediaAnalyticsVersion = 'v1.0.0';
+const yuktamediaAnalyticsVersion = 'v2.0.0';
 
 let initOptions;
 let auctionTimestamp;
@@ -55,7 +55,7 @@ function mapBidRequests(params) {
         requestId: bid.bidderRequestId,
         auctionId: bid.auctionId,
         transactionId: bid.transactionId,
-        sizes: utils.parseSizesInput(bid.sizes).toString(),
+        sizes: utils.parseSizesInput(bid.mediaTypes.banner.sizes).toString(),
         renderStatus: 1,
         requestTimestamp: params.auctionStart
       });
@@ -109,10 +109,10 @@ function mapBidResponse(bidResponse, status) {
 }
 
 function send(data, status) {
-  let location = utils.getTopWindowLocation();
+  let location = utils.getWindowLocation();
   let secure = location.protocol == 'https:';
   if (typeof data !== 'undefined' && typeof data.auctionInit !== 'undefined') {
-    data.auctionInit = Object.assign({ host: location.host, path: location.pathname, hash: location.hash, search: location.search }, data.auctionInit);
+    data.auctionInit = Object.assign({ host: location.host, path: location.pathname, search: location.search }, data.auctionInit);
   }
   data.initOptions = initOptions;
 
@@ -127,7 +127,7 @@ function send(data, status) {
     }
   });
 
-  ajax(yuktamediaAnalyticsRequestUrl, undefined, JSON.stringify(data), { method: 'POST', contentType: 'application/json' });
+  ajax(yuktamediaAnalyticsRequestUrl, undefined, JSON.stringify(data), { method: 'POST', contentType: 'text/plain' });
 }
 
 yuktamediaAnalyticsAdapter.originEnableAnalytics = yuktamediaAnalyticsAdapter.enableAnalytics;
@@ -136,7 +136,7 @@ yuktamediaAnalyticsAdapter.enableAnalytics = function (config) {
   yuktamediaAnalyticsAdapter.originEnableAnalytics(config);
 };
 
-adaptermanager.registerAnalyticsAdapter({
+adapterManager.registerAnalyticsAdapter({
   adapter: yuktamediaAnalyticsAdapter,
   code: 'yuktamedia'
 });
