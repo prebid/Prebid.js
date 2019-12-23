435d8baa6518d77638bbc3afa90e4cfc02296ba3
Valentin Souche <valentin.souche@teads.tv>
2019-12-23 11:04:29 +0100

    Add us privacy 2.X

--- test/spec/modules/teadsBidAdapter_spec.js
+++ test/spec/modules/teadsBidAdapter_spec.js
@@ -109,6 +109,22 @@ describe('teadsBidAdapter', () => {
       expect(request.method).to.equal('POST');
     });
 
+    it('should send US Privacy to endpoint', function() {
+      let usPrivacy = 'OHHHFCP1'
+      let bidderRequest = {
+        'auctionId': '1d1a030790a475',
+        'bidderRequestId': '22edbae2733bf6',
+        'timeout': 3000,
+        'uspConsent': usPrivacy
+      };
+
+      const request = spec.buildRequests(bidRequests, bidderRequest);
+      const payload = JSON.parse(request.data);
+
+      expect(payload.us_privacy).to.exist;
+      expect(payload.us_privacy).to.equal(usPrivacy);
+    });
+
     it('should send GDPR to endpoint', function() {
       let consentString = 'JRJ8RKfDeBNsERRDCSAAZ+A==';
       let bidderRequest = {
