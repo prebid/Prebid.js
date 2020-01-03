2e32e3d9f0a63423c8f59ef7d5f9c68eb44b31b0
Rich Snapp <rich.snapp@gmail.com>
2020-01-03 16:29:54 -0700

    third-party scripts loaded with loadExternalScript

--- test/mocks/adloaderStub.js
+++ test/mocks/adloaderStub.js
@@ -10,7 +10,10 @@ function createStub() {
   return sinon.stub(adloader, 'loadExternalScript').callsFake((...args) => {
     if (typeof args[2] === 'function') {
       args[2]();
+    } else if (typeof args[3] === 'function') {
+      args[3]();
     }
+    return document.createElement('script');
   });
 }
 
