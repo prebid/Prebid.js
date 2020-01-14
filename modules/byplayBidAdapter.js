36c1dd400ee7d7febd90eabe49070bb8329fe153
Dai Sato <d-sato@tsumikiinc.com>
2020-01-14 17:59:57 +0900

    remove tracker

--- modules/byplayBidAdapter.js
+++ modules/byplayBidAdapter.js
@@ -7,9 +7,6 @@ import { VIDEO } from '../src/mediaTypes';
 const BIDDER_CODE = 'byplay';
 const ENDPOINT_URL = 'https://tasp0g98f2.execute-api.ap-northeast-1.amazonaws.com/v1/bidder';
 const VIDEO_PLAYER_URL = 'https://cdn.byplay.net/prebid-byplay-v2.js';
-const A1_CODE = '!function(e,r,t,a){if(!e.a1tracker){var n=e.a1tracker={};n.VERSION="v0.6.1",n.queue=n.queue||[];for(var s=0;s<t.length;s++){var u=t[s];n[u]=n[u]||function(e){return function(){for(var r=arguments.length,t=Array(r),a=0;a<r;a++)t[a]=arguments[a];n.queue.push({name:e,arguments:t.slice()})}}(u)}var c=r.createElement("script"),i=r.getElementsByTagName("script")[0];c.async=!0,c.src="//img.ak.impact-ad.jp/ut/a1/tracking.min.js",i.parentNode.insertBefore(c,i)}}(window,document,["ready","send","sendEvent","setParams","initParams","sendCustomerId","getSegments","getUserId"]);'
-const A1_OID = 'b13242ebb93a372e';
-const A1_COOKIE_NAME = 'a1_segment_ids';
 
 export const spec = {
   code: BIDDER_CODE,
@@ -18,8 +15,6 @@ export const spec = {
     return !!bid.params.sectionId;
   },
   buildRequests: function(validBidRequests) {
-    const segments = getSegments();
-
     return validBidRequests.map(req => {
       const payload = {
         requestId: req.bidId,
@@ -55,8 +50,6 @@ export const spec = {
       renderer: createRenderer()
     };
 
-    setSegments();
-
     return [bidResponse];
   }
 };
@@ -71,35 +64,6 @@ function createRenderer() {
   });
 
   return renderer;
-}
-
-function getSegments() {
-  const ids = utils.getCookie(A1_COOKIE_NAME);
-
-  return ids ? { 'seg': ids } : {};
-}
-
-function setSegments() {
-  pageTracking(segments => {
-    utils.setCookie(A1_COOKIE_NAME, segments.map(x => `${x}`).join('_'));
-  });
-}
-
-function pageTracking(f) {
-  const script = document.createElement('script');
-
-  script.type = 'text/javascript';
-  script.textContent = A1_CODE;
-
-  document.head.appendChild(script);
-
-  a1tracker.send(A1_OID, (error, data) => {
-    if (error) {
-      throw error;
-    }
-
-    f(data.segments);
-  });
 }
 
 registerBidder(spec);
