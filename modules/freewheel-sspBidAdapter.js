63a8dd6ca6dc10ab1686ff9d972a161d126f2b83
Frank Wang <zhewang@freewheel.tv>
2020-01-24 14:18:55 -0500

    rearrange size logic

--- modules/freewheel-sspBidAdapter.js
+++ modules/freewheel-sspBidAdapter.js
@@ -284,15 +284,15 @@ export const spec = {
     }
 
     var playerSize = [];
-    if (currentBidRequest.sizes) {
-      // Backward compatible code, in case size still pass by sizes in bid request
-      playerSize = getBiggerSize(currentBidRequest.sizes);
-    } else if (currentBidRequest.mediaTypes.video && currentBidRequest.mediaTypes.video.playerSize) {
+    if (currentBidRequest.mediaTypes.video && currentBidRequest.mediaTypes.video.playerSize) {
       // If mediaTypes is video, get size from mediaTypes.video.playerSize per http://prebid.org/blog/pbjs-3
       playerSize = currentBidRequest.mediaTypes.video.playerSize;
     } else if (currentBidRequest.mediaTypes.banner.sizes) {
       // If mediaTypes is banner, get size from mediaTypes.banner.sizes per http://prebid.org/blog/pbjs-3
       playerSize = getBiggerSizeWithLimit(currentBidRequest.mediaTypes.banner.sizes, currentBidRequest.mediaTypes.banner.minSizeLimit, currentBidRequest.mediaTypes.banner.maxSizeLimit);
+    } else {
+      // Backward compatible code, in case size still pass by sizes in bid request
+      playerSize = getBiggerSize(currentBidRequest.sizes);
     }
 
     if (playerSize[0] > 0 || playerSize[1] > 0) {
@@ -317,15 +317,15 @@ export const spec = {
   interpretResponse: function(serverResponse, request) {
     var bidrequest = request.bidRequest;
     var playerSize = [];
-    if (bidrequest.sizes) {
-      // Backward compatible code, in case size still pass by sizes in bid request
-      playerSize = getBiggerSize(bidrequest.sizes);
-    } else if (bidrequest.mediaTypes.video && bidrequest.mediaTypes.video.playerSize) {
+    if (bidrequest.mediaTypes.video && bidrequest.mediaTypes.video.playerSize) {
       // If mediaTypes is video, get size from mediaTypes.video.playerSize per http://prebid.org/blog/pbjs-3
       playerSize = bidrequest.mediaTypes.video.playerSize;
     } else if (bidrequest.mediaTypes.banner.sizes) {
       // If mediaTypes is banner, get size from mediaTypes.banner.sizes per http://prebid.org/blog/pbjs-3
       playerSize = getBiggerSizeWithLimit(bidrequest.mediaTypes.banner.sizes, bidrequest.mediaTypes.banner.minSizeLimit, bidrequest.mediaTypes.banner.maxSizeLimit);
+    } else {
+      // Backward compatible code, in case size still pass by sizes in bid request
+      playerSize = getBiggerSize(bidrequest.sizes);
     }
 
     if (typeof serverResponse == 'object' && typeof serverResponse.body == 'string') {
