4bfc23ee97be76d95c575966f627037f07038eb8
IX Prebid Support <pdu-supply-prebid@indexexchange.com>
2020-02-21 11:58:52 -0500

    IX Adapter - Increase banner TTL to 300s

--- test/spec/modules/ixBidAdapter_spec.js
+++ test/spec/modules/ixBidAdapter_spec.js
@@ -807,7 +807,7 @@ describe('IndexexchangeAdapter', function () {
           mediaType: 'banner',
           ad: '<a target="_blank" href="http://www.indexexchange.com"></a>',
           currency: 'USD',
-          ttl: 35,
+          ttl: 300,
           netRevenue: true,
           dealId: undefined,
           meta: {
@@ -834,7 +834,7 @@ describe('IndexexchangeAdapter', function () {
           mediaType: 'banner',
           ad: '<a target="_blank" href="http://www.indexexchange.com"></a>',
           currency: 'USD',
-          ttl: 35,
+          ttl: 300,
           netRevenue: true,
           dealId: undefined,
           meta: {
@@ -860,7 +860,7 @@ describe('IndexexchangeAdapter', function () {
           mediaType: 'banner',
           ad: '<a target="_blank" href="http://www.indexexchange.com"></a>',
           currency: 'JPY',
-          ttl: 35,
+          ttl: 300,
           netRevenue: true,
           dealId: undefined,
           meta: {
@@ -887,7 +887,7 @@ describe('IndexexchangeAdapter', function () {
           mediaType: 'banner',
           ad: '<a target="_blank" href="http://www.indexexchange.com"></a>',
           currency: 'USD',
-          ttl: 35,
+          ttl: 300,
           netRevenue: true,
           dealId: 'deal',
           meta: {
