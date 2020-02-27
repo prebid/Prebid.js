a823a3c24ad92c54c741196f440c44dead47640e
Harshad Mane <harshad.mane@pubmatic.com>
2020-02-27 12:00:13 -0800

    adding unit test cases

---
+++ test/spec/modules/eids_spec.js
@@ -0,0 +1,89 @@
+import {createEidsArray} from 'modules/userId/eids.js';
+import {expect} from 'chai';
+
+// cases
+// Known user id sub-modules
+//		here tdid ext case will be handled
+//		here liveIntentId ext case will be handled
+// 		here custom digitrust case will be handled
+// unknown user id sub module not passed in array
+
+// 	Note: 	In unit tets cases for bidders, call the createEidsArray function over userId object that is used for calling fetchBids
+//			this way the request will stay consistent and unit test cases will not need lots of changes.
+
+describe('eids array generation for known sub-modules', function() {
+  it('pubCommonId', function() {
+    const userId = {
+      pubcid: 'some-random-id-value'
+    };
+    const newEids = createEidsArray(userId);
+    expect(newEids.length).to.equal(1);
+    expect(newEids[0]).to.deep.equal({
+      source: 'pubcid.org',
+      uids: [{id: 'some-random-id-value', atype: 1}]
+    });
+  });
+
+  it('unifiedId: ext generation', function() {
+    const userId = {
+      tdid: 'some-random-id-value'
+    };
+    const newEids = createEidsArray(userId);
+    expect(newEids.length).to.equal(1);
+    expect(newEids[0]).to.deep.equal({
+      source: 'adserver.org',
+      uids: [{id: 'some-random-id-value', atype: 1, ext: { rtiPartner: 'TDID' }}]
+    });
+  });
+
+  it('id5Id', function() {
+    const userId = {
+      id5id: 'some-random-id-value'
+    };
+    const newEids = createEidsArray(userId);
+    expect(newEids.length).to.equal(1);
+    expect(newEids[0]).to.deep.equal({
+      source: 'id5-sync.com',
+      uids: [{id: 'some-random-id-value', atype: 1}]
+    });
+  });
+
+  it('parrableId', function() {
+    const userId = {
+      parrableid: 'some-random-id-value'
+    };
+    const newEids = createEidsArray(userId);
+    expect(newEids.length).to.equal(1);
+    expect(newEids[0]).to.deep.equal({
+      source: 'parrable.com',
+      uids: [{id: 'some-random-id-value', atype: 1}]
+    });
+  });
+
+  it('identityLink', function() {
+    const userId = {
+      idl_env: 'some-random-id-value'
+    };
+    const newEids = createEidsArray(userId);
+    expect(newEids.length).to.equal(1);
+    expect(newEids[0]).to.deep.equal({
+      source: 'liveramp.com',
+      uids: [{id: 'some-random-id-value', atype: 1}]
+    });
+  });
+
+  it('liveIntentId; getValue call', function() {
+    const userId = {
+      lipb: {
+        lipbid: 'some-random-id-value',
+        segments: ['s1', 's2']
+      }
+    };
+    const newEids = createEidsArray(userId);
+    expect(newEids.length).to.equal(1);
+    expect(newEids[0]).to.deep.equal({
+      source: 'liveintent.com',
+      uids: [{id: 'some-random-id-value', atype: 1, ext: {segments: ['s1', 's2']}}]
+    });
+  });
+});
