d1f0ea1a2d2520c19260f2f2a6b2256ebec370d3
Neelanjan Sen <14229985+Fawke@users.noreply.github.com>
2020-03-17 23:30:04 +0530

    Fix failing circle-ci build job. (#4840)
    * fix lint errors
    
    * update browser versions
    
    * remove problematic test cases
    
    * remove addtional files
    
    * fix errors in adagioBidAdapter.js and adagioBidAdapter_spec.js file
    
    * change karma config
    
    * change config
    
    * update timeout values
    
    * resolve merge conflict
    
    * -update karma\n-fix userIdSpec unit tests\n-update version of karma-browserstack-launcher to 1.4.0
    
    * fix issue with userId_spec.js file
    
    * fix for realvu and sortable, potential fix for userId
    
    * resolve faulty merge
    
    * add a guideline to CONTRIBUGING.md and revert auction time back in userId_spec.js file

--- modules/adagioBidAdapter.js
+++ modules/adagioBidAdapter.js
@@ -83,12 +83,12 @@ if (canAccessTopWindow()) {
 }
 
 const _features = {
-  getPrintNumber: function(adUnitCode) {
+  getPrintNumber: function (adUnitCode) {
     const adagioAdUnit = _getOrAddAdagioAdUnit(adUnitCode);
     return adagioAdUnit.printNumber || 1;
   },
 
-  getPageDimensions: function() {
+  getPageDimensions: function () {
     const viewportDims = _features.getViewPortDimensions().split('x');
     const w = utils.getWindowTop();
     const body = w.document.body;
@@ -101,7 +101,7 @@ const _features = {
     return viewportDims[0] + 'x' + pageHeight;
   },
 
-  getViewPortDimensions: function() {
+  getViewPortDimensions: function () {
     let viewPortWidth;
     let viewPortHeight;
     const w = utils.getWindowTop();
@@ -118,7 +118,7 @@ const _features = {
     return viewPortWidth + 'x' + viewPortHeight;
   },
 
-  isDomLoading: function() {
+  isDomLoading: function () {
     const w = utils.getWindowTop();
     let performance = w.performance || w.msPerformance || w.webkitPerformance || w.mozPerformance;
     let domLoading = -1;
@@ -130,7 +130,7 @@ const _features = {
     return domLoading;
   },
 
-  getSlotPosition: function(element) {
+  getSlotPosition: function (element) {
     if (!element) return '';
 
     const w = utils.getWindowTop();
@@ -164,11 +164,11 @@ const _features = {
     return position.x + 'x' + position.y;
   },
 
-  getTimestamp: function() {
+  getTimestamp: function () {
     return Math.floor(new Date().getTime() / 1000) - new Date().getTimezoneOffset() * 60;
   },
 
-  getDevice: function() {
+  getDevice: function () {
     if (!canAccessTopWindow()) return false;
     const w = utils.getWindowTop();
     const ua = w.navigator.userAgent;
@@ -182,14 +182,14 @@ const _features = {
     return 2; // personal computers
   },
 
-  getBrowser: function() {
+  getBrowser: function () {
     const w = utils.getWindowTop();
     const ua = w.navigator.userAgent;
     const uaLowerCase = ua.toLowerCase();
     return /Edge\/\d./i.test(ua) ? 'edge' : uaLowerCase.indexOf('chrome') > 0 ? 'chrome' : uaLowerCase.indexOf('firefox') > 0 ? 'firefox' : uaLowerCase.indexOf('safari') > 0 ? 'safari' : uaLowerCase.indexOf('opera') > 0 ? 'opera' : uaLowerCase.indexOf('msie') > 0 || w.MSStream ? 'ie' : 'unknow';
   },
 
-  getOS: function() {
+  getOS: function () {
     const w = window.top;
     const ua = w.navigator.userAgent;
     const uaLowerCase = ua.toLowerCase();
@@ -198,9 +198,11 @@ const _features = {
 }
 
 function _pushInAdagioQueue(ob) {
-  if (!canAccessTopWindow()) return;
-  const w = utils.getWindowTop();
-  w.ADAGIO.queue.push(ob);
+  try {
+    if (!canAccessTopWindow()) return;
+    const w = utils.getWindowTop();
+    w.ADAGIO.queue.push(ob);
+  } catch (e) {}
 };
 
 function _getOrAddAdagioAdUnit(adUnitCode) {
@@ -342,42 +344,45 @@ export const spec = {
 
   supportedMediaType: SUPPORTED_MEDIA_TYPES,
 
-  isBidRequestValid: function(bid) {
+  isBidRequestValid: function (bid) {
     const { adUnitCode, auctionId, sizes, bidder, params, mediaTypes } = bid;
     const { organizationId, site, placement, adUnitElementId } = bid.params;
     let isValid = false;
 
-    if (canAccessTopWindow()) {
-      const w = utils.getWindowTop();
-      w.ADAGIO = w.ADAGIO || {};
-      w.ADAGIO.adUnits = w.ADAGIO.adUnits || {};
-      w.ADAGIO.pbjsAdUnits = w.ADAGIO.pbjsAdUnits || [];
-      isValid = !!(organizationId && site && placement && adUnitElementId);
-      const tempAdUnits = w.ADAGIO.pbjsAdUnits.filter((adUnit) => adUnit.code !== adUnitCode);
-      tempAdUnits.push({
-        code: adUnitCode,
-        sizes: (mediaTypes && mediaTypes.banner && Array.isArray(mediaTypes.banner.sizes)) ? mediaTypes.banner.sizes : sizes,
-        bids: [{
-          bidder,
-          params
-        }]
-      });
-      w.ADAGIO.pbjsAdUnits = tempAdUnits;
+    try {
+      if (canAccessTopWindow()) {
+        const w = utils.getWindowTop();
+        w.ADAGIO = w.ADAGIO || {};
+        w.ADAGIO.adUnits = w.ADAGIO.adUnits || {};
+        w.ADAGIO.pbjsAdUnits = w.ADAGIO.pbjsAdUnits || [];
+        isValid = !!(organizationId && site && placement && adUnitElementId);
+        const tempAdUnits = w.ADAGIO.pbjsAdUnits.filter((adUnit) => adUnit.code !== adUnitCode);
+        tempAdUnits.push({
+          code: adUnitCode,
+          sizes: (mediaTypes && mediaTypes.banner && Array.isArray(mediaTypes.banner.sizes)) ? mediaTypes.banner.sizes : sizes,
+          bids: [{
+            bidder,
+            params
+          }]
+        });
+        w.ADAGIO.pbjsAdUnits = tempAdUnits;
 
-      if (isValid === true) {
-        let printNumber = _computePrintNumber(adUnitCode);
-        w.ADAGIO.adUnits[adUnitCode] = {
-          auctionId: auctionId,
-          pageviewId: _getPageviewId(),
-          printNumber
-        };
+        if (isValid === true) {
+          let printNumber = _computePrintNumber(adUnitCode);
+          w.ADAGIO.adUnits[adUnitCode] = {
+            auctionId: auctionId,
+            pageviewId: _getPageviewId(),
+            printNumber
+          };
+        }
       }
+    } catch (e) {
+      return isValid;
     }
-
     return isValid;
   },
 
-  buildRequests: function(validBidRequests, bidderRequest) {
+  buildRequests: function (validBidRequests, bidderRequest) {
     // AdagioBidAdapter works when window.top can be reached only
     if (!bidderRequest.refererInfo.reachedTop) return [];
 
@@ -427,7 +432,7 @@ export const spec = {
     return requests;
   },
 
-  interpretResponse: function(serverResponse, bidRequest) {
+  interpretResponse: function (serverResponse, bidRequest) {
     let bidResponses = [];
     try {
       const response = serverResponse.body;
@@ -460,7 +465,7 @@ export const spec = {
     return bidResponses;
   },
 
-  getUserSyncs: function(syncOptions, serverResponses) {
+  getUserSyncs: function (syncOptions, serverResponses) {
     if (!serverResponses.length || serverResponses[0].body === '' || !serverResponses[0].body.userSyncs) {
       return false;
     }
