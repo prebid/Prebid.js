45989c68bae03a31e0459d346f494f33e164766f
Neelanjan Sen <neelanjan.sen@gmail.com>
2020-03-19 20:40:10 +0530

    increase timeout value

--- wdio.conf.js
+++ wdio.conf.js
@@ -47,13 +47,13 @@ exports.config = {
   capabilities: getCapabilities(),
   logLevel: 'silent', // Level of logging verbosity: silent | verbose | command | data | result | error
   coloredLogs: true,
-  waitforTimeout: 120000, // Default timeout for all waitFor* commands.
+  waitforTimeout: 3e5, // Default timeout for all waitFor* commands.
   connectionRetryTimeout: 60000, // Default timeout in milliseconds for request if Selenium Grid doesn't send response
   connectionRetryCount: 3, // Default request retries count
   framework: 'mocha',
   mochaOpts: {
     ui: 'bdd',
-    timeout: 120000,
+    timeout: 3e5,
     compilers: ['js:babel-register'],
   },
   // if you see error, update this to spec reporter and logLevel above to get detailed report.
