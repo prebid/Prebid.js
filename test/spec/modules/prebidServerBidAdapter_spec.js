9204046c201474aff626b477636aaf3159cf4687
idettman <idettman@rubiconproject.com>
2020-03-27 15:50:36 -0700

    add test

--- test/spec/modules/prebidServerBidAdapter_spec.js
+++ test/spec/modules/prebidServerBidAdapter_spec.js
@@ -1774,6 +1774,36 @@ describe('S2S Adapter', function () {
       expect(response).to.have.property('wurl', 'https://wurl.com?a=1&b=2');
     });
 
+    it('handles response cache from ext.prebid.targeting with wurl and removes invalid targeting', function () {
+      const s2sConfig = Object.assign({}, CONFIG, {
+        endpoint: 'https://prebidserverurl/openrtb2/auction?querystring=param'
+      });
+      config.setConfig({ s2sConfig });
+      const cacheResponse = utils.deepClone(RESPONSE_OPENRTB_VIDEO);
+      cacheResponse.seatbid.forEach(item => {
+        item.bid[0].wurl = 'https://wurl.com?a=1&b=2';
+        item.bid[0].ext.prebid.targeting = {
+          hb_uuid: 'a5ad3993',
+          hb_cache_host: 'prebid-cache.net',
+          hb_cache_path: '/cache',
+          hb_winurl: 'https://hbwinurl.com?a=1&b=2',
+          hb_bidid: '1234567890',
+        }
+      });
+      server.respondWith(JSON.stringify(cacheResponse));
+      adapter.callBids(VIDEO_REQUEST, BID_REQUESTS, addBidResponse, done, ajax);
+      server.respond();
+
+      sinon.assert.calledOnce(addBidResponse);
+      const response = addBidResponse.firstCall.args[1];
+
+      expect(response.adserverTargeting).to.deep.equal({
+        hb_uuid: 'a5ad3993',
+        hb_cache_host: 'prebid-cache.net',
+        hb_cache_path: '/cache'
+      })
+    });
+
     it('handles OpenRTB native responses', function () {
       sinon.stub(utils, 'getBidRequest').returns({
         adUnitCode: 'div-gpt-ad-1460505748561-0',
