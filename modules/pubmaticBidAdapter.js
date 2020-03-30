1c10e0fbb8427fc26ca1804005ec28a5b2c402c8
Harshad Mane <harshad.mane@pubmatic.com>
2020-03-29 23:42:40 -0700

    PubMatic: setting pm_seat and pm_dspid in bid response

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -989,7 +989,9 @@ export const spec = {
                 netRevenue: NET_REVENUE,
                 ttl: 300,
                 referrer: parsedReferrer,
-                ad: bid.adm
+                ad: bid.adm,
+                pm_seat: seatbidder.seat || null,
+                pm_dspid: bid.ext ? bid.ext.dspid : null
               };
               if (parsedRequest.imp && parsedRequest.imp.length > 0) {
                 parsedRequest.imp.forEach(req => {
