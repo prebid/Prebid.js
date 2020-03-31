9339b8ad450431854e7e32276e29bb224c5fab5a
Rich Snapp <rich.snapp@gmail.com>
2020-03-31 16:37:35 -0600

    add typescript definitions

---
+++ src/mediaTypes.d.ts
@@ -0,0 +1,6 @@
+export const NATIVE: MediaType;
+export const VIDEO: MediaType;
+export const BANNER: MediaType;
+export const ADPOD: VideoContext;
+export type MediaType = "native" | "video" | "banner";
+export type VideoContext = "adpod";
