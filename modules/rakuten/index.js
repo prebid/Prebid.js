a515043ae614d3fcb5974484a926c0ddc59dc8f7
Rich Snapp <rich.snapp@gmail.com>
2020-03-31 16:37:55 -0600

    add rakuten bid adapter

---
+++ modules/rakuten/index.js
@@ -0,0 +1,72 @@
+import { registerBidder } from '../../src/adapters/bidderFactory.js';
+import { BANNER } from '../../src/mediaTypes.js';
+import { config } from '../../src/config.js';
+var BIDDER_CODE = 'rakuten';
+var ENDPOINT = 'https://s-bid.rmp.rakuten.com/h';
+export var spec = {
+  code: BIDDER_CODE,
+  isBidRequestValid: function (bid) { return !!bid.params.adSpotId; },
+  buildRequests: function (validBidRequests, bidderRequest) {
+    var bidRequests = [];
+    validBidRequests.forEach(function (bid) {
+      var params = bid.params;
+      bidRequests.push({
+        method: 'GET',
+        url: config.getConfig('rakuten.endpoint') || ENDPOINT,
+        data: {
+          bi: bid.bidId,
+          t: params.adSpotId,
+          s: document.location.protocol,
+          ua: navigator.userAgent,
+          l: navigator.browserLanguage ||
+                        navigator.language,
+          d: document.domain,
+          tp: bidderRequest.refererInfo.stack[0] || window.location.href,
+          pp: bidderRequest.refererInfo.referer
+        }
+      });
+    });
+    return bidRequests;
+  },
+  interpretResponse: function (response, request) {
+    var sb = response.body;
+    var bidResponses = [];
+    if (sb.cpm && sb.ad) {
+      bidResponses.push({
+        requestId: sb.bid_id,
+        cpm: sb.cpm,
+        width: sb.width || 0,
+        height: sb.height || 0,
+        creativeId: sb.creative_id || 0,
+        dealId: sb.deal_id || '',
+        currency: sb.currency || 'JPY',
+        netRevenue: (typeof sb.net_revenue === 'undefined') ? true : !!sb.net_revenue,
+        mediaType: BANNER,
+        ttl: sb.ttl,
+        ad: sb.ad
+      });
+    }
+    return bidResponses;
+  },
+  getUserSyncs: function (syncOptions, serverResponses) {
+    var syncs = [];
+    if (syncOptions.pixelEnabled && serverResponses[0].body !== undefined) {
+      var bidResponseObj = serverResponses[0].body;
+      if (!bidResponseObj) {
+        return [];
+      }
+      if (bidResponseObj.sync_urls && bidResponseObj.sync_urls.length > 0) {
+        bidResponseObj.sync_urls.forEach(function (syncUrl) {
+          if (syncUrl && syncUrl !== 'null' && syncUrl.length > 0) {
+            syncs.push({
+              type: 'image',
+              url: syncUrl
+            });
+          }
+        });
+      }
+    }
+    return syncs;
+  }
+};
+registerBidder(spec);
---
+++ modules/rakuten/index.ts
@@ -0,0 +1,112 @@
+import { registerBidder, BidderSpec, ServerRequest, BidRequest, BidResponse } from '../../src/adapters/bidderFactory.js'
+import { BANNER } from '../../src/mediaTypes.js'
+import { config } from '../../src/config.js'
+
+const BIDDER_CODE = 'rakuten';
+const ENDPOINT = 'https://s-bid.rmp.rakuten.com/h';
+
+declare var navigator: NavigatorIE;
+interface NavigatorIE extends Navigator {
+    browserLanguage: string
+}
+
+type BidParams = {
+    adSpotId: number;
+};
+
+type RequestData = {
+    bi: BidRequest<BidParams>['bidId']
+    t: BidParams['adSpotId'];
+    s: string;                  // protocol: "http:" | "https:"
+    ua: string;                 // user agent
+    l: string;                  // language
+    d: string;                  // domain
+    tp: string;                 // window location
+    pp: string;                 // referer
+}
+
+type ServerResponse = {
+    cpm: number;
+    ad: string;
+    bid_id: BidRequest<BidParams>['bidId'];
+    width: number;
+    height: number;
+    creative_id: number;
+    deal_id: string;
+    currency: string;
+    ttl: number;
+    net_revenue?: boolean;
+    sync_urls?: string[]
+}
+
+export const spec : BidderSpec<BidParams, RequestData, ServerResponse> = {
+    code: BIDDER_CODE,
+    isBidRequestValid: bid => !!bid.params.adSpotId,
+    buildRequests: (validBidRequests, bidderRequest) => {
+        const bidRequests : ServerRequest<RequestData>[] = [];
+        validBidRequests.forEach(bid => {
+            const params = bid.params;
+            bidRequests.push({
+                method: 'GET',
+                url: config.getConfig('rakuten.endpoint') || ENDPOINT,
+                data: {
+                    bi: bid.bidId,
+                    t: params.adSpotId,
+                    s: document.location.protocol,
+                    ua: navigator.userAgent,
+                    l:
+                        navigator.browserLanguage ||
+                        navigator.language,
+                    d: document.domain,
+                    tp: bidderRequest.refererInfo.stack[0] || window.location.href,
+                    pp: bidderRequest.refererInfo.referer
+                }
+            })
+        });
+        return bidRequests
+    },
+    interpretResponse: (response, request) => {
+        const sb = response.body;
+        const bidResponses : BidResponse[] = [];
+
+        if (sb.cpm && sb.ad) {
+            bidResponses.push({
+                requestId: sb.bid_id,
+                cpm: sb.cpm,
+                width: sb.width || 0,
+                height: sb.height || 0,
+                creativeId: sb.creative_id || 0,
+                dealId: sb.deal_id || '',
+                currency: sb.currency || 'JPY',
+                netRevenue: (typeof sb.net_revenue === 'undefined') ? true : !!sb.net_revenue,
+                mediaType: BANNER,
+                ttl: sb.ttl,
+                ad: sb.ad
+            });
+        }
+
+        return bidResponses
+    },
+
+    getUserSyncs: function(syncOptions, serverResponses) {
+        const syncs = [];
+        if (syncOptions.pixelEnabled && serverResponses[0].body !== undefined) {const bidResponseObj = serverResponses[0].body;
+            if (!bidResponseObj) {
+                return [];
+            }
+            if (bidResponseObj.sync_urls && bidResponseObj.sync_urls.length > 0) {
+                bidResponseObj.sync_urls.forEach(syncUrl => {
+                    if (syncUrl && syncUrl !== 'null' && syncUrl.length > 0) {
+                        syncs.push({
+                            type: 'image',
+                            url: syncUrl
+                        });
+                    }
+                });
+            }
+        }
+        return syncs;
+    }
+};
+
+registerBidder(spec);
