56e854c3fca4dede11c451cbd4824f6043486474
Neelanjan Sen <neelanjan.sen@gmail.com>
2020-04-01 16:31:12 +0530

    add support for identical ad units and improve log messages

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import { config } from './config.js';
 import clone from 'just-clone';
+import deepequal from 'deep-equal';
 import find from 'core-js/library/fn/array/find.js';
 import includes from 'core-js/library/fn/array/includes.js';
 
@@ -36,7 +37,8 @@ export const internal = {
   logError,
   logWarn,
   logMessage,
-  logInfo
+  logInfo,
+  deepEqual
 };
 
 var uniqueRef = {};
@@ -1347,4 +1349,13 @@ export function compareOn(property) {
     }
     return 0;
   }
+}
+
+/**
+ * This function compares two objects for checking their equivalence.
+ * @param {Object} obj1
+ * @param {Object} obj2
+ */
+export function deepEqual(obj1, obj2) {
+  return deepequal(obj1, obj2);
 }
