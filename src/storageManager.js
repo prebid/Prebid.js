ef32c05febcdb8d63c2f4668d4e49467d900b8dc
Chris Cole <goosemanjack@gmail.com>
2020-04-02 10:57:53 -0700

    Minor change to use const instead of var per review request.

--- src/storageManager.js
+++ src/storageManager.js
@@ -64,8 +64,8 @@ export function newStorageManager({gvlid, moduleName, moduleType} = {}) {
       if (result && result.valid) {
         const domainPortion = (domain && domain !== '') ? ` ;domain=${encodeURIComponent(domain)}` : '';
         const expiresPortion = (expires && expires !== '') ? ` ;expires=${expires}` : '';
-        var isNone = (sameSite != null && sameSite.toLowerCase() == 'none')
-        var secure = (isNone) ? '; Secure' : '';
+        const isNone = (sameSite != null && sameSite.toLowerCase() == 'none')
+        const secure = (isNone) ? '; Secure' : '';
         document.cookie = `${key}=${encodeURIComponent(value)}${expiresPortion}; path=/${domainPortion}${sameSite ? `; SameSite=${sameSite}` : ''}${secure}`;
       }
     }
