6be50abd53d22193515f72ea13bc147a25aa31ef
Kenan Shifflett <kenan@spanishdict.com>
2020-04-02 13:19:01 -0400

    Wrap ajax call in setTimeout to split long task.

--- src/ajax.js
+++ src/ajax.js
@@ -18,83 +18,85 @@ export const ajax = ajaxBuilder();
 
 export function ajaxBuilder(timeout = 3000, {request, done} = {}) {
   return function(url, callback, data, options = {}) {
-    try {
-      let x;
-      let method = options.method || (data ? 'POST' : 'GET');
-      let parser = document.createElement('a');
-      parser.href = url;
+    setTimeout(() => {
+      try {
+        let x;
+        let method = options.method || (data ? 'POST' : 'GET');
+        let parser = document.createElement('a');
+        parser.href = url;
 
-      let callbacks = typeof callback === 'object' && callback !== null ? callback : {
-        success: function() {
-          utils.logMessage('xhr success');
-        },
-        error: function(e) {
-          utils.logError('xhr error', null, e);
-        }
-      };
-
-      if (typeof callback === 'function') {
-        callbacks.success = callback;
-      }
-
-      x = new window.XMLHttpRequest();
-
-      x.onreadystatechange = function () {
-        if (x.readyState === XHR_DONE) {
-          if (typeof done === 'function') {
-            done(parser.origin);
+        let callbacks = typeof callback === 'object' && callback !== null ? callback : {
+          success: function() {
+            utils.logMessage('xhr success');
+          },
+          error: function(e) {
+            utils.logError('xhr error', null, e);
           }
-          let status = x.status;
-          if ((status >= 200 && status < 300) || status === 304) {
-            callbacks.success(x.responseText, x);
-          } else {
-            callbacks.error(x.statusText, x);
-          }
-        }
-      };
-
-      // Disabled timeout temporarily to avoid xhr failed requests. https://github.com/prebid/Prebid.js/issues/2648
-      if (!config.getConfig('disableAjaxTimeout')) {
-        x.ontimeout = function () {
-          utils.logError('  xhr timeout after ', x.timeout, 'ms');
         };
-      }
 
-      if (method === 'GET' && data) {
-        let urlInfo = parseURL(url, options);
-        Object.assign(urlInfo.search, data);
-        url = formatURL(urlInfo);
-      }
+        if (typeof callback === 'function') {
+          callbacks.success = callback;
+        }
 
-      x.open(method, url, true);
-      // IE needs timoeut to be set after open - see #1410
-      // Disabled timeout temporarily to avoid xhr failed requests. https://github.com/prebid/Prebid.js/issues/2648
-      if (!config.getConfig('disableAjaxTimeout')) {
-        x.timeout = timeout;
-      }
+        x = new window.XMLHttpRequest();
 
-      if (options.withCredentials) {
-        x.withCredentials = true;
-      }
-      utils._each(options.customHeaders, (value, header) => {
-        x.setRequestHeader(header, value);
-      });
-      if (options.preflight) {
-        x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      }
-      x.setRequestHeader('Content-Type', options.contentType || 'text/plain');
+        x.onreadystatechange = function () {
+          if (x.readyState === XHR_DONE) {
+            if (typeof done === 'function') {
+              done(parser.origin);
+            }
+            let status = x.status;
+            if ((status >= 200 && status < 300) || status === 304) {
+              callbacks.success(x.responseText, x);
+            } else {
+              callbacks.error(x.statusText, x);
+            }
+          }
+        };
 
-      if (typeof request === 'function') {
-        request(parser.origin);
-      }
+        // Disabled timeout temporarily to avoid xhr failed requests. https://github.com/prebid/Prebid.js/issues/2648
+        if (!config.getConfig('disableAjaxTimeout')) {
+          x.ontimeout = function () {
+            utils.logError('  xhr timeout after ', x.timeout, 'ms');
+          };
+        }
 
-      if (method === 'POST' && data) {
-        x.send(data);
-      } else {
-        x.send();
+        if (method === 'GET' && data) {
+          let urlInfo = parseURL(url, options);
+          Object.assign(urlInfo.search, data);
+          url = formatURL(urlInfo);
+        }
+
+        x.open(method, url, true);
+        // IE needs timoeut to be set after open - see #1410
+        // Disabled timeout temporarily to avoid xhr failed requests. https://github.com/prebid/Prebid.js/issues/2648
+        if (!config.getConfig('disableAjaxTimeout')) {
+          x.timeout = timeout;
+        }
+
+        if (options.withCredentials) {
+          x.withCredentials = true;
+        }
+        utils._each(options.customHeaders, (value, header) => {
+          x.setRequestHeader(header, value);
+        });
+        if (options.preflight) {
+          x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        }
+        x.setRequestHeader('Content-Type', options.contentType || 'text/plain');
+
+        if (typeof request === 'function') {
+          request(parser.origin);
+        }
+
+        if (method === 'POST' && data) {
+          x.send(data);
+        } else {
+          x.send();
+        }
+      } catch (error) {
+        utils.logError('xhr construction', error);
       }
-    } catch (error) {
-      utils.logError('xhr construction', error);
-    }
+    })
   }
 }
