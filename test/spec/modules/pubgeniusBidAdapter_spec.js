0209f44afb08dc79ce2203ae243f765e46f8b51c
Meng Luan <edmonluan@gmail.com>
2020-04-28 03:01:56 -0400

    more tests

--- test/spec/modules/pubgeniusBidAdapter_spec.js
+++ test/spec/modules/pubgeniusBidAdapter_spec.js
@@ -1,8 +1,9 @@
 import { expect } from 'chai';
 
 import { spec } from 'modules/pubgeniusBidAdapter.js';
-import { deepClone } from 'src/utils.js';
+import { deepClone, parseQueryStringParameters } from 'src/utils.js';
 import { config } from 'src/config.js';
+import { server } from 'test/mocks/xhr.js';
 
 const {
   code,
@@ -10,6 +11,8 @@ const {
   isBidRequestValid,
   buildRequests,
   interpretResponse,
+  getUserSyncs,
+  onTimeout,
 } = spec;
 
 describe('pubGENIUS adapter', () => {
@@ -26,51 +29,50 @@ describe('pubGENIUS adapter', () => {
   });
 
   describe('isBidRequestValid', () => {
-    const validBid = Object.freeze({
-      mediaTypes: {
-        banner: {
-          sizes: [[300, 600], [300, 250]],
+    let bid = null;
+
+    beforeEach(() => {
+      bid = {
+        mediaTypes: {
+          banner: {
+            sizes: [[300, 600], [300, 250]],
+          },
         },
-      },
-      params: {
-        adUnitId: 1112,
-      },
+        params: {
+          adUnitId: 1112,
+        },
+      };
     });
 
     it('should return true with numeric adUnitId ', () => {
-      expect(isBidRequestValid(validBid)).to.be.true;
+      expect(isBidRequestValid(bid)).to.be.true;
     });
 
     it('should return true with string adUnitId ', () => {
-      const bid = deepClone(validBid);
       bid.params.adUnitId = '1112';
 
       expect(isBidRequestValid(bid)).to.be.true;
     });
 
     it('should return false without adUnitId', () => {
-      const bid = deepClone(validBid);
       delete bid.params.adUnitId;
 
       expect(isBidRequestValid(bid)).to.be.false;
     });
 
     it('should return false with adUnitId of invalid type', () => {
-      const bid = deepClone(validBid);
       bid.params.adUnitId = [1112];
 
       expect(isBidRequestValid(bid)).to.be.false;
     });
 
     it('should return false with empty sizes', () => {
-      const bid = deepClone(validBid);
       bid.mediaTypes.banner.sizes = [];
 
       expect(isBidRequestValid(bid)).to.be.false;
     });
 
     it('should return false with invalid size', () => {
-      const bid = deepClone(validBid);
       bid.mediaTypes.banner.sizes = [[300, 600, 250]];
 
       expect(isBidRequestValid(bid)).to.be.false;
@@ -78,55 +80,6 @@ describe('pubGENIUS adapter', () => {
   });
 
   describe('buildRequests', () => {
-    const bidRequest = {
-      adUnitCode: 'test-div',
-      auctionId: 'fake-auction-id',
-      bidId: 'fakebidid',
-      bidder: 'pubgenius',
-      bidderRequestId: 'fakebidderrequestid',
-      bidRequestsCount: 1,
-      bidderRequestsCount: 1,
-      bidderWinsCount: 0,
-      mediaTypes: {
-        banner: {
-          sizes: [[300, 600], [300, 250]],
-        },
-      },
-      params: {
-        adUnitId: 1112,
-      },
-      transactionId: 'fake-transaction-id',
-    };
-    const bidderRequest = {
-      auctionId: 'fake-auction-id',
-      bidderCode: 'pubgenius',
-      bidderRequestId: 'fakebidderrequestid',
-      refererInfo: {},
-    };
-    const expectedRequest = {
-      method: 'POST',
-      url: 'https://ortb.adpearl.io/prebid/auction',
-      options: { contentType: 'application/json' },
-      data: {
-        id: 'fake-auction-id',
-        imp: [
-          {
-            id: 'fakebidid',
-            banner: {
-              format: [{ w: 300, h: 600 }, { w: 300, h: 250 }],
-              topframe: 0,
-            },
-            tagid: '1112',
-          },
-        ],
-        tmax: 1200,
-        ext: {
-          pbadapter: {
-            version: '1.0.0',
-          },
-        },
-      },
-    };
     const origBidderTimeout = config.getConfig('bidderTimeout');
     const origPageUrl = config.getConfig('pageUrl');
     const origCoppa = config.getConfig('coppa');
@@ -139,7 +92,61 @@ describe('pubGENIUS adapter', () => {
       });
     });
 
+    let bidRequest = null;
+    let bidderRequest = null;
+    let expectedRequest = null;
+
     beforeEach(() => {
+      bidRequest = {
+        adUnitCode: 'test-div',
+        auctionId: 'fake-auction-id',
+        bidId: 'fakebidid',
+        bidder: 'pubgenius',
+        bidderRequestId: 'fakebidderrequestid',
+        bidRequestsCount: 1,
+        bidderRequestsCount: 1,
+        bidderWinsCount: 0,
+        mediaTypes: {
+          banner: {
+            sizes: [[300, 600], [300, 250]],
+          },
+        },
+        params: {
+          adUnitId: 1112,
+        },
+        transactionId: 'fake-transaction-id',
+      };
+      bidderRequest = {
+        auctionId: 'fake-auction-id',
+        bidderCode: 'pubgenius',
+        bidderRequestId: 'fakebidderrequestid',
+        refererInfo: {},
+      };
+      expectedRequest = {
+        method: 'POST',
+        url: 'https://ortb.adpearl.io/prebid/auction',
+        options: { contentType: 'application/json' },
+        data: {
+          id: 'fake-auction-id',
+          imp: [
+            {
+              id: 'fakebidid',
+              banner: {
+                format: [{ w: 300, h: 600 }, { w: 300, h: 250 }],
+                topframe: 0,
+              },
+              tagid: '1112',
+            },
+          ],
+          tmax: 1200,
+          ext: {
+            pbadapter: {
+              version: '1.0.0',
+            },
+          },
+        },
+      };
+
       config.setConfig({
         bidderTimeout: 1200,
         pageUrl: undefined,
@@ -152,73 +159,61 @@ describe('pubGENIUS adapter', () => {
     });
 
     it('should take bid floor in bidder params', () => {
-      const bidRequestWithBidFloor = deepClone(bidRequest);
-      bidRequestWithBidFloor.params.bidFloor = 0.5;
-      const expectedWithBidFloor = deepClone(expectedRequest);
-      expectedWithBidFloor.data.imp[0].bidfloor = 0.5;
+      bidRequest.params.bidFloor = 0.5;
+      expectedRequest.data.imp[0].bidfloor = 0.5;
 
-      expect(buildRequests([bidRequestWithBidFloor], bidderRequest)).to.deep.equal(expectedWithBidFloor);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take position in bidder params', () => {
-      const bidRequestWithPosition = deepClone(bidRequest);
-      bidRequestWithPosition.params.position = 3;
-      const expectedWithPosition = deepClone(expectedRequest);
-      expectedWithPosition.data.imp[0].banner.pos = 3;
+      bidRequest.params.position = 3;
+      expectedRequest.data.imp[0].banner.pos = 3;
 
-      expect(buildRequests([bidRequestWithPosition], bidderRequest)).to.deep.equal(expectedWithPosition);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take pageUrl in config over referer in refererInfo', () => {
       config.setConfig({ pageUrl: 'http://pageurl.org' });
-      const bidderRequestWithReferer = deepClone(bidderRequest);
-      bidderRequestWithReferer.refererInfo.referer = 'http://referer.org';
-      const expectedWithSitePage = deepClone(expectedRequest);
-      expectedWithSitePage.data.site = { page: encodeURIComponent('http://pageurl.org') };
+      bidderRequest.refererInfo.referer = 'http://referer.org';
+      expectedRequest.data.site = { page: encodeURIComponent('http://pageurl.org') };
 
-      expect(buildRequests([bidRequest], bidderRequestWithReferer)).to.deep.equal(expectedWithSitePage);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take gdprConsent when GDPR does not apply', () => {
-      const bidderRequestWithGdpr = deepClone(bidderRequest);
-      bidderRequestWithGdpr.gdprConsent = {
+      bidderRequest.gdprConsent = {
         gdprApplies: false,
         consentString: 'fakeconsent',
       };
-      const expectedWithGdpr = deepClone(expectedRequest);
-      expectedWithGdpr.data.regs = {
+      expectedRequest.data.regs = {
         ext: { gdpr: 0 },
       };
 
-      expect(buildRequests([bidRequest], bidderRequestWithGdpr)).to.deep.equal(expectedWithGdpr);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take gdprConsent when GDPR applies', () => {
-      const bidderRequestWithGdpr = deepClone(bidderRequest);
-      bidderRequestWithGdpr.gdprConsent = {
+      bidderRequest.gdprConsent = {
         gdprApplies: true,
         consentString: 'fakeconsent',
       };
-      const expectedWithGdpr = deepClone(expectedRequest);
-      expectedWithGdpr.data.regs = {
+      expectedRequest.data.regs = {
         ext: { gdpr: 1 },
       };
-      expectedWithGdpr.data.user = {
+      expectedRequest.data.user = {
         ext: { consent: 'fakeconsent' },
       };
 
-      expect(buildRequests([bidRequest], bidderRequestWithGdpr)).to.deep.equal(expectedWithGdpr);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take uspConsent', () => {
-      const bidderRequestWithUsp = deepClone(bidderRequest);
-      bidderRequestWithUsp.uspConsent = '1---';
-      const expectedWithUsp = deepClone(expectedRequest);
-      expectedWithUsp.data.regs = {
+      bidderRequest.uspConsent = '1---';
+      expectedRequest.data.regs = {
         ext: { us_privacy: '1---' },
       };
 
-      expect(buildRequests([bidRequest], bidderRequestWithUsp)).to.deep.equal(expectedWithUsp);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take schain', () => {
@@ -233,75 +228,193 @@ describe('pubGENIUS adapter', () => {
           }
         ]
       };
-      const bidderRequestWithSchain = deepClone(bidderRequest);
-      bidderRequestWithSchain.schain = deepClone(schain);
-      const expectedWithSchain = deepClone(expectedRequest);
-      expectedWithSchain.data.source = {
+      bidderRequest.schain = deepClone(schain);
+      expectedRequest.data.source = {
         ext: { schain: deepClone(schain) },
       };
 
-      expect(buildRequests([bidRequest], bidderRequestWithSchain)).to.deep.equal(expectedWithSchain);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
 
     it('should take coppa', () => {
       config.setConfig({ coppa: true });
-      const expectedWithCoppa = deepClone(expectedRequest);
-      expectedWithCoppa.data.regs = { coppa: 1 };
+      expectedRequest.data.regs = { coppa: 1 };
 
-      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedWithCoppa);
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
+    });
+
+    it('should take user IDs', () => {
+      const eid = {
+        source: 'adserver.org',
+        uids: [
+          {
+            id: 'fake-user-id',
+            atype: 1,
+            ext: { rtiPartner: 'TDID' },
+          },
+        ],
+      };
+      bidRequest.userIdAsEids = [deepClone(eid)];
+      expectedRequest.data.user = {
+        ext: {
+          eids: [deepClone(eid)],
+        },
+      };
+
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
+    });
+
+    it('should not take unsupported user IDs', () => {
+      bidRequest.userIdAsEids = [
+        {
+          source: 'pubcid.org',
+          uids: [
+            {
+              id: 'fake-user-id',
+              atype: 1,
+            },
+          ],
+        },
+      ];
+
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
+    });
+
+    it('should not take empty user IDs', () => {
+      bidRequest.userIdAsEids = [];
+
+      expect(buildRequests([bidRequest], bidderRequest)).to.deep.equal(expectedRequest);
     });
   });
 
   describe('interpretResponse', () => {
-    const serverResponse = {
-      body: {
-        seatbid: [
-          {
-            seat: 'pubgenius',
-            bid: [
-              {
-                impid: 'fakebidid',
-                price: 0.3,
-                w: 300,
-                h: 250,
-                adm: 'fake_creative',
-                exp: 60,
-                crid: 'fakecreativeid',
-              },
-            ],
-          },
-        ],
-      },
-    };
-    const expectedBidResponse = {
-      requestId: 'fakebidid',
-      cpm: 0.3,
-      currency: 'USD',
-      width: 300,
-      height: 250,
-      ad: 'fake_creative',
-      ttl: 60,
-      creativeId: 'fakecreativeid',
-      netRevenue: true,
-    };
+    let serverResponse = null;
+    let expectedBidResponse = null;
+
+    beforeEach(() => {
+      serverResponse = {
+        body: {
+          seatbid: [
+            {
+              seat: 'pubgenius',
+              bid: [
+                {
+                  impid: 'fakebidid',
+                  price: 0.3,
+                  w: 300,
+                  h: 250,
+                  adm: 'fake_creative',
+                  exp: 60,
+                  crid: 'fakecreativeid',
+                },
+              ],
+            },
+          ],
+        },
+      };
+      expectedBidResponse = {
+        requestId: 'fakebidid',
+        cpm: 0.3,
+        currency: 'USD',
+        width: 300,
+        height: 250,
+        ad: 'fake_creative',
+        ttl: 60,
+        creativeId: 'fakecreativeid',
+        netRevenue: true,
+      };
+    });
 
     it('should interpret response correctly', () => {
       expect(interpretResponse(serverResponse)).to.deep.equal([expectedBidResponse]);
     });
 
     it('should interpret response with adomain', () => {
-      const serverResponseWithAdomain = deepClone(serverResponse);
-      serverResponseWithAdomain.body.seatbid[0].bid[0].adomain = ['fakeaddomain'];
-      const expectedWithAdDomains = deepClone(expectedBidResponse);
-      expectedWithAdDomains.meta = {
+      serverResponse.body.seatbid[0].bid[0].adomain = ['fakeaddomain'];
+      expectedBidResponse.meta = {
         advertiserDomains: ['fakeaddomain'],
       };
 
-      expect(interpretResponse(serverResponseWithAdomain)).to.deep.equal([expectedWithAdDomains]);
+      expect(interpretResponse(serverResponse)).to.deep.equal([expectedBidResponse]);
     });
 
     it('should interpret no bids', () => {
       expect(interpretResponse({ body: {} })).to.deep.equal([]);
+    });
+  });
+
+  describe('getUserSyncs', () => {
+    let syncOptions = null;
+    let expectedSync = null;
+
+    beforeEach(() => {
+      syncOptions = {
+        iframeEnabled: true,
+        pixelEnabled: true,
+      };
+      expectedSync = {
+        type: 'iframe',
+        url: 'https://ortb.adpearl.io/usersync/pixels.html?',
+      };
+    });
+
+    it('should return iframe pixels', () => {
+      expect(getUserSyncs(syncOptions)).to.deep.equal([expectedSync]);
+    });
+
+    it('should return empty when iframe is not enabled', () => {
+      syncOptions.iframeEnabled = false;
+
+      expect(getUserSyncs(syncOptions)).to.deep.equal([]);
+    });
+
+    it('should return sync when GDPR applies', () => {
+      const gdprConsent = {
+        gdprApplies: true,
+        consentString: 'fake-gdpr-consent',
+      };
+      expectedSync.url = expectedSync.url + parseQueryStringParameters({
+        gdpr: 1,
+        consent: 'fake-gdpr-consent',
+      });
+
+      expect(getUserSyncs(syncOptions, [], gdprConsent)).to.deep.equal([expectedSync]);
+    });
+
+    it('should return sync when GDPR does not apply', () => {
+      const gdprConsent = {
+        gdprApplies: false,
+      };
+      expectedSync.url = expectedSync.url + parseQueryStringParameters({ gdpr: 0 });
+
+      expect(getUserSyncs(syncOptions, [], gdprConsent)).to.deep.equal([expectedSync]);
+    });
+
+    it('should return sync with US privacy', () => {
+      expectedSync.url = expectedSync.url + parseQueryStringParameters({ us_privacy: '1---' });
+
+      expect(getUserSyncs(syncOptions, [], undefined, '1---')).to.deep.equal([expectedSync]);
+    });
+  });
+
+  describe('onTimeout', () => {
+    it('should send timeout data', () => {
+      const timeoutData = {
+        bidder: 'pubgenius',
+        bidId: 'fakebidid',
+        params: {
+          adUnitId: 1234,
+        },
+        adUnitCode: 'fake-ad-unit-code',
+        timeout: 3000,
+        auctionId: 'fake-auction-id',
+      };
+      onTimeout(timeoutData);
+
+      expect(server.requests[0].method).to.equal('POST');
+      expect(server.requests[0].url).to.equal('https://ortb.adpearl.io/prebid/events?type=timeout');
+      expect(server.requests[0].requestHeaders['Content-Type']).to.equal('application/json;charset=utf-8');
+      expect(JSON.parse(server.requests[0].requestBody)).to.deep.equal(timeoutData);
     });
   });
 });
