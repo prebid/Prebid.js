7266b828b81d2037e53fc711ab0659c33f712194
pm-shashank-jain <40654031+pm-shashank-jain@users.noreply.github.com>
2020-05-12 14:22:04 +0530

    Updated rubiconBidAdapter for alias

--- modules/rubiconBidAdapter.js
+++ modules/rubiconBidAdapter.js
@@ -114,6 +114,7 @@ utils._each(sizeMap, (item, key) => sizeMap[item] = key);
 
 export const spec = {
   code: 'rubicon',
+  aliases: ['rubicon2'],
   gvlid: GVLID,
   supportedMediaTypes: [BANNER, VIDEO],
   /**
