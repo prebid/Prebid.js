c8c7b355e542adb2c98b586d45ba3b9f203f658b
kanchika-automatad <kanchika@automatad.com>
2020-05-19 22:51:51 +0530

    lint fixes

--- modules/automatadBidAdapter.js
+++ modules/automatadBidAdapter.js
@@ -99,8 +99,8 @@ export const spec = {
   },
   onBidWon: function(bid) {
     if (!bid.nurl) { return }
-    const winCpm = (bid.hasOwnProperty('originalCpm'))?bid.originalCpm:bid.cpm
-    const winCurr = (bid.hasOwnProperty('originalCurrency') && bid.hasOwnProperty('originalCpm'))?bid.originalCurrency:bid.currency
+    const winCpm = (bid.hasOwnProperty('originalCpm')) ? bid.originalCpm : bid.cpm
+    const winCurr = (bid.hasOwnProperty('originalCurrency') && bid.hasOwnProperty('originalCpm')) ? bid.originalCurrency : bid.currency
     const winUrl = bid.nurl.replace(
       /\$\{AUCTION_PRICE\}/,
       winCpm
