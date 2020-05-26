e3dabbf135775e33722bd54bc3e7e96f747cea08
Estavillo <fernando@gumgum.com>
2020-05-26 12:33:59 -0700

    add unit tests and fix dependencies.

--- modules/gumgumBidAdapter.js
+++ modules/gumgumBidAdapter.js
@@ -4,7 +4,7 @@ import { BANNER, VIDEO } from '../src/mediaTypes.js';
 
 import { config } from '../src/config.js'
 import { getStorageManager } from '../src/storageManager.js';
-import includes from 'core-js/library/fn/array/includes.js';
+import includes from 'core-js-pure/features/array/includes';
 import { registerBidder } from '../src/adapters/bidderFactory.js'
 
 const storage = getStorageManager();
