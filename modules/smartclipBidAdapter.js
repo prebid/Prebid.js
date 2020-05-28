ad6b063201825331a0a33bba8bd33d3d17037c1a
smartclip-adtech <65160328+smartclip-adtech@users.noreply.github.com>
2020-05-28 14:57:16 +0200

    Updated imported files

--- modules/smartclipBidAdapter.js
+++ modules/smartclipBidAdapter.js
@@ -1,7 +1,7 @@
-import * as utils from 'src/utils';
-import { Renderer } from 'src/Renderer';
-import { registerBidder } from 'src/adapters/bidderFactory';
-import { VIDEO } from 'src/mediaTypes';
+import * as utils from '../src/utils';
+import { Renderer } from '../src/Renderer';
+import { registerBidder } from '../src/adapters/bidderFactory';
+import { VIDEO } from '../src/mediaTypes';
 
 const BIDDER_CODE = 'smartx';
 const URL = '//bid.sxp.smartclip.net/bid/1000';
