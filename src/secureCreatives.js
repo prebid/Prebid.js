f7df94a09dc49412987f68f8bf1f260aa606b826
pedrolopezmrf <pedro.lopez@marfeel.com>
2020-06-09 16:26:17 +0200

    adapt secure creatives

--- src/secureCreatives.js
+++ src/secureCreatives.js
@@ -6,11 +6,9 @@
 import events from './events.js';
 import { fireNativeTrackers, getAssetMessage } from './native.js';
 import { EVENTS } from './constants.json';
-import { logWarn, replaceAuctionPrice } from './utils.js';
+import { replaceAuctionPrice } from './utils.js';
 import { auctionManager } from './auctionManager.js';
 import find from 'core-js-pure/features/array/find.js';
-import { isRendererRequired, executeRenderer } from './Renderer.js';
-import includes from 'core-js-pure/features/array/includes.js';
 
 const BID_WON = EVENTS.BID_WON;
 
@@ -33,7 +31,7 @@ function receiveMessage(ev) {
     });
 
     if (adObject && data.message === 'Prebid Request') {
-      _sendAdToCreative(adObject, ev);
+      _sendAdToCreative(adObject, ev, data.adServerDomain);
 
       // save winning bids
       auctionManager.addWinningBid(adObject);
@@ -62,13 +60,9 @@ function receiveMessage(ev) {
   }
 }
 
-export function _sendAdToCreative(adObject, ev) {
-  const { adId, ad, adUrl, width, height, renderer, cpm } = adObject;
-  // rendering for outstream safeframe
-  if (isRendererRequired(renderer)) {
-    executeRenderer(renderer, adObject);
-  } else if (adId) {
-    resizeRemoteCreative(adObject);
+export function _sendAdToCreative(adObject, ev, remoteDomain) {
+  const { adId, ad, adUrl, width, height, cpm } = adObject;
+  if (adId) {
     ev.source.postMessage(JSON.stringify({
       message: 'Prebid Response',
       ad: replaceAuctionPrice(ad, cpm),
@@ -76,50 +70,6 @@ export function _sendAdToCreative(adObject, ev) {
       adId,
       width,
       height
-    }), ev.origin);
-  }
-}
-
-function resizeRemoteCreative({ adId, adUnitCode, width, height }) {
-  // resize both container div + iframe
-  ['div', 'iframe'].forEach(elmType => {
-    // not select element that gets removed after dfp render
-    let element = getElementByAdUnit(elmType + ':not([style*="display: none"])');
-    if (element) {
-      let elementStyle = element.style;
-      elementStyle.width = width + 'px';
-      elementStyle.height = height + 'px';
-    } else {
-      logWarn(`Unable to locate matching page element for adUnitCode ${adUnitCode}.  Can't resize it to ad's dimensions.  Please review setup.`);
-    }
-  });
-
-  function getElementByAdUnit(elmType) {
-    let id = getElementIdBasedOnAdServer(adId, adUnitCode);
-    let parentDivEle = document.getElementById(id);
-    return parentDivEle && parentDivEle.querySelector(elmType);
-  }
-
-  function getElementIdBasedOnAdServer(adId, adUnitCode) {
-    if (window.googletag) {
-      return getDfpElementId(adId)
-    } else if (window.apntag) {
-      return getAstElementId(adUnitCode)
-    } else {
-      return adUnitCode;
-    }
-  }
-
-  function getDfpElementId(adId) {
-    return find(window.googletag.pubads().getSlots(), slot => {
-      return find(slot.getTargetingKeys(), key => {
-        return includes(slot.getTargeting(key), adId);
-      });
-    }).getSlotElementId();
-  }
-
-  function getAstElementId(adUnitCode) {
-    let astTag = window.apntag.getTag(adUnitCode);
-    return astTag && astTag.targetId;
+    }), remoteDomain);
   }
 }
