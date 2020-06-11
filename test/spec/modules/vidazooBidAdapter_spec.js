4c86e0d994d8a781cd00575746b628cac002bc55
Udi Talias ⚛️ <udi.talias@gmail.com>
2020-06-11 18:59:30 +0300

    Vidazoo Adapter: Feature/direct deal targeting (#5343)
    * feat(module): multi size request
    
    * fix getUserSyncs
    added tests
    
    * update(module): package-lock.json from master
    
    * feat(client): send deal id with each request
    
    Co-authored-by: roman <shmoop207@gmail.com>

--- test/spec/modules/vidazooBidAdapter_spec.js
+++ test/spec/modules/vidazooBidAdapter_spec.js
@@ -1,5 +1,5 @@
-import {expect} from 'chai';
-import {spec as adapter, URL} from 'modules/vidazooBidAdapter.js';
+import { expect } from 'chai';
+import { spec as adapter, URL } from 'modules/vidazooBidAdapter.js';
 import * as utils from 'src/utils.js';
 
 const BID = {
@@ -139,6 +139,7 @@ describe('VidazooBidAdapter', function () {
           bidFloor: 0.1,
           bidId: '2d52001cabd527',
           publisherId: '59ac17c192832d0011283fe3',
+          dealId: 1,
           'ext.param1': 'loremipsum',
           'ext.param2': 'dolorsitamet',
         }
@@ -151,7 +152,7 @@ describe('VidazooBidAdapter', function () {
   });
   describe('getUserSyncs', function () {
     it('should have valid user sync with iframeEnabled', function () {
-      const result = adapter.getUserSyncs({iframeEnabled: true}, [SERVER_RESPONSE]);
+      const result = adapter.getUserSyncs({ iframeEnabled: true }, [SERVER_RESPONSE]);
 
       expect(result).to.deep.equal([{
         type: 'iframe',
@@ -160,7 +161,7 @@ describe('VidazooBidAdapter', function () {
     });
 
     it('should have valid user sync with pixelEnabled', function () {
-      const result = adapter.getUserSyncs({pixelEnabled: true}, [SERVER_RESPONSE]);
+      const result = adapter.getUserSyncs({ pixelEnabled: true }, [SERVER_RESPONSE]);
 
       expect(result).to.deep.equal([{
         'url': 'https://sync.com',
@@ -176,12 +177,12 @@ describe('VidazooBidAdapter', function () {
     });
 
     it('should return empty array when there is no ad', function () {
-      const responses = adapter.interpretResponse({price: 1, ad: ''});
+      const responses = adapter.interpretResponse({ price: 1, ad: '' });
       expect(responses).to.be.empty;
     });
 
     it('should return empty array when there is no price', function () {
-      const responses = adapter.interpretResponse({price: null, ad: 'great ad'});
+      const responses = adapter.interpretResponse({ price: null, ad: 'great ad' });
       expect(responses).to.be.empty;
     });
 
