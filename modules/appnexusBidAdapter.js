dd3a1c89f7f4794a7e5b99c35fea9db3a7413984
Jaimin Panchal <email@example.com>
2020-06-29 17:39:27 -0400

    Updating appnexus payload for criteo

--- modules/appnexusBidAdapter.js
+++ modules/appnexusBidAdapter.js
@@ -189,24 +189,25 @@ export const spec = {
       });
     }
 
+    let eids = [];
     const criteoId = utils.deepAccess(bidRequests[0], `userId.criteoId`);
     if (criteoId) {
-      let tpuids = [];
-      tpuids.push({
-        'provider': 'criteo',
-        'user_id': criteoId
+      eids.push({
+        source: 'criteo.com',
+        id: criteoId
       });
-      payload.tpuids = tpuids;
     }
 
     const tdid = utils.deepAccess(bidRequests[0], `userId.tdid`);
     if (tdid) {
-      let eids = [];
       eids.push({
         source: 'adserver.org',
         id: tdid,
         rti_partner: 'TDID'
       });
+    }
+
+    if (eids.length) {
       payload.eids = eids;
     }
 
--- test/spec/modules/appnexusBidAdapter_spec.js
+++ test/spec/modules/appnexusBidAdapter_spec.js
@@ -738,18 +738,6 @@ describe('AppNexusAdapter', function () {
       });
     });
 
-    it('should populate tpids array when userId is available', function () {
-      const bidRequest = Object.assign({}, bidRequests[0], {
-        userId: {
-          criteoId: 'sample-userid'
-        }
-      });
-
-      const request = spec.buildRequests([bidRequest]);
-      const payload = JSON.parse(request.data);
-      expect(payload.tpuids).to.deep.equal([{provider: 'criteo', user_id: 'sample-userid'}]);
-    });
-
     it('should populate schain if available', function () {
       const bidRequest = Object.assign({}, bidRequests[0], {
         schain: {
@@ -820,20 +808,26 @@ describe('AppNexusAdapter', function () {
       expect(request.options).to.deep.equal({withCredentials: false});
     });
 
-    it('should populate eids array when ttd id is available', function () {
+    it('should populate eids array when ttd id and criteo is available', function () {
       const bidRequest = Object.assign({}, bidRequests[0], {
         userId: {
-          tdid: 'sample-userid'
+          tdid: 'sample-userid',
+          criteoId: 'sample-criteo-userid'
         }
       });
 
       const request = spec.buildRequests([bidRequest]);
       const payload = JSON.parse(request.data);
-      expect(payload.eids).to.deep.equal([{
+      expect(payload.eids).to.deep.include({
         source: 'adserver.org',
         id: 'sample-userid',
         rti_partner: 'TDID'
-      }]);
+      });
+
+      expect(payload.eids).to.deep.include({
+        source: 'criteo.com',
+        id: 'sample-criteo-userid',
+      });
     });
   })
 
