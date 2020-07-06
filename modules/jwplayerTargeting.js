88ec998cfc5fbd17bba7882ebdc318858f06f218
karimJWP <karimJWP@github.com>
2020-07-06 18:34:45 -0400

    implements getTargeting

--- modules/jwplayerTargeting.js
+++ modules/jwplayerTargeting.js
@@ -1,81 +1,82 @@
-import { config } from '../src/config.js';
-import { ajaxBuilder } from '../src/ajax.js';
-import { submodule } from '../src/hook.js';
+import config from '../src/config.js';
+import ajaxBuilder from '../src/ajax.js';
+import logError from '../src/utils.js';
 
-// const segCache = {};
-export function getTargetingInfoForBid(bid) {
-
-}
-
-let subModules = [];
-
-/**
- * enable submodule in User ID
- * @param {RtdSubmodule} submodule
- */
-export function attachProvider(submodule) {
-  subModules.push(submodule);
-}
+const segCache = {};
 
 /**
  * @param bidRequest {object} - the bid which is passed to a prebid adapter for use in `buildRequests`
  * @returns {Array<string>} - an array of jwpseg targeting segments found for the given bidRequest information
  */
 export function getTargetingForBid(bidRequest) {
-  /*
-  * */
   console.log('karim getTargetingForBid');
   const jwpTargeting = bidRequest.jwpTargeting;
-  if (jwpTargeting) {
-    /*
-    jwpTargeting.playerID
-    jwpTargeting.mediaID
-     */
+  if (!jwpTargeting) {
+    return [];
   }
+  const { mediaID, playerID } = jwpTargeting;
+  let segments = segCache[mediaID];
+  if (segments) {
+    return segments;
+  }
+
+  const player = getPlayer(playerID);
+  if (!player) {
+    return [];
+  }
+
+  let item = player.getPlaylist().filter(item => item.mediaid === mediaID);
+  if (item) {
+    segments = item.jwpseg;
+    segCache[mediaID] = segments;
+    return segments;
+  }
+  return player.getPlaylistItem().jwpseg;
 }
 
-export function setup () {
-  console.log('karim setup');
+function getPlayer(playerID) {
+  return null;
+}
+
+function setup () {
   config.getConfig('jwpTargeting', (config) => {
-    getTargetingForBid();
     // fetch media ids
-    console.log('karim jwpTargeting set:', config);
+    const targeting = config.jwpTargeting;
+    if (!targeting) {
+      return;
+    }
+    const mediaIDs = targeting.mediaIDs;
+    mediaIDs.forEach(mediaID => {
+      console.log(mediaID);
+      fetchTargetingForMediaId(mediaID);
+    })
   });
 }
 
-export function getMediaId(mediaId) {
+function fetchTargetingForMediaId(mediaId) {
   let ajax = ajaxBuilder(1500);
-  ajax(`https://cdn.jwplayer.com/v2/media/${mediaId}/`,
+  ajax(`https://cdn.jwplayer.com/v2/media/${mediaId}`,
     {
-      success: function (response, req) {
-        if (req.status === 200) {
-          try {
-            // const data = JSON.parse(response);
-            // if (data && data.p && data.kn) {
-            //   setData({p: data.p, kn: data.kn, pmd: data.pmd});
-            // } else {
-            //   setData({});
-            // }
-            // addBrowsiTag(data);
-          } catch (err) {
-            // utils.logError('unable to parse data');
-            // setData({})
+      success: function (response) {
+        try {
+          const data = JSON.parse(response);
+          if (!data) {
+            return;
           }
-        } else if (req.status === 204) {
-          // unrecognized site key
-          // setData({});
+          const jwpseg = data.playlist[0].jwpseg;
+          if (jwpseg) {
+            segCache[mediaId] = jwpseg;
+            console.log('writing to cache: ', segCache);
+          }
+        } catch (err) {
+          logError('failed to parse response');
         }
       },
       error: function () {
-        // setData({});
-        // utils.logError('unable to get prediction data');
+        logError('failed to retrieve targeting information');
       }
     }
   );
 }
 
-console.log('karim');
-
-submodule('jwplayer', getTargetingInfoForBid);
 setup();
-// submodule('jwplayer', getTargetingInfoForBid);
