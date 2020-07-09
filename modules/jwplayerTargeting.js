a54c56727431d1d206a49d4e41a05c1625588408
karimJWP <karimJWP@github.com>
2020-07-08 23:32:13 -0400

    checks current item only if mediaid is missing

--- modules/jwplayerTargeting.js
+++ modules/jwplayerTargeting.js
@@ -12,7 +12,6 @@ let resumeBidRequest;
 function setup () {
   config.getConfig('jwpTargeting', (config) => {
     // fetch media ids
-    console.log('karim config');
     const mediaIDs = config.jwpTargeting.mediaIDs;
     requestCount = mediaIDs.length;
     mediaIDs.forEach(mediaID => {
@@ -22,18 +21,14 @@ function setup () {
   });
 
   getGlobal().requestBids.before(function(nextFn, reqBidsConfigObj) {
-    console.error('karim before requestBids', reqBidsConfigObj);
     if (requestCount <= 0) {
-      console.log('karim no pending reqs');
       nextFn.apply(this, [reqBidsConfigObj]);
       return;
     }
     requestTimeout = setTimeout(() => {
-      console.log('karim Request for targeting info timed out')
       nextFn.apply(this, [reqBidsConfigObj]);
     }, 1500);
 
-    console.log('karim storing req');
     resumeBidRequest = nextFn.bind(this, reqBidsConfigObj);
   });
 }
@@ -43,43 +38,32 @@ function setup () {
  * @returns {Array<string>} - an array of jwpseg targeting segments found for the given bidRequest information
  */
 export function getTargetingForBid(bidRequest) {
-  console.log('karim getTargetingForBid');
   const jwpTargeting = bidRequest.jwpTargeting;
   if (!jwpTargeting) {
-    console.log('karim no targeting');
     return [];
   }
+
   const { mediaID, playerID } = jwpTargeting;
   let segments = segCache[mediaID];
   if (segments) {
-    console.log('karim got segs from cache');
     return segments;
   }
 
   const player = getPlayer(playerID);
   if (!player) {
-    console.log('karim no player');
     return [];
   }
-  console.log('karim playlist ? ', player.getPlaylist());
 
-  const playlist = player.getPlaylist();
-  let item = playlist.find(item => item.mediaid === mediaID);
+  let item = mediaID ? player.getPlaylist().find(item => item.mediaid === mediaID) : player.getPlaylistItem();
   if (item) {
     segments = item.jwpseg;
-    segCache[mediaID] = segments;
-    console.log('karim mediaId: ', item);
-    console.log('karim got seg from playlist');
-    return segments || [];
   }
 
-  item = player.getPlaylistItem();
-  if (item) {
-    console.log('karim got seg from current item');
-    console.log('karim getPlaylistItem ? ', player.getPlaylistItem());
-    return item.jwpseg || [];
+  if (segments && mediaID) {
+    segCache[mediaID] = segments;
   }
-  return [];
+
+  return segments || [];
 }
 
 function getPlayer(playerID) {
@@ -88,6 +72,7 @@ function getPlayer(playerID) {
     logError('jwplayer.js was not found on page');
     return;
   }
+
   const player = jwplayer(playerID);
   if (!player || !player.getPlaylist) {
     logError('player ID did not match any players');
@@ -106,10 +91,15 @@ function fetchTargetingForMediaId(mediaId) {
           if (!data) {
             return;
           }
-          const jwpseg = data.playlist[0].jwpseg;
+
+          const playlist = data.playlist;
+          if (!playlist || !playlist.length) {
+            return;
+          }
+
+          const jwpseg = playlist[0].jwpseg;
           if (jwpseg) {
             segCache[mediaId] = jwpseg;
-            console.log('writing to cache: ', segCache);
           }
         } catch (err) {
           logError('failed to parse response');
@@ -131,32 +121,28 @@ function onRequestCompleted() {
   }
 
   if (requestTimeout) {
-    console.log('karim clear Timeout bid req');
     clearTimeout(requestTimeout);
   }
 
   if (resumeBidRequest) {
-    console.log('karim resume bid req');
     resumeBidRequest();
   }
 }
 
 setup();
 
-const sharedMethods = {
+const jwplayerUtilities = {
   'getTargetingForBid': getTargetingForBid
-}
-Object.freeze(sharedMethods);
+};
 
-module('jwplayer', function shareJWPlayerUtilities(...args) {
-  if (!isPlainObject(args[0])) {
+module('jwplayer', function shareJWPlayerUtilities() {
+  const host = arguments[0];
+  if (!isPlainObject(host)) {
     logError('JW Player module requires plain object to share methods with submodule');
     return;
   }
-  function addMethods(object, func) {
-    for (let name in func) {
-      object[name] = func[name];
-    }
+
+  for (let method in jwplayerUtilities) {
+    host[method] = jwplayerUtilities[method];
   }
-  addMethods(args[0], sharedMethods);
 });
