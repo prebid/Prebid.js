44c673f10d146a2ba12b7c3c6eff80ed1c6c83d9
Patrick McCann <patmmccann@gmail.com>
2020-08-05 11:34:10 -0400

    Update AnalyticsAdapter_spec.js

--- test/spec/AnalyticsAdapter_spec.js
+++ test/spec/AnalyticsAdapter_spec.js
@@ -9,6 +9,7 @@ const BID_RESPONSE = CONSTANTS.EVENTS.BID_RESPONSE;
 const BID_WON = CONSTANTS.EVENTS.BID_WON;
 const BID_TIMEOUT = CONSTANTS.EVENTS.BID_TIMEOUT;
 const AD_RENDER_FAILED = CONSTANTS.EVENTS.AD_RENDER_FAILED;
+const AUCTION_ERROR = CONSTANTS.EVENTS.AUCTION_ERROR;
 const ADD_AD_UNITS = CONSTANTS.EVENTS.ADD_AD_UNITS;
 
 const AnalyticsAdapter = require('src/AnalyticsAdapter').default;
@@ -81,6 +82,17 @@ FEATURE: Analytics Adapters API
 
       let result = JSON.parse(server.requests[0].requestBody);
       expect(result).to.deep.equal({args: {call: 'adRenderFailed'}, eventType: 'adRenderFailed'});
+    });
+    
+    it('SHOULD call global when an auction error event occurs', function () {
+      const eventType = AUCTION_ERROR;
+      const args = { call: 'auctionError' };
+
+      adapter.enableAnalytics();
+      events.emit(eventType, args);
+
+      let result = JSON.parse(server.requests[0].requestBody);
+      expect(result).to.deep.equal({args: {call: 'auctionError'}, eventType: 'auctionError'});
     });
 
     it('SHOULD call global when an addAdUnits event occurs', function () {
