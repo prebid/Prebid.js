d52d905211b1896a0bb2d7d70a2506031fa7bda1
angelamerkelprebid <69318193+angelamerkelprebid@users.noreply.github.com>
2020-08-06 14:54:48 -0400

    Update merkleIdSystem.js
    add consentData as param to getId

--- modules/merkleIdSystem.js
+++ modules/merkleIdSystem.js
@@ -31,9 +31,10 @@ export const merkleIdSubmodule = {
    * performs action to obtain id and return a value in the callback's response argument
    * @function
    * @param {SubmoduleParams} [configParams]
+   * @param {ConsentData} [consentData]
    * @returns {IdResponse|undefined}
    */
-  getId(configParams) {
+  getId(configParams, consentData) {
     if (!configParams || typeof configParams.pubid !== 'string') {
       utils.logError('User ID - merkleId submodule requires a valid pubid to be defined');
       return;
