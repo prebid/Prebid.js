2e0f6d87eba1e7d3d776e0403ff8f030a09d6169
myerkovich <myerkovich@rubiconproject.com>
2020-09-22 15:05:07 +0300

    Revert "type changes"
    This reverts commit 114005a5

--- modules/pubProvidedSystem.js
+++ modules/pubProvidedSystem.js
@@ -10,18 +10,6 @@ import * as utils from '../src/utils.js';
 
 const MODULE_NAME = 'pubProvidedId';
 
-function addType(uid) {
-  if (!uid.ext) {
-    uid.ext = {
-      types: ['ppuid']
-    };
-  } else if (!uid.ext.types) {
-    uid.ext.types = ['ppuid'];
-  } else if (!uid.ext.types.includes('ppuid')) {
-    uid.ext.types.push('ppuid');
-  }
-}
-
 /** @type {Submodule} */
 export const pubProvidedIdSubmodule = {
   /**
@@ -56,7 +44,6 @@ export const pubProvidedIdSubmodule = {
     if (typeof configParams.eidsFunction === 'function') {
       res = res.concat(configParams.eidsFunction());
     }
-    res.forEach(id => id.uids.forEach(uid => addType(uid)));
     return {id: res};
   }
 };
