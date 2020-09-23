2253c928da81e04927ccaaa81153abb439824d1c
myerkovich <myerkovich@rubiconproject.com>
2020-09-23 15:13:58 +0300

    fix

--- modules/rubiconBidAdapter.js
+++ modules/rubiconBidAdapter.js
@@ -563,7 +563,7 @@ export const spec = {
       // if config.getConfig('user.id') doesn't return anything, then look for the first eid.uids[*].ext.stype === 'ppuid'
       for (const eid of bidRequest.userIdAsEids) {
         if (eid.uids) {
-          const pubProvidedId = eid.uids.find(uid => uid.ext && uid.ext.stype === 'ppuid');
+          const pubProvidedId = find(eid.uids, uid => uid.ext && uid.ext.stype === 'ppuid');
           if (pubProvidedId && pubProvidedId.id) {
             data['ppuid'] = pubProvidedId.id;
             break;
