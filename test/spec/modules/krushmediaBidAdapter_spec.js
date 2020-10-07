f00b280f899fadb270d51436a8c2f425661e0231
Aiholkin <artem.iholkin@smartyads.com>
2020-10-07 17:23:37 +0300

    fix

--- test/spec/modules/krushmediaBidAdapter_spec.js
+++ test/spec/modules/krushmediaBidAdapter_spec.js
@@ -56,7 +56,7 @@ describe('KrushmediabBidAdapter', function () {
       expect(data.ccpa).to.not.exist;
       let placement = data['placements'][0];
       expect(placement).to.have.keys('key', 'bidId', 'traffic', 'sizes', 'hPlayer', 'wPlayer', 'schain');
-      expect(placement.key).to.equal(0);
+      expect(placement.key).to.equal(783);
       expect(placement.bidId).to.equal('23fhj33i987f');
       expect(placement.traffic).to.equal(BANNER);
       expect(placement.schain).to.be.an('object');
