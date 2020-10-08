1aa8f14bfda8aa811de54fc9fd34d8ec7ad2f2f1
karimJWP <karimJWP@github.com>
2020-10-08 16:43:12 -0400

    tests getCache and formatting

--- test/spec/modules/jwplayerRtdProvider_spec.js
+++ test/spec/modules/jwplayerRtdProvider_spec.js
@@ -1,4 +1,5 @@
-import { fetchTargetingForMediaId, getTargetingForBid,
+import { fetchTargetingForMediaId, enrichBidRequest,
+  getVatFromCache, formatTargetingResponse,
   fetchTargetingInformation, jwplayerSubmodule } from 'modules/jwplayerRtdProvider.js';
 import { server } from 'test/mocks/xhr.js';
 
@@ -39,11 +40,7 @@ describe('jwplayerRtdProvider', function() {
           })
         );
 
-        const targetingInfo = getTargetingForBid({
-          jwTargeting: {
-            mediaID: testIdForSuccess
-          }
-        });
+        const targetingInfo = getVatFromCache(testIdForSuccess);
 
         const validTargeting = {
           segments: validSegments,
@@ -62,21 +59,13 @@ describe('jwplayerRtdProvider', function() {
 
       it('should not write to cache when response is malformed', function() {
         request.respond('{]');
-        const targetingInfo = getTargetingForBid({
-          jwTargeting: {
-            mediaID: testIdForFailure
-          }
-        });
+        const targetingInfo = getVatFromCache(testIdForFailure);
         expect(targetingInfo).to.be.null;
       });
 
       it('should not write to cache when playlist is absent', function() {
         request.respond({});
-        const targetingInfo = getTargetingForBid({
-          jwTargeting: {
-            mediaID: testIdForFailure
-          }
-        });
+        const targetingInfo = getVatFromCache(testIdForFailure);
         expect(targetingInfo).to.be.null;
       });
 
@@ -92,21 +81,13 @@ describe('jwplayerRtdProvider', function() {
             ]
           })
         );
-        const targetingInfo = getTargetingForBid({
-          jwTargeting: {
-            mediaID: testIdForFailure
-          }
-        });
+        const targetingInfo = getVatFromCache(testIdForFailure);
         expect(targetingInfo).to.be.null;
       });
 
       it('should not write to cache when request errors', function() {
         request.error();
-        const targetingInfo = getTargetingForBid({
-          jwTargeting: {
-            mediaID: testIdForFailure
-          }
-        });
+        const targetingInfo = getVatFromCache(testIdForFailure);
         expect(targetingInfo).to.be.null;
       });
     });
@@ -393,6 +374,170 @@ describe('jwplayerRtdProvider', function() {
 
         jwplayerSubmodule.getData([adUnitWithMediaId, adUnitEmpty], bidRequestSpy);
         expect(bidRequestSpy.calledOnceWithExactly({})).to.be.true;
+      });
+    });
+  });
+
+  describe('Format targeting response', function () {
+    it('should exclude segment key when absent', function () {
+      const targeting = formatTargetingResponse({ mediaID: 'test' });
+      expect(targeting).to.not.have.property('segments');
+    });
+
+    it('should exclude content block when mediaId is absent', function () {
+      const targeting = formatTargetingResponse({ segments: ['test'] });
+      expect(targeting).to.not.have.property('content');
+    });
+
+    it('should return proper format', function () {
+      const segments = ['123'];
+      const mediaID = 'test';
+      const expectedContentId = 'jw_' + mediaID;
+      const expectedContent = {
+        id: expectedContentId
+      };
+      const targeting = formatTargetingResponse({
+        segments,
+        mediaID
+      });
+      expect(targeting).to.have.deep.property('segments', segments);
+      expect(targeting).to.have.deep.property('content', expectedContent);
+    });
+  });
+
+  describe('Get VAT from player', function () {
+    it('returns null when jwplayer.js is absent from page', function () {
+      const targeting = getTargetingForBid({
+        jwTargeting: {
+          playerID: invalidPlayerID,
+          mediaID: mediaIdNotCached
+        }
+      });
+      expect(targeting).to.be.null;
+    });
+
+    describe('When jwplayer.js is on page', function () {
+      const playlistItemWithSegmentMock = {
+        mediaid: mediaIdWithSegment,
+        jwpseg: validSegments
+      };
+
+      const targetingForMediaWithSegment = {
+        segments: validSegments,
+        mediaID: mediaIdWithSegment
+      };
+
+      const playlistItemNoSegmentMock = {
+        mediaid: mediaIdNoSegment
+      };
+
+      const currentItemSegments = ['test_seg_3', 'test_seg_4'];
+      const currentPlaylistItemMock = {
+        mediaid: mediaIdForCurrentItem,
+        jwpseg: currentItemSegments
+      };
+      const targetingForCurrentItem = {
+        segments: currentItemSegments,
+        mediaID: mediaIdForCurrentItem
+      };
+
+      const playerInstanceMock = {
+        getPlaylist: function () {
+          return [playlistItemWithSegmentMock, playlistItemNoSegmentMock];
+        },
+
+        getPlaylistItem: function () {
+          return currentPlaylistItemMock;
+        }
+      };
+
+      const jwplayerMock = function(playerID) {
+        if (playerID === validPlayerID) {
+          return playerInstanceMock;
+        } else {
+          return {};
+        }
+      };
+
+      beforeEach(function () {
+        window.jwplayer = jwplayerMock;
+      });
+
+      it('returns null when player ID does not match player on page', function () {
+        const targeting = getTargetingForBid({
+          jwTargeting: {
+            playerID: invalidPlayerID,
+            mediaID: mediaIdNotCached
+          }
+        });
+        expect(targeting).to.be.null;
+      });
+
+      it('returns segments when media ID matches a playlist item with segments', function () {
+        const targeting = getTargetingForBid({
+          jwTargeting: {
+            playerID: validPlayerID,
+            mediaID: mediaIdWithSegment
+          }
+        });
+        expect(targeting).to.deep.equal(targetingForMediaWithSegment);
+      });
+
+      it('caches segments media ID matches a playist item with segments', function () {
+        getTargetingForBid({
+          jwTargeting: {
+            playerID: validPlayerID,
+            mediaID: mediaIdWithSegment
+          }
+        });
+
+        window.jwplayer = null;
+        const targeting2 = getTargetingForBid({
+          jwTargeting: {
+            playerID: invalidPlayerID,
+            mediaID: mediaIdWithSegment
+          }
+        });
+        expect(targeting2).to.deep.equal(targetingForMediaWithSegment);
+      });
+
+      it('returns segments of current item when media ID is missing', function () {
+        const targeting = getTargetingForBid({
+          jwTargeting: {
+            playerID: validPlayerID
+          }
+        });
+        expect(targeting).to.deep.equal(targetingForCurrentItem);
+      });
+
+      it('caches segments from the current item', function () {
+        getTargetingForBid({
+          jwTargeting: {
+            playerID: validPlayerID
+          }
+        });
+
+        window.jwplayer = null;
+        const targeting2 = getTargetingForBid({
+          jwTargeting: {
+            playerID: invalidPlayerID,
+            mediaID: mediaIdForCurrentItem
+          }
+        });
+        expect(targeting2).to.deep.equal(targetingForCurrentItem);
+      });
+
+      it('returns undefined segments when segments are absent', function () {
+        const targeting = getTargetingForBid({
+          jwTargeting: {
+            playerID: validPlayerID,
+            mediaID: mediaIdNoSegment
+          }
+        });
+        expect(targeting).to.deep.equal({
+          mediaID: mediaIdNoSegment,
+          segments: undefined
+        });
       });
     });
   });
