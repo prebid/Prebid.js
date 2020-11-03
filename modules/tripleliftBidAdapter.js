84649e8c3dc836d65a9efccfb0b518c952eb3279
sdao-tl <49252703+sdao-tl@users.noreply.github.com>
2020-11-03 07:29:16 -0800

    TL: Add GVLID, update validation method, add unit tests (#5904)
    * Add IdentityLink support and fix UnifiedId.
    
    It appears we've been looking for UnifiedId userIds
    on the bidderRequest object, when they are found on bidRequests.
    This commit fixes that error, and adds support for IdentityLink.
    
    * change maintainer email to group
    
    * TripleLift: Sending schain (#1)
    
    * Sending schain
    
    * null -> undefined
    
    * Hardcode sync endpoint protocol
    
    * Switch to EB2 sync endpoint
    
    * Add support for image based user syncing
    
    * Rename endpoint variable
    
    * Add assertion
    
    * Add CCPA query param
    
    * Simplify check for usPrivacy argument
    
    * put advertiser name in the bid.meta field if it exists
    
    * update unit tests with meta.advertiserName field
    
    * Triplelift: FPD key value pair support (#5)
    
    * Triplelift: Add support for global fpd
    
    * don't filter fpd
    
    * adds coppa support back in
    
    * add gvlid, update validation method, add unit tests
    
    * remove advertiserDomains logic
    
    * typo
    
    * update _buildResponseObject to use new instream validation
    
    Co-authored-by: Will Chapin <wrchapin@gmail.com>
    Co-authored-by: colbertk <50499465+colbertk@users.noreply.github.com>
    Co-authored-by: David Andersen <davidwoodsandersen@gmail.com>
    Co-authored-by: Brandon Ling <bling@triplelift.com>
    Co-authored-by: colbertk <kcolbert@triplelift.com>
    Co-authored-by: Kevin Zhou <kzhou@triplelift.com>
    Co-authored-by: kzhouTL <43545828+kzhouTL@users.noreply.github.com>
    Co-authored-by: Sy Dao <iam.sydao@gmail.com>

--- modules/tripleliftBidAdapter.js
+++ modules/tripleliftBidAdapter.js
@@ -3,20 +3,17 @@ import { registerBidder } from '../src/adapters/bidderFactory.js';
 import * as utils from '../src/utils.js';
 import { config } from '../src/config.js';
 
+const GVLID = 28;
 const BIDDER_CODE = 'triplelift';
 const STR_ENDPOINT = 'https://tlx.3lift.com/header/auction?';
 let gdprApplies = true;
 let consentString = null;
 
 export const tripleliftAdapterSpec = {
-
+  gvlid: GVLID,
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER, VIDEO],
   isBidRequestValid: function (bid) {
-    if (bid.mediaTypes.video) {
-      let video = _getORTBVideo(bid);
-      if (!video.w || !video.h) return false;
-    }
     return typeof bid.params.inventoryCode !== 'undefined';
   },
 
@@ -288,7 +285,7 @@ function _buildResponseObject(bidderRequest, bid) {
       meta: {}
     };
 
-    if (breq.mediaTypes.video) {
+    if (_isInstreamBidRequest(breq)) {
       bidResponse.vastXml = bid.ad;
       bidResponse.mediaType = 'video';
     };
