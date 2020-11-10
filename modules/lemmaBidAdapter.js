9fddb97e8a276eaa28516ebca02fb065f009a41d
Lemma Dev <54662130+lemmadev@users.noreply.github.com>
2020-11-10 10:27:47 +0530

    updated include modules file extension.
    updated include modules js file extension.

--- modules/lemmaBidAdapter.js
+++ modules/lemmaBidAdapter.js
@@ -1,4 +1,4 @@
-import * as utils from '../src/utils';
+import * as utils from '../src/utils.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, VIDEO } from '../src/mediaTypes.js';
 
