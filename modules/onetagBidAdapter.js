792b728fcf25157754aeb47aab6bea31bcf7de63
Nicola <ciao@ciao>
2020-11-30 13:12:35 +0100

    Add bid info for outstream videos

--- modules/onetagBidAdapter.js
+++ modules/onetagBidAdapter.js
@@ -88,47 +88,34 @@ function interpretResponse(serverResponse, bidderRequest) {
   if (!body.bids || !Array.isArray(body.bids) || body.bids.length === 0) {
     return bids;
   }
-  body.bids.forEach(({
-    requestId,
-    cpm,
-    width,
-    height,
-    creativeId,
-    dealId,
-    currency,
-    mediaType,
-    ttl,
-    rendererUrl,
-    ad,
-    vastUrl,
-    videoCacheKey
-  }) => {
+  body.bids.forEach(bid => {
     const responseBid = {
-      requestId,
-      cpm,
-      width,
-      height,
-      creativeId,
-      dealId: dealId == null ? dealId : '',
-      currency,
+      requestId: bid.requestId,
+      cpm: bid.cpm,
+      width: bid.width,
+      height: bid.height,
+      creativeId: bid.creativeId,
+      dealId: bid.dealId == null ? bid.dealId : '',
+      currency: bid.currency,
       netRevenue: false,
+      mediaType: bid.mediaType,
       meta: {
-        mediaType
+        mediaType: bid.mediaType
       },
-      ttl: ttl || 300
+      ttl: bid.ttl || 300
     };
-    if (mediaType === BANNER) {
-      responseBid.ad = ad;
-    } else if (mediaType === VIDEO) {
-      const {context, adUnitCode} = find(requestData.bids, (item) => item.bidId === requestId);
+    if (bid.mediaType === BANNER) {
+      responseBid.ad = bid.ad;
+    } else if (bid.mediaType === VIDEO) {
+      const {context, adUnitCode} = find(requestData.bids, (item) => item.bidId === bid.requestId);
       if (context === INSTREAM) {
-        responseBid.vastUrl = vastUrl;
-        responseBid.videoCacheKey = videoCacheKey;
+        responseBid.vastUrl = bid.vastUrl;
+        responseBid.videoCacheKey = bid.videoCacheKey;
       } else if (context === OUTSTREAM) {
-        responseBid.vastXml = ad;
-        responseBid.vastUrl = vastUrl;
-        if (rendererUrl) {
-          responseBid.renderer = createRenderer({requestId, rendererUrl, adUnitCode});
+        responseBid.vastXml = bid.ad;
+        responseBid.vastUrl = bid.vastUrl;
+        if (bid.rendererUrl) {
+          responseBid.renderer = createRenderer({ ...bid, adUnitCode });
         }
       }
     }
@@ -146,23 +133,22 @@ function createRenderer(bid, rendererOptions = {}) {
     loaded: false
   });
   try {
-    renderer.setRender(onetagRenderer);
+    renderer.setRender(({renderer, width, height, vastXml, adUnitCode}) => {
+      renderer.push(() => {
+        window.onetag.Player.init({
+          ...bid,
+          width,
+          height,
+          vastXml,
+          nodeId: adUnitCode,
+          config: renderer.getConfig()
+        });
+      });
+    });
   } catch (e) {
 
   }
   return renderer;
-}
-
-function onetagRenderer({renderer, width, height, vastXml, adUnitCode}) {
-  renderer.push(() => {
-    window.onetag.Player.init({
-      width,
-      height,
-      vastXml,
-      nodeId: adUnitCode,
-      config: renderer.getConfig()
-    });
-  });
 }
 
 function getFrameNesting() {
