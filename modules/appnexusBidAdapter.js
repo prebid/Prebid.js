53b0ed0fbe9fca4cb8bd45d583db6a0de767ae6f
Neelanjan Sen <14229985+Fawke@users.noreply.github.com>
2020-12-02 23:27:04 +0530

    appnexusBidAdapter - remove tpuids (#6074)

--- modules/appnexusBidAdapter.js
+++ modules/appnexusBidAdapter.js
@@ -231,16 +231,10 @@ export const spec = {
     const criteoId = utils.deepAccess(bidRequests[0], `userId.criteoId`);
     let eids = [];
     if (criteoId) {
-      let tpuids = [];
-      tpuids.push({
-        'provider': 'criteo',
-        'user_id': criteoId
-      });
       eids.push({
         source: 'criteo.com',
         id: criteoId
       });
-      payload.tpuids = tpuids;
     }
 
     const tdid = utils.deepAccess(bidRequests[0], `userId.tdid`);
