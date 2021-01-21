144915d3928519e87be7c1f0a4aa586feb464f34
Harshad Mane <harshad.mane@pubmatic.com>
2021-01-20 18:11:27 -0800

    new module: bidViewability
    details in bidViewability.md

---
+++ modules/bidViewability.js
@@ -0,0 +1,52 @@
+// This module, when included, will trigger a BID_VIEWABLE event which can be consumed by Bidders and Analytics adapters
+// GPT API is used to find when a bid is viewable, https://developers.google.com/publisher-tag/reference#googletag.events.impressionviewableevent
+// Does not work with other than GPT integration
+
+import { config } from '../src/config.js';
+import * as events from '../src/events.js';
+import { EVENTS } from '../src/constants.json';
+import { logWarn, isFn } from '../src/utils.js';
+
+const MODULE_NAME = 'bidViewability';
+const CONFIG_FIRE_PIXELS = 'firePixels';
+const CONFIG_CUSTOM_MATCH = 'customMatchFunction';
+const BID_VURL_ARRAY = 'vurls';
+
+export function init() {
+
+	let isBidAdUnitCodeMatchingSlot = (bid, slot) => (slot.getAdUnitPath() === bid.adUnitCode || slot.getSlotElementId() === bid.adUnitCode);
+	
+	events.on(EVENTS.AUCTION_INIT, function(){
+		// add the GPT event listener
+	  	window.googletag = window.googletag || {};
+	    googletag.cmd = googletag.cmd || [];
+	  	googletag.cmd.push(function(){
+	        googletag.pubads().addEventListener('impressionViewable', function(event) {
+				let slot = event.slot;
+
+				// read the config for the module
+				const globalModuleConfig = config.getConfig(MODULE_NAME) || {};
+
+				// supports custom match function from config
+				let respectiveBid = pbjs.getAllWinningBids().find(
+					bid => isFn(globalModuleConfig[CONFIG_CUSTOM_MATCH]) ? globalModuleConfig[CONFIG_CUSTOM_MATCH](bid, slot) : isBidAdUnitCodeMatchingSlot(bid, slot)
+				);
+
+				if(respectiveBid === undefined){
+					logWarn(`bid details could not be found for ${slot.getSlotElementId()}, probable reasons: a non-prebid bid is served OR check the prebid.AdUnit.code to GPT.AdSlot relation.`);
+					return;
+				}				
+
+				// if config is enabled AND VURL array is present then execute each pixel
+				if(globalModuleConfig[CONFIG_FIRE_PIXELS] === true && respectiveBid.hasOwnProperty(BID_VURL_ARRAY)){
+					respectiveBid[BID_VURL_ARRAY].forEach(url => (new Image()).src = url );
+				}
+				
+				// emit the BID_VIEWABLE event with bid details, this event can be consumed by bidders and analytics pixels
+				events.emit(EVENTS.BID_VIEWABLE, respectiveBid);
+	        });
+	    });
+	});
+}
+
+init()
---
+++ modules/bidviewability.md
@@ -0,0 +1,43 @@
+# Overview
+
+Module Name: bidViewability
+
+Purpose: Track when a bid is viewable
+
+Maintainer: harshad.mane@pubmatic.com
+
+# Description
+- This module, when included, will trigger a BID_VIEWABLE event which can be consumed by Bidders and Analytics adapters
+- GPT API is used to find when a bid is viewable, https://developers.google.com/publisher-tag/reference#googletag.events.impressionviewableevent
+- The module does not work with adserver other than GAM with GPT integration
+- Logic used to find a matching pbjs-bid for a GPT slot is ``` (slot.getAdUnitPath() === bid.adUnitCode || slot.getSlotElementId() === bid.adUnitCode) ``` this logic can be changed by using param ```customMatchFunction```
+- When a rendered PBJS bid is viewable the module will trigger BID_VIEWABLE event, which can be consumed by bidders and analytics adapters
+- For the viewable bid if ```bid.vurls type array``` param is and module config ``` firePixels: true ``` is set then the URLs mentioned in bid.vurls will be executed
+
+# Params
+- firePixels [optional] [type: boolean], when set to true, will fire the urls mentioned in bid.vurls which should be array of urls
+- customMatchFunction [optional] [type: function(bid, slot)], when passed 
+
+As both params are optional, you do not need to set config if you do not want to set value for any param
+
+# Example of consuming BID_VIEWABLE event
+```
+	pbjs.onEvent('bidViewable', function(bid){
+		console.log('got bid details in bidViewable event', bid);
+	});
+
+```
+
+# Example of using config
+```
+	pbjs.setConfig({
+        bidViewability: {
+            firePixels: true,
+            customMatchFunction: function(bid, slot){
+                console.log('using custom match function....');
+                return bid.adUnitCode === slot.getAdUnitPath();
+            }
+        }
+    });
+```
+
