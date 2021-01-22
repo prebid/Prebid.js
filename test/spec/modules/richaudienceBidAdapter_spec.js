47b293c2ae56f3a628c6c17a80cbcd55fc75d5e2
sgimenez <sergi.gimenez@richaudience.com>
2021-01-22 10:17:09 +0100

    remove singlequote

--- test/spec/modules/richaudienceBidAdapter_spec.js
+++ test/spec/modules/richaudienceBidAdapter_spec.js
@@ -241,9 +241,9 @@ describe('Richaudience adapter tests', function () {
     expect(requestContent).to.have.property('transactionId').and.to.equal('29df2112-348b-4961-8863-1b33684d95e6');
     expect(requestContent).to.have.property('timeout').and.to.equal(3000);
     expect(requestContent).to.have.property('numIframes').and.to.equal(0);
-    expect(requestContent).to.have.property('scr_rsl').and.to.equal("1440x900");
+    expect(requestContent).to.have.property('scr_rsl').and.to.equal('1440x900');
     expect(requestContent).to.have.property('cpuc').and.to.equal(4);
-    expect(requestContent).to.have.property('kws').and.to.equal("coche=mercedes;coche=audi");
+    expect(requestContent).to.have.property('kws').and.to.equal('coche=mercedes;coche=audi');
   })
 
   it('Verify build request to prebid video inestream', function() {
