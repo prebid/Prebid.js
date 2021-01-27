0c33976a99110a7b6fc7a26602a2f807b56e05c4
mi.chen <mi.chen@criteo.com>
2021-01-27 17:39:10 +0100

    Update default fastbid adapter version

--- modules/criteoBidAdapter.js
+++ modules/criteoBidAdapter.js
@@ -18,7 +18,7 @@ const LOG_PREFIX = 'Criteo: ';
 
 // Unminified source code can be found in: https://github.com/Prebid-org/prebid-js-external-js-criteo/blob/master/dist/prod.js
 const FAST_BID_VERSION_PLACEHOLDER = '%FAST_BID_VERSION%';
-export const FAST_BID_VERSION_CURRENT = 102;
+export const FAST_BID_VERSION_CURRENT = 103;
 const FAST_BID_VERSION_LATEST = 'latest';
 const FAST_BID_VERSION_NONE = 'none';
 const PUBLISHER_TAG_URL_TEMPLATE = 'https://static.criteo.net/js/ld/publishertag.prebid' + FAST_BID_VERSION_PLACEHOLDER + '.js';
