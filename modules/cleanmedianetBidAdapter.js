8d2e7b44d1ca46b3cfad769e8b21c18f25dd75ff
CleanMediaNet <46790659+CleanMediaNet@users.noreply.github.com>
2021-02-25 02:49:24 -0500

    Update cleanmedianetBidAdapter.js

--- modules/cleanmedianetBidAdapter.js
+++ modules/cleanmedianetBidAdapter.js
@@ -64,7 +64,7 @@ export const spec = {
         } else {
           bidResponse.ad = resItem.adm;
         }
-         response.push(bidResponse);
+        response.push(bidResponse);
       }
     }
     return response;
