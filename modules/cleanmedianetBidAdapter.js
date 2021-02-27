abcf18509f19b2f9187b2e501ed4cc79511a1300
CleanMediaNet <46790659+CleanMediaNet@users.noreply.github.com>
2021-02-27 04:02:57 -0500

    Update cleanmedianetBidAdapter.js

--- modules/cleanmedianetBidAdapter.js
+++ modules/cleanmedianetBidAdapter.js
@@ -31,7 +31,7 @@ export const spec = {
   code: 'cleanmedianet',
   aliases: [],
   supportedMediaTypes: [BANNER, VIDEO],
-
+  
   isBidRequestValid: function (bid) {
     return (
       !!bid.params.supplyPartnerId &&
