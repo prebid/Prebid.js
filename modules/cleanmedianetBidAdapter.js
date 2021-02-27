430010509f1e949fce94a188152d125911c7a5b8
CleanMediaNet <46790659+CleanMediaNet@users.noreply.github.com>
2021-02-27 02:29:32 -0500

    Update cleanmedianetBidAdapter.js

--- modules/cleanmedianetBidAdapter.js
+++ modules/cleanmedianetBidAdapter.js
@@ -1,6 +1,5 @@
 import * as utils from '../src/utils.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
-import {config} from '../src/config.js';
 import {Renderer} from '../src/Renderer.js';
 import {BANNER, VIDEO} from '../src/mediaTypes.js';
 import includes from 'core-js-pure/features/array/includes.js';
