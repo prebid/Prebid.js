908a3c9ef8f423dadad520063e3d4521ee9c47ae
Tiago Faria <faria@interactiveoffers.com>
2021-03-09 19:57:03 +0000

    Interactive Offers bid adapter

--- test/spec/modules/interactiveOffersBidAdapter_spec.js
+++ test/spec/modules/interactiveOffersBidAdapter_spec.js
@@ -27,7 +27,7 @@ describe('Interactive Offers Prebbid.js Adapter', function() {
 
     it('returns a Prebid.js request object with a valid json string at the "data" property', function() {
       let request = spec.buildRequests(validBidRequests, bidderRequest);
-      expect(request.data).to.not.be.empty;
+      expect(request.data).length !== 0;
     });
   });
   describe('interpretResponse function', function() {
