ec1a8e215d575fd4fe76f10cbc8897074f6558aa
acsbendi <acsbendi28@gmail.com>
2021-03-12 10:32:48 +0100

    Implemented tests for isBidRequestValid.

---
+++ test/spec/modules/koblerBidAdapter_spec.js
@@ -0,0 +1,55 @@
+import {expect} from 'chai';
+import {spec} from '../../../modules/koblerBidAdapter.js';
+
+describe('KoblerAdapter', function () {
+  describe('isBidRequestValid', function() {
+    it('should not accept a request without bidId as valid', function() {
+      const bid = {
+        params: {
+          someParam: 'abc'
+        }
+      };
+
+      const result = spec.isBidRequestValid(bid);
+
+      expect(result).to.be.false;
+    });
+
+    it('should not accept a request without params as valid', function() {
+      const bid = {
+        bidId: 'e11768e8-3b71-4453-8698-0a2feb866589'
+      };
+
+      const result = spec.isBidRequestValid(bid);
+
+      expect(result).to.be.false;
+    });
+
+    it('should not accept a request without placementId as valid', function() {
+      const bid = {
+        bidId: 'e11768e8-3b71-4453-8698-0a2feb866589',
+        params: {
+          someParam: 'abc'
+        }
+      };
+
+      const result = spec.isBidRequestValid(bid);
+
+      expect(result).to.be.false;
+    });
+
+    it('should accept a request with bidId and placementId as valid', function() {
+      const bid = {
+        bidId: 'e11768e8-3b71-4453-8698-0a2feb866589',
+        params: {
+          someParam: 'abc',
+          placementId: '8bde0923-1409-4253-9594-495b58d931ba'
+        }
+      };
+
+      const result = spec.isBidRequestValid(bid);
+
+      expect(result).to.be.true;
+    });
+  });
+});
