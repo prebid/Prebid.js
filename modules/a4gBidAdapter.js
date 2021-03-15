71e6d1bc4c021c74a4cc44745270f06c0f7a9f56
junus <a.junusbek@gmail.com>
2021-03-15 13:29:59 +0600

    set crid if it's exist and added unit tests

--- modules/a4gBidAdapter.js
+++ modules/a4gBidAdapter.js
@@ -70,7 +70,7 @@ export const spec = {
       if (response.cpm > 0) {
         const bidResponse = {
           requestId: response.id,
-          creativeId: response.id,
+          creativeId: response.crid || response.id,
           cpm: response.cpm,
           width: response.width,
           height: response.height,
