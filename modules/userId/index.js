732cb02c42daba61bd648d3f0f72ba5dab4f6cbd
Michael Moschovas <michael.moschovas@yahoo.com>
2021-03-17 10:43:56 -0400

    Revert userId update. Committed in error

--- modules/userId/index.js
+++ modules/userId/index.js
@@ -762,7 +762,7 @@ function updateSubmodules() {
       callback: undefined,
       idObj: undefined
     } : null;
-  }).filter(submodule => submodule !== null);console.log(submodules);
+  }).filter(submodule => submodule !== null);
 
   if (!addedUserIdHook && submodules.length) {
     // priority value 40 will load after consentManagement with a priority of 50
