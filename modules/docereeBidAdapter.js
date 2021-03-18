4676090400732f0e3bbe3c5273520d5c8e253e92
sourbh-doceree <sourbh.gupta@doceree.com>
2021-03-18 16:24:22 +0530

    doceree adapter

---
+++ modules/docereeBidAdapter.js
@@ -0,0 +1,96 @@
+import * as utils from '../src/utils.js';
+import { ajax } from '../src/ajax.js';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { config } from '../src/config.js';
+import { BANNER } from '../src/mediaTypes.js';
+const BIDDER_CODE = 'doceree';
+const END_POINT = 'https://bidder.doceree.com'
+
+function processHTMLAndRender(responseJson, creativeHtml) {
+  if (responseJson.sourceURL) {
+    creativeHtml = creativeHtml.replace('<head>', `<head><style>html,body{padding:0;margin:0;}</style><base href="${responseJson.sourceURL.split('unzip')[0]}unzip/" target="_blank">`);
+  }
+  creativeHtml = creativeHtml.replaceAll('[TRACKING_LINK]', responseJson.ctaLink);
+  creativeHtml = creativeHtml.replaceAll(/DOCEREE_CLICK_URL_UNESC/g, responseJson.ctaLink);
+  while (creativeHtml.indexOf('_ucid') > -1) {
+    creativeHtml = creativeHtml.replace('_ucid', utils.generateUUID())
+  }
+  return creativeHtml;
+}
+
+export const spec = {
+  code: BIDDER_CODE,
+  url: '',
+  supportedMediaTypes: [ BANNER ],
+
+  isBidRequestValid: (bid) => {
+    const { placementId } = bid.params;
+    return !!placementId
+  },
+  buildRequests: (validBidRequests) => {
+    const serverRequests = [];
+    const { data } = config.getConfig('doceree.user')
+    const { page, domain, token } = config.getConfig('doceree.context')
+    const encodedUserInfo = window.btoa(encodeURIComponent(JSON.stringify(data)))
+
+    validBidRequests.forEach(function(validBidRequest) {
+      const { publisherUrl, placementId } = validBidRequest.params;
+      const url = publisherUrl || page
+      let queryString = '';
+      queryString = utils.tryAppendQueryString(queryString, 'id', placementId);
+      queryString = utils.tryAppendQueryString(queryString, 'publisherDomain', domain);
+      queryString = utils.tryAppendQueryString(queryString, 'pubRequestedURL', encodeURIComponent(url));
+      queryString = utils.tryAppendQueryString(queryString, 'loggedInUser', encodedUserInfo);
+      queryString = utils.tryAppendQueryString(queryString, 'currentUrl', url);
+      queryString = utils.tryAppendQueryString(queryString, 'prebidjs', true);
+      queryString = utils.tryAppendQueryString(queryString, 'token', token);
+      queryString = utils.tryAppendQueryString(queryString, 'requestId', validBidRequest.bidId);
+
+      serverRequests.push({
+        method: 'GET',
+        url: END_POINT + '/v1/adrequest?' + queryString
+      })
+    })
+    return serverRequests;
+  },
+  interpretResponse: (serverResponse, request) => {
+    const responseJson = serverResponse ? serverResponse.body : {};
+    const placementId = responseJson.DIVID;
+    const bidResponse = {
+      width: Number(responseJson.width),
+      height: Number(responseJson.height),
+      requestId: responseJson.guid,
+      netRevenue: true,
+      ttl: 30,
+      cpm: responseJson.cpmBid,
+      currency: responseJson.currency,
+      mediaType: 'banner',
+      creativeId: placementId,
+    };
+
+    if (responseJson.creativeType && responseJson.creativeType !== 'banner') {
+      if (responseJson.sourceHTML) {
+        responseJson.sourceHTML = responseJson.sourceHTML.replace(/[\uE000-\uF8FF]/g, '');
+        let creative = processHTMLAndRender(responseJson, responseJson.sourceHTML)
+        bidResponse.ad = `<div id="${placementId}">${creative}</div>`
+      } else {
+        ajax(responseJson.sourceURL, function(error, creativeHtml) {
+          if (!error) {
+            let creative = processHTMLAndRender(responseJson, creativeHtml)
+            bidResponse.ad = `<div id="${placementId}">${creative}</div>`
+          }
+        }, null, {method: 'GET', customHeaders: {'Content-type': 'plain/html; charset=utf-8'}});
+      }
+    } else {
+      if (responseJson.ctaLink) {
+        bidResponse.ad = `<a  href="${responseJson.ctaLink}" target="_blank"><img onload="${responseJson.impressionUrl}" src="${responseJson.sourceURL}" /></a>`;
+      } else {
+        bidResponse.ad = `<div id="${placementId}"><img onload="${responseJson.impressionUrl}" src="${responseJson.sourceURL}" /></div>`;
+      }
+    }
+    var bidResponses = [bidResponse];
+    return bidResponses
+  }
+};
+
+registerBidder(spec);
---
+++ test/spec/modules/docereeBidAdapter_spec.js
@@ -0,0 +1,93 @@
+import {expect} from 'chai';
+import {spec} from '../../../modules/docereeBidAdapter.js';
+import { config } from '../../../src/config.js';
+
+describe('BidlabBidAdapter', function () {
+  config.setConfig({
+    doceree: {
+      context: {
+        data: {
+          token: 'testing-token', // required
+        }
+      },
+      user: {
+        data: {
+          gender: '',
+          email: '',
+          hashedEmail: '',
+          firstName: '',
+          lastName: '',
+          npi: '',
+          hashedNPI: '',
+          city: '',
+          zipCode: '',
+          specialization: '',
+        }
+      }
+    }
+  });
+  let bid = {
+    bidId: 'testing',
+    bidder: 'doceree',
+    params: {
+      placementId: 'DOC_7jm9j5eqkl0xvc5w',
+    }
+  };
+
+  describe('isBidRequestValid', function () {
+    it('Should return true if placementId is present', function () {
+      expect(spec.isBidRequestValid(bid)).to.be.true;
+    });
+    it('Should return false if placementId is not present', function () {
+      delete bid.params.placementId;
+      expect(spec.isBidRequestValid(bid)).to.be.false;
+    });
+  });
+
+  describe('buildRequests', function () {
+    let serverRequest = spec.buildRequests([bid]);
+    serverRequest = serverRequest[0]
+    it('Creates a ServerRequest object with method, URL and data', function () {
+      expect(serverRequest).to.exist;
+      expect(serverRequest.method).to.exist;
+      expect(serverRequest.url).to.exist;
+    });
+    it('Returns GET method', function () {
+      expect(serverRequest.method).to.equal('GET');
+    });
+    it('Returns valid URL', function () {
+      expect(serverRequest.url).to.equal('https://bidder.doceree.com/v1/adrequest?id=DOC_7jm9j5eqkl0xvc5w&pubRequestedURL=undefined&loggedInUser=JTdCJTIyZ2VuZGVyJTIyJTNBJTIyJTIyJTJDJTIyZW1haWwlMjIlM0ElMjIlMjIlMkMlMjJoYXNoZWRFbWFpbCUyMiUzQSUyMiUyMiUyQyUyMmZpcnN0TmFtZSUyMiUzQSUyMiUyMiUyQyUyMmxhc3ROYW1lJTIyJTNBJTIyJTIyJTJDJTIybnBpJTIyJTNBJTIyJTIyJTJDJTIyaGFzaGVkTlBJJTIyJTNBJTIyJTIyJTJDJTIyY2l0eSUyMiUzQSUyMiUyMiUyQyUyMnppcENvZGUlMjIlM0ElMjIlMjIlMkMlMjJzcGVjaWFsaXphdGlvbiUyMiUzQSUyMiUyMiU3RA%3D%3D&prebidjs=true&requestId=testing&');
+    });
+  });
+  describe('interpretResponse', function () {
+    it('Should interpret banner response', function () {
+      const banner = {
+        body: {
+          DIVID: 'DOC_7jm9j5eqkl0xvc5w',
+          creativeType: 'banner',
+          guid: 'G125fzC5NKl3FHeOT8yvL98ILfQS9TVUgk6Q',
+          currency: 'USD',
+          cpmBid: 2,
+          height: '250',
+          width: '300',
+          ctaLink: 'https://doceree.com/',
+          sourceURL: 'https://servedbydoceree.doceree.com/generic/300x250_DocereeGeneralAd.jpg',
+        }
+      };
+      let bannerResponses = spec.interpretResponse(banner);
+      expect(bannerResponses).to.be.an('array').that.is.not.empty;
+      let dataItem = bannerResponses[0];
+      expect(dataItem).to.have.all.keys('requestId', 'cpm', 'width', 'height', 'ad', 'ttl',
+        'netRevenue', 'currency', 'mediaType', 'creativeId');
+      expect(dataItem.requestId).to.equal('G125fzC5NKl3FHeOT8yvL98ILfQS9TVUgk6Q');
+      expect(dataItem.cpm).to.equal(2);
+      expect(dataItem.width).to.equal(300);
+      expect(dataItem.height).to.equal(250);
+      expect(dataItem.ad).to.equal('<a  href="https://doceree.com/" target="_blank"><img onload="undefined" src="https://servedbydoceree.doceree.com/generic/300x250_DocereeGeneralAd.jpg" /></a>');
+      expect(dataItem.ttl).to.equal(30);
+      expect(dataItem.netRevenue).to.be.true;
+      expect(dataItem.currency).to.equal('USD');
+      expect(dataItem.creativeId).to.equal('DOC_7jm9j5eqkl0xvc5w');
+    });
+  })
+});
