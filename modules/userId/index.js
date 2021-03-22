6b01eabfb48d904e094a630a65979142d69b5dea
Patrick McCann <patmmccann@gmail.com>
2021-03-22 13:10:06 -0400

    Update index.js

--- modules/userId/index.js
+++ modules/userId/index.js
@@ -823,12 +823,11 @@ export function init(config) {
     }
     // userSync.ppid should be one of the 'source' values in getUserIdsAsEids() eg pubcid.org or id5-sync.com
     if (userSync && userSync.userIds && userSync.ppid && findIndex(getUserIdsAsEids(), x => x.source === userSync.ppid) >= 0 && typeof getUserIdsAsEids().find(x => x.source === userSync.ppid).uids[0].id === 'string') {
-      let googletag = window.googletag || {};
+      window.googletag = window.googletag || { cmd: [] };
       const ppid = getUserIdsAsEids().find(x => x.source === userSync.ppid).uids[0].id.replace(/[\W_]/g, '');
       if (ppid.length >= 32 && ppid.length <= 150) {
-        googletag.cmd = googletag.cmd || [];
-        googletag.cmd.push(function() {
-          googletag.pubads().setPublisherProvidedId(ppid)
+        window.googletag.cmd.push(function() {
+          window.googletag.pubads().setPublisherProvidedId(ppid)
         });
       }
     }
