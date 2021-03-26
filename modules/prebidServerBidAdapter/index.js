ec640c63c4c2646111bc283195cf7bd70ab275c0
Jason Snellbaker <jsnellbaker@appnexus.com>
2021-03-26 12:14:38 -0400

    add support for non-purpose1 consent domains

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -124,6 +124,17 @@ function validateConfigRequiredProps(option) {
   }
 }
 
+// temporary change to modify the s2sConfig for new format used for endpoint URLs;
+// could be removed later as part of a major release, if we decide to not support the old format
+function formatUrlParams(option) {
+  ['endpoint', 'syncEndpoint'].forEach((prop) => {
+    if (utils.isStr(option[prop])) {
+      let temp = option[prop];
+      option[prop] = { p1Consent: temp, noP1Consent: temp };
+    }
+  });
+}
+
 /**
  * @param {(S2SConfig[]|S2SConfig)} options
  */
@@ -135,6 +146,7 @@ function setS2sConfig(options) {
 
   const activeBidders = [];
   const optionsValid = normalizedOptions.every((option, i, array) => {
+    formatUrlParams(options);
     const updateSuccess = updateConfigDefaultVendor(option);
     if (updateSuccess !== false) {
       const valid = validateConfigRequiredProps(option);
@@ -208,7 +220,7 @@ function queueSync(bidderCodes, gdprConsent, uspConsent, s2sConfig) {
   }
 
   const jsonPayload = JSON.stringify(payload);
-  ajax(s2sConfig.syncEndpoint,
+  ajax(getMatchingConsentUrl(s2sConfig.syncEndpoint, gdprConsent),
     (response) => {
       try {
         response = JSON.parse(response);
@@ -966,6 +978,20 @@ function bidWonHandler(bid) {
   }
 }
 
+function hasPurpose1Consent(gdprConsent) {
+  let result = true;
+  if (gdprConsent) {
+    if (gdprConsent.gdprApplies && gdprConsent.apiVersion === 2) {
+      result = !!(utils.deepAccess(gdprConsent, 'vendorData.purpose.consents.1') === true);
+    }
+  }
+  return result;
+}
+
+function getMatchingConsentUrl(urlProp, gdprConsent) {
+  return hasPurpose1Consent(gdprConsent) ? urlProp.p1Consent : urlProp.noP1Consent;
+}
+
 /**
  * Bidder adapter for Prebid Server
  */
@@ -975,6 +1001,11 @@ export function PrebidServer() {
   /* Prebid executes this function when the page asks to send out bid requests */
   baseAdapter.callBids = function(s2sBidRequest, bidRequests, addBidResponse, done, ajax) {
     const adUnits = utils.deepClone(s2sBidRequest.ad_units);
+    let gdprConsent, uspConsent;
+    if (Array.isArray(bidRequests) && bidRequests.length > 0) {
+      gdprConsent = bidRequests[0].gdprConsent;
+      uspConsent = bidRequests[0].uspConsent;
+    }
 
     // at this point ad units should have a size array either directly or mapped so filter for that
     const validAdUnits = adUnits.filter(unit =>
@@ -988,13 +1019,7 @@ export function PrebidServer() {
       .filter(utils.uniques);
 
     if (Array.isArray(_s2sConfigs)) {
-      if (s2sBidRequest.s2sConfig && s2sBidRequest.s2sConfig.syncEndpoint) {
-        let gdprConsent, uspConsent;
-        if (Array.isArray(bidRequests) && bidRequests.length > 0) {
-          gdprConsent = bidRequests[0].gdprConsent;
-          uspConsent = bidRequests[0].uspConsent;
-        }
-
+      if (s2sBidRequest.s2sConfig && s2sBidRequest.s2sConfig.syncEndpoint && getMatchingConsentUrl(s2sBidRequest.s2sConfig.syncEndpoint, gdprConsent)) {
         let syncBidders = s2sBidRequest.s2sConfig.bidders
           .map(bidder => adapterManager.aliasRegistry[bidder] || bidder)
           .filter((bidder, index, array) => (array.indexOf(bidder) === index));
@@ -1007,7 +1032,7 @@ export function PrebidServer() {
       utils.logInfo('BidRequest: ' + requestJson);
       if (request && requestJson) {
         ajax(
-          s2sBidRequest.s2sConfig.endpoint,
+          getMatchingConsentUrl(s2sBidRequest.s2sConfig.endpoint, gdprConsent),
           {
             success: response => handleResponse(response, requestedBidders, bidRequests, addBidResponse, done, s2sBidRequest.s2sConfig),
             error: done
