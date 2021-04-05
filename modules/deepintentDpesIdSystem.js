61908ee79366c4f6434c56a175e40b41a54d59e8
Sourabh Gandhe <sourabh@Sourabhs-MacBook-Pro.local>
2021-04-05 21:32:15 +0530

    fix for passing the eids

--- modules/deepintentDpesIdSystem.js
+++ modules/deepintentDpesIdSystem.js
@@ -1,16 +1,17 @@
 /**
  * This module adds DPES to the User ID module
  * The {@link module:modules/userId} module is required
- * @module modules/deepintentDpesSubmodule
+ * @module modules/deepintentDpesSystem
  * @requires module:modules/userId
  */
 
-import * as utils from "../src/utils.js";
-import { submodule } from "../src/hook.js";
-import { getStorageManager } from "../src/storageManager.js";
+import * as utils from '../src/utils.js';
+import { submodule } from '../src/hook.js';
+import { getStorageManager } from '../src/storageManager.js';
 
-const DEEPINTENT_DPES_ID = "di_dpes";
-export const storage = getStorageManager();
+const MODULE_NAME = 'deepintentId';
+export const storage = getStorageManager(null, MODULE_NAME);
+const DEEPINTENT_DPES_ID = 'di_dpes';
 
 function readCookie(cookieName) {
   // return 1231231;
@@ -31,15 +32,24 @@ export const deepintentDpesSubmodule = {
    * used to link submodule with config
    * @type {string}
    */
-  name: "deepintentDpesIdSystem",
+  name: MODULE_NAME,
   /**
    * decode the stored id value for passing to bid requests
    * @function
-   * @param { Object | string | undefined } value
-   * @return { Object | string | undefined }
+   * @param {{value:string}} value
+   * @returns {{deepintentId:Object}}
    */
-  decode(value) {
-    return value ? value : undefined;
+  decode(value, config) {
+    const configParams = (config && config.params) || {};
+    if (configParams && configParams.identityKey && configParams.siteId && value.siteId && configParams.siteId == value.siteId) {
+      if (configParams.identityKey === 'hashedEmail') {
+        value = {'deepintentId': value.email}
+        return value;
+      } else if (configParams.identityKey === 'hashedNPI') {
+        value = {'deepintentId': value.npi}
+        return value;
+      }
+    }
   },
   /**
    * performs action to obtain id and return a value in the callback's response argument
@@ -48,21 +58,24 @@ export const deepintentDpesSubmodule = {
    * @return {{id: string | undefined} | undefined}
    */
   getId(config) {
-    const configParams = (config && config.params) || {};
+    const configStorage = (config && config.storage) || {};
+
     let id = null;
     if (
-      configParams &&
-      Array.isArray(configParams.identifiersToResolve) &&
-      configParams.identifiersToResolve.length > 0 &&
-      configParams.type === "cookie"
+      configStorage &&
+      Array.isArray(configStorage.identifiersToResolve) &&
+      configStorage.identifiersToResolve.length > 0 &&
+      configStorage.name &&
+      configStorage.type === 'cookie'
     ) {
-      id = readCookie(configParams.identifiersToResolve[0]);
+      id = readCookie(configStorage.name);
     }
-    if (configParams && configParams.type === "html5") {
+    if (configStorage && configStorage.type === 'html5') {
       id = readFromLocalStorage();
     }
+
     return id ? { id } : undefined;
   },
 };
 
-submodule("userId", deepintentDpesSubmodule);
+submodule('userId', deepintentDpesSubmodule);
