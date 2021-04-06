546164181c20a5d41a9e5804b3fd5a2303fd49e5
Sergio <sergio.anru@gmail.com>
2021-04-06 11:43:23 +0200

    remove bid.meta.advertiserDomains

--- test/spec/modules/adponeBidAdapter_spec.js
+++ test/spec/modules/adponeBidAdapter_spec.js
@@ -126,9 +126,6 @@ describe('adponeBidAdapter', function () {
                   impid: '2579e20c0bb89_0',
                   price: 1,
                   adm: '<html><a href="https://www.adpone.com" target="_blank"><img src ="https://placehold.it/300x250" /></a></html>',
-                  adomain: [
-                    'www.addomain.com'
-                  ],
                   iurl: 'https://localhost11',
                   crid: 'creative111',
                   h: 250,
