ca113dbaa75090696c20fcf7cbda0c585bc20093
Patrick McCann <patmmccann@gmail.com>
2021-04-26 23:39:15 -0400

    Update aolBidAdapter.js

--- modules/aolBidAdapter.js
+++ modules/aolBidAdapter.js
@@ -417,6 +417,8 @@ export const spec = {
       currency: response.cur || 'USD',
       dealId: bidData.dealid,
       netRevenue: true,
+      meta: {
+        advertiserDomains: bidData && bidData.adomain : bidData.adomain ? []
       ttl: bidRequest.ttl
     };
   },
