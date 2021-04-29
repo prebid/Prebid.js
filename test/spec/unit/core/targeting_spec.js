4d9a6a45458f2131f289d722c8e60d0a9fa17b5b
Patrick McCann <patmmccann@gmail.com>
2021-04-29 03:47:09 -0400

    PBjs Core: remove cache_id and source as default targeting keys (#6496)
    * Remove uuid and source as default targeting keys
    
    * Update targeting_spec.js
    
    * Update targeting_spec.js
    
    * Update targeting_spec.js
    
    * Update constants.json
    
    * Update targeting_spec.js
    
    * Update dfpAdServerVideo.js

--- test/spec/unit/core/targeting_spec.js
+++ test/spec/unit/core/targeting_spec.js
@@ -456,6 +456,44 @@ describe('targeting tests', function () {
       });
     });
 
+    describe('targetingControls:only allow default_targeting_keys', function () {
+      let bid4;
+
+      beforeEach(function() {
+        bid4 = utils.deepClone(bid1);
+        bid4.adserverTargeting = {
+          hb_deal: '4321',
+          hb_pb: '0.1',
+          hb_uuid: 'myuuid',
+          hb_source: 's2s',
+          hb_adid: '567891011',
+          hb_bidder: 'appnexus',
+        };
+        bid4.bidder = bid4.bidderCode = 'appnexus';
+        bid4.cpm = 0.1; // losing bid so not included if enableSendAllBids === false
+        bid4.dealId = '4321';
+        enableSendAllBids = true;
+        bidsReceived.push(bid4);
+      });
+
+      it('targeting should include custom keys', function () {
+        const targeting = targetingInstance.getAllTargeting(['/123456/header-bid-tag-0']);
+        expect(targeting['/123456/header-bid-tag-0']).to.include.all.keys('foobar');
+      });
+
+      it('targeting should include keys in default targeting keys', function () {
+        const targeting = targetingInstance.getAllTargeting(['/123456/header-bid-tag-0']);
+        expect(targeting['/123456/header-bid-tag-0']).to.include.all.keys('hb_bidder_rubicon', 'hb_adid_rubicon', 'hb_pb_rubicon', 'hb_deal_rubicon');
+        expect(targeting['/123456/header-bid-tag-0']).to.include.all.keys('hb_bidder_appnexus', 'hb_adid_appnexus', 'hb_pb_appnexus', 'hb_deal_appnexus');
+      });
+
+      it('targeting should not include keys not in default targeting keys', function () {
+        const targeting = targetingInstance.getAllTargeting(['/123456/header-bid-tag-0']);
+        expect(targeting['/123456/header-bid-tag-0']).to.not.have.all.keys(['hb_cache_id_appnexus', 'hb_cache_id_rubicon', 'hb_source_appnexus', 'hb_source_rubicon']);
+        expect(targeting['/123456/header-bid-tag-0']).to.not.have.all.keys(['hb_cache_id', 'hb_source']);
+      });
+    });
+
     describe('targetingControls.alwaysIncludeDeals', function () {
       let bid4;
 
