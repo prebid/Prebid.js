79e1f35265565a7b01d1d39df76738be4813290e
Max Duval <max.duval@guardian.co.uk>
2021-04-30 16:14:29 +0100

    bump analytics version to 9

--- test/spec/modules/guAnalyticsAdapter_spec.js
+++ test/spec/modules/guAnalyticsAdapter_spec.js
@@ -443,7 +443,7 @@ describe('Gu analytics adapter', () => {
   it('should have a version number', () => {
     events.emit(CONSTANTS.EVENTS.BID_WON, BIDWONEXAMPLE);
     const payload = JSON.parse(ajaxStub.firstCall.args);
-    expect(payload.v).to.be.equal(8);
+    expect(payload.v).to.be.equal(9);
   });
 
   it('should ignore responses sent with bid won event', () => {
