8cc86d2b150197e809a7fd016f9c60add45a9ef0
marc_tappx <prebid@tappx.com>
2021-05-14 14:26:08 +0200

    tappxBidAdapter: update tappx adapter version

--- modules/tappxBidAdapter.js
+++ modules/tappxBidAdapter.js
@@ -8,7 +8,7 @@ import { config } from '../src/config.js';
 const BIDDER_CODE = 'tappx';
 const TTL = 360;
 const CUR = 'USD';
-const TAPPX_BIDDER_VERSION = '0.1.10420';
+const TAPPX_BIDDER_VERSION = '0.1.10514';
 const TYPE_CNN = 'prebidjs';
 const VIDEO_SUPPORT = ['instream'];
 
