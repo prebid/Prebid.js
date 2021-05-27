f1327dd5f59b363150db5c1f63ac2657660b0b3c
atkachov <atkachov@admixer.ua>
2021-05-27 16:02:08 +0300

    add tests

--- test/spec/modules/admixerIdSystem_spec.js
+++ test/spec/modules/admixerIdSystem_spec.js
@@ -1,12 +1,16 @@
 import {admixerIdSubmodule} from 'modules/admixerIdSystem.js';
+import {spec as admixerBidAdapter} from 'modules/admixerBidAdapter.js';
 import * as utils from 'src/utils.js';
 import {server} from 'test/mocks/xhr.js';
 import {getStorageManager} from '../../../src/storageManager.js';
+import {getHook} from '../../../src/hook.js';
 
 export const storage = getStorageManager();
 
-const pid = '4D393FAC-B6BB-4E19-8396-0A4813607316';
-const getIdParams = {params: {pid: pid}};
+const PID = '4D393FAC-B6BB-4E19-8396-0A4813607316';
+const SID = '4DF4419E-8BF2-40DC-8F8A-6E2D4B418644';
+const RID = '05E5B75A-8936-444F-9867-29E71D47E113';
+const getIdParams = {params: {pid: PID}};
 describe('admixerId tests', function () {
   let logErrorStub;
 
@@ -42,7 +46,7 @@ describe('admixerId tests', function () {
     let submoduleCallback = admixerIdSubmodule.getId(getIdParams).callback;
     submoduleCallback(callBackSpy);
     let request = server.requests[0];
-    expect(request.url).to.be.eq(`https://inv-nets.admixer.net/cntcm.aspx?ssp=${pid}`);
+    expect(request.url).to.be.eq(`https://inv-nets.admixer.net/cntcm.aspx?ssp=${PID}`);
     request.respond(
       200,
       {},
@@ -51,12 +55,30 @@ describe('admixerId tests', function () {
     expect(callBackSpy.calledOnce).to.be.true;
   });
 
+  it('should call the admixer id endpoint with sid', function () {
+    let submoduleCallback = admixerIdSubmodule.getId(getIdParams, {gdprApplies: false}, SID).callback;
+    submoduleCallback(function() {});
+    let request = server.requests[0];
+    const requestUrlObj = utils.parseUrl(request.url);
+    expect(requestUrlObj.search).to.deep.equal({'ssp': PID, 'sid': SID});
+  });
+
+  it('should call the admixer id endpoint with rid', function () {
+    let submoduleCallback = admixerIdSubmodule.getId(getIdParams).callback;
+    admixerBidAdapter.interpretResponse({body: {ads: [], visitorId: RID}});
+    submoduleCallback(function() {});
+    let request = server.requests[0];
+    const requestUrlObj = utils.parseUrl(request.url);
+    expect(requestUrlObj.search).to.deep.equal({'ssp': PID, 'rid': RID});
+    getHook('admixerIdSystem.retrieveVisitorId').removeAll();
+  });
+
   it('should call callback with user id', function () {
     let callBackSpy = sinon.spy();
     let submoduleCallback = admixerIdSubmodule.getId(getIdParams).callback;
     submoduleCallback(callBackSpy);
     let request = server.requests[0];
-    expect(request.url).to.be.eq(`https://inv-nets.admixer.net/cntcm.aspx?ssp=${pid}`);
+    expect(request.url).to.be.eq(`https://inv-nets.admixer.net/cntcm.aspx?ssp=${PID}`);
     request.respond(
       200,
       {},
@@ -71,7 +93,7 @@ describe('admixerId tests', function () {
     let submoduleCallback = admixerIdSubmodule.getId(getIdParams).callback;
     submoduleCallback(callBackSpy);
     let request = server.requests[0];
-    expect(request.url).to.be.eq(`https://inv-nets.admixer.net/cntcm.aspx?ssp=${pid}`);
+    expect(request.url).to.be.eq(`https://inv-nets.admixer.net/cntcm.aspx?ssp=${PID}`);
     request.respond(
       204
     );
