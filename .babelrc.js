1952e367e7fc118908bd069b1b7e8da7c2c80fe4
Kenan Shifflett <kenan@spanishdict.com>
2021-06-01 18:53:11 -0400

    Replace uglify with terser.

--- .babelrc.js
+++ .babelrc.js
@@ -29,6 +29,7 @@ module.exports = {
   ],
   "plugins": [
     path.resolve(__dirname, './plugins/pbjsGlobals.js'),
-    useLocal('babel-plugin-transform-object-assign')
+    useLocal('babel-plugin-transform-object-assign'),
+    useLocal('@babel/plugin-proposal-object-rest-spread')
   ]
 };
