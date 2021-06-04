00f6caa75129461df417b771cef08a61ec9e9e8a
Myles Wehr <mwehr@zetaglobal.com>
2021-06-04 11:20:02 -0400

    update Prebid definer id

--- modules/zetaBidAdapter.js
+++ modules/zetaBidAdapter.js
@@ -3,7 +3,7 @@ import { registerBidder } from '../src/adapters/bidderFactory.js';
 import {BANNER} from '../src/mediaTypes.js';
 const BIDDER_CODE = 'zeta_global';
 const ENDPOINT_URL = 'https://prebid.rfihub.com/prebid';
-const USER_SYNC_URL = 'https://p.rfihub.com/cm?pub=42770&in=1';
+const USER_SYNC_URL = 'https://p.rfihub.com/cm?pub=44253&in=1';
 const DEFAULT_CUR = 'USD';
 const TTL = 200;
 const NET_REV = true;
