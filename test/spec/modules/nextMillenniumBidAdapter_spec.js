8cac6d7b7dc202ea48a1de4cf03a5d84e689d6b1
Mikhail Ivanchenko <mifanich1991@gmail.com>
2021-06-07 21:57:17 +0300

    Nextmillinnium adapter improvement (#6957)
    * change structure of request and response
    
    * fixes
    
    * remove logInfo
    
    * update doc

--- test/spec/modules/nextMillenniumBidAdapter_spec.js
+++ test/spec/modules/nextMillenniumBidAdapter_spec.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import * as utils from '../../../src/utils.js';
 import { spec } from 'modules/nextMillenniumBidAdapter.js';
 
 describe('nextMillenniumBidAdapterTests', function() {
@@ -8,20 +9,19 @@ describe('nextMillenniumBidAdapterTests', function() {
         bidId: 'transaction_1234',
         bidder: 'nextMillennium',
         params: {
-          placement_id: 12345
+          placement_id: '12345'
         },
         sizes: [[300, 250]]
       }
     ]
   };
-  let request = [];
 
   it('validate_pub_params', function() {
     expect(
       spec.isBidRequestValid({
         bidder: 'nextMillennium',
         params: {
-          placement_id: 12345
+          placement_id: '12345'
         }
       })
     ).to.equal(true);
@@ -32,7 +32,7 @@ describe('nextMillenniumBidAdapterTests', function() {
       {
         bidId: 'bid1234',
         bidder: 'nextMillennium',
-        params: { placement_id: -1 },
+        params: { placement_id: '-1' },
         sizes: [[300, 250]]
       }
     ];
@@ -40,34 +40,36 @@ describe('nextMillenniumBidAdapterTests', function() {
     expect(request[0].bidId).to.equal('bid1234');
   });
 
-  it('validate_getUserSyncs_function', function() {
-    expect(spec.getUserSyncs({ iframeEnabled: true })).to.have.lengthOf(1);
-    expect(spec.getUserSyncs({ iframeEnabled: false })).to.have.lengthOf(0);
-
-    let pixel = spec.getUserSyncs({ iframeEnabled: true });
-    expect(pixel[0].type).to.equal('iframe');
-    expect(pixel[0].url).to.equal('https://brainlyads.com/hb/s2s/matching');
-  });
-
   it('validate_response_params', function() {
     let serverResponse = {
       body: {
-        cpm: 1.7,
-        width: 300,
-        height: 250,
-        creativeId: 'p35t0enob6twbt9mofjc8e',
-        ad: 'Hello! It\'s a test ad!'
+        id: 'f7b3d2da-e762-410c-b069-424f92c4c4b2',
+        seatbid: [
+          {
+            bid: [
+              {
+                id: '7457329903666272789',
+                price: 0.5,
+                adm: 'Hello! It\'s a test ad!',
+                adid: '96846035',
+                adomain: ['test.addomain.com'],
+                w: 300,
+                h: 250
+              }
+            ]
+          }
+        ],
+        cur: 'USD'
       }
     };
 
     let bids = spec.interpretResponse(serverResponse, bidRequestData.bids[0]);
     expect(bids).to.have.lengthOf(1);
-
     let bid = bids[0];
 
-    expect(bid.creativeId).to.equal('p35t0enob6twbt9mofjc8e');
+    expect(bid.creativeId).to.equal('96846035');
     expect(bid.ad).to.equal('Hello! It\'s a test ad!');
-    expect(bid.cpm).to.equal(1.7);
+    expect(bid.cpm).to.equal(0.5);
     expect(bid.width).to.equal(300);
     expect(bid.height).to.equal(250);
     expect(bid.currency).to.equal('USD');
