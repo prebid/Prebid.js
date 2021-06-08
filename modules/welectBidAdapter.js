ee90abe6909505677565e071477dc1fcd6cc3c2e
Nick Duitz <42961155+nduitz@users.noreply.github.com>
2021-06-08 16:31:14 +0200

    Welect Bid Adapter: support advertiser domain (#6966)
    * be explicit about what our server returns
    
    * support advertiserDomains in response

--- modules/welectBidAdapter.js
+++ modules/welectBidAdapter.js
@@ -84,7 +84,21 @@ export const spec = {
     }
 
     const bidResponses = [];
-    const bidResponse = responseBody.bidResponse;
+    const bidResponse = {
+      requestId: responseBody.bidResponse.requestId,
+      cpm: responseBody.bidResponse.cpm,
+      width: responseBody.bidResponse.width,
+      height: responseBody.bidResponse.height,
+      creativeId: responseBody.bidResponse.creativeId,
+      currency: responseBody.bidResponse.currency,
+      netRevenue: responseBody.bidResponse.netRevenue,
+      ttl: responseBody.bidResponse.ttl,
+      ad: responseBody.bidResponse.ad,
+      vastUrl: responseBody.bidResponse.vastUrl,
+      meta: {
+        advertiserDomains: responseBody.bidResponse.meta.advertiserDomains
+      }
+    };
     bidResponses.push(bidResponse);
     return bidResponses;
   },
