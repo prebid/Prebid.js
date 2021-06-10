2ba110266c91830375d70634ac9b7b2cccf7260f
John Mayor <john@Johns-iMac.local>
2021-06-10 15:28:56 -0400

    removed user agent bot check and IE URL parsing issue

--- modules/datablocksBidAdapter.js
+++ modules/datablocksBidAdapter.js
@@ -455,21 +455,23 @@ export const spec = {
     // APPEND PARAMS TO SYNC URL
     function addParams(sync) {
       // PARSE THE URL
-      let url = new URL(sync.url);
-      let urlParams = {};
-      for (const [key, value] of url.searchParams.entries()) {
-        urlParams[key] = value;
-      };
+      try {
+        let url = new URL(sync.url);
+        let urlParams = {};
+        for (const [key, value] of url.searchParams.entries()) {
+          urlParams[key] = value;
+        };
 
-      // APPLY EXTRA VARS
-      urlParams.gdpr = gdprData.gdpr;
-      urlParams.gdprConsent = gdprData.gdprConsent;
-      urlParams.bidid = bidResponse.bidid;
-      urlParams.id = bidResponse.id;
-      urlParams.uid = dbBuyerId;
+        // APPLY EXTRA VARS
+        urlParams.gdpr = gdprData.gdpr;
+        urlParams.gdprConsent = gdprData.gdprConsent;
+        urlParams.bidid = bidResponse.bidid;
+        urlParams.id = bidResponse.id;
+        urlParams.uid = dbBuyerId;
 
-      // REBUILD URL
-      sync.url = `${url.origin}${url.pathname}?${Object.keys(urlParams).map(key => key + '=' + encodeURIComponent(urlParams[key])).join('&')}`;
+        // REBUILD URL
+        sync.url = `${url.origin}${url.pathname}?${Object.keys(urlParams).map(key => key + '=' + encodeURIComponent(urlParams[key])).join('&')}`;
+      } catch (e) {};
 
       // RETURN THE REBUILT URL
       return sync;
@@ -573,17 +575,6 @@ export class BotClientTests {
         }
 
         return false;
-      },
-      user_agent: function() {
-        try {
-          var re = new RegExp('(googlebot\/|bot|Googlebot-Mobile|Googlebot-Image|Google favicon|Mediapartners-Google|bingbot|slurp|java|wget|curl|Commons-HttpClient|Python-urllib|libwww|httpunit|nutch|phpcrawl|msnbot|jyxobot|FAST-WebCrawler|FAST Enterprise Crawler|biglotron|teoma|convera|seekbot|gigablast|exabot|ngbot|ia_archiver|GingerCrawler|webmon |httrack|webcrawler|grub.org|UsineNouvelleCrawler|antibot|netresearchserver|speedy|fluffy|bibnum.bnf|findlink|msrbot|panscient|yacybot|AISearchBot|IOI|ips-agent|tagoobot|MJ12bot|dotbot|woriobot|yanga|buzzbot|mlbot|yandexbot|purebot|Linguee Bot|Voyager|CyberPatrol|voilabot|baiduspider|citeseerxbot|spbot|twengabot|postrank|turnitinbot|scribdbot|page2rss|sitebot|linkdex|Adidxbot|blekkobot|ezooms|dotbot|Mail.RU_Bot|discobot|heritrix|findthatfile|europarchive.org|NerdByNature.Bot|sistrix crawler|ahrefsbot|Aboundex|domaincrawler|wbsearchbot|summify|ccbot|edisterbot|seznambot|ec2linkfinder|gslfbot|aihitbot|intelium_bot|facebookexternalhit|yeti|RetrevoPageAnalyzer|lb-spider|sogou|lssbot|careerbot|wotbox|wocbot|ichiro|DuckDuckBot|lssrocketcrawler|drupact|webcompanycrawler|acoonbot|openindexspider|gnam gnam spider|web-archive-net.com.bot|backlinkcrawler|coccoc|integromedb|content crawler spider|toplistbot|seokicks-robot|it2media-domain-crawler|ip-web-crawler.com|siteexplorer.info|elisabot|proximic|changedetection|blexbot|arabot|WeSEE:Search|niki-bot|CrystalSemanticsBot|rogerbot|360Spider|psbot|InterfaxScanBot|Lipperhey SEO Service|CC Metadata Scaper|g00g1e.net|GrapeshotCrawler|urlappendbot|brainobot|fr-crawler|binlar|SimpleCrawler|Livelapbot|Twitterbot|cXensebot|smtbot|bnf.fr_bot|A6-Indexer|ADmantX|Facebot|Twitterbot|OrangeBot|memorybot|AdvBot|MegaIndex|SemanticScholarBot|ltx71|nerdybot|xovibot|BUbiNG|Qwantify|archive.org_bot|Applebot|TweetmemeBot|crawler4j|findxbot|SemrushBot|yoozBot|lipperhey|y!j-asr|Domain Re-Animator Bot|AddThis)', 'i');
-          if (re.test(navigator.userAgent)) {
-            return true;
-          }
-          return false;
-        } catch (e) {
-          return false;
-        }
       },
 
       selenium: function () {
