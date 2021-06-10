e862f7cc4ef9405367ebe11a719a7d7f171caca2
Wojciech Biały <wb@WojciechBialy.local>
2021-06-10 13:23:50 +0200

    [sspbc-adapter] Update to adapter version 5.0 - support for first party data and user agent client hints

--- modules/sspBCBidAdapter.js
+++ modules/sspBCBidAdapter.js
@@ -1,7 +1,7 @@
 import * as utils from '../src/utils.js';
 import { ajax } from '../src/ajax.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
-import { BANNER } from '../src/mediaTypes.js';
+import { BANNER, NATIVE } from '../src/mediaTypes.js';
 import strIncludes from 'core-js-pure/features/string/includes.js';
 
 const BIDDER_CODE = 'sspBC';
@@ -9,10 +9,11 @@ const BIDDER_URL = 'https://ssp.wp.pl/bidder/';
 const SYNC_URL = 'https://ssp.wp.pl/bidder/usersync';
 const NOTIFY_URL = 'https://ssp.wp.pl/bidder/notify';
 const TMAX = 450;
-const BIDDER_VERSION = '4.9';
+const BIDDER_VERSION = '5.1';
 const W = window;
 const { navigator } = W;
 const oneCodeDetection = {};
+const adSizesCalled = {};
 var consentApiVersion;
 
 /**
@@ -69,7 +70,7 @@ const cookieSupport = () => {
 };
 
 const applyClientHints = ortbRequest => {
-  const connection = navigator.connection || false;
+  const { connection = {}, deviceMemory, userAgentData = {} } = navigator;
   const viewport = W.visualViewport || false;
   const segments = [];
   const hints = {
@@ -77,9 +78,11 @@ const applyClientHints = ortbRequest => {
     'CH-Rtt': connection.rtt,
     'CH-SaveData': connection.saveData,
     'CH-Downlink': connection.downlink,
-    'CH-DeviceMemory': navigator.deviceMemory,
+    'CH-DeviceMemory': deviceMemory,
     'CH-Dpr': W.devicePixelRatio,
     'CH-ViewportWidth': viewport.width,
+    'CH-BrowserBrands': JSON.stringify(userAgentData.brands),
+    'CH-isMobile': userAgentData.mobile,
   };
 
   Object.keys(hints).forEach(key => {
@@ -152,23 +155,112 @@ const mapBanner = slot => {
   }
 }
 
+/**
+ * @param {string} paramName Native parameter name
+ * @param {object} paramValue Native parameter value
+ * @returns {object} native asset object that conforms to ortb native ads spec
+ */
+const mapAsset = (paramName, paramValue) => {
+  let asset;
+  switch (paramName) {
+    case 'title':
+      asset = {
+        id: 0,
+        required: paramValue.required,
+        title: { len: paramValue.len }
+      }
+      break;
+    case 'cta':
+      asset = {
+        id: 1,
+        required: paramValue.required,
+        data: { type: 12 }
+      }
+      break;
+    case 'icon':
+      asset = {
+        id: 2,
+        required: paramValue.required,
+        img: { type: 1, w: paramValue.sizes[0], h: paramValue.sizes[1] }
+      }
+      break;
+    case 'image':
+      asset = {
+        id: 3,
+        required: paramValue.required,
+        img: { type: 3, w: paramValue.sizes[0], h: paramValue.sizes[1] }
+      }
+      break;
+    case 'body':
+      asset = {
+        id: 4,
+        required: paramValue.required,
+        data: { type: 2 }
+      }
+      break;
+    case 'sponsoredBy':
+      asset = {
+        id: 5,
+        required: paramValue.required,
+        data: { type: 1 }
+      }
+      break;
+  }
+  return asset;
+}
+
+/**
+ * @param {object} slot Ad Unit Params by Prebid
+ * @returns {object} native object that conforms to ortb native ads spec
+ */
+const mapNative = slot => {
+  const native = utils.deepAccess(slot, 'mediaTypes.native');
+  let assets;
+  if (native) {
+    const nativeParams = Object.keys(native);
+    assets = [];
+    nativeParams.forEach(par => {
+      const newAsset = mapAsset(par, native[par]);
+      if (newAsset) { assets.push(newAsset) };
+    });
+  }
+  return assets;
+}
+
 const mapImpression = slot => {
-  const { adUnitCode, bidId, params } = slot;
-  const { id, siteId } = params || {};
+  const { adUnitCode, bidId, params = {}, ortb2Imp = {} } = slot;
+  const { id, siteId } = params;
+  const { ext = {} } = ortb2Imp;
+
+  /*
+     check max size for this imp, and check/store number this size was called (for current view)
+     send this info as ext.pbsize
+  */
+  const slotSize = slot.sizes.length ? slot.sizes.reduce((prev, next) => prev[0] * prev[1] <= next[0] * next[1] ? next : prev).join('x') : '1x1';
+  adSizesCalled[slotSize] = adSizesCalled[slotSize] ? adSizesCalled[slotSize] += 1 : 1;
+  ext.data = Object.assign({ pbsize: `${slotSize}_${adSizesCalled[slotSize]}` }, ext.data);
+
   const imp = {
     id: id && siteId ? id : 'bidid-' + bidId,
     banner: mapBanner(slot),
-    /* native: mapNative(slot), */
+    native: mapNative(slot),
     tagid: adUnitCode,
+    ext,
   };
 
   // Check floorprices for this imp
   if (typeof slot.getFloor === 'function') {
-    // sspBC adapter accepts only floor per imp - check for maximum value for requested ad sizes
-    imp.bidfloor = slot.sizes.reduce((prev, next) => {
-      const currentFloor = slot.getFloor({ mediaType: 'banner', size: next }).floor;
-      return prev > currentFloor ? prev : currentFloor;
-    }, 0);
+    let bannerFloor = 0;
+    let nativeFloor = 0;
+    // sspBC adapter accepts only floor per imp - check for maximum value for requested ad types and sizes
+    if (slot.sizes.length) {
+      bannerFloor = slot.sizes.reduce((prev, next) => {
+        const currentFloor = slot.getFloor({ mediaType: 'banner', size: next }).floor;
+        return prev > currentFloor ? prev : currentFloor;
+      }, 0);
+    }
+    nativeFloor = slot.getFloor({ mediaType: 'native' });
+    imp.bidfloor = Math.max(bannerFloor, nativeFloor);
   }
   return imp;
 }
@@ -250,7 +342,7 @@ const renderCreative = (site, auctionId, bid, seat, request) => {
 const spec = {
   code: BIDDER_CODE,
   aliases: [],
-  supportedMediaTypes: [BANNER],
+  supportedMediaTypes: [BANNER, NATIVE],
   isBidRequestValid(bid) {
     // as per OneCode integration, bids without params are valid
     return true;
