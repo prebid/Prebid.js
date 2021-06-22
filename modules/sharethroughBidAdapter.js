4bc9bb785fc130f4eb4a213961e5a7126136c124
epechuzal <epechuzal@sharethrough.com>
2021-06-22 16:44:34 -0700

    Parse and send gpid param

--- modules/sharethroughBidAdapter.js
+++ modules/sharethroughBidAdapter.js
@@ -29,8 +29,13 @@ export const sharethroughAdapterSpec = {
         instant_play_capable: canAutoPlayHTML5Video(),
         hbSource: 'prebid',
         hbVersion: '$prebid.version$',
-        strVersion: VERSION
+        strVersion: VERSION,
       };
+
+      const gpid = utils.deepAccess(bidRequest, 'ortb2Imp.ext.data.pbadslot');
+      if (gpid) {
+        query.gpid = gpid;
+      }
 
       Object.assign(query, handleUniversalIds(bidRequest));
 
