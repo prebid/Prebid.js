a7feeff78992d23d6759e26c5917ef1e0d3e0e85
Manasi Moghe <manasi.moghe@pubmatic.com>
2021-06-24 18:33:24 +0530

    fix for linting error

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -739,7 +739,7 @@ function _addFloorFromFloorModule(impObj, bid) {
   impObj.bidfloor = ((!isNaN(bidFloor) && bidFloor > 0) ? bidFloor : UNDEFINED);
 }
 
-function _populateSegmentDataIfAvailable(fpdUserData={}, segmentData=[]) {
+function _populateSegmentDataIfAvailable(fpdUserData = {}, segmentData = []) {
   // fpdUserData - data set from global and bidder level configs
   // segmentData - data set from permutive
   var dataMerged = false;
