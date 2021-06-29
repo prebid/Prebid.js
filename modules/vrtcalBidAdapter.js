9ecb7d5a40f392a5283dacfd02f0d8d6212f5c97
Chris Huie <phoenixtechnerd@gmail.com>
2021-06-29 06:20:00 -0700

    fix lint errors

--- modules/vrtcalBidAdapter.js
+++ modules/vrtcalBidAdapter.js
@@ -73,7 +73,7 @@ export const spec = {
       if (response.seatbid[0].bid[0].adomain && response.seatbid[0].bid[0].adomain.length) {
 	    bidResponse.meta = {
 	      advertiserDomains: response.seatbid[0].bid[0].adomain
-	    };
+	      };
       }
 
       bidResponses.push(bidResponse);
