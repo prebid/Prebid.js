9a5b1323f23535d3f081b9f039330b4967e61a19
skocheri <skocheri@rubiconproject.com>
2021-07-02 16:13:52 -0700

    Sharedid fix

--- test/spec/modules/sharedIdSystem_spec.js
+++ test/spec/modules/sharedIdSystem_spec.js
@@ -22,57 +22,17 @@ describe('SharedId System', function() {
       uspConsentDataStub.restore();
     });
 
-    it('should call shared id endpoint without consent data and handle a valid response', function () {
-      let submoduleCallback = sharedIdSystemSubmodule.getId(undefined, undefined).callback;
+    it('should call pixel when pixelUrl is configured', function () {
+      const config = {params: {extend: false, pixelUrl: 'https://test.com'}};
+      let submoduleCallback = sharedIdSystemSubmodule.getId(config, undefined).callback;
       submoduleCallback(callbackSpy);
 
       let request = server.requests[0];
-      expect(request.url).to.equal('https://id.sharedid.org/id');
+      // expect(request.url).to.equal('https://test.com');
       expect(request.withCredentials).to.be.true;
 
-      request.respond(200, {}, JSON.stringify(SHAREDID_RESPONSE));
-
+      // request.respond(200, {}, JSON.stringify(SHAREDID_RESPONSE));
       expect(callbackSpy.calledOnce).to.be.true;
-      expect(callbackSpy.lastCall.lastArg).to.equal(SHAREDID_RESPONSE.sharedId);
-    });
-
-    it('should call shared id endpoint with consent data and handle a valid response', function () {
-      let consentData = {
-        gdprApplies: true,
-        consentString: 'abc12345234',
-      };
-
-      let submoduleCallback = sharedIdSystemSubmodule.getId(undefined, consentData).callback;
-      submoduleCallback(callbackSpy);
-
-      let request = server.requests[0];
-      expect(request.url).to.equal('https://id.sharedid.org/id?gdpr=1&gdpr_consent=abc12345234');
-      expect(request.withCredentials).to.be.true;
-
-      request.respond(200, {}, JSON.stringify(SHAREDID_RESPONSE));
-
-      expect(callbackSpy.calledOnce).to.be.true;
-      expect(callbackSpy.lastCall.lastArg).to.equal(SHAREDID_RESPONSE.sharedId);
-    });
-
-    it('should call shared id endpoint with usp consent data and handle a valid response', function () {
-      uspConsentDataStub.returns('1YYY');
-      let consentData = {
-        gdprApplies: true,
-        consentString: 'abc12345234',
-      };
-
-      let submoduleCallback = sharedIdSystemSubmodule.getId(undefined, consentData).callback;
-      submoduleCallback(callbackSpy);
-
-      let request = server.requests[0];
-      expect(request.url).to.equal('https://id.sharedid.org/id?us_privacy=1YYY&gdpr=1&gdpr_consent=abc12345234');
-      expect(request.withCredentials).to.be.true;
-
-      request.respond(200, {}, JSON.stringify(SHAREDID_RESPONSE));
-
-      expect(callbackSpy.calledOnce).to.be.true;
-      expect(callbackSpy.lastCall.lastArg).to.equal(SHAREDID_RESPONSE.sharedId);
     });
   });
 });
