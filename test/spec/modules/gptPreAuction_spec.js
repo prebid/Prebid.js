8767a27f1a12ab8d8b1d9fd2c71036bf929077d0
Michael  Moschovas <mmoschovas@mmoschovas-mac.fios-router.home>
2021-08-02 13:50:07 -0400

    Fix unit test desc typo

--- test/spec/modules/gptPreAuction_spec.js
+++ test/spec/modules/gptPreAuction_spec.js
@@ -95,7 +95,7 @@ describe('GPT pre-auction module', () => {
       expect(adUnit.ortb2Imp.ext.data.adserver).to.deep.equal({ name: 'gam', adslot: 'slotCode2' });
     });
 
-    it('will trim child id if mcmEnabled is not set to true', () => {
+    it('will trim child id if mcmEnabled is set to true', () => {
       config.setConfig({ gptPreAuction: { enabled: true, mcmEnabled: true } });
       window.googletag.pubads().setSlots([
         makeSlot({ code: '/12345,21212/slotCode1', divId: 'div1' }),
