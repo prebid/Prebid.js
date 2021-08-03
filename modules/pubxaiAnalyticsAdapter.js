d3bc772b7ab39a63ae9edfa789529d37580cc6e0
Phaneendra Hegde <phaneendra.hegde@gmail.com>
2021-08-03 14:20:27 +0530

    Replaced custom code to get the matching gptSlot with an existing utils function

--- modules/pubxaiAnalyticsAdapter.js
+++ modules/pubxaiAnalyticsAdapter.js
@@ -20,7 +20,6 @@ let events = {
   pageDetail: {},
   deviceDetail: {}
 };
-let googletag = window.googletag || {};
 
 var pubxaiAnalyticsAdapter = Object.assign(adapter(
   {
@@ -54,20 +53,11 @@ var pubxaiAnalyticsAdapter = Object.assign(adapter(
   }
 });
 
-function getGptSlotName(adUnitCode) {
-  let slots = googletag.pubads().getSlots();
-  let slot = slots.find(slot => (slot.getSlotElementId() === adUnitCode || slot.getSlotId().getName() === adUnitCode));
-  if (slot) {
-    return slot.getSlotId().getName();
-  }
-  return null;
-}
-
 function mapBidResponse(bidResponse, status) {
   if (typeof bidResponse !== 'undefined') {
     let bid = {
       adUnitCode: bidResponse.adUnitCode,
-      gptSlotCode: getGptSlotName(bidResponse.adUnitCode),
+      gptSlotCode: utils.getGptSlotInfoForAdUnitCode(bidResponse.adUnitCode).gptSlot || null,
       auctionId: bidResponse.auctionId,
       bidderCode: bidResponse.bidder,
       cpm: bidResponse.cpm,
