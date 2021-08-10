0ccf3135ec7a1b3b455c4c31ec41fae405260cd7
karimJWP <karimJWP@github.com>
2021-08-09 20:24:10 -0400

    tests ad and time states

--- test/spec/modules/videoModule/submodules/jwplayerVideoProvider_spec.js
+++ test/spec/modules/videoModule/submodules/jwplayerVideoProvider_spec.js
@@ -8,18 +8,201 @@ import {
 
 import {
   PROTOCOLS, API_FRAMEWORKS, VIDEO_MIME_TYPE, PLAYBACK_METHODS, PLACEMENT, VPAID_MIME_TYPE
-} from 'modules/videoModule/constants/ortb.js'
+} from 'modules/videoModule/constants/ortb.js';
+
+import {
+  PLAYBACK_MODE
+} from 'modules/videoModule/constants/events.js'
 
 describe('JWPlayerProvider', function () {
 
 });
 
 describe('adStateFactory', function () {
+  let adState = adStateFactory();
 
+  beforeEach(function() {
+    adState.clearState();
+  });
+
+  it('should update state for ad events', function () {
+    const tag = 'tag';
+    const adPosition = 'adPosition';
+    const timeLoading = 'timeLoading';
+    const id = 'id';
+    const description = 'description';
+    const adsystem = 'adsystem';
+    const adtitle = 'adtitle';
+    const advertiserId = 'advertiserId';
+    const advertiser = 'advertiser';
+    const dealId = 'dealId';
+    const linear = 'linear';
+    const vastversion = 'vastversion';
+    const mediaFile = 'mediaFile';
+    const adId = 'adId';
+    const universalAdId = 'universalAdId';
+    const creativeAdId = 'creativeAdId';
+    const creativetype = 'creativetype';
+    const clickThroughUrl = 'clickThroughUrl';
+    const witem = 'witem';
+    const wcount = 'wcount';
+    const podcount = 'podcount';
+    const sequence = 'sequence';
+
+    adState.updateForEvent({
+      tag,
+      adPosition,
+      timeLoading,
+      id,
+      description,
+      adsystem,
+      adtitle,
+      advertiserId,
+      advertiser,
+      dealId,
+      linear,
+      vastversion,
+      mediaFile,
+      adId,
+      universalAdId,
+      creativeAdId,
+      creativetype,
+      clickThroughUrl,
+      witem,
+      wcount,
+      podcount,
+      sequence
+    });
+
+    const state = adState.getState();
+    expect(state.adTagUrl).to.equal(tag);
+    expect(state.offset).to.equal(adPosition);
+    expect(state.loadTime).to.equal(timeLoading);
+    expect(state.vastAdId).to.equal(id);
+    expect(state.adDescription).to.equal(description);
+    expect(state.adServer).to.equal(adsystem);
+    expect(state.adTitle).to.equal(adtitle);
+    expect(state.advertiserId).to.equal(advertiserId);
+    expect(state.dealId).to.equal(dealId);
+    expect(state.linear).to.equal(linear);
+    expect(state.vastVersion).to.equal(vastversion);
+    expect(state.creativeUrl).to.equal(mediaFile);
+    expect(state.adId).to.equal(adId);
+    expect(state.universalAdId).to.equal(universalAdId);
+    expect(state.creativeId).to.equal(creativeAdId);
+    expect(state.creativeType).to.equal(creativetype);
+    expect(state.redirectUrl).to.equal(clickThroughUrl);
+    expect(state).to.have.property('adPlacementType');
+    expect(state.adPlacementType).to.be.undefined;
+    expect(state.waterfallIndex).to.equal(witem);
+    expect(state.waterfallCount).to.equal(wcount);
+    expect(state.adPodCount).to.equal(podcount);
+    expect(state.adPodIndex).to.equal(sequence);
+  });
+
+  it('should convert placement to oRTB value', function () {
+    adState.updateForEvent({
+      placement: 'instream'
+    });
+
+    let state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.IN_STREAM);
+
+    adState.updateForEvent({
+      placement: 'banner'
+    });
+
+    state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.BANNER);
+
+    adState.updateForEvent({
+      placement: 'article'
+    });
+
+    state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.ARTICLE);
+
+    adState.updateForEvent({
+      placement: 'feed'
+    });
+
+    state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.FEED);
+
+    adState.updateForEvent({
+      placement: 'interstitial'
+    });
+
+    state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.INTERSTITIAL);
+
+    adState.updateForEvent({
+      placement: 'slider'
+    });
+
+    state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.SLIDER);
+
+    adState.updateForEvent({
+      placement: 'floating'
+    });
+
+    state = adState.getState();
+    expect(state.adPlacementType).to.be.equal(PLACEMENT.FLOATING);
+  });
 });
 
 describe('timeStateFactory', function () {
+  let timeState = timeStateFactory();
 
+  beforeEach(function() {
+    timeState.clearState();
+  });
+
+  it('should update state for VOD time event', function() {
+    const position = 5;
+    const test_duration = 30;
+
+    timeState.updateForEvent({
+      position,
+      duration: test_duration
+    });
+
+    const { time, duration, playbackMode } = timeState.getState();
+    expect(time).to.be.equal(position);
+    expect(duration).to.be.equal(test_duration);
+    expect(playbackMode).to.be.equal(PLAYBACK_MODE.VOD);
+  });
+
+  it('should update state for LIVE time events', function() {
+    const position = 0;
+    const test_duration = 0;
+
+    timeState.updateForEvent({
+      position,
+      duration: test_duration
+    });
+
+    const { time, duration, playbackMode } = timeState.getState();
+    expect(time).to.be.equal(position);
+    expect(duration).to.be.equal(test_duration);
+    expect(playbackMode).to.be.equal(PLAYBACK_MODE.LIVE);
+  });
+
+  it('should update state for DVR time events', function() {
+    const position = -5;
+    const test_duration = -30;
+
+    timeState.updateForEvent({
+      position,
+      duration: test_duration
+    });
+
+    const { time, duration, playbackMode } = timeState.getState();
+    expect(time).to.be.equal(position);
+    expect(duration).to.be.equal(test_duration);
+    expect(playbackMode).to.be.equal(PLAYBACK_MODE.DVR);
+  });
 });
 
 describe('callbackStorageFactory', function () {
@@ -63,6 +246,7 @@ describe('callbackStorageFactory', function () {
     expect(callbackStorage.getCallback('event', callback1)).to.be.undefined;
   });
 });
+
 describe('utils', function () {
   describe('getJwConfig', function () {
     const getJwConfig = utils.getJwConfig;
