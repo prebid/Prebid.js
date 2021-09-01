d40f47899fe9232c46e2d1f767f12153d7dd82a7
Karim Mourra <karim@jwplayer.com>
2021-09-01 18:32:52 -0400

    implements ad unit enrichment

--- modules/videoModule/index.js
+++ modules/videoModule/index.js
@@ -1,40 +1,43 @@
-import {getGlobal} from '../../src/prebidGlobal.js';
 import {config} from '../../src/config.js';
 import events from '../../src/events.js';
 import CONSTANTS from '../../src/constants.json'
 import { videoCoreFactory } from './coreVideo.js'
 
-export function PbVideo(videoCore_, getConfig_, requestBids_, onPbEvents_) {
+export function PbVideo(videoCore_, getConfig_, pbGlobal_, onPbEvents_) {
   const videoCore = videoCore_;
   const getConfig = getConfig_;
-  const requestBids = requestBids_;
+  const pbGlobal = pbGlobal_;
+  const requestBids = pbGlobal.requestBids;
   const onPbEvents = onPbEvents_;
 
   function init() {
     getConfig('video', ({ video }) => {
       video.providers.forEach(provider => {
-        videoCore.registerProvider(provider);
+        try {
+          videoCore.registerProvider(provider);
+        } catch (e) {}
       });
-      // maybe video.providers to get changes on providers
-      // called whenever 'video' is updated
-      // instantiate anc check for new submodules
     });
 
-    // before bids are requested , getOrtbParams and write to the ad units.
     requestBids.before(enrichAdUnits, 40);
 
     // bidsBackHandler -> setAdTagUrl
     onPbEvents(CONSTANTS.EVENTS.AUCTION_END, function(auctionResult) {
-      // get winning bid + vast xml
+      // get ad tag from adServer - auctionResult.winningBids
+      // coreVideo.setAdTagUrl(adTag, divId);
+      this.winningBids = auctionResult.winningBids;
     });
 
     // analytics registering and surfacing
   }
 
   function enrichAdUnits(nextFn, bidRequest) {
-    // get oRTB arams
-    // write to ad units
-    // let adUnits = bidRequest.adUnits
+    const adUnits = bidRequest.adUnits || pbGlobal.adUnits;
+    adUnits.forEach(adUnit => {
+      const oRtbParams = videoCore.getOrtbParams(adUnit.video.divId);
+      adUnit.mediaTypes.video = Object.assign({}, adUnit.mediaTypes.video, oRtbParams);
+    });
+    return nextFn.call(this, config);
   }
 
   return { init };
@@ -42,7 +45,7 @@ export function PbVideo(videoCore_, getConfig_, requestBids_, onPbEvents_) {
 
 function pbVideoFactory() {
   const videoCore = videoCoreFactory();
-  const pbVideo = PbVideo(videoCore, config.getConfig, getGlobal().requestBids, events.on);
+  const pbVideo = PbVideo(videoCore, config.getConfig, $$PREBID_GLOBAL$$, events.on);
   pbVideo.init();
   return pbVideo;
 }
