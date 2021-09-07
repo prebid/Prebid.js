81bba06059e44d0c042cf8fef42ec4f6b9b993fc
Karim Mourra <karim@jwplayer.com>
2021-09-07 18:11:02 -0400

    restores factory

--- modules/videoModule/index.js
+++ modules/videoModule/index.js
@@ -1,8 +1,8 @@
-// import {config} from '../../src/config.js';
+import {config} from '../../src/config.js';
 import events from '../../src/events.js';
 import { allVideoEvents } from './constants/events.js';
 import CONSTANTS from '../../src/constants.json';
-// import { videoCoreFactory } from './coreVideo.js';
+import { videoCoreFactory } from './coreVideo.js';
 
 events.addEvents(allVideoEvents);
 
@@ -47,11 +47,11 @@ export function PbVideo(videoCore_, getConfig_, pbGlobal_, pbEvents_, videoEvent
   return { init };
 }
 
-// function pbVideoFactory() {
-//   const videoCore = videoCoreFactory();
-//   const pbVideo = PbVideo(videoCore, config.getConfig, $$PREBID_GLOBAL$$, events, allVideoEvents);
-//   pbVideo.init();
-//   return pbVideo;
-// }
-//
-// pbVideoFactory();
+function pbVideoFactory() {
+  const videoCore = videoCoreFactory();
+  const pbVideo = PbVideo(videoCore, config.getConfig, $$PREBID_GLOBAL$$, events, allVideoEvents);
+  pbVideo.init();
+  return pbVideo;
+}
+
+pbVideoFactory();
