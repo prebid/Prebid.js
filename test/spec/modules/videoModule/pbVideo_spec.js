02a4b7d6e6f3c3c20d3c3be9eaecd98d1d1750ce
Karim Mourra <karim@jwplayer.com>
2021-09-07 16:53:20 -0400

    nests the event trigger tests

--- test/spec/modules/videoModule/pbVideo_spec.js
+++ test/spec/modules/videoModule/pbVideo_spec.js
@@ -51,12 +51,12 @@ describe('Prebid Video', function () {
   describe('Setting video to config', function () {
     let providers = [{ divId: 'div1' }, { divId: 'div2' }];
     let getConfigCallback;
-    let customGetConfig = (video, callback) => {
+    let getConfig = (video, callback) => {
       getConfigCallback = callback;
     };
 
     beforeEach(() => {
-      const pbVideo = pbVideoFactory(null, customGetConfig, null, null, null);
+      pbVideoFactory(null, getConfig);
       getConfigCallback({ video: { providers } });
     });
 
@@ -71,25 +71,21 @@ describe('Prebid Video', function () {
       expect(onEventsSpy.getCall(1).args[2]).to.be.equal('div2');
     });
 
-    it('Should emit events off of Prebid\'s Events', function () {
-      let eventHandler;
-      const customVideoCore = Object.assign({}, videoCoreMock, {
-        onEvents: (events, eventHandler_) => eventHandler = eventHandler_
+    describe('Event triggering', function () {
+      it('Should emit events off of Prebid\'s Events', function () {
+        let eventHandler;
+        const videoCore = Object.assign({}, videoCoreMock, {
+          onEvents: (events, eventHandler_) => eventHandler = eventHandler_
+        });
+        pbVideoFactory(videoCore, getConfig);
+        getConfigCallback({ video: { providers } });
+        const expectedType = 'test_event';
+        const expectedPayload = {'test': 'data'};
+        eventHandler(expectedType, expectedPayload);
+        expect(pbEventsMock.emit.calledOnce).to.be.true;
+        expect(pbEventsMock.emit.getCall(0).args[0]).to.be.equal(expectedType);
+        expect(pbEventsMock.emit.getCall(0).args[1]).to.be.equal(expectedPayload);
       });
-      const pbVideo = pbVideoFactory(customVideoCore, customGetConfig, null, null, null);
-      getConfigCallback({ video: { providers } });
-      const expectedType = 'test_event';
-      const expectedPayload = {'test': 'data'};
-      eventHandler(expectedType, expectedPayload);
-      expect(pbEventsMock.emit.calledOnce).to.be.true;
-      expect(pbEventsMock.emit.getCall(0).args[0]).to.be.equal(expectedType);
-      expect(pbEventsMock.emit.getCall(0).args[1]).to.be.equal(expectedPayload);
-    });
-  });
-
-  describe('Event triggering', function () {
-    it('Should emit events off of Prebid\'s Events', function () {
-
     });
   });
 
@@ -105,7 +101,8 @@ describe('Prebid Video', function () {
       const requestBids = {
         before: callback_ => beforeBidRequestCallback = callback_
       };
-      const pbVideo = pbVideoFactory(null, null, { requestBids }, null);
+
+      const pbVideo = pbVideoFactory(null, null, { requestBids });
       expect(beforeBidRequestCallback).to.not.be.undefined;
       const nextFn = sinon.spy();
       const adUnits = [{
@@ -120,6 +117,7 @@ describe('Prebid Video', function () {
       const adUnit = adUnits[0];
       expect(adUnit.mediaTypes.video).to.have.property('video');
       expect(adUnit.mediaTypes.video).to.have.property('content');
+      expect(nextFn.calledOnce).to.be.true;
     });
   });
 
