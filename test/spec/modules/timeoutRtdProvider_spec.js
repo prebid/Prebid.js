2d4bbc17761b7d56f0c617594e0324b76ec2d175
Luigi Sayson <luigi.sayson@openx.com>
2021-09-09 11:50:16 -0700

    increase test coverage

--- test/spec/modules/timeoutRtdProvider_spec.js
+++ test/spec/modules/timeoutRtdProvider_spec.js
@@ -4,6 +4,75 @@ import { expect } from 'chai';
 import * as ajax from 'src/ajax.js';
 import * as prebidGlobal from 'src/prebidGlobal.js';
 
+const DEFAULT_USER_AGENT = window.navigator.userAgent;
+const DEFAULT_CONNECTION = window.navigator.connection;
+
+const PC_USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246';
+const MOBILE_USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1';
+const TABLET_USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148';
+
+function resetUserAgent() {
+   window.navigator.__defineGetter__('userAgent', () => DEFAULT_USER_AGENT);
+};
+
+function setUserAgent(userAgent){
+  window.navigator.__defineGetter__('userAgent', () => userAgent);
+}
+
+function resetConnection(){
+  window.navigator.__defineGetter__('connection', () => DEFAULT_CONNECTION);
+}
+function setConnectionType(connectionType){
+  window.navigator.__defineGetter__('connection', () => { return {'type': connectionType} });
+}
+
+describe('getDeviceType', () => {
+  afterEach(() => {
+    resetUserAgent();
+  });
+
+  [
+    // deviceType, userAgent, deviceTypeNum
+    ['pc', PC_USER_AGENT, 2],
+    ['mobile', MOBILE_USER_AGENT, 4],
+    ['tablet', TABLET_USER_AGENT, 5],
+  ].forEach(function(args){
+    const [deviceType, userAgent, deviceTypeNum] = args;
+    it(`should be able to recognize ${deviceType} devices`, () => {
+      setUserAgent(userAgent);
+      const res = timeoutRtdFunctions.getDeviceType();
+      expect(res).to.equal(deviceTypeNum)
+    })
+  })
+});
+
+describe('getConnectionSpeed', () => {
+  afterEach(() => {
+    resetConnection();
+  });
+  [
+    // connectionType, connectionSpeed
+    ['slow-2g', 'slow'],
+    ['2g', 'slow'],
+    ['3g', 'medium'],
+    ['bluetooth', 'fast'],
+    ['cellular', 'fast'],
+    ['ethernet', 'fast'],
+    ['wifi', 'fast'],
+    ['wimax', 'fast'],
+    ['4g', 'fast'],
+    ['not known', 'unknown'],
+    [undefined, 'unknown'],
+  ].forEach(function(args){
+    const [connectionType, connectionSpeed] = args;
+    it(`should be able to categorize connection speed when the connection type is ${connectionType}`, () => {
+      setConnectionType(connectionType);
+      const res = timeoutRtdFunctions.getConnectionSpeed();
+      expect(res).to.equal(connectionSpeed)
+    })
+  })
+});
+
 describe('Timeout modifier calculations', () => {
   let sandbox;
   beforeEach(() => {
