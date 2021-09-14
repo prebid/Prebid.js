61c92c9bc6ebc279cfa832505e12c126444fbec4
Dragan Bajcic <dragan@bajcic.com>
2021-09-14 06:09:38 +0200

    write prebid tests, and prepare for release

--- modules/cwireBidAdapter.js
+++ modules/cwireBidAdapter.js
@@ -7,10 +7,10 @@ import find from 'core-js-pure/features/array/find.js';
 const utils = require('../src/utils.js');
 // ------------------------------------
 const BIDDER_CODE = 'cwire';
-const ENDPOINT_URL = 'http://localhost:3002/api/prebid';
-const RENDERER_URL = 'http://localhost:3002/static/fif/out.js';
+export const ENDPOINT_URL = 'http://localhost:3002/api/prebid';
+export const RENDERER_URL = 'http://localhost:3002/static/fif/outstreamRender.out.js';
 // ------------------------------------
-const _PAGE_VIEW_ID = utils.generateUUID();
+export const CW_PAGE_VIEW_ID = utils.generateUUID();
 const LS_CWID_KEY = 'cw_cwid';
 
 /**
@@ -19,16 +19,20 @@ const LS_CWID_KEY = 'cw_cwid';
  * @param bid
  * @returns {Array<string>}
  */
-function getSlotSizes(bid) {
+export function getSlotSizes(bid) {
   return utils.parseSizesInput(getAllMediaSizes(bid));
 }
 
-function _renderer(bid) {
-  bid.renderer.push(() => {
-    console.log('SIMPLE INSTALL');
-    console.log(bid);
-  });
-}
+// ------------------------------------
+// Extract GPT div id
+// ------------------------------------
+function autoDetectAdUnitElementIdFromGpt(adUnitCode) {
+  const autoDetectedAdUnit = utils.getGptSlotInfoForAdUnitCode(adUnitCode);
+
+  if (autoDetectedAdUnit && autoDetectedAdUnit.divId) {
+    return autoDetectedAdUnit.divId;
+  }
+};
 
 /**
  * ------------------------------------
@@ -36,7 +40,7 @@ function _renderer(bid) {
  * @param bid
  * @returns {*[]}
  */
-function getAllMediaSizes(bid) {
+export function getAllMediaSizes(bid) {
   // eslint-disable-next-line no-debugger
   let playerSizes = utils.deepAccess(bid, 'mediaTypes.video.playerSize');
   let videoSizes = utils.deepAccess(bid, 'mediaTypes.video.sizes');
@@ -61,9 +65,6 @@ function getAllMediaSizes(bid) {
       sizes.push(s);
     })
   }
-
-  // eslint-disable-next-line no-console
-  console.log('RETURNING SIZES', sizes);
   return sizes;
 }
 
@@ -73,20 +74,22 @@ function getAllMediaSizes(bid) {
  * @param validBidRequests
  * @returns {*[]}
  */
-const mapSlotsData = function(validBidRequests) {
+export const mapSlotsData = function(validBidRequests) {
   const slots = [];
   validBidRequests.forEach(bid => {
     const bidObj = {};
     // get the pacement and page ids
     let placementId = utils.getValue(bid.params, 'placementId');
     let pageId = utils.getValue(bid.params, 'pageId');
+    let adUnitElementId = utils.getValue(bid.params, 'adUnitElementId');
     // get the rest of the auction/bid/transaction info
     bidObj.auctionId = utils.getBidIdParameter('auctionId', bid);
     bidObj.adUnitCode = utils.getBidIdParameter('adUnitCode', bid);
+    bidObj.adUnitElementId = adUnitElementId;
     bidObj.bidId = utils.getBidIdParameter('bidId', bid);
     bidObj.bidderRequestId = utils.getBidIdParameter('bidderRequestId', bid);
-    bidObj.placementId = parseInt(placementId, 10);
-    bidObj.pageId = parseInt(pageId, 10);
+    bidObj.placementId = placementId;
+    bidObj.pageId = pageId;
     bidObj.mediaTypes = utils.getBidIdParameter('mediaTypes', bid);
     bidObj.transactionId = utils.getBidIdParameter('transactionId', bid);
     bidObj.sizes = getSlotSizes(bid);
@@ -98,7 +101,7 @@ const mapSlotsData = function(validBidRequests) {
 
 export const spec = {
   code: BIDDER_CODE,
-  supportedMediaTypes: [VIDEO, BANNER],
+  supportedMediaTypes: [BANNER, VIDEO],
   /**
    * Determines whether or not the given bid request is valid.
    *
@@ -106,8 +109,29 @@ export const spec = {
    * @return boolean True if this is a valid bid, and false otherwise.
    */
   isBidRequestValid: function(bid) {
-    // eslint-disable-next-line no-debugger
-    return !!(bid.params.placementId || (bid.params.member && bid.params.invCode));
+    bid.params = bid.params || {};
+
+    // if ad unit elemt id not provided - use adUnitCode by default
+    if (!bid.params.adUnitElementId) {
+      bid.params.adUnitElementId = bid.code;
+    }
+
+    if (bid.params.useGPTElementId) {
+      bid.params.adUnitElementId = autoDetectAdUnitElementIdFromGpt(bid.adUnitCode);
+    }
+
+    if (!bid.params.placementId || !utils.isStr(bid.params.placementId)) {
+      utils.logError('[CWIRE] placementId not provided or invalid');
+      return false;
+    }
+
+    if (!bid.params.pageId || !utils.isStr(bid.params.pageId)) {
+      utils.logError('[CWIRE] pageId not provided');
+      return false;
+    }
+
+    console.log('VALID REQUEST');
+    return true;
   },
 
   /**
@@ -118,7 +142,7 @@ export const spec = {
    * @param {validBidRequests[]} - an array of bids
    * @return ServerRequest Info describing the request to the server.
    */
-  buildRequests: function(validBidRequests) {
+  buildRequests: function(validBidRequests, bidderRequest) {
     let slots = [];
     let referer;
     try {
@@ -126,19 +150,15 @@ export const spec = {
       slots = mapSlotsData(validBidRequests);
     } catch (e) {
       // eslint-disable-next-line no-console
-      console.warn(e);
+      utils.logWarn(e);
     }
 
     const payload = {
       cwid: localStorage.getItem(LS_CWID_KEY),
       slots: slots,
       httpRef: referer || '',
-      pageViewId: _PAGE_VIEW_ID,
+      pageViewId: CW_PAGE_VIEW_ID,
     };
-    const payloadString = JSON.stringify(payload);
-
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(payload, null, 2));
 
     return {
       method: 'POST',
@@ -159,10 +179,7 @@ export const spec = {
     try {
       if (typeof bidRequest.data === 'string') {
         bidRequest.data = JSON.parse(bidRequest.data);
-        console.log(JSON.stringify(bidRequest));
       }
-      // eslint-disable-next-line no-console
-      console.log(serverResponse);
       const serverBody = serverResponse.body;
       // const headerValue = serverResponse.headers.get('some-response-header');
       serverBody.bids.forEach((br) => {
@@ -183,64 +200,61 @@ export const spec = {
           meta: {
             advertiserDomains: br.adomains ? br.advertiserDomains : [],
           },
-          ad: br.html,
+
         };
 
+        // ------------------------------------
+        // IF BANNER
+        // ------------------------------------
+
+        if (utils.deepAccess(bidReq, 'mediaTypes.banner')) {
+          bidResponse.ad = br.html;
+        }
         // ------------------------------------
         // IF VIDEO
         // ------------------------------------
         if (utils.deepAccess(bidReq, 'mediaTypes.video')) {
           mediaType = VIDEO;
           bidResponse.vastXml = br.vastXml;
-
+          // bidResponse.vastUrl = 'data:text/xml;charset=utf-8;base64,' + btoa(br.vastXml.replace(/\\"/g, '"'));
+          // bidResponse.adResponse =  serverBody;
+          bidResponse.videoScript = br.html;
           const mediaTypeContext = utils.deepAccess(bidReq, 'mediaTypes.video.context');
           if (mediaTypeContext === OUTSTREAM) {
             const r = Renderer.install({
-              id: br.requestId,
-              adUnitCode: br.adUnitCode,
+              id: bidResponse.requestId,
+              adUnitCode: bidReq.adUnitCode,
               url: RENDERER_URL,
+              loaded: false,
+              config: {
+                ...utils.deepAccess(bidReq, 'mediaTypes.video'),
+                ...utils.deepAccess(br, 'outstream', {})
+              }
             });
-            // eslint-disable-next-line no-console
-            console.log('OUTSTREAM RENDERER INSTALL', r);
 
-            br.renderer = r;
-            br.renderer.setRender(_renderer);
+            // set renderer
+            try {
+              bidResponse.renderer = r;
+              bidResponse.renderer.setRender(function(bid) {
+                if (window.CWIRE && window.CWIRE.outstream) {
+                  window.CWIRE.outstream.renderAd(bid);
+                }
+              });
+            } catch (err) {
+              utils.logWarn('Prebid Error calling setRender on newRenderer', err);
+            }
           }
         }
 
-        // bidResponse.mediaType = mediaType;
+        bidResponse.mediaType = mediaType;
 
         bidResponses.push(bidResponse);
       });
     } catch (e) {
-      // eslint-disable-next-line no-console
-      console.warn(e);
+      utils.logWarn(e);
     }
 
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify(bidResponses, null, 2));
-
     return bidResponses;
-  },
-
-  /**
-   * Register bidder specific code, which will execute if bidder timed out after an auction
-   * @param {data} Containing timeout specific data
-   */
-  onTimeout: function(data) {
-    // Bidder specifc code
-    // eslint-disable-next-line no-console
-    console.log('TIMEOUT', data);
-  },
-
-  /**
-   * Register bidder specific code, which will execute if a bid from this bidder won the auction
-   * @param {Bid} The bid that won the auction
-   */
-  onBidWon: function(bid) {
-    // eslint-disable-next-line no-console
-    console.log('BID WON!!!', bid);
-    // Bidder specific code
   },
 
   /**
