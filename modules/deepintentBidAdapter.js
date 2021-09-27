410f3135ab9d0530a80bfd2f0d0cb1c88c73ebb5
Sourabh Gandhe <sourabh@Sourabhs-MacBook-Pro.local>
2021-09-27 14:07:09 +0530

    instream video support added

--- modules/deepintentBidAdapter.js
+++ modules/deepintentBidAdapter.js
@@ -1,10 +1,35 @@
 import {registerBidder} from '../src/adapters/bidderFactory.js';
-import {BANNER} from '../src/mediaTypes.js';
+import {BANNER, VIDEO} from '../src/mediaTypes.js';
 import * as utils from '../src/utils.js';
 const BIDDER_CODE = 'deepintent';
 const BIDDER_ENDPOINT = 'https://prebid.deepintent.com/prebid';
 const USER_SYNC_URL = 'https://cdn.deepintent.com/syncpixel.html';
 const DI_M_V = '1.0.0';
+export const ORTB_VIDEO_PARAMS = {
+  'mimes': (value) => Array.isArray(value) && value.length > 0 && value.every(v => typeof v === 'string'),
+  'minduration': (value) => utils.isInteger(value),
+  'maxduration': (value) => utils.isInteger(value),
+  'protocols': (value) => Array.isArray(value) && value.every(v => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].indexOf(v) !== -1),
+  'w': (value) => utils.isInteger(value),
+  'h': (value) => utils.isInteger(value),
+  'startdelay': (value) => utils.isInteger(value),
+  'placement': (value) => Array.isArray(value) && value.every(v => [1, 2, 3, 4, 5].indexOf(v) !== -1),
+  'linearity': (value) => [1, 2].indexOf(value) !== -1,
+  'skip': (value) => [0, 1].indexOf(value) !== -1,
+  'skipmin': (value) => utils.isInteger(value),
+  'skipafter': (value) => utils.isInteger(value),
+  'sequence': (value) => utils.isInteger(value),
+  'battr': (value) => Array.isArray(value) && value.every(v => Array.from({length: 17}, (_, i) => i + 1).indexOf(v) !== -1),
+  'maxextended': (value) => utils.isInteger(value),
+  'minbitrate': (value) => utils.isInteger(value),
+  'maxbitrate': (value) => utils.isInteger(value),
+  'boxingallowed': (value) => [0, 1].indexOf(value) !== -1,
+  'playbackmethod': (value) => Array.isArray(value) && value.every(v => [1, 2, 3, 4, 5, 6].indexOf(v) !== -1),
+  'playbackend': (value) => [1, 2, 3].indexOf(value) !== -1,
+  'delivery': (value) => [1, 2, 3].indexOf(value) !== -1,
+  'pos': (value) => [0, 1, 2, 3, 4, 5, 6, 7].indexOf(value) !== -1,
+  'api': (value) => Array.isArray(value) && value.every(v => [1, 2, 3, 4, 5, 6].indexOf(v) !== -1)
+};
 export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER],
@@ -18,13 +43,35 @@ export const spec = {
         valid = true;
       }
     }
+    if (bid.hasOwnProperty('mediaTypes') && bid.mediaTypes.hasOwnProperty(VIDEO)) {
+      if (bid.mediaTypes[VIDEO].hasOwnProperty('context')) {
+        valid = true;
+      }
+    }
+
     return valid;
   },
-  interpretResponse: function(bidResponse, request) {
+  interpretResponse: function(bidResponse, bidRequest) {
     let responses = [];
     if (bidResponse && bidResponse.body) {
-      let bids = bidResponse.body.seatbid && bidResponse.body.seatbid[0] ? bidResponse.body.seatbid[0].bid : [];
-      responses = bids.map(bid => formatResponse(bid))
+      try {
+        let bids = bidResponse.body.seatbid && bidResponse.body.seatbid[0] ? bidResponse.body.seatbid[0].bid : [];
+        if (bids) {
+          bids.forEach(bidObj => {
+            let newBid = formatResponse(bidObj);
+            let mediaType = _checkMediaType(bidObj);
+            if (mediaType === BANNER) {
+              newBid.mediaType = BANNER;
+            } else if (mediaType === VIDEO) {
+              newBid.mediaType = VIDEO;
+              newBid.vastXml = bidObj.adm;
+            }
+            responses.push(newBid);
+          });
+        }
+      } catch (err) {
+        utils.logError(err);
+      }
     }
     return responses;
   },
@@ -73,6 +120,18 @@ export const spec = {
   }
 
 };
+function _checkMediaType(bid) {
+  let videoRegex = new RegExp(/VAST\s+version/);
+  let mediaType;
+  if (bid.adm && bid.adm.indexOf('deepintent_wrapper') >= 0) {
+    mediaType = BANNER;
+  } else if (videoRegex.test(bid.adm)) {
+    mediaType = VIDEO;
+  }
+  mediaType = VIDEO;
+  return mediaType;
+}
+
 function clean(obj) {
   for (let propName in obj) {
     if (obj[propName] === null || obj[propName] === undefined) {
@@ -100,16 +159,56 @@ function formatResponse(bid) {
 }
 
 function buildImpression(bid) {
-  return {
+  let impression = {};
+  impression = {
     id: bid.bidId,
     tagid: bid.params.tagId || '',
     secure: window.location.protocol === 'https' ? 1 : 0,
-    banner: buildBanner(bid),
     displaymanager: 'di_prebid',
     displaymanagerver: DI_M_V,
     ext: buildCustomParams(bid)
   };
+  if (utils.deepAccess(bid, 'mediaTypes.banner')) {
+    impression['banner'] = buildBanner(bid);
+  }
+  if (utils.deepAccess(bid, 'mediaTypes.video')) {
+    impression['video'] = _buildVideo(bid);
+  }
+  return impression;
 }
+
+function _buildVideo(bid) {
+  let videoObj = {};
+  const videoAdUnitParams = utils.deepAccess(bid, 'mediaTypes.video', {});
+  const videoBidderParams = utils.deepAccess(bid, 'params.video', {});
+  const computedParams = {};
+
+  if (Array.isArray(videoAdUnitParams.playerSize)) {
+    const tempSize = (Array.isArray(videoAdUnitParams.playerSize[0])) ? videoAdUnitParams.playerSize[0] : videoAdUnitParams.playerSize;
+    computedParams.w = tempSize[0];
+    computedParams.h = tempSize[1];
+  }
+
+  const videoParams = {
+    ...computedParams,
+    ...videoAdUnitParams,
+    ...videoBidderParams
+  };
+
+  Object.keys(ORTB_VIDEO_PARAMS).forEach(paramName => {
+    if (videoParams.hasOwnProperty(paramName)) {
+      if (ORTB_VIDEO_PARAMS[paramName](videoParams[paramName])) {
+        videoObj[paramName] = videoParams[paramName];
+      } else {
+        delete videoObj[paramName];
+        utils.logWarn(`The OpenRTB video param ${paramName} has been skipped due to misformating. Please refer to OpenRTB 2.5 spec.`);
+      }
+    }
+  });
+
+  return videoObj;
+};
+
 function buildCustomParams(bid) {
   if (bid.params && bid.params.custom) {
     return {
