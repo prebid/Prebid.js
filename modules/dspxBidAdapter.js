38c353a04994806c55ad50179a76bb37b94608ea
Harshad Mane <harshad.mane@pubmatic.com>
2021-09-28 11:46:24 -0700

    Multiple Bid/Analytics/ID Adapters: import utils functions as needed and not the whole module (#7483)
    * added support for pubcommon, digitrust, id5id
    
    * added support for IdentityLink
    
    * changed the source for id5
    
    * added unit test cases
    
    * changed source param for identityLink
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module
    
    * Revert "import utils functions as needed and not the whole module"
    
    This reverts commit bc6c9f61f889e9aa2ef8ab207b87d4e7b49e3e57.
    
    * Revert "import utils functions as needed and not the whole module"
    
    This reverts commit ef500abb06648c763caa066ccd18fd5a18f2a1b5.
    
    * Revert "import utils functions as needed and not the whole module"
    
    This reverts commit 7e3fa3feba9ec9b8e81524419c3c13e94ee1049e.
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module

--- modules/dspxBidAdapter.js
+++ modules/dspxBidAdapter.js
@@ -1,4 +1,4 @@
-import * as utils from '../src/utils.js';
+import { deepAccess } from '../src/utils.js';
 import {config} from '../src/config.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
 import { BANNER, VIDEO } from '../src/mediaTypes.js';
@@ -199,7 +199,7 @@ function objectToQueryString(obj, prefix) {
  * @returns {boolean} True if it's a banner bid
  */
 function isBannerRequest(bid) {
-  return bid.mediaType === 'banner' || !!utils.deepAccess(bid, 'mediaTypes.banner') || !isVideoRequest(bid);
+  return bid.mediaType === 'banner' || !!deepAccess(bid, 'mediaTypes.banner') || !isVideoRequest(bid);
 }
 
 /**
@@ -209,7 +209,7 @@ function isBannerRequest(bid) {
  * @returns {boolean} True if it's a video bid
  */
 function isVideoRequest(bid) {
-  return bid.mediaType === 'video' || !!utils.deepAccess(bid, 'mediaTypes.video');
+  return bid.mediaType === 'video' || !!deepAccess(bid, 'mediaTypes.video');
 }
 
 /**
@@ -219,7 +219,7 @@ function isVideoRequest(bid) {
  * @returns {object} True if it's a video bid
  */
 function getVideoSizes(bid) {
-  return parseSizes(utils.deepAccess(bid, 'mediaTypes.video.playerSize') || bid.sizes);
+  return parseSizes(deepAccess(bid, 'mediaTypes.video.playerSize') || bid.sizes);
 }
 
 /**
@@ -229,7 +229,7 @@ function getVideoSizes(bid) {
  * @returns {object} True if it's a video bid
  */
 function getBannerSizes(bid) {
-  return parseSizes(utils.deepAccess(bid, 'mediaTypes.banner.sizes') || bid.sizes);
+  return parseSizes(deepAccess(bid, 'mediaTypes.banner.sizes') || bid.sizes);
 }
 
 /**
