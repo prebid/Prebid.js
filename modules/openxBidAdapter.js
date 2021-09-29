fab00613712caf68d08d88c31d7dd6ac6fea7d58
Harshad Mane <harshad.mane@pubmatic.com>
2021-09-28 17:51:57 -0700

    Multiple Bid/Analytics/ID/ other modules: import utils functions as needed and not the whole module (#7492)
    * added support for pubcommon, digitrust, id5id
    
    * added support for IdentityLink
    
    * changed the source for id5
    
    * added unit test cases
    
    * changed source param for identityLink
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module
    
    * Revert "import utils functions as needed and not the whole module"
    
    This reverts commit bc6c9f61f889e9aa2ef8ab207b87d4e7b49e3e57.
    
    * Revert "import utils functions as needed and not the whole module"
    
    This reverts commit ef500abb06648c763caa066ccd18fd5a18f2a1b5.
    
    * Revert "import utils functions as needed and not the whole module"
    
    This reverts commit 7e3fa3feba9ec9b8e81524419c3c13e94ee1049e.
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module
    
    * import utils functions as needed and not the whole module

--- modules/openxBidAdapter.js
+++ modules/openxBidAdapter.js
@@ -1,6 +1,6 @@
+import { deepAccess, convertTypes, isArray, inIframe, _map, deepSetValue, _each, parseSizesInput, parseUrl } from '../src/utils.js';
 import {config} from '../src/config.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
-import * as utils from '../src/utils.js';
 import {BANNER, VIDEO} from '../src/mediaTypes.js';
 import includes from 'core-js-pure/features/array/includes.js'
 
@@ -51,8 +51,8 @@ export const spec = {
   supportedMediaTypes: SUPPORTED_AD_TYPES,
   isBidRequestValid: function (bidRequest) {
     const hasDelDomainOrPlatform = bidRequest.params.delDomain || bidRequest.params.platform;
-    if (utils.deepAccess(bidRequest, 'mediaTypes.banner') && hasDelDomainOrPlatform) {
-      return !!bidRequest.params.unit || utils.deepAccess(bidRequest, 'mediaTypes.banner.sizes.length') > 0;
+    if (deepAccess(bidRequest, 'mediaTypes.banner') && hasDelDomainOrPlatform) {
+      return !!bidRequest.params.unit || deepAccess(bidRequest, 'mediaTypes.banner.sizes.length') > 0;
     }
 
     return !!(bidRequest.params.unit && hasDelDomainOrPlatform);
@@ -87,8 +87,8 @@ export const spec = {
   getUserSyncs: function (syncOptions, responses, gdprConsent, uspConsent) {
     if (syncOptions.iframeEnabled || syncOptions.pixelEnabled) {
       let pixelType = syncOptions.iframeEnabled ? 'iframe' : 'image';
-      let url = utils.deepAccess(responses, '0.body.ads.pixels') ||
-        utils.deepAccess(responses, '0.body.pixels') ||
+      let url = deepAccess(responses, '0.body.ads.pixels') ||
+        deepAccess(responses, '0.body.pixels') ||
         generateDefaultSyncUrl(gdprConsent, uspConsent);
 
       return [{
@@ -98,7 +98,7 @@ export const spec = {
     }
   },
   transformBidParams: function(params, isOpenRtb) {
-    return utils.convertTypes({
+    return convertTypes({
       'unit': 'string',
       'customFloor': 'number'
     }, params);
@@ -123,7 +123,7 @@ function generateDefaultSyncUrl(gdprConsent, uspConsent) {
 }
 
 function isVideoRequest(bidRequest) {
-  return (utils.deepAccess(bidRequest, 'mediaTypes.video') && !utils.deepAccess(bidRequest, 'mediaTypes.banner')) || bidRequest.mediaType === VIDEO;
+  return (deepAccess(bidRequest, 'mediaTypes.video') && !deepAccess(bidRequest, 'mediaTypes.banner')) || bidRequest.mediaType === VIDEO;
 }
 
 function createBannerBidResponses(oxResponseObj, {bids, startTime}) {
@@ -215,7 +215,7 @@ function getViewportDimensions(isIfr) {
 
 function formatCustomParms(customKey, customParams) {
   let value = customParams[customKey];
-  if (utils.isArray(value)) {
+  if (isArray(value)) {
     // if value is an array, join them with commas first
     value = value.join(',');
   }
@@ -240,7 +240,7 @@ function getMediaTypeFromRequest(serverRequest) {
 }
 
 function buildCommonQueryParamsFromBids(bids, bidderRequest) {
-  const isInIframe = utils.inIframe();
+  const isInIframe = inIframe();
   let defaultParams;
 
   defaultParams = {
@@ -252,7 +252,7 @@ function buildCommonQueryParamsFromBids(bids, bidderRequest) {
     tws: getViewportDimensions(isInIframe),
     be: 1,
     bc: bids[0].params.bc || `${BIDDER_CONFIG}_${BIDDER_VERSION}`,
-    dddid: utils._map(bids, bid => bid.transactionId).join(','),
+    dddid: _map(bids, bid => bid.transactionId).join(','),
     nocache: new Date().getTime()
   };
 
@@ -304,8 +304,8 @@ function buildCommonQueryParamsFromBids(bids, bidderRequest) {
   }
 
   // normalize publisher common id
-  if (utils.deepAccess(bids[0], 'crumbs.pubcid')) {
-    utils.deepSetValue(bids[0], 'userId.pubcid', utils.deepAccess(bids[0], 'crumbs.pubcid'));
+  if (deepAccess(bids[0], 'crumbs.pubcid')) {
+    deepSetValue(bids[0], 'userId.pubcid', deepAccess(bids[0], 'crumbs.pubcid'));
   }
   defaultParams = appendUserIdsToQueryParams(defaultParams, bids[0].userId);
 
@@ -318,7 +318,7 @@ function buildCommonQueryParamsFromBids(bids, bidderRequest) {
 }
 
 function appendUserIdsToQueryParams(queryParams, userIds) {
-  utils._each(userIds, (userIdObjectOrValue, userIdProviderKey) => {
+  _each(userIds, (userIdObjectOrValue, userIdProviderKey) => {
     const key = USER_ID_CODE_TO_QUERY_ARG[userIdProviderKey];
 
     if (USER_ID_CODE_TO_QUERY_ARG.hasOwnProperty(userIdProviderKey)) {
@@ -371,13 +371,13 @@ function buildOXBannerRequest(bids, bidderRequest) {
   let customParamsForAllBids = [];
   let hasCustomParam = false;
   let queryParams = buildCommonQueryParamsFromBids(bids, bidderRequest);
-  let auids = utils._map(bids, bid => bid.params.unit);
+  let auids = _map(bids, bid => bid.params.unit);
 
-  queryParams.aus = utils._map(bids, bid => utils.parseSizesInput(bid.mediaTypes.banner.sizes).join(',')).join('|');
-  queryParams.divids = utils._map(bids, bid => encodeURIComponent(bid.adUnitCode)).join(',');
+  queryParams.aus = _map(bids, bid => parseSizesInput(bid.mediaTypes.banner.sizes).join(',')).join('|');
+  queryParams.divids = _map(bids, bid => encodeURIComponent(bid.adUnitCode)).join(',');
   // gpid
-  queryParams.aucs = utils._map(bids, function (bid) {
-    let gpid = utils.deepAccess(bid, 'ortb2Imp.ext.data.pbadslot');
+  queryParams.aucs = _map(bids, function (bid) {
+    let gpid = deepAccess(bid, 'ortb2Imp.ext.data.pbadslot');
     return encodeURIComponent(gpid || '')
   }).join(',');
 
@@ -395,7 +395,7 @@ function buildOXBannerRequest(bids, bidderRequest) {
 
   bids.forEach(function (bid) {
     if (bid.params.customParams) {
-      let customParamsForBid = utils._map(Object.keys(bid.params.customParams), customKey => formatCustomParms(customKey, bid.params.customParams));
+      let customParamsForBid = _map(Object.keys(bid.params.customParams), customKey => formatCustomParms(customKey, bid.params.customParams));
       let formattedCustomParams = window.btoa(customParamsForBid.join('&'));
       hasCustomParam = true;
       customParamsForAllBids.push(formattedCustomParams);
@@ -435,22 +435,22 @@ function buildOXVideoRequest(bid, bidderRequest) {
 }
 
 function generateVideoParameters(bid, bidderRequest) {
-  const videoMediaType = utils.deepAccess(bid, `mediaTypes.video`);
+  const videoMediaType = deepAccess(bid, `mediaTypes.video`);
   let queryParams = buildCommonQueryParamsFromBids([bid], bidderRequest);
-  let oxVideoConfig = utils.deepAccess(bid, 'params.video') || {};
-  let context = utils.deepAccess(bid, 'mediaTypes.video.context');
-  let playerSize = utils.deepAccess(bid, 'mediaTypes.video.playerSize');
+  let oxVideoConfig = deepAccess(bid, 'params.video') || {};
+  let context = deepAccess(bid, 'mediaTypes.video.context');
+  let playerSize = deepAccess(bid, 'mediaTypes.video.playerSize');
   let width;
   let height;
 
   // normalize config for video size
-  if (utils.isArray(bid.sizes) && bid.sizes.length === 2 && !utils.isArray(bid.sizes[0])) {
+  if (isArray(bid.sizes) && bid.sizes.length === 2 && !isArray(bid.sizes[0])) {
     width = parseInt(bid.sizes[0], 10);
     height = parseInt(bid.sizes[1], 10);
-  } else if (utils.isArray(bid.sizes) && utils.isArray(bid.sizes[0]) && bid.sizes[0].length === 2) {
+  } else if (isArray(bid.sizes) && isArray(bid.sizes[0]) && bid.sizes[0].length === 2) {
     width = parseInt(bid.sizes[0][0], 10);
     height = parseInt(bid.sizes[0][1], 10);
-  } else if (utils.isArray(playerSize) && playerSize.length === 2) {
+  } else if (isArray(playerSize) && playerSize.length === 2) {
     width = parseInt(playerSize[0], 10);
     height = parseInt(playerSize[1], 10);
   }
@@ -463,7 +463,7 @@ function generateVideoParameters(bid, bidderRequest) {
     legacyParams = legacyParams.openrtb;
   }
   // support for video object or full openrtb object
-  if (utils.isArray(legacyParams.imp)) {
+  if (isArray(legacyParams.imp)) {
     legacyParams = legacyParams.imp[0].video;
   }
   Object.keys(legacyParams)
@@ -502,7 +502,7 @@ function generateVideoParameters(bid, bidderRequest) {
     queryParams.vtest = 1;
   }
 
-  let gpid = utils.deepAccess(bid, 'ortb2Imp.ext.data.pbadslot');
+  let gpid = deepAccess(bid, 'ortb2Imp.ext.data.pbadslot');
   if (gpid) {
     queryParams.aucs = encodeURIComponent(gpid)
   }
@@ -517,7 +517,7 @@ function createVideoBidResponses(response, {bid, startTime}) {
   let bidResponses = [];
 
   if (response !== undefined && response.vastUrl !== '' && response.pub_rev > 0) {
-    let vastQueryParams = utils.parseUrl(response.vastUrl).search || {};
+    let vastQueryParams = parseUrl(response.vastUrl).search || {};
     let bidResponse = {};
     bidResponse.requestId = bid.bidId;
     if (response.deal_id) {
