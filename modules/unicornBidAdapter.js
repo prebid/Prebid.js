6995af973154b30fce94bc6354f0fc07e948b6f5
faithnh <faithnh.stepmania.master@gmail.com>
2021-10-06 19:57:48 +0900

    increment version

--- modules/unicornBidAdapter.js
+++ modules/unicornBidAdapter.js
@@ -8,7 +8,7 @@ const BIDDER_CODE = 'unicorn';
 const UNICORN_ENDPOINT = 'https://ds.uncn.jp/pb/0/bid.json';
 const UNICORN_DEFAULT_CURRENCY = 'JPY';
 const UNICORN_PB_COOKIE_KEY = '__pb_unicorn_aud';
-const UNICORN_PB_VERSION = '1.0';
+const UNICORN_PB_VERSION = '1.1';
 
 /**
  * Placement ID and Account ID are required.
