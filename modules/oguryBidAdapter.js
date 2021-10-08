6d8a1465f3fd1b144d93adf795bec04cb847f0f5
Mehdi Bouallagui <45876988+mbouallagui@users.noreply.github.com>
2021-10-08 17:22:19 +0200

    Ogury Bid Adapter: adding onTimeout support (#7535)
    * [SPY-10216] adding onTimeout support
    
    * [SPY-12216] reformulating test name

--- modules/oguryBidAdapter.js
+++ modules/oguryBidAdapter.js
@@ -8,6 +8,7 @@ import { ajax } from '../src/ajax.js'
 const BIDDER_CODE = 'ogury';
 const DEFAULT_TIMEOUT = 1000;
 const BID_HOST = 'https://mweb-hb.presage.io/api/header-bidding-request';
+const TIMEOUT_MONITORING_HOST = 'https://ms-ads-monitoring-events.presage.io';
 const MS_COOKIE_SYNC_DOMAIN = 'https://ms-cookie-sync.presage.io';
 
 function isBidRequestValid(bid) {
@@ -141,6 +142,13 @@ function onBidWon(bid) {
   if (bid && bid.hasOwnProperty('nurl') && bid.nurl.length > 0) ajax(bid['nurl'], null);
 }
 
+function onTimeout(timeoutData) {
+  ajax(`${TIMEOUT_MONITORING_HOST}/bid_timeout`, null, JSON.stringify(timeoutData[0]), {
+    method: 'POST',
+    contentType: 'application/json'
+  });
+}
+
 export const spec = {
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER],
@@ -149,7 +157,8 @@ export const spec = {
   buildRequests,
   interpretResponse,
   getFloor,
-  onBidWon
+  onBidWon,
+  onTimeout
 }
 
 registerBidder(spec);
