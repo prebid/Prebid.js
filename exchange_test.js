b60cbe60f9b44926588f52c1248af5ee1678ff52
Kevin <kevin.hagens@brealtime.com>
2021-10-19 08:49:19 -0500

    test complete

--- exchange_test.js
+++ exchange_test.js
@@ -16,10 +16,43 @@ const { json } = require('body-parser');
         console.log('EMX Header Bidding Request: ' + request.url());
 
         //inspect json body
-        const hbRequest = JSON.stringify(request.postData());
-        console.log(hbRequest);
+        const hbRequest = JSON.parse(request.postData());
+        
+        //do validation logic here....
+        console.log('checking to ensure EMX header bidding request present');
+        if(!hbRequest){
+          throw new Error('no header bidding request found');
+        }
 
-        //do parsing and validation logic here....
+        
+        console.log('checking to ensure request has an id');
+        if(!hbRequest.id){ //check id
+          throw new Error('header bidding request must have an id');
+        }
+
+        console.log('checking to ensure request has an imp object');
+        if(!hbRequest.imp){ //check to make sure containts imp
+          throw new Error('header bidding request must have an imp');
+        }
+
+        console.log('checking to ensure imp object is an array');
+        if(!Array.isArray(hbRequest.imp)){ // make sure that imp object is and array 
+          throw new Error('imp object is not an array');
+        }
+
+        console.log('checking to ensure imp array is not empty');
+        if(hbRequest.imp.length == 0){ //check it has at least one impression object
+          throw new Error('imp array is empty')
+        }
+
+        console.log('checking to ensure request is of type site or app');
+        if(!hbRequest.site && !hbRequest.app){ //check to ensure site object or app object, app for education, always site here
+          throw new Error('header bidding request must have site or app object')
+        }
+
+        console.log(JSON.stringify(hbRequest));
+        
+        return;
 
     }else{
         request.continue();
