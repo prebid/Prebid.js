685d3e05ddb3074c448286d33abdb48419aa7d30
Harshad Mane <harshad.mane@pubmatic.com>
2021-10-19 20:59:31 -0700

    in-dev

--- modules/pubmaticIdSystem.js
+++ modules/pubmaticIdSystem.js
@@ -24,50 +24,54 @@ export const dmdIdSubmodule = {
 	},
 
 	getId(config, consentData, cacheIdObj) {
-	    const configParams = (config && config.params) || {};
+		const configParams = (config && config.params) || {};
 	    
-	    if (
-	      !configParams ||
-	      !configParams.api_key ||
-	      typeof configParams.api_key !== 'string'
-	    ) {
-	      logError('dmd submodule requires an api_key.');
-	      return;
-	    }
+		// if (
+		//   !configParams ||
+		//   !configParams.api_key ||
+		//   typeof configParams.api_key !== 'string'
+		// ) {
+		//   logError('dmd submodule requires an api_key.');
+		//   return;
+		// }
 
 	    // If cahceIdObj is null or undefined - calling AIX-API
 	    if (cacheIdObj) {
 	      return cacheIdObj;
 	    } else {
-	      const url = `https://image6.pubmatic.com/UCookieSetPug?oid=2&`;
-	      // Setting headers	      
-	      const headers = {};
-	      headers['x-api-key'] = configParams.api_key;
-	      headers['x-domain'] = getWindowLocation();
+			const hasGdprData = consentData && typeof consentData.gdprApplies === 'boolean' && consentData.gdprApplies;
+    		const gdprConsentString = hasGdprData ? consentData.consentString : undefined;
+			const url = `https://image6.pubmatic.com/UCookieSetPug?oid=2`;
+			url += '&gdpr=' + (hasGdprData ? 1 : 0);
+      		url += '&gdpr_consent=' + encodeURIComponent(gdprConsentString || '');
+			// Setting headers
+			const headers = {};
+			// headers['x-api-key'] = configParams.api_key;
+			// headers['x-domain'] = getWindowLocation();
 
-	      // Response callbacks
-	      const resp = function (callback) {
-	        const callbacks = {
-	          success: response => {
-	            let responseObj;
-	            let responseId;
-	            try {
-	              responseObj = JSON.parse(response);
-	              if (responseObj && responseObj.id) {
-	                responseId = responseObj.id;
-	              }
-	            } catch (error) {
-	              logError(error);
-	            }
-	            callback(responseId);
-	          },
-	          error: error => {
-	            logError(`${MODULE_NAME}: ID fetch encountered an error`, error);
-	            callback();
-	          }
-	        };
-	        ajax(url, callbacks, undefined, { method: 'GET', withCredentials: true, customHeaders: headers });
-	      };
+			// Response callbacks
+			const resp = function (callback) {
+				const callbacks = {
+					success: response => {
+						let responseObj;
+						let responseId;
+						try {
+							responseObj = JSON.parse(response);
+							if (responseObj && responseObj.id) {
+								responseId = responseObj.id;
+							}
+						} catch (error) {
+							logError(error);
+						}
+						callback(responseId);
+					},
+					error: error => {
+						logError(`${MODULE_NAME}: ID fetch encountered an error`, error);
+						callback();
+					}
+				};
+				ajax(url, callbacks, undefined, { method: 'GET', withCredentials: true, customHeaders: headers });
+			};
 	      return { callback: resp };
 	    }
 	}
