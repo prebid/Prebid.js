82e925aa1d9f7fdf6c2b36b39a6e57655681002c
Karim Mourra <karim@jwplayer.com>
2021-11-01 15:54:28 -0400

    modifies vast xml

--- modules/videoModule/index.js
+++ modules/videoModule/index.js
@@ -4,6 +4,8 @@ import { allVideoEvents } from './constants/events.js';
 import CONSTANTS from '../../src/constants.json';
 import { videoCoreFactory } from './coreVideo.js';
 import { coreAdServerFactory } from './adServer.js';
+import find from 'core-js-pure/features/array/find.js';
+import {buildVastWrapper, getErrorNode, getImpressionNode} from './shared/vastXmlBuilder';
 
 events.addEvents(allVideoEvents);
 
@@ -42,6 +44,63 @@ export function PbVideo(videoCore_, getConfig_, pbGlobal_, pbEvents_, videoEvent
         }
       });
     });
+
+    const cache = getConfig('cache');
+    if (!cache) {
+      return;
+    }
+
+    pbEvents.on(CONSTANTS.EVENTS.BID_ADJUSTMENT, function (bid) {
+      const adUnitCode = bid.adUnitCode;
+      const adUnit = find($$PREBID_GLOBAL$$.adUnits, adUnit => adUnitCode === adUnit.code);
+      const videoConfig = adUnit && adUnit.video;
+      const adServerConfig = videoConfig && videoConfig.adServer;
+      if (!adServerConfig) {
+        return;
+      }
+
+      let { vastXml, vastUrl } = bid;
+      const adId = bid.adId;
+      let impressionUrl;
+      let impressionId;
+      let errorUrl;
+      const trackingConfig = adServerConfig.tracking || {};
+      const impressionTracking = trackingConfig.impression;
+      const errorTracking = trackingConfig.error;
+
+      if (impressionTracking) {
+        impressionUrl = impressionTracking.url;
+        impressionId = impressionTracking.id || adId + '-impression';
+      }
+
+      if (errorTracking) {
+        errorUrl = errorTracking.url;
+      }
+
+      if (vastXml) {
+        vastXml = getVastXmlWithTrackingNodes(vastXml, impressionUrl, impressionId, errorUrl);
+      } else if (vastUrl) {
+        vastXml = buildVastWrapper(adId, vastUrl, impressionUrl, impressionId, errorUrl);
+      }
+
+      bid.vastXml = vastXml;
+    });
+
+
+
+    // must tie adId to bid id and auction id possibly
+    // bid.auctionId .adId .creativeId .requestId
+
+    // it could be that bid.adId should be equal to the VAST XML adId.
+    // asked in pbjs slack and media grid
+
+    // confirmation comes from jwplayer ad impression event.adId
+    // this is not sent for ad error
+    // check how/if this works for vmap
+    // what happens when there are many redirects ? - initial tests were done with ad tag in event, not ad tag loaded to player
+    // works for IMA!
+
+    // then check impression/error event
   }
 
   return { init };
