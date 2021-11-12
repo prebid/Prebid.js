f968488fdb1f0dd083fdc6dfa3f294d86d873eb7
Demetrio Girardi <dgirardi@prebid.org>
2021-11-12 11:48:45 -0800

    Ensure bidder code is passed to stub factory functions

--- src/adapters/bidderDepFactories.js
+++ src/adapters/bidderDepFactories.js
@@ -3,9 +3,17 @@ import {config} from '../config.js';
 
 // these are just stubs for a proof of concept
 export function storageManagerFactory(bidderCode) {
+  ensureBidderCode(bidderCode);
   return getStorageManager;
 }
 
 export function configFactory(bidderCode) {
+  ensureBidderCode(bidderCode);
   return config;
+}
+
+function ensureBidderCode(code) {
+  if (!code) {
+    throw new Error('No bidder code');
+  }
 }
