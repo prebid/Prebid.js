1befe07d86dc12ea45c40216885404897893081c
Kevin <kevin.hagens@brealtime.com>
2021-11-16 14:44:57 -0600

    remove comments from unit test

--- test/spec/modules/emx_digitalBidAdapter_spec.js
+++ test/spec/modules/emx_digitalBidAdapter_spec.js
@@ -433,14 +433,13 @@ describe('emx_digital Adapter', function () {
     });
 
     it('should add gpid to request if present', () => {
-      // inject gpid
       const gpid = '/12345/my-gpt-tag-0';
       let bid = utils.deepClone(bidderRequest.bids[0]);
-      bid.ortb2Imp = { ext: { data: { adserver: { adslot: gpid } } } }; // put gpid in both spots
+      bid.ortb2Imp = { ext: { data: { adserver: { adslot: gpid } } } };
       bid.ortb2Imp = { ext: { data: { pbadslot: gpid } } };
-      let requestWithGPID = spec.buildRequests([bid], bidderRequest); // build bid request
+      let requestWithGPID = spec.buildRequests([bid], bidderRequest);
       requestWithGPID = JSON.parse(requestWithGPID.data);
-      expect(requestWithGPID.imp[0].ext.gpid).to.exist.and.equal(gpid);   // ensure gpid inside bid request
+      expect(requestWithGPID.imp[0].ext.gpid).to.exist.and.equal(gpid);
     });
 
     it('should add UID 2.0 to request', () => {
