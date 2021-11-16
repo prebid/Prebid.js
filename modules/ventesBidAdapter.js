9cacac12f22ff163cd10b1b9dcaffc75fce54a16
Jesso <jesso@ventesavenues.net>
2021-11-16 12:29:26 +0530

    Support Web Placements

--- modules/ventesBidAdapter.js
+++ modules/ventesBidAdapter.js
@@ -1,11 +1,21 @@
-import {BANNER, NATIVE, VIDEO} from '../src/mediaTypes.js';
 import {
-  convertCamelToUnderscore, isStr, isArray, 
-  isNumber, isPlainObject, replaceAuctionPrice
+  BANNER,
+  NATIVE,
+  VIDEO
+} from '../src/mediaTypes.js';
+import {
+  convertCamelToUnderscore,
+  isStr,
+  isArray,
+  isNumber,
+  isPlainObject,
+  replaceAuctionPrice
 } from '../src/utils.js';
 import find from 'core-js-pure/features/array/find.js';
 import includes from 'core-js-pure/features/array/includes.js';
-import { registerBidder } from '../src/adapters/bidderFactory.js';
+import {
+  registerBidder
+} from '../src/adapters/bidderFactory.js';
 
 const BID_METHOD = 'POST';
 const BIDDER_URL = 'http://13.234.201.146:8088/va/ad';
@@ -17,17 +27,21 @@ const DOMAIN_REGEX = new RegExp('//([^/]*)');
 
 function groupBy(values, key) {
   const groups = values.reduce((acc, value) => {
-    const groupId = value[key];
+      const groupId = value[key];
 
-    if (!acc[groupId]) acc[groupId] = [];
-    acc[groupId].push(value);
+      if (!acc[groupId]) acc[groupId] = [];
+      acc[groupId].push(value);
 
-    return acc;
+      return acc;
   }, {});
 
   return Object
-    .keys(groups)
-    .map(id => ({id, key, values: groups[id]}));
+      .keys(groups)
+      .map(id => ({
+          id,
+          key,
+          values: groups[id]
+      }));
 }
 
 function validateMediaTypes(mediaTypes, allowedMediaTypes) {
@@ -35,7 +49,7 @@ function validateMediaTypes(mediaTypes, allowedMediaTypes) {
   if (!allowedMediaTypes.some(mediaType => mediaType in mediaTypes)) return false;
 
   if (isBanner(mediaTypes)) {
-    if (!validateBanner(mediaTypes.banner)) return false;
+      if (!validateBanner(mediaTypes.banner)) return false;
   }
   return true;
 }
@@ -46,15 +60,15 @@ function isBanner(mediaTypes) {
 
 function validateBanner(banner) {
   return isPlainObject(banner) &&
-    isArray(banner.sizes) &&
-    (banner.sizes.length > 0) &&
-    banner.sizes.every(validateMediaSizes);
+      isArray(banner.sizes) &&
+      (banner.sizes.length > 0) &&
+      banner.sizes.every(validateMediaSizes);
 }
 
 function validateMediaSizes(mediaSize) {
   return isArray(mediaSize) &&
-    (mediaSize.length === 2) &&
-    mediaSize.every(size => (isNumber(size) && size >= 0));
+      (mediaSize.length === 2) &&
+      mediaSize.every(size => (isNumber(size) && size >= 0));
 }
 
 function hasUserInfo(bid) {
@@ -63,10 +77,10 @@ function hasUserInfo(bid) {
 
 function validateParameters(parameters, adUnit) {
   if (!(parameters.placementId)) {
-    return false;
+      return false;
   }
   if (!(parameters.publisherId)) {
-    return false;
+      return false;
   }
 
   return true;
@@ -91,30 +105,30 @@ function generateSiteFromAdUnitContext(bidRequests, adUnitContext) {
   if (!domain) return null;
 
   return {
-    page: adUnitContext.refererInfo.referer,
-    domain: domain,
-    name: domain,
-    publisher: {
-      id: publisherId
-    }
+      page: adUnitContext.refererInfo.referer,
+      domain: domain,
+      name: domain,
+      publisher: {
+          id: publisherId
+      }
   };
 }
 
 function validateServerRequest(serverRequest) {
   return isPlainObject(serverRequest) &&
-    isPlainObject(serverRequest.data) &&
-    isArray(serverRequest.data.imp)
+      isPlainObject(serverRequest.data) &&
+      isArray(serverRequest.data.imp)
 }
 
 function createServerRequestFromAdUnits(adUnits, bidRequestId, adUnitContext) {
   return {
-    method: BID_METHOD,
-    url: BIDDER_URL,
-    data: generateBidRequestsFromAdUnits(adUnits, bidRequestId, adUnitContext),
-    options: {
-      contentType: 'application/json',
-      withCredentials: false,
-    }
+      method: BID_METHOD,
+      url: BIDDER_URL,
+      data: generateBidRequestsFromAdUnits(adUnits, bidRequestId, adUnitContext),
+      options: {
+          contentType: 'application/json',
+          withCredentials: false,
+      }
   }
 }
 
@@ -122,42 +136,44 @@ function generateBidRequestsFromAdUnits(bidRequests, bidRequestId, adUnitContext
   const userObjBid = find(bidRequests, hasUserInfo);
   let userObj = {};
   if (userObjBid) {
-    Object.keys(userObjBid.params.user)
-      .filter(param => includes(USER_PARAMS, param))
-      .forEach((param) => {
-        let uparam = convertCamelToUnderscore(param);
-        if (param === 'segments' && isArray(userObjBid.params.user[param])) {
-          let segs = [];
-          userObjBid.params.user[param].forEach(val => {
-            if (isNumber(val)) {
-              segs.push({'id': val});
-            } else if (isPlainObject(val)) {
-              segs.push(val);
-            }
+      Object.keys(userObjBid.params.user)
+          .filter(param => includes(USER_PARAMS, param))
+          .forEach((param) => {
+              let uparam = convertCamelToUnderscore(param);
+              if (param === 'segments' && isArray(userObjBid.params.user[param])) {
+                  let segs = [];
+                  userObjBid.params.user[param].forEach(val => {
+                      if (isNumber(val)) {
+                          segs.push({
+                              'id': val
+                          });
+                      } else if (isPlainObject(val)) {
+                          segs.push(val);
+                      }
+                  });
+                  userObj[uparam] = segs;
+              } else if (param !== 'segments') {
+                  userObj[uparam] = userObjBid.params.user[param];
+              }
           });
-          userObj[uparam] = segs;
-        } else if (param !== 'segments') {
-          userObj[uparam] = userObjBid.params.user[param];
-        }
-      });
   }
 
   const deviceObjBid = find(bidRequests, hasDeviceInfo);
   let deviceObj;
   if (deviceObjBid && deviceObjBid.params && deviceObjBid.params.device) {
-    deviceObj = {};
-    Object.keys(deviceObjBid.params.device)
-      .forEach(param => deviceObj[param] = deviceObjBid.params.device[param]);
-    if (!deviceObjBid.hasOwnProperty('ua')) {
-      deviceObj.ua = navigator.userAgent;
-    }
-    if (!deviceObjBid.hasOwnProperty('language')) {
-      deviceObj.language = navigator.language;
-    }
+      deviceObj = {};
+      Object.keys(deviceObjBid.params.device)
+          .forEach(param => deviceObj[param] = deviceObjBid.params.device[param]);
+      if (!deviceObjBid.hasOwnProperty('ua')) {
+          deviceObj.ua = navigator.userAgent;
+      }
+      if (!deviceObjBid.hasOwnProperty('language')) {
+          deviceObj.language = navigator.language;
+      }
   } else {
-    deviceObj = {};
-    deviceObj.ua = navigator.userAgent;
-    deviceObj.language = navigator.language;
+      deviceObj = {};
+      deviceObj.ua = navigator.userAgent;
+      deviceObj.language = navigator.language;
   }
 
   const payload = {}
@@ -166,16 +182,16 @@ function generateBidRequestsFromAdUnits(bidRequests, bidRequestId, adUnitContext
   payload.cur = ['USD']
   payload.imp = bidRequests.reduce(generateImpressionsFromAdUnit, [])
   const appDeviceObjBid = find(bidRequests, hasAppInfo);
-  if(!appDeviceObjBid){
-    payload.site = generateSiteFromAdUnitContext(bidRequests, adUnitContext)
-  }else{
-    let appIdObj;
-    if (appDeviceObjBid && appDeviceObjBid.params && appDeviceObjBid.params.app && appDeviceObjBid.params.app.id) {
-    appIdObj = {};
-    Object.keys(appDeviceObjBid.params.app)
-    .forEach(param => appIdObj[param] = appDeviceObjBid.params.app[param]);
-    }
-    payload.app = appIdObj;
+  if (!appDeviceObjBid) {
+      payload.site = generateSiteFromAdUnitContext(bidRequests, adUnitContext)
+  } else {
+      let appIdObj;
+      if (appDeviceObjBid && appDeviceObjBid.params && appDeviceObjBid.params.app && appDeviceObjBid.params.app.id) {
+          appIdObj = {};
+          Object.keys(appDeviceObjBid.params.app)
+              .forEach(param => appIdObj[param] = appDeviceObjBid.params.app[param]);
+      }
+      payload.app = appIdObj;
   }
   payload.device = deviceObj;
   payload.user = userObj
@@ -183,46 +199,75 @@ function generateBidRequestsFromAdUnits(bidRequests, bidRequestId, adUnitContext
 }
 
 function generateImpressionsFromAdUnit(acc, adUnit) {
-  const {bidId, mediaTypes, params} = adUnit;
-  const {placementId} = params;
+  const {
+      bidId,
+      mediaTypes,
+      params
+  } = adUnit;
+  const {
+      placementId
+  } = params;
   const pmp = {};
 
-  if (placementId) pmp.deals = [{id: placementId}]
+  if (placementId) pmp.deals = [{
+      id: placementId
+  }]
 
   const imps = Object
-    .keys(mediaTypes)
-    .reduce((acc, mediaType) => {
-      const data = mediaTypes[mediaType];
-      const impId = `${bidId}`;
+      .keys(mediaTypes)
+      .reduce((acc, mediaType) => {
+          const data = mediaTypes[mediaType];
+          const impId = `${bidId}`;
 
-      if (mediaType === 'banner') return acc.concat(generateBannerFromAdUnit(impId, data, params));
-    }, []);
+          if (mediaType === 'banner') return acc.concat(generateBannerFromAdUnit(impId, data, params));
+      }, []);
 
   return acc.concat(imps);
 }
 
 function generateBannerFromAdUnit(impId, data, params) {
-  const {position, placementId} = params;
+  const {
+      position,
+      placementId
+  } = params;
   const pos = position || 0;
   const pmp = {};
-  const ext = {placementId};
+  const ext = {
+      placementId
+  };
 
-  if (placementId) pmp.deals = [{id: placementId}]
+  if (placementId) pmp.deals = [{
+      id: placementId
+  }]
 
-  return data.sizes.map(([w, h]) => ({id: `${impId}`, banner: {format: [{w, h}], w, h, pos}, pmp, ext, tagid: placementId}));
+  return data.sizes.map(([w, h]) => ({
+      id: `${impId}`,
+      banner: {
+          format: [{
+              w,
+              h
+          }],
+          w,
+          h,
+          pos
+      },
+      pmp,
+      ext,
+      tagid: placementId
+  }));
 }
 
 function validateServerResponse(serverResponse) {
   return isPlainObject(serverResponse) &&
-    isPlainObject(serverResponse.body) &&
-    isStr(serverResponse.body.cur) &&
-    isArray(serverResponse.body.seatbid);
+      isPlainObject(serverResponse.body) &&
+      isStr(serverResponse.body.cur) &&
+      isArray(serverResponse.body.seatbid);
 }
 
 function seatBidsToAds(seatBid, bidResponse, serverRequest) {
   return seatBid.bid
-    .filter(bid => validateBids(bid))
-    .map(bid => generateAdFromBid(bid, bidResponse));
+      .filter(bid => validateBids(bid))
+      .map(bid => generateAdFromBid(bid, bidResponse));
 }
 
 function validateBids(bid) {
@@ -234,24 +279,22 @@ function validateBids(bid) {
   if (!isNumber(bid.h)) return false;
   if (!bid.adm) return false;
   if (bid.adm) {
-    if (!isStr(bid.adm)) return false;
+      if (!isStr(bid.adm)) return false;
   }
   return true;
 }
 
-const VAST_REGEXP = /VAST\s+version/;
-
 function getMediaType(adm) {
-  const videoRegex = new RegExp(VAST_REGEXP);
+  const videoRegex = new RegExp(/VAST\s+version/);
 
   if (videoRegex.test(adm)) {
-    return VIDEO;
+      return VIDEO;
   }
 
   const markup = safeJSONparse(adm.replace(/\\/g, ''));
 
   if (markup && isPlainObject(markup.native)) {
-    return NATIVE;
+      return NATIVE;
   }
 
   return BANNER;
@@ -259,68 +302,74 @@ function getMediaType(adm) {
 
 function safeJSONparse(...args) {
   try {
-    return JSON.parse(...args);
+      return JSON.parse(...args);
   } catch (_) {
-    return undefined;
+      return undefined;
   }
 }
 
 function generateAdFromBid(bid, bidResponse) {
   const mediaType = getMediaType(bid.adm);
   const base = {
-    requestId: bid.impid,
-    cpm: bid.price,
-    currency: bidResponse.cur,
-    ttl: TTL,
-    creativeId: bid.crid,
-    mediaType: mediaType,
-    netRevenue: NET_REVENUE
+      requestId: bid.impid,
+      cpm: bid.price,
+      currency: bidResponse.cur,
+      ttl: TTL,
+      creativeId: bid.crid,
+      mediaType: mediaType,
+      netRevenue: NET_REVENUE
   };
 
   if (bid.adomain) {
-    base.meta = { advertiserDomains: bid.adomain };
+      base.meta = {
+          advertiserDomains: bid.adomain
+      };
   }
 
   const size = getSizeFromBid(bid);
   const creative = getCreativeFromBid(bid);
 
   return {
-    ...base,
-    height: size.height,
-    width: size.width,
-    ad: creative.markup,
-    adUrl: creative.markupUrl,
-    // vastXml: isVideo && !isStr(creative.markupUrl) ? creative.markup : null,
-    // vastUrl: isVideo && isStr(creative.markupUrl) ? creative.markupUrl : null,
-    renderer: creative.renderer
+      ...base,
+      height: size.height,
+      width: size.width,
+      ad: creative.markup,
+      adUrl: creative.markupUrl,
+      renderer: creative.renderer
   };
 }
 
 function getSizeFromBid(bid) {
   if (isNumber(bid.w) && isNumber(bid.h)) {
-    return { width: bid.w, height: bid.h };
+      return {
+          width: bid.w,
+          height: bid.h
+      };
   }
-  return { width: null, height: null };
+  return {
+      width: null,
+      height: null
+  };
 }
 
 function getCreativeFromBid(bid) {
   const shouldUseAdMarkup = !!bid.adm;
   const price = bid.price;
   return {
-    markup: shouldUseAdMarkup ? replaceAuctionPrice(bid.adm, price) : null,
-    markupUrl: !shouldUseAdMarkup ? replaceAuctionPrice(bid.nurl, price) : null
+      markup: shouldUseAdMarkup ? replaceAuctionPrice(bid.adm, price) : null,
+      markupUrl: !shouldUseAdMarkup ? replaceAuctionPrice(bid.nurl, price) : null
   };
 }
 
 function hasDeviceInfo(bid) {
   if (bid.params) {
-    return !!bid.params.device
+      return !!bid.params.device
   }
 }
 
 function hasAppInfo(bid) {
   if (bid.params) {
-    return !!bid.params.app
+      return !!bid.params.app
   }
 }
 
@@ -328,41 +377,43 @@ const venavenBidderSpec = {
   code: 'ventes',
   supportedMediaTypes: [BANNER],
   isBidRequestValid(adUnit) {
-    const allowedBidderCodes = [this.code];
+      const allowedBidderCodes = [this.code];
 
-    return isPlainObject(adUnit) &&
-      allowedBidderCodes.indexOf(adUnit.bidder) !== -1 &&
-      isStr(adUnit.adUnitCode) &&
-      isStr(adUnit.bidderRequestId) &&
-      isStr(adUnit.bidId) &&
-      validateMediaTypes(adUnit.mediaTypes, this.supportedMediaTypes) &&
-      validateParameters(adUnit.params, adUnit);
+      return isPlainObject(adUnit) &&
+          allowedBidderCodes.indexOf(adUnit.bidder) !== -1 &&
+          isStr(adUnit.adUnitCode) &&
+          isStr(adUnit.bidderRequestId) &&
+          isStr(adUnit.bidId) &&
+          validateMediaTypes(adUnit.mediaTypes, this.supportedMediaTypes) &&
+          validateParameters(adUnit.params, adUnit);
   },
   buildRequests(bidRequests, bidderRequest) {
-    if (!bidRequests) return null;
+      if (!bidRequests) return null;
 
-    return groupBy(bidRequests, 'bidderRequestId').map(group => {
-      const bidRequestId = group.id;
-      const adUnits = groupBy(group.values, 'bidId').map((group) => {
-        const length = group.values.length;
-        return length > 0 && group.values[length - 1]
+      return groupBy(bidRequests, 'bidderRequestId').map(group => {
+          const bidRequestId = group.id;
+          const adUnits = groupBy(group.values, 'bidId').map((group) => {
+              const length = group.values.length;
+              return length > 0 && group.values[length - 1]
+          });
+
+          return createServerRequestFromAdUnits(adUnits, bidRequestId, bidderRequest)
       });
-
-      return createServerRequestFromAdUnits(adUnits, bidRequestId, bidderRequest)
-    });
   },
   interpretResponse(serverResponse, serverRequest) {
-    if (!validateServerRequest(serverRequest)) return [];
-    if (!validateServerResponse(serverResponse)) return [];
+      if (!validateServerRequest(serverRequest)) return [];
+      if (!validateServerResponse(serverResponse)) return [];
 
-    const bidResponse = serverResponse.body;
+      const bidResponse = serverResponse.body;
 
-    return bidResponse.seatbid
-      .filter(seatBid => isPlainObject(seatBid) && isArray(seatBid.bid))
-      .reduce((acc, seatBid) => acc.concat(seatBidsToAds(seatBid, bidResponse, serverRequest)), []);
+      return bidResponse.seatbid
+          .filter(seatBid => isPlainObject(seatBid) && isArray(seatBid.bid))
+          .reduce((acc, seatBid) => acc.concat(seatBidsToAds(seatBid, bidResponse, serverRequest)), []);
   }
 };
 
 registerBidder(venavenBidderSpec);
 
-export {venavenBidderSpec as spec};
+export {
+  venavenBidderSpec as spec
+};
