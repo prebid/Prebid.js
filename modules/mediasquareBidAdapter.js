61c67cbbf92c64c25d3ed5fd4710dfd90d26e41b
matthieularere-msq <63732822+matthieularere-msq@users.noreply.github.com>
2021-11-30 19:51:44 +0100

    Update mediasquareBidAdapter.js

--- modules/mediasquareBidAdapter.js
+++ modules/mediasquareBidAdapter.js
@@ -7,7 +7,6 @@ const BIDDER_CODE = 'mediasquare';
 const BIDDER_URL_PROD = 'https://pbs-front.mediasquare.fr/'
 const BIDDER_URL_TEST = 'https://bidder-test.mediasquare.fr/'
 const BIDDER_ENDPOINT_AUCTION = 'msq_prebid';
-const BIDDER_ENDPOINT_SYNC = 'cookie_sync';
 const BIDDER_ENDPOINT_WINNING = 'winning';
 
 export const spec = {
@@ -132,8 +131,6 @@ export const spec = {
      * @return {UserSync[]} The user syncs which should be dropped.
      */
   getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {
-    let params = '';
-    let endpoint = document.location.search.match(/msq_test=true/) ? BIDDER_URL_TEST : BIDDER_URL_PROD;
     if (typeof serverResponses === 'object' && serverResponses != null && serverResponses.length > 0 && serverResponses[0].hasOwnProperty('body') &&
         serverResponses[0].body.hasOwnProperty('cookies') && typeof serverResponses[0].body.cookies === 'object') {
       return serverResponses[0].body.cookies;
