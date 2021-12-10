8a79cdd7f3f9f16d6b3bb943f8450a8cf2db8174
Filip Stamenkovic <ficadub@gmail.com>
2021-12-10 13:35:36 +0100

    switch native assets to ortb2 format

--- src/native.js
+++ src/native.js
@@ -1,4 +1,4 @@
-import { deepAccess, getKeyByValue, insertHtmlIntoIframe, logError, triggerPixel } from './utils.js';
+import { deepAccess, getKeyByValue, insertHtmlIntoIframe, isNumber, isPlainObject, logError, triggerPixel } from './utils.js';
 import {includes} from './polyfill.js';
 import {auctionManager} from './auctionManager.js';
 import CONSTANTS from './constants.json';
@@ -10,6 +10,50 @@ export const NATIVE_TARGETING_KEYS = Object.keys(CONSTANTS.NATIVE_KEYS).map(
 );
 
 const IMAGE = {
+  ortb2: {
+    ver: '1.2',
+    assets: [
+      {
+        required: 1,
+        id: 1,
+        img: {
+          type: 3,
+          wmin: 100,
+          hmin: 100,
+        }
+      },
+      {
+        required: 1,
+        id: 2,
+        title: {
+          len: 140,
+        }
+      },
+      {
+        required: 1,
+        id: 3,
+        data: {
+          type: 1,
+        }
+      },
+      {
+        required: 0,
+        id: 4,
+        data: {
+          type: 2,
+        }
+      },
+      {
+        required: 0,
+        id: 5,
+        img: {
+          type: 1,
+          wmin: 20,
+          hmin: 20,
+        }
+      },
+    ],
+  },
   image: { required: true },
   title: { required: true },
   sponsoredBy: { required: true },
@@ -29,9 +73,12 @@ const SUPPORTED_TYPES = {
  */
 export function processNativeAdUnitParams(params) {
   if (params && params.type && typeIsSupported(params.type)) {
-    return SUPPORTED_TYPES[params.type];
+    params = SUPPORTED_TYPES[params.type];
   }
 
+  if (params && params.ortb2 && !isOpenRTBBidRequestValid(params.ortb2)) {
+    return;
+  }
   return params;
 }
 
@@ -43,6 +90,61 @@ export function decorateAdUnitsWithNativeParams(adUnits) {
       adUnit.nativeParams = processNativeAdUnitParams(nativeParams);
     }
   });
+}
+export function isOpenRTBBidRequestValid(ortb2) {
+  const assets = ortb2.assets;
+  if (!Array.isArray(assets) || assets.length === 0) {
+    logError(`assets in mediaTypes.native.ortb2 is not an array, or it's empty. Assets: `, assets);
+    return false;
+  }
+
+  // validate that ids exist, that they are unique and that they are numbers
+  const ids = assets.map(asset => asset.id);
+  if (assets.length !== new Set(ids).size || ids.some(id => id !== parseInt(id, 10))) {
+    logError(`each asset object must have 'id' property, it must be unique and it must be an integer`);
+    return false;
+  }
+
+  if (ortb2.hasOwnProperty('eventtrackers') && !Array.isArray(ortb2.eventtrackers)) {
+    logError('ortb2.eventtrackers is not an array. Eventtrackers: ', ortb2.eventtrackers);
+    return false;
+  }
+
+  return assets.every(asset => isOpenRTBAssetValid(asset))
+}
+
+function isOpenRTBAssetValid(asset) {
+  if (!isPlainObject(asset)) {
+    logError(`asset must be an object. Provided asset: `, asset);
+    return false;
+  }
+  if (asset.img) {
+    if (!isNumber(asset.img.w) && !isNumber(asset.img.wmin)) {
+      logError(`for img asset there must be 'w' or 'wmin' property`);
+      return false;
+    }
+    if (!isNumber(asset.img.h) && !isNumber(asset.img.hmin)) {
+      logError(`for img asset there must be 'h' or 'hmin' property`);
+      return false;
+    }
+  } else if (asset.title) {
+    if (!isNumber(asset.title.len)) {
+      logError(`for title asset there must be 'len' property defined`);
+      return false;
+    }
+  } else if (asset.data) {
+    if (!isNumber(asset.data.type)) {
+      logError(`for data asset 'type' property must be a number`);
+      return false;
+    }
+  } else if (asset.video) {
+    if (!Array.isArray(asset.video.mimes) || !Array.isArray(asset.video.protocols) ||
+      !isNumber(asset.video.minduration) || !isNumber(asset.video.maxduration)) {
+      logError('video asset is not properly configured');
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -79,12 +181,18 @@ export const hasNonNativeBidder = adUnit =>
  * @return {Boolean} If object is valid
  */
 export function nativeBidIsValid(bid, {index = auctionManager.index} = {}) {
+  const bidRequest = index.getAdUnit(bid);
+  if (!bidRequest) { return false; }
+
+  if (deepAccess(bid, 'native.ortb2') && deepAccess(bidRequest, 'nativeParams.ortb2')) {
+    return isNativeOpenRTBBidValid(bid.native.ortb2, bidRequest.nativeParams.ortb2);
+  }
   // all native bid responses must define a landing page url
   if (!deepAccess(bid, 'native.clickUrl')) {
     return false;
   }
 
-  const requestedAssets = index.getAdUnit(bid).nativeParams;
+  const requestedAssets = bidRequest.nativeParams;
   if (!requestedAssets) {
     return true;
   }
@@ -97,6 +205,23 @@ export function nativeBidIsValid(bid, {index = auctionManager.index} = {}) {
   );
 
   return requiredAssets.every(asset => includes(returnedAssets, asset));
+}
+
+export function isNativeOpenRTBBidValid(bidORTB, bidRequestORTB) {
+  if (!deepAccess(bidORTB, 'link.url')) {
+    logError(`native response doesn't have 'link' property. Ortb2 response: `, bidORTB);
+    return false;
+  }
+
+  let requiredAssetIds = bidRequestORTB.assets.filter(asset => asset.required === 1).map(a => a.id);
+  let returnedAssetIds = bidORTB.assets.map(asset => asset.id);
+
+  const match = requiredAssetIds.every(assetId => includes(returnedAssetIds, assetId));
+  if (!match) {
+    logError(`didn't receive a bit with all required assets. Required ids: ${requiredAssetIds}, but received ids in response: ${returnedAssetIds}`);
+  }
+
+  return match;
 }
 
 /*
@@ -229,8 +354,13 @@ export function getAllAssetsMessage(data, adObject) {
   const message = {
     message: 'assetResponse',
     adId: data.adId,
-    assets: []
   };
+
+  if (adObject.native.ortb2) {
+    message.native = adObject.native;
+    return message;
+  }
+  message.assets = [];
 
   Object.keys(adObject.native).forEach(function(key, index) {
     if (key === 'adTemplate' && adObject.native[key]) {
