8ef677b7c145317cb6c4f0479973c79489039121
RAJKUMAR NATARAJAN <raj.cs.psg@gmail.com>
2022-01-10 12:01:00 -0500

    Synacormedia bid adapter: add support for floors module (#7682)
    * issue 6465 add support for floors module
    
    * issue 6465 fix typo
    
    Co-authored-by: rajkumar.natarajan <rajkumar.natarajan@synacor.com>

--- modules/synacormediaBidAdapter.js
+++ modules/synacormediaBidAdapter.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { getAdUnitSizes, logWarn, deepSetValue } from '../src/utils.js';
+import { getAdUnitSizes, logWarn, deepSetValue, isFn, isPlainObject } from '../src/utils.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, VIDEO } from '../src/mediaTypes.js';
 import includes from 'core-js-pure/features/array/includes.js';
@@ -67,11 +67,7 @@ export const spec = {
       } else {
         seatId = bid.params.seatId;
       }
-      const tagIdOrplacementId = bid.params.tagId || bid.params.placementId;
-      const bidFloor = bid.params.bidfloor ? parseFloat(bid.params.bidfloor) : null;
-      if (isNaN(bidFloor)) {
-        logWarn(`Synacormedia: there is an invalid bid floor: ${bid.params.bidfloor}`);
-      }
+      const tagIdOrPlacementId = bid.params.tagId || bid.params.placementId;
       let pos = parseInt(bid.params.pos, 10);
       if (isNaN(pos)) {
         logWarn(`Synacormedia: there is an invalid POS: ${bid.params.pos}`);
@@ -83,9 +79,9 @@ export const spec = {
 
       let imps = [];
       if (videoOrBannerKey === 'banner') {
-        imps = this.buildBannerImpressions(adSizes, bid, tagIdOrplacementId, pos, bidFloor, videoOrBannerKey);
+        imps = this.buildBannerImpressions(adSizes, bid, tagIdOrPlacementId, pos, videoOrBannerKey);
       } else if (videoOrBannerKey === 'video') {
-        imps = this.buildVideoImpressions(adSizes, bid, tagIdOrplacementId, pos, bidFloor, videoOrBannerKey);
+        imps = this.buildVideoImpressions(adSizes, bid, tagIdOrPlacementId, pos, videoOrBannerKey);
       }
       if (imps.length > 0) {
         imps.forEach(i => openRtbBidRequest.imp.push(i));
@@ -128,7 +124,7 @@ export const spec = {
     return eids;
   },
 
-  buildBannerImpressions: function (adSizes, bid, tagIdOrPlacementId, pos, bidFloor, videoOrBannerKey) {
+  buildBannerImpressions: function (adSizes, bid, tagIdOrPlacementId, pos, videoOrBannerKey) {
     let format = [];
     let imps = [];
     adSizes.forEach((size, i) => {
@@ -151,6 +147,10 @@ export const spec = {
         },
         tagid: tagIdOrPlacementId,
       };
+      const bidFloor = getBidFloor(bid, 'banner', '*');
+      if (isNaN(bidFloor)) {
+        logWarn(`Synacormedia: there is an invalid bid floor: ${bid.params.bidfloor}`);
+      }
       if (bidFloor !== null && !isNaN(bidFloor)) {
         imp.bidfloor = bidFloor;
       }
@@ -159,7 +159,7 @@ export const spec = {
     return imps;
   },
 
-  buildVideoImpressions: function(adSizes, bid, tagIdOrPlacementId, pos, bidFloor, videoOrBannerKey) {
+  buildVideoImpressions: function(adSizes, bid, tagIdOrPlacementId, pos, videoOrBannerKey) {
     let imps = [];
     adSizes.forEach((size, i) => {
       if (!size || size.length != 2) {
@@ -171,6 +171,11 @@ export const spec = {
         id: `${videoOrBannerKey.substring(0, 1)}${bid.bidId}-${size0}x${size1}`,
         tagid: tagIdOrPlacementId
       };
+      const bidFloor = getBidFloor(bid, 'video', size);
+      if (isNaN(bidFloor)) {
+        logWarn(`Synacormedia: there is an invalid bid floor: ${bid.params.bidfloor}`);
+      }
+
       if (bidFloor !== null && !isNaN(bidFloor)) {
         imp.bidfloor = bidFloor;
       }
@@ -286,5 +291,21 @@ export const spec = {
     return syncs;
   }
 };
+
+function getBidFloor(bid, mediaType, size) {
+  if (!isFn(bid.getFloor)) {
+    return bid.params.bidfloor ? parseFloat(bid.params.bidfloor) : null;
+  }
+  let floor = bid.getFloor({
+    currency: 'USD',
+    mediaType,
+    size
+  });
+
+  if (isPlainObject(floor) && !isNaN(floor.floor) && floor.currency === 'USD') {
+    return floor.floor;
+  }
+  return null;
+}
 
 registerBidder(spec);
