e5d94179bf7a09923d2bc2b0928f02ed42366ba5
Demetrio Girardi <dgirardi@prebid.org>
2022-01-14 14:09:37 -0800

    Update allowedModules / validateImports

--- allowedModules.js
+++ allowedModules.js
@@ -1,11 +1,20 @@
 
+const sharedWhiteList = [
+  'poly/arrayFrom',
+  'poly/find',
+  'poly/findIndex',
+  'poly/includes',
+];
+
 module.exports = {
   'modules': [
+    ...sharedWhiteList,
     'criteo-direct-rsa-validate',
     'crypto-js',
     'live-connect' // Maintained by LiveIntent : https://github.com/liveintent-berlin/live-connect/
   ],
   'src': [
+    ...sharedWhiteList,
     'fun-hooks/no-eval',
     'just-clone',
     'dlv',
