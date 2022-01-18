515b793c1fcc71c9b334c956af3956ef7085170a
AnirudhRahul <anirudhrahul@anjurahul.com>
2022-01-18 10:55:16 -0500

    Formatting to pass linter

--- modules/videojsVideoProvider.js
+++ modules/videojsVideoProvider.js
@@ -1,14 +1,14 @@
-import {
-  PROTOCOLS, API_FRAMEWORKS, VIDEO_MIME_TYPE, PLAYBACK_METHODS, PLACEMENT, VPAID_MIME_TYPE
-} from './videoModule/constants/ortb.js';
-import {
-  SETUP_COMPLETE, SETUP_FAILED, DESTROYED, AD_REQUEST, AD_BREAK_START, AD_LOADED, AD_STARTED, AD_IMPRESSION, AD_PLAY,
-  AD_TIME, AD_PAUSE, AD_CLICK, AD_SKIPPED, AD_ERROR, AD_COMPLETE, AD_BREAK_END, PLAYLIST, PLAYBACK_REQUEST,
-  AUTOSTART_BLOCKED, PLAY_ATTEMPT_FAILED, CONTENT_LOADED, PLAY, PAUSE, BUFFER, TIME, SEEK_START, SEEK_END, MUTE, VOLUME,
-  RENDITION_UPDATE, ERROR, COMPLETE, PLAYLIST_COMPLETE, FULLSCREEN, PLAYER_RESIZE, VIEWABLE, CAST, PLAYBACK_MODE
-} from './videoModule/constants/events.js';
-import stateFactory from './videoModule/shared/state.js';
-import { adStateFactory, timeStateFactory,  callbackStorageFactory} from './jwplayerVideoProvider.js';
+// import {
+//   PROTOCOLS, API_FRAMEWORKS, VIDEO_MIME_TYPE, PLAYBACK_METHODS, PLACEMENT, VPAID_MIME_TYPE
+// } from './videoModule/constants/ortb.js';
+// import {
+//   SETUP_COMPLETE, SETUP_FAILED, DESTROYED, AD_REQUEST, AD_BREAK_START, AD_LOADED, AD_STARTED, AD_IMPRESSION, AD_PLAY,
+//   AD_TIME, AD_PAUSE, AD_CLICK, AD_SKIPPED, AD_ERROR, AD_COMPLETE, AD_BREAK_END, PLAYLIST, PLAYBACK_REQUEST,
+//   AUTOSTART_BLOCKED, PLAY_ATTEMPT_FAILED, CONTENT_LOADED, PLAY, PAUSE, BUFFER, TIME, SEEK_START, SEEK_END, MUTE, VOLUME,
+//   RENDITION_UPDATE, ERROR, COMPLETE, PLAYLIST_COMPLETE, FULLSCREEN, PLAYER_RESIZE, VIEWABLE, CAST, PLAYBACK_MODE
+// } from './videoModule/constants/events.js';
+// import stateFactory from './videoModule/shared/state.js';
+// import { adStateFactory, timeStateFactory, callbackStorageFactory } from './jwplayerVideoProvider.js';
 import { VIDEO_JS_VENDOR } from './videoModule/constants/vendorCodes.js';
 import { videoVendorDirectory } from './videoModule/vendorDirectory.js';
 
@@ -18,34 +18,29 @@ export function VideojsProvider(config, videojs_, adState_, timeState_, callback
   let playerVersion = null;
   const {playerConfig, divId} = config;
 
-  let adState = adState_;
-  let timeState = timeState_;
-  let callbackStorage = callbackStorage_;
+  //   let adState = adState_;
+  //   let timeState = timeState_;
+  //   let callbackStorage = callbackStorage_;
   // TODO: test with older videojs versions
   let minimumSupportedPlayerVersion = '7.17.0';
 
-
   function init() {
     if (!videojs) {
-      console.log("not founds videojs")
-      triggerSetupFailure(-1); // TODO: come up with code for player absent
+    //   triggerSetupFailure(-1); // TODO: come up with code for player absent
       return;
     }
-    console.log("VideoJS found")
 
     playerVersion = videojs.VERSION;
-    
 
     if (playerVersion < minimumSupportedPlayerVersion) {
-      triggerSetupFailure(-2); // TODO: come up with code for version not supported
+    //   triggerSetupFailure(-2); // TODO: come up with code for version not supported
       return;
     }
 
     // returns the player if it exists, or attempts to instantiate a new one
-    player = videojs(divId, playerConfig, function(){
-        //setup complete
+    player = videojs(divId, playerConfig, function() {
+      // setup complete
     })
-    
   }
 
   function getId() {
@@ -53,28 +48,20 @@ export function VideojsProvider(config, videojs_, adState_, timeState_, callback
   }
 
   function getOrtbParams() {
-    console.log("Requested ortb params")
     // if (!player) {
     //   return;
     // }
     // const config = player.getConfig();
     // const adConfig = config.advertising || {};
-    supportedMediaTypes = supportedMediaTypes || utils.getSupportedMediaTypes(MEDIA_TYPES);
+    // supportedMediaTypes = supportedMediaTypes || utils.getSupportedMediaTypes(MEDIA_TYPES);
 
     const video = {
       mimes: [],
-      w: 0, 
+      w: 0,
       h: 0,
     };
 
-    const content = {
-      id: item.mediaid,
-      url: item.file,
-      title: item.title,
-      cat: item.iabCategories,
-      keywords: item.tags,
-      len: duration,
-    };
+    const content = {};
 
     return {
       video,
@@ -83,7 +70,6 @@ export function VideojsProvider(config, videojs_, adState_, timeState_, callback
   }
 
   function setAdTagUrl(adTagUrl, options) {
-    console.log("Set ad tag url:", adTagUrl)
     // if (!player) {
     //   return;
     // }
@@ -91,16 +77,15 @@ export function VideojsProvider(config, videojs_, adState_, timeState_, callback
   }
 
   function onEvents(events, callback) {
-    console.log("Added callback for", events)
+    // console.log('Added callback for', events)
   }
 
   function offEvents(events, callback) {
-    console.log("Removed callback for", events)
-
+    // console.log('Removed callback for', events)
   }
 
   function destroy() {
-    console.log("Destroying player")
+    // console.log('Destroying player')
     if (!player) {
       return;
     }
@@ -119,13 +104,11 @@ export function VideojsProvider(config, videojs_, adState_, timeState_, callback
   };
 }
 
-
-
 const videojsSubmoduleFactory = function (config) {
-    const adState = adStateFactory();
-    const timeState = timeStateFactory();
-    const callbackStorage = callbackStorageFactory();
-    return VideojsProvider(config, videojs, adState, timeState, callbackStorage, {});
+  const adState = null;
+  const timeState = null;
+  const callbackStorage = null;
+  return VideojsProvider(config, window.videojs, adState, timeState, callbackStorage, {});
 }
 videojsSubmoduleFactory.vendorCode = VIDEO_JS_VENDOR;
 
--- test/spec/modules/videoModule/submodules/videojsVideoProvier_spec.js
+++ test/spec/modules/videoModule/submodules/videojsVideoProvier_spec.js
@@ -6,31 +6,32 @@ import {
 } from 'modules/videojsVideoProvider';
 
 function videojsMock(overrides) {
-    const players = {}
-    const vjs =  function(id){
-        if(!players[id])
-            players[id] = {
-                controls: function() {},
-                getViewable: function () {},
-                getPercentViewable: function () {},
-                getMute: function () {},
-                volume: function () {},
-                getConfig: function () {},
-                videoHeight: function () {},
-                videoWidth: function () {},
-                getFullscreen: function () {},
-                getPlaylistItem: function () {},
-                playAd: function () {},
-                on: function () {},
-                off: function () {},
-                remove: function () {},
-                ...overrides
-            }
-        return players[id]
-    };
-    vjs.players = players
-    vjs.version = '7.17.0';
-    return vjs;
+  const players = {}
+  const vjs = function(id) {
+    if (!players[id]) {
+      players[id] = {
+        controls: function() {},
+        getViewable: function () {},
+        getPercentViewable: function () {},
+        getMute: function () {},
+        volume: function () {},
+        getConfig: function () {},
+        videoHeight: function () {},
+        videoWidth: function () {},
+        getFullscreen: function () {},
+        getPlaylistItem: function () {},
+        playAd: function () {},
+        on: function () {},
+        off: function () {},
+        remove: function () {},
+        ...overrides
+      }
+    }
+    return players[id]
+  };
+  vjs.players = players
+  vjs.version = '7.17.0';
+  return vjs;
 }
 
 describe('videojsProvider', function () {
@@ -43,9 +44,9 @@ describe('videojsProvider', function () {
 
     beforeEach(() => {
       config = {};
-    //   adState = adStateFactory();
-    //   timeState = timeStateFactory();
-    //   callbackStorage = callbackStorageFactory();
+      //   adState = adStateFactory();
+      //   timeState = timeStateFactory();
+      //   callbackStorage = callbackStorageFactory();
       utilsMock = null;
     });
 
@@ -59,17 +60,17 @@ describe('videojsProvider', function () {
     //   expect(payload.errorCode).to.be.equal(-1);
     // });
 
-  //   it('should trigger failure when jwplayer version is under min supported version', function () {
-  //     let jwplayerMock = () => {};
-  //     jwplayerMock.version = '8.20.0';
-  //     const provider = VideojsProvider(config, jwplayerMock, adState, timeState, callbackStorage, utilsMock);
-  //     const setupFailed = sinon.spy();
-  //     provider.onEvents([SETUP_FAILED], setupFailed);
-  //     provider.init();
-  //     expect(setupFailed.calledOnce).to.be.true;
-  //     const payload = setupFailed.args[0][1];
-  //     expect(payload.errorCode).to.be.equal(-2);
-  //   });
+    //   it('should trigger failure when jwplayer version is under min supported version', function () {
+    //     let jwplayerMock = () => {};
+    //     jwplayerMock.version = '8.20.0';
+    //     const provider = VideojsProvider(config, jwplayerMock, adState, timeState, callbackStorage, utilsMock);
+    //     const setupFailed = sinon.spy();
+    //     provider.onEvents([SETUP_FAILED], setupFailed);
+    //     provider.init();
+    //     expect(setupFailed.calledOnce).to.be.true;
+    //     const payload = setupFailed.args[0][1];
+    //     expect(payload.errorCode).to.be.equal(-2);
+    //   });
 
     it('should instantiate the player when uninstantied', function () {
       config.playerConfig = {};
@@ -80,27 +81,26 @@ describe('videojsProvider', function () {
       expect(videojs.players['test-div']).to.be.an('object')
     });
 
-  //   it('should trigger setup complete when player is already instantied', function () {
-  //     const player = getPlayerMock();
-  //     player.getState = () => 'idle';
-  //     const provider = VideojsProvider(config, makePlayerFactoryMock(player), adState, timeState, callbackStorage, utilsMock);
-  //     const setupComplete = sinon.spy();
-  //     provider.onEvents([SETUP_COMPLETE], setupComplete);
-  //     provider.init();
-  //     expect(setupComplete.calledOnce).to.be.true;
-  //   });
+    //   it('should trigger setup complete when player is already instantied', function () {
+    //     const player = getPlayerMock();
+    //     player.getState = () => 'idle';
+    //     const provider = VideojsProvider(config, makePlayerFactoryMock(player), adState, timeState, callbackStorage, utilsMock);
+    //     const setupComplete = sinon.spy();
+    //     provider.onEvents([SETUP_COMPLETE], setupComplete);
+    //     provider.init();
+    //     expect(setupComplete.calledOnce).to.be.true;
+    //   });
 
     it('should not reinstantiate player', function () {
-        config.playerConfig = {};
-        config.divId = 'test-div'
-        const videojs = videojsMock();
-        const player = videojs('test-div')
-        const provider = VideojsProvider(config, videojs, adState, timeState, callbackStorage, utilsMock);
-        provider.init();
-        expect(videojs.players['test-div']).to.be.equal(player)
+      config.playerConfig = {};
+      config.divId = 'test-div'
+      const videojs = videojsMock();
+      const player = videojs('test-div')
+      const provider = VideojsProvider(config, videojs, adState, timeState, callbackStorage, utilsMock);
+      provider.init();
+      expect(videojs.players['test-div']).to.be.equal(player)
     });
-
-});
+  });
 
   describe('getId', function () {
     it('should return configured div id', function () {
