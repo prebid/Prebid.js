ac314e363a5c159684d20c516f61ec4dea70578f
AnirudhRahul <anirudhrahul@anjurahul.com>
2022-01-20 10:43:12 -0500

    Removed comments/redundant tests, clarified some sections of code

--- test/spec/modules/videoModule/submodules/videojsVideoProvier_spec.js
+++ test/spec/modules/videoModule/submodules/videojsVideoProvier_spec.js
@@ -1,8 +1,5 @@
 import {
-  VideojsProvider,
-  adStateFactory,
-  timeStateFactory,
-  callbackStorageFactory
+  VideojsProvider
 } from 'modules/videojsVideoProvider';
 
 function videojsMock(overrides) {
@@ -50,27 +47,13 @@ describe('videojsProvider', function () {
       utilsMock = null;
     });
 
-    // it('should trigger failure when jwplayer is missing', function () {
-    //   const provider = VideojsProvider(config, null, adState, timeState, callbackStorage, utilsMock);
-    //   const setupFailed = sinon.spy();
-    //   provider.onEvents([SETUP_FAILED], setupFailed);
-    //   provider.init();
-    //   expect(setupFailed.calledOnce).to.be.true;
-    //   const payload = setupFailed.args[0][1];
-    //   expect(payload.errorCode).to.be.equal(-1);
-    // });
+    it('should trigger failure when videojs is missing', function () {
+      // TODO: Implement when callbacks are added
+    });
 
-    //   it('should trigger failure when jwplayer version is under min supported version', function () {
-    //     let jwplayerMock = () => {};
-    //     jwplayerMock.version = '8.20.0';
-    //     const provider = VideojsProvider(config, jwplayerMock, adState, timeState, callbackStorage, utilsMock);
-    //     const setupFailed = sinon.spy();
-    //     provider.onEvents([SETUP_FAILED], setupFailed);
-    //     provider.init();
-    //     expect(setupFailed.calledOnce).to.be.true;
-    //     const payload = setupFailed.args[0][1];
-    //     expect(payload.errorCode).to.be.equal(-2);
-    //   });
+    it('should trigger failure when videojs version is under min supported version', function () {
+      // TODO: Implement when callbacks are added
+    });
 
     it('should instantiate the player when uninstantied', function () {
       config.playerConfig = {};
@@ -81,24 +64,8 @@ describe('videojsProvider', function () {
       expect(videojs.players['test-div']).to.be.an('object')
     });
 
-    //   it('should trigger setup complete when player is already instantied', function () {
-    //     const player = getPlayerMock();
-    //     player.getState = () => 'idle';
-    //     const provider = VideojsProvider(config, makePlayerFactoryMock(player), adState, timeState, callbackStorage, utilsMock);
-    //     const setupComplete = sinon.spy();
-    //     provider.onEvents([SETUP_COMPLETE], setupComplete);
-    //     provider.init();
-    //     expect(setupComplete.calledOnce).to.be.true;
-    //   });
-
-    it('should not reinstantiate player', function () {
-      config.playerConfig = {};
-      config.divId = 'test-div'
-      const videojs = videojsMock();
-      const player = videojs('test-div')
-      const provider = VideojsProvider(config, videojs, adState, timeState, callbackStorage, utilsMock);
-      provider.init();
-      expect(videojs.players['test-div']).to.be.equal(player)
+    it('should trigger setup complete when player is already instantied', function () {
+      // TODO: Implement when callbacks are added
     });
   });
 
