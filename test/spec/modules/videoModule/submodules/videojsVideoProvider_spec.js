e46b9848b653964a0d3dabda395318c9bbcf7129
AnirudhRahul <anirudhrahul@anjurahul.com>
2022-01-24 06:35:55 -0500

    Changed video source, finished tests for ortb

--- test/spec/modules/videoModule/submodules/videojsVideoProvider_spec.js
+++ test/spec/modules/videoModule/submodules/videojsVideoProvider_spec.js
@@ -3,6 +3,10 @@ import {
 } from 'modules/videojsVideoProvider';
 import videojs from 'video.js';
 
+import {
+  PROTOCOLS, API_FRAMEWORKS, VIDEO_MIME_TYPE, PLAYBACK_METHODS, PLACEMENT, VPAID_MIME_TYPE
+} from 'modules/videoModule/constants/ortb.js';
+
 describe('videojsProvider', function () {
   describe('init', function () {
     let config;
@@ -64,4 +68,113 @@ describe('videojsProvider', function () {
       expect(provider.getId()).to.be.equal('test_id');
     });
   });
+
+  describe('getOrtbParams', function () {
+    beforeEach(() => {
+      // const test_media_type = VIDEO_MIME_TYPE.MP4;
+      const test_height = 100;
+      const test_width = 200;
+      // const test_start_delay = 5;
+      // const test_placement = PLACEMENT.ARTICLE;
+      const test_battr = 'battr';
+      // const test_playback_method = PLAYBACK_METHODS.CLICK_TO_PLAY;
+      const test_skip = 0;
+
+      const test_duration = 9*60+56;
+      // let test_playback_mode = PLAYBACK_MODE.;
+      if(videojs.getPlayer('test')){
+        videojs.getPlayer('test').dispose()
+      }
+
+      document.body.innerHTML = `
+      <video preload id='test' width="${test_width}" height="${test_height}">
+      <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4">
+      </video>`
+
+    });
+    it('should populate oRTB params', function () {
+      const provider = VideojsProvider({divId:'test'}, videojs, null, null, null, null);
+      provider.init();
+
+      const oRTB = provider.getOrtbParams();
+      expect(oRTB).to.have.property('video');
+      expect(oRTB).to.have.property('content');
+      const { video, content } = oRTB;
+
+      expect(video.mimes).to.include(VIDEO_MIME_TYPE.MP4);
+      expect(video.protocols).to.include.members([
+        PROTOCOLS.VAST_2_0,
+      ]);
+      expect(video.h).to.equal(100);
+      expect(video.w).to.equal(200);
+      expect(video).to.not.have.property('pos');      
+      // Should we check for these if they are hard coded?
+      expect(video.maxextended).to.equal(-1);
+      expect(video.boxingallowed).to.equal(1);
+      expect(video.playbackmethod).to.include(PLAYBACK_METHODS.CLICK_TO_PLAY);
+      expect(video.playbackend).to.equal(1);
+      expect(video.api).to.include.members([API_FRAMEWORKS.VPAID_2_0]);
+
+      expect(content.url).to.be.equal("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+      expect(content).to.not.have.property('len');
+    });
+
+    it('should populate position when fullscreen', function () {
+      const provider = VideojsProvider({divId:'test'}, videojs, null, null, null, null);
+      provider.init();
+      const player = videojs.getPlayer('test')
+      player.isFullscreen = () => true;
+      const { video, content } = provider.getOrtbParams();;
+      expect(video.pos).to.equal(7);
+    });
+
+    it('should populate length when loaded', function (done) {
+      const provider = VideojsProvider({divId:'test'}, videojs, null, null, null, null);
+      provider.init();
+      const player = videojs.getPlayer('test')
+      player.preload = true
+      setTimeout(()=>{
+        const { video, content } = provider.getOrtbParams();
+        expect(content.len).to.equal(9*60+56);
+        done();
+      }, 1000);
+    });
+
+    it('should return the correct playback method for autoplay', function () {
+      const provider = VideojsProvider({divId:'test'}, videojs, null, null, null, null);
+      provider.init();
+      const player = videojs.getPlayer('test')
+      player.autoplay(true)
+      const { video, content } = provider.getOrtbParams();
+      expect(video.playbackmethod).to.include(PLAYBACK_METHODS.AUTOPLAY);
+    });
+
+    it('should return the correct playback method for autoplay muted', function (done) {
+      const provider = VideojsProvider({divId:'test'}, videojs, null, null, null, null);
+      provider.init();
+      const player = videojs.getPlayer('test') 
+      player.muted(true)
+      player.autoplay(true)
+      setTimeout(()=>{
+        const { video, content } = provider.getOrtbParams();
+        expect(video.playbackmethod).to.include(PLAYBACK_METHODS.AUTOPLAY_MUTED);
+        done();
+      }, 100);
+    });
+
+    it('should return the correct playback method for the other autoplay muted', function (done) {
+      const provider = VideojsProvider({divId:'test'}, videojs, null, null, null, null);
+      provider.init();
+      const player = videojs.getPlayer('test') 
+      player.autoplay('muted')
+      setTimeout(()=>{
+        const { video, content } = provider.getOrtbParams();
+        expect(video.playbackmethod).to.include(PLAYBACK_METHODS.AUTOPLAY_MUTED);
+        done();
+      }, 100);
+    });
+
+
+  });
+
 });
