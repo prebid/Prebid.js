b2b41be9da4eab0db1680c6e294d132bdc58bd40
Jeremy Sadwith <jeremy@kargo.com>
2022-02-24 17:43:42 -0500

    Adding support for noBid flag

--- modules/kargoAnalyticsAdapter.js
+++ modules/kargoAnalyticsAdapter.js
@@ -3,44 +3,89 @@ import { ajax } from '../src/ajax.js';
 import adapter from '../src/AnalyticsAdapter.js';
 import adapterManager from '../src/adapterManager.js';
 
-const EVENT_URL = 'https://jeremy.dev.kargo.com/tools/tracker/';
+const EVENT_URL = 'https://jeremy.dev.kargo.com/tools/tracker';
 const KARGO_BIDDER_CODE = 'kargo';
 const CONSTANTS = require('../src/constants.json');
 
-const analyticsType = 'endpoint;'
+const analyticsType = 'endpoint';
+let _bidResponseData = {
+  timeout: 0,
+  auctionId: '',
+  adUnitCode: '',
+  bidId: '',
+  domain: '',
+  isNoBid: 0
+};
 
 var kargoAnalyticsAdapter = Object.assign(
   adapter({ analyticsType }), {
     track({ eventType, args }) {
-      if (typeof args !== 'undefined' && eventType === CONSTANTS.EVENTS.BID_TIMEOUT) {
-        // Only recording timeouts for now
-        _each(args, timeout => {
-          if (timeout.bidder === KARGO_BIDDER_CODE) {
-            const { auctionId, adUnitCode, bidId } = timeout;
-
-            setTimeout(function() {
-              ajax(
-                `${EVENT_URL}`, {
-                // `${EVENT_URL}/timeout`, {
-                  success: function() {},
-                  error: function() {}
-                },
-                JSON.stringify({
-                  auctionId,
-                  adUnitCode,
-                  bidId,
-                  domain: window.location.hostname
-                }), {
-                  method: 'POST'
-                }
-              );
-            }, 3000);
+      console.log(eventType, args);
+      if (typeof args !== 'undefined') {
+        switch(eventType) {
+          case CONSTANTS.EVENTS.AUCTION_INIT: {
+            _bidResponseData.timeout = args.timeout;
+            break;
           }
-        });
+          case CONSTANTS.EVENTS.BID_TIMEOUT: {
+            handleTimeout(args);
+            break;
+          }
+          case CONSTANTS.EVENTS.NO_BID: {
+            handleNoBid(args);
+            break;
+          }
+          case CONSTANTS.EVENTS.BIDDER_DONE: {
+            handleBidderDone(args);
+            break;
+          }
+        }
       }
     }
   }
 );
+
+function handleTimeout (timeouts) {
+  _each(timeouts, timeout => {
+    if (timeout.bidder === KARGO_BIDDER_CODE) {
+      const { auctionId, adUnitCode, bidId } = timeout;
+
+      _bidResponseData = {
+        ..._bidResponseData,
+        auctionId,
+        adUnitCode,
+        bidId,
+        domain: window.location.hostname,
+      };
+    }
+  });
+}
+
+function handleNoBid (nobid) {
+  if (nobid.bidder === KARGO_BIDDER_CODE) {
+    _bidResponseData.isNoBid = 1;
+  }
+}
+
+function handleBidderDone (bidderDone) {
+  if (bidderDone.bidderCode === KARGO_BIDDER_CODE) {
+    sendData('', _bidResponseData);
+  }
+}
+
+function sendData (route, data) {
+  setTimeout(function() {
+    ajax(
+      `${EVENT_URL}/${route}`, {
+        success: function() {},
+        error: function() {}
+      },
+      JSON.stringify(data), {
+        method: 'POST'
+      }
+    );
+  }, 3000);
+}
 
 adapterManager.registerAnalyticsAdapter({
   adapter: kargoAnalyticsAdapter,
