e1f6f9717f0b833ac1579beb3b2b0732fcfd81fa
Jeremy Sadwith <jeremy@kargo.com>
2022-02-24 17:05:34 -0500

    Initial draft of timeout support

--- modules/kargoAnalyticsAdapter.js
+++ modules/kargoAnalyticsAdapter.js
@@ -1,14 +1,50 @@
+import { _each } from '../src/utils.js';
+import { ajax } from '../src/ajax.js';
 import adapter from '../src/AnalyticsAdapter.js';
 import adapterManager from '../src/adapterManager.js';
 
-var kargoAdapter = adapter({
-  analyticsType: 'endpoint',
-  url: 'https://krk.kargo.com/api/v1/event/prebid'
-});
+const EVENT_URL = 'https://jeremy.dev.kargo.com/tools/tracker/';
+const KARGO_BIDDER_CODE = 'kargo';
+const CONSTANTS = require('../src/constants.json');
+
+const analyticsType = 'endpoint;'
+
+var kargoAnalyticsAdapter = Object.assign(
+  adapter({ analyticsType }), {
+    track({ eventType, args }) {
+      if (typeof args !== 'undefined' && eventType === CONSTANTS.EVENTS.BID_TIMEOUT) {
+        // Only recording timeouts for now
+        _each(args, timeout => {
+          if (timeout.bidder === KARGO_BIDDER_CODE) {
+            const { auctionId, adUnitCode, bidId } = timeout;
+
+            setTimeout(function() {
+              ajax(
+                `${EVENT_URL}`, {
+                // `${EVENT_URL}/timeout`, {
+                  success: function() {},
+                  error: function() {}
+                },
+                JSON.stringify({
+                  auctionId,
+                  adUnitCode,
+                  bidId,
+                  domain: window.location.hostname
+                }), {
+                  method: 'POST'
+                }
+              );
+            }, 3000);
+          }
+        });
+      }
+    }
+  }
+);
 
 adapterManager.registerAnalyticsAdapter({
-  adapter: kargoAdapter,
+  adapter: kargoAnalyticsAdapter,
   code: 'kargo'
 });
 
-export default kargoAdapter;
+export default kargoAnalyticsAdapter;
