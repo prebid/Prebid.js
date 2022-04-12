ea2c8f30d7607df589eb8a897b07c24d6369ea55
Patrick McCann <patmmccann@gmail.com>
2022-04-12 16:17:28 -0400

    Update kargoBidAdapter_spec.js

--- test/spec/modules/kargoBidAdapter_spec.js
+++ test/spec/modules/kargoBidAdapter_spec.js
@@ -39,7 +39,7 @@ describe('kargo adapter tests', function () {
     beforeEach(function () {
       $$PREBID_GLOBAL$$.bidderSettings = {
         kargo: {
-          storageAllowed: true 
+          storageAllowed: true
         }
       };
       undefinedCurrency = false;
