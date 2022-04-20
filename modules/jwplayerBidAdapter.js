9d410f8cb50759f4408ff1a5acafa34047075bbd
Jorge Rocha <jrocha@jwplayer.com>
2022-04-20 01:08:32 -0400

    implemented isBidRequestValid with unit tests

--- modules/jwplayerBidAdapter.js
+++ modules/jwplayerBidAdapter.js
@@ -1,15 +1,16 @@
-import * as utils from 'src/utils';
-import { registerBidder } from 'src/adapters/bidderFactory';
-import { config } from 'src/config';
-import { VIDEO } from 'src/mediaTypes';
+// import * as utils from 'src/utils';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+// import { config } from 'src/config';
+import { VIDEO } from '../src/mediaTypes.js';
+
 const BIDDER_CODE = 'jwplayer';
 const GVLID = 1046;
 const SUPPORTED_AD_TYPES = [VIDEO];
 
 export const spec = {
-    code: BIDDER_CODE,
-    gvlid: GVLID,
-    supportedMediaTypes: SUPPORTED_AD_TYPES,
+  code: BIDDER_CODE,
+  gvlid: GVLID,
+  supportedMediaTypes: SUPPORTED_AD_TYPES,
 
   /**
    * Determines whether or not the given bid request is valid.
@@ -17,7 +18,13 @@ export const spec = {
    * @param {object} bid The bid to validate.
    * @return boolean True if this is a valid bid, and false otherwise.
    */
-    isBidRequestValid: function(bid) {},
+  isBidRequestValid: function(bid) {
+    if (!bid || !bid.params) {
+      return false;
+    }
+
+    return !!bid.params.placementId && !!bid.params.pubId;
+  },
 
   /**
    * Make a server request from the list of BidRequests.
@@ -25,7 +32,7 @@ export const spec = {
    * @param {BidRequest[]} bidRequests A non-empty list of bid requests which should be sent to the Server.
    * @return ServerRequest Info describing the request to the server.
    */
-    buildRequests: function(validBidRequests, bidderRequest) {},
+  buildRequests: function(validBidRequests, bidderRequest) {},
 
   /**
    * Unpack the response from the server into a list of bids.
@@ -33,15 +40,14 @@ export const spec = {
    * @param {*} serverResponse A successful response from the server.
    * @return {Bid[]} An array of bids which were nested inside the server.
    */
-    interpretResponse: function(serverResponse, request) {},
-    getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {},
+  interpretResponse: function(serverResponse, request) {},
+  getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {},
 
-
-    // Optional?
-    onTimeout: function(timeoutData) {},
-    onBidWon: function(bid) {},
-    onSetTargeting: function(bid) {},
-    onBidderError: function({ error, bidderRequest }) {}
+  // Optional?
+  // onTimeout: function(timeoutData) {},
+  // onBidWon: function(bid) {},
+  // onSetTargeting: function(bid) {},
+  // onBidderError: function({ error, bidderRequest }) {}
 };
 
 registerBidder(spec);
--- test/spec/modules/jwplayerBidAdapter_spec.js
+++ test/spec/modules/jwplayerBidAdapter_spec.js
@@ -3,23 +3,43 @@ import { spec } from 'modules/jwplayerBidAdapter.js';
 import { config } from 'src/config.js';
 
 describe('jwplayer adapter tests', function() {
-    var sandbox, clock, frozenNow = new Date();
+  var sandbox, clock, frozenNow = new Date();
 
-    beforeEach(function() {
-        sandbox = sinon.sandbox.create();
-        clock = sinon.useFakeTimers(frozenNow.getTime());
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
+    clock = sinon.useFakeTimers(frozenNow.getTime());
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+    clock.restore();
+  });
+
+  describe('isBidRequestValid', function() {
+    it('passes when the bid includes a placement ID and a publisher ID', function() {
+      assert(spec.isBidRequestValid({params: {placementId: 'foo', pubId: 'bar'}}) === true);
     });
 
-    afterEach(function() {
-        sandbox.restore();
-        clock.restore();
+    it('fails when the bid does not include a placement ID', function() {
+      assert(spec.isBidRequestValid({params: {pubId: 'foo'}}) === false);
     });
 
-    describe('isBidRequestValid', function() {});
+    it('fails when the bid does not include a publisher ID', function() {
+      assert(spec.isBidRequestValid({params: {placementId: 'foo'}}) === false);
+    });
 
-    describe('buildRequests for video', function() {});
+    it('fails when bid is falsey', function() {
+      assert(spec.isBidRequestValid() === false);
+    });
 
-    describe('interpretResponse for video', function() {});
+    it('fails when the bid has no params at all', function() {
+      assert(spec.isBidRequestValid({}) === false);
+    });
+  });
 
-    describe('user sync handler', function() {});
+  describe('buildRequests for video', function() {});
+
+  describe('interpretResponse for video', function() {});
+
+  describe('user sync handler', function() {});
 });
