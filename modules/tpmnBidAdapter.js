c7f6f1d4292f4323722ac966c781a1df12af0d8c
changjun <changjun@tpmn.io>
2022-05-02 18:19:29 +0900

    add TPMN UserSync Bidder Adapter(Test Modify) Updating the source code that was forked in the past. make test case more. - add pb7 bidderSettings option - userSync fix.

--- modules/tpmnBidAdapter.js
+++ modules/tpmnBidAdapter.js
@@ -1,13 +1,16 @@
 /* eslint-disable no-tabs */
 import { registerBidder } from '../src/adapters/bidderFactory.js';
-import { parseUrl, deepAccess } from '../src/utils.js';
+import { parseUrl, deepAccess, logWarn, generateUUID } from '../src/utils.js';
+import { getStorageManager } from '../src/storageManager.js';
 import { BANNER } from '../src/mediaTypes.js';
+import { config } from '../src/config.js';
 
 export const ADAPTER_VERSION = '1';
 const SUPPORTED_AD_TYPES = [BANNER];
-
 const BIDDER_CODE = 'tpmn';
 const URL = 'https://ad.tpmn.co.kr/prebidhb.tpmn';
+const IFRAMESYNC = 'https://ad.tpmn.co.kr/sync.tpmn?type=iframe';
+export const storage = getStorageManager({bidderCode: BIDDER_CODE});
 
 export const spec = {
   code: BIDDER_CODE,
@@ -20,18 +23,18 @@ export const spec = {
    */
   isBidRequestValid: function(bid) {
     return 'params' in bid &&
-    'inventoryId' in bid.params &&
-    'publisherId' in bid.params &&
-    !isNaN(Number(bid.params.inventoryId)) &&
-    bid.params.inventoryId > 0 &&
-    (typeof bid.mediaTypes.banner.sizes != 'undefined'); // only accepting appropriate sizes
+      'inventoryId' in bid.params &&
+      'publisherId' in bid.params &&
+      !isNaN(Number(bid.params.inventoryId)) &&
+      bid.params.inventoryId > 0 &&
+      (typeof bid.mediaTypes.banner.sizes != 'undefined'); // only accepting appropriate sizes
   },
 
   /**
-	 * @param {BidRequest[]} bidRequests
-	 * @param {*} bidderRequest
-	 * @return {ServerRequest}
-	 */
+   * @param {BidRequest[]} bidRequests
+   * @param {*} bidderRequest
+   * @return {ServerRequest}
+   */
   buildRequests: (bidRequests, bidderRequest) => {
     if (bidRequests.length === 0) {
       return [];
@@ -49,11 +52,11 @@ export const spec = {
     }];
   },
   /**
-	 * Unpack the response from the server into a list of bids.
-	 *
-	 * @param {serverResponse} serverResponse A successful response from the server.
-	 * @return {Bid[]} An array of bids which were nested inside the server.
-	 */
+   * Unpack the response from the server into a list of bids.
+   *
+   * @param {serverResponse} serverResponse A successful response from the server.
+   * @return {Bid[]} An array of bids which were nested inside the server.
+   */
   interpretResponse: function (serverResponse, serverRequest) {
     if (!Array.isArray(serverResponse.body)) {
       return [];
@@ -63,7 +66,86 @@ export const spec = {
     // our server directly returns the format needed by prebid.js so no more
     // transformation is needed here.
     return bidResults;
-  }
+  },
+
+  getUserSyncs: function (syncOptions, serverResponses, gdprConsent, uspConsent) {
+    const syncArr = [];
+    if (syncOptions.iframeEnabled) {
+      let policyParam = '';
+      if (gdprConsent && gdprConsent.consentString) {
+        if (typeof gdprConsent.gdprApplies === 'boolean') {
+          policyParam += `&gdpr=${Number(gdprConsent.gdprApplies)}&gdpr_consent=${gdprConsent.consentString}`;
+        } else {
+          policyParam += `&gdpr=0&gdpr_consent=${gdprConsent.consentString}`;
+        }
+      }
+      if (uspConsent && uspConsent.consentString) {
+        policyParam += `&ccpa_consent=${uspConsent.consentString}`;
+      }
+      const coppa = config.getConfig('coppa') ? 1 : 0;
+      policyParam += `&coppa=${coppa}`;
+      syncArr.push({
+        type: 'iframe',
+        url: IFRAMESYNC + policyParam
+      })
+    } else {
+      const uuid = getUuid();
+      if (uuid) {
+        const bidswitchId = storage.getCookie('bidswitch');
+        if (!bidswitchId) {
+          syncArr.push({
+            type: 'image',
+            url: 'https://x.bidswitch.net/sync?ssp=tpmn'
+          });
+        }
+        const appierId = storage.getCookie('appier');
+        if (!appierId) {
+          syncArr.push({
+            type: 'image',
+            url: 'https://gocm.c.appier.net/tpmn'
+          });
+        }
+        const adbguId = storage.getCookie('adb_guid');
+        if (!adbguId) {
+          syncArr.push({
+            type: 'image',
+            url: 'https://info.mmnneo.com/getGuidRedirect.info?url=https%3A%2F%2Fad.tpmn.co.kr%2Fcookiesync.tpmn%3Ftpmn_nid%3Dbf91e8b3b9d3f1af3fc1d657f090b4fb%26tpmn_buid%3D'
+          });
+        }
+        const ucfunnelId = storage.getCookie('ucfunnel');
+        if (!ucfunnelId) {
+          syncArr.push({
+            type: 'image',
+            url: 'https://sync.aralego.com/idSync?redirect=https%3A%2F%2Fad.tpmn.co.kr%2FpixelCt.tpmn%3Ftpmn_nid%3Dde91e8b3b9d3f1af3fc1d657f090b815%26tpmn_buid%3DSspCookieUserId'
+          });
+        }
+        const nasmediaId = storage.getCookie('nasmedia');
+        if (!nasmediaId) {
+          syncArr.push({
+            type: 'image',
+            url: `https://idsync.admixer.co.kr:4450/idsync?pid=105&uid=${uuid}`
+          });
+        }
+        const mezzomediaId = storage.getCookie('mezzomedia');
+        if (!mezzomediaId) {
+          syncArr.push({
+            type: 'image',
+            url: `https://cm.meba.kr/cm.mezzo/?buyerid=${uuid}&partnerkey=tpmn`
+          });
+        }
+        const admixernetId = storage.getCookie('admixernet');
+        if (!admixernetId) {
+          syncArr.push({
+            type: 'image',
+            url: `https://inv-nets.admixer.net/adxcm.aspx?ssp=51DC18E4-86E3-42DE-9A82-341B2CB39C2E&id=${uuid}&rurl=https%3A%2F%2Fad.tpmn.co.kr%2FpixelCt.tpmn%3Ftpmn_nid%3Dbf91e8b3b9d3f1af3fc1d657f090b4ec%26tpmn_buid%3D%24%24visitor_cookie%24%24`
+          });
+        }
+      } else {
+        logWarn('Pixel Cookie-Sync Fail. check plz.')
+      }
+    }
+    return syncArr;
+  },
 };
 
 registerBidder(spec);
@@ -116,4 +198,13 @@ function bidToRequest(bid) {
   bidObj.auctionId = bid.auctionId;
 
   return bidObj;
+}
+
+function getUuid() {
+  const UUID_KEY = 'uuid';
+  const id = storage.getCookie(UUID_KEY);
+  if (id) return id;
+  let newId = generateUUID().replace(/-/g, '');
+  storage.setCookie(UUID_KEY, newId);
+  return newId;
 }
--- test/spec/modules/tpmnBidAdapter_spec.js
+++ test/spec/modules/tpmnBidAdapter_spec.js
@@ -1,8 +1,38 @@
 /* eslint-disable no-tabs */
 import { expect } from 'chai';
-import { spec } from 'modules/tpmnBidAdapter.js';
+import { spec, storage } from 'modules/tpmnBidAdapter.js';
+import { generateUUID } from '../../../src/utils.js';
+import { newBidder } from '../../../src/adapters/bidderFactory';
+import * as sinon from 'sinon';
 
 describe('tpmnAdapterTests', function() {
+  const adapter = newBidder(spec);
+  const BIDDER_CODE = 'tpmn';
+  let sandbox = sinon.sandbox.create();
+  let getCookieStub;
+
+  beforeEach(function () {
+    $$PREBID_GLOBAL$$.bidderSettings = {
+      tpmn: {
+        storageAllowed: true
+      }
+    };
+    sandbox = sinon.sandbox.create();
+    getCookieStub = sinon.stub(storage, 'getCookie');
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+    getCookieStub.restore();
+    $$PREBID_GLOBAL$$.bidderSettings = {};
+  });
+
+  describe('inherited functions', function () {
+    it('exists and is a function', function () {
+      expect(adapter.callBids).to.exist.and.to.be.a('function')
+    })
+  });
+
   describe('isBidRequestValid', function() {
     let bid = {
       adUnitCode: 'temp-unitcode',
@@ -20,6 +50,7 @@ describe('tpmnAdapterTests', function() {
         }
       }
     };
+
     it('should return true if a bid is valid banner bid request', function() {
       expect(spec.isBidRequestValid(bid)).to.be.equal(true);
     });
@@ -66,12 +97,12 @@ describe('tpmnAdapterTests', function() {
       };
       const tempBidRequests = [bid];
       const tempBidderRequest = {refererInfo: {
-        referer: 'http://localhost/test',
-        site: {
-          domain: 'localhost',
-          page: 'http://localhost/test'
-        }
-      }};
+          referer: 'http://localhost/test',
+          site: {
+            domain: 'localhost',
+            page: 'http://localhost/test'
+          }
+        }};
       const builtRequest = spec.buildRequests(tempBidRequests, tempBidderRequest);
 
       expect(builtRequest).to.have.lengthOf(1);
@@ -139,6 +170,115 @@ describe('tpmnAdapterTests', function() {
       };
       const bidResponses = spec.interpretResponse(testServerResponse, tempBidRequests);
       expect(bidResponses).deep.equal([mockBidResult]);
+    });
+  });
+
+  describe('getUserSync', function() {
+    const KEY_ID = 'uuid';
+    const TMP_UUID = generateUUID().replace(/-/g, '');
+
+    it('getCookie mock Test', () => {
+      const uuid = storage.getCookie(KEY_ID);
+      expect(uuid).to.equal(undefined);
+    });
+
+    it('getCookie mock Test', () => {
+      expect(TMP_UUID.length).to.equal(32);
+      getCookieStub.withArgs(KEY_ID).returns(TMP_UUID);
+      const uuid = storage.getCookie(KEY_ID);
+      expect(uuid).to.equal(TMP_UUID);
+    });
+
+    it('case 1 -> allow iframe', () => {
+      // eslint-disable-next-line no-console
+      console.log('case 1');
+      const syncs = spec.getUserSyncs({ iframeEnabled: true, pixelEnabled: true });
+      // eslint-disable-next-line no-console
+      console.log(syncs);
+      expect(syncs.length).to.equal(1);
+      expect(syncs[0].type).to.equal('iframe');
+    });
+
+    it('case 2 -> allow pixel with static sync', () => {
+      // eslint-disable-next-line no-console
+      console.log('case 2');
+      const syncs = spec.getUserSyncs({ iframeEnabled: false, pixelEnabled: true });
+      // eslint-disable-next-line no-console
+      console.log(syncs);
+      expect(syncs.length).to.be.equal(7);
+      expect(syncs[0].type).to.be.equal('image');
+      expect(syncs[1].type).to.be.equal('image');
+      expect(syncs[2].type).to.be.equal('image');
+      expect(syncs[3].type).to.be.equal('image');
+    });
+
+    it('case 3 -> allow pixel with uuid static sync', () => {
+      // eslint-disable-next-line no-console
+      console.log('case 3');
+      getCookieStub.withArgs(KEY_ID).returns(TMP_UUID);
+      const uuid = storage.getCookie(KEY_ID);
+      expect(uuid).to.equal(TMP_UUID);
+      const syncs = spec.getUserSyncs({ iframeEnabled: false, pixelEnabled: true });
+      // eslint-disable-next-line no-console
+      console.log(syncs);
+      expect(syncs.length).to.be.equal(7);
+      expect(syncs[0].type).to.be.equal('image');
+    });
+
+    it('case 4 -> allow pixel with uuid static sync', () => {
+      // eslint-disable-next-line no-console
+      console.log('case 4');
+
+      getCookieStub.withArgs(KEY_ID).returns(TMP_UUID);
+      getCookieStub.withArgs('bidswitch').returns('123456789');
+      getCookieStub.withArgs('appier').returns('123456789');
+      getCookieStub.withArgs('adb_guid').returns('123456789');
+      getCookieStub.withArgs('ucfunnel').returns('123456789');
+      getCookieStub.withArgs('nasmedia').returns('123456789');
+      getCookieStub.withArgs('mezzomedia').returns('123456789');
+      getCookieStub.withArgs('admixernet').returns('123456789');
+
+      expect(storage.getCookie(KEY_ID)).to.equal(TMP_UUID);
+      expect(storage.getCookie('bidswitch')).to.equal('123456789');
+      expect(storage.getCookie('appier')).to.equal('123456789');
+      expect(storage.getCookie('adb_guid')).to.equal('123456789');
+      expect(storage.getCookie('ucfunnel')).to.equal('123456789');
+      expect(storage.getCookie('nasmedia')).to.equal('123456789');
+      expect(storage.getCookie('mezzomedia')).to.equal('123456789');
+      expect(storage.getCookie('admixernet')).to.equal('123456789');
+
+      const syncs = spec.getUserSyncs({ iframeEnabled: false, pixelEnabled: true });
+      // eslint-disable-next-line no-console
+      console.log(syncs);
+      expect(syncs.length).to.be.equal(0);
+    });
+
+    it('case 5 -> allow pixel with Only partners who are not sync yet proceed.', () => {
+      // eslint-disable-next-line no-console
+      console.log('case 5');
+
+      getCookieStub.withArgs(KEY_ID).returns(TMP_UUID);
+      // getCookieStub.withArgs('bidswitch').returns('123456789');
+      getCookieStub.withArgs('appier').returns('123456789');
+      getCookieStub.withArgs('adb_guid').returns('123456789');
+      getCookieStub.withArgs('ucfunnel').returns('123456789');
+      // getCookieStub.withArgs('nasmedia').returns('123456789');
+      getCookieStub.withArgs('mezzomedia').returns('123456789');
+      // getCookieStub.withArgs('admixernet').returns('123456789');
+
+      expect(storage.getCookie(KEY_ID)).to.equal(TMP_UUID);
+      expect(storage.getCookie('bidswitch')).to.equal(undefined);
+      expect(storage.getCookie('appier')).to.equal('123456789');
+      expect(storage.getCookie('adb_guid')).to.equal('123456789');
+      expect(storage.getCookie('ucfunnel')).to.equal('123456789');
+      expect(storage.getCookie('nasmedia')).to.equal(undefined);
+      expect(storage.getCookie('mezzomedia')).to.equal('123456789');
+      expect(storage.getCookie('admixernet')).to.equal(undefined);
+
+      const syncs = spec.getUserSyncs({ iframeEnabled: false, pixelEnabled: true });
+      // eslint-disable-next-line no-console
+      console.log(syncs);
+      expect(syncs.length).to.be.equal(3);
     });
   });
 });
