8574f590ff77a8abdece162f692460a3283f4bed
john.buco <john.buco@loblaw.ca>
2022-05-25 17:27:25 -0400

    updates

--- modules/mabidderBidAdapter.js
+++ modules/mabidderBidAdapter.js
@@ -1,57 +1,55 @@
-import { registerBidder } from 'src/adapters/bidderFactory';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { config } from '../src/config.js';
 import { BANNER } from '../src/mediaTypes.js';
 const BIDDER_CODE = 'mabidder';
-const baseUrl =  'https://prometheus-ix.ecdrsvc.com/prometheus/bid';
+export const baseUrl = 'https://prometheus-ix.ecdrsvc.com/prometheus/bid';
 export const spec = {
-    code: BIDDER_CODE,
-    gvlid: none,
-    isBidRequestValid: function(bid) {
-      if (bid.BIDDER_CODE !== BIDDER_CODE || typeof bid.params === 'undefined') {
-        return false;
-      }
-      return (bid.params.accountId && bid.params.placementId && bid.sizes)
-    },
-    buildRequests: function(validBidRequests, bidderRequest) {
-      const referer = bidderRequest.refererInfo.referer || '';
-      const fpd = config.getConfig('ortb2');
-
-      return validBidRequests.map((bidRequest) => {
-        let req = {
-          url: baseUrl,
-          method: 'POST',
-          data: {
-            v: $$PREBID_GLOBAL$$.version,
-            referer: referer,
-            bidId: bidRequest.bidId,
-            auctionId: bidRequest.auctionId,
-            transactionId: bidRequest.transactionId,
-            adUnitCode: bidRequest.adUnitCode,
-            bidRequestCount: bidRequest.bidRequestCount,
-            params: bidRequest.params,
-            sizes: bidRequest.sizes,
-            mediaTypes: bidRequest.mediaTypes,
-            fpd: fpd ? JSON.stringify(fpd) : JSON.stringify({})
-          }
-        };
-        return req;
-      });
-    },
-    interpretResponse: function(serverResponse, request) {
-      const bidResponses = [];
-      if (serverResponse.body) {
-        const body = serverResponse.body;
-        body.forEach((bidResponse) => {
-          if (Array.isArray(bidResponse.advertiserDomains)) {
-            bidResponse.meta = { advertiserDomains: bidResponse.advertiserDomains };
-          } else {
-            bidResponse.meta = { advertiserDomains: [] };
-          }
-          bidResponses.push(bidResponse);
-        })
-      }
-      return bidResponses;
-    },
-    supportedMediaTypes: [BANNER]
+  code: BIDDER_CODE,
+  isBidRequestValid: function(bid) {
+    if (typeof bid.params === 'undefined') {
+      return false;
+    }
+    return !!(bid.params.accountId && bid.params.placementId && bid.sizes)
+  },
+  buildRequests: function(validBidRequests, bidderRequest) {
+    const referer = bidderRequest.refererInfo.referer || '';
+    const fpd = config.getConfig('ortb2');
+    return validBidRequests.map((bidRequest) => {
+      let req = {
+        url: baseUrl,
+        method: 'POST',
+        data: {
+          v: $$PREBID_GLOBAL$$.version,
+          referer: referer,
+          bidId: bidRequest.bidId,
+          auctionId: bidRequest.auctionId,
+          transactionId: bidRequest.transactionId,
+          adUnitCode: bidRequest.adUnitCode,
+          bidRequestCount: bidRequest.bidRequestCount,
+          params: bidRequest.params,
+          sizes: bidRequest.sizes,
+          mediaTypes: bidRequest.mediaTypes,
+          fpd: fpd ? JSON.stringify(fpd) : JSON.stringify({})
+        }
+      };
+      return req;
+    });
+  },
+  interpretResponse: function(serverResponse, request) {
+    const bidResponses = [];
+    if (serverResponse.body) {
+      const body = serverResponse.body;
+      body.forEach((bidResponse) => {
+        if (Array.isArray(bidResponse.advertiserDomains)) {
+          bidResponse.meta = { advertiserDomains: bidResponse.advertiserDomains };
+        } else {
+          bidResponse.meta = { advertiserDomains: [] };
+        }
+        bidResponses.push(bidResponse);
+      })
+    }
+    return bidResponses;
+  },
+  supportedMediaTypes: [BANNER]
 }
 registerBidder(spec);
