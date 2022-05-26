55a10266afb81ea862b111a2f3df2286ccaa44bb
Shivam Marwaha <shivam.marwaha@adpushup.com>
2022-05-26 13:19:19 +0530

    IxBidderAdapter custom changes

--- modules/ixBidAdapter.js
+++ modules/ixBidAdapter.js
@@ -110,6 +110,27 @@ const MEDIA_TYPES = {
   Native: 4
 }
 
+const AP_VALID_SIZES = [
+  [300, 50],
+  [120, 600],
+  [250, 250],
+  [300, 250],
+  [300, 600],
+  [728, 90],
+  [160, 600],
+  [728, 400],
+  [480, 320],
+  [320, 50],
+  [580, 400],
+  [970, 90],
+  [970, 250],
+  [320, 480]
+];
+
+function apIsValidSize (width, height) {
+  return find(AP_VALID_SIZES, ([validWidth, validHeight]) => validWidth === width && validHeight === height);
+}
+
 /**
  * Transform valid bid request config object to banner impression object that will be sent to ad server.
  *
@@ -901,7 +922,7 @@ function removeFromSizes(bannerSizeList, bannerSize) {
  */
 function createVideoImps(validBidRequest, videoImps) {
   const imp = bidToVideoImp(validBidRequest);
-  if (Object.keys(imp).length != 0) {
+  if (Object.keys(imp).length != 0 && apIsValidSize(imp.video.w, imp.video.h)) {
     videoImps[validBidRequest.transactionId] = {};
     videoImps[validBidRequest.transactionId].ixImps = [];
     videoImps[validBidRequest.transactionId].ixImps.push(imp);
@@ -934,7 +955,7 @@ function createBannerImps(validBidRequest, missingBannerSizes, bannerImps) {
   bannerImps[validBidRequest.transactionId].dfp_ad_unit_code = deepAccess(validBidRequest, 'ortb2Imp.ext.data.adserver.adslot');
 
   // Create IX imps from params.size
-  if (bannerSizeDefined) {
+  if (bannerSizeDefined && apIsValidSize(imp.banner.w, imp.banner.h)) {
     if (!bannerImps[validBidRequest.transactionId].hasOwnProperty('ixImps')) {
       bannerImps[validBidRequest.transactionId].ixImps = []
     }
@@ -992,7 +1013,7 @@ function updateMissingSizes(validBidRequest, missingBannerSizes, imp) {
   } else {
     // New Ad Unit
     if (deepAccess(validBidRequest, 'mediaTypes.banner.sizes')) {
-      let sizeList = deepClone(validBidRequest.mediaTypes.banner.sizes);
+      let sizeList = deepClone(validBidRequest.mediaTypes.banner.sizes.filter(size => apIsValidSize(...size)));
       removeFromSizes(sizeList, validBidRequest.params.size);
       let newAdUnitEntry = {
         'missingSizes': sizeList,
