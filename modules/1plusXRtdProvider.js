3cac5cced064346edc6cc34989f6dc30242acab0
Anass Seddiki <bwh.anass@gmail.com>
2022-06-08 14:35:27 +0200

    Empty shell for 1plusX RTD submodule (#1)

---
+++ modules/1plusXRtdProvider.js
@@ -0,0 +1,14 @@
+// Constants 
+const MODULE_NAME = ''
+
+// Functions
+
+// Functions exported in submodule object
+const init = () => { }
+const getBidRequestData = () => { }
+// The RTD submodule object to be exported
+export const onePlusXSubmodule = {
+  name: MODULE_NAME,
+  init,
+  getBidRequestData
+}
