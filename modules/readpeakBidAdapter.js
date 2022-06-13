ff89ca40336a60a5ecfb109844fb03e400937a89
Tuomo Tilli <tuomo.tilli@readpeak.com>
2022-06-13 10:32:10 +0300

    Fix merge

--- modules/readpeakBidAdapter.js
+++ modules/readpeakBidAdapter.js
@@ -1,4 +1,4 @@
-import { logError, replaceAuctionPrice, parseUrl, triggerPixel, isStr } from '../src/utils.js';
+import { logError, replaceAuctionPrice, triggerPixel, isStr } from '../src/utils.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { config } from '../src/config.js';
 import { NATIVE, BANNER } from '../src/mediaTypes.js';
