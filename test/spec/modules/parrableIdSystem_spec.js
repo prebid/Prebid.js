48ad7190decf77cdda00827b38f334856eff4de8
Demetrio Girardi <dgirardi@prebid.org>
2022-06-14 11:26:07 -0700

    Reset ID system at end of parrable tests (#8560)

--- test/spec/modules/parrableIdSystem_spec.js
+++ test/spec/modules/parrableIdSystem_spec.js
@@ -96,6 +96,12 @@ function decodeBase64UrlSafe(encBase64) {
 }
 
 describe('Parrable ID System', function() {
+  after(() => {
+    // reset ID system to avoid delayed callbacks in other tests
+    config.resetConfig();
+    init(config);
+  });
+
   describe('parrableIdSystem.getId()', function() {
     describe('response callback function', function() {
       let logErrorStub;
