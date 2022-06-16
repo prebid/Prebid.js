583c80be0252c3a041c78fd53723a4adb0f6a801
Petre Damoc <pdamoc@gmail.com>
2022-06-16 23:49:13 +0300

    Missena Bid Adapter: added getUserSyncs and testmode (#8547)
    * Missena Bid Adapter: added getUserSyncs and testmode
    
    * Slight parameter rename
    
    * Add getUserSyncs tests
    
    * Hardcode the sync url

--- modules/missenaBidAdapter.js
+++ modules/missenaBidAdapter.js
@@ -45,6 +45,9 @@ export const spec = {
         payload.consent_required = bidderRequest.gdprConsent.gdprApplies;
       }
       const baseUrl = bidRequest.params.baseUrl || ENDPOINT_URL;
+      if (bidRequest.params.test) {
+        payload.test = bidRequest.params.test;
+      }
       return {
         method: 'POST',
         url: baseUrl + '?' + formatQS({ t: bidRequest.params.apiKey }),
@@ -69,7 +72,12 @@ export const spec = {
 
     return bidResponses;
   },
-
+  getUserSyncs: function (syncOptions, serverResponses) {
+    if (!syncOptions.iframeEnabled) {
+      return [];
+    }
+    return [{ type: 'iframe', url: 'https://sync.missena.io/iframe' }];
+  },
   /**
    * Register bidder specific code, which will execute if bidder timed out after an auction
    * @param {data} Containing timeout specific data
