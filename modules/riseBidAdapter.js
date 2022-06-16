effa16da565dc454415161a18edf07221ae681b1
Laslo Chechur <laslo.chechur@ironsrc.com>
2022-06-16 11:19:59 +0300

    change param name to loop

--- modules/riseBidAdapter.js
+++ modules/riseBidAdapter.js
@@ -287,7 +287,7 @@ function generateBidParameters(bid, bidderRequest) {
     floorPrice: Math.max(getFloor(bid, mediaType), params.floorPrice),
     bidId: getBidIdParameter('bidId', bid),
     bidderRequestId: getBidIdParameter('bidderRequestId', bid),
-    bidderRequestsCount: getBidIdParameter('bidderRequestsCount', bid),
+    loop: getBidIdParameter('bidderRequestsCount', bid),
     transactionId: getBidIdParameter('transactionId', bid),
   };
 
