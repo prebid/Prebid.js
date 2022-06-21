9e201a2fa204808068aad2bda1dfe456e835419b
Probably Kira <yulia.puchnina@gmail.com>
2022-06-21 15:23:18 -0400

    Fixed typo in tests

--- test/spec/modules/lifestreetBidAdapter_spec.js
+++ test/spec/modules/lifestreetBidAdapter_spec.js
@@ -42,8 +42,7 @@ describe('lifestreetBidAdapter', function() {
         content: '<iframe src=\'https://ads.lfstmedia.com/displayAd?__ads=ip37346-u0T8r2uJ8245jv6i2QE8Qu&adt=5748927953915945176&seq=2&plc=K73sUSWIH9WwpIQiGybtg8lYdYFld5NvswMmfGBMz6U&__df=true&clickUrl=https%3A%2F%2Fads.lfstmedia.com%2Fclick%2Fcmp15299%2F5748927953915945176%2F2%3F__ads%3Dip37346-u0T8r2uJ8245jv6i2QE8Qu%26adkey%3D6bd%26slot%3Dslot166704%26__stamp%3D1583331395140%26ad%3Dcrv197050%26_cx%3D%24%24CX%24%24%26_cy%3D%24%24CY%24%24%26_celt%3D%24%24ELT-ID%24%24%26redirectURL%3D\' width=\'160\' height=\'600\' frameBorder=\'0\' scrolling=\'no\' framespacing=\'0\' marginheight=\'0\' marginwidth=\'0\'><!--test--><\/iframe>\n',
         creativeId: 7985076,
         height: 600,
-        status: 1,
-        ad
+        status: 1
       }
     };
     videoBidRequests = [
