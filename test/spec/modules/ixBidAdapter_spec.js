e9cea12824266b761563001d65bd47d7b887790f
shahin.rahbariasl <shahin.rahbariasl@indexexchange.com>
2022-06-21 11:45:39 -0400

    fix unit test

--- test/spec/modules/ixBidAdapter_spec.js
+++ test/spec/modules/ixBidAdapter_spec.js
@@ -2200,7 +2200,7 @@ describe('IndexexchangeAdapter', function () {
         expect(diagObj.mfu).to.equal(2);
         expect(diagObj.allu).to.equal(2);
         expect(diagObj.version).to.equal('$prebid.version$');
-        expect(diagObj.url).to.equal('http://localhost:9876/context.html')
+        expect(diagObj.url).to.exist
       });
     });
   });
