9d869f0a9d4e9dd9deb18bb75e470ade3f5a36ef
Demetrio Girardi <dgirardi@prebid.org>
2022-06-22 13:15:11 -0700

    Move AnalyticsAdapter under libraries

--- modules/fintezaAnalyticsAdapter.js
+++ modules/fintezaAnalyticsAdapter.js
@@ -1,6 +1,6 @@
 import { parseUrl, logError } from '../src/utils.js';
 import { ajax } from '../src/ajax.js';
-import adapter from '../src/AnalyticsAdapter.js';
+import adapter from '../libraries/analyticsAdapter/AnalyticsAdapter.js';
 import adapterManager from '../src/adapterManager.js';
 import { getStorageManager } from '../src/storageManager.js';
 import CONSTANTS from '../src/constants.json';
