3b70063bc416ae8e950398e5c191420569d3bde7
Demetrio Girardi <dgirardi@prebid.org>
2022-07-06 11:04:35 -0700

    Price floors module: improve logging on bid rejections to clarify which CPM is being compared with which floor

--- modules/priceFloors.js
+++ modules/priceFloors.js
@@ -751,7 +751,14 @@ export function addBidResponseHook(fn, adUnitCode, bid) {
     flooredBid.status = CONSTANTS.BID_STATUS.BID_REJECTED;
     // if floor not met update bid with 0 cpm so it is not included downstream and marked as no-bid
     flooredBid.cpm = 0;
-    logWarn(`${MODULE_NAME}: ${flooredBid.bidderCode}'s Bid Response for ${adUnitCode} was rejected due to floor not met`, bid);
+    const floorMsg = (() => {
+      try {
+        return ` (adjusted cpm: ${bid.floorData.cpmAfterAdjustments.toFixed(2)}, floor: ${floorInfo.matchingFloor.toFixed(2)})`
+      } catch (e) {
+        return ''
+      }
+    })();
+    logWarn(`${MODULE_NAME}: ${flooredBid.bidderCode}'s Bid Response for ${adUnitCode} was rejected due to floor not met${floorMsg}`, bid);
     return fn.call(this, adUnitCode, flooredBid);
   }
   return fn.call(this, adUnitCode, bid);
