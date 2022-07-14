14e5c2ccf08f0264cb462ca18f17f9960b2d253b
Patrick McCann <patmmccann@gmail.com>
2022-07-14 13:26:06 -0400

    Update kargoBidAdapter.js

--- modules/kargoBidAdapter.js
+++ modules/kargoBidAdapter.js
@@ -148,23 +148,6 @@ export const spec = {
 
   // PRIVATE
   _readCookie(name) {
-    if (!storage.cookiesAreEnabled()) {
-      return null;
-    }
-    let nameEquals = `${name}=`;
-    let cookies = document.cookie.split(';');
-
-    for (let i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i];
-      while (cookie.charAt(0) === ' ') {
-        cookie = cookie.substring(1, cookie.length);
-      }
-
-      if (cookie.indexOf(nameEquals) === 0) {
-        return cookie.substring(nameEquals.length, cookie.length);
-      }
-    }
-
     return null;
   },
 
