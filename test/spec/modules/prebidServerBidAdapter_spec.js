9712872e98c05a6d4d70f2da0952cb4c6513fc71
pm-azhar-mulla <azhar@L1119.local>
2022-07-20 15:57:01 +0530

    Added unit test cases

--- test/spec/modules/prebidServerBidAdapter_spec.js
+++ test/spec/modules/prebidServerBidAdapter_spec.js
@@ -2692,6 +2692,26 @@ describe('S2S Adapter', function () {
       sinon.assert.match(actual.imp[0], sinon.match(ortb2Imp));
     });
 
+    it('setting adapterCode for default bidder', function () {
+      config.setConfig({ CONFIG });
+      adapter.callBids(REQUEST, BID_REQUESTS, addBidResponse, done, ajax);
+      server.requests[0].respond(200, {}, JSON.stringify(RESPONSE_OPENRTB));
+
+      const response = addBidResponse.firstCall.args[1];
+      expect(response).to.have.property('adapterCode', 'appnexus');
+    });
+
+    it('setting adapterCode for alternate bidder', function () {
+      config.setConfig({ CONFIG });
+      RESPONSE_OPENRTB.seatbid[0].bid[0].ext.prebid.meta.adaptercode = 'appnexus2'
+      adapter.callBids(REQUEST, BID_REQUESTS, addBidResponse, done, ajax);
+      server.requests[0].respond(200, {}, JSON.stringify(RESPONSE_OPENRTB));
+
+      const response = addBidResponse.firstCall.args[1];
+      expect(response).to.have.property('adapterCode', 'appnexus2');
+      delete RESPONSE_OPENRTB.seatbid[0].bid[0].ext.prebid.meta.adaptercode;
+    });
+
     describe('on sync requested with no cookie', () => {
       let cfg, req, csRes;
 
