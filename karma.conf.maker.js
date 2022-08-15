effe3dee6671d2aa172fe54a61f85273131061ba
caseywhitmire <60086994+caseywhitmire@users.noreply.github.com>
2022-08-15 04:38:18 -0700

    Update karma.conf.maker.js

--- karma.conf.maker.js
+++ karma.conf.maker.js
@@ -187,7 +187,7 @@ module.exports = function(codeCoverage, browserstack, watchMode, file, disableFe
     concurrency: 6,
 
     plugins: plugins
-  }
+  };
 
   // To ensure that, we are able to run single spec file
   // here we are adding preprocessors, when file is passed
