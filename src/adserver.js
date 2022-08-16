d3b00174885601949b2745e4884042232317972e
caseywhitmire <60086994+caseywhitmire@users.noreply.github.com>
2022-08-16 02:10:07 -0700

    Update adserver.js

--- src/adserver.js
+++ src/adserver.js
@@ -2,16 +2,6 @@ import { formatQS } from './utils.js';
 import { targeting } from './targeting.js';
 import {hook} from './hook.js';
 
-// Adserver parent class
-const AdServer = function(attr) {
-  this.name = attr.adserver;
-  this.code = attr.code;
-  this.getWinningBidByCode = function() {
-    return targeting.getWinningBids(this.code)[0];
-  };
-};
-
-
 /**
  * return the GAM PPID, if available (eid for the userID configured with `userSync.ppidSource`)
  */
