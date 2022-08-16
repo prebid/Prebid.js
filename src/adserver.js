d80b430dc3dca429ee620b1ceeb954e0639b43b3
caseywhitmire <60086994+caseywhitmire@users.noreply.github.com>
2022-08-16 02:04:31 -0700

    adserver.js: remove unused code

--- src/adserver.js
+++ src/adserver.js
@@ -11,50 +11,6 @@ const AdServer = function(attr) {
   };
 };
 
-// DFP ad server
-// TODO: this seems to be unused?
-export function dfpAdserver(options, urlComponents) {
-  var adserver = new AdServer(options);
-  adserver.urlComponents = urlComponents;
-
-  var dfpReqParams = {
-    'env': 'vp',
-    'gdfp_req': '1',
-    'impl': 's',
-    'unviewed_position_start': '1'
-  };
-
-  var dfpParamsWithVariableValue = ['output', 'iu', 'sz', 'url', 'correlator', 'description_url', 'hl'];
-
-  var getCustomParams = function(targeting) {
-    return encodeURIComponent(formatQS(targeting));
-  };
-
-  adserver.appendQueryParams = function() {
-    var bid = adserver.getWinningBidByCode();
-    if (bid) {
-      this.urlComponents.search.description_url = encodeURIComponent(bid.vastUrl);
-      this.urlComponents.search.cust_params = getCustomParams(bid.adserverTargeting);
-      this.urlComponents.search.correlator = Date.now();
-    }
-  };
-
-  adserver.verifyAdserverTag = function() {
-    for (var key in dfpReqParams) {
-      if (!this.urlComponents.search.hasOwnProperty(key) || this.urlComponents.search[key] !== dfpReqParams[key]) {
-        return false;
-      }
-    }
-    for (var i in dfpParamsWithVariableValue) {
-      if (!this.urlComponents.search.hasOwnProperty(dfpParamsWithVariableValue[i])) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  return adserver;
-};
 
 /**
  * return the GAM PPID, if available (eid for the userID configured with `userSync.ppidSource`)
