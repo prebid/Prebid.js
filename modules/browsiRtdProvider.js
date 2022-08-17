a4fdf7d98d7d0b1522d1fa3b1fbf47d2c5230e93
omerBrowsi <54346241+omerBrowsi@users.noreply.github.com>
2022-08-17 20:45:03 +0300

    Browsi RTD Module: add support for page view billable events (#8829)
    * real time data module,
    browsi sub module for real time data,
    new hook bidsBackCallback,
    fix for config unsubscribe
    
    * change timeout&primary ad server only to auctionDelay
    update docs
    
    * support multiple providers
    
    * change promise to callbacks
    configure submodule on submodules.json
    
    * bug fixes
    
    * use Prebid ajax
    
    * tests fix
    
    * browsi real time data provider improvements
    
    * real time data module,
    browsi sub module for real time data,
    new hook bidsBackCallback,
    fix for config unsubscribe
    
    * change timeout&primary ad server only to auctionDelay
    update docs
    
    * support multiple providers
    
    * change promise to callbacks
    configure submodule on submodules.json
    
    * bug fixes
    
    * use Prebid ajax
    
    * tests fix
    
    * browsi real time data provider improvements
    
    * Browsi RTD provider - add billiable event type
    
    * Browsi RTD provider - add billiable event type
    
    * lint fixes

--- modules/browsiRtdProvider.js
+++ modules/browsiRtdProvider.js
@@ -57,6 +57,16 @@ export function addBrowsiTag(data) {
   return script;
 }
 
+export function sendPageviewEvent(eventType) {
+  if (eventType === 'PAGEVIEW') {
+    events.emit(CONSTANTS.EVENTS.BILLABLE_EVENT, {
+      vendor: 'browsi',
+      type: 'pageview',
+      billingId: generateUUID()
+    })
+  }
+}
+
 /**
  * collect required data from page
  * send data to browsi server to get predictions
@@ -262,10 +272,11 @@ function getPredictionsFromServer(url) {
           try {
             const data = JSON.parse(response);
             if (data && data.p && data.kn) {
-              setData({p: data.p, kn: data.kn, pmd: data.pmd});
+              setData({p: data.p, kn: data.kn, pmd: data.pmd, bet: data.bet});
             } else {
               setData({});
             }
+            sendPageviewEvent(data.bet);
             addBrowsiTag(data);
           } catch (err) {
             logError('unable to parse data');
@@ -336,19 +347,22 @@ export const browsiSubmodule = {
 
 function getTargetingData(uc, c, us, a) {
   const targetingData = getRTD(uc);
-  const auctionId = a.auctionId
+  const auctionId = a.auctionId;
+  const sendAdRequestEvent = _browsiData['bet'] === 'AD_REQUEST';
   uc.forEach(auc => {
     if (isNumber(_ic[auc])) {
       _ic[auc] = _ic[auc] + 1;
     }
-    const transactionId = a.adUnits.find(adUnit => adUnit.code === auc).transactionId;
-    events.emit(CONSTANTS.EVENTS.BILLABLE_EVENT, {
-      vendor: 'browsi',
-      type: 'adRequest',
-      billingId: generateUUID(),
-      transactionId: transactionId,
-      auctionId: auctionId
-    })
+    if (sendAdRequestEvent) {
+      const transactionId = a.adUnits.find(adUnit => adUnit.code === auc).transactionId;
+      events.emit(CONSTANTS.EVENTS.BILLABLE_EVENT, {
+        vendor: 'browsi',
+        type: 'adRequest',
+        billingId: generateUUID(),
+        transactionId: transactionId,
+        auctionId: auctionId
+      })
+    }
   });
   logInfo('Browsi RTD provider returned targeting data', targetingData, 'for', uc)
   return targetingData;
