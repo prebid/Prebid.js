b560fd1220158fb7fdd83fcf78709e7491ee868f
Kenan Gillet <kenan.gillet@openx.com>
2022-08-31 11:41:25 -0700

    Update unit test after switching to GPT only

--- test/spec/modules/fledge_spec.js
+++ test/spec/modules/fledge_spec.js
@@ -1,20 +1,15 @@
 import {
   expect
 } from 'chai';
-import * as fledge from 'modules/fledge.js';
-import {
-  config
-} from 'src/config.js';
-import {
-  auctionManager
-} from '../../../src/auctionManager.js';
-import * as utils from 'src/utils.js';
+import { init } from 'gulp-sourcemaps';
+import * as fledge from 'modules/fledgeForGpt.js';
 
 const CODE = 'sampleBidder';
 const AD_UNIT_CODE = 'mock/placement';
 
-describe('fledge module', function() {
+describe('fledgeForGpt module', function() {
   let nextFnSpy;
+  init({enabled: true})
 
   const bidRequest = {
     adUnitCode: AD_UNIT_CODE,
@@ -57,80 +52,6 @@ describe('fledge module', function() {
       fledge.addComponentAuctionHook(nextFnSpy, bidRequest, undefined);
       fledge.addComponentAuctionHook(nextFnSpy, undefined, fledgeAuctionConfig);
       expect(nextFnSpy.called).to.not.be.true;
-    });
-  });
-
-  describe('renderAdHook', function() {
-    const origRunAdAuction = navigator?.runAdAuction;
-    let runAdAuctionThenStub;
-    let findBidByAdIdStub;
-
-    beforeEach(function() {
-      // navigator.runAdAuction doesn't exist, so we can't stub it normally with
-      // sinon.stub(navigator, 'runAdAuction') or something
-      navigator.runAdAuction = sinon.stub();
-      runAdAuctionThenStub = sinon.stub();
-
-      // this is to get runAdAuction to run synchronously
-      navigator.runAdAuction.returns({
-        then: runAdAuctionThenStub,
-      })
-      nextFnSpy = sinon.spy();
-      findBidByAdIdStub = sinon.stub(auctionManager, 'findBidByAdId');
-      findBidByAdIdStub.returns(bidRequest.bids[0])
-    });
-
-    after(function() {
-      navigator.runAdAuction = origRunAdAuction;
-    })
-
-    afterEach(function() {
-      config.resetConfig();
-      findBidByAdIdStub.restore();
-      fledge.clearComponentAuctions();
-    });
-
-    it('should call next() when there is no doc/id', function() {
-      fledge.renderAdHook(nextFnSpy, undefined, undefined);
-      expect(nextFnSpy.called).to.be.true;
-    });
-
-    it('should run fledge auction when there is a valid doc/id and a component auction config has been added', function() {
-      fledge.addComponentAuctionHook(nextFnSpy, bidRequest, fledgeAuctionConfig);
-      fledge.renderAdHook(nextFnSpy, sinon.stub(), sinon.stub());
-      expect(runAdAuctionThenStub.called).to.be.true;
-    });
-
-    it('should not run fledge auction when there are no component auctions added', function() {
-      fledge.renderAdHook(nextFnSpy, sinon.stub(), sinon.stub());
-      expect(runAdAuctionThenStub.called).to.be.false;
-      expect(nextFnSpy.called).to.be.true;
-    });
-  });
-
-  describe('renderFledgeAd', function() {
-    let createInvisibleIframeStub;
-    let insertElementStub;
-
-    beforeEach(function() {
-      createInvisibleIframeStub = sinon.stub(utils, 'createInvisibleIframe');
-      insertElementStub = sinon.stub(utils, 'insertElement');
-    });
-
-    afterEach(function() {
-      createInvisibleIframeStub.restore();
-      insertElementStub.restore();
-    });
-
-    it('it should set the fledge ad uri in the iframe', function() {
-      const iframeStub = sinon.stub();
-      iframeStub.style = {};
-      createInvisibleIframeStub.returns(iframeStub);
-      const fledgeAdUri = 'testuri';
-
-      fledge.renderFledgeAd(bidRequest.bids[0], sinon.stub(), fledgeAdUri);
-      expect(insertElementStub.called).to.equal(true);
-      expect(iframeStub.src).to.equal(fledgeAdUri);
     });
   });
 });
