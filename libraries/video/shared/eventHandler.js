86834d5dfe0abf8fcc11cb1cc0ae0c4319f9eeaf
Karim Mourra <karim@jwplayer.com>
2022-09-12 16:50:12 -0400

    adds fn desc

--- libraries/video/shared/eventHandler.js
+++ libraries/video/shared/eventHandler.js
@@ -1,3 +1,11 @@
+/**
+ * Builds a standard event handler
+ * @param {String} type Event name
+ * @param {(function(String, Object): Object)} callback Callback defined by publisher to be executed when the event occurs
+ * @param {Object} payload Base payload defined when the event is registered
+ * @param {(function(*): Object)|null|undefined} getExtraPayload Parses the player's event payload to return a normalized payload
+ * @returns {(function(*): void)|*} event handler
+ */
 export function getEventHandler(type, callback, payload, getExtraPayload) {
   return event => {
     if (getExtraPayload) {
