e05842560c2eb6ec89ee0d7091062445c323c02d
Demetrio Girardi <dgirardi@prebid.org>
2022-09-15 11:09:14 -0600

    Update karma `concurrency` config

--- karma.conf.maker.js
+++ karma.conf.maker.js
@@ -171,7 +171,7 @@ module.exports = function(codeCoverage, browserstack, watchMode, file, disableFe
     browserNoActivityTimeout: 3e5, // default 10000
     captureTimeout: 3e5, // default 60000,
     browserDisconnectTolerance: 3,
-    concurrency: 6,
+    concurrency: 5, // browserstack allows us 5 concurrent sessions
 
     plugins: plugins
   };
