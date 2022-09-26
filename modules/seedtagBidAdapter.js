2d08bc82313a6d3cb4778cc1e19d1366b07ad011
Yohan Boutin <yohan@seedtag.com>
2022-09-26 11:16:56 +0200

    fix lint

--- modules/seedtagBidAdapter.js
+++ modules/seedtagBidAdapter.js
@@ -239,7 +239,7 @@ export const spec = {
     if (bidderRequest.uspConsent) {
       payload['uspConsent'] = bidderRequest.uspConsent
     }
-    
+
     if (validBidRequests[0].schain) {
       payload.schain = validBidRequests[0].schain;
     }
