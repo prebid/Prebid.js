ef16d7369867f51dbb33463572763a2747562aa5
John Conway <john.conway@washpost.com>
2022-10-25 15:20:49 -0400

    chore: fix issue with params in the initModule function

--- test/spec/modules/zeusPrimeRtdProvider_spec.js
+++ test/spec/modules/zeusPrimeRtdProvider_spec.js
@@ -1,6 +1,6 @@
-import { zeusPrimeSubmodule } from 'modules/zeusPrimeRtdProvider';
-import { server } from 'test/mocks/xhr.js';
-import * as utils from 'src/utils';
+import { zeusPrimeSubmodule } from "modules/zeusPrimeRtdProvider";
+import { server } from "test/mocks/xhr.js";
+import * as utils from "src/utils";
 
 async function waitForStatus(statusVar) {
   const MAX_COUNT = 20;
@@ -15,26 +15,26 @@ async function waitForStatus(statusVar) {
   }
 
   if (count === MAX_COUNT) {
-    throw new Error('Timeout waiting for zeusPrimeRtdProvider to complete');
+    throw new Error("Timeout waiting for zeusPrimeRtdProvider to complete");
   }
 }
 
 /**
- * Execute all the commands in the googletag.cmd queue. 
+ * Execute all the commands in the googletag.cmd queue.
  */
 function executeGoogletagTargeting() {
   window.googletag.cmd.forEach((cmd) => cmd());
 }
 
-describe('Zeus Prime RTD submodule', () => {
+describe("Zeus Prime RTD submodule", () => {
   let logErrorSpy;
   let logMessageSpy;
   let setTargetingStub;
   let originalGtag;
 
   beforeEach(() => {
-    logErrorSpy = sinon.spy(utils, 'logError');
-    logMessageSpy = sinon.spy(utils, 'logMessage');
+    logErrorSpy = sinon.spy(utils, "logError");
+    logMessageSpy = sinon.spy(utils, "logMessage");
     setTargetingStub = sinon.stub();
     window.zeusPrime = { cmd: [] };
     originalGtag = window.googletag;
@@ -53,62 +53,66 @@ describe('Zeus Prime RTD submodule', () => {
     delete window.zeusPrime;
   });
 
-  it('should init and set key-value for zeus_<gamId>', async () => {
+  it("should init and set key-value for zeus_<gamId>", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/",
+      },
     });
 
     // wait for the script to finish
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
 
     expect(window.googletag.cmd).to.have.length(1);
     sinon.assert.callCount(setTargetingStub, 1);
-    sinon.assert.calledWith(setTargetingStub, 'zeus_1234', 'www.example.com');
+    sinon.assert.calledWith(setTargetingStub, "zeus_1234", "www.example.com");
   });
 
-  it('should init and set key-value for zeus_<gamId> from command queue', async () => {
-    window.zeusPrime.cmd.push((prime) => (prime.gamId = '9876'));
+  it("should init and set key-value for zeus_<gamId> from command queue", async () => {
+    window.zeusPrime.cmd.push((prime) => (prime.gamId = "9876"));
     zeusPrimeSubmodule.init({
-      hostname: 'www.example.com',
-      pathname: '/',
+      params: {
+        hostname: "www.example.com",
+        pathname: "/",
+      },
     });
 
     // wait for the script to finish
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
 
     expect(window.googletag.cmd).to.have.length(1);
     sinon.assert.callCount(setTargetingStub, 1);
-    sinon.assert.calledWith(setTargetingStub, 'zeus_9876', 'www.example.com');
+    sinon.assert.calledWith(setTargetingStub, "zeus_9876", "www.example.com");
   });
 
-  it('should init with values from location and set key-value for zeus_<gamId>', async () => {
-    zeusPrimeSubmodule.init({ gamId: '1234' });
+  it("should init with values from location and set key-value for zeus_<gamId>", async () => {
+    zeusPrimeSubmodule.init({ params: { gamId: "1234" } });
 
     // wait for the script to finish
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
 
     expect(window.googletag.cmd).to.have.length(1);
     sinon.assert.callCount(setTargetingStub, 1);
-    sinon.assert.calledWith(setTargetingStub, 'zeus_1234', 'localhost');
-    expect(window.zeusPrime.pathname).to.equal('/context.html');
+    sinon.assert.calledWith(setTargetingStub, "zeus_1234", "localhost");
+    expect(window.zeusPrime.pathname).to.equal("/context.html");
   });
 
-  it('should emit error when gamId is not set', async () => {
+  it("should emit error when gamId is not set", async () => {
     zeusPrimeSubmodule.init({});
 
     // wait for the script to finish
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
@@ -117,44 +121,48 @@ describe('Zeus Prime RTD submodule', () => {
     sinon.assert.callCount(setTargetingStub, 0);
     sinon.assert.calledWith(
       logErrorSpy,
-      'zeusPrimeRtdProvider: ',
-      'Failed to run.',
-      'window.zeusPrime.gamId must be a string. Received: undefined'
+      "zeusPrimeRtdProvider: ",
+      "Failed to run.",
+      "window.zeusPrime.gamId must be a string. Received: undefined"
     );
   });
 
-  it('should not make a call to the server when url is a homepage', async () => {
+  it("should not make a call to the server when url is a homepage", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/",
+      },
     });
 
     // wait for the script to finish
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     expect(server.requests).to.have.length(0);
   });
 
-  it('should make a call to the server and set key-vlaue when url is an article page and returns topics', async () => {
+  it("should make a call to the server and set key-vlaue when url is an article page and returns topics", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('insightsReqReceived');
+    await waitForStatus("insightsReqReceived");
 
     // Response
     server.requests[0].respond(
       200,
-      { 'Content-Type': 'application/json' },
+      { "Content-Type": "application/json" },
       '{"topics": ["bs0"]}'
     );
 
     // Wait for the script to process the response
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
@@ -164,30 +172,32 @@ describe('Zeus Prime RTD submodule', () => {
     sinon.assert.calledTwice(setTargetingStub);
     sinon.assert.calledWith(
       setTargetingStub.firstCall,
-      'zeus_1234',
-      'www.example.com'
+      "zeus_1234",
+      "www.example.com"
     );
-    sinon.assert.calledWith(setTargetingStub.secondCall, 'zeus_insights', [
-      'bs0',
+    sinon.assert.calledWith(setTargetingStub.secondCall, "zeus_insights", [
+      "bs0",
     ]);
     sinon.assert.notCalled(logErrorSpy);
   });
 
-  it('should not set insights keyvalue when server returns 204', async () => {
+  it("should not set insights keyvalue when server returns 204", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('insightsReqReceived');
+    await waitForStatus("insightsReqReceived");
 
     // Response
-    server.requests[0].respond(204, { 'Content-Type': 'application/json' });
+    server.requests[0].respond(204, { "Content-Type": "application/json" });
 
     // Wait for the script to process the response
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
@@ -197,31 +207,33 @@ describe('Zeus Prime RTD submodule', () => {
     sinon.assert.calledOnce(setTargetingStub);
     sinon.assert.calledWith(
       setTargetingStub.firstCall,
-      'zeus_1234',
-      'www.example.com'
+      "zeus_1234",
+      "www.example.com"
     );
     sinon.assert.notCalled(logErrorSpy);
   });
 
-  it('should not set insights keyvalue when server returns empty topics array', async () => {
+  it("should not set insights keyvalue when server returns empty topics array", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('insightsReqReceived');
+    await waitForStatus("insightsReqReceived");
 
     // Respond
     server.requests[0].respond(
       200,
-      { 'Content-Type': 'application/json' },
+      { "Content-Type": "application/json" },
       '{ "topics": [] }'
     );
 
     // Wait for the script to process the response
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
@@ -231,31 +243,33 @@ describe('Zeus Prime RTD submodule', () => {
     sinon.assert.calledOnce(setTargetingStub);
     sinon.assert.calledWith(
       setTargetingStub.firstCall,
-      'zeus_1234',
-      'www.example.com'
+      "zeus_1234",
+      "www.example.com"
     );
     sinon.assert.notCalled(logErrorSpy);
   });
 
-  it('should not set insights keyvalue and emit error when server returns error status (400)', async () => {
+  it("should not set insights keyvalue and emit error when server returns error status (400)", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('insightsReqReceived');
+    await waitForStatus("insightsReqReceived");
 
     // Response
     server.requests[0].respond(
       404,
-      { 'Content-Type': 'application/json' },
+      { "Content-Type": "application/json" },
       '{"message": "Not found"}'
     );
 
     // Wait for the script to process the response
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
@@ -265,31 +279,33 @@ describe('Zeus Prime RTD submodule', () => {
     sinon.assert.calledOnce(setTargetingStub);
     sinon.assert.calledWith(
       setTargetingStub.firstCall,
-      'zeus_1234',
-      'www.example.com'
+      "zeus_1234",
+      "www.example.com"
     );
     sinon.assert.calledWith(
       logErrorSpy,
-      'zeusPrimeRtdProvider: ',
-      'Topics request returned error: 404'
+      "zeusPrimeRtdProvider: ",
+      "Topics request returned error: 404"
     );
   });
 
-  it('should not set insights keyvalue and emit error when response is not received (network request)', async () => {
+  it("should not set insights keyvalue and emit error when response is not received (network request)", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('insightsReqReceived');
+    await waitForStatus("insightsReqReceived");
 
     // Response
     server.requests[0].error();
 
     // Wait for the script to process the response
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
@@ -299,82 +315,90 @@ describe('Zeus Prime RTD submodule', () => {
     sinon.assert.calledOnce(setTargetingStub);
     sinon.assert.calledWith(
       setTargetingStub.firstCall,
-      'zeus_1234',
-      'www.example.com'
+      "zeus_1234",
+      "www.example.com"
     );
     sinon.assert.calledWith(
       logErrorSpy,
-      'zeusPrimeRtdProvider: ',
-      'failed to request topics'
+      "zeusPrimeRtdProvider: ",
+      "failed to request topics"
     );
   });
 
-  it('fails gracefully when crypto fails', async () => {
-    const digestStub = sinon.stub(window.crypto.subtle, 'digest');
-    digestStub.throwsException('Failed to generate digest.');
+  it("fails gracefully when crypto fails", async () => {
+    const digestStub = sinon.stub(window.crypto.subtle, "digest");
+    digestStub.throwsException("Failed to generate digest.");
 
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     sinon.assert.calledWith(
       logErrorSpy,
-      'zeusPrimeRtdProvider: ',
-      'Failed to load hash'
+      "zeusPrimeRtdProvider: ",
+      "Failed to load hash"
     );
 
     digestStub.restore();
   });
 
-  it('script should add zeus_prime key and not send request when disabled is set', async () => {
+  it("script should add zeus_prime key and not send request when disabled is set", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      disabled: true,
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        disabled: true,
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     // execute any googletag commands added to the queue during execution
     executeGoogletagTargeting();
 
     expect(server.requestCount).to.be.equal(0);
     expect(window.googletag.cmd).to.have.length(1);
-    sinon.assert.calledWith(setTargetingStub, 'zeus_prime', 'false');
+    sinon.assert.calledWith(setTargetingStub, "zeus_prime", "false");
   });
 
-  it('debug true enables debug logging', async () => {
+  it("debug true enables debug logging", async () => {
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      debug: true,
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        debug: true,
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     sinon.assert.called(logMessageSpy);
     sinon.assert.notCalled(logErrorSpy);
   });
 
-  it('debug false disables debug logging', async () => {
+  it("debug false disables debug logging", async () => {
     window.zeusPrime.disabled = false;
     zeusPrimeSubmodule.init({
-      gamId: '1234',
-      hostname: 'www.example.com',
-      pathname: '/article/some-article',
+      params: {
+        gamId: "1234",
+        hostname: "www.example.com",
+        pathname: "/article/some-article",
+      },
     });
 
     // Wait for request to be sent
-    await waitForStatus('scriptComplete');
+    await waitForStatus("scriptComplete");
 
     sinon.assert.notCalled(logMessageSpy);
     sinon.assert.notCalled(logErrorSpy);
