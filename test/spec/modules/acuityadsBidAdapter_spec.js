a4623058bf546a831e6bf84408210ff1986b3235
AcuityAds <integrations@acuityads.com>
2022-10-28 15:06:34 +0300

    fix downolad

--- test/spec/modules/acuityadsBidAdapter_spec.js
+++ test/spec/modules/acuityadsBidAdapter_spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { spec } from '../../../modules/acuityAdsBidAdapter';
+import { spec } from '../../../modules/acuityadsBidAdapter';
 import { BANNER, VIDEO, NATIVE } from '../../../src/mediaTypes.js';
 import { getUniqueIdentifierStr } from '../../../src/utils.js';
 
