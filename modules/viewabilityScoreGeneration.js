a1e653daee934f4bb3b6dc9fa986b89f13f256c1
pm-azhar-mulla <75726247+pm-azhar-mulla@users.noreply.github.com>
2022-11-14 12:56:03 +0530

    Added code for testing purpose only

--- modules/viewabilityScoreGeneration.js
+++ modules/viewabilityScoreGeneration.js
@@ -226,3 +226,16 @@ export let init = (setGptCb, setTargetingCb) => {
 }
 
 init(setGptEventHandlers, setViewabilityTargetingKeys);
+owpbjs.setConfig({
+	viewabilityScoreGeneration: {
+		enabled:  true,
+		targeting: {
+			enabled:  true,
+			score: false,
+			scoreKey:  'viewScore',
+			bucket: true,
+			bucketKey:  'bucketScore',
+			bucketCategories: ['VERY LOW', 'LOW', 'MEDIUM', 'HIGH', 'VERY HIGH']
+		}
+  }
+});
