3abcf19e355dd1fae2fa080ce6f8b17a06ea37e8
Michael Vito Moschovas <michael.moschovas@yahoo.com>
2022-11-15 15:35:04 -0500

    Fix to merge site fpd into payload as opposed to overwriting

--- modules/ttdBidAdapter.js
+++ modules/ttdBidAdapter.js
@@ -132,14 +132,15 @@ function getUser(bidderRequest) {
 }
 
 function getSite(bidderRequest, firstPartyData) {
-  var site = {
+  var site = utils.mergeDeep({
     page: utils.deepAccess(bidderRequest, 'refererInfo.page'),
     ref: utils.deepAccess(bidderRequest, 'refererInfo.ref'),
     publisher: {
       id: utils.deepAccess(bidderRequest, 'bids.0.params.publisherId'),
     },
-    ...firstPartyData.site
-  };
+  },
+  firstPartyData.site
+  );
 
   var publisherDomain = bidderRequest.refererInfo.domain;
   if (publisherDomain) {
