484327bd24776137ab00b6786de32fe79b44b7f2
Patrick McCann <pmccann@cafemedia.com>
2022-11-29 15:02:18 -0500

    Revert "AcuityAds adapter: fix issue with download (#9164)"
    This reverts commit 4f21a5bc9e2e4ace066a4d26ca1c9c637c46a477.

--- test/spec/modules/acuityAdsBidAdapter_spec.js
+++ test/spec/modules/acuityAdsBidAdapter_spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { spec } from '../../../modules/acuityadsBidAdapter';
+import { spec } from '../../../modules/acuityAdsBidAdapter';
 import { BANNER, VIDEO, NATIVE } from '../../../src/mediaTypes.js';
 import { getUniqueIdentifierStr } from '../../../src/utils.js';
 
