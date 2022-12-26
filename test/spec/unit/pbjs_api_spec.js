a5ea3097aacb6052a030220bdf87131d4ae5d59b
Demetrio Girardi <dgirardi@prebid.org>
2022-12-26 07:34:57 -0700

    Prebid core: filter adUnits (by `adUnitCodes`) before sending them to RTD and FPD modules (#9355)

--- test/spec/unit/pbjs_api_spec.js
+++ test/spec/unit/pbjs_api_spec.js
@@ -1810,6 +1810,16 @@ describe('Unit: Prebid Module', function () {
         })
       });
 
+      it('filtering adUnits by adUnitCodes', () => {
+        $$PREBID_GLOBAL$$.requestBids({
+          adUnits: [{code: 'one'}, {code: 'two'}],
+          adUnitCodes: 'two'
+        });
+        sinon.assert.calledWith(startAuctionStub, sinon.match({
+          adUnits: [{code: 'two'}]
+        }));
+      });
+
       it('passing bidder-specific FPD as ortb2Fragments.bidder', () => {
         configObj.setBidderConfig({
           bidders: ['bidderA', 'bidderC'],
@@ -1869,6 +1879,7 @@ describe('Unit: Prebid Module', function () {
           mediaTypes: {banner: {sizes: [[300, 250]]}},
           bids: [{bidder: 'bd'}]
         }],
+        adUnitCodes: ['au'],
         ortb2Fragments
       });
       sinon.assert.calledWith(newAuctionStub, sinon.match({
