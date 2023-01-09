42f9513545eb8e0510b4aa0803973424031a57c9
Nayan Savla <nsavla@yieldmo.com>
2023-01-09 08:35:40 -0800

    Yieldmo Adapter: Add support for structured user agent  (#9380)
    * Adding sua to device object
    
    * Update
    
    * import pick
    
    * ESLint fixes
    
    * Adding unit test
    
    * Copying entire device object

--- modules/yieldmoBidAdapter.js
+++ modules/yieldmoBidAdapter.js
@@ -385,7 +385,7 @@ function openRtbRequest(bidRequests, bidderRequest) {
     at: 1,
     imp: bidRequests.map(bidRequest => openRtbImpression(bidRequest)),
     site: openRtbSite(bidRequests[0], bidderRequest),
-    device: openRtbDevice(bidRequests[0]),
+    device: deepAccess(bidderRequest, 'ortb2.device'),
     badv: bidRequests[0].params.badv || [],
     bcat: deepAccess(bidderRequest, 'bcat') || bidRequests[0].params.bcat || [],
     ext: {
@@ -505,17 +505,6 @@ function openRtbSite(bidRequest, bidderRequest) {
       .forEach(param => result[param] = siteParams[param]);
   }
   return result;
-}
-
-/**
- * @return Object OpenRTB's 'device' object
- */
-function openRtbDevice(bidRequest) {
-  const deviceObj = {
-    ua: navigator.userAgent,
-    language: (navigator.language || navigator.browserLanguage || navigator.userLanguage || navigator.systemLanguage),
-  };
-  return deviceObj;
 }
 
 /**
