70c09dad2448b78b71319f60d6e1f53a5f0eeee1
Jose Cabal-Ugaz <6942011+josecu@users.noreply.github.com>
2023-01-11 14:22:46 -0500

    Implemented alter bid request function in ArcSpan RTD Provider

--- modules/arcspanRtdProvider.js
+++ modules/arcspanRtdProvider.js
@@ -1,4 +1,5 @@
 import { submodule } from '../src/hook.js';
+import { mergeDeep } from '../src/utils.js';
 
 /** @type {string} */
 const MODULE_NAME = 'realTimeData';
@@ -7,7 +8,8 @@ const SUBMODULE_NAME = 'arcspan';
 /** @type {RtdSubmodule} */
 export const subModuleObj = {
   name: SUBMODULE_NAME,
-  init: init
+  init: init,
+  getBidRequestData: alterBidRequests,
 };
 
 function init(config, userConsent) {
@@ -23,6 +25,47 @@ function init(config, userConsent) {
   }
   document.head.appendChild(_s);
   return true;
+}
+
+function alterBidRequests(reqBidsConfigObj, callback, config, userConsent) {
+  var _v1 = [];
+  var _v1s = [];
+  var _v2 = [];
+  var arcobj1 = window.arcobj1;
+  if (typeof arcobj1 != 'undefined') {
+    if (typeof arcobj1.page_iab_codes.text != 'undefined') { _v1 = _v1.concat(arcobj1.page_iab_codes.text); }
+    if (typeof arcobj1.page_iab_codes.images != 'undefined') { _v1 = _v1.concat(arcobj1.page_iab_codes.images); }
+    if (typeof arcobj1.page_iab.text != 'undefined') { _v1s = _v1s.concat(arcobj1.page_iab.text); }
+    if (typeof arcobj1.page_iab.images != 'undefined') { _v1s = _v1s.concat(arcobj1.page_iab.images); }
+    if (typeof arcobj1.page_iab_newcodes.text != 'undefined') { _v2 = [...new Set([..._v2, ...arcobj1.page_iab_newcodes.text])]; }
+    if (typeof arcobj1.page_iab_newcodes.images != 'undefined') { _v2 = [...new Set([..._v2, ...arcobj1.page_iab_newcodes.images])]; }
+
+    var _content = {};
+    _content.data = [];
+    var p = {};
+    p.name = 'arcspan';
+    p.segment = [];
+    p.ext = { segtax: 6 };
+    _v2.forEach(function (e) {
+      p.segment = p.segment.concat({ id: e });
+    });
+    _content.data = _content.data.concat(p);
+    var _ortb2 = {
+      site: {
+        name: 'arcspan',
+        domain: new URL(location.href).hostname,
+        cat: _v1,
+        sectioncat: _v1,
+        pagecat: _v1,
+        page: location.href,
+        ref: document.referrer,
+        keywords: _v1s.toString(),
+        content: _content,
+      },
+    };
+    mergeDeep(reqBidsConfigObj.ortb2Fragments.global, _ortb2);
+  }
+  callback();
 }
 
 submodule(MODULE_NAME, subModuleObj);
