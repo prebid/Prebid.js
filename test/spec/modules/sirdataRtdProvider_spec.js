0837fd36311ce324dddce0bd1813c65cb526cf15
Bob <bob@sirdata.com>
2023-02-07 17:36:39 +0100

    Update sirdataRtdProvider_spec.js

--- test/spec/modules/sirdataRtdProvider_spec.js
+++ test/spec/modules/sirdataRtdProvider_spec.js
@@ -50,7 +50,6 @@ describe('sirdataRtdProvider', function () {
 
   describe('Get Segments And Categories', function () {
     it('gets data from async request and adds segment data', function () {
-
       const overrideAppnexus = function (adUnit, list, data, bid) {
         deepSetValue(bid, 'params.keywords.custom', list);
       }
@@ -83,17 +82,17 @@ describe('sirdataRtdProvider', function () {
               placementId: 13144370
             }
           }, {
-            bidder: "smartadserver",
+            bidder: 'smartadserver',
             params: {
               siteId: 207435,
               pageId: 896536,
               formatId: 62913
             }
           }, {
-            bidder: "proxistore",
-            params: {website: "demo.sirdata.com", language: "fr"},
-            adUnitCode: "HALFPAGE_CENTER_LOADER",
-            transactionId: "92ac333a-a569-4827-abf1-01fc9d19278a",
+            bidder: 'proxistore',
+            params: {website: 'demo.sirdata.com', language: 'fr'},
+            adUnitCode: 'HALFPAGE_CENTER_LOADER',
+            transactionId: '92ac333a-a569-4827-abf1-01fc9d19278a',
             sizes: [[300, 600]],
             mediaTypes: {
               banner: {
@@ -109,10 +108,10 @@ describe('sirdataRtdProvider', function () {
                 sizes: [[300, 600]],
               },
             },
-            bidId: "190bab495bc5f6e",
-            bidderRequestId: "18c0b0f0c91cd88",
-            auctionId: "9bdd917b-908d-4d9f-8f2f-d443277a62fc",
-            src: "client",
+            bidId: '190bab495bc5f6e',
+            bidderRequestId: '18c0b0f0c91cd88',
+            auctionId: '9bdd917b-908d-4d9f-8f2f-d443277a62fc',
+            src: 'client',
             bidRequestsCount: 1,
             bidderRequestsCount: 1,
             bidderWinsCount: 0,
@@ -123,7 +122,7 @@ describe('sirdataRtdProvider', function () {
               size: [300, 600]
             }
           }, {
-            bidder: "rubicon",
+            bidder: 'rubicon',
             params: {
               accountId: 14062,
               siteId: 70608,
@@ -137,24 +136,24 @@ describe('sirdataRtdProvider', function () {
       };
 
       let data = {
-        "segments": [111111, 222222],
-        "segtaxid": null,
-        "cattaxid": null,
-        "contextual_categories": {"333333": 100},
-        "shared_taxonomy": {
-          "27440": {
-            "segments": [444444, 555555],
-            "segtaxid": 552,
-            "cattaxid": 553,
-            "contextual_categories": {"666666": 100}
+        'segments': [111111, 222222],
+        'segtaxid': null,
+        'cattaxid': null,
+        'contextual_categories': {'333333': 100},
+        'shared_taxonomy': {
+          '27440': {
+            'segments': [444444, 555555],
+            'segtaxid': 552,
+            'cattaxid': 553,
+            'contextual_categories': {'666666': 100}
           }
         },
-        "global_taxonomy": {
-          "9998": {
-            "segments": [123, 234],
-            "segtaxid": 4,
-            "cattaxid": 7,
-            "contextual_categories": {"345": 100, "456": 100}
+        'global_taxonomy': {
+          '9998': {
+            'segments': [123, 234],
+            'segtaxid': 4,
+            'cattaxid': 7,
+            'contextual_categories': {'345': 100, '456': 100}
           }
         }
       };
@@ -165,8 +164,7 @@ describe('sirdataRtdProvider', function () {
       let request = server.requests[0];
       request.respond(200, responseHeader, JSON.stringify(data));
 
-      //expect(reqBidsConfigObj.adUnits[0].bids[0].params.keywords).to.have.deep.property('custom', ['111111', '222222', '333333']);
-      expect(reqBidsConfigObj.adUnits[0].bids[1].params).to.have.deep.property('target', "sd_rtd=111111;sd_rtd=222222;sd_rtd=333333;sd_rtd=444444;sd_rtd=555555;sd_rtd=666666");
+      expect(reqBidsConfigObj.adUnits[0].bids[1].params).to.have.deep.property('target', 'sd_rtd=111111;sd_rtd=222222;sd_rtd=333333;sd_rtd=444444;sd_rtd=555555;sd_rtd=666666');
 
       expect(reqBidsConfigObj.ortb2Fragments.global.site.content.data[0].name).to.equal(
         'sirdata.com'
@@ -185,7 +183,6 @@ describe('sirdataRtdProvider', function () {
         {id: '234'}
       ]);
       expect(reqBidsConfigObj.ortb2Fragments.global.user.data[0].ext.segtax).to.equal(4);
-
     });
   });
 });
