4ccad9650ee167140091cf04c09db9e12f332752
pfeifer.daniel83 <pfeifer.daniel83@gmail.com>
2023-02-24 15:47:05 +0100

    AdsInteractive adapter first commit

---
+++ modules/adsinteractiveBidAdapter.js
@@ -0,0 +1,98 @@
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER } from '../src/mediaTypes.js';
+
+const ADSINTERACTIVE_CODE = 'adsinteractive';
+
+export const spec = {
+  code: ADSINTERACTIVE_CODE,
+  supportedMediaTypes: [BANNER],
+
+  isBidRequestValid: (bid) => {
+    return (
+      !!bid.params.adUnit && !!bid.bidId && bid.bidder === 'adsinteractive'
+    );
+  },
+
+  buildRequests: (bidRequests, bidderRequest) => {
+    return bidRequests.map((bid) => {
+      let url = 'https://pb.adsinteractive.com/prebid';
+      const data = {
+        id: bid.bidId,
+        site: {
+          page: bid.ortb2.site.page,
+          domain: bid.ortb2.site.domain,
+          publisher: {
+            domain: bid.ortb2.site.domain,
+          },
+          ext: {
+            amp: Number(bidderRequest.refererInfo.isAmp),
+          },
+        },
+        regs: bid.ortb2.regs,
+        device: bid.ortb2.device,
+        user: bid.ortb2.user,
+        imp: [
+          {
+            id: bid.params.adUnit,
+            banner: {
+              format: bid.sizes.map((size) => ({
+                w: size[0],
+                h: size[1],
+              })),
+            },
+            ext: {
+              bidder: {
+                adUnit: bid.params.adUnit,
+              },
+            },
+          },
+        ],
+        tmax: bidderRequest.timeout,
+      };
+      const options = {
+        withCredentials: true,
+      };
+      return {
+        method: 'POST',
+        url,
+        data,
+        options,
+      };
+    });
+  },
+
+  interpretResponse: (serverResponse, bidRequest) => {
+    if (!serverResponse || !serverResponse.body) {
+      return [];
+    }
+
+    let answer = [];
+    if (serverResponse.body.seatbid != null) {
+      serverResponse.body.seatbid.forEach((seatbid) => {
+        if (seatbid.bid.length) {
+          answer = [
+            ...answer,
+            ...seatbid.bid
+              .filter((bid) => bid.price > 0)
+              .map((adsinteractiveBid) => {
+                const bid = {
+                  id: adsinteractiveBid.id,
+                  requestId: bidRequest.data.id,
+                  cpm: adsinteractiveBid.price,
+                  ad: adsinteractiveBid.adm,
+                  adomain: adsinteractiveBid.adomain,
+                  width: adsinteractiveBid.w || 0,
+                  height: adsinteractiveBid.h || 0,
+                  currency: serverResponse.body.cur || 'USD',
+                  creativeId: adsinteractiveBid.crid || 0,
+                };
+                return bid;
+              }),
+          ];
+        }
+      });
+    }
+    return answer;
+  },
+};
+registerBidder(spec);
