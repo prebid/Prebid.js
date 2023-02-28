8275dc416d8c11fcc70c5543126c45a5ec207278
Jason Quaccia <jlquaccia@gmail.com>
2023-02-28 11:55:15 -0800

    Prebid Core: Only Send Optional Category Targeting Key if ACAT is Present on Page (#9530)
    * only send hb_acat if acat is present
    
    * removed a couple unneeded files
    
    * reverted a few more unneeded changes
    
    * updated acat test

--- src/auction.js
+++ src/auction.js
@@ -914,6 +914,7 @@ export function getStandardBidderSettings(mediaType, bidderCode) {
       }
     }
   }
+
   return standardSettings;
 }
 
@@ -965,7 +966,7 @@ function setKeys(keyValues, bidderSettings, custBidObj, bidReq) {
 
     if (
       ((typeof bidderSettings.suppressEmptyKeys !== 'undefined' && bidderSettings.suppressEmptyKeys === true) ||
-      key === CONSTANTS.TARGETING_KEYS.DEAL) && // hb_deal is suppressed automatically if not set
+      key === CONSTANTS.TARGETING_KEYS.DEAL || key === CONSTANTS.TARGETING_KEYS.ACAT) && // hb_deal & hb_acat are suppressed automatically if not set
       (
         isEmptyStr(value) ||
         value === null ||
