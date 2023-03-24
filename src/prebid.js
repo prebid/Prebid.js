20d1092d1435027c970346c9385826d849342387
Karim Mourra <karim@jwplayer.com>
2023-03-24 16:28:24 -0300

    updates tests

--- src/prebid.js
+++ src/prebid.js
@@ -1009,7 +1009,9 @@ $$PREBID_GLOBAL$$.markWinningBidAsUsed = function (markBidRequest) {
   }
 
   if (bids.length > 0) {
-    auctionManager.addWinningBid(bids[0]);
+    const winningBid = bids[0];
+    auctionManager.addWinningBid(winningBid);
+    winningBid.status = CONSTANTS.BID_STATUS.RENDERED;
   }
 };
 
