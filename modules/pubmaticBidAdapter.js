9990d377bac298b5ebca0dd034bf6095e91f68b3
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2023-03-28 15:35:30 +0530

    Test

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -20,6 +20,7 @@ const PREBID_NATIVE_HELP_LINK = 'http://prebid.org/dev-docs/show-native-ads.html
 const PUBLICATION = 'pubmatic'; // Your publication on Blue Billywig, potentially with environment (e.g. publication.bbvms.com or publication.test.bbvms.com)
 const RENDERER_URL = 'https://pubmatic.bbvms.com/r/'.concat('$RENDERER', '.js'); // URL of the renderer application
 const MSG_VIDEO_PLACEMENT_MISSING = 'Video.Placement param missing';
+
 const CUSTOM_PARAMS = {
   'kadpageurl': '', // Custom page url
   'gender': '', // User gender
