fb46b4b03277dff120ac2f11ad00bc88e3ecf1fb
Chris Huie <phoenixtechnerd@gmail.com>
2023-03-30 11:54:19 -0700

    remove timeout on synchronous test

--- test/spec/modules/realvuAnalyticsAdapter_spec.js
+++ test/spec/modules/realvuAnalyticsAdapter_spec.js
@@ -50,7 +50,6 @@ describe('RealVu', function() {
 
   describe('Analytics Adapter.', function () {
     it('enableAnalytics', function () {
-      this.timeout(3500)
       const config = {
         options: {
           partnerId: '1Y',
