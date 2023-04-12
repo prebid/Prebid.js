7cbb7950f12a732a007094bed6bd56ee89cc29b5
southern-growthcode <chris@growthcode.io>
2023-04-12 14:06:43 -0400

    GrowthCode new RTD Module

--- modules/growthCodeRtdProvider.js
+++ modules/growthCodeRtdProvider.js
@@ -12,6 +12,7 @@ import {ajax} from '../src/ajax.js';
 const MODULE_NAME = 'growthCodeRtd';
 const LOG_PREFIX = 'GrowthCodeRtd: ';
 const ENDPOINT_URL = 'https://p2.gcprivacy.com/v2/rtd?'
+const RTD_EXPIRE_KEY = 'gc_rtd_expire_at'
 const RTD_CACHE_KEY = 'gc_rtd_items'
 
 export const storage = getStorageManager({ gvlid: undefined, moduleName: MODULE_NAME });
@@ -47,22 +48,37 @@ function init(config, userConsent) {
   logMessage(LOG_PREFIX + 'Init RTB');
 
   const configParams = (config && config.params) || {};
+  let expiresAt = parseInt(storage.getDataFromLocalStorage(RTD_EXPIRE_KEY, null));
 
   items = tryParse(storage.getDataFromLocalStorage(RTD_CACHE_KEY, null));
-  if (items === null) {
+
+  // Expire Cache
+  let now = Math.trunc(Date.now() / 1000);
+  if ((!isNaN(expiresAt)) && (now > expiresAt)) {
+    expiresAt = NaN;
+    storage.removeDataFromLocalStorage(RTD_CACHE_KEY, null)
+    storage.removeDataFromLocalStorage(RTD_EXPIRE_KEY, null)
+  }
+  if ((items === null) && (isNaN(expiresAt))) {
     let gcid = localStorage.getItem('gcid')
 
     let url = configParams.url ? configParams.url : ENDPOINT_URL;
     url = tryAppendQueryString(url, 'pid', configParams.pid);
     url = tryAppendQueryString(url, 'u', window.location.href);
     url = tryAppendQueryString(url, 'gcid', gcid);
+    if ((userConsent.gdpr !== null) && (userConsent.gdpr.consentData.getTCData.tcString)) {
+      url = tryAppendQueryString(url, 'tcf', userConsent.gdpr.consentData.getTCData.tcString)
+    }
 
     ajax(url, {
       success: response => {
         let respJson = tryParse(response);
         // If response is a valid json and should save is true
-        if (respJson && respJson.items >= 1) {
+        if (respJson && respJson.results >= 1) {
           storage.setDataInLocalStorage(RTD_CACHE_KEY, JSON.stringify(respJson.items), null);
+          storage.setDataInLocalStorage(RTD_EXPIRE_KEY, respJson.expires_at, null)
+        } else {
+          storage.setDataInLocalStorage(RTD_EXPIRE_KEY, respJson.expires_at, null)
         }
       },
       error: error => {
@@ -83,17 +99,19 @@ function init(config, userConsent) {
  */
 function alterBidRequests(reqBidsConfigObj, callback, config, userConsent) {
   if (items != null) {
-    let adUnits = reqBidsConfigObj.adUnits
-    for (let i = 0; i < adUnits.length; i++) {
-      let adUnit = adUnits[i];
-      for (let k = 0; k < adUnit.bids.length; k++) {
-        let bid = adUnit.bids[k]
-        for (let j = 0; j < items.length; j++) {
-          let item = items[j]
-          if (item.bidder === bid.bidder) {
-            let data = JSON.parse(item.parameters);
-            if (item.attachment_point === 'reqBidsConfigObj.ortb2Fragments.bidder') {
-              mergeDeep(reqBidsConfigObj.ortb2Fragments.bidder, data)
+    for (let j = 0; j < items.length; j++) {
+      let item = items[j]
+      let data = JSON.parse(item.parameters);
+      if (item.attachment_point === 'data') {
+        mergeDeep(reqBidsConfigObj.ortb2Fragments.bidder, data)
+      } else if (item.attachment_point === 'config') {
+        let adUnits = reqBidsConfigObj.adUnits
+        for (let i = 0; i < adUnits.length; i++) {
+          let adUnit = adUnits[i];
+          for (let k = 0; k < adUnit.bids.length; k++) {
+            let bid = adUnit.bids[k]
+            if (item.bidder === bid.bidder) {
+              mergeDeep(reqBidsConfigObj.adUnits[i].bids[k], data)
             }
           }
         }
