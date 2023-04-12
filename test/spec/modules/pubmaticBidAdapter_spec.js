050dd844c199d4056a35aca630d65b9977a36ab8
Patrick McCann <patmmccann@gmail.com>
2023-04-12 17:45:37 -0400

    Update pubmaticBidAdapter_spec.js

--- test/spec/modules/pubmaticBidAdapter_spec.js
+++ test/spec/modules/pubmaticBidAdapter_spec.js
@@ -1116,18 +1116,6 @@ describe('PubMatic adapter', function () {
         expect(data.test).to.equal(undefined);
       });
 
-      // disabled this test case as  it refreshes the whole suite when in karma watch mode
-      // todo: needs a fix
-      xit('test flag set to 1 when pubmaticTest=true is present in page url', function() {
-        window.location.href += '#pubmaticTest=true';
-        // now all the test cases below will have window.location.href with #pubmaticTest=true
-        let request = spec.buildRequests(bidRequests, {
-          auctionId: 'new-auction-id'
-        });
-        let data = JSON.parse(request.data);
-        expect(data.test).to.equal(1);
-      });
-
   		it('Request params check', function () {
         let request = spec.buildRequests(bidRequests, {
           auctionId: 'new-auction-id'
