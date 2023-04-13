1ee5428882650b25bc8142edde964f70eb9787c5
aslagle <aslagle@spotx.tv>
2023-04-13 11:05:35 -0600

    Add context to emitted seatnonbid event

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -474,7 +474,13 @@ export function PrebidServer() {
             bidRequests.forEach(bidderRequest => events.emit(CONSTANTS.EVENTS.BIDDER_DONE, bidderRequest));
           }
           if (shouldEmitNonbids(s2sBidRequest.s2sConfig, response)) {
-            emitNonBids(response.ext.seatnonbid, bidRequests[0].auctionId);
+            emitNonBids({
+              seatnonbid: response.ext.seatnonbid,
+              auctionId: bidRequests[0].auctionId,
+              requestedBidders,
+              response,
+              adapterMetrics
+            });
           }
           done();
           doClientSideSyncs(requestedBidders, gdprConsent, uspConsent, gppConsent);
@@ -580,10 +586,14 @@ function shouldEmitNonbids(s2sConfig, response) {
   return s2sConfig?.extPrebid?.returnallbidstatus && response?.ext?.seatnonbid;
 }
 
-function emitNonBids(seatnonbid, auctionId) {
+function emitNonBids(context) {
+  const {seatnonbid, auctionId, response, requestedBidders, adapterMetrics} = context;
   events.emit(CONSTANTS.EVENTS.SEAT_NON_BID, {
     seatnonbid,
-    auctionId
+    auctionId,
+    response,
+    requestedBidders,
+    adapterMetrics
   });
 }
 
