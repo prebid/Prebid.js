e8dd230e089f29b4295f4db4a7cf4a57dd26056d
aslagle <aslagle@spotx.tv>
2023-04-18 13:35:09 -0600

    Remove sanitize step for emitted data

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -474,7 +474,7 @@ export function PrebidServer() {
             bidRequests.forEach(bidderRequest => events.emit(CONSTANTS.EVENTS.BIDDER_DONE, bidderRequest));
           }
           if (shouldEmitNonbids(s2sBidRequest.s2sConfig, response)) {
-            emitNonBids({
+            events.emit(CONSTANTS.EVENTS.SEAT_NON_BID, {
               seatnonbid: response.ext.seatnonbid,
               auctionId: bidRequests[0].auctionId,
               requestedBidders,
@@ -584,12 +584,6 @@ export const processPBSRequest = hook('sync', function (s2sBidRequest, bidReques
 
 function shouldEmitNonbids(s2sConfig, response) {
   return s2sConfig?.extPrebid?.returnallbidstatus && response?.ext?.seatnonbid;
-}
-
-function emitNonBids(context) {
-  // parse/stringify to prevent manipulation of original data through the event
-  const sanitizedData = JSON.parse(JSON.stringify({...context}))
-  events.emit(CONSTANTS.EVENTS.SEAT_NON_BID, sanitizedData);
 }
 
 /**
