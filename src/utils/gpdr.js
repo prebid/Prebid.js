a7124c46bc0e9c91eee618b60f6c87efec1c24f9
Nitin Nimbalkar <96475150+nitin0610@users.noreply.github.com>
2023-05-02 16:02:27 +0530

    Defaultgdprscope (#657)
    * changes to handle gdpr null case
    
    * GDPR namespace undefined check added
    
    * undefined check in consent management
    
    * Logs added for privacy gdpr checks
    
    * Used Template literals
    
    ---------
    
    Co-authored-by: Manasi Moghe <manasi.moghe@pubmatic.com>

--- src/utils/gpdr.js
+++ src/utils/gpdr.js
@@ -7,13 +7,13 @@ import {deepAccess, logWarn} from '../utils.js';
  * @returns {boolean} true if the gdprConsent is null-y; or GDPR does not apply; or if purpose 1 consent was given.
  */
 export function hasPurpose1Consent(gdprConsent) {
-  logWarn("Privacy - checking purpose1Consent - "+gdprConsent);
+  logWarn(`Privacy - checking purpose1Consent - ${gdprConsent}`);
   if(gdprConsent === null) {
-    logWarn("Privacy - gdprConsent is null, checking value of defaultGdprScope = "+owpbjs?.getConfig().consentManagement?.gdpr?.defaultGdprScope);
+    logWarn(`Privacy - gdprConsent is null, checking value of defaultGdprScope = ${owpbjs?.getConfig().consentManagement?.gdpr?.defaultGdprScope}`);
     return !(owpbjs?.getConfig().consentManagement?.gdpr?.defaultGdprScope === true)
   }
   if (gdprConsent?.gdprApplies) {
-    logWarn("Privacy - gdprConsent?.gdprApplies = "+gdprConsent?.gdprApplies+" and purpose consent = "+gdprConsent.vendorData.purpose.consents);
+    logWarn(`Privacy - gdprConsent?.gdprApplies = ${gdprConsent?.gdprApplies} and purpose consent = ${gdprConsent.vendorData.purpose.consents}`);
     return deepAccess(gdprConsent, 'vendorData.purpose.consents.1') === true;
   }
   return true;
