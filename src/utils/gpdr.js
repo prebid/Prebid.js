a085429fa302530a1fc7bc96207f26933d4fb267
pm-azhar-mulla <azhar@L1119.local>
2023-05-03 18:02:58 +0530

    Commented logWarns for consent

--- src/utils/gpdr.js
+++ src/utils/gpdr.js
@@ -9,11 +9,11 @@ import {deepAccess, logWarn} from '../utils.js';
 export function hasPurpose1Consent(gdprConsent) {
   logWarn(`Privacy - checking purpose1Consent - ${gdprConsent}`);
   if(gdprConsent === null) {
-    logWarn(`Privacy - gdprConsent is null, checking value of defaultGdprScope = ${owpbjs?.getConfig().consentManagement?.gdpr?.defaultGdprScope}`);
+    //logWarn(`Privacy - gdprConsent is null, checking value of defaultGdprScope = ${owpbjs?.getConfig().consentManagement?.gdpr?.defaultGdprScope}`);
     return !(owpbjs?.getConfig().consentManagement?.gdpr?.defaultGdprScope === true)
   }
   if (gdprConsent?.gdprApplies) {
-    logWarn(`Privacy - gdprConsent?.gdprApplies = ${gdprConsent?.gdprApplies} and purpose consent = ${gdprConsent.vendorData.purpose.consents}`);
+    //logWarn(`Privacy - gdprConsent?.gdprApplies = ${gdprConsent?.gdprApplies} and purpose consent = ${gdprConsent.vendorData.purpose.consents}`);
     return deepAccess(gdprConsent, 'vendorData.purpose.consents.1') === true;
   }
   return true;
