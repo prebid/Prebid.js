4e3c052b89f6419b0915b627b687fecde68f890d
Pierre Gerbelot Barillon <p.gerbelotbarillon@criteo.com>
2023-05-05 10:13:37 +0200

    Criteo: Add mapping for bid.meta.networkName

--- test/spec/modules/criteoBidAdapter_spec.js
+++ test/spec/modules/criteoBidAdapter_spec.js
@@ -1509,6 +1509,11 @@ describe('The Criteo bidding adapter', function () {
             height: 90,
             deal: 'myDealCode',
             adomain: ['criteo.com'],
+            ext: {
+              meta: {
+                networkName: 'Criteo'
+              }
+            }
           }],
         },
       };
@@ -1531,6 +1536,7 @@ describe('The Criteo bidding adapter', function () {
       expect(bids[0].height).to.equal(90);
       expect(bids[0].dealId).to.equal('myDealCode');
       expect(bids[0].meta.advertiserDomains[0]).to.equal('criteo.com');
+      expect(bids[0].meta.networkName).to.equal('Criteo');
     });
 
     it('should properly parse a bid response with a zoneId', function () {
