c46e38c8f324c3788fbce88ee48018352cd5d702
Jason Quaccia <jlquaccia@gmail.com>
2023-05-17 13:10:12 -0700

    Prebid 8: set pubmatic source.tid to auctionId (#9954)
    * set source.tid to auctionId
    * removed changes to test page
    * added support for setting ext.wrapper.transactionId as well

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -1112,7 +1112,11 @@ export const spec = {
     payload.ext.wrapper.wiid = conf.wiid || bidderRequest.auctionId;
     // eslint-disable-next-line no-undef
     payload.ext.wrapper.wv = $$REPO_AND_VERSION$$;
-    payload.ext.wrapper.transactionId = conf.transactionId;
+
+    if (bidderRequest) {
+      payload.ext.wrapper.transactionId = bidderRequest.auctionId;
+    }
+
     payload.ext.wrapper.wp = 'pbjs';
     const allowAlternateBidder = bidderRequest ? bidderSettings.get(bidderRequest.bidderCode, 'allowAlternateBidderCodes') : undefined;
     if (allowAlternateBidder !== undefined) {
@@ -1151,7 +1155,9 @@ export const spec = {
     // update device.language to ISO-639-1-alpha-2 (2 character language)
     payload.device.language = payload.device.language && payload.device.language.split('-')[0];
 
-    // passing nothing in source.tid -- Pubmatic can correct in a future PR
+    // passing auctionId in source.tid
+    if (bidderRequest) deepSetValue(payload, 'source.tid', bidderRequest.auctionId);
+
     // test bids
     if (window.location.href.indexOf('pubmaticTest=true') !== -1) {
       payload.test = 1;
