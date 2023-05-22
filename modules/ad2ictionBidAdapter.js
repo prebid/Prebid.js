6ce8a735b66ffb765f49178126b231a14fb94600
Richard Lee <14349+dlackty@users.noreply.github.com>
2023-05-22 11:01:25 +0800

    Update version

--- modules/ad2ictionBidAdapter.js
+++ modules/ad2ictionBidAdapter.js
@@ -1,13 +1,12 @@
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER } from '../src/mediaTypes.js';
-import { config } from '../src/config.js';
-import { generateUUID, getWindowTop, getWindowSelf } from '../src/utils.js';
 import { getStorageManager } from '../src/storageManager.js';
+import { getWindowSelf, getWindowTop } from '../src/utils.js';
 
 export const BIDDER_CODE = 'ad2iction';
 export const SUPPORTED_AD_TYPES = [BANNER];
-export const API_ENDPOINT = 'https://testads.ad2iction.com/html/v2/';
-export const API_VERSION_NUMBER = 8;
+export const API_ENDPOINT = 'https://ads.ad2iction.com/html/prebid/';
+export const API_VERSION_NUMBER = 2;
 export const COOKIE_NAME = 'ad2udid';
 
 export const storage = getStorageManager({ bidderCode: BIDDER_CODE });
@@ -27,43 +26,42 @@ export const spec = {
   isBidRequestValid: (bid) =>
     !!bid.params.id && typeof bid.params.id === 'string',
   buildRequests: (validBidRequests, bidderRequest) => {
-    return validBidRequests.map(bid => {
-      const w = canAccessTopWindow() ? getWindowTop() : getWindowSelf();
-      const options = {
-        contentType: 'application/json',
-        withCredentials: false,
-      };
-
-      const data = {
-        id: bid.params.id,
-        rf: bidderRequest.refererInfo.page,
-        o: w.innerWidth > w.innerHeight ? 'l' : 'p',
-        v: API_VERSION_NUMBER,
-        size: 'infinity',
-        prebid: 1,
-        iso: navigator.browserLanguage ||
-                        navigator.language,
-        udid: storage.getCookie(COOKIE_NAME),
-        bidId: bid.bidId,
-        _: Math.round(new Date().getTime()),
-      };
-      return {
-        method: 'GET',
-        url: API_ENDPOINT,
-        data,
-        options
-      };
+    const ids = validBidRequests.map(bid => {
+      return { bannerId: bid.params.id, bidId: bid.bidId };
     });
+    const w = canAccessTopWindow() ? getWindowTop() : getWindowSelf();
+    const options = {
+      contentType: 'application/json',
+      withCredentials: false,
+    };
+    const udid = storage.cookiesAreEnabled() && storage.getCookie(COOKIE_NAME);
+
+    const data = {
+      ids: JSON.stringify(ids),
+      rf: bidderRequest.refererInfo.page,
+      o: w.innerWidth > w.innerHeight ? 'l' : 'p',
+      v: API_VERSION_NUMBER,
+      iso: navigator.browserLanguage ||
+                      navigator.language,
+      udid: udid || '',
+      _: Math.round(new Date().getTime()),
+    };
+    return {
+      method: 'GET',
+      url: API_ENDPOINT,
+      data,
+      options
+    };
   },
-  interpretResponse: function (responseObj) {
-    if (!responseObj) return [];
-    const ad = responseObj.body;
+  interpretResponse: (serverResponse, bidRequest) => {
+    if (!Array.isArray(serverResponse.body)) {
+      return [];
+    }
 
-    ad['requestId'] = ad['bidId'];
-    delete ad['bidId'];
+    const bidResponses = serverResponse.body;
 
-    return [ad];
-  }
+    return bidResponses;
+  },
 };
 
 registerBidder(spec);
