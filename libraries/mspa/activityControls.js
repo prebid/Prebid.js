a61c7ec090255920c1fe0fc667c8fa65ccf91ee3
Demetrio Girardi <dgirardi@prebid.org>
2023-06-19 09:18:27 -0700

    gppControl_usnat

---
+++ libraries/mspa/activityControls.js
@@ -0,0 +1,70 @@
+import {registerActivityControl} from '../../src/activities/rules.js';
+import {
+  ACTIVITY_ENRICH_EIDS,
+  ACTIVITY_ENRICH_UFPD,
+  ACTIVITY_SYNC_USER,
+  ACTIVITY_TRANSMIT_PRECISE_GEO
+} from '../../src/activities/activities.js';
+import {gppDataHandler} from '../../src/adapterManager.js';
+
+// default interpretation for MSPA consent(s):
+// https://docs.google.com/document/d/1yPEVx3bBdSkIw-QNkQR5qi1ztmn9zoXk-LaGQB6iw7Q
+
+function isBasicConsentDenied(cd) {
+  return ['MspaServiceProviderMode', 'Gpc'].some(prop => cd[prop] === 1) ||
+    cd.PersonalDataConsents === 2 ||
+    cd.KnownChildSensitiveDataConsents[1] === 1 || cd.KnownChildSensitiveDataConsents[2] !== 0;
+}
+
+function isSensitiveConsentDenied(cd) {
+  return ['SensitiveDataProcessingOptOutNotice', 'SensitiveDataLimitUseNotice'].some(prop => cd[prop] === 2)
+}
+
+function isConsentDenied(cd) {
+  return isBasicConsentDenied(cd) ||
+    ['SaleOptOut', 'SharingOptOut', 'TargetedAdvertisingOptOut'].some(prop => cd[prop] === 1) ||
+    ['SaleOptOutNotice', 'SharingNotice', 'SharingOptOutNotice', 'TargetedAdvertisingOptOutNotice'].some(prop => cd[prop] === 2);
+}
+
+function isTransmitUfpdConsentDenied(cd) {
+  return isConsentDenied(cd) ||
+    isSensitiveConsentDenied(cd) ||
+    cd.SensitveDataProcessing.some((val) => val === 1) ||
+    cd.SensitveDataProcessing[7] !== 0;
+}
+
+function isTransmitGeoConsentDenied(cd) {
+  return isBasicConsentDenied(cd) ||
+    isSensitiveConsentDenied(cd) ||
+    cd.SensitiveDataProcessing[8] === 1
+}
+
+const CONSENT_RULES = {
+  [ACTIVITY_SYNC_USER]: isConsentDenied,
+  [ACTIVITY_ENRICH_EIDS]: isConsentDenied,
+  [ACTIVITY_ENRICH_UFPD]: isConsentDenied,
+  [ACTIVITY_ENRICH_UFPD]: isTransmitUfpdConsentDenied,
+  [ACTIVITY_TRANSMIT_PRECISE_GEO]: isTransmitGeoConsentDenied
+}
+
+export function mspaRule(sids, getConsent, denies, applicableSids = () => gppDataHandler.getConsentData()?.applicableSections) {
+  return function() {
+    if (applicableSids().some(sid => sids.includes(sid))) {
+      const consent = getConsent();
+      if (consent == null) {
+        return {allow: false, reason: 'consent data not available'};
+      }
+      if (denies(consent)) {
+        return {allow: false}
+      }
+    }
+  }
+}
+
+export function setupRules(api, sids, normalizeConsent = (c) => c, rules = CONSENT_RULES, registerRule = registerActivityControl, getConsentData = () => gppDataHandler.getConsentData()) {
+  const unreg = [];
+  Object.entries(rules).forEach(([activity, denies]) => {
+    unreg.push(registerRule(activity, `MSPA (${api})`, mspaRule(sids, () => normalizeConsent(getConsentData()?.sectionData?.[api]), denies, () => getConsentData()?.applicableSections || [])))
+  })
+  return () => unreg.forEach(ur => ur())
+}
---
+++ test/spec/libraries/mspa/activityControls_spec.js
@@ -0,0 +1,100 @@
+import {mspaRule, setupRules} from '../../../../libraries/mspa/activityControls.js';
+import {ruleRegistry} from '../../../../src/activities/rules.js';
+
+describe('mspaRule', () => {
+  it('does not apply if SID is not applicable', () => {
+    const rule = mspaRule([1, 2], () => null, () => true, () => [3, 4]);
+    expect(rule()).to.not.exist;
+  });
+
+  it('does not apply when no SID is applicable', () => {
+    const rule = mspaRule([1], () => null, () => true, () => []);
+    expect(rule()).to.not.exist;
+  });
+
+  describe('when SID is applicable', () => {
+    let consent, denies;
+    function mkRule() {
+      return mspaRule([1, 2], () => consent, denies, () => [2])
+    }
+
+    beforeEach(() => {
+      consent = null;
+      denies = sinon.stub();
+    });
+
+    it('should deny when no consent is available', () => {
+      expect(mkRule()().allow).to.equal(false);
+    });
+
+    Object.entries({
+      'denies': true,
+      'allows': false
+    }).forEach(([t, denied]) => {
+      it(`should check if deny fn ${t}`, () => {
+        denies.returns(denied);
+        consent = {mock: 'value'};
+        const result = mkRule()();
+        sinon.assert.calledWith(denies, consent);
+        if (denied) {
+          expect(result.allow).to.equal(false);
+        } else {
+          expect(result).to.not.exist;
+        }
+      })
+    })
+  })
+});
+
+describe('setupRules', () => {
+  let rules, registerRule, isAllowed, consent;
+  beforeEach(() => {
+    rules = {
+      mockActivity: sinon.stub().returns(true)
+    };
+    ([registerRule, isAllowed] = ruleRegistry());
+    consent = {
+      applicableSections: [1],
+      sectionData: {
+        mockApi: {
+          mock: 'consent'
+        }
+      }
+    };
+  });
+
+  function runSetup(api, sids, normalize) {
+    return setupRules(api, sids, normalize, rules, registerRule, () => consent)
+  }
+
+  it('should use section data for the given api', () => {
+    runSetup('mockApi', [1]);
+    expect(isAllowed('mockActivity', {})).to.equal(false);
+    sinon.assert.calledWith(rules.mockActivity, {mock: 'consent'})
+  });
+
+  it('should not choke when no consent data is available', () => {
+    consent = null;
+    runSetup('mockApi', [1]);
+    expect(isAllowed('mockActivity', {})).to.equal(true);
+  })
+
+  it('should check applicableSeciotns against given SIDs', () => {
+    runSetup('mockApi', [2]);
+    expect(isAllowed('mockActivity', {})).to.equal(true);
+  });
+
+  it('should pass consent through normalizeConsent', () => {
+    const normalize = sinon.stub().returns({normalized: 'consent'})
+    runSetup('mockApi', [1], normalize);
+    expect(isAllowed('mockActivity', {})).to.equal(false);
+    sinon.assert.calledWith(normalize, {mock: 'consent'});
+    sinon.assert.calledWith(rules.mockActivity, {normalized: 'consent'});
+  });
+
+  it('should return a function that unregisters activity controls', () => {
+    const dereg = runSetup('mockApi', [1]);
+    dereg();
+    expect(isAllowed('mockActivity', {})).to.equal(true);
+  })
+})
