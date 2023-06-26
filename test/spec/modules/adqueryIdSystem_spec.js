fea9b7910ad62e1dbd9b506d9e4ce07373ea2d58
adquery <p.pydys@openmobi.pl>
2023-06-26 09:31:10 +0200

    adquery/prebid_qid_work1

--- test/spec/modules/adqueryIdSystem_spec.js
+++ test/spec/modules/adqueryIdSystem_spec.js
@@ -1,6 +1,5 @@
 import {adqueryIdSubmodule, storage} from 'modules/adqueryIdSystem.js';
 import {server} from 'test/mocks/xhr.js';
-import {GlobalExchange, internal as adagio, VERSION} from "../../../modules/adagioBidAdapter";
 import * as utils from "../../../src/utils";
 
 const config = {
@@ -28,14 +27,16 @@ describe('AdqueryIdSystem', function () {
 
   describe('getId', function () {
     let getDataFromLocalStorageStub;
+    let getUniqueIdentifierStrStub;
 
     beforeEach(function () {
       getDataFromLocalStorageStub = sinon.stub(storage, 'getDataFromLocalStorage');
-      sandbox.stub(utils, 'getUniqueIdentifierStr').returns('6dd9eab7df9ca5763001fb');
+      getUniqueIdentifierStrStub = sandbox.stub(utils, 'getUniqueIdentifierStr').returns('6dd9eab7df9ca5763001fb');
     });
 
     afterEach(function () {
       getDataFromLocalStorageStub.restore();
+      getUniqueIdentifierStrStub.restore();
     });
 
     it('gets a adqueryId', function () {
@@ -47,35 +48,23 @@ describe('AdqueryIdSystem', function () {
       callback(callbackSpy);
       const request = server.requests[0];
       expect(request.url).to.eq(`https://bidder.adquery.io/prebid/qid?qid=6dd9eab7df9ca5763001fb`);
-      request.respond(200, {'Content-Type': 'application/json'}, JSON.stringify({qid: 'qid'}));
-      expect(callbackSpy.lastCall.lastArg).to.deep.equal({qid: 'qid'});
-    });
-
-    it('gets a cached adqueryId', function () {
-      const config = {
-        params: {}
-      };
-      getDataFromLocalStorageStub.withArgs('qid').returns('qid');
-
-      const callbackSpy = sinon.spy();
-      const callback = adqueryIdSubmodule.getId(config).callback;
-      callback(callbackSpy);
-      expect(callbackSpy.lastCall.lastArg).to.deep.equal({qid: 'qid'});
+      request.respond(200, {'Content-Type': 'application/json'}, JSON.stringify({qid: '6dd9eab7df9ca5763001fb'}));
+      expect(callbackSpy.lastCall.lastArg).to.deep.equal('6dd9eab7df9ca5763001fb');
     });
 
     it('allows configurable id url', function () {
       const config = {
         params: {
-          url: 'https://bidder.adquery.io'
+          url: 'https://another_bidder.adquery.io/qid'
         }
       };
       const callbackSpy = sinon.spy();
       const callback = adqueryIdSubmodule.getId(config).callback;
       callback(callbackSpy);
       const request = server.requests[0];
-      expect(request.url).to.eq('https://bidder.adquery.io');
+      expect(request.url).to.contains('https://another_bidder.adquery.io');
       request.respond(200, {'Content-Type': 'application/json'}, JSON.stringify({qid: 'testqid'}));
-      expect(callbackSpy.lastCall.lastArg).to.deep.equal({qid: 'testqid'});
+      expect(callbackSpy.lastCall.lastArg).to.deep.equal('testqid');
     });
   });
 });
