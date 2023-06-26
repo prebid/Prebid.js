9394f468acb1cb0c64c4672860d004445503d593
Timothy M. Ace <tace@imds.tv>
2023-06-26 18:28:47 -0400

    CAPT-787: Add gppsid parameter to usersync to mirror the prebid-server instance

--- modules/imdsBidAdapter.js
+++ modules/imdsBidAdapter.js
@@ -315,7 +315,7 @@ export const spec = {
       queryParams.push('us_privacy=' + encodeURIComponent(uspConsent));
     }
     if (gppConsent) {
-      queryParams.push('gpp=' + encodeURIComponent(gppConsent));
+      queryParams.push('gpp=' + encodeURIComponent(gppConsent.gppString) + '&gppsid=' + encodeURIComponent((gppConsent.applicableSections || []).join(',')));
     }
 
     if (syncOptions.iframeEnabled) {
