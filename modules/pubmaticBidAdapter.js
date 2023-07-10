b38612fc96c81642e922cea0283b5299d3b3f00b
pm-nitin-shirsat <107102698+pm-nitin-shirsat@users.noreply.github.com>
2023-07-10 23:26:44 +0530

    PubMatic Bid Adapter : add GPP support (#10165)
    * Implement functionality for deal priority
    
    * Update test cases
    
    * kick off test manually
    
    * Added support of GPP to PubMatic adapter
    
    * gpp_sid in user syncs supposed to encode as a string, not an array
    
    * Remove extra space
    
    * Remove trailing spaces
    
    ---------
    
    Co-authored-by: Chris Huie <phoenixtechnerd@gmail.com>

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -1194,6 +1194,15 @@ export const spec = {
       deepSetValue(payload, 'regs.ext.us_privacy', bidderRequest.uspConsent);
     }
 
+    // Attaching GPP Consent Params
+    if (bidderRequest?.gppConsent?.gppString) {
+      deepSetValue(payload, 'regs.gpp', bidderRequest.gppConsent.gppString);
+      deepSetValue(payload, 'regs.gpp_sid', bidderRequest.gppConsent.applicableSections);
+    } else if (bidderRequest?.ortb2?.regs?.gpp) {
+      deepSetValue(payload, 'regs.gpp', bidderRequest.ortb2.regs.gpp);
+      deepSetValue(payload, 'regs.gpp_sid', bidderRequest.ortb2.regs.gpp_sid);
+    }
+
     // coppa compliance
     if (config.getConfig('coppa') === true) {
       deepSetValue(payload, 'regs.coppa', 1);
@@ -1352,7 +1361,7 @@ export const spec = {
   /**
    * Register User Sync.
    */
-  getUserSyncs: (syncOptions, responses, gdprConsent, uspConsent) => {
+  getUserSyncs: (syncOptions, responses, gdprConsent, uspConsent, gppConsent) => {
     let syncurl = '' + publisherId;
 
     // Attaching GDPR Consent Params in UserSync url
@@ -1364,6 +1373,12 @@ export const spec = {
     // CCPA
     if (uspConsent) {
       syncurl += '&us_privacy=' + encodeURIComponent(uspConsent);
+    }
+
+    // GPP Consent
+    if (gppConsent?.gppString && gppConsent?.applicableSections?.length) {
+      syncurl += '&gpp=' + encodeURIComponent(gppConsent.gppString);
+      syncurl += '&gpp_sid=' + encodeURIComponent(gppConsent?.applicableSections?.join(','));
     }
 
     // coppa compliance
