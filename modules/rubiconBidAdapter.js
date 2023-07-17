a36de494d67d74a7b42d6882a924626b3924445a
aslagle <aslagle@spotx.tv>
2023-07-17 10:30:44 -0600

    Add tests for multiformat data

--- modules/rubiconBidAdapter.js
+++ modules/rubiconBidAdapter.js
@@ -200,6 +200,10 @@ export const converter = ortbConverter({
     bidRequest.params.position === 'btf' && (imp.video.pos = 3);
     delete imp.ext?.prebid?.storedrequest;
 
+    if (bidRequest.params.bidonmultiformat === true && bidRequestType.length > 1) {
+      deepSetValue(imp, 'ext.prebid.bidder.rubicon.params.formats', bidRequestType);
+    }
+
     setBidFloors(bidRequest, imp);
 
     return imp;
@@ -495,6 +499,11 @@ export const spec = {
         logError('Rubicon: getFloor threw an error: ', e);
       }
       data['rp_hard_floor'] = typeof floorInfo === 'object' && floorInfo.currency === 'USD' && !isNaN(parseInt(floorInfo.floor)) ? floorInfo.floor : undefined;
+    }
+
+    // Send multiformat data if requested
+    if (params.bidonmultiformat === true && deepAccess(params, 'params.formats.length') && params.params.formats.length > 1) {
+      data['p_formats'] = params.params.formats.join(',');
     }
 
     // add p_pos only if specified and valid
