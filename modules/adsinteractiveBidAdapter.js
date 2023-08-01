39ee39954a7b1d66c1667553ca9a0497a9f94544
Balint Vargha <varghabalint@gmail.com>
2023-08-01 11:57:04 +0200

    added gvlid to bid adapter

--- modules/adsinteractiveBidAdapter.js
+++ modules/adsinteractiveBidAdapter.js
@@ -8,10 +8,13 @@ import { BANNER } from '../src/mediaTypes.js';
 const ADSINTERACTIVE_CODE = 'adsinteractive';
 const USER_SYNC_URL_IMAGE = 'https://sync.adsinteractive.com/img';
 const USER_SYNC_URL_IFRAME = 'https://sync.adsinteractive.com/sync';
+const GVLID = 1212;
+
 
 export const spec = {
   code: ADSINTERACTIVE_CODE,
   supportedMediaTypes: [BANNER],
+  gvlid: GVLID,
 
   isBidRequestValid: (bid) => {
     return (
