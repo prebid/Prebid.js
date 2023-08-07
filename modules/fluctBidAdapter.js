eb87c1c5f4a6771ac7a77b3731db53659c53571b
Chris Huie <phoenixtechnerd@gmail.com>
2023-08-07 05:58:58 -0700

    run circleci

--- modules/fluctBidAdapter.js
+++ modules/fluctBidAdapter.js
@@ -26,7 +26,7 @@ export const spec = {
    * Make a server request from the list of BidRequests.
    *
    * @param {validBidRequests[]} - an array of bids.
-   * @param {bidderRequest} bidderRequest bidder request object
+   * @param {bidderRequest} bidderRequest bidder request object.
    * @return ServerRequest Info describing the request to the server.
    */
   buildRequests: (validBidRequests, bidderRequest) => {
