06ae9174635304beafda9e232c747494571d3178
Demetrio Girardi <dgirardi@prebid.org>
2023-08-17 05:44:56 -0700

    UserID: check for all consent (not just GDPR) to check if an ID needs refreshing (#10286)
    * consent hash & multiHandler
    
    * userId: check any consent (not just GDPR) for changes when deciding if a stored ID needs to be refreshed

--- src/consentHandler.js
+++ src/consentHandler.js
@@ -1,5 +1,6 @@
-import {isStr, timestamp} from './utils.js';
+import {cyrb53Hash, isStr, timestamp} from './utils.js';
 import {defer, GreedyPromise} from './utils/promise.js';
+import {config} from './config.js';
 
 /**
  * Placeholder gvlid for when vendor consent is not required. When this value is used as gvlid, the gdpr
@@ -14,7 +15,10 @@ export class ConsentHandler {
   #data;
   #defer;
   #ready;
+  #dirty = true;
+  #hash;
   generatedTime;
+  hashFields;
 
   constructor() {
     this.reset();
@@ -74,15 +78,24 @@ export class ConsentHandler {
 
   setConsentData(data, time = timestamp()) {
     this.generatedTime = time;
+    this.#dirty = true;
     this.#resolve(data);
   }
 
   getConsentData() {
     return this.#data;
   }
+
+  get hash() {
+    if (this.#dirty) {
+      this.#hash = cyrb53Hash(JSON.stringify(this.#data && this.hashFields ? this.hashFields.map(f => this.#data[f]) : this.#data))
+      this.#dirty = false;
+    }
+    return this.#hash;
+  }
 }
 
-export class UspConsentHandler extends ConsentHandler {
+class UspConsentHandler extends ConsentHandler {
   getConsentMeta() {
     const consentData = this.getConsentData();
     if (consentData && this.generatedTime) {
@@ -94,7 +107,8 @@ export class UspConsentHandler extends ConsentHandler {
   }
 }
 
-export class GdprConsentHandler extends ConsentHandler {
+class GdprConsentHandler extends ConsentHandler {
+  hashFields = ['gdprApplies', 'consentString']
   getConsentMeta() {
     const consentData = this.getConsentData();
     if (consentData && consentData.vendorData && this.generatedTime) {
@@ -108,7 +122,8 @@ export class GdprConsentHandler extends ConsentHandler {
   }
 }
 
-export class GppConsentHandler extends ConsentHandler {
+class GppConsentHandler extends ConsentHandler {
+  hashFields = ['applicableSections', 'gppString'];
   getConsentMeta() {
     const consentData = this.getConsentData();
     if (consentData && this.generatedTime) {
@@ -159,4 +174,55 @@ export function gvlidRegistry() {
   }
 }
 
+export const gdprDataHandler = new GdprConsentHandler();
+export const uspDataHandler = new UspConsentHandler();
+export const gppDataHandler = new GppConsentHandler();
+export const coppaDataHandler = (() => {
+  function getCoppa() {
+    return !!(config.getConfig('coppa'))
+  }
+  return {
+    getCoppa,
+    getConsentData: getCoppa,
+    getConsentMeta: getCoppa,
+    reset() {},
+    get promise() {
+      return GreedyPromise.resolve(getCoppa())
+    },
+    get hash() {
+      return getCoppa() ? '1' : '0'
+    }
+  }
+})();
+
 export const GDPR_GVLIDS = gvlidRegistry();
+
+const ALL_HANDLERS = {
+  gdpr: gdprDataHandler,
+  usp: uspDataHandler,
+  gpp: gppDataHandler,
+  coppa: coppaDataHandler,
+}
+
+export function multiHandler(handlers = ALL_HANDLERS) {
+  handlers = Object.entries(handlers);
+  function collector(method) {
+    return function () {
+      return Object.fromEntries(handlers.map(([name, handler]) => [name, handler[method]()]))
+    }
+  }
+  return Object.assign(
+    {
+      get promise() {
+        return GreedyPromise.all(handlers.map(([name, handler]) => handler.promise.then(val => [name, val])))
+          .then(entries => Object.fromEntries(entries));
+      },
+      get hash() {
+        return cyrb53Hash(handlers.map(([_, handler]) => handler.hash).join(':'));
+      }
+    },
+    Object.fromEntries(['getConsentData', 'getConsentMeta', 'reset'].map(n => [n, collector(n)])),
+  )
+}
+
+export const allConsent = multiHandler();
