579edf6ab07d6aa28f54d30359c22f0fdb3466af
Reda Guermas <reda.guermas@nobid.io>
2023-08-21 13:22:02 -0700

    Added NoBid Analytics Adapter unit tests.

--- modules/nobidAnalyticsAdapter.js
+++ modules/nobidAnalyticsAdapter.js
@@ -6,18 +6,16 @@ import CONSTANTS from '../src/constants.json';
 import adapterManager from '../src/adapterManager.js';
 import {MODULE_TYPE_ANALYTICS} from '../src/activities/modules.js';
 
-const VERSION = '1.0.3';
+const VERSION = '1.0.4';
 const MODULE_NAME = 'nobidAnalyticsAdapter';
 const ANALYTICS_DATA_NAME = 'analytics.nobid.io';
-const RETENTION_DAYS = 7;
+const RETENTION_SECONDS = 7 * 24 * 3600;
 const TEST_ALLOCATION_PERCENTAGE = 5; // dont block 5% of the time;
 window.nobidAnalyticsVersion = VERSION;
 const analyticsType = 'endpoint';
 const url = 'localhost:8383/event';
 const GVLID = 816;
 const storage = getStorageManager({gvlid: GVLID, moduleName: MODULE_NAME, moduleType: MODULE_TYPE_ANALYTICS});
-const initOptions = {};
-let topLocation;
 const {
   EVENTS: {
     AUCTION_INIT,
@@ -29,30 +27,17 @@ const {
     AD_RENDER_SUCCEEDED
   }
 } = CONSTANTS;
-function log (msg, obj) {
+function log (msg) {
   // eslint-disable-next-line no-console
-  if (obj) console.log(`%cNoBid Analytics ${VERSION}`, 'padding: 2px 8px 2px 8px; background-color:#f50057; color: white', msg, obj);
-  // eslint-disable-next-line no-console
-  else console.log(`%cNoBid Analytics ${VERSION}`, 'padding: 2px 8px 2px 8px; background-color:#f50057; color: white', msg);
+  console.log(`%cNoBid Analytics ${VERSION}`, 'padding: 2px 8px 2px 8px; background-color:#f50057; color: white', msg);
 }
 function isJson (str) {
   return str && str.startsWith('{') && str.endsWith('}');
 }
-function isExpired (data) {
-  if (data.ts + RETENTION_DAYS * 24 * 3600 * 1000 < Date.now()) return true;
+function isExpired (data, retentionSeconds) {
+  retentionSeconds = retentionSeconds || RETENTION_SECONDS;
+  if (data.ts + retentionSeconds * 1000 < Date.now()) return true;
   return false;
-}
-function processServerResponse (response) {
-  if (!isJson(response)) return;
-  const resp = JSON.parse(response);
-  storage.setDataInLocalStorage(ANALYTICS_DATA_NAME, JSON.stringify({ ...resp, ts: Date.now() }));
-}
-function isAnalyticsDisabled () {
-  let stored = storage.getDataFromLocalStorage(ANALYTICS_DATA_NAME);
-  if (!isJson(stored)) return false;
-  stored = JSON.parse(stored);
-  if (isExpired(stored)) return false;
-  return stored.disabled;
 }
 function sendEvent (event, eventType) {
   function resolveEndpoint() {
@@ -63,17 +48,17 @@ function sendEvent (event, eventType) {
     else if (env == 'dev') ret = 'https://localhost:8383';
     return ret;
   }
-  if (!initOptions || !initOptions.options || !initOptions.options.siteId || !event) return;
-  if (isAnalyticsDisabled()) {
+  if (!nobidAnalytics.initOptions || !nobidAnalytics.initOptions.siteId || !event) return;
+  if (nobidAnalytics.isAnalyticsDisabled()) {
     log('NoBid Analytics is Disabled');
     return;
   }
   try {
-    const endpoint = `${resolveEndpoint()}/event/${eventType}?pubid=${initOptions.options.siteId}`;
+    const endpoint = `${resolveEndpoint()}/event/${eventType}?pubid=${nobidAnalytics.initOptions.siteId}`;
     ajax(endpoint,
       function (response) {
         try {
-          processServerResponse(response);
+          nobidAnalytics.processServerResponse(response);
         } catch (e) {
           logError(e);
         }
@@ -85,7 +70,7 @@ function sendEvent (event, eventType) {
       }
     );
   } catch (err) {
-    log('Sending event error: ' + err);
+    log(`Sending event error ${err}`);
   }
 }
 function cleanupObjectAttributes (obj, attributes) {
@@ -99,13 +84,15 @@ function cleanupObjectAttributes (obj, attributes) {
 function sendBidWonEvent (event, eventType) {
   const data = deepClone(event);
   cleanupObjectAttributes(data, ['bidderCode', 'size', 'statusMessage', 'adId', 'requestId', 'mediaType', 'adUnitCode', 'cpm', 'timeToRespond']);
-  if (topLocation) data.topLocation = topLocation;
+  if (nobidAnalytics.topLocation) data.topLocation = nobidAnalytics.topLocation;
   sendEvent(data, eventType);
 }
 function sendAuctionEndEvent (event, eventType) {
-  if (!topLocation && event && event.bidderRequests && event.bidderRequests.length > 0 && event.bidderRequests[0].refererInfo &&
-    event.bidderRequests[0].refererInfo.topmostLocation) topLocation = event.bidderRequests[0].refererInfo.topmostLocation
+  if (event?.bidderRequests?.length > 0 && event?.bidderRequests[0]?.refererInfo?.topmostLocation) {
+    nobidAnalytics.topLocation = event.bidderRequests[0].refererInfo.topmostLocation;
+  }
   const data = deepClone(event);
+
   cleanupObjectAttributes(data, ['timestamp', 'timeout', 'auctionId', 'bidderRequests', 'bidsReceived']);
   if (data) cleanupObjectAttributes(data.bidderRequests, ['bidderCode', 'bidderRequestId', 'bids', 'refererInfo']);
   if (data) cleanupObjectAttributes(data.bidsReceived, ['bidderCode', 'width', 'height', 'adUnitCode', 'statusMessage', 'requestId', 'mediaType', 'cpm']);
@@ -115,8 +102,9 @@ function sendAuctionEndEvent (event, eventType) {
   sendEvent(data, eventType);
 }
 function auctionInit (event) {
-  if (!topLocation && event && event.bidderRequests && event.bidderRequests.length > 0 && event.bidderRequests[0].refererInfo &&
-    event.bidderRequests[0].refererInfo.topmostLocation) topLocation = event.bidderRequests[0].refererInfo.topmostLocation
+  if (event?.bidderRequests?.length > 0 && event?.bidderRequests[0]?.refererInfo?.topmostLocation) {
+    nobidAnalytics.topLocation = event.bidderRequests[0].refererInfo.topmostLocation;
+  }
 }
 let nobidAnalytics = Object.assign(adapter({url, analyticsType}), {
   track({ eventType, args }) {
@@ -144,17 +132,34 @@ let nobidAnalytics = Object.assign(adapter({url, analyticsType}), {
   }
 });
 
-// save the base class function
-nobidAnalytics.originEnableAnalytics = nobidAnalytics.enableAnalytics;
-// override enableAnalytics so we can get access to the config passed in from the page
-nobidAnalytics.enableAnalytics = function (config) {
-  initOptions.options = config.options;
-  if (!config.options.siteId) {
-    logError('NoBid Analytics - siteId parameter is not defined. Analytics won\'t work');
-    return;
+nobidAnalytics = {
+  ...nobidAnalytics,
+  originEnableAnalytics: nobidAnalytics.enableAnalytics, // save the base class function
+  enableAnalytics: function (config) { // override enableAnalytics so we can get access to the config passed in from the page
+    if (!config.options.siteId) {
+      logError('NoBid Analytics - siteId parameter is not defined. Analytics won\'t work');
+      return;
+    }
+    this.initOptions = config.options;
+    this.originEnableAnalytics(config); // call the base class function
+  },
+  retentionSeconds: RETENTION_SECONDS,
+  isExpired (data) {
+    return isExpired(data, this.retentionSeconds);
+  },
+  isAnalyticsDisabled () {
+    let stored = storage.getDataFromLocalStorage(ANALYTICS_DATA_NAME);
+    if (!isJson(stored)) return false;
+    stored = JSON.parse(stored);
+    if (this.isExpired(stored)) return false;
+    return stored.disabled;
+  },
+  processServerResponse (response) {
+    if (!isJson(response)) return;
+    const resp = JSON.parse(response);
+    storage.setDataInLocalStorage(ANALYTICS_DATA_NAME, JSON.stringify({ ...resp, ts: Date.now() }));
   }
-  nobidAnalytics.originEnableAnalytics(config); // call the base class function
-};
+}
 
 adapterManager.registerAnalyticsAdapter({
   adapter: nobidAnalytics,
@@ -162,32 +167,32 @@ adapterManager.registerAnalyticsAdapter({
   gvlid: GVLID
 });
 window.nobidCarbonizer = {
+  getStoredLocalData: function () {
+    return storage.getDataFromLocalStorage(ANALYTICS_DATA_NAME);
+  },
   isActive: function () {
     let stored = storage.getDataFromLocalStorage(ANALYTICS_DATA_NAME);
     if (!isJson(stored)) return false;
     stored = JSON.parse(stored);
-    if (isExpired(stored)) return false;
-    return stored.carbonizer_active;
+    if (isExpired(stored, nobidAnalytics.retentionSeconds)) return false;
+    return stored.carbonizer_active || false;
   },
-  carbonizeAdunits (adunits) {
-    if (this.isActive()) {
-      // 5% of the time do not block;
-      if (Math.floor(Math.random() * 101) <= 5) return;
-      adunits.forEach(adunit => {
-        this.carbonizeAdunit(adunit);
-      });
-    }
-  },
-  carbonizeAdunit (adunit) {
-    if (this.isActive()) {
-      if (Math.floor(Math.random() * 101) <= TEST_ALLOCATION_PERCENTAGE) return;
+  carbonizeAdunits: function (adunits, skipTestGroup) {
+    function carbonizeAdunit (adunit) {
       let stored = storage.getDataFromLocalStorage(ANALYTICS_DATA_NAME);
       if (!isJson(stored)) return;
       stored = JSON.parse(stored);
-      if (isExpired(stored)) return;
+      if (isExpired(stored, nobidAnalytics.retentionSeconds)) return;
       const carbonizerBidders = stored.bidders || [];
       const allowedBidders = adunit.bids.filter(rec => carbonizerBidders.includes(rec.bidder));
       adunit.bids = allowedBidders;
+    }
+    if (this.isActive()) {
+      // 5% of the time do not block;
+      if (!skipTestGroup && Math.floor(Math.random() * 101) <= TEST_ALLOCATION_PERCENTAGE) return;
+      adunits.forEach(adunit => {
+        carbonizeAdunit(adunit);
+      });
     }
   }
 };
