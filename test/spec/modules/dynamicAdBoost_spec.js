ea1fec2edd0bea41537e054287afcf341dc46a55
adxpremium <55161519+adxpremium@users.noreply.github.com>
2023-08-24 12:53:49 +0200

    dynamicAdBoost: Fixed tests

--- test/spec/modules/dynamicAdBoost_spec.js
+++ test/spec/modules/dynamicAdBoost_spec.js
@@ -1,27 +1,31 @@
-import { subModuleObj } from 'modules/dynamicAdBoost.js';
+import { init } from 'modules/dynamicAdBoost.js';
 import { loadExternalScript } from '../../../src/adloader.js';
 import { expect } from 'chai';
+import { config as sourceConfig } from 'src/config.js';
 
 const configWithParams = {
-  keyId: 'dynamic',
-  adUnits: ['gpt-123']
+  dynamicAdBoost: {
+    keyId: 'dynamic',
+    adUnits: ['gpt-123'],
+    threshold: 1
+  }
 };
-
-const configWithoutParams = {};
 
 describe('dynamicAdBoost', function() {
   describe('init', function() {
-    describe('initialize with expected params', function() {
-      it('successfully initialize with load script', function() {
-        expect(subModuleObj.init(configWithParams)).to.be.true;
-        expect(loadExternalScript.called).to.be.true;
-      })
-    });
-
     describe('initialize without expected params', function() {
       it('fails initalize when keyId is not present', function() {
-        expect(subModuleObj.init(configWithoutParams)).to.be.false;
+        expect(init()).to.be.false;
       })
     })
+
+    describe('initialize with expected params', function() {
+      it('successfully initialize with load script', function() {
+        sourceConfig.setConfig(configWithParams);
+        setTimeout(function() {
+          expect(loadExternalScript.called).to.be.true;
+        }, 5000);
+      })
+    });
   });
 })
