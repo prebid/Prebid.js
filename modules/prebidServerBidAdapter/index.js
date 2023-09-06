865a304beb702abd2a37d95b843ff5421c894a5e
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2023-09-06 19:19:11 +0530

    Identation fix

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -292,9 +292,9 @@ function queueSync(bidderCodes, gdprConsent, uspConsent, gppConsent, s2sConfig) 
 function replaceSyncEndpointMacros(url, extSyncMacros) {
   for (const macro in extSyncMacros) {
     if (extSyncMacros.hasOwnProperty(macro)) {
-        const replacement = extSyncMacros[macro];
-        const macroPattern = new RegExp(`{{${macro}}}`, 'g');
-        url = url.replace(macroPattern, replacement);
+      const replacement = extSyncMacros[macro];
+      const macroPattern = new RegExp(`{{${macro}}}`, 'g');
+      url = url.replace(macroPattern, replacement);
     }
   }
   return url;
