1f77d16fd0f0e9cd11eff2d1bc665a6bd9fd41ab
Toshiyuki Oike <toto71117@gmail.com>
2023-09-15 18:29:00 +0900

    Delete replicated adUnit conversion processing

--- modules/fluctAnalyticsAdapter.js
+++ modules/fluctAnalyticsAdapter.js
@@ -19,7 +19,7 @@ const MODULE_NAME = 'fluctAnalyticsAdapter';
 const url = 'https://an.adingo.jp';
 
 /** AdUnit */
-/** @typedef {{ code: string, originalCode?: string, dwid: string, path?: string, analytics?: { bidder: string, dwid: string }[], bids: Bid[], mediaTypes: MediaTypes, transactionId: string, ortb2Imp?: Ortb2Imp }} AdUnit */
+/** @typedef {{ code: string, dwid: string, path?: string, analytics?: { bidder: string, dwid: string }[], bids: Bid[], mediaTypes: MediaTypes, transactionId: string, ortb2Imp?: Ortb2Imp }} AdUnit */
 /** @typedef {{ banner: { name: string, sizes?: [number, number][], sizeConfig?: { minViewPort: [number, number], sizes: [number, number][] }[] }, video: any }} MediaTypes */
 /** @typedef {{ ext: { data: { adserver?: { name: string, adSlot: string }, pbadslot: string } } }} Ortb2Imp */
 /** @typedef {{ [key: string]: string|number }[] } Params */
@@ -49,93 +49,6 @@ const cache = {
 };
 $$PREBID_GLOBAL$$.fluct = { cache: cache }; /** for debug */
 
-/** @type {(id: string) => boolean} */
-const isBrowsiId = (id) => Boolean(id.match(/^browsi_/g));
-
-/**
- * 各adUnitCodeに対応したadUnitPathを取得する
- * @type {() => {[divId: string]: string }}
- * @sample
- * ```
- * {
- *   'div-gpt-ad-1629864618640-0': '/62532913/p_fluctmagazine_320x50_surface_15377',
- *   'browsi_ad_0_ai_1_rc_0': '/62532913/p_fluctmagazine_320x50_surface_15377'
- * }
- * ```
- */
-const getAdUnitMap = () => window.googletag.pubads().getSlots().reduce((prev, slot) => Object.assign(prev, { [slot.getSlotElementId()]: slot.getAdUnitPath() }), {});
-
-/**
- * Browsiが新たに枠を生成してオークションを行った場合、その枠にはanalytics (各bidderのDWID情報) の情報が紐づかない
- * そのため、新たに生成された枠に対して、複製元の枠のanalytics情報をコピーする
- * @param {AdUnit} adUnit - 複製元の枠
- * @param {AdUnit[]} [adUnits=$$PREBID_GLOBAL$$.adUnits] - 複製元の枠を含む全ての枠
- * @param {{ [divId: string]: string }} [divIdToUnitPathMap=getAdUnitMap()] - 紐付けに使用する複製元の枠を含む全ての枠のdivIdとadUnitPathのマッピング
- * @returns {AdUnit | undefined} - 複製元の枠のanalytics情報をコピーした枠
- * */
-export const convertReplicatedAdUnit = (adUnit, adUnits = $$PREBID_GLOBAL$$.adUnits, divIdToUnitPathMap = getAdUnitMap()) => {
-  /** 既にanalytics情報が紐付けられている場合は何もしない */
-  if (adUnit.analytics) {
-    return adUnit;
-  }
-
-  /** `adUnit.analytics`が存在しない場合、GAMの広告ユニットのフルパス (adUnitPath) を用いて紐付けを行う */
-  const adUnitPath = divIdToUnitPathMap[adUnit.code];
-  if (!adUnitPath) {
-    logError({
-      message: `複製枠のdivId (${adUnit.code}) に対応するGAMユニットのフルパスを取得できません。`,
-      adUnitCode: adUnit.code,
-      module: MODULE_NAME,
-    });
-    return adUnit;
-  }
-
-    /** `複製枠と共通のadUnitPathを持つ配信設定を探す */
-  const originalAdUnit = find(adUnits, adUnit => adUnitPath.match(new RegExp(`${adUnit.path}$`)));
-  if (!originalAdUnit) {
-    logError({
-      message: `複製枠のadUnitPath (${adUnitPath}) を元に複製元の配信設定が取得できません。`,
-      replicationAdUnitCode: adUnit.code,
-      replicationAdUnitPath: adUnitPath,
-      module: MODULE_NAME,
-    });
-    return adUnit;
-  }
-
-
-  /** @type {AdUnit} */
-  const _adUnit = deepClone(adUnit);
-  try {
-    const { analytics, bids, code, mediaTypes: { banner: { name } } } = originalAdUnit;
-    _adUnit.analytics = analytics;
-    _adUnit.bids = bids;
-    _adUnit.originalCode = adUnit.code; /** 変換前: `browsi_ad_..` */
-    _adUnit.code = code;
-    _adUnit.mediaTypes.banner.name = name;
-  } catch (_error) {
-    logError({
-      message: '複製元枠のanalytics情報を複製枠に移し替える際に問題が発生しました。',
-      error: {
-        name: _error.name,
-        message: _error.message
-      },
-      adUnitCode: _adUnit.code,
-      adUnitPath,
-      module: MODULE_NAME,
-    });
-  }
-  return _adUnit;
-};
-
-/** @type {(event: { slot: { getSlotElementId: () => string }}) => void} */
-const browsiEventListener = (event) => {
-  const divId = event.slot.getSlotElementId();
-  if (isBrowsiId(divId)) {
-    const auction = find(Object.values(cache.auctions), auction => auction.adUnitCodes.every(adUnitCode => adUnitCode === divId));
-    auction && sendMessage(auction.auctionId);
-  }
-}
-
 let fluctAnalyticsAdapter = Object.assign(
   adapter({ url, analyticsType: 'endpoint' }), {
   track({ eventType, args }) {
@@ -147,7 +60,6 @@ let fluctAnalyticsAdapter = Object.assign(
           let auctionInitEvent = args;
           cache.auctions[auctionInitEvent.auctionId] = { ...auctionInitEvent, bids: {} };
           if (!cache.gpt.registered && isGptPubadsDefined()) {
-            window.googletag.pubads().addEventListener('slotOnload', browsiEventListener)
             cache.gpt.registered = true;
           }
           break;
@@ -177,7 +89,7 @@ let fluctAnalyticsAdapter = Object.assign(
           /** @type {AuctionEvent} */
           let auctionEndEvent = args;
           let { adUnitCodes, auctionId, bidsReceived, noBids } = auctionEndEvent;
-          Object.assign(cache.auctions[auctionId], auctionEndEvent, { aidSuffix: isBrowsiId(auctionId) ? generateUUID() : undefined });
+          Object.assign(cache.auctions[auctionId], auctionEndEvent);
 
           /** @type {string[]} */
           let prebidWonBidRequestIds = adUnitCodes.map(adUnitCode =>
@@ -207,10 +119,7 @@ let fluctAnalyticsAdapter = Object.assign(
           ];
           bidResults.forEach(bid => cache.auctions[auctionId].bids[bid.requestId || bid.bidId] = bid);
 
-          /** browsi auction の場合 `browsiEventListener` から送信する */
-          if (!isBrowsiId(auctionId)) {
-            sendMessage(auctionId);
-          }
+          sendMessage(auctionId);
           break;
         }
         case EVENTS.AD_RENDER_SUCCEEDED: {
@@ -224,15 +133,10 @@ let fluctAnalyticsAdapter = Object.assign(
             timeout: false,
           });
           /** オークション単位で `AD_RENDER_SUCCEEDED` イベントをまとめて送信する */
-          if (isBrowsiId(bid.auctionId)) {
-            /** 原則1オークション:1枠 */
+          clearTimeout(cache.timeouts[bid.auctionId]);
+          cache.timeouts[bid.auctionId] = setTimeout(() => {
             sendMessage(bid.auctionId);
-          } else {
-            clearTimeout(cache.timeouts[bid.auctionId]);
-            cache.timeouts[bid.auctionId] = setTimeout(() => {
-              sendMessage(bid.auctionId);
-            }, Math.min(config.getConfig('timeoutBuffer') || 400, 400));
-          }
+          }, Math.min(config.getConfig('timeoutBuffer') || 400, 400));
           break;
         }
         default:
@@ -247,11 +151,10 @@ let fluctAnalyticsAdapter = Object.assign(
 
 /** @type {(auctionId: string) => void} */
 const sendMessage = (auctionId) => {
-  let { adUnits, aidSuffix, bids } = cache.auctions[auctionId];
-  adUnits = adUnits.map(adUnit => convertReplicatedAdUnit(adUnit, $$PREBID_GLOBAL$$.adUnits));
+  let { adUnits, bids } = cache.auctions[auctionId];
 
   const payload = {
-    auctionId: aidSuffix ? `${auctionId}_${aidSuffix}` : auctionId,
+    auctionId: auctionId,
     bids: Object.values(bids).map(bid => {
       const {
         adserverTargeting,
@@ -271,7 +174,11 @@ const sendMessage = (auctionId) => {
         width
       } = bid;
 
-      const { analytics, code, originalCode, ortb2Imp } = find(adUnits, adUnit => [adUnit.originalCode, adUnit.code].includes(adUnitCode));
+      if (!dwid) {
+        logError(`[fluctAnalyticsAdapter] bid.dwid is undefined in adUnit ${adUnitCode}`, { bid, module: MODULE_NAME });
+      }
+
+      const { code, ortb2Imp } = find(adUnits, adUnit => adUnitCode === adUnit.code);
       return {
         adserverTargeting: {
           fbs_adid: adserverTargeting?.fbs_adid ?? null,
@@ -282,17 +189,16 @@ const sendMessage = (auctionId) => {
           fbs_size: adserverTargeting?.fbs_size ?? null,
           fbs_source: adserverTargeting?.fbs_source ?? null,
         },
-        adUnitCode: code,
+        adUnitCode,
         eidsSource: userIdAsEids?.map(userIdAsEid => userIdAsEid.source) ?? null,
         bidder,
         bidWon,
         cpm,
         creativeId,
-        dwid: dwid || (find(analytics, param => param.bidder === bidder) || {}).dwid,
+        dwid,
         height,
         netRevenue,
         noBid,
-        originalAdUnitCode: originalCode,
         pbadslot: ortb2Imp?.ext?.data?.pbadslot ?? null,
         prebidWon,
         timeout,
