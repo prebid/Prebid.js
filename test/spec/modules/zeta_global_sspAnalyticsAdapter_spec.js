378b301e7fba7c299e8e2c9c6312ace9e962add2
Aleksei Surovenko <asurovenko@zetaglobal.com>
2023-11-07 13:27:31 +0100

    unit test 2

--- test/spec/modules/zeta_global_sspAnalyticsAdapter_spec.js
+++ test/spec/modules/zeta_global_sspAnalyticsAdapter_spec.js
@@ -408,6 +408,268 @@ describe('Zeta Global SSP Analytics Adapter', function() {
       expect(requests.length).to.equal(2);
       const auctionEnd = JSON.parse(requests[0].requestBody);
       const auctionSucceeded = JSON.parse(requests[1].requestBody);
+      console.warn(auctionSucceeded);
+      expect(auctionEnd).to.be.deep.equal({
+        adUnitCodes: [
+          '/19968336/header-bid-tag-0'
+        ],
+        adUnits: [
+          {
+            code: '/19968336/header-bid-tag-0',
+            mediaTypes: {
+              banner: {
+                sizes: [
+                  [
+                    300,
+                    250
+                  ],
+                  [
+                    300,
+                    600
+                  ]
+                ]
+              }
+            },
+            bids: [
+              {
+                bidder: 'zeta_global_ssp',
+                params: {
+                  sid: 111,
+                  tags: {
+                    shortname: 'prebid_analytics_event_test_shortname',
+                    position: 'test_position'
+                  }
+                }
+              },
+              {
+                bidder: 'appnexus',
+                params: {
+                  placementId: 13232385
+                }
+              }
+            ],
+            sizes: [
+              [
+                300,
+                250
+              ],
+              [
+                300,
+                600
+              ]
+            ],
+            transactionId: '6b29369c-0c2e-414e-be1f-5867aec18d83'
+          }
+        ],
+        auctionEnd: 1638441234784,
+        auctionId: '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+        bidderRequests: [
+          {
+            'bidderCode': 'zeta_global_ssp',
+            'auctionId': '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+            'bidderRequestId': '1207cb49191887',
+            'bids': [
+              {
+                'bidder': 'zeta_global_ssp',
+                'mediaTypes': {
+                  'banner': {
+                    'sizes': [
+                      [
+                        300,
+                        250
+                      ],
+                      [
+                        300,
+                        600
+                      ]
+                    ]
+                  }
+                },
+                'adUnitCode': '/19968336/header-bid-tag-0',
+                'transactionId': '6b29369c-0c2e-414e-be1f-5867aec18d83',
+                'sizes': [
+                  [
+                    300,
+                    250
+                  ],
+                  [
+                    300,
+                    600
+                  ]
+                ],
+                'bidId': '206be9a13236af',
+                'bidderRequestId': '1207cb49191887',
+                'auctionId': '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+                'src': 'client',
+                'bidRequestsCount': 1,
+                'bidderRequestsCount': 1,
+                'bidderWinsCount': 0
+              }
+            ],
+            'auctionStart': 1638441234544,
+            'timeout': 400,
+            'refererInfo': {
+              'referer': 'http://test-zeta-ssp.net:63342/zeta-ssp/ssp/_dev/examples/page_banner.html',
+              'reachedTop': true,
+              'isAmp': false,
+              'numIframes': 0,
+              'stack': [
+                'http://test-zeta-ssp.net:63342/zeta-ssp/ssp/_dev/examples/page_banner.html'
+              ],
+              'canonicalUrl': null
+            },
+            'start': 1638441234547
+          },
+          {
+            'bidderCode': 'appnexus',
+            'auctionId': '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+            'bidderRequestId': '32b97f0a935422',
+            'bids': [
+              {
+                'bidder': 'appnexus',
+                'params': {
+                  'placementId': 13232385
+                },
+                'mediaTypes': {
+                  'banner': {
+                    'sizes': [
+                      [
+                        300,
+                        250
+                      ],
+                      [
+                        300,
+                        600
+                      ]
+                    ]
+                  }
+                },
+                'adUnitCode': '/19968336/header-bid-tag-0',
+                'transactionId': '6b29369c-0c2e-414e-be1f-5867aec18d83',
+                'sizes': [
+                  [
+                    300,
+                    250
+                  ],
+                  [
+                    300,
+                    600
+                  ]
+                ],
+                'bidId': '41badc0e164c758',
+                'bidderRequestId': '32b97f0a935422',
+                'auctionId': '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+                'src': 'client',
+                'bidRequestsCount': 1,
+                'bidderRequestsCount': 1,
+                'bidderWinsCount': 0
+              }
+            ],
+            'auctionStart': 1638441234544,
+            'timeout': 400,
+            'refererInfo': {
+              'referer': 'http://test-zeta-ssp.net:63342/zeta-ssp/ssp/_dev/examples/page_banner.html',
+              'reachedTop': true,
+              'isAmp': false,
+              'numIframes': 0,
+              'stack': [
+                'http://test-zeta-ssp.net:63342/zeta-ssp/ssp/_dev/examples/page_banner.html'
+              ],
+              'canonicalUrl': null
+            },
+            'start': 1638441234550
+          }
+        ],
+        bidsReceived: [
+          {
+            'bidderCode': 'zeta_global_ssp',
+            'width': 480,
+            'height': 320,
+            'statusMessage': 'Bid available',
+            'adId': '5759bb3ef7be1e8',
+            'requestId': '206be9a13236af',
+            'mediaType': 'banner',
+            'source': 'client',
+            'cpm': 2.258302852806723,
+            'currency': 'USD',
+            'ad': 'test_ad',
+            'ttl': 200,
+            'creativeId': '456456456',
+            'netRevenue': true,
+            'meta': {
+              'advertiserDomains': [
+                'viaplay.fi'
+              ]
+            },
+            'originalCpm': 2.258302852806723,
+            'originalCurrency': 'USD',
+            'auctionId': '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+            'responseTimestamp': 1638441234670,
+            'requestTimestamp': 1638441234547,
+            'bidder': 'zeta_global_ssp',
+            'adUnitCode': '/19968336/header-bid-tag-0',
+            'timeToRespond': 123,
+            'pbLg': '2.00',
+            'pbMg': '2.20',
+            'pbHg': '2.25',
+            'pbAg': '2.25',
+            'pbDg': '2.25',
+            'pbCg': '',
+            'size': '480x320',
+            'adserverTargeting': {
+              'hb_bidder': 'zeta_global_ssp',
+              'hb_adid': '5759bb3ef7be1e8',
+              'hb_pb': '2.20',
+              'hb_size': '480x320',
+              'hb_source': 'client',
+              'hb_format': 'banner',
+              'hb_adomain': 'viaplay.fi'
+            }
+          }
+        ],
+        noBids: [
+          {
+            'bidder': 'appnexus',
+            'params': {
+              'placementId': 13232385
+            },
+            'mediaTypes': {
+              'banner': {
+                'sizes': [
+                  [
+                    300,
+                    250
+                  ],
+                  [
+                    300,
+                    600
+                  ]
+                ]
+              }
+            },
+            'adUnitCode': '/19968336/header-bid-tag-0',
+            'transactionId': '6b29369c-0c2e-414e-be1f-5867aec18d83',
+            'sizes': [
+              [
+                300,
+                250
+              ],
+              [
+                300,
+                600
+              ]
+            ],
+            'bidId': '41badc0e164c758',
+            'bidderRequestId': '32b97f0a935422',
+            'auctionId': '75e394d9-ccce-4978-9238-91e6a1ac88a1',
+            'src': 'client',
+            'bidRequestsCount': 1,
+            'bidderRequestsCount': 1,
+            'bidderWinsCount': 0
+          }
+        ],
+        winningBids: []
+      });
 
       expect(auctionSucceeded).to.be.deep.equal({
         adId: '5759bb3ef7be1e8',
