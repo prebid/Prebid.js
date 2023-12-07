2dd64ea68aa8258b3d5e16534efd74f9f718ea51
Demetrio Girardi <dgirardi@prebid.org>
2023-12-07 09:56:18 -0800

    native rendering and messages

--- creative/renderers/native/renderer.js
+++ creative/renderers/native/renderer.js
@@ -1,9 +1,89 @@
-export function getTemplate(nativeData, win) {
+import {
+  ACTION_IMP,
+  ACTION_RESIZE,
+  EVENT_AD_RENDER_FAILED,
+  EVENT_AD_RENDER_SUCCEEDED,
+  FAILURE_REASON_EXCEPTION,
+  MESSAGE_EVENT, MESSAGE_NATIVE,
+  ORTB_ASSETS
+} from './constants.js';
 
+export function getReplacements(adId, {assets = [], ortb, nativeKeys = {}}) {
+  const assetValues = Object.fromEntries((assets).map(({key, value}) => [key, value]));
+  const repl = Object.fromEntries(
+    Object.entries(nativeKeys).flatMap(([name, key]) => {
+      const value = assetValues.hasOwnProperty(name) ? assetValues[name] : undefined;
+      return [
+        [`##${key}##`, value],
+        [`${key}:${adId}`, value]
+      ]
+    })
+  );
+  if (ortb) {
+    Object.assign(repl,
+      {
+        '##hb_native_linkurl##': ortb.link?.url,
+        '##hb_native_privacy##': ortb.privacy
+      },
+      Object.fromEntries(
+        (ortb.assets || []).flatMap(asset => {
+          const type = Object.keys(ORTB_ASSETS).find(type => asset[type]);
+          return [
+            type && [`##hb_native_asset_id_${asset.id}##`, asset[type][ORTB_ASSETS[type]]],
+            asset.link?.url && [`##hb_native_asset_link_id_${asset.id}##`, asset.link.url]
+          ].filter(e => e);
+        })
+      )
+    );
+  }
+  return repl;
 }
 
-export function render({native}, {sendMessage}, win) {
+export function replace(template, replacements) {
+  return Object.entries(replacements)
+    .concat([[/##hb_native_asset_(link_)?id_\d+##/g]])
+    .reduce((text, [pattern, value]) => text.replaceAll(pattern, value || ''), template);
+}
 
+function loadScript(url, win) {
+  return new Promise((resolve, reject) => {
+    const script = win.document.createElement('script');
+    script.onload = resolve;
+    script.onerror = reject;
+    script.src = url;
+    win.document.head.appendChild(script);
+  })
+}
+
+export function getAdMarkup(adId, nativeData, win, load = loadScript) {
+  const {rendererUrl, assets, ortb, adTemplate} = nativeData;
+  if (rendererUrl) {
+    delete win.renderAd; // renderAd from renderer may clash with renderAd defined in creative
+    return load(rendererUrl, win).then(() => {
+      if (typeof win.renderAd !== 'function') {
+        throw new Error(`Renderer from '${rendererUrl}' does not define renderAd()`)
+      }
+      const payload = assets || [];
+      payload.ortb = ortb;
+      return win.renderAd(payload);
+    });
+  } else {
+    return Promise.resolve(replace(adTemplate ?? win.document.body.innerHTML, getReplacements(adId, nativeData)))
+  }
+}
+
+export function render({adId, native}, {sendMessage}, win, getMarkup = getAdMarkup) {
+  const resize = () => sendMessage(MESSAGE_NATIVE, {
+    action: ACTION_RESIZE,
+    height: win.document.body.clientHeight || win.document.body.offsetHeight,
+    width: win.document.body.clientWidth
+  });
+  return getMarkup(adId, native, win).then(markup => {
+    win.document.body.innerHTML = markup;
+    sendMessage(MESSAGE_EVENT, {event: EVENT_AD_RENDER_SUCCEEDED});
+    sendMessage(MESSAGE_NATIVE, {action: ACTION_IMP});
+    win.document.readyState === 'complete' ? resize() : win.onload = resize;
+  }).catch(e => sendMessage(MESSAGE_EVENT, {event: EVENT_AD_RENDER_FAILED, info: {reason: FAILURE_REASON_EXCEPTION, message: e?.message}}));
 }
 
 window.render = render;
--- test/spec/creative/nativeRenderer_spec.js
+++ test/spec/creative/nativeRenderer_spec.js
@@ -1,10 +1,231 @@
+import {getAdMarkup, getReplacements, replace} from '../../../creative/renderers/native/renderer.js';
+import {
+  ACTION_IMP,
+  ACTION_RESIZE, EVENT_AD_RENDER_FAILED,
+  EVENT_AD_RENDER_SUCCEEDED,
+  FAILURE_REASON_EXCEPTION,
+  MESSAGE_EVENT, MESSAGE_NATIVE
+} from '../../../creative/renderers/native/constants.js';
+
 describe('Native creative renderer', () => {
   let win;
   beforeEach(() => {
-    win = {}
-  })
-  describe('getTemplate', () => {
-    it('uses adTemplate if defined', () => {
+    win = {};
+  });
+
+  describe('getAdMarkup', () => {
+    let loadScript;
+    beforeEach(() => {
+      loadScript = sinon.stub();
+    });
+    it('uses rendererUrl if present', () => {
+      const data = {
+        assets: ['1', '2'],
+        ortb: 'ortb',
+        rendererUrl: 'renderer'
+      };
+      const renderAd = sinon.stub().returns('markup');
+      loadScript.returns(Promise.resolve().then(() => {
+        win.renderAd = renderAd;
+      }));
+      return getAdMarkup('123', data, win, loadScript).then((markup) => {
+        expect(markup).to.eql('markup');
+        sinon.assert.calledWith(loadScript, data.rendererUrl, win);
+        sinon.assert.calledWith(renderAd, sinon.match(arg => {
+          expect(arg).to.have.members(data.assets);
+          expect(arg.ortb).to.eql(data.ortb);
+          return true;
+        }));
+      });
+    });
+
+    it('does not use window.renderAd that does not come from rendererUrl', () => {
+      win.renderAd = sinon.stub();
+      loadScript.returns(Promise.resolve());
+      return getAdMarkup('123', {rendererUrl: 'renderer.com'}, win, loadScript)
+        // eslint-disable-next-line prefer-promise-reject-errors
+        .then(() => {
+          throw new Error('should not resolve');
+        })
+        .catch((e) => expect(e.message).to.contain('renderer.com'));
+    });
+  });
+
+  describe('getReplacements', () => {
+    it('generates empty entries for missing assets', () => {
+      const repl = getReplacements('123', {
+        nativeKeys: {
+          'k': 'hb_native_k'
+        }
+      });
+      expect(repl).to.eql({
+        '##hb_native_k##': undefined,
+        'hb_native_k:123': undefined
+      });
+    });
+    it('generates entries for legacy assets', () => {
+      const repl = getReplacements('123', {
+        assets: [
+          {key: 'k1', value: 'v1'}, {key: 'k2', value: 'v2'}
+        ],
+        nativeKeys: {
+          k1: 'hb_native_k1',
+          k2: 'hb_native_k2'
+        }
+      });
+      expect(repl).to.eql({
+        '##hb_native_k1##': 'v1',
+        'hb_native_k1:123': 'v1',
+        '##hb_native_k2##': 'v2',
+        'hb_native_k2:123': 'v2'
+      });
+    });
+
+    describe('ORTB response top-level (non-asset) fields', () => {
+      const ortb = {
+        link: {
+          url: 'link.url'
+        },
+        privacy: 'privacy.url'
+      };
+      const expected = {
+        '##hb_native_linkurl##': 'link.url',
+        '##hb_native_privacy##': 'privacy.url'
+      };
+      it('generate entries', () => {
+        const repl = getReplacements('123', {
+          ortb
+        });
+        sinon.assert.match(repl, expected);
+      });
+      it('take precedence over legacy counterparts', () => {
+        const repl = getReplacements('123', {
+          ortb,
+          assets: [
+            {key: 'clickUrl', value: 'overridden'},
+            {key: 'privacyLink', value: 'overridden'}
+          ],
+          nativeKeys: {
+            clickUrl: 'hb_native_linkurl',
+            privacyLink: 'hb_native_privacy'
+          }
+        });
+        sinon.assert.match(repl, expected);
+      });
+      it('generate empty entries', () => {
+        const repl = getReplacements('123', {
+          ortb: {}
+        });
+        expect(repl).to.eql({
+          '##hb_native_linkurl##': undefined,
+          '##hb_native_privacy##': undefined,
+        });
+      });
+    });
+
+    Object.entries({
+      title: {text: 'val'},
+      data: {value: 'val'},
+      img: {url: 'val'},
+      video: {vasttag: 'val'}
+    }).forEach(([type, contents]) => {
+      describe(`for ortb ${type} asset`, () => {
+        let ortb;
+        beforeEach(() => {
+          ortb = {
+            assets: [
+              {
+                id: 123,
+                [type]: contents
+              }
+            ]
+          };
+        });
+        it('generates entries', () => {
+          const repl = getReplacements('', {ortb});
+          expect(repl['##hb_native_asset_id_123##']).to.eql('val');
+        });
+        it('generates link entries', () => {
+          ortb.assets[0].link = {url: 'link'};
+          const repl = getReplacements('', {ortb});
+          expect(repl['##hb_native_asset_link_id_123##']).to.eql('link');
+        });
+      });
+    });
+  });
+
+  describe('replace', () => {
+    it('replaces values', () => {
+      const result = replace('pre?middle?post', {'?': '.'});
+      expect(result).to.eql('pre.middle.post');
+    });
+    it('defaults to empty string', () => {
+      const result = replace('pre?post', {'?': undefined});
+      expect(result).to.eql('prepost');
+    });
+    it('uses empty string for missing asset id placeholders', () => {
+      const repl = {
+        '##hb_native_asset_id_1##': 'v1',
+        '##hb_native_asset_link_id_1##': 'l1'
+      };
+      const template = '.##hb_native_asset_id_1##.##hb_native_asset_id_2##.##hb_native_asset_link_id_1##.##hb_native_asset_link_id_2##';
+      expect(replace(template, repl)).to.eql('.v1..l1.');
+    });
+  });
+
+  describe('render', () => {
+    let getMarkup, sendMessage, adId, nativeData;
+    beforeEach(() => {
+      adId = '123';
+      nativeData = {}
+      getMarkup = sinon.stub();
+      sendMessage = sinon.stub()
+      win.document = {
+        body: {}
+      }
+    });
+
+    function runRender() {
+      return render({adId, native: nativeData}, {sendMessage}, win, getMarkup)
+    }
+
+    it('drops markup on body, and emits AD_RENDER_SUCCEEDED and fires imp trackers', () => {
+      getMarkup.returns(Promise.resolve('markup'));
+      return runRender().then(() => {
+        expect(win.document.body.innerHTML).to.eql('markup');
+        sinon.assert.calledWith(sendMessage, MESSAGE_EVENT, {event: EVENT_AD_RENDER_SUCCEEDED});
+        sinon.assert.calledWith(sendMessage, MESSAGE_NATIVE, {action: ACTION_IMP});
+      })
+    });
+
+    it('emits AD_RENDER_FAILED on error', () => {
+      getMarkup.returns(Promise.reject(new Error('failure')));
+      return runRender().then(() => {
+        sinon.assert.calledWith(sendMessage, MESSAGE_EVENT, {event: EVENT_AD_RENDER_FAILED, info: {reason: FAILURE_REASON_EXCEPTION, message: 'failure'}})
+      })
+    });
+
+    describe('requests resize', () => {
+      beforeEach(() => {
+        getMarkup.returns(Promise.resolve('markup'));
+        win.document.body.clientHeight = 123;
+        win.document.body.clientWidth = 321;
+      });
+
+      it('immediately, if document is loaded', () => {
+        win.document.readyState = 'complete';
+        return runRender().then(() => {
+          sinon.assert.calledWith(sendMessage, MESSAGE_NATIVE, {action: ACTION_RESIZE, height: 123, width: 321})
+        })
+      });
+
+      it('on document load otherwise', () => {
+        return runRender().then(() => {
+          sinon.assert.neverCalledWith(sendMessage, MESSAGE_NATIVE, sinon.match({action: ACTION_RESIZE}));
+          win.onload();
+          sinon.assert.calledWith(sendMessage, MESSAGE_NATIVE, {action: ACTION_RESIZE, height: 123, width: 321});
+        })
+      })
     })
   })
-})
+});
