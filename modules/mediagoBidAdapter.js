0f8378074b5b020418c0833fcf88058f7dfefba1
Yimilh <1012150927@qq.com>
2024-01-10 16:40:45 +0800

    MediaGo Bid Adapter : update buyerId and add  pmguid，title, description and keywords.

--- modules/mediagoBidAdapter.js
+++ modules/mediagoBidAdapter.js
@@ -11,7 +11,7 @@ import { registerBidder } from '../src/adapters/bidderFactory.js';
 const BIDDER_CODE = 'mediago';
 // const PROTOCOL = window.document.location.protocol;
 const ENDPOINT_URL = 'https://gbid.mediago.io/api/bid?tn=';
-const COOKY_SYNC_URL = 'https://gtrace.mediago.io/ju/cs/eplist';
+// const COOKY_SYNC_URL = 'https://gtrace.mediago.io/ju/cs/eplist';
 const COOKY_SYNC_IFRAME_URL = 'https://cdn.mediago.io/js/cookieSync.html';
 
 const TIME_TO_LIVE = 500;
