b82fe1bd70e1167b43eb644ba9110c4c93377d3d
pm-azhar-mulla <azhar@L1119.local>
2024-01-15 14:54:52 +0530

    Added test cases

--- test/spec/modules/pubmaticBidAdapter_spec.js
+++ test/spec/modules/pubmaticBidAdapter_spec.js
@@ -1922,6 +1922,15 @@ describe('PubMatic adapter', function () {
           expect(data.user.yob).to.equal(1985);
         });
 
+        it('ortb2.badv should be merged in the request', function() {
+          const ortb2 = {
+            badv: ['example.com']
+          };
+          const request = spec.buildRequests(bidRequests, {ortb2});
+          let data = JSON.parse(request.data);
+          expect(data.badv).to.deep.equal(['example.com']);
+        });
+
         describe('ortb2Imp', function() {
           describe('ortb2Imp.ext.gpid', function() {
             beforeEach(function () {
