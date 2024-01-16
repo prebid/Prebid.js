32811268f348e3e03d8dd03a9ec7a969bdba5e48
Peixun Zhang <max317backup@gmail.com>
2024-01-16 18:12:55 +0100

    ext field

--- libraries/pubcidEids/pubcidEids.js
+++ libraries/pubcidEids/pubcidEids.js
@@ -1,6 +1,11 @@
 export const PUBCID_EIDS = {
   'pubcid': {
     source: 'pubcid.org',
-    atype: 1
+    atype: 1,
+    getUidExt: function(data) {
+      if (data.ext) {
+        return data.ext;
+      }
+    }
   }
 }
