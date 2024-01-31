e12df4fc2eb5d00fcf7d2f463b45987ef88892e1
adam <L12354*com>
2024-01-31 11:50:36 +0800

    Merge branch 'prebid:master' into master
    Signed-off-by: adam <L12354*。com>

--- modules/zmaticooBidAdapter.js
+++ modules/zmaticooBidAdapter.js
@@ -1,6 +1,6 @@
-import {deepAccess, logWarn} from '../src/utils.js';
+import {deepAccess, isArray, isBoolean, isNumber, isStr, logWarn} from '../src/utils.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
-import {BANNER} from '../src/mediaTypes.js';
+import {BANNER, VIDEO} from '../src/mediaTypes.js';
 
 const BIDDER_CODE = 'zmaticoo';
 const ENDPOINT_URL = 'https://bid.zmaticoo.com/prebid/bid';
@@ -8,9 +8,35 @@ const DEFAULT_CUR = 'USD';
 const TTL = 200;
 const NET_REV = true;
 
+const DATA_TYPES = {
+  'NUMBER': 'number',
+  'STRING': 'string',
+  'BOOLEAN': 'boolean',
+  'ARRAY': 'array',
+  'OBJECT': 'object'
+};
+const VIDEO_CUSTOM_PARAMS = {
+  'mimes': DATA_TYPES.ARRAY,
+  'minduration': DATA_TYPES.NUMBER,
+  'maxduration': DATA_TYPES.NUMBER,
+  'startdelay': DATA_TYPES.NUMBER,
+  'playbackmethod': DATA_TYPES.ARRAY,
+  'api': DATA_TYPES.ARRAY,
+  'protocols': DATA_TYPES.ARRAY,
+  'w': DATA_TYPES.NUMBER,
+  'h': DATA_TYPES.NUMBER,
+  'battr': DATA_TYPES.ARRAY,
+  'linearity': DATA_TYPES.NUMBER,
+  'placement': DATA_TYPES.NUMBER,
+  'plcmt': DATA_TYPES.NUMBER,
+  'minbitrate': DATA_TYPES.NUMBER,
+  'maxbitrate': DATA_TYPES.NUMBER,
+  'skip': DATA_TYPES.NUMBER
+}
+
 export const spec = {
   code: BIDDER_CODE,
-  supportedMediaTypes: [BANNER],
+  supportedMediaTypes: [BANNER, VIDEO],
 
   /**
    * Determines whether or not the given bid request is valid.
@@ -48,19 +74,53 @@ export const spec = {
     const secure = 1;
     const request = validBidRequests[0];
     const params = request.params;
-    let impData = {
-      id: request.bidId,
-      secure: secure,
-      banner: buildBanner(request),
-      ext: {
-        bidder: {
-          pubId: params.pubId
+    const imps = validBidRequests.map(request => {
+      const impData = {
+        id: request.bidId,
+        secure: secure,
+        ext: {
+          bidder: {
+            pubId: params.pubId
+          }
+        }
+      };
+      if (params.tagid) {
+        impData.tagid = params.tagid;
+      }
+      if (request.mediaTypes) {
+        for (const mediaType in request.mediaTypes) {
+          switch (mediaType) {
+            case BANNER:
+              impData.banner = buildBanner(request);
+              break;
+            case VIDEO:
+              impData.video = buildVideo(request);
+              break;
+          }
         }
       }
-    };
+      if (!impData.banner && !impData.video) {
+        impData.banner = buildBanner(request);
+      }
+
+      if (typeof request.getFloor === 'function') {
+        const floorInfo = request.getFloor({
+          currency: 'USD',
+          mediaType: impData.video ? 'video' : 'banner',
+          size: [impData.video ? impData.video.w : impData.banner.w, impData.video ? impData.video.h : impData.banner.h]
+        });
+        if (floorInfo && floorInfo.floor) {
+          impData.bidfloor = floorInfo.floor;
+        }
+      }
+      if (!impData.bidfloor && params.bidfloor) {
+        impData.bidfloor = params.bidfloor;
+      }
+      return impData;
+    });
     let payload = {
       id: bidderRequest.bidderRequestId,
-      imp: [impData],
+      imp: imps,
       site: params.site ? params.site : {},
       app: params.app ? params.app : {},
       device: params.device ? params.device : {},
@@ -103,28 +163,36 @@ export const spec = {
    * Unpack the response from the server into a list of bids.
    *
    * @param {ServerResponse} serverResponse A successful response from the server.
-   * @param bidRequest The payload from the server's response.
+   * @param {BidRequest} bidRequest The payload from the server's response.
    * @return {Bid[]} An array of bids which were nested inside the server.
    */
   interpretResponse: function (serverResponse, bidRequest) {
-    let bidResponse = [];
-    if (Object.keys(serverResponse.body).length !== 0) {
-      let zresponse = serverResponse.body;
-      let zbid = zresponse.seatbid[0].bid[0];
-      let bid = {
-        requestId: zbid.impid,
-        cpm: zbid.price,
-        currency: zbid.cur,
-        width: zbid.w,
-        height: zbid.h,
-        ad: zbid.adm,
-        ttl: TTL,
-        creativeId: zbid.crid,
-        netRevenue: NET_REV
-      };
-      bidResponse.push(bid);
+    let bidResponses = [];
+    const response = (serverResponse || {}).body;
+    if (response && response.seatbid && response.seatbid[0].bid && response.seatbid[0].bid.length) {
+      response.seatbid.forEach(zmSeatbid => {
+        zmSeatbid.bid.forEach(zmBid => {
+          let bid = {
+            requestId: zmBid.impid,
+            cpm: zmBid.price,
+            currency: response.cur,
+            width: zmBid.w,
+            height: zmBid.h,
+            ad: zmBid.adm,
+            ttl: TTL,
+            creativeId: zmBid.crid,
+            netRevenue: NET_REV,
+          };
+          if (zmBid.adomain && zmBid.adomain.length) {
+            bid.meta = {
+              advertiserDomains: zmBid.adomain
+            };
+          }
+          bidResponses.push(bid);
+        })
+      })
     }
-    return bidResponse;
+    return bidResponses;
   }
 }
 
@@ -136,6 +204,49 @@ function buildBanner(request) {
   return {
     w: sizes[0][0], h: sizes[0][1]
   };
+}
+
+function buildVideo(request) {
+  let video = {};
+  const videoParams = deepAccess(request, 'mediaTypes.video', {});
+  for (const key in VIDEO_CUSTOM_PARAMS) {
+    if (videoParams.hasOwnProperty(key)) {
+      video[key] = checkParamDataType(key, videoParams[key], VIDEO_CUSTOM_PARAMS[key]);
+    }
+  }
+  if (videoParams.playerSize) {
+    if (isArray(videoParams.playerSize[0])) {
+      video.w = parseInt(videoParams.playerSize[0][0], 10);
+      video.h = parseInt(videoParams.playerSize[0][1], 10);
+    } else if (isNumber(videoParams.playerSize[0])) {
+      video.w = parseInt(videoParams.playerSize[0], 10);
+      video.h = parseInt(videoParams.playerSize[1], 10);
+    }
+  }
+  return video;
+}
+
+function checkParamDataType(key, value, datatype) {
+  let functionToExecute;
+  switch (datatype) {
+    case DATA_TYPES.BOOLEAN:
+      functionToExecute = isBoolean;
+      break;
+    case DATA_TYPES.NUMBER:
+      functionToExecute = isNumber;
+      break;
+    case DATA_TYPES.STRING:
+      functionToExecute = isStr;
+      break;
+    case DATA_TYPES.ARRAY:
+      functionToExecute = isArray;
+      break;
+  }
+  if (functionToExecute(value)) {
+    return value;
+  }
+  logWarn('Ignoring param key: ' + key + ', expects ' + datatype + ', found ' + typeof value);
+  return undefined;
 }
 
 registerBidder(spec);
