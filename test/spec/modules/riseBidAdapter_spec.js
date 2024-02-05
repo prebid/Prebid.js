f0920e5bf51b499da2483a52ed39d94835340ebb
Inna Yaretsky <>
2024-02-05 12:25:13 +0200

    updated rise test

--- test/spec/modules/riseBidAdapter_spec.js
+++ test/spec/modules/riseBidAdapter_spec.js
@@ -333,10 +333,10 @@ describe('riseAdapter', function () {
     });
 
     it('should send the gpp param if gppConsent is true in the bidRequest', function () {
-      const bidderRequestWithGPP = Object.assign({gppConsent: {gppString: 'test-consent-string', applicableSections: [7]}}, bidderRequest);
+      const bidderRequestWithGPP = Object.assign({gppConsent: {gppString: 'gpp-consent', applicableSections: [7]}}, bidderRequest);
       const request = spec.buildRequests(bidRequests, bidderRequestWithGPP);
       expect(request.data.params).to.be.an('object');
-      expect(request.data.params).to.have.property('gpp', 'test-consent-string');
+      expect(request.data.params).to.have.property('gpp', 'gpp-consent');
       expect(request.data.params.gpp_sid).to.be.equal([7]);
     });
 
