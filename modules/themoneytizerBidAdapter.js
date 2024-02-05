73ccb50683a59c75857131d4ebb4e4feb432b336
themoneytizer <tech@themoneytizer.com>
2024-02-05 09:28:32 -0500

    fix(themoneytizerBidAdapter): fix import mediatypes

--- modules/themoneytizerBidAdapter.js
+++ modules/themoneytizerBidAdapter.js
@@ -1,5 +1,5 @@
 import { logInfo, logWarn } from '../src/utils.js';
-import { BANNER, VIDEO, NATIVE } from '../src/mediaTypes.js';
+import { BANNER } from '../src/mediaTypes.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 
 const BIDDER_CODE = 'themoneytizer';
