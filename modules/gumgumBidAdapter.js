a2d35e613c084d11b517ae04afdea8f816962a71
Martin Altunyan <martin.altunyan@gumgum.com>
2024-02-06 10:47:18 -0800

    Fix  multi-size for in - slot

--- modules/gumgumBidAdapter.js
+++ modules/gumgumBidAdapter.js
@@ -544,7 +544,7 @@ function interpretResponse(serverResponse, bidRequest) {
     sizes = [`${maxw}x${maxh}`];
   } else if (product === 5 && includes(sizes, '1x1')) {
     sizes = ['1x1'];
-  } else if ((product === 2 && includes(sizes, '1x1')) || product === 3){
+  } else if ((product === 2 && includes(sizes, '1x1')) || product === 3) {
     const requestSizesThatMatchResponse = (bidRequest.sizes && bidRequest.sizes.reduce((result, current) => {
       const [ width, height ] = current;
       if (responseWidth === width && responseHeight === height) result.push(current.join('x'));
