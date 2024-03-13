8440764ec61c5f6871f6d90c484cd2002866302f
Shashank <=>
2024-03-13 16:24:42 +0530

    fix: removed changes to gulp file and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -32,11 +32,6 @@ const INTEG_SERVER_HOST = argv.host ? argv.host : 'localhost';
 const INTEG_SERVER_PORT = 4444;
 const { spawn, fork } = require('child_process');
 const TerserPlugin = require('terser-webpack-plugin');
-const s3Config = {
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-}
-var s3 = require('gulp-s3-upload')(s3Config);
 
 // these modules must be explicitly listed in --modules to be included in the build, won't be part of "all" modules
 var explicitModules = [
@@ -478,17 +473,6 @@ gulp.task('test-coverage', gulp.series(clean, testCoverage));
 gulp.task(viewCoverage);
 
 gulp.task('coveralls', gulp.series('test-coverage', coveralls));
-gulp.task('upload-to-s3', () => gulp.src('build/dist/' + argv.bundleName)
-  .pipe(s3({
-    keyTransform: function(relative_filename) {
-      return 'yetijs/' + relative_filename;
-    },
-    Bucket: process.env.PREBID_S3_UPLOAD_BUCKET_NAME,
-  }, {
-    maxRetries: 5
-  })
-));
-gulp.task('build', gulp.series(clean, 'build-bundle-prod', 'build-creative', 'upload-to-s3'));
 gulp.task('build-postbid', gulp.series(escapePostbidConfig, buildPostbid));
 
 gulp.task('serve', gulp.series(clean, lint, gulp.parallel('build-bundle-dev', watch, test)));
