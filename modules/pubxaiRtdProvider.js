f32ebdd9ba882ba3502cda0efe6b799f802fb51c
Pruthvi Muga <pruthvi@pubx.ai>
2024-03-23 20:29:43 +0530

    * Remove coonsole.log * move __pubxFloorRulesPromise__ to window * Remove unused var error

--- modules/pubxaiRtdProvider.js
+++ modules/pubxaiRtdProvider.js
@@ -2,11 +2,11 @@ import { ajax } from "../src/ajax.js";
 import { config } from "../src/config.js";
 import { submodule } from "../src/hook.js";
 import { deepAccess } from "../src/utils.js";
-import { createFloorsDataForAuction } from "./priceFloors.js";
+import { createFloorsDataForAuction } from "./priceFloors.js"; // eslint-disable-line prebid/validate-imports
 
 const MODULE_NAME = "realTimeData";
 const SUBMODULE_NAME = "pubxai";
-let floorRulesPromise = null;
+window.__pubxFloorRulesPromise__ = null;
 const FloorsApiStatus = Object.freeze({
   IN_PROGRESS: "IN_PROGRESS",
   SUCCESS: "SUCCESS",
@@ -15,7 +15,6 @@ const FloorsApiStatus = Object.freeze({
 const FLOORS_EVENT_HANDLE = "floorsApi";
 
 export const getFloorsConfig = (provider, floorsResponse) => {
-  console.log("pubx", "getFloorsConfig called");
   const floorsConfig = {
     floors: {
       enforcement: { floorDeals: true },
@@ -33,7 +32,6 @@ export const getFloorsConfig = (provider, floorsResponse) => {
 };
 
 export const setFloorsConfig = (provider, data) => {
-  console.log("pubx", "setFloorsConfig called");
   if (data) {
     const floorsConfig = getFloorsConfig(provider, data);
     config.setConfig(floorsConfig);
@@ -44,18 +42,14 @@ export const setFloorsConfig = (provider, data) => {
     window.__pubxLoaded__ = false;
     window.__pubxFloorsConfig__ = null;
   }
-  console.log("pubx", "loaded", window.__pubxLoaded__);
-  console.log("pubx", "floors config", window.__pubxFloorsConfig__);
 };
 
 export const setDefaultPriceFloors = (provider) => {
-  console.log("pubx", "setDefaultFloorsConfig called");
   const data = deepAccess(provider, "params.data");
   setFloorsConfig(provider, data);
 };
 
 export const setPriceFloors = async (provider) => {
-  console.log("pubx", "setPriceFloors called");
   window.__pubxPrevFloorsConfig__ = config.getConfig("floors");
   setDefaultPriceFloors(provider);
   return fetchFloorRules(provider)
@@ -63,14 +57,13 @@ export const setPriceFloors = async (provider) => {
       setFloorsConfig(provider, floorsResponse);
       setFloorsApiStatus(FloorsApiStatus.SUCCESS);
     })
-    .catch((error) => {
+    .catch((_) => {
       setFloorsApiStatus(FloorsApiStatus.ERROR);
     });
 };
 
 const setFloorsApiStatus = (status) => {
   window.__pubxFloorsApiStatus__ = status;
-  console.log("pubx", "api status set to", status);
   window.dispatchEvent(
     new CustomEvent(FLOORS_EVENT_HANDLE, { detail: { status } })
   );
@@ -78,32 +71,24 @@ const setFloorsApiStatus = (status) => {
 
 export const fetchFloorRules = async (provider) => {
   const url = deepAccess(provider, "params.endpoint");
-  console.log("pubx", "fetchFloorRules called", url);
   return new Promise((resolve, reject) => {
-    console.log("pubx", "initiate ajax call");
     setFloorsApiStatus(FloorsApiStatus.IN_PROGRESS);
     // When the api call exceeds auctionDelay, the api call doesn't fail but the auction starts.
-    // TODO: do we want to introduce a timeout for the api call?
     ajax(url, {
       success: (responseText, response) => {
-        console.log("pubx", "success response", response);
-        console.log("pubx", "success responseText", responseText);
         try {
           if (response && response.response) {
             // "content-type" = 'text/plain; charset=utf-8'
             const floorsResponse = JSON.parse(response.response);
-            console.log("pubx", "success floorsResponse", floorsResponse);
             resolve(floorsResponse);
           } else {
             resolve(null);
           }
         } catch (error) {
-          console.log("pubx", "error parsing success response", error);
           reject(error);
         }
       },
       error: (responseText, response) => {
-        console.log("pubx", "ajax error response", response);
         reject(response);
       },
     });
@@ -111,45 +96,28 @@ export const fetchFloorRules = async (provider) => {
 };
 
 const init = (provider) => {
-  console.log("pubx", "init called");
   const useRtd = deepAccess(provider, "params.useRtd");
   if (!useRtd) {
     return false;
   }
-  floorRulesPromise = setPriceFloors(provider);
+  window.__pubxFloorRulesPromise__ = setPriceFloors(provider);
   return true;
 };
 
 // TODO: is this called before every auction of before every bid request? Is it okay if it runs only once?
 const getBidRequestData = (() => {
   let floorsAttached = false;
-  return (reqBidsConfigObj, onDone, config) => {
+  return (reqBidsConfigObj, onDone) => {
     if (!floorsAttached) {
-      console.log("pubx", "getBidRequestData called", config);
       createFloorsDataForAuction(
         reqBidsConfigObj.adUnits,
         reqBidsConfigObj.auctionId
       );
-      floorRulesPromise.then(() => {
+      window.__pubxFloorRulesPromise__.then(() => {
         createFloorsDataForAuction(
           reqBidsConfigObj.adUnits,
           reqBidsConfigObj.auctionId
         );
-        // const myCustomData = {
-        //   pubxData: "myCustomData",
-        // };
-        // mergeDeep(reqBidsConfigObj.ortb2Fragments.global, myCustomData);
-        // reqBidsConfigObj.adUnits.forEach((adUnit) =>
-        //   mergeDeep(adUnit, "ortb2Imp.ext.bids", myCustomData)
-        // );
-        // reqBidsConfigObj.adUnits.forEach((adUnit) =>
-        //   adUnit.bids.forEach((bid) =>
-        //     mergeDeep(bid.floorData, {
-        //       skipRate: 50,
-        //       modelVersion: "server 274",
-        //     })
-        //   )
-        // );
         onDone();
       });
       floorsAttached = true;
@@ -168,4 +136,3 @@ export const beforeInit = () => {
 };
 
 beforeInit();
-console.log("pubx.ai:", "pubxaiRtdProvider module loaded");
