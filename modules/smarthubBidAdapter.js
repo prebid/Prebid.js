729f31cf662a849935f81405484d1c3adc9b6208
Victor <victor.banudz@smartyads.com>
2024-04-28 16:04:59 +0300

    Update adapter SmartHub

--- modules/smarthubBidAdapter.js
+++ modules/smarthubBidAdapter.js
@@ -4,6 +4,20 @@ import {BANNER, NATIVE, VIDEO} from '../src/mediaTypes.js';
 import {config} from '../src/config.js';
 
 const BIDDER_CODE = 'smarthub';
+const ALIASES = [{code: 'markapp', skipPbsAliasing: true}];
+const BASE_URLS = {
+  smarthub: 'https://prebid.smart-hub.io/pbjs',
+  markapp: 'https://markapp-prebid.smart-hub.io/pbjs'
+};
+
+function getUrl(partnerName) {
+  const aliases = ALIASES.map(el => el.code);
+  if (aliases.includes(partnerName)) {
+    return BASE_URLS[partnerName];
+  }
+
+  return `${BASE_URLS[BIDDER_CODE]}?partnerName=${partnerName}`;
+}
 
 function isBidResponseValid(bid) {
   if (!bid.requestId || !bid.cpm || !bid.creativeId || !bid.ttl || !bid.currency || !bid.hasOwnProperty('netRevenue')) {
@@ -22,13 +36,13 @@ function isBidResponseValid(bid) {
 }
 
 function getPlacementReqData(bid) {
-  const { params, bidId, mediaTypes } = bid;
+  const { params, bidId, mediaTypes, bidder } = bid;
   const schain = bid.schain || {};
   const { partnerName, seat, token, iabCat, minBidfloor, pos } = params;
   const bidfloor = getBidFloor(bid);
 
   const placement = {
-    partnerName: partnerName.toLowerCase(),
+    partnerName: String(partnerName || bidder).toLowerCase(),
     seat,
     token,
     iabCat,
@@ -36,7 +50,7 @@ function getPlacementReqData(bid) {
     pos,
     bidId,
     schain,
-    bidfloor
+    bidfloor,
   };
 
   if (mediaTypes && mediaTypes[BANNER]) {
@@ -130,11 +144,12 @@ function buildRequestParams(bidderRequest = {}, placements = []) {
 
 export const spec = {
   code: BIDDER_CODE,
+  aliases: ALIASES,
   supportedMediaTypes: [BANNER, VIDEO, NATIVE],
 
   isBidRequestValid: (bid = {}) => {
     const { params, bidId, mediaTypes } = bid;
-    let valid = Boolean(bidId && params && params.partnerName && params.seat && params.token);
+    let valid = Boolean(bidId && params && params.seat && params.token);
 
     if (mediaTypes && mediaTypes[BANNER]) {
       valid = valid && Boolean(mediaTypes[BANNER] && mediaTypes[BANNER].sizes);
@@ -163,7 +178,7 @@ export const spec = {
       const request = buildRequestParams(bidderRequest, tempObj[key]);
       return {
         method: 'POST',
-        url: `https://${key}-prebid.smart-hub.io/pbjs`,
+        url: getUrl(key),
         data: request,
       }
     });
