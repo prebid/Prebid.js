28bd4e80e6f8a43fb20d78723b3bf44334f0502c
anand-venkatraman <avenkatraman@pulsepoint.com>
2024-05-10 00:14:12 +0530

    fixing linting issue

--- modules/pulsepointBidAdapter.js
+++ modules/pulsepointBidAdapter.js
@@ -1,7 +1,6 @@
 import { ortbConverter } from '../libraries/ortbConverter/converter.js';
 import {isArray} from '../src/utils.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
-import {convertTypes} from '../libraries/transformParamsUtils/convertTypes.js';
 
 const DEFAULT_CURRENCY = 'USD';
 const KNOWN_PARAMS = ['cp', 'ct', 'cf', 'battr', 'deals'];
