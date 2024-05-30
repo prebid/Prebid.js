2f4be872190009a5a29052844ebda2afaffac362
Patrick McCann <patmmccann@gmail.com>
2024-05-30 11:51:10 -0400

    Update index.js

--- plugins/eslint/index.js
+++ plugins/eslint/index.js
@@ -3,6 +3,28 @@ const { flagErrors } = require('./validateImports.js');
 
 module.exports = {
   rules: {
+    'no-outerText': {
+      meta: {
+        docs: {
+          description: '.outerText property on DOM elements should not be used due to performance issues'
+        },
+        messages: {
+          noInnerText: 'Use of `.outerText` is not allowed. Use `.textContent` instead.',
+        }
+      },
+      create: function(context) {
+        return {
+          MemberExpression(node) {
+            if (node.property && node.property.name === 'outerText') {
+              context.report({
+                node: node.property,
+                messageId: 'noOuterText',
+              });
+            }
+          }
+        }
+      }
+    },
     'no-innerText': {
       meta: {
         docs: {
