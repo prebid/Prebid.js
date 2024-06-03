f45ecf7a03e04d504b03ddbb8206e0702af600f8
Patrick McCann <patmmccann@gmail.com>
2024-06-03 10:46:36 -0400

    Update adloader.js

--- src/adloader.js
+++ src/adloader.js
@@ -7,8 +7,9 @@ const _approvedLoadExternalJSList = [
   // Prebid maintained modules:
   'debugging',
   'outstream',
-  // Bid Modules:
+  // Bid Modules - only exception is on rendering edge cases, to clean up in Prebid 10:
   'improvedigital',
+  'showheroes-bs',
   // RTD modules:
   'aaxBlockmeter',
   'adagio',
