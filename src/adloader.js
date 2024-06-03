8af7077b9b2d41c654b57b61923e45544ed210e5
Demetrio Girardi <dgirardi@prebid.org>
2024-06-03 12:27:59 -0700

    Prebid 9: re-whitelist 'oustream' for loadExternalScript (#11671)
    * re-whitelist 'outstream' for loadExternalScript
    
    * Update adloader.js

--- src/adloader.js
+++ src/adloader.js
@@ -6,6 +6,7 @@ const _requestCache = new WeakMap();
 const _approvedLoadExternalJSList = [
   // Prebid maintained modules:
   'debugging',
+  'outstream',
   // Bid Modules - only exception is on rendering edge cases, to clean up in Prebid 10:
   'improvedigital',
   'showheroes-bs',
