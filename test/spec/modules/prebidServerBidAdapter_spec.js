0ee4efdfde777045bdf5e00539efcfb878ff22d6
Demetrio Girardi <dgirardi@prebid.org>
2024-06-03 05:43:25 -0700

    PAAPI: add support for protected audience extensions and "direct" buyers (`igb`) (#11277)
    * set ortb2Imp.ext.igs
    
    * PAAPI: add ortbConverter support for request ext.igi and response ext.igi.igs
    
    * Update paapi hook to accept auction config as one field
    
    * mergeBuyers
    
    * partitionBuyers
    
    * add fpd to perBuyerSignals
    
    * complete igb treatment
    
    * improve names
    
    * parse ext.igi.igb
    
    * naming improvement
    
    * update debugging mod to work with igb
    
    * fix mergeBuyers to work with actual URL origins
    
    * rename componentBuyers to componentSeller
    
    * signal componentSeller config to bid adapters

--- test/spec/modules/prebidServerBidAdapter_spec.js
+++ test/spec/modules/prebidServerBidAdapter_spec.js
@@ -24,14 +24,14 @@ import 'modules/priceFloors.js';
 import 'modules/consentManagement.js';
 import 'modules/consentManagementUsp.js';
 import 'modules/schain.js';
-import 'modules/fledgeForGpt.js';
+import 'modules/paapi.js';
 import * as redactor from 'src/activities/redactor.js';
 import * as activityRules from 'src/activities/rules.js';
 import {hook} from '../../../src/hook.js';
 import {decorateAdUnitsWithNativeParams} from '../../../src/native.js';
 import {auctionManager} from '../../../src/auctionManager.js';
 import {stubAuctionIndex} from '../../helpers/indexStub.js';
-import {addComponentAuction, registerBidder} from 'src/adapters/bidderFactory.js';
+import {addPaapiConfig, registerBidder} from 'src/adapters/bidderFactory.js';
 import {getGlobal} from '../../../src/prebidGlobal.js';
 import {syncAddFPDToBidderRequest} from '../../helpers/fpd.js';
 import {deepSetValue} from '../../../src/utils.js';
@@ -3487,11 +3487,11 @@ describe('S2S Adapter', function () {
       }
 
       before(() => {
-        addComponentAuction.before(fledgeHook);
+        addPaapiConfig.before(fledgeHook);
       });
 
       after(() => {
-        addComponentAuction.getHooks({hook: fledgeHook}).remove();
+        addPaapiConfig.getHooks({hook: fledgeHook}).remove();
       })
 
       beforeEach(function () {
@@ -3520,8 +3520,8 @@ describe('S2S Adapter', function () {
 
       function expectFledgeCalls() {
         const auctionId = bidderRequests[0].auctionId;
-        sinon.assert.calledWith(fledgeStub, sinon.match({auctionId, adUnitCode: AU, ortb2: bidderRequests[0].ortb2, ortb2Imp: bidderRequests[0].bids[0].ortb2Imp}), {id: 1})
-        sinon.assert.calledWith(fledgeStub, sinon.match({auctionId, adUnitCode: AU, ortb2: undefined, ortb2Imp: undefined}), {id: 2})
+        sinon.assert.calledWith(fledgeStub, sinon.match({auctionId, adUnitCode: AU, ortb2: bidderRequests[0].ortb2, ortb2Imp: bidderRequests[0].bids[0].ortb2Imp}), {config: {id: 1}})
+        sinon.assert.calledWith(fledgeStub, sinon.match({auctionId, adUnitCode: AU, ortb2: undefined, ortb2Imp: undefined}), {config: {id: 2}})
       }
 
       it('calls addComponentAuction alongside addBidResponse', function () {
