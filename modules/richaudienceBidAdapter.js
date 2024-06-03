5d9fdb5f5972248b1172ce793f7b1ec7025c9901
Patrick McCann <patmmccann@gmail.com>
2024-06-03 12:57:40 -0400

    Update richaudienceBidAdapter.js

--- modules/richaudienceBidAdapter.js
+++ modules/richaudienceBidAdapter.js
@@ -320,6 +320,9 @@ function raiGetResolution() {
 
 function raiSetPbAdSlot(bid) {
   let pbAdSlot = '';
+  if (deepAccess(bid, 'ortb2Imp.ext.data.gpid') != null) {
+    pbAdSlot = deepAccess(bid, 'ortb2Imp.ext.data.gpid')
+  }
   if (deepAccess(bid, 'ortb2Imp.ext.data.pbadslot') != null) {
     pbAdSlot = deepAccess(bid, 'ortb2Imp.ext.data.pbadslot')
   }
