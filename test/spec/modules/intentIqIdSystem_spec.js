e8cc0c17da5f76dfe15e20d846ccf22aa1c8ca01
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2024-06-05 18:09:50 +0100

    added tests and small fixes

--- test/spec/modules/intentIqIdSystem_spec.js
+++ test/spec/modules/intentIqIdSystem_spec.js
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { intentIqIdSubmodule, storage } from 'modules/intentIqIdSystem.js';
 import * as utils from 'src/utils.js';
 import { server } from 'test/mocks/xhr.js';
+import { FIRST_PARTY_KEY, decryptData, detectBrowserFromUserAgent, detectBrowserFromUserAgentData, handleGPPData } from '../../../modules/intentIqIdSystem';
+import { gppDataHandler, uspDataHandler } from '../../../src/consentHandler';
 
 const partner = 10;
 const pai = '11';
@@ -40,6 +42,9 @@ describe('IntentIQ tests', function () {
 
   beforeEach(function () {
     localStorage.clear();
+    const expiredDate = new Date(0).toUTCString();
+    storage.setCookie(FIRST_PARTY_KEY, '', expiredDate, 'Lax');
+    storage.setCookie(FIRST_PARTY_KEY + '_' + partner, '', expiredDate, 'Lax');
     logErrorStub = sinon.stub(utils, 'logError');
   });
 
@@ -92,9 +97,10 @@ describe('IntentIQ tests', function () {
       JSON.stringify({ pid: 'test_pid', data: 'test_personid', ls: true })
     );
     expect(callBackSpy.calledOnce).to.be.true;
-    const cookieValue = storage.getCookie('_iiq_fdata_' + partner)
-    expect(cookieValue).to.not.equal(null)
-    expect(JSON.parse(cookieValue).data).to.be.equal('test_personid');
+    const cookieValue = storage.getCookie('_iiq_fdata_' + partner);
+    expect(cookieValue).to.not.equal(null);
+    const decryptedData = JSON.parse(decryptData(JSON.parse(cookieValue).data));
+    expect(decryptedData).to.deep.equal(['test_personid']);
   });
 
   it('should call the IntentIQ endpoint with only partner', function () {
@@ -112,8 +118,6 @@ describe('IntentIQ tests', function () {
   });
 
   it('should ignore INVALID_ID and invalid responses in decode', function () {
-    // let resp = JSON.stringify({'RESULT': 'NA'});
-    // expect(intentIqIdSubmodule.decode(resp)).to.equal(undefined);
     expect(intentIqIdSubmodule.decode('INVALID_ID')).to.equal(undefined);
     expect(intentIqIdSubmodule.decode('')).to.equal(undefined);
     expect(intentIqIdSubmodule.decode(undefined)).to.equal(undefined);
@@ -200,7 +204,7 @@ describe('IntentIQ tests', function () {
       JSON.stringify({ pid: 'test_pid', data: 'test_personid', ls: true })
     );
     expect(callBackSpy.calledOnce).to.be.true;
-    expect(callBackSpy.args[0][0]).to.be.eq('test_personid');
+    expect(callBackSpy.args[0][0]).to.deep.equal(['test_personid']);
   });
 
   it('dont save result if ls=false', function () {
@@ -216,21 +220,6 @@ describe('IntentIQ tests', function () {
     );
     expect(callBackSpy.calledOnce).to.be.true;
     expect(callBackSpy.args[0][0]).to.be.undefined;
-  });
-
-  it('save result as INVALID_ID on empty data and ls=true ', function () {
-    let callBackSpy = sinon.spy();
-    let submoduleCallback = intentIqIdSubmodule.getId(allConfigParams).callback;
-    submoduleCallback(callBackSpy);
-    let request = server.requests[0];
-    expect(request.url).to.contain('https://api.intentiq.com/profiles_engine/ProfilesEngineServlet?at=39&mi=10&dpi=10&pt=17&dpn=1&pcid=12&pai=11&iiqidtype=2&iiqpcid=');
-    request.respond(
-      200,
-      responseHeader,
-      JSON.stringify({ pid: 'test_pid', data: '', ls: true })
-    );
-    expect(callBackSpy.calledOnce).to.be.true;
-    expect(callBackSpy.args[0][0]).to.be.eq('INVALID_ID');
   });
 
   it('send addition parameters if were found in localstorage', function () {
@@ -249,7 +238,7 @@ describe('IntentIQ tests', function () {
       JSON.stringify(testResponseWithValues)
     );
     expect(callBackSpy.calledOnce).to.be.true;
-    expect(callBackSpy.args[0][0]).to.be.eq(testResponseWithValues.data);
+    expect(callBackSpy.args[0][0]).to.deep.equal([testResponseWithValues.data]);
   });
 
   it('return data stored in local storage ', function () {
@@ -259,61 +248,10 @@ describe('IntentIQ tests', function () {
     submoduleCallback(callBackSpy);
     expect(server.requests.length).to.be.equal(0);
     expect(callBackSpy.calledOnce).to.be.true;
-    expect(callBackSpy.args[0][0]).to.be.equal(testLSValueWithData.data);
-  });
-
-  it('should handle encrypted data correctly', function () {
-    let encryptedData = AES.encrypt('test_personid', MODULE_NAME).toString();
-    localStorage.setItem(FIRST_PARTY_DATA_KEY + '_' + partner, JSON.stringify({ data: encryptedData }));
-    let callBackSpy = sinon.spy();
-    let submoduleCallback = intentIqIdSubmodule.getId(defaultConfigParams).callback;
-    submoduleCallback(callBackSpy);
-    expect(callBackSpy.calledOnce).to.be.true;
-    expect(callBackSpy.args[0][0]).to.equal('test_personid');
-  });
-
-  it('should handle getId with high entropy values', function (done) {
-    let callBackSpy = sinon.spy();
-    sinon.stub(navigator.userAgentData, 'getHighEntropyValues').returns(Promise.resolve({
-      brands: [{ brand: 'Google Chrome', version: '89' }],
-      mobile: false,
-      platform: 'Windows',
-      architecture: 'x86',
-      bitness: '64',
-      model: '',
-      platformVersion: '10.0',
-      wow64: false,
-      fullVersionList: [{ brand: 'Google Chrome', version: '89' }]
-    }));
-    let submoduleCallback = intentIqIdSubmodule.getId(defaultConfigParams).callback;
-    submoduleCallback(callBackSpy);
-    setTimeout(() => {
-      expect(callBackSpy.calledOnce).to.be.true;
-      navigator.userAgentData.getHighEntropyValues.restore();
-      done();
-    }, 1000);
-  });
-
-  it('should store and read data with cookie storage enabled through getId', function () {
-    let callBackSpy = sinon.spy();
-    let submoduleCallback = intentIqIdSubmodule.getId(enableCookieConfigParams).callback;
-    submoduleCallback(callBackSpy);
-    let request = server.requests[0];
-    expect(request.url).to.contain('https://api.intentiq.com/profiles_engine/ProfilesEngineServlet?at=39&mi=10&dpi=10&pt=17&dpn=1&iiqidtype=2&iiqpcid=');
-    request.respond(
-      200,
-      responseHeader,
-      JSON.stringify({ pid: 'test_pid', data: 'test_personid', ls: true })
-    );
-    expect(callBackSpy.calledOnce).to.be.true;
-    // Now check if data is stored correctly in cookies
-    const cookieValue = storage.getCookie('_iiq_fdata_' + partner);
-    expect(cookieValue).to.not.equal(null);
-    expect(JSON.parse(cookieValue).data).to.be.equal('test_personid');
+    expect(callBackSpy.args[0][0]).to.deep.equal(testLSValueWithData.data);
   });
 
   it('should handle browser blacklisting', function () {
-    let callBackSpy = sinon.spy();
     let configParamsWithBlacklist = {
       params: { partner: partner, browserBlackList: 'chrome' }
     };
@@ -321,7 +259,6 @@ describe('IntentIQ tests', function () {
     let submoduleCallback = intentIqIdSubmodule.getId(configParamsWithBlacklist);
     expect(logErrorStub.calledOnce).to.be.true;
     expect(submoduleCallback).to.be.undefined;
-    navigator.userAgent.restore();
   });
 
   it('should handle invalid JSON in readData', function () {
@@ -338,5 +275,181 @@ describe('IntentIQ tests', function () {
     );
     expect(callBackSpy.calledOnce).to.be.true;
     expect(logErrorStub.called).to.be.true;
+  });
+
+  describe('handleGPPData', function () {
+    it('should convert array of objects to a single JSON string', function () {
+      const input = [
+        { key1: 'value1' },
+        { key2: 'value2' }
+      ];
+      const expectedOutput = JSON.stringify({ key1: 'value1', key2: 'value2' });
+      const result = handleGPPData(input);
+      expect(result).to.equal(expectedOutput);
+    });
+
+    it('should convert a single object to a JSON string', function () {
+      const input = { key1: 'value1', key2: 'value2' };
+      const expectedOutput = JSON.stringify(input);
+      const result = handleGPPData(input);
+      expect(result).to.equal(expectedOutput);
+    });
+
+    it('should handle empty object', function () {
+      const input = {};
+      const expectedOutput = JSON.stringify(input);
+      const result = handleGPPData(input);
+      expect(result).to.equal(expectedOutput);
+    });
+
+    it('should handle empty array', function () {
+      const input = [];
+      const expectedOutput = JSON.stringify({});
+      const result = handleGPPData(input);
+      expect(result).to.equal(expectedOutput);
+    });
+  });
+
+  describe('detectBrowserFromUserAgent', function () {
+    it('should detect Chrome browser', function () {
+      const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+      const result = detectBrowserFromUserAgent(userAgent);
+      expect(result).to.equal('chrome');
+    });
+
+    it('should detect Safari browser', function () {
+      const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15';
+      const result = detectBrowserFromUserAgent(userAgent);
+      expect(result).to.equal('safari');
+    });
+
+    it('should detect Firefox browser', function () {
+      const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0';
+      const result = detectBrowserFromUserAgent(userAgent);
+      expect(result).to.equal('firefox');
+    });
+  });
+
+  describe('detectBrowserFromUserAgentData', function () {
+    it('should detect Microsoft Edge browser', function () {
+      const userAgentData = {
+        brands: [
+          { brand: 'Microsoft Edge', version: '91' },
+          { brand: 'Chromium', version: '91' }
+        ]
+      };
+      const result = detectBrowserFromUserAgentData(userAgentData);
+      expect(result).to.equal('edge');
+    });
+
+    it('should detect Chrome browser', function () {
+      const userAgentData = {
+        brands: [
+          { brand: 'Google Chrome', version: '91' },
+          { brand: 'Chromium', version: '91' }
+        ]
+      };
+      const result = detectBrowserFromUserAgentData(userAgentData);
+      expect(result).to.equal('chrome');
+    });
+
+    it('should return unknown for unrecognized user agent data', function () {
+      const userAgentData = {
+        brands: [
+          { brand: 'Unknown Browser', version: '1.0' }
+        ]
+      };
+      const result = detectBrowserFromUserAgentData(userAgentData);
+      expect(result).to.equal('unknown');
+    });
+  });
+
+  describe('IntentIQ consent management within getId', function () {
+    let uspDataHandlerStub;
+    let gppDataHandlerStub;
+
+    beforeEach(function () {
+      localStorage.clear();
+      const expiredDate = new Date(0).toUTCString();
+      storage.setCookie(FIRST_PARTY_KEY, '', expiredDate, 'Lax');
+      storage.setCookie(FIRST_PARTY_KEY + '_' + partner, '', expiredDate, 'Lax');
+      uspDataHandlerStub = sinon.stub(uspDataHandler, 'getConsentData');
+      gppDataHandlerStub = sinon.stub(gppDataHandler, 'getConsentData');
+    });
+
+    afterEach(function () {
+      uspDataHandlerStub.restore();
+      gppDataHandlerStub.restore();
+    });
+
+    it('should set cmpData.us_privacy if uspData exists', function () {
+      const uspData = '1NYN';
+      uspDataHandlerStub.returns(uspData);
+      let callBackSpy = sinon.spy();
+      let submoduleCallback = intentIqIdSubmodule.getId(defaultConfigParams).callback;
+      submoduleCallback(callBackSpy);
+      let request = server.requests[0];
+      expect(request.url).to.contain('https://api.intentiq.com/profiles_engine/ProfilesEngineServlet?at=39&mi=10&dpi=10&pt=17&dpn=1&iiqidtype=2&iiqpcid=');
+      request.respond(
+        200,
+        responseHeader,
+        JSON.stringify({ pid: 'test_pid', data: 'test_personid', ls: false, isOptedOut: false })
+      );
+      expect(callBackSpy.calledOnce).to.be.true;
+
+      // Check the local storage directly to see if cmpData.us_privacy was set correctly
+      const firstPartyData = JSON.parse(localStorage.getItem(FIRST_PARTY_KEY));
+      expect(firstPartyData.uspapi_value).to.equal(uspData);
+    });
+
+    it('should set cmpData.gpp and cmpData.gpp_sid if gppData exists and has parsedSections with usnat', function () {
+      const gppData = {
+        parsedSections: {
+          usnat: { key1: 'value1', key2: 'value2' }
+        },
+        applicableSections: ['usnat']
+      };
+      gppDataHandlerStub.returns(gppData);
+
+      let callBackSpy = sinon.spy();
+      let submoduleCallback = intentIqIdSubmodule.getId(defaultConfigParams).callback;
+      submoduleCallback(callBackSpy);
+      let request = server.requests[0];
+      expect(request.url).to.contain('https://api.intentiq.com/profiles_engine/ProfilesEngineServlet?at=39&mi=10&dpi=10&pt=17&dpn=1&iiqidtype=2&iiqpcid=');
+      request.respond(
+        200,
+        responseHeader,
+        JSON.stringify({ pid: 'test_pid', data: 'test_personid', ls: false, isOptedOut: false })
+      );
+      expect(callBackSpy.calledOnce).to.be.true;
+
+      const firstPartyData = JSON.parse(localStorage.getItem(FIRST_PARTY_KEY));
+      expect(firstPartyData.gpp_value).to.equal(JSON.stringify({ key1: 'value1', key2: 'value2' }));
+    });
+
+    it('should handle gppData without usnat in parsedSections', function () {
+      const gppData = {
+        parsedSections: {
+          euconsent: { key1: 'value1' }
+        },
+        applicableSections: ['euconsent']
+      };
+      gppDataHandlerStub.returns(gppData);
+
+      let callBackSpy = sinon.spy();
+      let submoduleCallback = intentIqIdSubmodule.getId(defaultConfigParams).callback;
+      submoduleCallback(callBackSpy);
+      let request = server.requests[0];
+      expect(request.url).to.contain('https://api.intentiq.com/profiles_engine/ProfilesEngineServlet?at=39&mi=10&dpi=10&pt=17&dpn=1&iiqidtype=2&iiqpcid=');
+      request.respond(
+        200,
+        responseHeader,
+        JSON.stringify({ pid: 'test_pid', data: 'test_personid', ls: false, isOptedOut: true })
+      );
+      expect(callBackSpy.calledOnce).to.be.true;
+
+      const firstPartyData = JSON.parse(localStorage.getItem(FIRST_PARTY_KEY));
+      expect(firstPartyData.gpp_value).to.equal('');
+    });
   });
 });
