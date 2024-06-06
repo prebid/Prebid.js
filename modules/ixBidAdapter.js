f80dfefb83456dc1f63b42407e0b17208a54e488
Patrick McCann <patmmccann@gmail.com>
2024-06-06 08:27:21 -0400

    Update ixBidAdapter.js

--- modules/ixBidAdapter.js
+++ modules/ixBidAdapter.js
@@ -21,7 +21,7 @@ import { config } from '../src/config.js';
 import { getStorageManager } from '../src/storageManager.js';
 import { find } from '../src/polyfill.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
-import { INSTREAM, OUTSTREAM } from '../src/video.js';
+import { OUTSTREAM } from '../src/video.js';
 import { Renderer } from '../src/Renderer.js';
 import {getGptSlotInfoForAdUnitCode} from '../libraries/gptUtils/gptUtils.js';
 
