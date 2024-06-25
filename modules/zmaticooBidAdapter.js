a0755830c3e54bb97c88b7bb283d9b9ee5f4ef82
ASurovenko <asurovenko@zetaglobal.com>
2024-06-25 16:17:21 +0200

    extract buildVideo into utils

--- modules/zmaticooBidAdapter.js
+++ modules/zmaticooBidAdapter.js
@@ -1,4 +1,4 @@
-import {deepAccess, isArray, isBoolean, isNumber, isStr, logWarn, triggerPixel} from '../src/utils.js';
+import {buildVideo, deepAccess, logWarn, triggerPixel} from '../src/utils.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
 import {BANNER, VIDEO} from '../src/mediaTypes.js';
 
@@ -15,28 +15,6 @@ const ENDPOINT_URL = 'https://bid.zmaticoo.com/prebid/bid';
 const DEFAULT_CUR = 'USD';
 const TTL = 200;
 const NET_REV = true;
-
-const DATA_TYPES = {
-  'NUMBER': 'number', 'STRING': 'string', 'BOOLEAN': 'boolean', 'ARRAY': 'array', 'OBJECT': 'object'
-};
-const VIDEO_CUSTOM_PARAMS = {
-  'mimes': DATA_TYPES.ARRAY,
-  'minduration': DATA_TYPES.NUMBER,
-  'maxduration': DATA_TYPES.NUMBER,
-  'startdelay': DATA_TYPES.NUMBER,
-  'playbackmethod': DATA_TYPES.ARRAY,
-  'api': DATA_TYPES.ARRAY,
-  'protocols': DATA_TYPES.ARRAY,
-  'w': DATA_TYPES.NUMBER,
-  'h': DATA_TYPES.NUMBER,
-  'battr': DATA_TYPES.ARRAY,
-  'linearity': DATA_TYPES.NUMBER,
-  'placement': DATA_TYPES.NUMBER,
-  'plcmt': DATA_TYPES.NUMBER,
-  'minbitrate': DATA_TYPES.NUMBER,
-  'maxbitrate': DATA_TYPES.NUMBER,
-  'skip': DATA_TYPES.NUMBER
-}
 
 export const spec = {
   code: BIDDER_CODE,
@@ -237,49 +215,6 @@ function buildBanner(request) {
   return {
     w: sizes[0][0], h: sizes[0][1]
   };
-}
-
-function buildVideo(request) {
-  let video = {};
-  const videoParams = deepAccess(request, 'mediaTypes.video', {});
-  for (const key in VIDEO_CUSTOM_PARAMS) {
-    if (videoParams.hasOwnProperty(key)) {
-      video[key] = checkParamDataType(key, videoParams[key], VIDEO_CUSTOM_PARAMS[key]);
-    }
-  }
-  if (videoParams.playerSize) {
-    if (isArray(videoParams.playerSize[0])) {
-      video.w = parseInt(videoParams.playerSize[0][0], 10);
-      video.h = parseInt(videoParams.playerSize[0][1], 10);
-    } else if (isNumber(videoParams.playerSize[0])) {
-      video.w = parseInt(videoParams.playerSize[0], 10);
-      video.h = parseInt(videoParams.playerSize[1], 10);
-    }
-  }
-  return video;
-}
-
-export function checkParamDataType(key, value, datatype) {
-  let functionToExecute;
-  switch (datatype) {
-    case DATA_TYPES.BOOLEAN:
-      functionToExecute = isBoolean;
-      break;
-    case DATA_TYPES.NUMBER:
-      functionToExecute = isNumber;
-      break;
-    case DATA_TYPES.STRING:
-      functionToExecute = isStr;
-      break;
-    case DATA_TYPES.ARRAY:
-      functionToExecute = isArray;
-      break;
-  }
-  if (functionToExecute(value)) {
-    return value;
-  }
-  logWarn('Ignoring param key: ' + key + ', expects ' + datatype + ', found ' + typeof value);
-  return undefined;
 }
 
 function hasBannerMediaType(bidRequest) {
--- src/utils.js
+++ src/utils.js
@@ -4,9 +4,36 @@ import {includes} from './polyfill.js';
 import { EVENTS, S2S } from './constants.js';
 import {GreedyPromise} from './utils/promise.js';
 import {getGlobal} from './prebidGlobal.js';
+import { default as deepAccess } from 'dlv/index.js';
 
 export { default as deepAccess } from 'dlv/index.js';
 export { dset as deepSetValue } from 'dset';
+
+const DATA_TYPES = {
+  'NUMBER': 'number',
+  'STRING': 'string',
+  'BOOLEAN': 'boolean',
+  'ARRAY': 'array',
+  'OBJECT': 'object'
+};
+const VIDEO_CUSTOM_PARAMS = {
+  'mimes': DATA_TYPES.ARRAY,
+  'minduration': DATA_TYPES.NUMBER,
+  'maxduration': DATA_TYPES.NUMBER,
+  'startdelay': DATA_TYPES.NUMBER,
+  'playbackmethod': DATA_TYPES.ARRAY,
+  'api': DATA_TYPES.ARRAY,
+  'protocols': DATA_TYPES.ARRAY,
+  'w': DATA_TYPES.NUMBER,
+  'h': DATA_TYPES.NUMBER,
+  'battr': DATA_TYPES.ARRAY,
+  'linearity': DATA_TYPES.NUMBER,
+  'placement': DATA_TYPES.NUMBER,
+  'plcmt': DATA_TYPES.NUMBER,
+  'minbitrate': DATA_TYPES.NUMBER,
+  'maxbitrate': DATA_TYPES.NUMBER,
+  'skip': DATA_TYPES.NUMBER
+}
 
 var tStr = 'String';
 var tFn = 'Function';
@@ -1188,4 +1215,46 @@ export function hasNonSerializableProperty(obj, checkedObjects = new Set()) {
     }
   }
   return false;
+}
+
+export function buildVideo(request) {
+  let video = {};
+  const videoParams = deepAccess(request, 'mediaTypes.video', {});
+  for (const key in VIDEO_CUSTOM_PARAMS) {
+    if (videoParams.hasOwnProperty(key)) {
+      video[key] = checkParamDataType(key, videoParams[key], VIDEO_CUSTOM_PARAMS[key]);
+    }
+  }
+  if (videoParams.playerSize) {
+    if (isArray(videoParams.playerSize[0])) {
+      video.w = parseInt(videoParams.playerSize[0][0], 10);
+      video.h = parseInt(videoParams.playerSize[0][1], 10);
+    } else if (isNumber(videoParams.playerSize[0])) {
+      video.w = parseInt(videoParams.playerSize[0], 10);
+      video.h = parseInt(videoParams.playerSize[1], 10);
+    }
+  }
+  return video;
+}
+
+function checkParamDataType(key, value, datatype) {
+  let functionToExecute;
+  switch (datatype) {
+    case DATA_TYPES.BOOLEAN:
+      functionToExecute = isBoolean;
+      break;
+    case DATA_TYPES.NUMBER:
+      functionToExecute = isNumber;
+      break;
+    case DATA_TYPES.STRING:
+      functionToExecute = isStr;
+      break;
+    case DATA_TYPES.ARRAY:
+      functionToExecute = isArray;
+      break;
+  }
+  if (functionToExecute(value)) {
+    return value;
+  }
+  return undefined;
 }
