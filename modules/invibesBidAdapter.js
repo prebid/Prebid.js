ce78bf0b07758b2d1202cd954b7c9c5c7101085c
Patrick McCann <patmmccann@gmail.com>
2024-06-28 15:47:31 -0400

    Update invibesBidAdapter.js

--- modules/invibesBidAdapter.js
+++ modules/invibesBidAdapter.js
@@ -792,6 +792,7 @@ let keywords = (function () {
     prefix = prefix || '';
     let title = document.title || headTag
       ? headTag.getElementsByTagName('title')[0]
+        // eslint-disable-next-line no-dom-manipulation
         ? headTag.getElementsByTagName('title')[0].innerHTML
         : ''
       : '';
