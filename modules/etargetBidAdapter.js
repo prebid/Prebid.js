66ec05cb80a04f36c4a364c69c837ebd3ecc2122
Patrick McCann <patmmccann@gmail.com>
2024-06-28 15:58:28 -0400

    Update etargetBidAdapter.js

--- modules/etargetBidAdapter.js
+++ modules/etargetBidAdapter.js
@@ -89,6 +89,7 @@ export const spec = {
             }
           }
         }
+        // eslint-disable-next-line no-dom-manipulation
         mts['title'] = [(document.getElementsByTagName('title')[0] || []).innerHTML];
         mts['base'] = [(document.getElementsByTagName('base')[0] || {}).href];
         mts['referer'] = [document.location.href];
