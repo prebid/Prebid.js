bbd9533361621ebbef9dd55fceefe0faa9829065
Patrick McCann <patmmccann@gmail.com>
2024-06-28 16:05:09 -0400

    Update ampliffyBidAdapter.js

--- modules/ampliffyBidAdapter.js
+++ modules/ampliffyBidAdapter.js
@@ -292,7 +292,7 @@ export function parseXML(xml, bid) {
     if (ct) {
       const companion = xml.getElementsByTagName('Companion')[0];
       const htmlResource = companion.getElementsByTagName('HTMLResource')[0];
-      // eslint-disable-next-line no-dom-manipulation
+      // eslint-disable-next-line prebid/no-dom-manipulation
       const htmlContent = document.createElement('html');
       htmlContent.innerHTML = htmlResource.textContent;
 
