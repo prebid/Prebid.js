6d77026b0a878cd10c9ce3a6b410d3610e29b3ed
Demetrio Girardi <dgirardi@prebid.org>
2024-07-02 10:35:57 -0700

    Fix dfp gdpr parameter dupes

--- libraries/dfpUtils/dfpUtils.js
+++ libraries/dfpUtils/dfpUtils.js
@@ -1,3 +1,5 @@
+import {gdprDataHandler} from '../../src/consentHandler.js';
+
 /** Safe defaults which work on pretty much all video calls. */
 export const DEFAULT_DFP_PARAMS = {
   env: 'vp',
@@ -10,4 +12,15 @@ export const DFP_ENDPOINT = {
   protocol: 'https',
   host: 'securepubads.g.doubleclick.net',
   pathname: '/gampad/ads'
+}
+
+export function gdprParams() {
+  const gdprConsent = gdprDataHandler.getConsentData();
+  const params = {};
+  if (gdprConsent) {
+    if (typeof gdprConsent.gdprApplies === 'boolean') { params.gdpr = Number(gdprConsent.gdprApplies); }
+    if (gdprConsent.consentString) { params.gdpr_consent = gdprConsent.consentString; }
+    if (gdprConsent.addtlConsent) { params.addtl_consent = gdprConsent.addtlConsent; }
+  }
+  return params;
 }
