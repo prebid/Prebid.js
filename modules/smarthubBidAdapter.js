3e6d0b637929d61c7085c4f974243b067cbab46e
Demetrio Girardi <dgirardi@prebid.org>
2024-07-09 09:40:29 -0700

    Smarthub: reuse teqblaze utility code

--- modules/smarthubBidAdapter.js
+++ modules/smarthubBidAdapter.js
@@ -1,9 +1,11 @@
-import {deepAccess, isFn, logError, logMessage} from '../src/utils.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
 import {BANNER, NATIVE, VIDEO} from '../src/mediaTypes.js';
-import {config} from '../src/config.js';
-import {convertOrtbRequestToProprietaryNative} from '../src/native.js';
-import { interpretResponse, isBidRequestValid } from '../libraries/teqblazeUtils/bidderUtils.js';
+import {
+  buildPlacementProcessingFunction,
+  buildRequestsBase,
+  interpretResponseBuilder,
+  isBidRequestValid
+} from '../libraries/teqblazeUtils/bidderUtils.js';
 
 const BIDDER_CODE = 'smarthub';
 const ALIASES = [
@@ -29,145 +31,50 @@ const _getUrl = (partnerName) => {
   return `${BASE_URLS[BIDDER_CODE]}?partnerName=${partnerName}`;
 }
 
-const _getPlacementReqData = (bid) => {
-  const { params, bidId, mediaTypes, bidder } = bid;
-  const schain = bid.schain || {};
-  const { partnerName, seat, token, iabCat, minBidfloor, pos } = params;
-  const bidfloor = _getBidFloor(bid);
+const getPartnerName = (bid) => String(bid.params?.partnerName || bid.bidder).toLowerCase();
 
-  const plcmt = {
-    partnerName: String(partnerName || bidder).toLowerCase(),
-    seat,
-    token,
-    iabCat,
-    minBidfloor,
-    pos,
-    bidId,
-    schain,
-    bidfloor,
-  };
-
-  if (mediaTypes && mediaTypes[BANNER]) {
-    plcmt.adFormat = BANNER;
-    plcmt.sizes = mediaTypes[BANNER].sizes;
-  } else if (mediaTypes && mediaTypes[VIDEO]) {
-    plcmt.adFormat = VIDEO;
-    plcmt.playerSize = mediaTypes[VIDEO].playerSize;
-    plcmt.minduration = mediaTypes[VIDEO].minduration;
-    plcmt.maxduration = mediaTypes[VIDEO].maxduration;
-    plcmt.mimes = mediaTypes[VIDEO].mimes;
-    plcmt.protocols = mediaTypes[VIDEO].protocols;
-    plcmt.startdelay = mediaTypes[VIDEO].startdelay;
-    plcmt.placement = mediaTypes[VIDEO].plcmt;
-    plcmt.plcmt = mediaTypes[VIDEO].plcmt; // https://github.com/prebid/Prebid.js/issues/10452
-    plcmt.skip = mediaTypes[VIDEO].skip;
-    plcmt.skipafter = mediaTypes[VIDEO].skipafter;
-    plcmt.minbitrate = mediaTypes[VIDEO].minbitrate;
-    plcmt.maxbitrate = mediaTypes[VIDEO].maxbitrate;
-    plcmt.delivery = mediaTypes[VIDEO].delivery;
-    plcmt.playbackmethod = mediaTypes[VIDEO].playbackmethod;
-    plcmt.api = mediaTypes[VIDEO].api;
-    plcmt.linearity = mediaTypes[VIDEO].linearity;
-  } else if (mediaTypes && mediaTypes[NATIVE]) {
-    plcmt.native = mediaTypes[NATIVE];
-    plcmt.adFormat = NATIVE;
+const getPlacementReqData = buildPlacementProcessingFunction({
+  addPlacementType() {},
+  addCustomFieldsToPlacement(bid, bidderRequest, placement) {
+    const { seat, token, iabCat, minBidfloor, pos } = bid.params;
+    Object.assign(placement, {
+      partnerName: getPartnerName(bid),
+      seat,
+      token,
+      iabCat,
+      minBidfloor,
+      pos,
+    });
   }
-
-  return plcmt;
-}
-
-const _getBidFloor = (bid) => {
-  if (!isFn(bid.getFloor)) {
-    return deepAccess(bid, 'params.bidfloor', 0);
-  }
-
-  let bidFloor = 0;
-  try {
-    const bidFloorObj = bid.getFloor({ currency: 'USD', mediaType: '*', size: '*' });
-    bidFloor = bidFloorObj.floor;
-    return bidFloor;
-  } catch (e) {
-    logError(e);
-    return bidFloor;
-  }
-}
-
-const _buildRequestParams = (bidderRequest = {}, placements = []) => {
-  let width = 0;
-  let height = 0;
-
-  let windowLocation;
-  try {
-    const windowTop = window.top;
-    width = windowTop.screen.width;
-    height = windowTop.screen.height;
-    windowLocation = windowTop.location;
-  } catch (e) {
-    logMessage(e);
-    windowLocation = window.location;
-  }
-
-  const refererUrl = bidderRequest.refererInfo && bidderRequest.refererInfo.page;
-  let referLocation;
-  try {
-    referLocation = refererUrl && new URL(refererUrl);
-  } catch (e) {
-    logMessage(e);
-  }
-
-  let location = referLocation || windowLocation;
-  const isNavigator = navigator && navigator.language;
-  const language = isNavigator ? navigator.language.split('-')[0] : '';
-  const locationHost = location.host;
-  const locationPath = location.pathname;
-  const locationSecure = location.protocol === 'https:' ? 1 : 0;
-  const coppaStatus = config.getConfig('coppa') ? 1 : 0;
-  return {
-    deviceWidth: width,
-    deviceHeight: height,
-    language,
-    secure: locationSecure,
-    host: locationHost,
-    page: locationPath,
-    placements,
-    coppa: coppaStatus,
-    ccpa: bidderRequest.uspConsent || undefined,
-    gdpr: bidderRequest.gdprConsent || undefined,
-    tmax: bidderRequest.timeout
-  };
-}
+})
 
 const buildRequests = (validBidRequests = [], bidderRequest = {}) => {
-  // convert Native ORTB definition to old-style prebid native definition
-  validBidRequests = convertOrtbRequestToProprietaryNative(validBidRequests);
-  const tempObj = {};
-
-  const len = validBidRequests.length;
-  for (let i = 0; i < len; i++) {
-    const bid = validBidRequests[i];
-    const data = _getPlacementReqData(bid);
-    tempObj[data.partnerName] = tempObj[data.partnerName] || [];
-    tempObj[data.partnerName].push(data);
-  }
-
-  return Object.keys(tempObj).map(key => {
-    const request = _buildRequestParams(bidderRequest, tempObj[key]);
-    return {
-      method: 'POST',
-      url: _getUrl(key),
-      data: request,
-    }
-  });
+  const bidsByPartner = validBidRequests.reduce((bidsByPartner, bid) => {
+    const partner = getPartnerName(bid);
+    (bidsByPartner[partner] = bidsByPartner[partner] || []).push(bid);
+    return bidsByPartner;
+  }, {});
+  return Object.entries(bidsByPartner).map(([partner, validBidRequests]) => {
+    return buildRequestsBase({
+      adUrl: _getUrl(partner),
+      bidderRequest,
+      validBidRequests,
+      placementProcessingFunction: getPlacementReqData
+    })
+  })
 }
 
 export const spec = {
   code: BIDDER_CODE,
   aliases: ALIASES,
   supportedMediaTypes: [BANNER, VIDEO, NATIVE],
-
-  isBidRequestValid,
+  isBidRequestValid: isBidRequestValid(['seat', 'token'], 'every'),
   buildRequests,
-  interpretResponse
+  interpretResponse: interpretResponseBuilder({
+    addtlBidValidation(bid) {
+      return bid.hasOwnProperty('netRevenue')
+    }
+  })
 };
 
 registerBidder(spec);
