eda006fd77bdcb53c5815993c1f883bface4baff
Patrick McCann <patmmccann@gmail.com>
2024-07-17 09:45:46 -0400

    Update index.js

--- modules/prebidServerBidAdapter/index.js
+++ modules/prebidServerBidAdapter/index.js
@@ -178,7 +178,7 @@ function setS2sConfig(options) {
 
   const activeBidders = [];
   const optionsValid = normalizedOptions.every((option, i, array) => {
-    formatUrlParams(options);
+    formatUrlParams(option);
     const updateSuccess = updateConfigDefaultVendor(option);
     if (updateSuccess !== false) {
       const valid = validateConfigRequiredProps(option);
