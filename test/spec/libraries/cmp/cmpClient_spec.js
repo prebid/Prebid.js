88c576d6a28bf1f55324a5e3c59eff1082ea879b
Patrick McCann <patmmccann@gmail.com>
2024-07-17 16:01:27 -0400

    Update cmpClient_spec.js

--- test/spec/libraries/cmp/cmpClient_spec.js
+++ test/spec/libraries/cmp/cmpClient_spec.js
@@ -255,7 +255,7 @@ describe('cmpClient', () => {
 
           beforeEach(() => {
             callId = null;
-            messenger.reset();
+            messenger.restore();
             messenger.callsFake((msg) => {
               if (msg.mockApiCall) callId = msg.mockApiCall.callId;
             });
