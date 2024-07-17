dd7011c30c7e337157671b8be368f407f4226df9
lokesh-doceree <Lokesh.Kumar@doceree.com>
2024-07-17 11:13:36 +0530

    Updated docereeAdManager bid adapter

--- test/spec/modules/docereeAdManagerBidAdapter_spec.js
+++ test/spec/modules/docereeAdManagerBidAdapter_spec.js
@@ -25,7 +25,7 @@ describe('docereeadmanager', function () {
           userid: '',
           zipcode: '',
           userconsent: '',
-          platformUid : ''
+          platformUid: ''
         },
       },
     },
