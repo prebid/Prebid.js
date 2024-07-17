9196c7794fa7b9d03f59e1eba79fc3eead0b0c3f
Patrick McCann <patmmccann@gmail.com>
2024-07-17 14:30:32 -0400

    Update saambaaBidAdapter.js

--- modules/saambaaBidAdapter.js
+++ modules/saambaaBidAdapter.js
@@ -4,8 +4,7 @@ import { deepAccess, generateUUID, isEmpty, parseUrl } from '../src/utils.js';
 import { config } from '../src/config.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, VIDEO } from '../src/mediaTypes.js';
-import { createRequestData, getBannerBidFloor, getBannerBidParam, getBannerSizes, getTopWindowLocation, getVideoBidFloor, getVideoBidParam, getVideoSizes, isBannerBid, isBannerBidValid, isVideoBid, isVideoBidValid, isConnectedTV, isMobile, getDoNotTrack, findAndFillParam, getOsVersion, getFirstSize, getVideoTargetingParams, VIDEO_TARGETING, DEFAULT_MIMES } from './bidAdapterUtils.js';
-
+import { createRequestData, getBannerBidFloor, getBannerBidParam, getBannerSizes, getTopWindowLocation, getVideoBidFloor, getVideoBidParam, getVideoSizes, isBannerBid, isBannerBidValid, isVideoBid, isVideoBidValid, isConnectedTV, isMobile, getDoNotTrack, findAndFillParam, getOsVersion, getFirstSize, getVideoTargetingParams, VIDEO_TARGETING, DEFAULT_MIMES } from '../libraries/advangUtils/index.js';
 const ADAPTER_VERSION = '1.0';
 const BIDDER_CODE = 'saambaa';
 
