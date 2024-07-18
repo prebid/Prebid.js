05c25b04eecbf0fed8e85792ef0800203ec97fc9
Pedro Ribeiro <pedromr21@gmail.com>
2024-07-18 22:00:23 +0000

    dev/ simple solution

--- modules/naveggIdSystem.js
+++ modules/naveggIdSystem.js
@@ -7,7 +7,7 @@
 import { isStr, isPlainObject, logError } from '../src/utils.js';
 import { submodule } from '../src/hook.js';
 import { ajax } from '../src/ajax.js';
-import {getStorageManager} from '../src/storageManager.js';
+import {getStorageManager, STORAGE_TYPE_COOKIES, STORAGE_TYPE_LOCALSTORAGE} from '../src/storageManager.js';
 import {MODULE_TYPE_UID} from '../src/activities/modules.js';
 
 /**
@@ -24,55 +24,59 @@ const INVALID_EXPIRE = 3600 * 1000;
 
 export const storage = getStorageManager({moduleType: MODULE_TYPE_UID, moduleName: MODULE_NAME});
 
-function getNaveggIdFromApi() {
-  const callbacks = {
-    success: response => {
-      if (response) {
-        try {
-          const responseObj = JSON.parse(response);
-          writeCookie(NAVEGG_ID, responseObj[NAVEGG_ID]);
-        } catch (error) {
-          logError(error);
+/* eslint-disable no-console */
+function getIdFromAPI(toStore, storedName) {
+  const resp = function (callback) {
+    ajax(BASE_URL, {
+      success: response => {
+        if (response) {
+          try {
+            const responseObj = {'nvggid': 'bmF2ZWdnaWQK'} // JSON.parse(response);
+            console.log('responseObj: ' + responseObj)
+            callback(responseObj[NAVEGG_ID])
+          } catch (error) {
+            logError(error);
+            callback()
+          }
         }
+      },
+      error: error => {
+        logError('Navegg ID fetch encountered an error', error);
       }
     },
-    error: error => {
-      logError('Navegg ID fetch encountered an error', error);
-    }
-  };
-  ajax(BASE_URL, callbacks, undefined, { method: 'GET', withCredentials: false });
-}
-
-function writeCookie(key, value) {
-  try {
-    if (storage.cookiesAreEnabled) {
-      let expTime = new Date();
-      const expires = value ? DEFAULT_EXPIRE : INVALID_EXPIRE;
-      expTime.setTime(expTime.getTime() + expires);
-      storage.setCookie(key, value, expTime.toUTCString(), 'none');
-    }
-  } catch (e) {
-    logError(e);
+    undefined, { method: 'GET', withCredentials: false });
   }
+  return resp
 }
 
-function readnaveggIdFromLocalStorage() {
+/**
+ * @returns {string | null}
+ */
+function readNaveggIdFromLocalStorage() {
   return storage.localStorageIsEnabled ? storage.getDataFromLocalStorage(NAVEGG_ID) : null;
 }
-
-function readnaveggIDFromCookie() {
+/**
+ * @returns {string | null}
+ */
+function readNaveggIdFromCookie() {
   return storage.cookiesAreEnabled ? storage.getCookie(NAVEGG_ID) : null;
 }
-
-function readoldnaveggIDFromCookie() {
+/**
+ * @returns {string | null}
+ */
+function readOldNaveggIdFromCookie() {
   return storage.cookiesAreEnabled ? storage.getCookie(OLD_NAVEGG_ID) : null;
 }
-
-function readnvgIDFromCookie() {
+/**
+ * @returns {string | null}
+ */
+function readNvgIdFromCookie() {
   return storage.cookiesAreEnabled ? (storage.findSimilarCookies('nvg') ? storage.findSimilarCookies('nvg')[0] : null) : null;
 }
-
-function readnavIDFromCookie() {
+/**
+ * @returns {string | null}
+ */
+function readNavIdFromCookie() {
   return storage.cookiesAreEnabled ? (storage.findSimilarCookies('nav') ? storage.findSimilarCookies('nav')[0] : null) : null;
 }
 
@@ -90,28 +94,45 @@ export const naveggIdSubmodule = {
    * @return { Object | string | undefined }
    */
   decode(value) {
+    console.log('decode value: ' + value)
     const naveggIdVal = value ? isStr(value) ? value : isPlainObject(value) ? value.id : undefined : undefined;
     return naveggIdVal ? {
       'naveggId': naveggIdVal.split('|')[0]
     } : undefined;
   },
+
   /**
    * performs action to obtain id and return a value in the callback's response argument
    * @function
    * @param {SubmoduleConfig} config
    * @return {{id: string | undefined } | undefined}
    */
-  getId() {
-    const naveggIdString = readnaveggIdFromLocalStorage() || readnaveggIDFromCookie() || getNaveggIdFromApi() || readoldnaveggIDFromCookie() || readnvgIDFromCookie() || readnavIDFromCookie();
+  getId({ name, enabledStorageTypes = [], storage: storageConfig = {} }) {
+    const resp = getIdFromAPI(enabledStorageTypes, storageConfig.name)
 
-    if (typeof naveggIdString == 'string' && naveggIdString) {
-      try {
-        return { id: naveggIdString };
-      } catch (error) {
-        logError(error);
-      }
-    }
-    return undefined;
+    // const naveggIdOldString = readOldNaveggIdFromCookie() || readNvgIdFromCookie() || readNavIdFromCookie()
+    // const naveggIdLocalStorage = readNaveggIdFromLocalStorage()
+    // const naveggIdCookie = readNaveggIdFromCookie()
+    // let toStore = []
+
+    // if (!naveggIdLocalStorage && enabledStorageTypes.indexOf(STORAGE_TYPE_LOCALSTORAGE) > -1) { toStore.push(STORAGE_TYPE_LOCALSTORAGE) }
+    // if (!naveggIdCookie && enabledStorageTypes.indexOf(STORAGE_TYPE_COOKIES) > -1) { toStore.push(STORAGE_TYPE_COOKIES) }
+    // saveNaveggIdFromApi(toStore, storageConfig.name)
+
+    // const naveggIdString = readNaveggIdFromCookie() || readNaveggIdFromLocalStorage();
+
+    // console.log('naveggIdString: ' + naveggIdString)
+    // console.log('----------------------------')
+
+    // if (typeof naveggIdString == 'string' && naveggIdString) {
+    //   try {
+    //     return { id: naveggIdString };
+    //   } catch (error) {
+    //     logError(error);
+    //   }
+    // }
+    // return undefined;
+    return {callback: resp}
   },
   eids: {
     'naveggId': {
