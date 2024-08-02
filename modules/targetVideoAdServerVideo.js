c8c88853db41bc2b9586b78df809d4061f95341d
Danijel Ristic <danijel-ristic@target-video.com>
2024-08-02 14:32:27 +0200

    Update ad server video module

--- modules/targetVideoAdServerVideo.js
+++ modules/targetVideoAdServerVideo.js
@@ -1,6 +1,6 @@
-import { registerVideoSupport } from 'src/adServerManager.js';
-import { targeting } from 'src/targeting.js';
-import { buildUrl, isPlainObject, logError } from 'src/utils.js';
+import { registerVideoSupport } from '../src/adServerManager.js';
+import { targeting } from '../src/targeting.js';
+import { buildUrl, isEmpty, isPlainObject, logError, parseUrl } from '../src/utils.js';
 
 /**
  * Merge all the bid data and publisher-supplied options into a single URL, and then return it.
@@ -23,31 +23,53 @@ export function buildVideoUrl(options) {
   }
 
   const isURL = /^(https?:\/\/)/i;
+  const defaultParameters = {
+    autoplay: '[autoplay]',
+    mute: '[vpmute]',
+    page_url: '[page_url]',
+    cachebuster: '[timestamp]',
+    consent: '[consent]',
+  }
+
   const adUnit = options.adUnit;
   const bid = options.bid || targeting.getWinningBids(adUnit.code)[0];
   const allTargetingData = getAllTargetingData(options);
-  const cust_params = options.params.cust_params;
+  const custParams = options.params.cust_params;
   let iu = options.params.iu;
 
   if (isURL.test(iu)) {
-    iu = appendCustParams(iu, cust_params);
-    const url = new URL(iu);
+    const urlComponents = parseUrl(iu, {noDecodeWholeURL: true});
 
-    for (const [key, value] of Object.entries({...allTargetingData, ...bid.adserverTargeting})) {
-      url.searchParams.append(key, value);
+    for (const [key, value] of Object.entries({...allTargetingData, ...bid.adserverTargeting, ...defaultParameters})) {
+      if (!urlComponents.search.hasOwnProperty(key)) {
+        urlComponents.search[key] = value;
+      }
     }
 
-    return url.toString();
+    if (urlComponents.search.cust_params) {
+      for (const [key, value] of Object.entries(custParams)) {
+        if (!urlComponents.search.cust_params.includes(key)) {
+          urlComponents.search.cust_params += '%26' + key + '%3D' + value;
+        }
+      }
+    }
+
+    if (!isEmpty(custParams) && !urlComponents.search.cust_params) {
+      urlComponents.search.cust_params = Object.entries(custParams).map(([key, value]) => key + '%3D' + value).join('%26');
+    }
+
+    return buildUrl(urlComponents);
   }
 
   const search = {
     iu,
+    ...defaultParameters,
+    ...allTargetingData,
+    ...bid.adserverTargeting,
   };
 
-  for (const [key, value] of Object.entries({...allTargetingData, ...bid.adserverTargeting})) {
-    if (!search.hasOwnProperty(key) && value && typeof value === 'string') {
-      search[key] = value;
-    }
+  if (!isEmpty(custParams)) {
+    search.cust_params = Object.entries(custParams).map(([key, value]) => key + '%3D' + value).join('%26');
   }
 
   return buildUrl({
@@ -67,36 +89,6 @@ function getAllTargetingData(options) {
   }
 
   return allTargetingData;
-}
-
-function appendCustParams(iu, cust_params) {
-  if (!Object.keys(cust_params).length) {
-    return iu;
-  }
-
-  if (iu.includes('cust_params')) {
-    const [url, search] = iu.split('?');
-    const searchParams = new URLSearchParams(search);
-    const merged = searchParams.get('cust_params') + "%26" + mergeCustParams();
-
-    searchParams.delete('cust_params');
-    searchParams.set('cust_params', merged);
-
-    return url + '?' + searchParams.toString();
-  } else {
-    iu += "&cust_params=" + mergeCustParams();
-  }
-
-  function mergeCustParams() {
-    let customParams = [];
-    for (const [key, value] of Object.entries(cust_params)) {
-      if (!iu.includes(key)) {
-        customParams.push(key + "=" + value);
-      }
-    }
-
-    return customParams.join("&");
-  }
 }
 
 registerVideoSupport('targetVideo', {
--- test/spec/modules/targetVideoAdServerVideo_spec.js
+++ test/spec/modules/targetVideoAdServerVideo_spec.js
@@ -1,9 +1,57 @@
-import { buildVideoUrl } from 'modules/targetVideoAdServerVideo.js';
-import { parseUrl } from 'src/utils.js';
-import { targeting } from 'src/targeting.js';
+import {expect} from 'chai';
+import {buildVideoUrl} from 'modules/targetVideoAdServerVideo.js';
+import {targeting} from 'src/targeting.js';
+import * as utils from 'src/utils.js';
+import {hook} from '../../../src/hook.js';
+import AD_UNIT from 'test/fixtures/video/adUnit.json';
 
 describe('TargetVideo Ad Server Video', function() {
-  it('should return undefined if required properties are missing', function() {
+  before(() => {
+    hook.ready();
+  });
+
+  let sandbox, bid, adUnit;
+  const unitUrl = { iu: 'https://example.com/ads/bid?iu=/video' };
+  const unitId = { iu: '/video' };
+  const allTargeting = {
+    'hb_format': 'video',
+    'hb_source': 'client',
+    'hb_size': '640x480',
+    'hb_pb': '5.00',
+    'hb_adid': '2c4f6cc3ba128a',
+    'hb_bidder': 'testBidder2',
+    'hb_format_testBidder2': 'video',
+    'hb_source_testBidder2': 'client',
+    'hb_size_testBidder2': '640x480',
+    'hb_pb_testBidder2': '5.00',
+    'hb_adid_testBidder2': '2c4f6cc3ba128a',
+    'hb_bidder_testBidder2': 'testBidder2',
+    'hb_format_targetVideo': 'video',
+    'hb_source_targetVideo': 'client',
+    'hb_size_targetVideo': '640x480',
+    'hb_pb_targetVideo': '5.00',
+    'hb_adid_targetVideo': '44e0b5f2e5cace',
+    'hb_bidder_targetVideo': 'targetVideo'
+  };
+
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    bid = {
+      videoCacheKey: '123',
+      adserverTargeting: {
+        hb_adid: 'ad_id',
+        hb_cache_id: '123',
+        hb_uuid: '123',
+      },
+    };
+    adUnit = utils.deepClone(AD_UNIT);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should return undefined if required properties are missing', () => {
     const url1 = buildVideoUrl({ params: {} });
     const url2 = buildVideoUrl({ adUnit: {} });
     const url3 = buildVideoUrl({ bid: {} });
@@ -13,98 +61,119 @@ describe('TargetVideo Ad Server Video', function() {
     expect(url3).to.be.undefined;
   });
 
-  it('should require options.adUnit or options.bid', function() {
-    const params = { iu: 'https://example.com/ads/bid?iu=video' };
-    const adUnit = {
-      code: 'ad-slot-1',
-      mediaTypes: {
-        video: {
-          context: 'instream',
-          playerSize: [ 640, 480 ]
-        }
-      },
-      bids: [
-        {
-          bidder: 'dummy'
-        }
-      ]
+  it('should require options.adUnit or options.bid', () => {
+    const options = {
+      params: { ...unitUrl }
     };
 
-    const BID = {
-      bidder: adUnit.bids[0].bidder,
-      adUnitCode: adUnit.code,
-      mediaType: 'video',
-      adserverTargeting: {
-        hb_adid: 'value1',
-        hb_cache: 'value2'
-      },
-    };
+    const getWinningBidsStub = sandbox.stub(targeting, 'getWinningBids').returns([bid]);
+    const getAllTargetingDataStub = sandbox.stub(targeting, 'getAllTargeting').returns(allTargeting);
 
-    const getWinningBidsStub = sinon.stub(targeting, 'getWinningBids');
-    getWinningBidsStub.withArgs(adUnit.code).returns([ BID ]);
-
-    const url1 = buildVideoUrl({ params });
+    const url1 = buildVideoUrl(options);
     expect(url1).to.be.undefined;
 
-    const url2 = buildVideoUrl({ params, adUnit });
+    const url2 = buildVideoUrl(Object.assign(options, { adUnit }));
     expect(url2).to.be.string;
 
-    const url3 = buildVideoUrl({ params, bid: BID });
+    const url3 = buildVideoUrl(Object.assign(options, { bid }));
     expect(url3).to.be.string;
 
     getWinningBidsStub.restore();
+    getAllTargetingDataStub.restore();
   });
 
-  it('should build URL correctly with valid parameters', function() {
-    const params = { iu: 'https://example.com/ads/bid?iu=/video' };
-    const adUnit = { code: 'ad_unit_1' };
-    const adserverTargeting = {
-      hb_adid: 'value1',
-      hb_cache: 'value2'
+  it('should build URL correctly with valid parameters', () => {
+    const optionsUrl = {
+      params: { ...unitUrl }
     };
-    const bid = { adserverTargeting };
 
-    const url = buildVideoUrl({ params, bid, adUnit });
-
-    expect(url).to.equal('https://example.com/ads/bid?iu=/video&hb_adid=value1&hb_cache=value2');
-  });
-
-  it('should build URL correctly with default parameters', function() {
-    const params = { iu: '/video' };
-    const adUnit = { code: 'ad_unit_1' };
-    const adserverTargeting = { hb_adid: 'value1', hb_cache: 'value2' };
-    const bid = { adserverTargeting };
-
-    const url = buildVideoUrl({ params, bid, adUnit });
-
-    expect(url).to.equal('https://vid.tvserve.io/ads/bid?iu=/video&hb_adid=value1&hb_cache=value2');
-  });
-
-  it('should append only valid parameters', function() {
-    const params = { iu: '/video' };
-    const adUnit = { code: 'ad_unit_1' };
-    const adserverTargeting = {
-      hb_adid: 'ad_id',
-      hb_cache_id: '123',
-      hb_uuid: '123',
-      invalidParam1: null,
-      invalidParam2: undefined,
-      invalidParam3: '',
-      invalidParam4: [],
-      invalidParam5: {},
+    const optionsId = {
+      params: { ...unitId }
     };
-    const bid = { adserverTargeting };
 
-    const url = buildVideoUrl({ params, bid, adUnit });
-    const { search } = parseUrl(url);
+    const getWinningBidsStub = sandbox.stub(targeting, 'getWinningBids').returns([bid]);
+    const getAllTargetingDataStub = sandbox.stub(targeting, 'getAllTargeting').returns(allTargeting);
 
-    expect(search).to.have.property('hb_adid', 'ad_id');
-    expect(search).to.have.property('hb_cache_id', '123');
-    expect(search).to.have.property('hb_uuid', '123');
-    expect(search).to.not.have.property('invalidParam1');
-    expect(search).to.not.have.property('invalidParam2');
-    expect(search).to.not.have.property('invalidParam3');
-    expect(search).to.not.have.property('invalidParam4');
-    expect(search).to.not.have.property('invalidParam5');
+    const url1 = buildVideoUrl(Object.assign(optionsUrl, { bid, adUnit }));
+    const url2 = buildVideoUrl(Object.assign(optionsId, { bid, adUnit }));
+
+    expect(url1).to.include('https://example.com/ads/bid?iu=/video');
+    expect(url1).to.include('hb_adid=ad_id');
+    expect(url1).to.include('hb_cache_id=123');
+    expect(url1).to.include('hb_uuid=123');
+
+    expect(url2).to.include('https://vid.tvserve.io/ads/bid?iu=/video');
+    expect(url2).to.include('hb_adid=ad_id');
+    expect(url2).to.include('hb_cache_id=123');
+    expect(url2).to.include('hb_uuid=123');
+
+    getWinningBidsStub.restore();
+    getAllTargetingDataStub.restore();
+  });
+
+  it('should include default query parameters', () => {
+    const options = {
+      params: { ...unitUrl }
+    };
+
+    const getWinningBidsStub = sandbox.stub(targeting, 'getWinningBids').returns([bid]);
+    const getAllTargetingDataStub = sandbox.stub(targeting, 'getAllTargeting').returns(allTargeting);
+
+    const url = buildVideoUrl(Object.assign(options, { bid, adUnit }));
+
+    expect(url).to.include('autoplay=[autoplay]');
+    expect(url).to.include('mute=[vpmute]');
+    expect(url).to.include('page_url=[page_url]');
+    expect(url).to.include('cachebuster=[timestamp]');
+    expect(url).to.include('consent=[consent]');
+
+    getWinningBidsStub.restore();
+    getAllTargetingDataStub.restore();
+  });
+
+  it('should add cust_params correctly', () => {
+    const optionsUrl = {
+      params: {
+        ...unitUrl,
+        cust_params: {
+          targeting_1: 'foo',
+          targeting_2: 'bar'
+        }
+      }
+    };
+
+    const optionsId = {
+      params: {
+        ...unitId,
+        cust_params: {
+          targeting_1: 'foo',
+          targeting_2: 'bar'
+        }
+      }
+    };
+
+    const optionsToMergeCustParams = {
+      params: {
+        iu: 'https://example.com/ads/bid?iu=/video&cust_params=targeting_1%3Dbaz',
+        cust_params: {
+          targeting_1: 'foo',
+          targeting_2: 'bar'
+        }
+      }
+    };
+
+    const getWinningBidsStub = sandbox.stub(targeting, 'getWinningBids').returns([bid]);
+    const getAllTargetingDataStub = sandbox.stub(targeting, 'getAllTargeting').returns(allTargeting);
+
+    const url1 = buildVideoUrl(Object.assign(optionsUrl, { bid, adUnit }));
+    const url2 = buildVideoUrl(Object.assign(optionsId, { bid, adUnit }));
+    const url3 = buildVideoUrl(Object.assign(optionsToMergeCustParams, { bid, adUnit }));
+
+    expect(url1).to.include('cust_params=targeting_1%3Dfoo%26targeting_2%3Dbar');
+    expect(url2).to.include('cust_params=targeting_1%3Dfoo%26targeting_2%3Dbar');
+    expect(url3).to.include('cust_params=targeting_1%3Dbaz%26targeting_2%3Dbar');
+
+    getWinningBidsStub.restore();
+    getAllTargetingDataStub.restore();
   });
 });
