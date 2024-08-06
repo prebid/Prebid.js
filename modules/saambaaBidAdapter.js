cfe81263b1b3b4e7ee97f5382cd268411bc5f3b3
Chris Huie <phoenixtechnerd@gmail.com>
2024-08-06 10:12:28 -0600

    fix file name

--- modules/saambaaBidAdapter.js
+++ modules/saambaaBidAdapter.js
@@ -1,3 +1,3 @@
-import { spec } from './advangelistBidAdapter.js'; // eslint-disable-line prebid/validate-imports
+import { spec } from './advangelistsBidAdapter.js'; // eslint-disable-line prebid/validate-imports
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 registerBidder(spec);
