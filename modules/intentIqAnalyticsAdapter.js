1238e7ee3807a4df5c0889eed06a627ce9dcef8a
Eyvaz Ahmadzada <eyvaz.ahmedzade.12@gmail.com>
2024-08-07 14:35:13 +0200

    add browser blacklist to analytics

--- modules/intentIqAnalyticsAdapter.js
+++ modules/intentIqAnalyticsAdapter.js
@@ -112,6 +112,8 @@ function initLsValues() {
       iiqAnalyticsAnalyticsAdapter.initOptions.partner = iiqArr[0].params.partner;
       iiqAnalyticsAnalyticsAdapter.initOptions.currentGroup = iiqAnalyticsAnalyticsAdapter.initOptions.fpid.group;
     }
+
+    iiqAnalyticsAnalyticsAdapter.initOptions.browserBlackList = typeof iiqArr[0].params.browserBlackList === 'string' ? iiqArr[0].params.browserBlackList : '';
   }
 }
 
@@ -133,6 +135,14 @@ function initReadLsIds() {
 
 function bidWon(args) {
   if (!iiqAnalyticsAnalyticsAdapter.initOptions.lsValueInitialized) { initLsValues(); }
+
+  const currentBrowserLowerCase = detectBrowser();
+  logInfo('IIQ Browser', currentBrowserLowerCase);
+  if (iiqAnalyticsAnalyticsAdapter.initOptions.browserBlackList?.includes(currentBrowserLowerCase)) {
+    logError('IIQ ANALYTICS -> Browser is in blacklist!');
+    return;
+  }
+
   if (iiqAnalyticsAnalyticsAdapter.initOptions.lsValueInitialized && !iiqAnalyticsAnalyticsAdapter.initOptions.lsIdsInitialized) { initReadLsIds(); }
   if (!iiqAnalyticsAnalyticsAdapter.initOptions.manualReport) {
     ajax(constructFullUrl(preparePayload(args, true)), undefined, null, { method: 'GET' });
@@ -225,6 +235,77 @@ iiqAnalyticsAnalyticsAdapter.originEnableAnalytics = iiqAnalyticsAnalyticsAdapte
 iiqAnalyticsAnalyticsAdapter.enableAnalytics = function (myConfig) {
   iiqAnalyticsAnalyticsAdapter.originEnableAnalytics(myConfig); // call the base class function
 };
+
+/**
+ * Detects the browser using either userAgent or userAgentData
+ * @return {string} The name of the detected browser or 'unknown' if unable to detect
+ */
+function detectBrowser() {
+  try {
+    if (navigator.userAgent) {
+      return detectBrowserFromUserAgent(navigator.userAgent);
+    } else if (navigator.userAgentData) {
+      return detectBrowserFromUserAgentData(navigator.userAgentData);
+    }
+  } catch (error) {
+    logError('Error detecting browser:', error);
+  }
+  return 'unknown';
+}
+
+/**
+ * Detects the browser from the user agent string
+ * @param {string} userAgent - The user agent string from the browser
+ * @return {string} The name of the detected browser or 'unknown' if unable to detect
+ */
+function detectBrowserFromUserAgent(userAgent) {
+  const browserRegexPatterns = {
+    opera: /Opera|OPR/,
+    edge: /Edg/,
+    chrome: /Chrome|CriOS/,
+    safari: /Safari/,
+    firefox: /Firefox/,
+    ie: /MSIE|Trident/,
+  };
+
+  // Check for Chrome first to avoid confusion with Safari
+  if (browserRegexPatterns.chrome.test(userAgent)) {
+    return 'chrome';
+  }
+
+  // Now we can safely check for Safari
+  if (browserRegexPatterns.safari.test(userAgent) && !browserRegexPatterns.chrome.test(userAgent)) {
+    return 'safari';
+  }
+
+  // Check other browsers
+  for (const browser in browserRegexPatterns) {
+    if (browserRegexPatterns[browser].test(userAgent)) {
+      return browser;
+    }
+  }
+
+  return 'unknown';
+}
+
+/**
+ * Detects the browser from the NavigatorUAData object
+ * @param {NavigatorUAData} userAgentData - The user agent data object from the browser
+ * @return {string} The name of the detected browser or 'unknown' if unable to detect
+ */
+function detectBrowserFromUserAgentData(userAgentData) {
+  const brandNames = userAgentData.brands.map(brand => brand.brand);
+
+  if (brandNames.includes('Microsoft Edge')) {
+    return 'edge';
+  } else if (brandNames.includes('Opera')) {
+    return 'opera';
+  } else if (brandNames.some(brand => brand === 'Chromium' || brand === 'Google Chrome')) {
+    return 'chrome';
+  }
+
+  return 'unknown';
+}
 
 adapterManager.registerAnalyticsAdapter({
   adapter: iiqAnalyticsAnalyticsAdapter,
