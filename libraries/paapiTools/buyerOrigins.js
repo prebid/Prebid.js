525c7b37a3d1e1151bbfd933dcc0cd6e3163e875
Patrick McCann <patmmccann@gmail.com>
2024-08-21 15:20:07 -0400

    Update buyerOrigins.js

--- libraries/paapiTools/buyerOrigins.js
+++ libraries/paapiTools/buyerOrigins.js
@@ -11,23 +11,25 @@
 
    Bidders should consider updating their interstGroupBuyer list
    with server communication for auctions initiated after the first bid response.
+
+   Known buyers without current importers are commented out. If you need one, uncomment it.
 */
 
 export const BO_CSR_ONET = 'https://csr.onet.pl';
-export const BO_DOUBLECLICK_GOOGLEADS = 'https://googleads.g.doubleclick.net';
-export const BO_DOUBLECLICK_TD = 'https://td.doubleclick.net';
-export const BO_RTBHOUSE = 'https://f.creativecdn.com';
-export const BO_CRITEO_US = 'https://fledge.us.criteo.com';
-export const BO_CRITEO_EU = 'https://fledge.eu.criteo.com';
-export const BO_CRITEO_AS = 'https://fledge.as.criteo.com';
-export const BO_CRITEO_GRID_MERCURY = 'https://grid-mercury.criteo.com';
-export const BO_CRITEO_BIDSWITCH_TRADR = 'https://tradr.bsw-sb.criteo.com';
-export const BO_CRITEO_BIDSWITCH_SANDBOX = 'https://dsp-paapi-sandbox.bsw-ig.criteo.com';
-export const BO_APPSPOT = 'https://fledge-buyer-testing-1.uc.r.appspot.com';
-export const BO_OPTABLE = 'https://ads.optable.co';
-export const BO_ADROLL = 'https://x.adroll.com';
-export const BO_ADFORM = 'https://a2.adform.net';
-export const BO_RETARGETLY = 'https://cookieless-campaign.prd-00.retargetly.com';
-export const BO_AUDIGENT = 'https://proton.ad.gt';
-export const BO_YAHOO = 'https://pa.ybp.yahoo.com';
-export const BO_DOTOMI = 'https://usadmm.dotomi.com';
+// export const BO_DOUBLECLICK_GOOGLEADS = 'https://googleads.g.doubleclick.net';
+// export const BO_DOUBLECLICK_TD = 'https://td.doubleclick.net';
+// export const BO_RTBHOUSE = 'https://f.creativecdn.com';
+// export const BO_CRITEO_US = 'https://fledge.us.criteo.com';
+// export const BO_CRITEO_EU = 'https://fledge.eu.criteo.com';
+// export const BO_CRITEO_AS = 'https://fledge.as.criteo.com';
+// export const BO_CRITEO_GRID_MERCURY = 'https://grid-mercury.criteo.com';
+// export const BO_CRITEO_BIDSWITCH_TRADR = 'https://tradr.bsw-sb.criteo.com';
+// export const BO_CRITEO_BIDSWITCH_SANDBOX = 'https://dsp-paapi-sandbox.bsw-ig.criteo.com';
+// export const BO_APPSPOT = 'https://fledge-buyer-testing-1.uc.r.appspot.com';
+// export const BO_OPTABLE = 'https://ads.optable.co';
+// export const BO_ADROLL = 'https://x.adroll.com';
+// export const BO_ADFORM = 'https://a2.adform.net';
+// export const BO_RETARGETLY = 'https://cookieless-campaign.prd-00.retargetly.com';
+// export const BO_AUDIGENT = 'https://proton.ad.gt';
+// export const BO_YAHOO = 'https://pa.ybp.yahoo.com';
+// export const BO_DOTOMI = 'https://usadmm.dotomi.com';
