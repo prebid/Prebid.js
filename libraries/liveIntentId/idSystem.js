37f90886f818e306a0bcab0d4d0097c683f9b70f
3link <34981284+3link@users.noreply.github.com>
2024-08-23 11:44:04 +0200

    Lint

--- libraries/liveIntentId/idSystem.js
+++ libraries/liveIntentId/idSystem.js
@@ -172,10 +172,10 @@ export const liveIntentIdSubmodule = {
   },
 
   /**
-   * Decode the stored id value for passing to bid requests. 
+   * Decode the stored id value for passing to bid requests.
    * Note that lipb object is a wrapper for everything, and
    * internally it could contain more data other than `lipbid`
-   * (e.g. `segments`) depending on the `partner` and `publisherId` 
+   * (e.g. `segments`) depending on the `partner` and `publisherId`
    * params.
    * @function
    * @param {{unifiedId:string}} value
