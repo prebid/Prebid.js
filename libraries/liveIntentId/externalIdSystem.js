1feb6d2dbb82c750a245fcc497b448386658dce8
3link <34981284+3link@users.noreply.github.com>
2024-08-23 11:46:48 +0200

    Lint more

--- libraries/liveIntentId/externalIdSystem.js
+++ libraries/liveIntentId/externalIdSystem.js
@@ -1,7 +1,7 @@
 import { logError } from '../../src/utils.js';
 import { gdprDataHandler, uspDataHandler, gppDataHandler } from '../../src/adapterManager.js';
 import { submodule } from '../../src/hook.js';
-import { DEFAULT_AJAX_TIMEOUT, MODULE_NAME, parseRequestedAttributes, composeIdObject, eids, GVLID, PRIMARY_IDS} from './shared.js'
+import { DEFAULT_AJAX_TIMEOUT, MODULE_NAME, parseRequestedAttributes, composeIdObject, eids, GVLID, PRIMARY_IDS } from './shared.js'
 
 // Reference to the client for the liQHub.
 let cachedClientRef
