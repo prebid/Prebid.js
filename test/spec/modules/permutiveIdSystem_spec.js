9e4c0614947f94f6b352bf85d5559fea69a793b0
James Talbot <james@permutive.com>
2024-10-15 18:03:55 +0100

    Implement permutiveIdSystem userId submodule

---
+++ test/spec/modules/permutiveIdSystem_spec.js
@@ -0,0 +1,136 @@
+import { permutiveIdSubmodule, storage } from 'modules/permutiveIdSystem'
+import { deepSetValue } from 'src/utils.js'
+
+const STORAGE_KEY = 'permutive-prebid-id'
+
+describe('permutiveIdSystem', () => {
+  afterEach(() => {
+    storage.removeDataFromLocalStorage(STORAGE_KEY)
+  })
+
+  describe('decode', () => {
+    it('returns the input unchanged', () => {
+      const input = {
+        id5id: {
+          uid: '0',
+          ext: {
+            abTestingControlGroup: false,
+            linkType: 2,
+            pba: 'somepba'
+          }
+        }
+      }
+      const result = permutiveIdSubmodule.decode(input)
+      expect(result).to.be.equal(input)
+    })
+  })
+
+  describe('getId', () => {
+    it('returns relevant IDs from localStorage and does not return unexpected IDs', () => {
+      const data = getUserIdData()
+      storage.setDataInLocalStorage(STORAGE_KEY, JSON.stringify(data))
+      const result = permutiveIdSubmodule.getId({})
+      const expected = {
+        'id': {
+          'id5id': {
+            'uid': '0',
+            'linkType': 0,
+            'ext': {
+              'abTestingControlGroup': false,
+              'linkType': 0,
+              'pba': 'EVqgf9vY0fSrsrqJZMOm+Q=='
+            }
+          }
+        }
+      }
+      expect(result).to.deep.equal(expected)
+    })
+
+    it('returns undefined if no relevant IDs are found in localStorage', () => {
+      storage.setDataInLocalStorage(STORAGE_KEY, '{}')
+      const result = permutiveIdSubmodule.getId({})
+      expect(result).to.be.undefined
+    })
+
+    it('will optionally wait for Permutive SDK if no identities are in local storage already', async () => {
+      const cleanup = setWindowPermutive()
+      const result = permutiveIdSubmodule.getId({params: {ajaxTimeout: 20}})
+      expect(result).not.to.be.undefined
+      expect(result.id).to.be.undefined
+      expect(result.callback).not.to.be.undefined
+      let r
+      result.callback((a) => r = a)
+      await sleep(25)
+      const expected = {
+        'id5id': {
+          'uid': '0',
+          'linkType': 0,
+          'ext': {
+            'abTestingControlGroup': false,
+            'linkType': 0,
+            'pba': 'EVqgf9vY0fSrsrqJZMOm+Q=='
+          }
+        }
+      }
+      expect(r).to.deep.equal(expected)
+      cleanup()
+    })
+  })
+})
+
+const setWindowPermutive = () => {
+  // Read from Permutive
+  const backup = window.permutive
+
+  deepSetValue(window, 'permutive.ready', (f) => {
+    setTimeout(() => f(), 5)
+  })
+
+  deepSetValue(window, 'permutive.addon', (name) => {
+    if (name === 'identityManager') {
+      return {
+        prebid: {
+          onReady: (f) => {
+            setTimeout(() => f(sdkUserIdData()), 5)
+          }
+        }
+      }
+    }
+  })
+
+  // Cleanup
+  return () => window.permutive = backup
+}
+
+const sleep = (timeMs) => new Promise(resolve => setTimeout(resolve, timeMs));
+
+const sdkUserIdData = () => ({
+  'id5id': {
+    'uid': '0',
+    'linkType': 0,
+    'ext': {
+      'abTestingControlGroup': false,
+      'linkType': 0,
+      'pba': 'EVqgf9vY0fSrsrqJZMOm+Q=='
+    }
+  },
+})
+
+const getUserIdData = () => ({
+  'id5id': {
+    'userId': {
+      'uid': '0',
+      'linkType': 0,
+      'ext': {
+        'abTestingControlGroup': false,
+        'linkType': 0,
+        'pba': 'EVqgf9vY0fSrsrqJZMOm+Q=='
+      }
+    }
+  },
+  'fooid': {
+    'userId': {
+      'id': '1'
+    }
+  }
+})
