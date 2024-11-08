79f342036e8da1bf2a75b1f2c41c7e5e4a4a9be9
Patrick McCann <patmmccann@gmail.com>
2024-11-08 14:29:02 -0500

    Update adpartnerBidAdapter_spec.js

--- test/spec/modules/adpartnerBidAdapter_spec.js
+++ test/spec/modules/adpartnerBidAdapter_spec.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import {spec, ENDPOINT_PROTOCOL, ENDPOINT_DOMAIN, ENDPOINT_PATH} from 'modules/adpartnerBidAdapter.js';
 import {newBidder} from 'src/adapters/bidderFactory.js';
+import {joinSizesToString} from 'libraries/mediaImpactUtils/index.js';
 
 const BIDDER_CODE = 'adpartner';
 
@@ -117,7 +118,7 @@ describe('AdpartnerAdapter', function () {
 
   describe('joinSizesToString', function () {
     it('success convert sizes list to string', function () {
-      const sizesStr = spec.joinSizesToString([[300, 250], [300, 600]]);
+      const sizesStr = joinSizesToString([[300, 250], [300, 600]]);
       expect(sizesStr).to.equal('300x250|300x600');
     });
   });
