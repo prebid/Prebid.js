8fc82d71d1dd33b1566aecad0f608ad432255176
Patrick McCann <patmmccann@gmail.com>
2024-11-09 15:42:30 -0500

    Update videoheroesBidAdapter.js

--- modules/videoheroesBidAdapter.js
+++ modules/videoheroesBidAdapter.js
@@ -1,8 +1,8 @@
-import { triggerPixel } from '../src/utils.js';
+import { triggerPixel, isStr } from '../src/utils.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
 import { parseNative } from '../libraries/braveUtils/index.js';
-import { buildRequests, interpretResponse } from '../libraries/bidderUtils.js';
+import { buildRequests, interpretResponse } from 'libraries/braveUtils/buildAndInterpret.js';
 
 /**
  * @typedef {import('../src/adapters/bidderFactory.js').BidRequest} BidRequest
