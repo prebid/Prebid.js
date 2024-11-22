ab55073d787279fab845a695b085c3285f10c62d
Tulio Duarte <tulioduarte@bluems.com>
2024-11-22 17:53:43 -0300

    add blue

---
+++ modules/blueBidAdapter.js
@@ -0,0 +1,300 @@
+import { deepAccess, deepSetValue, logError } from '../src/utils.js';
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER } from '../src/mediaTypes.js';
+import { getStorageManager } from '../src/storageManager.js';
+import { ortbConverter } from '../libraries/ortbConverter/converter.js';
+import { ortb25Translator } from '../libraries/ortb2.5Translator/translator.js';
+
+/**
+ * @typedef {import('../src/adapters/bidderFactory.js').BidRequest} BidRequest
+ * @typedef {import('../src/adapters/bidderFactory.js').Bid} Bid
+ * @typedef {import('../src/adapters/bidderFactory.js').ServerRequest} ServerRequest
+ * @typedef {import('../src/adapters/bidderFactory.js').BidderSpec} BidderSpec
+ * @typedef {import('../src/adapters/bidderFactory.js').TimedOutBid} TimedOutBid
+ */
+
+const BIDDER_CODE = 'blue';
+const GVLID = 620;
+const CDB_ENDPOINT = 'https://bidder-us-east-1.getblue.io/engine/?src=prebid';
+
+export const storage = getStorageManager({ bidderCode: BIDDER_CODE });
+const TRANSLATOR = ortb25Translator();
+
+/**
+ * Defines the generic oRTB converter and all customization functions.
+ */
+const CONVERTER = ortbConverter({
+  context: {
+    netRevenue: true,
+    ttl: 60,
+  },
+  imp,
+  request,
+  bidResponse,
+  response,
+});
+
+/**
+ * Builds an impression object for the ORTB 2.5 request.
+ *
+ * @param {function} buildImp - The function for building an imp object.
+ * @param {Object} bidRequest - The bid request object.
+ * @param {Object} context - The context object.
+ * @returns {Object} The ORTB 2.5 imp object.
+ */
+function imp(buildImp, bidRequest, context) {
+  let imp = buildImp(bidRequest, context);
+  const params = bidRequest.params;
+
+  imp.tagid = bidRequest.adUnitCode;
+  deepSetValue(imp, 'ext', {
+    ...bidRequest.params.ext,
+    ...imp.ext,
+    rwdd: imp.rwdd,
+    floors: getFloors(bidRequest),
+    bidder: {
+      publishersubid: params?.publisherSubId,
+      zoneid: params?.zoneId,
+      uid: params?.uid,
+    },
+  });
+
+  delete imp.rwdd; // oRTB 2.6 field moved to ext
+
+  return imp;
+}
+
+/**
+ * Builds a request object for the ORTB 2.5 request.
+ *
+ * @param {function} buildRequest - The function for building a request object.
+ * @param {Array} imps - An array of ORTB 2.5 impression objects.
+ * @param {Object} bidderRequest - The bidder request object.
+ * @param {Object} context - The context object.
+ * @returns {Object} The ORTB 2.5 request object.
+ */
+function request(buildRequest, imps, bidderRequest, context) {
+  let request = buildRequest(imps, bidderRequest, context);
+
+  // params.pubid should override publisher id
+  if (typeof context.publisherId !== 'undefined') {
+    if (typeof request.app !== 'undefined') {
+      deepSetValue(request, 'app.publisher.id', context.publisherId);
+    } else {
+      deepSetValue(request, 'site.publisher.id', context.publisherId);
+    }
+  }
+
+  if (bidderRequest && bidderRequest.gdprConsent) {
+    deepSetValue(
+      request,
+      'regs.ext.gdprversion',
+      bidderRequest.gdprConsent.apiVersion
+    );
+  }
+
+  // Translate 2.6 OpenRTB request into 2.5 OpenRTB request
+  request = TRANSLATOR(request);
+
+  return request;
+}
+
+/**
+ * Build bid from oRTB 2.5 bid.
+ *
+ * @param buildBidResponse
+ * @param bid
+ * @param context
+ * @returns {*}
+ */
+function bidResponse(buildBidResponse, bid, context) {
+  context.mediaType = deepAccess(bid, 'ext.mediatype');
+  let bidResponse = buildBidResponse(bid, context);
+
+  bidResponse.currency = deepAccess(bid, 'ext.cur');
+
+  if (typeof deepAccess(bid, 'ext.meta') !== 'undefined') {
+    deepSetValue(bidResponse, 'meta', {
+      ...bidResponse.meta,
+      ...bid.ext.meta,
+    });
+  }
+  if (typeof deepAccess(bid, 'ext.paf.content_id') !== 'undefined') {
+    deepSetValue(bidResponse, 'meta.paf.content_id', bid.ext.paf.content_id);
+  }
+
+  return bidResponse;
+}
+
+/**
+ * Builds bid response from the oRTB 2.5 bid response.
+ *
+ * @param buildResponse
+ * @param bidResponses
+ * @param ortbResponse
+ * @param context
+ * @returns *
+ */
+function response(buildResponse, bidResponses, ortbResponse, context) {
+  let response = buildResponse(bidResponses, ortbResponse, context);
+
+  const pafTransmission = deepAccess(ortbResponse, 'ext.paf.transmission');
+  response.bids.forEach((bid) => {
+    if (
+      typeof pafTransmission !== 'undefined' &&
+      typeof deepAccess(bid, 'meta.paf.content_id') !== 'undefined'
+    ) {
+      deepSetValue(bid, 'meta.paf.transmission', pafTransmission);
+    } else {
+      delete bid.meta.paf;
+    }
+  });
+
+  return response;
+}
+
+/** @type {BidderSpec} */
+export const spec = {
+  code: BIDDER_CODE,
+  gvlid: GVLID,
+  supportedMediaTypes: [BANNER],
+
+  /**
+   * f
+   * @param {object} bid
+   * @return {boolean}
+   */
+  isBidRequestValid: (bid) => {
+    // either one of zoneId or networkId should be set
+    if (!(bid && bid.params && bid.params.publisherId)) {
+      return false;
+    }
+
+    return true;
+  },
+
+  /**
+   * @param {BidRequest[]} bidRequests
+   * @param {*} bidderRequest
+   * @return {ServerRequest}
+   */
+  buildRequests: (bidRequests, bidderRequest) => {
+    const context = buildContext(bidRequests, bidderRequest);
+    const url = buildCdbUrl(context);
+    const data = CONVERTER.toORTB({ bidderRequest, bidRequests, context });
+
+    if (data) {
+      return { method: 'POST', url, data, bidRequests };
+    }
+  },
+
+  /**
+   * @param {*} response
+   * @param {ServerRequest} request
+   * @return {Bid[] | {bids: Bid[], fledgeAuctionConfigs: object[]}}
+   */
+  interpretResponse: (response, request) => {
+    if (typeof response?.body == 'undefined') {
+      return []; // no bid
+    }
+
+    const interpretedResponse = CONVERTER.fromORTB({
+      response: response.body,
+      request: request.data,
+    });
+    const bids = interpretedResponse.bids || [];
+
+    return bids;
+  },
+};
+
+/**
+ * @param {BidRequest[]} bidRequests
+ * @param bidderRequest
+ */
+function buildContext(bidRequests, bidderRequest) {
+  return {
+    url: bidderRequest?.refererInfo?.page || '',
+    publisherId: bidRequests.find((bidRequest) => bidRequest.params?.pubid)
+      ?.params.pubid,
+  };
+}
+
+/**
+ * @param {Object} context
+ * @return {string}
+ */
+function buildCdbUrl(context) {
+  let url = CDB_ENDPOINT;
+  url += '&wv=' + encodeURIComponent('$prebid.version$');
+  url += '&cb=' + String(Math.floor(Math.random() * 99999999999));
+
+  if (context.publisherId) {
+    url += `&publisherId=` + context.publisherId;
+  }
+
+  return url;
+}
+
+function parseSizes(sizes, parser = (s) => s) {
+  if (sizes == undefined) {
+    return [];
+  }
+  if (Array.isArray(sizes[0])) {
+    // is there several sizes ? (ie. [[728,90],[200,300]])
+    return sizes.map((size) => parser(size));
+  }
+  return [parser(sizes)]; // or a single one ? (ie. [728,90])
+}
+
+function parseSize(size) {
+  return size[0] + 'x' + size[1];
+}
+
+function pickAvailableGetFloorFunc(bidRequest) {
+  if (bidRequest.getFloor) {
+    return bidRequest.getFloor;
+  }
+  if (bidRequest.params.bidFloor && bidRequest.params.bidFloorCur) {
+    try {
+      const floor = parseFloat(bidRequest.params.bidFloor);
+      return () => {
+        return {
+          currency: bidRequest.params.bidFloorCur,
+          floor: floor,
+        };
+      };
+    } catch {}
+  }
+  return undefined;
+}
+
+function getFloors(bidRequest) {
+  try {
+    const floors = {};
+
+    const getFloor = pickAvailableGetFloorFunc(bidRequest);
+
+    if (getFloor) {
+      if (bidRequest.mediaTypes?.banner) {
+        floors.banner = {};
+        const bannerSizes = parseSizes(
+          deepAccess(bidRequest, 'mediaTypes.banner.sizes')
+        );
+        bannerSizes.forEach(
+          (bannerSize) =>
+            (floors.banner[parseSize(bannerSize).toString()] = getFloor.call(
+              bidRequest,
+              { size: bannerSize, mediaType: BANNER }
+            ))
+        );
+      }
+
+      return floors;
+    }
+  } catch (e) {
+    logError('Could not parse floors from Prebid: ' + e);
+  }
+}
+
+registerBidder(spec);
---
+++ modules/blueBidAdapter.md
@@ -0,0 +1,27 @@
+# Overview
+
+Module Name: Blue Bidder Adapter
+Module Type: Bidder Adapter
+Maintainer: celsooliveira@getblue.io
+
+# Description
+
+Module that connects to Blue's demand sources.
+
+# Test Parameters
+```
+    var adUnits = [
+        {
+            code: 'banner-ad-div',
+            sizes: [[300, 250], [728, 90]],
+            bids: [
+                {
+                    bidder: 'blue',
+                    params: {
+                        zoneId: 497747
+                    }
+                }
+            ]
+        }
+    ];
+```
