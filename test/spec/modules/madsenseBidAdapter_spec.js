134eb7ae53d53e358a846c1101e9c3baf8721d9a
MadSense Ops <prebid@madsense.io>
2024-12-04 11:48:36 +0100

    Restructure test helpers and improve maintainability

--- test/spec/modules/madsenseBidAdapter_spec.js
+++ test/spec/modules/madsenseBidAdapter_spec.js
@@ -1,327 +1,188 @@
-import {expect} from 'chai';
-import {spec} from 'modules/madsenseBidAdapter.js';
-import {BANNER, VIDEO} from 'src/mediaTypes.js';
+import { expect } from 'chai';
+import { spec } from 'modules/madsenseBidAdapter.js';
+import { BANNER, VIDEO } from 'src/mediaTypes.js';
+import { generateUUID } from '../../../src/utils.js';
 
-const getBannerRequest = () => {
-  return {
-    bidderCode: 'madsense',
-    auctionId: 'ba87bfdf-493e-4a88-8e26-17b4cbc9adbd',
-    bidderRequestId: 'bidderRequestId',
-    bids: [
+const getCommonParams = () => ({
+  company_id: '1234567'
+});
+
+const getVideoParams = () => ({
+  video: {
+    playerWidth: 640,
+    playerHeight: 480,
+    mimes: ['video/mp4', 'application/javascript'],
+    protocols: [2, 5],
+    api: [2],
+    position: 1,
+    delivery: [2],
+    sid: 134,
+    rewarded: 1,
+    placement: 1,
+    plcmt: 1,
+    hp: 1,
+    inventoryid: 123,
+  },
+  site: {
+    id: 1,
+    page: 'https://test.io',
+    referrer: 'http://test.io',
+  },
+});
+
+const getBannerRequest = () => ({
+  bidderCode: 'madsense',
+  auctionId: generateUUID(),
+  bidderRequestId: 'bidderRequestId',
+  bids: [
+    {
+      bidder: 'madsense',
+      params: getCommonParams(),
+      auctionId: generateUUID(),
+      placementCode: 'dummy-placement-code',
+      mediaTypes: {
+        banner: {
+          sizes: [[300, 250]],
+        },
+      },
+      bidId: 'a1b2c3d4',
+      bidderRequestId: 'bidderRequestId',
+    },
+  ],
+  start: Date.now(),
+  auctionStart: Date.now() - 1,
+  timeout: 3000,
+});
+
+const getVideoBid = (bidId) => ({
+  mediaTypes: {
+    video: {
+      context: 'instream',
+      playerSize: [[640, 480]],
+    },
+  },
+  bidder: 'madsense',
+  sizes: [640, 480],
+  bidId,
+  adUnitCode: 'video1',
+  params: {
+    ...getVideoParams(),
+    ...getCommonParams(),
+  },
+});
+
+const getVideoRequest = () => ({
+  bidderCode: 'madsense',
+  auctionId: generateUUID(),
+  bidderRequestId: 'q1w2e3r4',
+  bids: [getVideoBid('i8u7y6t5'), getVideoBid('i8u7y6t5')],
+  auctionStart: Date.now(),
+  timeout: 5000,
+  start: Date.now() + 4,
+  refererInfo: {
+    numIframes: 1,
+    reachedTop: true,
+    referer: 'test.io',
+  },
+});
+
+const getBidderResponse = () => ({
+  headers: null,
+  body: {
+    id: 'bid-response',
+    seatbid: [
       {
-        bidder: 'madsense',
-        params: {
-          company_id: '1234567',
-          bidfloor: 10,
-        },
-        auctionId: 'auctionId-56a2-4f71-9098-720a68f2f708',
-        placementCode: 'div-gpt-dummy-placement-code',
-        mediaTypes: {
-          banner: {
-            sizes: [
-              [300, 250],
-            ]
-          }
-        },
-        bidId: '2e9f38ea93bb9e',
-        bidderRequestId: 'bidderRequestId',
-      }
-    ],
-    start: 1487883186070,
-    auctionStart: 1487883186069,
-    timeout: 3000
-  }
-};
-
-const getVideoRequest = () => {
-  return {
-    bidderCode: 'madsense',
-    auctionId: 'e158486f-8c7f-472f-94ce-b0cbfbb50ab4',
-    bidderRequestId: '34feaad34lkj2',
-    bids: [{
-      mediaTypes: {
-        video: {
-          context: 'instream',
-          playerSize: [[640, 480]],
-        }
-      },
-      bidder: 'madsense',
-      sizes: [640, 480],
-      bidId: '30b3efwfwe1e',
-      adUnitCode: 'video1',
-      params: {
-        video: {
-          playerWidth: 640,
-          playerHeight: 480,
-          mimes: ['video/mp4', 'application/javascript'],
-          protocols: [2, 5],
-          api: [2],
-          position: 1,
-          delivery: [2],
-          sid: 134,
-          rewarded: 1,
-          placement: 1,
-          plcmt: 1,
-          hp: 1,
-          inventoryid: 123
-        },
-        site: {
-          id: 1,
-          page: 'https://test.com',
-          referrer: 'http://test.com'
-        },
-        bidfloor: 10,
-      }
-    }, {
-      mediaTypes: {
-        video: {
-          context: 'instream',
-          playerSize: [[640, 480]],
-        }
-      },
-      bidder: 'madsense',
-      sizes: [640, 480],
-      bidId: '30b3efwfwe2e',
-      adUnitCode: 'video1',
-      params: {
-        video: {
-          playerWidth: 640,
-          playerHeight: 480,
-          mimes: ['video/mp4', 'application/javascript'],
-          protocols: [2, 5],
-          api: [2],
-          position: 1,
-          delivery: [2],
-          sid: 134,
-          rewarded: 1,
-          placement: 1,
-          plcmt: 1,
-          hp: 1,
-          inventoryid: 123
-        },
-        site: {
-          id: 1,
-          page: 'https://test.com',
-          referrer: 'http://test.com'
-        },
-        company_id: '1234567',
-        bidfloor: 10,
-      }
-    }],
-    auctionStart: 1520001292880,
-    timeout: 5000,
-    start: 1520001292884,
-    doneCbCallCount: 0,
-    refererInfo: {
-      numIframes: 1,
-      reachedTop: true,
-      referer: 'test.com'
-    }
-  };
-};
-
-const getBidderResponse = () => {
-  return {
-    headers: null,
-    body: {
-      id: 'bid-response',
-      seatbid: [
-        {
-          bid: [
-            {
-              id: '2e9f38ea93bb9e',
-              impid: '2e9f38ea93bb9e',
-              price: 0.18,
-              adm: '<script>adm</script>',
-              adid: '144762342',
-              adomain: [
-                'https://dummydomain.com'
-              ],
-              iurl: 'iurl',
-              cid: '109',
-              crid: 'creativeId',
-              cat: [],
-              w: 300,
-              h: 250,
-              ext: {
-                prebid: {
-                  type: 'banner'
+        bid: [
+          {
+            id: 'a1b2c3d4',
+            impid: 'a1b2c3d4',
+            price: 0.18,
+            adm: '<script>adm</script>',
+            adid: '144762342',
+            adomain: ['https://test.io'],
+            iurl: 'iurl',
+            cid: '109',
+            crid: 'creativeId',
+            w: 300,
+            h: 250,
+            ext: {
+              prebid: { type: 'banner' },
+              bidder: {
+                appnexus: {
+                  brand_id: 321654987,
+                  auction_id: 321654987000000,
+                  bidder_id: 2,
+                  bid_ad_type: 0,
                 },
-                bidder: {
-                  appnexus: {
-                    brand_id: 334553,
-                    auction_id: 514667951122925701,
-                    bidder_id: 2,
-                    bid_ad_type: 0
-                  }
-                }
-              }
-            }
-          ],
-          seat: 'madsense'
-        }
-      ],
-      ext: {
-        usersync: {
-          sovrn: {
-            status: 'none',
-            syncs: [
-              {
-                url: 'urlsovrn',
-                type: 'iframe'
-              }
-            ]
+              },
+            },
           },
-          appnexus: {
-            status: 'none',
-            syncs: [
-              {
-                url: 'urlappnexus',
-                type: 'pixel'
-              }
-            ]
-          }
-        },
-        responsetimemillis: {
-          appnexus: 127
-        }
-      }
-    }
-  };
-}
-
-describe('madsenseBidAdapter', function() {
-  let videoBidRequest;
-
-  const VIDEO_REQUEST = {
-    'bidderCode': 'madsense',
-    'auctionId': 'e158486f-8c7f-472f-94ce-b0cbfbb50ab4',
-    'bidderRequestId': '34feaad34lkj2',
-    'bids': videoBidRequest,
-    'auctionStart': 1520001292880,
-    'timeout': 3000,
-    'start': 1520001292884,
-    'doneCbCallCount': 0,
-    'refererInfo': {
-      'numIframes': 1,
-      'reachedTop': true,
-      'referer': 'test.com'
-    }
-  };
-
-  beforeEach(function () {
-    videoBidRequest = {
-      mediaTypes: {
-        video: {
-          context: 'instream',
-          playerSize: [[640, 480]],
-        }
+        ],
+        seat: 'madsense',
       },
-      bidder: 'madsense',
-      sizes: [640, 480],
-      bidId: '30b3efwfwe1e',
-      adUnitCode: 'video1',
-      params: {
-        video: {
-          playerWidth: 640,
-          playerHeight: 480,
-          mimes: ['video/mp4', 'application/javascript'],
-          protocols: [2, 5],
-          api: [2],
-          position: 1,
-          delivery: [2],
-          sid: 134,
-          rewarded: 1,
-          placement: 1,
-          plcmt: 1,
-          hp: 1,
-          inventoryid: 123
-        },
-        site: {
-          id: 1,
-          page: 'https://test.com',
-          referrer: 'http://test.com'
-        },
-        company_id: '1234567',
-        bidfloor: 0
-      }
-    };
-  });
+    ]
+  },
+});
 
-  describe('interpretResponse', function() {
-    context('when mediaType is banner', function() {
-      let bidRequest, bidderResponse;
-      beforeEach(function() {
-        const bidderRequest = getBannerRequest();
-        bidRequest = spec.buildRequests(bidderRequest.bids, bidderRequest);
-        bidderResponse = getBidderResponse();
-      });
-
+describe('madsenseBidAdapter', function () {
+  describe('interpretResponse', function () {
+    context('when mediaType is banner', function () {
       it('handles empty response', function () {
-        const EMPTY_RESP = Object.assign({}, bidderResponse, {'body': {}});
+        const bidderRequest = getBannerRequest();
+        const bidRequest = spec.buildRequests(bidderRequest.bids, bidderRequest);
+        const EMPTY_RESP = { ...getBidderResponse(), body: {} };
         const bids = spec.interpretResponse(EMPTY_RESP, bidRequest);
 
         expect(bids).to.be.empty;
       });
 
-      it('have bids', function () {
-        let bids = spec.interpretResponse(bidderResponse, bidRequest);
-        expect(bids).to.be.an('array').that.is.not.empty;
-        validateBidOnIndex(0);
+      it('validates banner bid', function () {
+        const bidderRequest = getBannerRequest();
+        const bidRequest = spec.buildRequests(bidderRequest.bids, bidderRequest);
+        const bidderResponse = getBidderResponse();
+        const bids = spec.interpretResponse(bidderResponse, bidRequest);
 
-        function validateBidOnIndex(index) {
-          expect(bids[index]).to.have.property('currency', 'USD');
-          expect(bids[index]).to.have.property('requestId', getBidderResponse().body.seatbid[0].bid[index].impid);
-          expect(bids[index]).to.have.property('cpm', getBidderResponse().body.seatbid[0].bid[index].price);
-          expect(bids[index]).to.have.property('width', getBidderResponse().body.seatbid[0].bid[index].w);
-          expect(bids[index]).to.have.property('height', getBidderResponse().body.seatbid[0].bid[index].h);
-          expect(bids[index]).to.have.property('ad', getBidderResponse().body.seatbid[0].bid[index].adm);
-          expect(bids[index]).to.have.property('creativeId', getBidderResponse().body.seatbid[0].bid[index].crid);
-          expect(bids[index].meta).to.have.property('advertiserDomains');
-          expect(bids[index]).to.have.property('ttl', 55);
-          expect(bids[index]).to.have.property('netRevenue', true);
-        }
+        expect(bids).to.be.an('array').that.is.not.empty;
+        validateBid(bids[0], bidderResponse.body.seatbid[0].bid[0]);
       });
     });
 
     context('when mediaType is video', function () {
-      let bidRequest, bidderResponse;
-      beforeEach(function() {
-        const bidderRequest = getVideoRequest();
-        bidRequest = spec.buildRequests(bidderRequest.bids, bidderRequest);
-        bidderResponse = getBidderResponse();
-      });
-
       it('handles empty response', function () {
-        const EMPTY_RESP = Object.assign({}, bidderResponse, {'body': {}});
+        const bidderRequest = getVideoRequest();
+        const bidRequest = spec.buildRequests(bidderRequest.bids, bidderRequest);
+        const EMPTY_RESP = { ...getBidderResponse(), body: {} };
         const bids = spec.interpretResponse(EMPTY_RESP, bidRequest);
 
         expect(bids).to.be.empty;
       });
 
-      it('should return no bids if the response "nurl" and "adm" are missing', function () {
-        const SERVER_RESP = Object.assign({}, bidderResponse, {'body': {
-          seatbid: [{
-            bid: [{
-              price: 6.01
-            }]
-          }]
-        }});
-        const bids = spec.interpretResponse(SERVER_RESP, bidRequest);
-        expect(bids.length).to.equal(0);
-      });
+      it('returns no bids if required fields are missing', function () {
+        const bidderRequest = getVideoRequest();
+        const bidRequest = spec.buildRequests(bidderRequest.bids, bidderRequest);
 
-      it('should return no bids if the response "price" is missing', function () {
-        const SERVER_RESP = Object.assign({}, bidderResponse, {'body': {
-          seatbid: [{
-            bid: [{
-              adm: '<VAST></VAST>'
-            }]
-          }]
-        }});
-        const bids = spec.interpretResponse(SERVER_RESP, bidRequest);
+        const MISSING_FIELDS_RESP = {
+          ...getBidderResponse(),
+          body: { seatbid: [{ bid: [{ price: 6.01 }] }] },
+        };
+        const bids = spec.interpretResponse(MISSING_FIELDS_RESP, bidRequest);
         expect(bids.length).to.equal(0);
       });
     });
   });
 });
+
+function validateBid(actualBid, expectedBid) {
+  expect(actualBid).to.include({
+    currency: 'USD',
+    requestId: expectedBid.impid,
+    cpm: expectedBid.price,
+    width: expectedBid.w,
+    height: expectedBid.h,
+    ad: expectedBid.adm,
+    creativeId: expectedBid.crid,
+    ttl: 55,
+    netRevenue: true,
+  });
+  expect(actualBid.meta).to.have.property('advertiserDomains');
+}
