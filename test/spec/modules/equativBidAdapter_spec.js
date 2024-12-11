ffd9f0ba48ec5dfe75bbf2a6772fff52dd51ddb3
Jeff Mahoney <jmahoney@sharethrough.com>
2024-12-11 11:55:21 -0500

    Equativ Bid Adapter: support native bid requests (#12566)
    * add support of dsa
    
    * restore topics
    
    * DSA fix for UT
    
    * drafy of adapter
    
    * fixes after dev test
    
    * make world simpler
    
    * fix prev commit
    
    * return empty userSyncs array by default
    
    * adjustments
    
    * apply prettier
    
    * unit tests for Equativ adapter
    
    * add dsp user sync
    
    * add readme
    
    * body can be undef
    
    * support additional br params
    
    * remove user sync
    
    * do not send dt param
    
    * handle floors and network id
    
    * handle empty media types
    
    * get min floor
    
    * fix desc for u.t.
    
    * better name for u.t.
    
    * add u.t. for not supported media type
    
    * improve currency u.t.
    
    * updates after pr review
    
    * SADR-6484: initial video setup for new PBJS adapter
    
    * SADR-6484: Adding logging requirement missed earlier
    
    * SADR-6484: handle ext.rewarded prop for video with new oRTBConverter
    
    * SADR-6484: test revision + not sending bid requests where video obj is empty
    
    * refactoring and u.t.
    
    * rename variable
    
    * Equativ: SADR-6615: adding unit tests for and additional logging to bid adapter to support native requests
    
    * revert changes rel. to test endpoint
    
    * revert changes rel. to test endpoint
    
    * split imp[0] into seperate requests and fix u.t.
    
    * Equativ bid adapter: adding support for native media type
    
    * Equativ bid adapter: update unit test for native-support work
    
    * Equativ bid adapter: removing console.log from unit test file
    
    * Equativ bid adapter: clarifying refinements regarding native-request processing
    
    * Equativ Bid Adapter: updating unit tests for native requests
    
    * PR feedback
    
    ---------
    
    Co-authored-by: Elżbieta SZPONDER <eszponder@smartadserver.com>
    Co-authored-by: eszponder <155961428+eszponder@users.noreply.github.com>
    Co-authored-by: Krzysztof Sokół <88041828+krzysztofequativ@users.noreply.github.com>
    Co-authored-by: Krzysztof Sokół <ksokol@smartadserver.com>
    Co-authored-by: janzych-smart <jzych@smartadserver.com>

--- test/spec/modules/equativBidAdapter_spec.js
+++ test/spec/modules/equativBidAdapter_spec.js
@@ -1,6 +1,6 @@
-import { BANNER } from 'src/mediaTypes.js';
-import { getBidFloor } from 'libraries/equativUtils/equativUtils.js'
+import { getBidFloor } from 'libraries/equativUtils/equativUtils.js';
 import { converter, spec, storage } from 'modules/equativBidAdapter.js';
+import { BANNER } from 'src/mediaTypes.js';
 import * as utils from '../../../src/utils.js';
 
 describe('Equativ bid adapter tests', () => {
@@ -78,6 +78,64 @@ describe('Equativ bid adapter tests', () => {
     }
   ];
 
+  const nativeOrtbRequest = {
+    assets: [{
+      id: 0,
+      required: 1,
+      title: {
+        len: 140
+      }
+    },
+    {
+      id: 1,
+      required: 1,
+      img: {
+        type: 3,
+        w: 300,
+        h: 600
+      }
+    },
+    {
+      id: 2,
+      required: 1,
+      data: {
+        type: 1
+      }
+    }],
+    context: 1,
+    eventtrackers: [{
+      event: 1,
+      methods: [1, 2]
+    }],
+    plcmttype: 1,
+    privacy: 1,
+    ver: '1.2',
+  };
+  const DEFAULT_NATIVE_BID_REQUESTS = [
+    {
+      adUnitCode: 'equativ_native_42',
+      bidId: 'equativ_native_bidid_42',
+      mediaTypes: {
+        native: {
+          ortb: {
+            ...nativeOrtbRequest
+          }
+        },
+      },
+      nativeOrtbRequest,
+      bidder: 'equativ',
+      params: {
+        networkId: 777,
+      },
+      requestId: 'equativ_native_reqid_42',
+      ortb2Imp: {
+        ext: {
+          tid: 'equativ_native_tid_42',
+        },
+      },
+    }
+  ];
+
   const DEFAULT_BANNER_BIDDER_REQUEST = {
     bidderCode: 'equativ',
     bids: DEFAULT_BANNER_BID_REQUESTS,
@@ -86,6 +144,11 @@ describe('Equativ bid adapter tests', () => {
   const DEFAULT_VIDEO_BIDDER_REQUEST = {
     bidderCode: 'equativ',
     bids: DEFAULT_VIDEO_BID_REQUESTS,
+  };
+
+  const DEFAULT_NATIVE_BIDDER_REQUEST = {
+    bidderCode: 'equativ',
+    bids: DEFAULT_NATIVE_BID_REQUESTS,
   };
 
   const SAMPLE_RESPONSE = {
@@ -494,6 +557,99 @@ describe('Equativ bid adapter tests', () => {
       expect(utils.logError.calledOnce).to.equal(true);
       expect(utils.logError.args[0][0]).to.satisfy(arg => arg.includes('No request'));
       expect(request).to.be.undefined;
+    });
+
+    it('should build a native request properly under normal circumstances', () => {
+      if (FEATURES.NATIVE) {
+        // ASSEMBLE
+        const expectedResult = true;
+
+        // ACT
+        const request = spec.buildRequests(DEFAULT_NATIVE_BID_REQUESTS, {})[0].data;
+
+        // ASSERT
+        expect(request.imp[0]).to.have.property('native');
+
+        const nativeObj = request.imp[0].native;
+        expect(nativeObj).to.have.property('ver').and.to.equal('1.2');
+        expect(nativeObj).to.have.property('request').and.to.be.a('string');
+
+        const requestObj = JSON.parse(nativeObj.request);
+        expect(requestObj).to.have.property('assets').and.to.be.an('array');
+        expect(requestObj).to.have.property('eventtrackers').and.to.be.an('array');
+        expect(requestObj).to.have.property('plcmttype').and.to.equal(1);
+        expect(requestObj).to.have.property('privacy').and.to.equal(1);
+        expect(requestObj).to.have.property('ver').and.to.equal('1.2');
+      }
+    });
+
+    it('should not send a native request when it has an empty body and no other impressions with any media types are defined', () => {
+      if (FEATURES.NATIVE) {
+        // ASSEMBLE
+        const emptyNativeRequest = {
+          ...DEFAULT_NATIVE_BID_REQUESTS[0],
+          mediaTypes: {
+            native: {}
+          }
+        };
+        const bidRequests = [ emptyNativeRequest ];
+        const bidderRequest = { ...DEFAULT_NATIVE_BIDDER_REQUEST, bids: bidRequests };
+
+        // ACT
+        const request = spec.buildRequests(bidRequests, bidderRequest);
+
+        // ASSERT
+        expect(utils.logError.calledOnce).to.equal(true);
+        expect(utils.logError.args[0][0]).to.satisfy(arg => arg.includes('No request'));
+        expect(request).to.be.undefined;
+      }
+    });
+
+    it('should warn about missing "assets" property for native requests', () => {
+      if (FEATURES.NATIVE) {
+        // ASSEMBLE
+        const missingRequiredNativeRequest = DEFAULT_NATIVE_BID_REQUESTS[0];
+
+        // removing just "assets" for this test
+        delete missingRequiredNativeRequest.nativeOrtbRequest.assets;
+        const bidRequests = [ missingRequiredNativeRequest ];
+        const bidderRequest = { ...DEFAULT_NATIVE_BIDDER_REQUEST, bids: bidRequests };
+
+        // this value comes from native.js, part of the ortbConverter library
+        const warningMsgFromLibrary = 'mediaTypes.native is set, but no assets were specified. Native request skipped.'
+
+        // ACT
+        spec.buildRequests(bidRequests, bidderRequest);
+
+        // ASSERT
+        expect(utils.logWarn.callCount).to.equal(1)
+        expect(utils.logWarn.getCall(0).args[0]).to.satisfy(arg => arg.includes(warningMsgFromLibrary));
+      }
+    });
+
+    it('should warn about other missing required properties for native requests', () => {
+      if (FEATURES.NATIVE) {
+        // ASSEMBLE
+        const missingRequiredNativeRequest = DEFAULT_NATIVE_BID_REQUESTS[0];
+
+        // ortbConverter library will warn about missing assets; we supply warnings for these properties here
+        delete missingRequiredNativeRequest.mediaTypes.native.ortb.eventtrackers;
+        delete missingRequiredNativeRequest.mediaTypes.native.ortb.plcmttype;
+        delete missingRequiredNativeRequest.mediaTypes.native.ortb.privacy;
+
+        const bidRequests = [ missingRequiredNativeRequest ];
+        const bidderRequest = { ...DEFAULT_NATIVE_BIDDER_REQUEST, bids: bidRequests };
+
+        // ACT
+        spec.buildRequests(bidRequests, bidderRequest);
+
+        // ASSERT
+        expect(utils.logWarn.callCount).to.equal(4); // the first message, regarding missing assets, is supplied by the ortbConverter library
+        expect(utils.logWarn.getCall(0).args[0]).to.satisfy(arg => arg.includes('no assets were specified'));
+        expect(utils.logWarn.getCall(1).args[0]).to.satisfy(arg => arg.includes('"privacy" is missing'));
+        expect(utils.logWarn.getCall(2).args[0]).to.satisfy(arg => arg.includes('"plcmttype" is missing'));
+        expect(utils.logWarn.getCall(3).args[0]).to.satisfy(arg => arg.includes('"eventtrackers" is missing'));
+      }
     });
   });
 
