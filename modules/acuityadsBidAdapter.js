598a8d3ddf4334cfa90686497e43a1c55335fb6a
AcuityAdsIntegrations <72594990+AcuityAdsIntegrations@users.noreply.github.com>
2024-12-16 15:46:11 +0200

    AcuityAds Bid Adapter: add gvlid (#12581)
    * add prebid.js adapter
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * fix downolad
    
    * add gpp
    
    * Merge remote-tracking branch 'prebid/master'
    
    * add gvlid

--- modules/acuityadsBidAdapter.js
+++ modules/acuityadsBidAdapter.js
@@ -3,11 +3,13 @@ import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
 import { isBidRequestValid, buildRequests, interpretResponse, getUserSyncs } from '../libraries/teqblazeUtils/bidderUtils.js';
 
 const BIDDER_CODE = 'acuityads';
+const GVLID = 231;
 const AD_URL = 'https://prebid.admanmedia.com/pbjs';
 const SYNC_URL = 'https://cs.admanmedia.com';
 
 export const spec = {
   code: BIDDER_CODE,
+  gvlid: GVLID,
   supportedMediaTypes: [BANNER, VIDEO, NATIVE],
 
   isBidRequestValid: isBidRequestValid(['placementId']),
