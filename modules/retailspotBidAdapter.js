3a565d3e43cec29a8d9b9c6bca2c5ed8267478d2
Guillaume Andouard <guiandouard@gmail.com>
2024-12-20 10:36:04 +0100

    simplify getSize workflow

--- modules/retailspotBidAdapter.js
+++ modules/retailspotBidAdapter.js
@@ -28,7 +28,7 @@ export const spec = {
    * @return boolean True if this is a valid bid, and false otherwise.
    */
   isBidRequestValid: function (bid) {
-    const sizes = getSize(getSizeArray(bid));
+    const sizes = getSize(bid);
     const sizeValid = sizes.width > 0 && sizes.height > 0;
 
     return deepAccess(bid, 'params.placement') && sizeValid;
@@ -99,40 +99,40 @@ export const spec = {
   }
 }
 
-function getSizeArray(bid) {
-  let inputSize = bid.sizes || [];
 
-  if (bid.mediaTypes && bid.mediaTypes.banner) {
+/* Get parsed size from request size */
+function getSize(bid) {
+  let inputSize = bid.sizes || [];
+  
+  if (bid.mediaTypes?.banner) {
     inputSize = bid.mediaTypes.banner.sizes || [];
   }
 
-  // handle size in bid.params in formats: [w, h] and [[w,h]].
-  if (bid.params && Array.isArray(bid.params.size)) {
+  // Size can be [w, h] or array of sizes : [[w,h]].
+  if (Array.isArray(bid.params?.size)) {
     inputSize = bid.params.size;
     if (!Array.isArray(inputSize[0])) {
       inputSize = [inputSize]
     }
   }
 
-  return parseSizesInput(inputSize);
-}
-
-/* Get parsed size from request size */
-function getSize(sizesArray) {
+  const sizesArray = parseSizesInput(inputSize);
   const parsed = {};
-  // the main requested size is the first one
+
+  // Use the first size as the main requested one 
   const size = sizesArray[0];
 
+  // size is ready
   if (typeof size !== 'string') {
     return parsed;
   }
 
-  const parsedSize = size.toUpperCase().split('X');
+  // size is given as string "wwwxhhh" or "www*hhh"
+  const parsedSize = size.includes('*') ? size.split('*') :  size.toUpperCase().split('X');
   const width = parseInt(parsedSize[0], 10);
   if (width) {
     parsed.width = width;
   }
-
   const height = parseInt(parsedSize[1], 10);
   if (height) {
     parsed.height = height;
