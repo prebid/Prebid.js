743f100fcfc2551b79cc1e677170a4e1796ebd51
Gabriela Międlar <155444733+gmiedlar-ox@users.noreply.github.com>
2025-01-13 15:16:30 +0100

    OpenX Bid Adapter : support native (#12625)
    * [EXCH-10877] Add support for native to OpenX bid adapter
    
    * [EXCH-10877] Add unit tests for interpretResponse and native
    
    * [EXCH-10877] Fix formatting
    
    * [EXCH-10877] Add support for native to OpenX bid adapter
    
    * [EXCH-10877] Add unit tests for interpretResponse and native
    
    * [EXCH-10877] Fix formatting
    
    * Revert "Merge remote-tracking branch 'origin/EXCH-10877-add-support-for-native-imps' into EXCH-10877-add-support-for-native-imps"
    
    This reverts commit c88138628dda3ac9409c1846d2baaf022521c4d6, reversing
    changes made to 55a23a5f526e2a4d2cebb4fe4bea4369dde22859.
    
    * [EXCH-10877] Add info about native support to the docs
    
    * [EXCH-10877] create const, use null instead of undefined
    
    * [EXCH-10877] Add support for native to OpenX bid adapter
    
    * [EXCH-10877] Add unit tests for interpretResponse and native
    
    * [EXCH-10877] Fix formatting
    
    * [EXCH-10877] Add support for native to OpenX bid adapter
    
    * [EXCH-10877] Add unit tests for interpretResponse and native
    
    * [EXCH-10877] Fix formatting
    
    * Revert "Merge remote-tracking branch 'origin/EXCH-10877-add-support-for-native-imps' into EXCH-10877-add-support-for-native-imps"
    
    This reverts commit c88138628dda3ac9409c1846d2baaf022521c4d6, reversing
    changes made to 55a23a5f526e2a4d2cebb4fe4bea4369dde22859.
    
    * [EXCH-10877] Add info about native support to the docs
    
    * [EXCH-10877] create const, use null instead of undefined
    
    * [EXCH-10877] Fix merge issues
    
    * [EXCH-10877] Use various params.platform in test

--- modules/openxBidAdapter.js
+++ modules/openxBidAdapter.js
@@ -2,7 +2,7 @@ import {config} from '../src/config.js';
 import {registerBidder} from '../src/adapters/bidderFactory.js';
 import * as utils from '../src/utils.js';
 import {mergeDeep} from '../src/utils.js';
-import {BANNER, VIDEO} from '../src/mediaTypes.js';
+import {BANNER, NATIVE, VIDEO} from '../src/mediaTypes.js';
 import {ortbConverter} from '../libraries/ortbConverter/converter.js';
 
 const bidderConfig = 'hb_pb_ortb';
@@ -13,7 +13,7 @@ export const DEFAULT_PH = '2d1251ae-7f3a-47cf-bd2a-2f288854a0ba';
 export const spec = {
   code: 'openx',
   gvlid: 69,
-  supportedMediaTypes: [BANNER, VIDEO],
+  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
   isBidRequestValid,
   buildRequests,
   interpretResponse,
@@ -25,7 +25,12 @@ registerBidder(spec);
 const converter = ortbConverter({
   context: {
     netRevenue: true,
-    ttl: 300
+    ttl: 300,
+    nativeRequest: {
+      eventtrackers: [
+        {event: 1, methods: [1, 2]},
+      ]
+    }
   },
   imp(buildImp, bidRequest, context) {
     const imp = buildImp(bidRequest, context);
@@ -158,8 +163,11 @@ function isBidRequestValid(bidRequest) {
 
 function buildRequests(bids, bidderRequest) {
   let videoBids = bids.filter(bid => isVideoBid(bid));
-  let bannerBids = bids.filter(bid => isBannerBid(bid));
-  let requests = bannerBids.length ? [createRequest(bannerBids, bidderRequest, BANNER)] : [];
+  let bannerAndNativeBids = bids.filter(bid => isBannerBid(bid) || isNativeBid(bid))
+    // In case of multi-format bids remove `video` from mediaTypes as for video a separate bid request is built
+    .map(bid => ({...bid, mediaTypes: {...bid.mediaTypes, video: undefined}}));
+
+  let requests = bannerAndNativeBids.length ? [createRequest(bannerAndNativeBids, bidderRequest, null)] : [];
   videoBids.forEach(bid => {
     requests.push(createRequest([bid], bidderRequest, VIDEO));
   });
@@ -178,8 +186,13 @@ function isVideoBid(bid) {
   return utils.deepAccess(bid, 'mediaTypes.video');
 }
 
+function isNativeBid(bid) {
+  return utils.deepAccess(bid, 'mediaTypes.native');
+}
+
 function isBannerBid(bid) {
-  return utils.deepAccess(bid, 'mediaTypes.banner') || !isVideoBid(bid);
+  const isNotVideoOrNativeBid = !isVideoBid(bid) && !isNativeBid(bid)
+  return utils.deepAccess(bid, 'mediaTypes.banner') || isNotVideoOrNativeBid;
 }
 
 function interpretResponse(resp, req) {
