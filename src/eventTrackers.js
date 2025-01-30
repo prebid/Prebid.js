428ace82ad4dfb6369ad4475bef0c72ab58eaba9
Demetrio Girardi <dgirardi@prebid.org>
2025-01-30 10:15:46 -0800

    try to appease jsdoc

--- src/eventTrackers.js
+++ src/eventTrackers.js
@@ -10,7 +10,7 @@ export const EVENT_TYPE_WIN = 500;
  *
  * @param {{}[]} eventTrackers an array of "Event Tracker Response Object" as defined
  *  in the ORTB native 1.2 spec (https://www.iab.com/wp-content/uploads/2018/03/OpenRTB-Native-Ads-Specification-Final-1.2.pdf, section 5.8)
- * @returns {{[type]: {[method]: string[]}}}
+ * @returns {{[type: string]: {[method: string]: string[]}}}
  */
 export function parseEventTrackers(eventTrackers) {
   return (eventTrackers ?? []).reduce((tally, {event, method, url}) => {
