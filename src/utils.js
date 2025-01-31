bb2a8a5f10dd60a76e16fd5ed39b50284e454b55
Patrick McCann <patmmccann@gmail.com>
2025-01-31 11:02:15 -0500

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1042,6 +1042,9 @@ function mergeDeepHelper(target, source) {
   const keys = Object.keys(source);
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
+    if (key === "__proto__" || key === "constructor") {
+      continue;
+    }
     const val = source[key];
 
     if (isPlainObject(val)) {
