69ff2606bdb1b580a31ef3fc2405ee25fe777848
BaronJHYu <254878848@qq.com>
2025-02-12 17:04:10 +0800

    Mediago Bid Adapter : add param publisherid

--- modules/discoveryBidAdapter.js
+++ modules/discoveryBidAdapter.js
@@ -484,7 +484,7 @@ export const spec = {
   },
 
   getUserSyncs: function (syncOptions, serverResponse, gdprConsent, uspConsent, gppConsent) {
-    cookieSync(syncOptions, gdprConsent, uspConsent, BIDDER_CODE, THIRD_PARTY_COOKIE_ORIGIN, getCookieTimeToUTCString());
+    return cookieSync(syncOptions, gdprConsent, uspConsent, BIDDER_CODE, THIRD_PARTY_COOKIE_ORIGIN, getCookieTimeToUTCString());
   },
 
   /**
