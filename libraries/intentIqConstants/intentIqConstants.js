bf0d917c129366449d4a01d054145a07be3a1e44
dmytro-po <dmytro.po@intentiq.com>
2025-02-26 17:09:07 +0200

    AGT-389: Change version

--- libraries/intentIqConstants/intentIqConstants.js
+++ libraries/intentIqConstants/intentIqConstants.js
@@ -8,4 +8,4 @@ export const BLACK_LIST = 'L';
 export const CLIENT_HINTS_KEY = '_iiq_ch';
 export const EMPTY = 'EMPTY';
 export const GVLID = '1323';
-export const VERSION = 0.26;
+export const VERSION = 0.27;
