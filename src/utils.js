108a5c2e3265baf0384adf052b8ea9e064515c73
Harshad Mane <harshad.mane@pubmatic.com>
2025-03-05 14:31:09 -0800

    we do not need isJsonObject util function

--- src/utils.js
+++ src/utils.js
@@ -1440,14 +1440,3 @@ export async function compressDataWithGZip(data) {
   //   compressedBlob
   // };
 }
-
-export function isJsonObject(value) {
-  return typeof value === 'object' &&
-         value !== null &&
-         !Array.isArray(value) &&
-         !(value instanceof Date) &&
-         !(value instanceof Blob) &&
-         !(value instanceof FormData) &&
-         !(value instanceof Map) &&
-         !(value instanceof Set);
-}
