402fe52791af73999712921d37a7fae325824ead
ThinhLy <giathinh.ly@urekamedia.vn>
2025-03-19 12:00:03 +0700

    init mediaEyesBidAdapter

---
+++ modules/mediaEyesBidAdapter.js
@@ -0,0 +1,97 @@
+import {
+  BANNER
+} from '../src/mediaTypes.js';
+import {
+  registerBidder
+} from '../src/adapters/bidderFactory.js';
+import { deepAccess, generateUUID } from '../src/utils.js';
+
+const ENDPOINT_URL = 'https://delivery.upremium.asia/ortb/open/auction';
+
+export const spec = {
+  code: 'mediaeyes',
+  supportedMediaTypes: BANNER,
+
+  isBidRequestValid: (bid) => {
+    return !!(bid.params.item_id);
+  },
+
+  buildRequests: (bidRequests, bidderRequest) => {
+    let requests = [];
+
+    bidRequests.map(bidRequest => {
+      let {item_id} = bidRequest.params;
+      let requestData = {
+        id: generateUUID(),
+        imp: [cookingImp(bidRequest)],
+        device: bidRequest.ortb2?.device,
+        site: bidRequest.ortb2?.site,
+      }
+      requests.push({
+        method: 'POST',
+        url: ENDPOINT_URL + "?item_id=" + item_id,
+        data: JSON.stringify(requestData),
+        options: {
+          contentType: 'application/json',
+        }
+      });
+    })
+
+    return requests
+  },
+
+  interpretResponse: (serverResponse, serverRequest) => {
+    let response = serverResponse.body;
+    if (!response.seatbid) {
+      return [];
+    }
+
+    let rtbBids = response.seatbid
+      .map(seatbid => seatbid.bid)
+      .reduce((a, b) => a.concat(b), []);
+
+    let data = rtbBids.map(rtbBid => {
+      let prBid = {
+        requestId: rtbBid.impid,
+        cpm: rtbBid.price,
+        creativeId: rtbBid.crid,
+        currency: response.cur || 'USD',
+        ttl: 360,
+        netRevenue: true
+      };
+
+      prBid.mediaType = BANNER;
+      prBid.width = rtbBid.w;
+      prBid.height = rtbBid.h;
+      prBid.ad = rtbBid.adm;
+
+      return prBid
+    });
+
+    return data
+  }
+}
+
+registerBidder(spec);
+
+function cookingImp(bidReq) {
+  let imp = {};
+  if (bidReq) {
+    let bidfloor = deepAccess(bidReq, 'params.bid_floor', 0)
+
+    imp.id = bidReq.bidId;
+    imp.bidfloor = bidfloor;
+    imp.banner = cookImpBanner(bidReq);
+  }
+  return imp;
+}
+
+const cookImpBanner = ({ mediaTypes, params }) => {
+  if (!mediaTypes?.banner) return {};
+
+  const { sizes } = mediaTypes.banner;
+  return {
+    h: params.height || sizes?.[0]?.[1] || undefined,
+    w: params.width || sizes?.[0]?.[0] || undefined,
+  };
+};
---
+++ test/spec/modules/mediaEyesBidAdapter_spec.js
@@ -0,0 +1,80 @@
+import { expect } from 'chai';
+import { spec } from '../../../modules/mediaEyesBidAdapter.js';
+import * as utils from '../../../src/utils.js';
+
+describe('mediaeyes adapter', function () {
+    let request;
+    let bannerResponse, invalidResponse;
+
+    beforeEach(function () {
+        request = [
+            {
+                bidder: 'mediaeyes',
+                mediaTypes: {
+                    banner: {
+                        sizes: [[300, 250]]
+                    }
+                },
+                params: {
+                    item_id: 'ec1d7389a4a5afa28a23c4',
+                    bid_floor: 0.1
+                }
+            }
+        ];
+        bannerResponse = {
+            'body': {
+                "id": "6b34f7cc-77d9-4c57-849a-4a2d6e9ca983",
+                "seatbid": [
+                    {
+                        "bid": [
+                            {
+                                "impid": "3d5d745d841579",
+                                "adm": "<img src='https://rtb-useast-v4.upremium.asia/win?i=Y9JuqDwhMOo_0&p=1742356725.249865&price=${AUCTION_PRICE}&f=imp' alt=' ' style='display:none'><a href=\"https://rtb-useast-v4.upremium.asia/click?i=Y9JuqDwhMOo_0&p=1742356725.249865\" target=\"_blank\"><img src=\"https://static.upremium.asia/n1191/ad/300x250_OWMrIjJQ.jpg\" width=\"300\" height=\"250\" border=\"0\" ></a>",
+                                "iurl": "https://static.upremium.asia/n1191/ad/300x250_OWMrIjJQ.jpg",
+                                "h": 250,
+                                "w": 300,
+                                "price": 0.25,
+                                "crid": "6808551"
+                            }
+                        ]
+                    }
+                ]
+            }
+        };
+        invalidResponse = {
+            'body': {
+
+            }
+        };
+    });
+
+    describe('validations', function () {
+        it('isBidValid : item_id is passed', function () {
+            let bid = {
+                bidder: 'mediaeyes',
+                params: {
+                    item_id: 'ec1d7389a4a5afa28a23c4',
+                }
+            },
+                isValid = spec.isBidRequestValid(bid);
+            expect(isValid).to.equals(true);
+        });
+        it('isBidValid : item_id is not passed', function () {
+            let bid = {
+                bidder: 'mediaeyes',
+                params: {
+
+                }
+            },
+                isValid = spec.isBidRequestValid(bid);
+            expect(isValid).to.equals(false);
+        });
+    });
+    describe('Validate Request', function () {
+        it('Immutable bid request validate', function () {
+            let _Request = utils.deepClone(request),
+                bidRequest = spec.buildRequests(request);
+            expect(request).to.deep.equal(_Request);
+        });
+    });
+});
