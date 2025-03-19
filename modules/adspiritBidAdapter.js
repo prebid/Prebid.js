552d0964b33467ae93f5acb2482f360b7948dc6f
carsten1980 <45483737+carsten1980@users.noreply.github.com>
2025-03-19 18:18:07 +0100

    Update adspiritBidAdapter.js
    Updated the assets field with default values for title, image, and data.

--- modules/adspiritBidAdapter.js
+++ modules/adspiritBidAdapter.js
@@ -67,7 +67,16 @@ export const spec = {
           native: (bidRequest.mediaTypes.native) ? {
             request: JSON.stringify({
               ver: '1.2',
-              assets: bidRequest.mediaTypes?.native?.ortb?.request?.assets || []
+              assets: bidRequest.mediaTypes.native.ortb?.assets?.length
+                ? bidRequest.mediaTypes.native.ortb.assets
+                : [
+                  { id: 1, required: 1, title: { len: 100 } },
+                  { id: 2, required: 1, img: {type: 3, wmin: 1200, hmin: 627, mimes: ['image/png', 'image/gif', 'image/jpeg']} },
+                  { id: 4, required: 1, data: {type: 2, len: 150} },
+                  { id: 3, required: 0, data: {type: 12, len: 50} },
+                  { id: 6, required: 0, data: {type: 1, len: 50} },
+                  { id: 5, required: 0, img: {type: 1, wmin: 50, hmin: 50, mimes: ['image/png', 'image/gif', 'image/jpeg'] }}
+                ]
             })
           } : undefined,
           ext: {
@@ -126,6 +135,7 @@ export const spec = {
           }
         };
       }
+
       requests.push({
         method: 'POST',
         url: reqUrl,
@@ -165,7 +175,6 @@ export const spec = {
           };
 
           let adm = bid.adm;
-
           if (typeof adm === 'string' && adm.trim().startsWith('{')) {
             adm = JSON.parse(adm || '{}');
             if (typeof adm !== 'object') adm = null;
