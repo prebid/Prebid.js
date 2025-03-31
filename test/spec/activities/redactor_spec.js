daad1833b8fdd8b4e2391932137f6c5a715cac80
Patrick McCann <patmmccann@gmail.com>
2025-03-31 17:00:32 -0400

    Update redactor_spec.js

--- test/spec/activities/redactor_spec.js
+++ test/spec/activities/redactor_spec.js
@@ -37,7 +37,7 @@ describe('objectTransformer', () => {
     });
 
     it('does not run rule once it is known that it does not apply', () => {
-      applies.reset();
+      applies.resetHistory();
       applies.callsFake(() => false);
       run.callsFake((_1, _2, _3, _4, applies) => applies());
       objectTransformer([rule])({}, {});
