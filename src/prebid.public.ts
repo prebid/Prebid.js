bd8f1c5ef108aeef8f740a098539d9e6951e59d0
Demetrio Girardi <dgirardi@prebid.org>
2025-04-16 15:24:12 -0700

    Fix ignored type declarations

--- src/prebid.public.ts
+++ src/prebid.public.ts
@@ -1,3 +1,13 @@
 export {default} from './prebid.ts';
 export type {PrebidJS} from './prebidGlobal.ts';
 export type {Bid, VideoBid, BannerBid, NativeBid} from './bidfactory.ts';
+
+// a ts file importing other ts files (and so on) picks up type declarations along the way,
+// but a ts file importing js importing ts does not.
+// these imports should no longer be needed once they can be reached all the way from prebid.ts
+// through only ts files.
+
+/* eslint-disable import-x/first */
+import './auction.ts'
+
+
