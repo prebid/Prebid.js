de52d3ad57ab28d912a05f1a416bfec5f4238ddd
boris-polyakov-dv <boris.p@dvgroup.com>
2025-04-25 01:07:26 +0300

    dvgroup bid adapter

---
+++ modules/dvgroupBidAdapter.js
@@ -0,0 +1,99 @@
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER, VIDEO } from '../src/mediaTypes.js';
+import { hasPurpose1Consent } from '../src/utils/gdpr.js';
+import {deepAccess, deepClone, deepSetValue, replaceAuctionPrice} from '../src/utils.js';
+import { ortbConverter } from '../libraries/ortbConverter/converter.js';
+
+const BIDDER_CODE = 'dvgroup';
+const DEFAULT_ENDPOINT = 'rtb.dvgroup.com';
+const SYNC_ENDPOINT = 'sync.dvgroup.com';
+const SSP_PREBID_UID = 'prebidssp';
+const ADOMAIN = 'dvgroup.com';
+const TIME_TO_LIVE = 360;
+
+const converter = ortbConverter({
+  context: {
+    netRevenue: true,
+    ttl: 30
+  },
+  request(buildRequest, imps, bidderRequest, context) {
+    const request = buildRequest(imps, bidderRequest, context);
+    deepSetValue(request, 'ext.prebid', true);
+
+    return request;
+  },
+  bidResponse(buildBidResponse, bid, context) {
+    const bidResponse = buildBidResponse(bid, context);
+    bidResponse.adm = replaceAuctionPrice(bidResponse.adm, bidResponse.price);
+    bidResponse.burl = replaceAuctionPrice(bidResponse.burl, bidResponse.price);
+    bidResponse.nurl = replaceAuctionPrice(bidResponse.nurl, bidResponse.price);
+
+    return bidResponse;
+  }
+});
+
+export const spec = {
+  code: BIDDER_CODE,
+
+  buildRequests: function(bids, bidderRequest) {
+    return bids.map((bid) => {
+      let endpoint = bid.params.endpoint || DEFAULT_ENDPOINT;
+      return {
+        method: 'POST',
+        url: `https://${endpoint}/bid?sspuid=${SSP_PREBID_UID}`,
+        data: converter.toORTB({
+          bidRequests: [bid],
+          bidderRequest: deepClone(bidderRequest),
+          context: {
+            mediaType: deepAccess(bid, 'mediaTypes.video') ? VIDEO : BANNER
+          },
+        }),
+      };
+    });
+  },
+
+  interpretResponse: function(response, request) {
+    if (!response?.body) {
+      return [];
+    }
+
+    const bids = converter.fromORTB({response: response.body, request: request.data}).bids;
+    bids.forEach((bid) => {
+      bid.meta = bid.meta || {};
+      bid.meta.advertiserDomains = bid.meta.advertiserDomains || [];
+      if (bid.meta.advertiserDomains.length == 0) {
+        bid.meta.advertiserDomains.push(ADOMAIN);
+      }
+
+      bid.ttl = bid.ttl || TIME_TO_LIVE;
+    });
+
+    return bids;
+  },
+
+    getUserSyncs: function(syncOptions, serverResponses, gdprConsent, uspConsent) {
+      const syncs = []
+
+      if (!hasPurpose1Consent(gdprConsent)) {
+        return syncs;
+      }
+
+      let params = `us_privacy=${uspConsent || ''}&gdpr_consent=${gdprConsent?.consentString ? gdprConsent.consentString : ''}`;
+      if (typeof gdprConsent?.gdprApplies === 'boolean') {
+        params += `&gdpr=${Number(gdprConsent.gdprApplies)}`;
+      }
+
+      if (syncOptions.pixelEnabled) {
+        syncs.push({
+          type: 'image',
+          url: `//${SYNC_ENDPOINT}/match/sp?${params}`
+        });
+      }
+
+      return syncs;
+    },
+
+    supportedMediaTypes: [ BANNER, VIDEO ]
+}
+
+registerBidder(spec);
