62fe076a81616560fcb2f44e139ea935e91eb053
Andrey Filipov <andrey@filipov.dev>
2025-05-05 11:01:51 +0300

    Yandex Bid Adapter : add support for video ads

--- modules/yandexBidAdapter.js
+++ modules/yandexBidAdapter.js
@@ -1,6 +1,6 @@
 import { getCurrencyFromBidderRequest } from '../libraries/ortb2Utils/currency.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
-import { BANNER, NATIVE } from '../src/mediaTypes.js';
+import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
 import { convertOrtbRequestToProprietaryNative } from '../src/native.js';
 import { _each, _map, deepAccess, deepSetValue, formatQS, triggerPixel, logInfo } from '../src/utils.js';
 
