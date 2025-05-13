bc95a68dfddbd591562014e009bbe40cfde39924
mkomorski <marcin.komorski@pollub.edu.pl>
2025-05-13 12:22:12 +0200

    moving accessRequestCredentials rule to storage manager

--- modules/tcfControl.js
+++ modules/tcfControl.js
@@ -23,7 +23,6 @@ import {
 import {registerActivityControl} from '../src/activities/rules.js';
 import {
   ACTIVITY_ACCESS_DEVICE,
-  ACTIVITY_ACCESS_REQUEST_CREDENTIALS,
   ACTIVITY_ENRICH_EIDS,
   ACTIVITY_ENRICH_UFPD,
   ACTIVITY_FETCH_BIDS,
@@ -271,13 +270,6 @@ export const accessDeviceRule = ((rule) => {
   };
 })(singlePurposeGdprRule(1, storageBlocked));
 
-export const accessRequestCredentialsRule = ((rule) => {
-  return function (params) {
-    if (!hasDeviceAccess()) return {allow: false};
-    return rule(params);
-  };
-})(singlePurposeGdprRule(1, storageBlocked));
-
 export const syncUserRule = singlePurposeGdprRule(1, storageBlocked);
 export const enrichEidsRule = singlePurposeGdprRule(1, storageBlocked);
 export const fetchBidsRule = exceptPrebidModules(singlePurposeGdprRule(2, biddersBlocked));
@@ -356,7 +348,6 @@ export function setEnforcementConfig(config) {
       RULE_HANDLES.push(registerActivityControl(ACTIVITY_ACCESS_DEVICE, RULE_NAME, accessDeviceRule));
       RULE_HANDLES.push(registerActivityControl(ACTIVITY_SYNC_USER, RULE_NAME, syncUserRule));
       RULE_HANDLES.push(registerActivityControl(ACTIVITY_ENRICH_EIDS, RULE_NAME, enrichEidsRule));
-      RULE_HANDLES.push(registerActivityControl(ACTIVITY_ACCESS_REQUEST_CREDENTIALS, RULE_NAME, accessRequestCredentialsRule));
       processRequestOptions.before(checkIfCredentialsAllowed);
     }
     if (ACTIVE_RULES.purpose[2] != null) {
