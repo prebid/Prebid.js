1e093984e24ad36e5afbb315b32b5db070af5680
Soman Sarker <gao.gram@gmail.com>
2025-05-20 16:30:01 +0600

    Updated logics

--- modules/adgridBidAdapter.js
+++ modules/adgridBidAdapter.js
@@ -1,189 +1,265 @@
-import { _each, isEmpty, deepAccess } from '../src/utils.js';
-import { config } from '../src/config.js';
+import { deepAccess, deepSetValue, generateUUID, logInfo } from '../src/utils.js';
+import { Renderer } from '../src/Renderer.js';
+import { getStorageManager } from '../src/storageManager.js';
 import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, VIDEO } from '../src/mediaTypes.js';
+import { ortbConverter } from '../libraries/ortbConverter/converter.js'
+import { INSTREAM, OUTSTREAM } from '../src/video.js';
+import { getCurrencyFromBidderRequest } from '../libraries/ortb2Utils/currency.js';
 
-const BIDDER = Object.freeze({
-  CODE: 'adgrid',
-  HOST: 'https://api-prebid.adgrid.io',
-  REQUEST_METHOD: 'POST',
-  REQUEST_ENDPOINT: '/api/v1/auction',
-  SUPPORTED_MEDIA_TYPES: [BANNER, VIDEO],
+/**
+ * @typedef {import('../src/adapters/bidderFactory.js').BidRequest} BidRequest
+ * @typedef {import('../src/adapters/bidderFactory.js').Bid} Bid
+ * @typedef {import('../src/adapters/bidderFactory.js').ServerResponse} ServerResponse
+ * @typedef {import('../src/adapters/bidderFactory.js').SyncOptions} SyncOptions
+ * @typedef {import('../src/adapters/bidderFactory.js').UserSync} UserSync
+ * @typedef {import('../src/adapters/bidderFactory.js').validBidRequests} validBidRequests
+ */
+
+const OUTSTREAM_RENDERER_URL = 'https://acdn.adnxs.com/video/outstream/ANOutstreamVideo.js';
+
+const BIDDER_CODE = 'adgrid';
+const REQUEST_URL = 'https://fast.nexx360.io/adgrid';
+const PAGE_VIEW_ID = generateUUID();
+const BIDDER_VERSION = '2.0';
+const ADGRID_KEY = 'adgrid';
+
+const ALIASES = [];
+
+export const storage = getStorageManager({
+  bidderCode: BIDDER_CODE,
 });
 
-const CURRENCY = Object.freeze({
-  KEY: 'currency',
-  US_DOLLAR: 'USD',
+/**
+ * Get the agdridId from local storage
+ * @param
+ * @return {object | false } false if localstorageNotEnabled
+ */
+
+export function getLocalStorage() {
+  if (!storage.localStorageIsEnabled()) {
+    logInfo(`localstorage not enabled for Adgrid`);
+    return false;
+  }
+  const output = storage.getDataFromLocalStorage(ADGRID_KEY);
+  if (output === null) {
+    const adgridStorage = { adgridId: generateUUID() };
+    storage.setDataInLocalStorage(ADGRID_KEY, JSON.stringify(adgridStorage));
+    return adgridStorage;
+  }
+  try {
+    return JSON.parse(output);
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Get the AMX ID
+ * @return { string | false } false if localstorageNotEnabled
+ */
+export function getAmxId() {
+  if (!storage.localStorageIsEnabled()) {
+    logInfo(`localstorage not enabled for Adgrid`);
+    return false;
+  }
+  const amxId = storage.getDataFromLocalStorage('__amuidpb');
+  return amxId || false;
+}
+
+const converter = ortbConverter({
+  context: {
+    netRevenue: true, // or false if your adapter should set bidResponse.netRevenue = false
+    ttl: 90, // default bidResponse.ttl (when not specified in ORTB response.seatbid[].bid[].exp)
+  },
+  imp(buildImp, bidRequest, context) {
+    const imp = buildImp(bidRequest, context);
+    deepSetValue(imp, 'tagid', bidRequest.adUnitCode);
+    deepSetValue(imp, 'ext.adUnitCode', bidRequest.adUnitCode);
+    if (bidRequest.params.domainId) deepSetValue(imp, 'ext.adgrid.domainId', bidRequest.params.domainId);
+    if (bidRequest.params.placement) deepSetValue(imp, 'ext.adgrid.placement', bidRequest.params.placement);
+
+    if (imp.video) {
+      const playerSize = deepAccess(bidRequest, 'mediaTypes.video.playerSize');
+      const videoContext = deepAccess(bidRequest, 'mediaTypes.video.context');
+      deepSetValue(imp, 'video.ext.playerSize', playerSize);
+      deepSetValue(imp, 'video.ext.context', videoContext);
+    }
+    return imp;
+  },
+  request(buildRequest, imps, bidderRequest, context) {
+    const request = buildRequest(imps, bidderRequest, context);
+    const localStorage = getLocalStorage();
+    if (localStorage) {
+      deepSetValue(request, 'ext.localStorage.adgridId', localStorage.adgridId);
+    }
+    const amxId = getAmxId();
+    if (amxId) deepSetValue(request, 'ext.localStorage.amxId', amxId);
+    deepSetValue(request, 'ext.version', '$prebid.version$');
+    deepSetValue(request, 'ext.source', 'prebid.js');
+    deepSetValue(request, 'ext.pageViewId', PAGE_VIEW_ID);
+    deepSetValue(request, 'ext.bidderVersion', BIDDER_VERSION);
+    deepSetValue(request, 'cur', [getCurrencyFromBidderRequest(bidderRequest) || 'USD']);
+    if (!request.user) request.user = {};
+    if (getAmxId()) {
+      if (!request.user.ext) request.user.ext = {};
+      if (!request.user.ext.eids) request.user.ext.eids = [];
+      request.user.ext.eids.push({
+        source: 'amxdt.net',
+        uids: [{
+          id: `${getAmxId()}`,
+          atype: 1
+        }]
+      });
+    }
+
+    return request;
+  },
 });
 
+/**
+ * Determines whether or not the given bid request is valid.
+ *
+ * @param {BidRequest} bid The bid params to validate.
+ * @return boolean True if this is a valid bid, and false otherwise.
+ */
 function isBidRequestValid(bid) {
   if (!bid || !bid.params) {
     return false;
   }
+  if (typeof bid.params.domainId !== 'number') {
+    return false;
+  }
 
-  return !!bid.params.domainId;
+  if (typeof bid.params.placement !== 'string') {
+    return false;
+  }
+  return true;
+}
+
+
+/**
+ * Make a server request from the list of BidRequests.
+ *
+ * @return ServerRequest Info describing the request to the server.
+ */
+
+function buildRequests(bidRequests, bidderRequest) {
+  const data = converter.toORTB({ bidRequests, bidderRequest })
+  return {
+    method: 'POST',
+    url: REQUEST_URL,
+    data,
+  }
 }
 
 /**
- * Return some extra params
+ * Unpack the response from the server into a list of bids.
+ *
+ * @param {ServerResponse} serverResponse A successful response from the server.
+ * @return {Bid[]} An array of bids which were nested inside the server.
  */
-function getAudience(validBidRequests, bidderRequest) {
-  const params = {
-    domain: deepAccess(bidderRequest, 'refererInfo.page')
-  };
 
-  if (deepAccess(bidderRequest, 'gdprConsent.gdprApplies')) {
-    params.gdpr = 1;
-    params.gdprConsent = deepAccess(bidderRequest, 'gdprConsent.consentString');
+function interpretResponse(serverResponse) {
+  const respBody = serverResponse.body;
+  if (!respBody || !Array.isArray(respBody.seatbid)) {
+    return [];
   }
 
-  if (deepAccess(bidderRequest, 'uspConsent')) {
-    params.usp = deepAccess(bidderRequest, 'uspConsent');
-  }
+  let responses = [];
+  for (let i = 0; i < respBody.seatbid.length; i++) {
+    const seatbid = respBody.seatbid[i];
+    for (let j = 0; j < seatbid.bid.length; j++) {
+      const bid = seatbid.bid[j];
+      const response = {
+        requestId: bid.impid,
+        cpm: bid.price,
+        width: bid.w,
+        height: bid.h,
+        creativeId: bid.crid,
+        currency: respBody.cur,
+        netRevenue: true,
+        ttl: 120,
+        mediaType: [OUTSTREAM, INSTREAM].includes(bid.ext.mediaType) ? 'video' : bid.ext.mediaType,
+        meta: {
+          advertiserDomains: bid.adomain,
+          demandSource: bid.ext.ssp,
+        },
+      };
+      if (bid.dealid) response.dealid = bid.dealid;
 
-  if (deepAccess(validBidRequests[0], 'schain')) {
-    params.schain = deepAccess(validBidRequests[0], 'schain');
-  }
+      if (bid.ext.mediaType === BANNER) response.ad = bid.adm;
+      if ([INSTREAM, OUTSTREAM].includes(bid.ext.mediaType)) response.vastXml = bid.adm;
 
-  if (deepAccess(validBidRequests[0], 'userId')) {
-    params.userIds = deepAccess(validBidRequests[0], 'userId');
-  }
-
-  if (deepAccess(validBidRequests[0], 'userIdAsEids')) {
-    params.userEids = deepAccess(validBidRequests[0], 'userIdAsEids');
-  }
-
-  if (bidderRequest.gppConsent) {
-    params.gpp = bidderRequest.gppConsent.gppString;
-    params.gppSid = bidderRequest.gppConsent.applicableSections?.toString();
-  } else if (bidderRequest.ortb2?.regs?.gpp) {
-    params.gpp = bidderRequest.ortb2.regs.gpp;
-    params.gppSid = bidderRequest.ortb2.regs.gpp_sid;
-  }
-
-  return params;
-}
-
-function buildRequests(validBidRequests, bidderRequest) {
-  const currencyObj = config.getConfig(CURRENCY.KEY);
-  const currency = (currencyObj && currencyObj.adServerCurrency) ? currencyObj.adServerCurrency : 'USD';
-  const bids = [];
-
-  _each(validBidRequests, bid => {
-    bids.push(getBidData(bid))
-  });
-
-  const bidsParams = Object.assign({}, {
-    url: window.location.href,
-    timeout: bidderRequest.timeout,
-    ts: new Date().getTime(),
-    device: {
-      size: [
-        window.screen.width,
-        window.screen.height
-      ]
-    },
-    bids
-  });
-
-  // Add currency if not USD
-  if (currency != null && currency != CURRENCY.US_DOLLAR) {
-    bidsParams.cur = currency;
-  }
-
-  bidsParams.audience = getAudience(validBidRequests, bidderRequest);
-
-  // Passing geo location data if found in prebid config
-  bidsParams.geodata = config.getConfig('adgGeodata') || {};
-
-  return Object.assign({}, bidderRequest, {
-    method: BIDDER.REQUEST_METHOD,
-    url: `${BIDDER.HOST}${BIDDER.REQUEST_ENDPOINT}`,
-    data: bidsParams,
-    currency: currency,
-    options: {
-      withCredentials: false,
-      contentType: 'application/json'
+      if (bid.ext.mediaType === OUTSTREAM) {
+        response.renderer = createRenderer(bid, OUTSTREAM_RENDERER_URL);
+        if (bid.ext.divId) response.divId = bid.ext.divId
+      };
+      responses.push(response);
     }
-  });
+  }
+  return responses;
 }
 
-function interpretResponse(response, bidRequest) {
-  let bids = response.body;
-  const bidResponses = [];
-
-  if (isEmpty(bids)) {
-    return bidResponses;
+/**
+ * Register the user sync pixels which should be dropped after the auction.
+ *
+ * @param {SyncOptions} syncOptions Which user syncs are allowed?
+ * @param {ServerResponse[]} serverResponses List of server's responses.
+ * @return {UserSync[]} The user syncs which should be dropped.
+ */
+function getUserSyncs(syncOptions, serverResponses, gdprConsent, uspConsent) {
+  if (typeof serverResponses === 'object' &&
+    serverResponses != null &&
+    serverResponses.length > 0 &&
+    serverResponses[0].hasOwnProperty('body') &&
+    serverResponses[0].body.hasOwnProperty('ext') &&
+    serverResponses[0].body.ext.hasOwnProperty('cookies') &&
+    typeof serverResponses[0].body.ext.cookies === 'object') {
+    return serverResponses[0].body.ext.cookies.slice(0, 5);
+  } else {
+    return [];
   }
+};
 
-  if (typeof bids !== 'object') {
-    return bidResponses;
-  }
-
-  bids = bids.bids;
-
-  bids.forEach((adUnit) => {
-    const bidResponse = {
-      requestId: adUnit.bidId,
-      cpm: Number(adUnit.cpm),
-      width: adUnit.width,
-      height: adUnit.height,
-      ttl: 300,
-      creativeId: adUnit.creativeId,
-      netRevenue: true,
-      currency: adUnit.currency || bidRequest.currency,
-      mediaType: adUnit.mediaType
-    };
-
-    if (adUnit.mediaType == 'video') {
-      if (adUnit.admUrl) {
-        bidResponse.vastUrl = adUnit.admUrl;
-      } else {
-        bidResponse.vastXml = adUnit.adm;
+function outstreamRender(response) {
+  response.renderer.push(() => {
+    window.ANOutstreamVideo.renderAd({
+      sizes: [response.width, response.height],
+      targetId: response.divId,
+      adResponse: response.vastXml,
+      rendererOptions: {
+        showBigPlayButton: false,
+        showProgressBar: 'bar',
+        showVolume: false,
+        allowFullscreen: true,
+        skippable: false,
+        content: response.vastXml
       }
-    } else {
-      bidResponse.ad = adUnit.ad;
-    }
-
-    bidResponses.push(bidResponse);
+    });
   });
-
-  return bidResponses;
 }
 
-function getBidData(bid) {
-  const bidData = {
-    requestId: bid.bidId,
-    tid: bid.ortb2Imp?.ext?.tid,
-    deviceW: bid.ortb2?.device?.w,
-    deviceH: bid.ortb2?.device?.h,
-    deviceUa: bid.ortb2?.device?.ua,
-    domain: bid.ortb2?.site?.publisher?.domain,
-    domainId: bid.params.domainId,
-    code: bid.adUnitCode
-  };
-
-  if (bid.mediaTypes != null) {
-    if (bid.mediaTypes.banner != null) {
-      bidData.mediaType = 'banner';
-      bidData.sizes = bid.mediaTypes.banner.sizes;
-    } else if (bid.mediaTypes.video != null) {
-      bidData.mediaType = 'video';
-      bidData.sizes = bid.mediaTypes.video.playerSize;
-      bidData.videoData = bid.mediaTypes.video;
-      bidData.videoParams = bid.params.video;
-    }
-  }
-
-  return bidData;
+function createRenderer(bid, url) {
+  const renderer = Renderer.install({
+    id: bid.id,
+    url: url,
+    loaded: false,
+    adUnitCode: bid.ext.adUnitCode,
+    targetId: bid.ext.divId,
+  });
+  renderer.setRender(outstreamRender);
+  return renderer;
 }
 
 export const spec = {
-  code: BIDDER.CODE,
+  code: BIDDER_CODE,
+  aliases: ALIASES,
+  supportedMediaTypes: [BANNER, VIDEO],
   isBidRequestValid,
   buildRequests,
   interpretResponse,
-  supportedMediaTypes: BIDDER.SUPPORTED_MEDIA_TYPES
+  getUserSyncs,
 };
 
 registerBidder(spec);
