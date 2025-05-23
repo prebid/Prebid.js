ea80a3642766f6a42292c7a59cf1804158674c69
Shivam Soni <shivam.soni@pubmatic.com>
2025-05-23 13:22:56 +0530

    fixed jsdoc issue for pubmaticBidAdapter and pubmaticRtdProvider modules

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -688,8 +688,9 @@ export const spec = {
   /**
    * Make a server request from the list of BidRequests.
    *
-   * @param {validBidRequests} - an array of bids
-   * @return ServerRequest Info describing the request to the server.
+   * @param {Array} validBidRequests - an array of bids
+   * @param {Object} bidderRequest - bidder request object
+   * @return {ServerRequest} Info describing the request to the server.
    */
   buildRequests: (validBidRequests, bidderRequest) => {
     const { page, ref } = bidderRequest?.refererInfo || {};
