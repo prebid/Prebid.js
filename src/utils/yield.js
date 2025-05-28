3a8469dd918109391edf9e24daea376e7f071887
Patrick McCann <patmmccann@gmail.com>
2025-05-28 15:46:01 -0400

    Update yield.js

--- src/utils/yield.js
+++ src/utils/yield.js
@@ -1,7 +1,5 @@
-import {getGlobal} from '../prebidGlobal.js';
 import {PbPromise} from './promise.js';
 
 export function pbYield() {
-  const scheduler = getGlobal().scheduler;
   return scheduler?.yield ? scheduler.yield() : PbPromise.resolve();
 }
