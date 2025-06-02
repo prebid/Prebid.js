dacdd4e693d7ce08664577bed5407953d38758f3
Komal Kumari <komalkumari@Komals-MacBook-Pro-L1834.local>
2025-06-02 17:23:57 +0530

    Add bidder as additional context in Pubmatic Rtd
    (cherry picked from commit a012b38722707937b57a01c8daa5d25b789aca76)

--- test/spec/modules/pubmaticRtdProvider_spec.js
+++ test/spec/modules/pubmaticRtdProvider_spec.js
@@ -6,7 +6,7 @@ import { config as conf } from '../../../src/config';
 import * as hook from '../../../src/hook.js';
 import {
     registerSubModule, pubmaticSubmodule, getFloorsConfig, fetchData,
-    getCurrentTimeOfDay, getBrowserType, getOs, getDeviceType, getCountry, getUtm, _country,
+    getCurrentTimeOfDay, getBrowserType, getOs, getDeviceType, getCountry, getBidder, getUtm, _country,
     _profileConfigs, _floorsData, defaultValueTemplate, withTimeout, configMerged
 } from '../../../modules/pubmaticRtdProvider.js';
 import sinon from 'sinon';
@@ -234,6 +234,13 @@ describe('Pubmatic RTD Provider', () => {
             expect(getUtm()).to.be.a('string');
             expect(getUtm()).to.be.oneOf(['0', '1']);
         });
+
+        // it('should extract bidder correctly', () => {
+        //     expect(getBidder({ bidder: 'pubmatic' })).to.equal('pubmatic');
+        //     expect(getBidder({})).to.be.undefined;
+        //     expect(getBidder(null)).to.be.undefined;
+        //     expect(getBidder(undefined)).to.be.undefined;
+        // });
     });
 
     describe('getFloorsConfig', () => {
@@ -305,7 +312,8 @@ describe('Pubmatic RTD Provider', () => {
                 'browser',
                 'os',
                 'country',
-                'utm'
+                'utm',
+                'bidder'
             ]);
 
             Object.values(result.floors.additionalSchemaFields).forEach(field => {
@@ -353,6 +361,7 @@ describe('Pubmatic RTD Provider', () => {
             expect(result.floors.additionalSchemaFields.os).to.equal(getOs);
             expect(result.floors.additionalSchemaFields.country).to.equal(getCountry);
             expect(result.floors.additionalSchemaFields.utm).to.equal(getUtm);
+            expect(result.floors.additionalSchemaFields.bidder).to.equal(getBidder);
         });
 
         it('should log error when profileConfigs is not an object', () => {
