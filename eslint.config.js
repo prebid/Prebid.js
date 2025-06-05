fc642db0dfb905ea696939b757e6d53d6d10f95e
Patrick McCann <pmccann@cafemedia.com>
2025-06-05 10:03:49 -0400

    core: enforce no-array-constructor in tests

--- eslint.config.js
+++ eslint.config.js
@@ -233,7 +233,6 @@ module.exports = [
       'no-unused-vars': 'off',
       'import/extensions': 'off',
       'camelcase': 'off',
-      'no-array-constructor': 'off',
       'import-x/no-duplicates': 'off',
       'import-x/no-absolute-path': 'off',
       'no-loss-of-precision': 'off',
