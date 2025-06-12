a3da2cafe7bb86bccc8c6bec574dddd358462c90
Patrick McCann <pmccann@cafemedia.com>
2025-06-12 09:06:48 -0400

    Update yield.ts

--- src/utils/yield.ts
+++ src/utils/yield.ts
@@ -1,7 +1,6 @@
 import {getGlobal} from '../prebidGlobal.js';
 import {PbPromise} from './promise.js';
 
-// This util was added by a codex bot.
 export function pbYield(): Promise<void> {
   const scheduler = (getGlobal() as any).scheduler as { yield?: () => Promise<void> } | undefined;
   return scheduler?.yield ? scheduler.yield() : PbPromise.resolve();
