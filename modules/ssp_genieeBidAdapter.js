e33782eb61a41e5b8e2ab35f8e84852a0d8a65eb
Thanh Tran <thanhtv@geniee.co.jp>
2025-06-17 13:48:50 +0700

    [CARPET-5190] Bid Adapter: Fix the default value of the ib parameter
    - change default value ib parameter

--- modules/ssp_genieeBidAdapter.js
+++ modules/ssp_genieeBidAdapter.js
@@ -158,7 +158,7 @@ function makeCommonRequestData(bid, geparameter, refererInfo) {
     ua: navigator.userAgent,
     tpaf: 1,
     cks: 1,
-    ib: 0,
+    ib: (bid.params.hasOwnProperty('invalidImpBeacon') && bid.params.invalidImpBeacon) ? 0 : 1,
     ...(gpid ? { gpid } : {}),
   };
 
