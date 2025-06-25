26779f187c801c4d797675615ab8a3ace00c9922
asit.sahoo <asit.sahoo@pubmatic.com>
2025-06-25 13:35:47 +0530

    Fixed linting error and updated logic
    Fixed linting error and updated logic

--- modules/pubmaticBidAdapter.js
+++ modules/pubmaticBidAdapter.js
@@ -327,7 +327,7 @@ const updateBannerImp = (bannerObj, adSlot) => {
   bannerObj.format = bannerObj.format.filter(
     (item) => !(item.w === bannerObj.w && item.h === bannerObj.h)
   );
-  if(!bannerObj.format?.length) delete bannerObj.format;
+  if (bannerObj.format?.length === 0) delete bannerObj.format;
   bannerObj.pos ??= 0;
 }
 
