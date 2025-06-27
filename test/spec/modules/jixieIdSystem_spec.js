5fdcac4bb48ea70c6abe07310a58574420f4bdd8
Patrick McCann <pmccann@cafemedia.com>
2025-06-27 07:22:49 -0400

    Update jixieIdSystem_spec.js

--- test/spec/modules/jixieIdSystem_spec.js
+++ test/spec/modules/jixieIdSystem_spec.js
@@ -25,7 +25,7 @@ describe('JixieId  Submodule', () => {
   const EID_TYPE2_LSNAME = 'somesha2ls';
   const EID_TYPE1_SAMPLEVALUE = 'pppppppppp';
   const EID_TYPE2_SAMPLEVALUE = 'eeeeeeeeee';
-  
+
   it('should have the correct module name declared', () => {
     expect(jixieIdSubmodule.name).to.equal('jixieId');
   });
