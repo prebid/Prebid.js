6d5c55bf2908ed9abee9f0a072cea36f28b9d15b
suruvu <ssuruvu@optimizerx.com>
2025-06-30 11:39:07 -0400

    Update oprxBidAdapter.js

--- modules/oprxBidAdapter.js
+++ modules/oprxBidAdapter.js
@@ -1,41 +1,73 @@
-import {
-  BANNER,
-  NATIVE
+import { 
+  BANNER 
 } from '../src/mediaTypes.js';
-import {
-  registerBidder
+import { 
+  ortbConverter 
+} from '../libraries/ortbConverter/converter.js';
+import { 
+  registerBidder 
 } from '../src/adapters/bidderFactory.js';
-import {
-  getBannerRequest,
-  getBannerResponse,
-  getNativeResponse,
-} from '../libraries/audUtils/bidderUtils.js';
 
-const EP = 'https://pb.optimizerx.com/pb';
-// Export const spec
+//bidder code
+const BIDDER_CODE = 'oprx';
+
+let EP = 'https://pb.optimizerx.com';
+let PLACEMENT_ID = null;
+let NPI = null;
+
+// Converts to ortb
+const converter = ortbConverter({
+  context: {
+    netRevenue: true,
+    ttl: 50,
+    currency: 'USD',
+    mediaType: BANNER,
+  },
+  imp(buildImp, bidRequest, context) {
+    const imp = buildImp(bidRequest, context);
+    return imp;
+  },
+});
+
 export const spec = {
-  code: 'oprx',
-  supportedMediaTypes: [BANNER, NATIVE],
-  // Determines whether or not the given bid request is valid
-  isBidRequestValid: (bidRequestParam) => {
-    return !!(bidRequestParam.params.placement_id);
-  },
-  // Make a server request from the list of BidRequests
-  buildRequests: (bidR, serverR) => {
-    // Get Requests based on media types
-    return getBannerRequest(bidR, serverR, EP);
-  },
-  // Unpack the response from the server into a list of bids.
-  interpretResponse: (bidRS, bidRQ) => {
-    let Response = {};
-    const mediaType = JSON.parse(bidRQ.data)[0].MediaType;
-    if (mediaType == BANNER) {
-      Response = getBannerResponse(bidRS, BANNER);
-    } else if (mediaType == NATIVE) {
-      Response = getNativeResponse(bidRS, bidRQ, NATIVE);
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER],
+
+  // Checks if the request is valid or not
+  isBidRequestValid: function (bid) {
+    if (
+      Boolean(bid.params.placement_id) &&
+      Boolean(bid.params.npi)
+    ) {
+      PLACEMENT_ID = bid.params.placement_id;
+      NPI = bid.params.npi;
+      EP = EP + '?placement_id=' + PLACEMENT_ID + '&npi=' + NPI;
+      return true;
     }
-    return Response;
-  }
-}
+    return false;
+  },
+
+  // To make a request from the list of BidRequests and BidderRequests
+  buildRequests(bRequests, brRequest) {
+    let data = converter.toORTB({ bRequests, brRequest });
+    return [
+      {
+        method: 'POST',
+        url: EP,
+        data,
+        options: { contentType: 'application/json;charset=utf-8' },
+      },
+    ];
+  },
+  
+  // Unpack the response from the server into a list of bids. 
+  interpretResponse(response, request) {
+    const bids = converter.fromORTB({
+      response: response.body,
+      request: request.data,
+    }).bids;
+    return bids;
+  },
+};
 
 registerBidder(spec);
