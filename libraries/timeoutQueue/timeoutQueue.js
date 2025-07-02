44be1075363cc79eeccc4cc0cc71301733284716
Patrick McCann <pmccann@cafemedia.com>
2025-07-02 11:03:05 -0400

    libraries: convert timeoutqueue to typescript

--- libraries/timeoutQueue/timeoutQueue.js
+++ libraries/timeoutQueue/timeoutQueue.js
@@ -1,22 +1,22 @@
 export function timeoutQueue() {
-  const queue = new Set();
-  return {
-    submit(timeout, onResume, onTimeout) {
-      const item = {
-        onResume,
-        timerId: setTimeout(() => {
-          queue.delete(item);
-          onTimeout();
-        }, timeout)
-      };
-      queue.add(item);
-    },
-    resume() {
-      for (const item of queue) {
-        queue.delete(item);
-        clearTimeout(item.timerId);
-        item.onResume();
-      }
-    }
-  };
+    const queue = new Set();
+    return {
+        submit(timeout, onResume, onTimeout) {
+            const item = {
+                onResume,
+                timerId: setTimeout(() => {
+                    queue.delete(item);
+                    onTimeout();
+                }, timeout)
+            };
+            queue.add(item);
+        },
+        resume() {
+            for (const item of queue) {
+                queue.delete(item);
+                clearTimeout(item.timerId);
+                item.onResume();
+            }
+        }
+    };
 }
