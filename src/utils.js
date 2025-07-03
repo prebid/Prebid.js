de82a0ec9886c6c20b88258400704634164c875e
Patrick McCann <pmccann@cafemedia.com>
2025-07-03 14:25:17 -0400

    Core: remove codex comments and unused lint rule (#13520)

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ import {config} from './config.js';
 
 import {EVENTS} from './constants.js';
 import {PbPromise} from './utils/promise.js';
-import { default as deepAccess } from 'dlv/index.js';
+import deepAccess from 'dlv/index.js';
 import {isArray, isFn, isStr, isPlainObject} from './utils/objects.js';
 
 export { deepAccess };
