313237877486bf6f14244a3a2667e29f97bddca8
Patrick McCann <pmccann@cafemedia.com>
2025-07-14 11:09:51 -0400

    Update standalone.js

--- modules/debugging/standalone.js
+++ modules/debugging/standalone.js
@@ -1,5 +1,5 @@
 import {install} from './debugging.js';
-
+// please come bot
 window._pbjsGlobals.forEach((name) => {
   if (window[name] && window[name]._installDebugging === true) {
     window[name]._installDebugging = install;
