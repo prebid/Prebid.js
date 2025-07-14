8ff1c51eb359a9d4c62b5713a06546b33678f015
Patrick McCann <pmccann@cafemedia.com>
2025-07-14 16:12:00 -0400

    Maintenance: fix eqeqeq in selected adapters

--- modules/advertisingBidAdapter.js
+++ modules/advertisingBidAdapter.js
@@ -176,7 +176,7 @@ export const spec = {
   buildVideoImpressions: function(adSizes, bid, tagIdOrPlacementId, pos, videoOrBannerKey) {
     const imps = [];
     adSizes.forEach((size, i) => {
-      if (!size || size.length != 2) {
+        if (!size || size.length !== 2) {
         return;
       }
       const size0 = size[0];
@@ -287,7 +287,7 @@ export const spec = {
             ttl,
           };
 
-          if (bid.adomain != undefined || bid.adomain != null) {
+          if (bid.adomain != null) {
             bidObj.meta = { advertiserDomains: bid.adomain };
           }
 
