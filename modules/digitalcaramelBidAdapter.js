580394ab4620543c8eb1df6ed7f639ceae94cde0
hamper <hamper.nhs@gmail.com>
2025-08-05 21:54:24 +0300

    Digitalcaramel Bid Adapter: initial release (#13731)
    * move common code to lib and add digitalcaramel adapter
    
    * more deduplication

--- modules/digitalcaramelBidAdapter.js
+++ modules/digitalcaramelBidAdapter.js
@@ -2,10 +2,10 @@ import { registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, VIDEO } from '../src/mediaTypes.js';
 import { getUserSyncs, sspBuildRequests, sspInterpretResponse, sspValidRequest } from '../libraries/vizionikUtils/vizionikUtils.js';
 
-const BIDDER_CODE = 'programmatica';
-const DEFAULT_ENDPOINT = 'asr.programmatica.com';
-const SYNC_ENDPOINT = 'sync.programmatica.com';
-const ADOMAIN = 'programmatica.com';
+const BIDDER_CODE = 'digitalcaramel';
+const DEFAULT_ENDPOINT = 'ssp-asr.digitalcaramel.com';
+const SYNC_ENDPOINT = 'sync.digitalcaramel.com';
+const ADOMAIN = 'digitalcaramel.com';
 const TIME_TO_LIVE = 360;
 
 export const spec = {
