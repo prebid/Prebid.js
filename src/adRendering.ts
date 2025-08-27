fa83da93c774f7e750cde14706892c32e6fbc449
Demetrio Girardi <dgirardi@prebid.org>
2025-08-27 13:43:21 -0700

    lint

--- src/adRendering.ts
+++ src/adRendering.ts
@@ -17,7 +17,6 @@ import {auctionManager} from './auctionManager.js';
 import {getCreativeRenderer} from './creativeRenderers.js';
 import {hook} from './hook.js';
 import {fireNativeTrackers} from './native.js';
-import {PbPromise} from './utils/promise.js';
 import adapterManager from './adapterManager.js';
 import {useMetrics} from './utils/perfMetrics.js';
 import {filters} from './targeting.js';
