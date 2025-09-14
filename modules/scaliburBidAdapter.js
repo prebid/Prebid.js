3b44c52fe66656ad39cf695ebf08b81565867726
Timor Bibi <timor@Timors-MacBook-Pro-2.local>
2025-09-14 11:31:32 +0300

    fix some more lint errors

--- modules/scaliburBidAdapter.js
+++ modules/scaliburBidAdapter.js
@@ -1,7 +1,6 @@
 import {registerBidder} from '../src/adapters/bidderFactory.js';
 import {config} from '../src/config.js';
 import {BANNER, VIDEO} from '../src/mediaTypes.js';
-import {getStorageManager} from '../src/storageManager.js';
 import {sizesToSizeTuples} from "../src/utils.js";
 
 const BIDDER_CODE = 'scalibur';
