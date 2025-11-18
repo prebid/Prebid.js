051937fd09734a66cc6e24b0749bd7b05e63155f
Demetrio Girardi <dgirardi@prebid.org>
2025-11-18 12:00:17 -0800

    enable debug logging

--- karma.conf.maker.js
+++ karma.conf.maker.js
@@ -160,7 +160,7 @@ module.exports = function(codeCoverage, browserstack, watchMode, file, disableFe
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: karmaConstants.LOG_INFO,
+    logLevel: karmaConstants.LOG_DEBUG,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: watchMode,
