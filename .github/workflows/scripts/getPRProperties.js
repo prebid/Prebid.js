744ddc382953a88c0cb6f5629b5622a7ffe1afb3
Demetrio Girardi <dgirardi@prebid.org>
2025-11-19 11:27:03 -0800

    try getPRProperties

---
+++ .github/workflows/scripts/getPRProperties.js
@@ -0,0 +1,13 @@
+async function getPRProperties({github, context, prNo}) {
+  const files = (await github.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: prNo,
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })).data.map(datum => datum.filename);
+  console.log(files);
+}
+
+module.exports = getPRProperties;
--- .github/workflows/scripts/is-core-change.js
+++
@@ -1,3 +0,0 @@
-async function isCoreChange({github}) {}
-
-module.exports = isCoreChange;
