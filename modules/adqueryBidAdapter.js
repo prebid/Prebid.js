0b622f3e891907b57c0716a2b24ff5608befce5b
adquery <p.pydys@openmobi.pl>
2025-11-19 09:35:23 +0100

    adquery/prebid_qid_work7_video_1

--- modules/adqueryBidAdapter.js
+++ modules/adqueryBidAdapter.js
@@ -1,6 +1,6 @@
 import {registerBidder} from '../src/adapters/bidderFactory.js';
 import {BANNER, VIDEO} from '../src/mediaTypes.js';
-import {buildUrl, logInfo, logMessage, parseSizesInput, triggerPixel} from '../src/utils.js';
+import {buildUrl, logInfo, logMessage, parseSizesInput, triggerPixel, getWinDimensions} from '../src/utils.js';
 
 /**
  * @typedef {import('../src/adapters/bidderFactory.js').BidRequest} BidRequest
@@ -32,6 +32,11 @@ export const spec = {
    * @return {boolean}
    */
   isBidRequestValid: (bid) => {
+    const video = bid.mediaTypes && bid.mediaTypes.video;
+    if (video) {
+      return !!(video.playerSize && video.context === 'outstream');  // Focus on outstream
+    }
+
     return !!(bid && bid.params && bid.params.placementId && bid.mediaTypes.banner.sizes)
   },
 
@@ -47,19 +52,34 @@ export const spec = {
       protocol: ADQUERY_BIDDER_DOMAIN_PROTOCOL,
       hostname: ADQUERY_BIDDER_DOMAIN,
       pathname: '/prebid/bid',
-      // search: params
     });
 
     for (let i = 0, len = bidRequests.length; i < len; i++) {
+      const bid = bidRequests[i];
+      const isVideo = bid.mediaTypes && bid.mediaTypes.video && bid.mediaTypes.video.context === 'outstream';
+
+      let requestUrl = adqueryRequestUrl;
+
+      if (isVideo) {
+        // Optionally use a dedicated video endpoint if your server differentiates
+        requestUrl = buildUrl({
+          protocol: ADQUERY_BIDDER_DOMAIN_PROTOCOL,
+          hostname: ADQUERY_BIDDER_DOMAIN,
+          pathname: '/openrtb2/auction2', // Adjust if your server has a specific video endpoint
+        });
+      }
+
       const request = {
         method: 'POST',
-        url: adqueryRequestUrl, // ADQUERY_BIDDER_DOMAIN_PROTOCOL + '://' + ADQUERY_BIDDER_DOMAIN + '/prebid/bid',
-        data: buildRequest(bidRequests[i], bidderRequest),
+        url: requestUrl, // ADQUERY_BIDDER_DOMAIN_PROTOCOL + '://' + ADQUERY_BIDDER_DOMAIN + '/prebid/bid',
+        data: buildRequest(bid, bidderRequest, isVideo),
         options: {
           withCredentials: false,
           crossOrigin: true
-        }
+        },
+        bidId: bid.bidId // Add bidId for mapping responses
       };
+
       requests.push(request);
     }
     return requests;
@@ -71,14 +91,55 @@ export const spec = {
    * @return {Bid[]}
    */
   interpretResponse: (response, request) => {
-    logMessage(request);
-    logMessage(response);
-
-    const res = response && response.body && response.body.data;
     const bidResponses = [];
 
+    const bidRequest = request.data;
+
+    if (response?.body?.seatbid) {
+      response.body.seatbid.forEach(seat => {
+        seat.bid.forEach(bid => {
+          logMessage('bidObj', bid);
+          const imp = bidRequest.imp.find(i => i.id === bid.impid) || bidRequest.imp[0];
+
+          // Only video is the target here
+          if (!imp.video) {
+            logMessage('bidObj NOT VIDEO', bid);
+            return;
+          }
+
+          const bidResponse = {
+            requestId: bid.impid,
+            mediaType: 'video',
+            cpm: bid.price,
+            currency: response.body.cur || 'USD',
+            ttl: 3600, // video żyje dłużej
+            creativeId: bid.crid || bid.id,
+            netRevenue: true,
+            dealId: bid.dealid || undefined,
+
+            // VAST – priority: inline XML > admurl > nurl as a wrapper
+            vastXml: bid.adm || null,
+            vastUrl: bid.admurl || (bid.adm ? null : bid.nurl) || null,
+
+            width: bid.w || imp.video.w || 640,
+            height: bid.h || imp.video.h || 360,
+
+            meta: {
+              advertiserDomains: bid.adomain && bid.adomain.length ? bid.adomain : [],
+              networkName: seat.seat || undefined,
+              mediaType: 'video'
+            }
+          };
+
+          bidResponses.push(bidResponse);
+        });
+      });
+    }
+
+    const res = response && response.body && response.body.data;
+
     if (!res) {
-      return [];
+      return bidResponses;
     }
 
     const bidResponse = {
@@ -222,7 +283,7 @@ export const spec = {
   }
 };
 
-function buildRequest(validBidRequests, bidderRequest) {
+function buildRequest(validBidRequests, bidderRequest, isVideo = false) {
   const bid = validBidRequests;
   logInfo('buildRequest: ', bid);
 
@@ -248,6 +309,77 @@ function buildRequest(validBidRequests, bidderRequest) {
     pageUrl = bidderRequest.refererInfo.page || '';
   }
 
+  if (isVideo) {
+    const baseRequest = {
+      id: bid.bidId,
+      cur: ['USD'],
+      site: {
+        page: bidderRequest.refererInfo.page,
+        domain: bidderRequest.refererInfo.domain
+      },
+      tmax: bidderRequest.timeout,
+      // Add GDPR/CCPA consent if applicable
+      regs: bidderRequest.gdprConsent ? {
+        ext: {
+          gdpr: bidderRequest.gdprConsent.gdprApplies ? 1 : 0,
+          consent: bidderRequest.gdprConsent.consentString
+        }
+      } : {},
+      // === USER ===
+      user: {
+        id: bidderRequest.userId ? bidderRequest.userId.uid2?.id : undefined,
+        buyeruid: bidderRequest.userId ? bidderRequest.userId.id5id?.uid : undefined,
+        // Dodaj inne ID: liveramp, criteo, etc.
+        ext: bidderRequest.userId ? {
+          eids: bidderRequest.userIdAsEids // Prebid 7+ format
+        } : undefined
+      },
+
+      // === DEVICE ===
+      device: {
+        ua: navigator.userAgent,
+        ip: bidderRequest.ip || undefined, // Jeśli masz IP (np. z serwera)
+        dnt: navigator.doNotTrack === '1' ? 1 : 0,
+        language: navigator.language || navigator.browserLanguage || '',
+        js: 1, // JavaScript enabled
+        // Opcjonalnie: w, h (viewport), ppi, pxratio
+        w: getWinDimensions().visualViewport.height,
+        h: getWinDimensions().visualViewport.width,
+        pxratio: window.top.devicePixelRatio || 1,
+        // Dla mobile:
+        ...(isMobile() && {
+          os: getOS(),
+          osv: getOSVersion(),
+          make: getDeviceMake(),
+          model: getDeviceModel(),
+          connectiontype: getConnectionType()
+        })
+      },
+    };
+    // Video-specific impression object
+    const videoParams = bid.mediaTypes.video;
+    baseRequest.imp = [{
+      bidfloorcur: 'USD',
+      id: bid.bidId,
+      video: {
+        w: videoParams.playerSize[0][0], // Width
+        h: videoParams.playerSize[0][1], // Height
+        mimes: videoParams.mimes || ['video/mp4', 'video/webm'],
+        protocols: videoParams.protocols || [2, 3, 5, 6, 7, 8], // VAST protocols
+        placement: 2, // Outstream
+        startdelay: videoParams.startdelay || 0,
+        skip: videoParams.skip || 0,
+        api: videoParams.api || [2], // VPAID 2.0
+        // Add other video params: minduration, maxduration, etc.
+      },
+      ext: {
+        bidder: bid.params // Pass custom adquery params
+      }
+    }];
+
+    return baseRequest;
+  }
+
   return {
     v: '$prebid.version$',
     placementCode: bid.params.placementId,
@@ -263,6 +395,50 @@ function buildRequest(validBidRequests, bidderRequest) {
     bidderRequestsCount: bid.bidderRequestsCount,
     sizes: parseSizesInput(bid.mediaTypes.banner.sizes).toString(),
   };
+}
+
+function isMobile() {
+  return /Mobi|Android|iPhone|iPad|iPod/.test(navigator.userAgent);
+}
+
+function getOS() {
+  const ua = navigator.userAgent;
+  if (/Windows/.test(ua)) return 'Windows';
+  if (/Mac OS X/.test(ua)) return 'Mac OS X';
+  if (/Android/.test(ua)) return 'Android';
+  if (/iPhone|iPad|iPod/.test(ua)) return 'iOS';
+  if (/Linux/.test(ua)) return 'Linux';
+  return 'Unknown';
+}
+
+function getOSVersion() {
+  const match = navigator.userAgent.match(/(Android|iPhone OS)[\s/]?([\d._]+)/i);
+  logMessage('getOSVersion', match)
+  return match ? match[2].replace(/_/g, '.') : undefined;
+}
+
+function getDeviceMake() {
+  const ua = navigator.userAgent;
+  if (/iPhone/.test(ua)) return 'Apple';
+  if (/iPad/.test(ua)) return 'Apple';
+  if (/Samsung/.test(ua)) return 'Samsung';
+  if (/Huawei/.test(ua)) return 'Huawei';
+  return undefined;
+}
+
+function getDeviceModel() {
+  const ua = navigator.userAgent;
+  const match = ua.match(/\(([^;]+);[\s/]?([^)]+)\)/);
+  logMessage('getDeviceModel', match)
+  return match ? match[2] : undefined;
+}
+
+function getConnectionType() {
+  const conn = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+  if (!conn) return undefined;
+  const type = conn.effectiveType || conn.type;
+  const map = { '4g': 5, '3g': 4, '2g': 3, 'slow-2g': 2 };
+  return map[type] || undefined;
 }
 
 registerBidder(spec);
