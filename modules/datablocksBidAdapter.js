630992173a082d4ba92bcf525191667c5fc89d31
Patrick McCann <patmmccann@gmail.com>
2025-11-24 17:01:45 -0500

    Update screen dimensions to null in datablocksBidAdapter
    Set screen availability width and height to null.

--- modules/datablocksBidAdapter.js
+++ modules/datablocksBidAdapter.js
@@ -208,8 +208,8 @@ export const spec = {
     return {
       'wiw': windowDimensions.innerWidth,
       'wih': windowDimensions.innerHeight,
-      'saw': windowDimensions.screen.availWidth,
-      'sah': windowDimensions.screen.availHeight,
+      'saw': null,
+      'sah': null,
       'scd': null,
       'sw': windowDimensions.screen.width,
       'sh': windowDimensions.screen.height,
