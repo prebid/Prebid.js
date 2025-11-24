1a20e3e6b77cadf8a7b0892cc54a0e82747b25c3
Patrick McCann <pmccann@cafemedia.com>
2025-11-24 12:42:38 -0500

    Core: move timezone helper to libraries

--- modules/screencoreBidAdapter.js
+++ modules/screencoreBidAdapter.js
@@ -7,6 +7,7 @@ import {
   createUserSyncGetter,
   isBidRequestValid,
 } from '../libraries/vidazooUtils/bidderUtils.js';
+import { getTimeZone } from '../libraries/timezone.js';
 
 const BIDDER_CODE = 'screencore';
 const GVLID = 1473;
@@ -23,8 +24,7 @@ const REGION_SUBDOMAIN_SUFFIX = {
  */
 function getRegionSubdomainSuffix() {
   try {
-    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const region = timezone.split('/')[0];
+    const region = (getTimeZone() || '').split('/')[0];
 
     switch (region) {
       case 'Asia':
