6a85e4111ad2c5f4dc5f85619960c4092a349bcb
pm-azhar-mulla <azhar@L2047.local>
2025-11-26 15:21:11 +0530

    Removed spread operator

--- modules/rtdModule/index.js
+++ modules/rtdModule/index.js
@@ -316,7 +316,8 @@ export const setBidRequestsData = timedAuctionHook('rtd', function setBidRequest
 
   relevantSubModules.forEach(sm => {
     const fpdGuard = guardOrtb2Fragments(reqBidsConfigObj.ortb2Fragments || {}, activityParams(MODULE_TYPE_RTD, sm.name));
-    sm.getBidRequestData({...reqBidsConfigObj, ortb2Fragments: fpdGuard}, onGetBidRequestDataCallback.bind(sm), sm.config, _userConsent, timeout);
+    reqBidsConfigObj.ortb2Fragments = mergeDeep({}, reqBidsConfigObj.ortb2Fragments || {}, fpdGuard);
+    sm.getBidRequestData(reqBidsConfigObj, onGetBidRequestDataCallback.bind(sm), sm.config, _userConsent, timeout);
   });
 
   function onGetBidRequestDataCallback() {
