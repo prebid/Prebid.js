a696ce26a456e5b7d37a34463595be6fe6d1142d
Demetrio Girardi <dgirardi@prebid.org>
2025-12-11 06:12:58 -0800

    Core: normalize FPD

--- src/fpd/normalize.js
+++ src/fpd/normalize.js
@@ -1,10 +1,16 @@
-import {deepEqual, deepSetValue, logWarn} from '../utils.js';
+import {deepEqual, deepSetValue, deepAccess, logWarn} from '../utils.js';
 import {hook} from '../hook.js';
 
 export const normalizeFPD = hook('sync', function(ortb2Fragments) {
   [
     normalizeEIDs,
-    normalizeSchain
+    makeNormalizer('source.schain', 'source.ext.schain', 'source.ext.schain'),
+    makeNormalizer('device.sua', 'device.ext.sua', 'device.sua'),
+    makeNormalizer('regs.gdpr', 'regs.ext.gdpr', 'regs.ext.gdpr'),
+    makeNormalizer('user.consent', 'user.ext.consent', 'user.ext.consent'),
+    makeNormalizer('regs.us_privacy', 'regs.ext.us_privacy', 'regs.ext.us_privacy'),
+    makeNormalizer('regs.gpp', 'regs.ext.gpp', 'regs.gpp'),
+    makeNormalizer('regs.gpp_sid', 'regs.ext.gpp_sid', 'regs.gpp_sid'),
   ].forEach(normalizer => applyNormalizer(normalizer, ortb2Fragments));
   return ortb2Fragments;
 })
@@ -40,19 +46,29 @@ export function normalizeEIDs(target, context) {
   return target;
 }
 
-export function normalizeSchain(target, context) {
-  if (!target) return target;
-  const schain = target.source?.schain;
-  const extSchain = target.source?.ext?.schain;
-  if (schain != null && extSchain != null && !deepEqual(schain, extSchain)) {
-    logWarn(`Conflicting source.schain and source.ext.schain (${context}), preferring source.schain`, {
-      'source.schain': schain,
-      'source.ext.schain': extSchain
-    })
+export function makeNormalizer(preferred, fallback, dest) {
+  if (dest !== preferred && dest !== fallback) throw new Error('invalid argument');
+  const delPath = (dest === preferred ? fallback : preferred).split('.');
+  const delProp = delPath.pop();
+  const delTarget = delPath.join('.');
+
+  return function (ortb2, context) {
+    if (!ortb2) return ortb2;
+    const prefData = deepAccess(ortb2, preferred);
+    const fallbackData = deepAccess(ortb2, fallback);
+    if (prefData != null && fallbackData != null && !deepEqual(prefData, fallbackData)) {
+      logWarn(`Conflicting ${preferred} and ${fallback} (${context}), preferring ${preferred}`, {
+        [preferred]: prefData,
+        [fallback]: fallbackData
+      })
+    }
+    if ((prefData ?? fallbackData) != null) {
+      deepSetValue(ortb2, dest, prefData ?? fallbackData);
+    }
+    const ignoredTarget = deepAccess(ortb2, delTarget);
+    if (ignoredTarget != null && typeof ignoredTarget === 'object') {
+      delete ignoredTarget[delProp];
+    }
+    return ortb2;
   }
-  if ((schain ?? extSchain) != null) {
-    deepSetValue(target, 'source.ext.schain', schain ?? extSchain);
-  }
-  delete target.source?.schain;
-  return target;
 }
