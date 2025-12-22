3fd4f23f3bbb51f7512332ef979f9acb87ebcc9e
ashleysmithTTD <ashley.smith@thetradedesk.com>
2025-12-22 11:59:10 -0700

    updated uid2 testing

--- test/spec/modules/uid2IdSystem_spec.js
+++ test/spec/modules/uid2IdSystem_spec.js
@@ -34,12 +34,9 @@ const cstgConfigParams = { serverPublicKey: 'UID2-X-L-24B8a/eLYBmRkXA9yPgRZt+ouK
 const makeUid2IdentityContainer = (token) => ({uid2: {id: token}});
 const makeUid2OptoutContainer = (token) => ({uid2: {optout: true}});
 let useLocalStorage = false;
-const makePrebidConfig = (params = null, extraSettings = {}, debug = false) => {
-  const {auctionDelay = auctionDelayMs, syncDelay, ...otherSettings} = extraSettings;
-  return {
-    userSync: { auctionDelay, ...(syncDelay !== undefined && {syncDelay}), userIds: [{name: 'uid2', params: {storage: useLocalStorage ? 'localStorage' : 'cookie', ...params}}] }, debug, ...otherSettings
-  };
-};
+const makePrebidConfig = (params = null, extraSettings = {}, debug = false) => ({
+  userSync: { auctionDelay: extraSettings.auctionDelay ?? auctionDelayMs, ...(extraSettings.syncDelay !== undefined && {syncDelay: extraSettings.syncDelay}), userIds: [{name: 'uid2', params: {storage: useLocalStorage ? 'localStorage' : 'cookie', ...params}}] }, debug
+});
 const makeOriginalIdentity = (identity, salt = 1) => ({
   identity: utils.cyrb53Hash(identity, salt),
   salt
