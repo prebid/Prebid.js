82080eadb01a78ab94f64e4aadc42239883ccfcd
mkomorski <marcin.komorski@pollub.edu.pl>
2026-01-12 12:49:00 +0100

    Core: bidder alwaysHasCapacity flag

--- src/adapterManager.ts
+++ src/adapterManager.ts
@@ -195,6 +195,7 @@ export interface BaseBidderRequest<BIDDER extends BidderCode | null> {
   gdprConsent?: ReturnType<typeof gdprDataHandler['getConsentData']>;
   uspConsent?: ReturnType<typeof uspDataHandler['getConsentData']>;
   gppConsent?: ReturnType<typeof gppDataHandler['getConsentData']>;
+  alwaysHasCapacity?: boolean;
 }
 
 export interface S2SBidderRequest<BIDDER extends BidderCode | null> extends BaseBidderRequest<BIDDER> {
@@ -594,9 +595,10 @@ const adapterManager = {
             }),
             auctionStart: auctionStart,
             timeout: s2sConfig.timeout,
-            src: S2S.SRC,
+            src: S2S.SRC, 
             refererInfo,
             metrics,
+            alwaysHasCapacity: s2sConfig.alwaysHasCapacity,
           }, s2sParams);
           if (bidderRequest.bids.length !== 0) {
             bidRequests.push(bidderRequest);
@@ -626,6 +628,7 @@ const adapterManager = {
       const bidderRequestId = generateUUID();
       const pageViewId = getPageViewIdForBidder(bidderCode);
       const metrics = auctionMetrics.fork();
+      const adapter = _bidderRegistry[bidderCode];
       const bidderRequest = addOrtb2({
         bidderCode,
         auctionId,
@@ -644,8 +647,8 @@ const adapterManager = {
         timeout: cbTimeout,
         refererInfo,
         metrics,
+        alwaysHasCapacity: adapter?.getSpec?.().alwaysHasCapacity,
       });
-      const adapter = _bidderRegistry[bidderCode];
       if (!adapter) {
         logError(`Trying to make a request for bidder that does not exist: ${bidderCode}`);
       }
