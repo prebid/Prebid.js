667b4b579915457567b555c411449df2ac4d8572
vrishko <vasyl.rishko@smartyads.com>
2026-01-23 00:46:26 +0200

    prices logic fix

--- test/spec/modules/aceexBidAdapter_spec.js
+++ test/spec/modules/aceexBidAdapter_spec.js
@@ -117,7 +117,7 @@ describe('aceexBidAdapter', function () {
               w: 300,
               adm: '<div>price=1.23</div>',
               nurl: 'https://win.example.com?c=1.23',
-              adomain: 'test.com'
+              adomain: [ 'test.com' ]
             }]
           }]
         }
