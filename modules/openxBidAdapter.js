48b7096fbd8370feb15e8ef28c1d8481e92401eb
Patrick McCann <patmmccann@gmail.com>
2026-02-17 13:57:32 -0500

    Update openxBidAdapter.js

--- modules/openxBidAdapter.js
+++ modules/openxBidAdapter.js
@@ -61,6 +61,7 @@ const converter = ortbConverter({
     if (bid.params.coppa) {
       utils.deepSetValue(req, 'regs.coppa', 1);
     }
+    utils.deepSetValue(req, 'device.dnt', 0);
     if (bid.params.platform) {
       utils.deepSetValue(req, 'ext.platform', bid.params.platform);
     }
