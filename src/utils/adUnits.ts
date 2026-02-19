f4b86ceabb53b908a94c91058a5c422ed3097906
Demetrio Girardi <dgirardi@prebid.org>
2026-02-19 13:00:24 -0800

    Remove TODO note; update type

--- src/utils/adUnits.ts
+++ src/utils/adUnits.ts
@@ -2,8 +2,6 @@ import type {AdUnitDefinition} from "../adUnits.ts";
 import type {BidRequest} from "../adapterManager.ts";
 import type {Bid} from "../bidfactory.ts";
 
-// TODO: for 11, this should be merged with the GPT/AST logic in secureCreatives
-// (after customSlotMatching becomes config - https://github.com/prebid/Prebid.js/issues/14408)
 export function getAdUnitElement(bidRequest: BidRequest<any>): HTMLElement
 export function getAdUnitElement(bidResponse: Bid): HTMLElement
 export function getAdUnitElement(adUnit: AdUnitDefinition): HTMLElement
@@ -11,7 +9,7 @@ export function getAdUnitElement(target: {
   code?: string,
   adUnitCode?: string,
   element?: HTMLElement
-}): HTMLElement {
+}): HTMLElement | null {
   if (target.element != null) {
     return target.element;
   }
