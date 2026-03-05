42b645ce4f123bc774be23787abe838d88a67751
adsmovil <prebid@adsmovil.com>
2026-03-05 14:18:18 +0200

    New adapter adsmovil

---
+++ modules/adsmovilBidAdapter.js
@@ -0,0 +1,19 @@
+import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
+import { isBidRequestValid, buildRequests, interpretResponse, getUserSyncs } from '../libraries/teqblazeUtils/bidderUtils.js';
+
+const BIDDER_CODE = 'adsmovil';
+const AD_URL = 'https://tag-ssp.adsmovil.com/pbjs';
+const SYNC_URL = 'http://sync-ssp.adsmovil.com';
+
+export const spec = {
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
+
+  isBidRequestValid: isBidRequestValid(),
+  buildRequests: buildRequests(AD_URL),
+  interpretResponse,
+  getUserSyncs: getUserSyncs(SYNC_URL)
+};
+
+registerBidder(spec);
