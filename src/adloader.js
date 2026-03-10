c5859c1bbbad495de0a1b7500b3dbc74a221a4df
Demetrio Girardi <dgirardi@prebid.org>
2026-03-10 13:27:33 -0700

    Core: fix error handling when loading debugging-standalone (#14554)
    * Core: fix error handling when loading debugging-standalone
    
    * lint
    
    * run error callback once
    
    * lint

--- src/adloader.js
+++ src/adloader.js
@@ -26,16 +26,30 @@ export function loadExternalScript(url, moduleType, moduleCode, callback, doc, a
     return;
   }
 
+  const hasCallback = typeof callback === 'function' || typeof callback?.success === 'function' || typeof callback?.error === 'function';
+
+  function runCallback(cb, err) {
+    if (err == null) {
+      if (typeof cb === 'function') {
+        cb()
+      } else {
+        cb.success?.();
+      }
+    } else {
+      cb.error?.(err);
+    }
+  }
+
   if (!doc) {
     doc = document; // provide a "valid" key for the WeakMap
   }
   // only load each asset once
   const storedCachedObject = getCacheObject(doc, url);
   if (storedCachedObject) {
-    if (callback && typeof callback === 'function') {
+    if (hasCallback) {
       if (storedCachedObject.loaded) {
         // invokeCallbacks immediately
-        callback();
+        runCallback(callback, storedCachedObject.error);
       } else {
         // queue the callback
         storedCachedObject.callbacks.push(callback);
@@ -45,6 +59,7 @@ export function loadExternalScript(url, moduleType, moduleCode, callback, doc, a
   }
   const cachedDocObj = _requestCache.get(doc) || {};
   const cacheObject = {
+    error: null,
     loaded: false,
     tag: null,
     callbacks: []
@@ -52,7 +67,7 @@ export function loadExternalScript(url, moduleType, moduleCode, callback, doc, a
   cachedDocObj[url] = cacheObject;
   _requestCache.set(doc, cachedDocObj);
 
-  if (callback && typeof callback === 'function') {
+  if (hasCallback) {
     cacheObject.callbacks.push(callback);
   }
 
@@ -61,7 +76,7 @@ export function loadExternalScript(url, moduleType, moduleCode, callback, doc, a
     cacheObject.loaded = true;
     try {
       for (let i = 0; i < cacheObject.callbacks.length; i++) {
-        cacheObject.callbacks[i]();
+        runCallback(cacheObject.callbacks[i], cacheObject.error);
       }
     } catch (e) {
       logError('Error executing callback', 'adloader.js:loadExternalScript', e);
@@ -81,16 +96,27 @@ export function loadExternalScript(url, moduleType, moduleCode, callback, doc, a
       cacheObject.tag = jptScript;
     }
 
+    function errorListener(e) {
+      cacheObject.error = e;
+      exit();
+    }
+    jptScript.addEventListener('error', errorListener)
+
+    function exit() {
+      jptScript.removeEventListener('error', errorListener);
+      callback();
+    }
+
     if (jptScript.readyState) {
       jptScript.onreadystatechange = function () {
         if (jptScript.readyState === 'loaded' || jptScript.readyState === 'complete') {
           jptScript.onreadystatechange = null;
-          callback();
+          exit();
         }
       };
     } else {
       jptScript.onload = function () {
-        callback();
+        exit();
       };
     }
 
