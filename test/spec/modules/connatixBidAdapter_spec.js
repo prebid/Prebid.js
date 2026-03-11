5bb2075a21860ac433faa2948b27393200a52462
Demetrio Girardi <dgirardi@prebid.org>
2026-03-11 11:36:45 -0700

    connatixBidAdapter: fix the fix to the test

--- test/spec/modules/connatixBidAdapter_spec.js
+++ test/spec/modules/connatixBidAdapter_spec.js
@@ -172,7 +172,7 @@ describe('connatixBidAdapter', function () {
         height: 250
       });
 
-      const getElementByIdStub = sinon.stub(document, 'getElementById');
+      const getElementByIdStub = sandbox.stub(document, 'getElementById');
       getElementByIdStub.withArgs('validElement').returns(element);
 
       const result = connatixDetectViewability(bid);
