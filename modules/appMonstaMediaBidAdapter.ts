d6354c150bf2c12a7878d10f7ec7315f32ff8f3b
AppMonstaMedia <>
2026-04-06 12:30:57 +0300

    migrate adapter to ts

--- modules/appMonstaMediaBidAdapter.ts
+++ modules/appMonstaMediaBidAdapter.ts
@@ -1,11 +1,11 @@
-import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { BidderSpec, registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
-import { isBidRequestValid, buildRequests, interpretResponse } from '../libraries/teqblazeUtils/bidderUtils.js';
+import { isBidRequestValid, buildRequests, interpretResponse } from '../libraries/teqblazeUtils/bidderUtils.ts';
 
 const BIDDER_CODE = 'appMonstaMedia';
 const AD_URL = 'https://ssp-us.appmonsta.ai/pbjs';
 
-export const spec = {
+export const spec: BidderSpec<typeof BIDDER_CODE> = {
   code: BIDDER_CODE,
   supportedMediaTypes: [BANNER, VIDEO, NATIVE],
 
