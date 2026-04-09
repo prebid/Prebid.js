4c6a45ac07aeffdfffe775345d4d573cd2e6ccee
adsmovil <prebid@adsmovil.com>
2026-04-09 14:32:49 +0300

    refactor(adapter): ts fix

--- modules/adsmovilBidAdapter.ts
+++ modules/adsmovilBidAdapter.ts
@@ -1,4 +1,4 @@
-import { registerBidder } from '../src/adapters/bidderFactory.js';
+import { type BidderSpec, registerBidder } from '../src/adapters/bidderFactory.js';
 import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
 import { isBidRequestValid, buildRequests, interpretResponse, getUserSyncs } from '../libraries/teqblazeUtils/bidderUtils.ts';
 
@@ -6,14 +6,14 @@ const BIDDER_CODE = 'adsmovil';
 const AD_URL = 'https://tag-ssp.adsmovil.com/pbjs';
 const SYNC_URL = 'https://sync-ssp.adsmovil.com';
 
-export const spec = {
+export const spec: BidderSpec<typeof BIDDER_CODE> = {
   code: BIDDER_CODE,
-  supportedMediaTypes: [BANNER, VIDEO, NATIVE] as const,
+  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
 
   isBidRequestValid: isBidRequestValid(),
   buildRequests: buildRequests(AD_URL),
   interpretResponse,
   getUserSyncs: getUserSyncs(SYNC_URL)
-} as const;
+};
 
 registerBidder(spec);
