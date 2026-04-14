0919d7c26b45170088d18f8baca11576179e6885
Marco Prontera <prontera.marco.pm@gmail.com>
2026-04-14 17:50:22 +0200

    fix: import

--- src/adRendering.ts
+++ src/adRendering.ts
@@ -19,7 +19,7 @@ import { hook } from './hook.js';
 import { fireNativeTrackers } from './native.js';
 import adapterManager from './adapterManager.js';
 import { useMetrics } from './utils/perfMetrics.js';
-import { bidFilters } from './targeting/filters.js';
+import { bidFilters } from './targeting/filters.ts';
 import { EVENT_TYPE_WIN, parseEventTrackers, TRACKER_METHOD_IMG } from './eventTrackers.js';
 import type { Bid } from "./bidfactory.ts";
 import { yieldsIf } from "./utils/yield.ts";
