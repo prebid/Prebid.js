96c5106927c1ce65e1bc8b493a8dee6dcd02ffda
rcheptanariu <35690143+rcheptanariu@users.noreply.github.com>
2026-04-16 15:22:33 +0300

    Fix lint

--- test/spec/modules/invibesBidAdapter_spec.js
+++ test/spec/modules/invibesBidAdapter_spec.js
@@ -1189,8 +1189,16 @@ describe('invibesBidAdapter:', function () {
       },
       purpose: {
         consents: {
-          1: true, 2: true, 3: true, 4: true, 5: true,
-          6: true, 7: true, 8: true, 9: true, 10: true
+          1: true,
+          2: true,
+          3: true,
+          4: true,
+          5: true,
+          6: true,
+          7: true,
+          8: true,
+          9: true,
+          10: true
         }
       }
     };
