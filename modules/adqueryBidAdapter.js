e95c70b7a4a3d1c73c75bbd55c01159e24030079
Adrian Dzida <a.dzida@openmobi.pl>
2026-04-20 14:22:42 +0200

    update onBidWon

--- modules/adqueryBidAdapter.js
+++ modules/adqueryBidAdapter.js
@@ -191,6 +191,9 @@ export const spec = {
     }
 
     const copyOfBid = { ...bid }
+
+    const uuidMatch = copyOfBid.ad && typeof copyOfBid.ad === 'string' ? copyOfBid.ad.match(/data-uuid="([^"]*)"/) : null;
+    copyOfBid.uuid = uuidMatch ? uuidMatch[1] : null;
     delete copyOfBid.ad
     const shortBidString = JSON.stringify(copyOfBid);
     const encodedBuf = window.btoa(shortBidString);
