846bbef82c6d0a167afa0c47adc3b99bbc1db8c0
Patrick McCann <pmccann@cafemedia.com>
2026-04-22 03:08:23 -0400

    Core: tighten apntag shim typing

--- src/types/local/shim.d.ts
+++ src/types/local/shim.d.ts
@@ -4,7 +4,16 @@ declare global {
   function parseInt(n: number, radix?: number): number;
   function parseFloat(n: number): number;
 
+  interface ApnTag {
+    getTag?: (unit: string) => { keywords?: Record<string, string> } | undefined;
+    modifyTag?: (unit: string, tag: { keywords?: Record<string, string> }) => void;
+    setKeywords?: (targetId: string, keywords: Record<string, string>, options?: { overrideKeyValue?: boolean }) => void;
+    anq?: Array<() => void>;
+    onEvent?: (eventName: string, callback: () => void) => void;
+    [key: string]: unknown;
+  }
+
   interface Window {
-    apntag: any;
+    apntag?: ApnTag;
   }
 }
