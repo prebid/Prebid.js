7396408e3d4b76490e3910172b31ebe8c762613b
mkomorski <marcin.komorski@pollub.edu.pl>
2026-04-28 10:23:56 +0200

    renderer refactor

--- creative/renderers/frame/renderer.js
+++ creative/renderers/frame/renderer.js
@@ -1,5 +1,3 @@
-import { ERROR_NO_AD } from './constants.js';
-
 /**
  * FrameRenderer (creative): builds an empty same-origin iframe, injects
  * `<script src="bid.frameRendererUrl">`, then calls `iframe.contentWindow.pbRenderInFrame(bid)`
@@ -13,51 +11,45 @@ import { ERROR_NO_AD } from './constants.js';
   'use strict';
 
   function render(data, { mkFrame }, win) {
-    var frameRendererUrl = data.frameRendererUrl;
-    var width = data.width;
-    var height = data.height;
-    var instl = data.instl;
-
-    if (!frameRendererUrl) {
-      var err = new Error('Missing frameRendererUrl');
-      err.reason = ERROR_NO_AD;
-      throw err;
-    }
+    const frameRendererUrl = data.frameRendererUrl;
+    const width = data.width;
+    const height = data.height;
+    const instl = data.instl;
 
     if (height == null) {
-      var body = win.document && win.document.body;
-      var parent = body && body.parentElement;
+      const body = win.document && win.document.body;
+      const parent = body && body.parentElement;
       if (body && body.style) body.style.height = '100%';
       if (parent && parent.style) parent.style.height = '100%';
     }
 
-    var doc = win.document;
-    var attrs = {
+    const doc = win.document;
+    const attrs = {
       width: width != null ? width : '100%',
       height: height != null ? height : '100%'
     };
 
     if (instl && win.frameElement) {
-      var style = win.frameElement.style;
+      const style = win.frameElement.style;
       style.width = width ? String(width) + 'px' : '100vw';
       style.height = height ? String(height) + 'px' : '100vh';
     }
 
     // Prebid will wait for it to resolve before firing AD_RENDER_SUCCEEDED.
     return new Promise(function (resolve, reject) {
-      var frame = mkFrame(doc, {
+      const frame = mkFrame(doc, {
         width: attrs.width,
         height: attrs.height
       });
       frame.onload = function () {
         try {
-          var cw = frame.contentWindow;
-          var idoc = cw.document;
-          var script = idoc.createElement('script');
+          const cw = frame.contentWindow;
+          const idoc = cw.document;
+          const script = idoc.createElement('script');
           script.src = frameRendererUrl;
           script.onload = function () {
             try {
-              var fn = cw.pbRenderInFrame;
+              const fn = cw.pbRenderInFrame;
               if (typeof fn !== 'function') {
                 throw new Error(
                   'Prebid FrameRenderer: frameRendererUrl script must define window.pbRenderInFrame as a function.'
