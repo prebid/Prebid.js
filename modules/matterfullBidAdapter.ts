97d4e94271700760f54d1495d03e1f7c177dca00
Pavlo <dedafu57@gmail.com>
2026-05-19 11:50:43 +0300

    Matterfull Bid Adapter: initial release

---
+++ modules/matterfullBidAdapter.ts
@@ -0,0 +1,28 @@
+import { BANNER, VIDEO } from '../src/mediaTypes.js';
+import { registerBidder, type AdapterRequest, type BidderSpec, type ServerResponse } from '../src/adapters/bidderFactory.js';
+import { buildRequests as xeBuildRequests, getUserSyncs, interpretResponse as xeInterpretResponse, isBidRequestValid } from '../libraries/xeUtils/bidderUtils.js';
+
+const BIDDER_CODE = 'matterfull';
+const ENDPOINT = 'pbjs.bematterfull.com';
+
+export type MatterfullBidParams = {
+  pid: string;
+  ext?: Record<string, unknown>;
+}
+
+declare module '../src/adUnits' {
+  interface BidderParams {
+    [BIDDER_CODE]: MatterfullBidParams;
+  }
+}
+
+export const spec: BidderSpec<typeof BIDDER_CODE> = {
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER, VIDEO],
+  isBidRequestValid: bid => isBidRequestValid(bid, ['pid']),
+  buildRequests: (validBidRequests, bidderRequest) => xeBuildRequests(validBidRequests, bidderRequest, ENDPOINT) as AdapterRequest,
+  interpretResponse: (response: ServerResponse, request: AdapterRequest) => xeInterpretResponse(response, request as any),
+  getUserSyncs
+}
+
+registerBidder(spec);
