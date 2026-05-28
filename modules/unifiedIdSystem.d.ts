6cbdd590123a06210cef948404df5ca74508746a
Patrick McCann <pmccann@cafemedia.com>
2026-05-28 10:22:42 -0400

    UnifiedId Adapter: require partner or url in types

--- modules/unifiedIdSystem.d.ts
+++ modules/unifiedIdSystem.d.ts
@@ -1,4 +1,5 @@
 import type { Ext } from '../src/types/ortb/common';
+import type { RequireAtLeastOne } from '../src/types/objects';
 
 export type UnifiedIdSystemModuleName = 'unifiedId';
 
@@ -9,10 +10,16 @@ export interface UnifiedIdObject {
 
 export type UnifiedId = string | UnifiedIdObject;
 
-export interface UnifiedIdConfig {
+export type UnifiedIdConfig = RequireAtLeastOne<{
+  /**
+   * This is the partner ID value obtained from registering with The Trade Desk or working with a Prebid.js managed services provider.
+   */
   partner?: string;
+  /**
+   * If specified for UnifiedId, overrides the default Trade Desk URL.
+   */
   url?: string;
-}
+}, 'partner' | 'url'>;
 
 declare module './userId/spec' {
   interface UserId {
