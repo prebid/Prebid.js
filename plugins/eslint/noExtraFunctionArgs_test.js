c6fbec175d84b274b551c7c6671dc80bf62af554
Patrick McCann <patmmccann@gmail.com>
2026-06-23 20:39:46 -0400

    Core: handle lexical arguments in lint rule (#15200)

---
+++ plugins/eslint/noExtraFunctionArgs_test.js
@@ -0,0 +1,48 @@
+const { RuleTester } = require('eslint');
+const rule = require('./noExtraFunctionArgs.js');
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  }
+});
+
+ruleTester.run('no-extra-function-args', rule, {
+  valid: [
+    {
+      code: `
+        function makeReader(value) {
+          const read = () => arguments[0] || value;
+          return read;
+        }
+        makeReader('one', 'two');
+      `
+    },
+    {
+      code: `
+        function makeReader(value) {
+          function read() {
+            return arguments[0];
+          }
+          return read;
+        }
+        makeReader('one');
+      `
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        function makeReader(value) {
+          function read() {
+            return arguments[0];
+          }
+          return read;
+        }
+        makeReader('one', 'two');
+      `,
+      errors: [{ message: "Function 'makeReader' expects at most 1 argument, but 2 were provided." }]
+    }
+  ]
+});
