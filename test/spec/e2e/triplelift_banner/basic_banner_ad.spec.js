b694c8ae5ea54a5d9e24c1739a113c99ba252019
Carlos Cobaleda <96302091+ccobaledaTL@users.noreply.github.com>
2026-06-24 11:04:25 -0500

    Update basic_banner_ad.spec.js

--- test/spec/e2e/triplelift_banner/basic_banner_ad.spec.js
+++ test/spec/e2e/triplelift_banner/basic_banner_ad.spec.js
@@ -5,23 +5,24 @@ const TEST_PAGE_URL = testPageURL('triplelift_banner.html?pbjs_debug=true');
 const CREATIVE_IFRAME_ID = 'google_ads_iframe_/19968336/header-bid-tag-0_0';
 const CREATIVE_IFRAME_CSS_SELECTOR = 'iframe[id="' + CREATIVE_IFRAME_ID + '"]';
 
-Object.entries({
-  'asynchronously': TEST_PAGE_URL,
-}).forEach(([t, testPage]) => {
-  setupTest({
-    url: testPage,
-    waitFor: CREATIVE_IFRAME_CSS_SELECTOR
-  }, `Prebid.js Banner Ad Unit Test (loading ${t})`, function () {
-    it('should load the targeting keys with correct values', async function () {
-      const { adServerTargeting, bidder, installedModules } = await browser.execute(function () {
-        return {
-          bidder: window.pbjs.adUnits[0].bids[0].bidder,
-          installedModules: window.pbjs.installedModules,
-        };
-      });
-
-      expect(bidder).to.equal('triplelift');
-      expect(installedModules).to.include('tripleliftBidAdapter');
+setupTest({
+  url: TEST_PAGE_URL,
+  waitFor: CREATIVE_IFRAME_CSS_SELECTOR
+}, `Prebid.js Banner Ad Unit Test (loading ${TEST_PAGE_URL})`, function () {
+  it('should load the TripleLift bidder and installed modules', async function () {
+    const { bidder, installedModules } = await browser.execute(function () {
+      return {
+        bidder: window.pbjs.adUnits[0].bids[0].bidder,
+        installedModules: window.pbjs.installedModules,
+      };
     });
+
+    expect(bidder).to.equal('triplelift');
+    expect(installedModules).to.include('tripleliftBidAdapter');
   });
+});
+it('should render the TripleLift banner ad on the page', async function() {
+  await switchFrame(CREATIVE_IFRAME_CSS_SELECTOR);
+  const existingImage = await $('img[src*="images.3lift.com/3303913.jpg"]').isExisting();
+  expect(existingImage).to.be.true;
 });
