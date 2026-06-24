154df46e877fc912c8e59b9ad040a68b7da30abb
Carlos Cobaleda <96302091+ccobaledaTL@users.noreply.github.com>
2026-06-24 11:38:07 -0500

    finished tests

--- test/spec/e2e/triplelift_banner/basic_banner_ad.spec.js
+++ test/spec/e2e/triplelift_banner/basic_banner_ad.spec.js
@@ -1,9 +1,11 @@
 const expect = require('chai').expect;
-const { setupTest, switchFrame, testPageURL } = require('../../../helpers/testing-utils.js');
+const { setupTest, testPageURL, switchFrames } = require('../../../helpers/testing-utils.js');
 
 const TEST_PAGE_URL = testPageURL('triplelift_banner.html?pbjs_debug=true');
 const CREATIVE_IFRAME_ID = 'google_ads_iframe_/19968336/header-bid-tag-0_0';
 const CREATIVE_IFRAME_CSS_SELECTOR = 'iframe[id="' + CREATIVE_IFRAME_ID + '"]';
+
+const TL_IFRAME_CSS_SELECTOR = 'iframe[class*="tl-iframe"]';
 
 setupTest({
   url: TEST_PAGE_URL,
@@ -21,7 +23,7 @@ setupTest({
     expect(installedModules).to.include('tripleliftBidAdapter');
   });
   it('should render the TripleLift banner ad on the page', async function() {
-    await switchFrame(CREATIVE_IFRAME_CSS_SELECTOR);
+    await switchFrames(CREATIVE_IFRAME_CSS_SELECTOR, 'iframe', TL_IFRAME_CSS_SELECTOR);
     const existingImage = await $('img[src*="images.3lift.com/3303913.jpg"]').isExisting();
     expect(existingImage).to.be.true;
   });
