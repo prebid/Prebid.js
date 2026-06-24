e5306a58641f4bd1e695879b8f6d740f60054416
Patrick McCann <patmmccann@gmail.com>
2026-06-23 23:21:57 -0400

    Tests: address adapter review comments (#15212)

--- test/spec/modules/temedyaBidAdapter_spec.js
+++ test/spec/modules/temedyaBidAdapter_spec.js
@@ -87,12 +87,14 @@ describe('temedya adapter', function() {
 
     it('buildRequests function should not modify original bidRequests object', function () {
       const originalBidRequests = utils.deepClone(bidRequests);
+      spec.buildRequests(bidRequests);
 
       expect(bidRequests).to.deep.equal(originalBidRequests);
     });
 
     it('buildRequests function should not modify original nativeBidRequests object', function () {
       const originalBidRequests = utils.deepClone(nativeBidRequests);
+      spec.buildRequests(nativeBidRequests);
 
       expect(nativeBidRequests).to.deep.equal(originalBidRequests);
     });
