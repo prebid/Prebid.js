884654893378afbdc22919c6a5618b57f97e6d11
Vitalii Sumlennyi <vitalii.sumlennyi@teqblaze.com>
2026-06-25 11:09:46 +0300

    feat(adapter): new adapter

---
+++ modules/SMBBidAdapter.ts
@@ -0,0 +1,17 @@
+import { type BidderSpec, registerBidder } from '../src/adapters/bidderFactory.js';
+import { BANNER, NATIVE, VIDEO } from '../src/mediaTypes.js';
+import { isBidRequestValid, buildRequests, interpretResponse } from '../libraries/teqblazeUtils/bidderUtils.ts';
+
+const BIDDER_CODE = 'SMB';
+const AD_URL = 'http://be.smartbid.ai/pbjs';
+
+export const spec: BidderSpec<typeof BIDDER_CODE> = {
+  code: BIDDER_CODE,
+  supportedMediaTypes: [BANNER, VIDEO, NATIVE],
+
+  isBidRequestValid: isBidRequestValid(),
+  buildRequests: buildRequests(AD_URL),
+  interpretResponse,
+};
+
+registerBidder(spec);
