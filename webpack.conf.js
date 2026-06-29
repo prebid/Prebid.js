33aef4e84c942d7ccb273e1d96f9f509f92ec753
Demetrio Girardi <dgirardi@prebid.org>
2026-06-29 10:05:02 -0700

    usage, not entry

--- webpack.conf.js
+++ webpack.conf.js
@@ -85,7 +85,7 @@ module.exports = {
                         [
                           '@babel/preset-env',
                           {
-                            'useBuiltIns': 'entry',
+                            'useBuiltIns': 'usage',
                             'corejs': '3.42.0',
                             'modules': 'commonjs',
                             'targets': {
