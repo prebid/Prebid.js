938236bcb1fead5915a043316f19b8b703ba2e49
Patrick McCann <patmmccann@gmail.com>
2026-08-04 23:06:29 -0400

    Clean up intentIqIdSystem.d.ts by removing import
    Removed unused import statement from the type definition file.

--- modules/intentIqIdSystem.d.ts
+++ modules/intentIqIdSystem.d.ts
@@ -1,6 +1,3 @@
-// the augmentation in this file only applies where the spec is part of the program
-import type {} from './userId/spec.js';
-
 export type IntentIqIdSystemModuleName = 'intentIqId';
 
 /**
