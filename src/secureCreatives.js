5860d5ef8d41a56f6da80e89066db01836f2f561
Demetrio Girardi <dgirardi@prebid.org>
2026-09-15 07:44:50 -0700

    give precedence to marked slots

--- src/secureCreatives.js
+++ src/secureCreatives.js
@@ -11,7 +11,12 @@ import { getCreativeRendererSource, PUC_MIN_VERSION } from './creativeRenderers.
 import { PbPromise } from './utils/promise.js';
 import { getAdUnitElement } from './utils/adUnits.js';
 import { auctionManager } from './auctionManager.js';
-import { getSlotTargeting, getSlotTargetingKeys, slotHasTargetedAdId } from './utils/gptTargeting.js';
+import {
+  findSlotElementIdByAdId,
+  getSlotTargeting,
+  getSlotTargetingKeys,
+  slotHasTargetedAdId
+} from './utils/gptTargeting.js';
 
 const { REQUEST, RESPONSE, NATIVE, EVENT } = MESSAGES;
 
@@ -204,8 +209,11 @@ export function resizeRemoteCreative({ instl, element, adId, adUnitCode, width, 
   }
 
   function getDfpElementId(adId) {
+    const dfpId = findSlotElementIdByAdId(adId);
+    if (dfpId) {
+      return dfpId;
+    }
     const slot = window.googletag.pubads().getSlots().find(slot => {
-      if (slotHasTargetedAdId(slot, adId)) return true;
       return getSlotTargetingKeys(slot).find(key => {
         return getSlotTargeting(slot, key).includes(adId);
       });
