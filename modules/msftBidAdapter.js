748b4a48076908242cc3e8b8cdf5932726b37d79
Jason Snellbaker <jsnellbaker@microsoft.com>
2026-09-22 09:55:00 -0400

    msft Bid Adapter - fix support for addtl_consent

--- modules/msftBidAdapter.js
+++ modules/msftBidAdapter.js
@@ -205,6 +205,16 @@ const converter = ortbConverter({
       });
     }
 
+    if (bidderRequest?.gdprConsent?.addtlConsent && bidderRequest.gdprConsent.addtlConsent.indexOf('~') !== -1) {
+      const ac = bidderRequest.gdprConsent.addtlConsent;
+      // pull only the ids from the string (after the ~) and convert them to an array of ints
+      const acStr = ac.substring(ac.indexOf('~') + 1);
+      const addtlConsent = acStr.split('.').map(id => parseInt(id, 10)).filter(id => !isNaN(id));
+      if (addtlConsent.length > 0) {
+        deepSetValue(request, 'user.ext.addtl_consent', addtlConsent);
+      }
+    }
+
     const extANData = {
       prebid: true,
       hb_source: 1,
